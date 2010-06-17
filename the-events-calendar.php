37cc683142d7f43ce1bc16caa5c940919983ee1f
Matt Wiebe <matt@shaneandpeter.com>
2010-06-17 21:02:50 +0000

    graceful version checking for PHP and WP.
    git-svn-id: https://central.tri.be/svn/premium-plugins/code/events/trunk@52 75e66736-2afa-4fbe-8bd5-dce959cd71f9

--- the-events-calendar.php
+++ the-events-calendar.php
@@ -2,7 +2,7 @@
 /*
  Plugin Name:  Events Calendar Premium
  Plugin URI: http://wordpress.org/extend/plugins/the-events-calendar/
- Description:  The Events Calendar plugin enables you to rapidly create and manage events using the post editor. Features include optional Eventbrite integration, Google Maps integration as well as default calendar grid and list templates for streamlined one click installation. When updating The Events Calendar, if EventBrite for The Events Calendar is being used, the two plugins must be updated together. Requires PHP 5.1 or above. 
+ Description:  The Events Calendar plugin enables you to rapidly create and manage events using the post editor. Features include optional Eventbrite integration, Google Maps integration as well as default calendar grid and list templates for streamlined one click installation. When updating The Events Calendar, if EventBrite for The Events Calendar is being used, the two plugins must be updated together. Requires WordPress 3.0 (or higher) and PHP 5.1 (or above). 
  Version: 1.0
  Author: Shane & Peter, Inc.
  Author URI: http://www.shaneandpeter.com/
@@ -11,39 +11,42 @@
 
 
 register_activation_hook(__FILE__, 'the_events_calendar_activate');
-add_action( 'admin_head', 'the_events_calendar_version_check' );
 
-if ( ! function_exists('the_events_calendar_version_check') ) {
-	function the_events_calendar_version_check() {
-	  if ( version_compare( PHP_VERSION, '5.1', '<') ) { 
-	    echo "<div class='error'>The Events Calendar requires PHP 5.1 or greater.  Please de-activate The Events Calendar.</div>";
-	    }   
-	}
-}
+define( 'SP_EVENTS_SUPPORTED_WP_VERSION', version_compare(get_bloginfo("version"), '3.0', '>=') );
+define( 'SP_EVENTS_SUPPORTED_PHP_VERSION', version_compare( phpversion(), '5.1', '>=') );
 
 
 if ( ! function_exists('the_events_calendar_activate') ) {
 	function the_events_calendar_activate() {
-		global $wp_version;
-	    if ( version_compare( phpversion(), '5.1', '<' ) || version_compare( $wp_version, '3', '<' ) ) { 
-	        trigger_error('', E_USER_ERROR);
-	    } else {
+		if ( SP_EVENTS_SUPPORTED_WP_VERSION && SP_EVENTS_SUPPORTED_PHP_VERSION ) {
+			the_events_calendar_load();
+			global $spEvents;
+			$spEvents->on_activate();
+		}
+	}
+}
+
+if ( ! function_exists('the_events_calendar_load') ) {
+	function the_events_calendar_load() {
+		if ( SP_EVENTS_SUPPORTED_WP_VERSION && SP_EVENTS_SUPPORTED_PHP_VERSION ) {
 			require_once(dirname(__FILE__) . "/the-events-calendar.class.php");
 			require_once(dirname(__FILE__) . "/the-events-calendar-exception.class.php");
 			require_once(dirname(__FILE__) . "/events-calendar-widget.class.php");
 			require_once(dirname(__FILE__) . "/events-list-widget.class.php");
 			require_once(dirname(__FILE__) . "/template-tags.php");
-			global $spEvents;
-			$spEvents->on_activate();
-	    }   
+		}
 	}
 }
 
-global $wp_version;
-if (version_compare( phpversion(), '5.1', '>=') || version_compare( $wp_version, '3.0', '>=' ) ) {
-	require_once(dirname(__FILE__) . "/the-events-calendar.class.php");
-	require_once(dirname(__FILE__) . "/the-events-calendar-exception.class.php");
-	require_once(dirname(__FILE__) . "/events-calendar-widget.class.php");
-	require_once(dirname(__FILE__) . "/events-list-widget.class.php");
-	require_once(dirname(__FILE__) . "/template-tags.php");
+the_events_calendar_load();
+
+add_action('admin_head', 'sp_events_notices');
+function sp_events_notices() {
+	if ( ! SP_EVENTS_SUPPORTED_WP_VERSION ) {
+		echo "<div class='error'><p>Events Calendar Premium requires WordPress 3.0 or higher. Please upgrade WordPress or deactivate Events Calendar Premium.</p></div>";
+	}
+	
+	if ( ! SP_EVENTS_SUPPORTED_PHP_VERSION ) {
+		echo "<div class='error'><p>Events Calendar Premium requires PHP 5.1 or higher. Talk to your Web host about not living in the past.</p></div>";
+	}
 }
