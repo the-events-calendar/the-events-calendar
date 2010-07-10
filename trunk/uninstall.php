6a00f54e9bccfdeb98b4e8f4a1f66533b16eea3a
Matt Wiebe <matt@shaneandpeter.com>
2010-07-10 21:30:29 +0000

    remove improper tagging
    git-svn-id: https://central.tri.be/svn/premium-plugins/code/events/tags/1.1@95 75e66736-2afa-4fbe-8bd5-dce959cd71f9

--- trunk/uninstall.php
+++
@@ -1,4 +0,0 @@
-<?php
-delete_option( 'sp_events_calendar_options' );
-global $wp_rewrite;
-$wp_rewrite->flush_rules();
--- trunk/views/datepicker.php
+++
@@ -1,9 +0,0 @@
-<form action="<?php echo sp_get_dropdown_link_prefix(); ?>" method="get" id="<?php echo $prefix; ?>events-picker">
-	<select id='<?php echo $prefix; ?>events-month' name='EventJumpToMonth' class='<?php echo $prefix; ?>events-dropdown'>
-		<?php echo $monthOptions; ?>
-	</select>
-	<select id='<?php echo $prefix; ?>events-year' name='EventJumpToYear' class='<?php echo $prefix; ?>events-dropdown'>
-		<?php echo $yearOptions; ?>
-	</select>
-	<noscript><input type="submit" value="&rarr;"></noscript>
-</form>
