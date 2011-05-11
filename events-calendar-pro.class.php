6df2296c7d79b756097fd9df4dba0041cd5e609e
John Gadbois <jgadbois@gmail.com>
2011-05-11 13:00:21 +0000

    #10772 - unescape html entities for ical
    git-svn-id: https://central.tri.be/svn/premium-plugins/code/events/trunk@500 75e66736-2afa-4fbe-8bd5-dce959cd71f9

--- events-calendar-pro.class.php
+++ events-calendar-pro.class.php
@@ -2630,7 +2630,7 @@ if ( !class_exists( 'Events_Calendar_Pro' ) ) {
 		        $events .= "UID:" . $eventPost->ID . "@" . $blogHome . "\n";
 		        $events .= "SUMMARY:" . $eventPost->post_title . "\n";				
 		        $events .= "DESCRIPTION:" . str_replace(",",'\,',$description) . "\n";
-				$events .= "LOCATION:" . sp_get_full_address( $eventPost->ID, true ) . "\n";
+				$events .= "LOCATION:" . html_entity_decode(sp_get_full_address( $eventPost->ID, true ), ENT_QUOTES) . "\n";
 				$events .= "URL:" . get_permalink( $eventPost->ID ) . "\n";
 		        $events .= "END:VEVENT\n";
 			}
