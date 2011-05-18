2438a7c8a871b65c5af4b36d84d26d08f7d70233
John Gadbois <jgadbois@gmail.com>
2011-05-18 13:55:18 +0000

    #10220 - Forgot to add a file.
    git-svn-id: https://central.tri.be/svn/premium-plugins/code/events/branches/2.0@520 75e66736-2afa-4fbe-8bd5-dce959cd71f9

---
+++ views/recurrence-dialog.php
@@ -0,0 +1,5 @@
+<div id="recurring-dialog"  title="Saving Recurring Event" style="display: none;">
+	Do you wish to update<br/>
+	<input type='radio' value="<?php echo Events_Recurrence_Meta::UPDATE_TYPE_SINGLE ?>" name="events_to_update" checked/> Only this event<br/>
+	<input type='radio' value="<?php echo Events_Recurrence_Meta::UPDATE_TYPE_FUTURE ?>" name="events_to_update"/> Future Events</p>
+</div>
