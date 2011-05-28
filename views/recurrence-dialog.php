415f150d61a2b099bb2d7009d59e8615f81acdf5
John Gadbois <jgadbois@gmail.com>
2011-05-28 12:25:50 +0000

    #10220 - i18n recurrence text
    git-svn-id: https://central.tri.be/svn/premium-plugins/code/events/branches/2.0@535 75e66736-2afa-4fbe-8bd5-dce959cd71f9

--- views/recurrence-dialog.php
+++ views/recurrence-dialog.php
@@ -1,6 +1,6 @@
 <div id="recurring-dialog"  title="Saving Recurring Event" style="display: none;">
-	Which events do you wish to update?<br/>
+	<?php _e('Which events do you wish to update?',$this->pluginDomain); ?><br/>
 </div>
 <div id="deletion-dialog"  title="Delete Recurring Event" style="display: none;" data-start="<?php echo $_REQUEST['event_start'] ?>" data-post="<?php echo $_REQUEST['post'] ?>">
-	Select your desired action<br/>
+	<?php _e('Select your desired action',$this->pluginDomain); ?><br/>
 </div>
