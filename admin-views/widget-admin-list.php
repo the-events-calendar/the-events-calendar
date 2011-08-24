5b8cf8b456993fa4285d860b1ec03df7650dcaad
John Gadbois <jgadbois@gmail.com>
2011-08-24 13:15:52 +0000

    Fixed some includes in the widget templates
    git-svn-id: https://central.tri.be/svn/premium-plugins/code/events/branches/2.0@751 75e66736-2afa-4fbe-8bd5-dce959cd71f9

--- admin-views/widget-admin-list.php
+++ admin-views/widget-admin-list.php
@@ -8,12 +8,12 @@ if ( !defined('ABSPATH') ) { die('-1'); }
 
 ?>
 <p>
-	<label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e('Title:',self::PLUGIN_DOMAIN);?></label>
+	<label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e('Title:',TribeEvents::PLUGIN_DOMAIN);?></label>
 	<input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo $instance['title']; ?>" />
 </p>
 
 <p>
-	<label for="<?php echo $this->get_field_id( 'limit' ); ?>"><?php _e('Show:',self::PLUGIN_DOMAIN);?></label>
+	<label for="<?php echo $this->get_field_id( 'limit' ); ?>"><?php _e('Show:',TribeEvents::PLUGIN_DOMAIN);?></label>
 	<select id="<?php echo $this->get_field_id( 'limit' ); ?>" name="<?php echo $this->get_field_name( 'limit' ); ?>" class="widefat">
 	<?php for ($i=1; $i<=10; $i++)
 	{?>
@@ -21,32 +21,32 @@ if ( !defined('ABSPATH') ) { die('-1'); }
 	<?php } ?>							
 	</select>
 </p>
-	<label for="<?php echo $this->get_field_id( 'no_upcoming_events' ); ?>"><?php _e('Show widget only if there are upcoming events:',self::PLUGIN_DOMAIN);?></label>
+	<label for="<?php echo $this->get_field_id( 'no_upcoming_events' ); ?>"><?php _e('Show widget only if there are upcoming events:',TribeEvents::PLUGIN_DOMAIN);?></label>
 	<input id="<?php echo $this->get_field_id( 'no_upcoming_events' ); ?>" name="<?php echo $this->get_field_name( 'no_upcoming_events' ); ?>" type="checkbox" <?php checked( $instance['no_upcoming_events'], 1 ); ?> value="1" />
 <p> 
 
 </p>
 
-<p><?php _e( 'Display:', self::PLUGIN_DOMAIN ); ?><br />
+<p><?php _e( 'Display:', TribeEvents::PLUGIN_DOMAIN ); ?><br />
 
 <?php $displayoptions = array (
-					"start" => __('Start Date & Time', self::PLUGIN_DOMAIN) .'<small><br/>'.__('(Widget will always show start date)', self::PLUGIN_DOMAIN).'</small>',
-					"end" => __("End Date & Time", self::PLUGIN_DOMAIN),
-					"venue" => __("Venue", self::PLUGIN_DOMAIN),
-					"address" => __("Address", self::PLUGIN_DOMAIN),
-					"city" => __("City", self::PLUGIN_DOMAIN),
-					"region" => __("State (US) Or Province (Int)", self::PLUGIN_DOMAIN),
-					"zip" => __("Postal Code", self::PLUGIN_DOMAIN),
-					"country" => __("Country", self::PLUGIN_DOMAIN),
-					"phone" => __("Phone", self::PLUGIN_DOMAIN),
-					"cost" => __("Price", self::PLUGIN_DOMAIN),
+					"start" => __('Start Date & Time', TribeEvents::PLUGIN_DOMAIN) .'<small><br/>'.__('(Widget will always show start date)', TribeEvents::PLUGIN_DOMAIN).'</small>',
+					"end" => __("End Date & Time", TribeEvents::PLUGIN_DOMAIN),
+					"venue" => __("Venue", TribeEvents::PLUGIN_DOMAIN),
+					"address" => __("Address", TribeEvents::PLUGIN_DOMAIN),
+					"city" => __("City", TribeEvents::PLUGIN_DOMAIN),
+					"region" => __("State (US) Or Province (Int)", TribeEvents::PLUGIN_DOMAIN),
+					"zip" => __("Postal Code", TribeEvents::PLUGIN_DOMAIN),
+					"country" => __("Country", TribeEvents::PLUGIN_DOMAIN),
+					"phone" => __("Phone", TribeEvents::PLUGIN_DOMAIN),
+					"cost" => __("Price", TribeEvents::PLUGIN_DOMAIN),
 				);
 	foreach ($displayoptions as $option => $label) { ?>
 		<input class="checkbox" type="checkbox" <?php checked( $instance[$option], true ); ?> id="<?php echo $this->get_field_id( $option ); ?>" name="<?php echo $this->get_field_name( $option ); ?>" style="margin-left:5px"/>
 		<label for="<?php echo $this->get_field_id( $option ); ?>"><?php echo $label ?></label>
 		<br/>
 <?php } ?>
-	<label for="<?php echo $this->get_field_id( 'category' ); ?>"><?php _e('Category:',self::PLUGIN_DOMAIN);?>
+	<label for="<?php echo $this->get_field_id( 'category' ); ?>"><?php _e('Category:',TribeEvents::PLUGIN_DOMAIN);?>
 		<?php 
 
 			echo wp_dropdown_categories( array(
@@ -60,4 +60,4 @@ if ( !defined('ABSPATH') ) { die('-1'); }
 			));
 		?>
 </p>
-<p><small><em><?php _e('If you wish to customize the widget display yourself, see the file views/events-list-load-widget-display.php inside the Events Premium plugin.', self::PLUGIN_DOMAIN);?></em></small></p>
+<p><small><em><?php _e('If you wish to customize the widget display yourself, see the file views/events-list-load-widget-display.php inside the Events Premium plugin.', TribeEvents::PLUGIN_DOMAIN);?></em></small></p>
