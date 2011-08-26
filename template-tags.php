e011d61be5b5667cd04af021d20736e48ddb632b
Peter Chester <peter@tri.be>
2011-08-26 23:40:22 +0000

    custom fields template tags
    git-svn-id: https://central.tri.be/svn/premium-plugins/code/events-premium/trunk@762 75e66736-2afa-4fbe-8bd5-dce959cd71f9

--- template-tags.php
+++ template-tags.php
@@ -5,39 +5,46 @@
  */
 if( class_exists( 'TribeEventsPro' ) && !function_exists( 'tribe_get_recurrence_text' ) ) {	
 	/**
-	 * tribe_get_recurrence_text 
-	 *
 	 * Get the textual version of event recurrence
 	 * e.g Repeats daily for three days 
 	 */
 	function tribe_get_recurrence_text( $postId = null )  {
-		$tribe_ecp = TribeEvents::instance();
 		$postId = tribe_post_id_helper( $postId );
-	   return apply_filters( 'tribe_get_recurrence_text', TribeEventsRecurrenceMeta::recurrenceToText( $postId ) );
+		$tribe_ecp = TribeEvents::instance();
+	  	return apply_filters( 'tribe_get_recurrence_text', TribeEventsRecurrenceMeta::recurrenceToText( $postId ) );
 	}
-
 	
-    /**
-     * tribe_event_meta
-     *
-	 * Get the custom meta for a particular event and return via template tags
-     * 
-     */
-	function tribe_event_meta( $postId = null ) {
-	  	$postId = tribe_post_id_helper( $postId );
-      	$customFields = tribe_get_option('custom-fields');
-	  	$meta_html = '';
-	
+	/**
+	 * Get an array of custom fields
+	 *
+	 * @param int $postId 
+	 * @return array $data of custom fields
+	 */
+	function tribe_get_custom_fields( $postId = null ) {
+		$postId = tribe_post_id_helper( $postId );
+	  	$customFields = tribe_get_option('custom-fields');
+	  	$data = array();
 	  	foreach ($customFields as $field) {
-	     	$meta = str_replace('|', ', ', get_post_meta($postId, $field['name'], true));
-	     	if($meta) {
-	     		// $meta is passed through kses
-				$meta_html .= '<dt>' . esc_html($field['label']) . ':</dt><dd>' . $meta . '</dd>';
+		 	$meta = str_replace('|', ', ', get_post_meta($postId, $field['name'], true));
+		 	if($meta) {
+				$data[esc_html($field['label'])] = esc_html($meta);
 			}
 		}
+		return $data;
+	}
 	
-		return $meta_html;
-    }
-	// TODO: create seperate template tags for each custom field
-
+	/**
+	 * Echo a definition term list of custom fields
+	 *
+	 * @param int $postId
+	 */
+	function tribe_the_custom_fields( $postId = null ) {
+		$fields = tribe_get_custom_fields( $postId );
+	  	$meta_html = "<dl class='column'>\n";
+	  	foreach ($fields as $label => $value) {
+			$meta_html .= apply_filters('tribe_the_custom_field',"<dt>$label:</dt><dd>$value</dd>\n",$label,$value);
+		}
+		$meta_html .= "</dl>\n";
+		echo $meta_html;
+	}
 }
