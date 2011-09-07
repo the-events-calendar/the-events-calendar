994c2e0897d3106423e14173f79c7855fa52ffad
Peter Chester <peter@tri.be>
2011-09-07 03:55:23 +0000

    Updates #13105 - fix plugin domain on single-venue.php
    git-svn-id: https://central.tri.be/svn/premium-plugins/code/events-premium/trunk@813 75e66736-2afa-4fbe-8bd5-dce959cd71f9

--- views/single-venue.php
+++ views/single-venue.php
@@ -10,24 +10,24 @@
 if ( !defined('ABSPATH') ) { die('-1'); }
 
 ?>
-<span class="back"><a href="<?php echo tribe_get_events_link(); ?>"><?php _e('&laquo; Back to Events', $tribe_ecp->pluginDomain); ?></a></span>								
+<span class="back"><a href="<?php echo tribe_get_events_link(); ?>"><?php _e('&laquo; Back to Events', TribeEventsPro::PLUGIN_DOMAIN); ?></a></span>								
 <div id="tribe-events-event-meta">
 	<div style='margin: 0 0 10px 0; float: right;'>
 		<?php echo tribe_get_embedded_map(get_the_ID(), '350px', '200px') ?>
 	</div>
 	<dl class="column location" itemprop="location" itemscope itemtype="http://schema.org/Place">
-		<dt><?php _e('Name:', $tribe_ecp->pluginDomain) ?></dt> 
+		<dt><?php _e('Name:', TribeEventsPro::PLUGIN_DOMAIN) ?></dt> 
 			<dd itemprop="name"><?php the_title() ?></dd>
 		<?php if(tribe_get_phone()) : ?>
-		<dt><?php _e('Phone:', $tribe_ecp->pluginDomain) ?></dt> 
+		<dt><?php _e('Phone:', TribeEventsPro::PLUGIN_DOMAIN) ?></dt> 
 			<dd itemprop="telephone"><?php echo tribe_get_phone(); ?></dd>
 		<?php endif; ?>
 		
 		<?php if( tribe_address_exists( get_the_ID() ) ) : ?>
 		<dt>
-			<?php _e('Address:', $tribe_ecp->pluginDomain) ?><br />
+			<?php _e('Address:', TribeEventsPro::PLUGIN_DOMAIN) ?><br />
 			<?php if( get_post_meta( get_the_ID(), '_EventShowMapLink', true ) == 'true' ) : ?>
-				<a class="gmap" itemprop="maps" href="<?php tribe_the_map_link() ?>" title="<?php _e('Click to view a Google Map', $tribe_ecp->pluginDomain); ?>" target="_blank"><?php _e('Google Map', $tribe_ecp->pluginDomain ); ?></a>
+				<a class="gmap" itemprop="maps" href="<?php tribe_the_map_link() ?>" title="<?php _e('Click to view a Google Map', TribeEventsPro::PLUGIN_DOMAIN); ?>" target="_blank"><?php _e('Google Map', TribeEventsPro::PLUGIN_DOMAIN ); ?></a>
 			<?php endif; ?>
 		</dt>
 			<dd>
@@ -59,11 +59,11 @@ if ( !defined('ABSPATH') ) { die('-1'); }
 				<div class="tribe-events-event-list-meta">
 					<table cellspacing="0">
 						<tr>
-							<td class="tribe-events-event-meta-desc"><?php _e('Start:', $tribe_ecp->pluginDomain) ?></td>
+							<td class="tribe-events-event-meta-desc"><?php _e('Start:', TribeEventsPro::PLUGIN_DOMAIN) ?></td>
 							<td class="tribe-events-event-meta-value" itemprop="startDate" content="<?php echo tribe_get_start_date( null, false, 'Y-m-d' ); ?>"><?php echo tribe_get_start_date(); ?></td>
 						</tr>
 						<tr>
-							<td class="tribe-events-event-meta-desc"><?php _e('End:', $tribe_ecp->pluginDomain) ?></td>
+							<td class="tribe-events-event-meta-desc"><?php _e('End:', TribeEventsPro::PLUGIN_DOMAIN) ?></td>
 							<td class="tribe-events-event-meta-value" itemprop="endDate" content="<?php echo tribe_get_end_date( null, false, 'Y-m-d' ); ?>"><?php echo tribe_get_end_date(); ?></td>
 						</tr>
 						<?php
@@ -71,7 +71,7 @@ if ( !defined('ABSPATH') ) { die('-1'); }
 						if ( !empty( $cost ) ) :
 						?>
 						<tr>
-							<td class="tribe-events-event-meta-desc"><?php _e('Cost:', $tribe_ecp->pluginDomain) ?></td>
+							<td class="tribe-events-event-meta-desc"><?php _e('Cost:', TribeEventsPro::PLUGIN_DOMAIN) ?></td>
 							<td class="tribe-events-event-meta-value" itemprop="price"><?php echo $cost; ?></td>
 						</tr>
 						<?php endif; ?>
