80807ac3a02f19c453ae2b510785602f1e9d68c3
Matt Wiebe <matt@shaneandpeter.com>
2011-09-13 22:38:10 +0000

    don't throw warnings when there's no custom fields
    git-svn-id: https://central.tri.be/svn/premium-plugins/code/events-premium/trunk@868 75e66736-2afa-4fbe-8bd5-dce959cd71f9

--- template-tags.php
+++ template-tags.php
@@ -22,11 +22,14 @@ if( class_exists( 'TribeEventsPro' ) && !function_exists( 'tribe_get_recurrence_
 	 */
 	function tribe_get_custom_fields( $postId = null ) {
 		$postId = tribe_post_id_helper( $postId );
-	  	$customFields = tribe_get_option('custom-fields');
-	  	$data = array();
-	  	foreach ($customFields as $field) {
-		 	$meta = str_replace('|', ', ', get_post_meta($postId, $field['name'], true));
-		 	if($meta) {
+		$data = array();
+		$customFields = tribe_get_option('custom-fields', false);
+		if ( ! trim($customFields) ) {
+			return $data;
+		}
+		foreach ($customFields as $field) {
+			$meta = str_replace('|', ', ', get_post_meta($postId, $field['name'], true));
+		if ( $meta ) {
 				$data[esc_html($field['label'])] = $meta; // $meta has been through wp_kses - links are allowed
 			}
 		}
