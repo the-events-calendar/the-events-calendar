6ce285a1baf5091d2b6a12abb7b1916471f47f14
John Gadbois <jgadbois@gmail.com>
2011-09-21 19:52:23 +0000

    #13388 - fix plugin copy
    git-svn-id: https://central.tri.be/svn/premium-plugins/code/events/branches/2.0@944 75e66736-2afa-4fbe-8bd5-dce959cd71f9

--- the-events-calendar.php
+++ the-events-calendar.php
@@ -1,7 +1,7 @@
 <?php
 /*
  Plugin Name: The Events Calendar
- Description: The Events Calendar open source plugin empowers you to rapidly create and manage events using the post editor. Google Maps, microformats for SEO, calendar grid view and event list view, widgets and much more. <a href="http://tribepro.dev.tri.be/events-calendar/features/">Checkout the full feature list</a>. Requires WordPress 3.0 (or higher) and PHP 5.2 (or above). Need more features, peruse the selection of Add-Ons.
+ Description: The Events Calendar open source plugin empowers you to rapidly create and manage events using the post editor. Google Maps, microformats for SEO, calendar grid view and event list view, widgets and much more. <a href="http://tribepro.dev.tri.be/events-calendar/features/">Check out the full feature list</a>. Requires WordPress 3.0 (or higher) and PHP 5.2 (or above). Need more features, peruse the selection of Add-Ons.
  Version: 2.0
  Author: Modern Tribe, Inc.
  Author URI: http://tri.be?ref=tec-plugin
