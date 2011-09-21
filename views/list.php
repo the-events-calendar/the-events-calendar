df760219670455a94350c5d2c166f110e4b8e779
John Gadbois <jgadbois@gmail.com>
2011-09-21 12:28:05 +0000

    #13406 - remove timestamp
    git-svn-id: https://central.tri.be/svn/premium-plugins/code/events/branches/2.0@930 75e66736-2afa-4fbe-8bd5-dce959cd71f9

--- views/list.php
+++ views/list.php
@@ -32,7 +32,7 @@ if ( !defined('ABSPATH') ) { die('-1'); }
                <h4 class="event-day"><?php echo tribe_get_start_date( null, false ); ?></h4>
            <?php endif; ?>
            <?php if ( tribe_is_day() && $first ) : $first = false; ?>
-               <h4 class="event-day"><?php echo tribe_event_format_date(strtotime(get_query_var('eventDate'))); ?></h4>
+               <h4 class="event-day"><?php echo tribe_event_format_date(strtotime(get_query_var('eventDate')), false); ?></h4>
            <?php endif; ?>
 					<?php the_title('<h2 class="entry-title" itemprop="name"><a href="' . tribe_get_event_link() . '" title="' . the_title_attribute('echo=0') . '" rel="bookmark">', '</a></h2>'); ?>
 				<div class="entry-content tribe-events-event-entry" itemprop="description">
