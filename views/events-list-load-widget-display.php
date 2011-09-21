a1dd75335dff4ba3f0dfce3adb5fd98980726768
Elliot Wiltshire <wccoder@gmail.com>
2011-09-21 21:24:41 +0000

    Updates #13430 - Removed microformat layout from listing widget.
    git-svn-id: https://central.tri.be/svn/premium-plugins/code/events/branches/2.0@948 75e66736-2afa-4fbe-8bd5-dce959cd71f9

--- views/events-list-load-widget-display.php
+++ views/events-list-load-widget-display.php
@@ -54,12 +54,7 @@ ob_end_clean();
       ?> 
 	</div>
 	<div class="event">
-		<a href="<?php echo get_permalink($post->ID) ?>"><span class="entry-title"><?php echo $post->post_title ?></span></a>
-		<div class="updated-info">Updated: <span class="date updated"><?php echo $post->post_date ?></span></div>
-		<?php if (tribe_get_organizer_link() != ''): ?>
-		<br />
-		<span class="vcard author"><span class="fn url"><?php echo tribe_get_organizer_link() ?></span></span>
-		<?php endif ?>
+		<a href="<?php echo get_permalink($post->ID) ?>"><?php echo $post->post_title ?></a>
 	</div>
 </li>
 <?php $alt_text = ( empty( $alt_text ) ) ? 'alt' : ''; ?>
