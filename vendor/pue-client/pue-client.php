d5562ff3cae915ab90736b08bd22910a4846a1ad
Peter Chester <peter@tri.be>
2011-09-30 19:27:40 +0000

    update nomenclature in pue client
    git-svn-id: https://central.tri.be/svn/premium-plugins/code/events-premium/trunk@985 75e66736-2afa-4fbe-8bd5-dce959cd71f9

--- vendor/pue-client/pue-client.php
+++ vendor/pue-client/pue-client.php
@@ -6,7 +6,7 @@
 // Don't load directly
 if ( !defined('ABSPATH') ) { die('-1'); }
 
-require_once('classes/pue_plugin_update_engine.class.php');
-require_once('classes/pue_plugin_info.class.php');
-require_once('classes/pue_plugin_update_utility.class.php');
+require_once('lib/pue_plugin_update_engine.class.php');
+require_once('lib/pue_plugin_info.class.php');
+require_once('lib/pue_plugin_update_utility.class.php');
 ?>
