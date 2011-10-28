aa731591d0451f41fa0e161f2498cb65b91cb76f
Peter Chester <peter@tri.be>
2011-10-28 03:44:02 +0000

    fix spacing
    git-svn-id: https://central.tri.be/svn/premium-plugins/code/events-premium/trunk@1134 75e66736-2afa-4fbe-8bd5-dce959cd71f9

--- events-calendar-pro.php
+++ events-calendar-pro.php
@@ -363,9 +363,9 @@ if ( !class_exists( 'TribeEventsPro' ) ) {
 	// Instantiate class and set up WordPress actions.
    function Tribe_ECP_Load() {
       if( class_exists( 'TribeEvents' ) && defined('TribeEvents::VERSION') && version_compare( TribeEvents::VERSION, '2.0', '>=') ) {
-         TribeEventsPro::instance();
+		TribeEventsPro::instance();
       } else {
-			add_action( 'admin_notices', 'tribe_show_fail_message' );
+		add_action( 'admin_notices', 'tribe_show_fail_message' );
       }
    }
 
