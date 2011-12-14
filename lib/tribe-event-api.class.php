bdedfda075c8c8b4696947d24024238ed5d07ac7
Joey Kudish <tribe@jkudish.com>
2011-12-14 00:18:32 +0000

    Updated #13965, fixed saving bugs with organizers, venues and provinces
    git-svn-id: https://central.tri.be/svn/premium-plugins/code/events/branches/2.1@1274 75e66736-2afa-4fbe-8bd5-dce959cd71f9

--- lib/tribe-event-api.class.php
+++ lib/tribe-event-api.class.php
@@ -9,7 +9,7 @@ if ( !defined('ABSPATH') ) { die('-1'); }
 if (!class_exists('TribeEventsAPI')) {
 	class TribeEventsAPI {
       public static $valid_venue_keys = array(
-         'Venue', 'Address', 'City', 'Province', 'State', 'StateProvince', 'Zip', 'Phone'
+         'Venue', 'Address', 'City', 'Province', 'State', 'StateProvince', 'Province', 'Zip', 'Phone'
       );
 
       public static $valid_organizer_keys = array(
@@ -242,10 +242,10 @@ if (!class_exists('TribeEventsAPI')) {
 		 */
 		private static function saveVenueMeta($venueId, $data) {
 			// TODO: We should probably do away with 'StateProvince' and stick to 'State' and 'Province'.
-			if (!isset($data['StateProvince'])) {
-				if (isset($data['State'])) {
+			if (!isset($data['StateProvince']) || $data['StateProvince'] == '') {
+				if (isset($data['State']) && $data['State'] != '') {
 					$data['StateProvince'] = $data['State'];
-				} else if(isset($data['Province'])) {
+				} else if(isset($data['Province'])  && $data['Province'] != '') {
 					$data['StateProvince'] = $data['Province'];					
 				}
 			}
