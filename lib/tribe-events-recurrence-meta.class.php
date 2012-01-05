fa36a526bd35a1803e634380fc26279b89ecf031
Joey Kudish <tribe@jkudish.com>
2012-01-05 22:24:48 +0000

    Updates #14325 manually fix a merge that didn't work properly
    git-svn-id: https://central.tri.be/svn/premium-plugins/code/events-premium/trunk@1347 75e66736-2afa-4fbe-8bd5-dce959cd71f9

--- lib/tribe-events-recurrence-meta.class.php
+++ lib/tribe-events-recurrence-meta.class.php
@@ -13,14 +13,15 @@ class TribeEventsRecurrenceMeta {
 	public static function init() {
 		add_action( 'tribe_events_update_meta', array( __CLASS__, 'updateRecurrenceMeta' ), 1, 3 );
 		add_action( 'tribe_events_date_display', array( __CLASS__, 'loadRecurrenceData' ) );
-		add_action('trash_post', array( __CLASS__, 'deleteRecurringEvent'));		
+		add_action(	'trash_post', array( __CLASS__, 'deleteRecurringEvent') ); // WP 3.2 and older
+		add_action(	'wp_trash_post', array( __CLASS__, 'deleteRecurringEvent') ); // WP 3.3 and newer
 
-		add_action('pre_post_update', array( __CLASS__, 'maybeBreakFromSeries' ));
+		add_action(	'pre_post_update', array( __CLASS__, 'maybeBreakFromSeries' ) );
 		add_action( 'admin_notices', array( __CLASS__, 'showRecurrenceErrorFlash') );
-		add_action( 'tribe_recurring_event_error', array( __CLASS__, 'setupRecurrenceErrorMsg'), 10, 2);
+		add_action( 'tribe_recurring_event_error', array( __CLASS__, 'setupRecurrenceErrorMsg'), 10, 2 );
 
-      add_filter( 'tribe_get_event_link', array( __CLASS__, 'addDateToEventPermalink'), 10, 2);
-      add_filter( 'post_row_actions', array( __CLASS__, 'removeQuickEdit'), 10, 2);
+    add_filter( 'tribe_get_event_link', array( __CLASS__, 'addDateToEventPermalink'), 10, 2 );
+    add_filter( 'post_row_actions', array( __CLASS__, 'removeQuickEdit'), 10, 2 );
 	}	
 
    public static function removeQuickEdit( $actions, $post ) {
