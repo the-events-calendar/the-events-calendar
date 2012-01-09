59ff667fa9cb5a018c67b706129f0a254cb7c9a7
Joey Kudish <tribe@jkudish.com>
2012-01-09 20:36:17 +0000

    Update description + readme from WP.org repo
    git-svn-id: https://central.tri.be/svn/premium-plugins/code/events/tags/2.0.3@1369 75e66736-2afa-4fbe-8bd5-dce959cd71f9

--- the-events-calendar.php
+++ the-events-calendar.php
@@ -1,7 +1,7 @@
 <?php
 /*
  Plugin Name: The Events Calendar
- Description: The Events Calendar is a fully featured event management system with multiple views, 3rd party integrations, and a slew of premium add-ons. <a href="http://tri.be/wordpress-events-calendar/?ref=tec-plugin">Check out the full feature list</a>.
+ Description: The Events Calendar is a fully featured event management system with multiple views, 3rd party integrations, and a slew of premium add-ons.
  Version: 2.0.3
  Author: Modern Tribe, Inc.
  Author URI: http://tri.be?ref=tec-plugin
