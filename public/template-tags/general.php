dc9d67ac41662839edf511d00c4b1c7cb82c7e90
Paul Hughes <supersmash@gmail.com>
2012-01-17 13:38:52 +0000

    Updates #13704 Made the generic classes added that were unclear more unique to tribe by adding the tribe- prefix.
    git-svn-id: https://central.tri.be/svn/premium-plugins/code/events/branches/2.1@1407 75e66736-2afa-4fbe-8bd5-dce959cd71f9

--- public/template-tags/general.php
+++ public/template-tags/general.php
@@ -105,7 +105,7 @@ if( class_exists( 'TribeEvents' ) ) {
 		if( !$label ) { $label = __('Category:', 'tribe-events-calendar'); }
 
 		$tribe_ecp = TribeEvents::instance();
-		apply_filters('tribe_meta_event_cats', the_terms( get_the_ID(), $tribe_ecp->get_event_taxonomy(), '<dt class="category-label">'.$label.'</dt><dd class="category-meta">', $separator, '</dd>' ));
+		apply_filters('tribe_meta_event_cats', the_terms( get_the_ID(), $tribe_ecp->get_event_taxonomy(), '<dt class="tribe-category-label">'.$label.'</dt><dd class="tribe-category-meta">', $separator, '</dd>' ));
 	}
 
 	/**
@@ -122,7 +122,7 @@ if( class_exists( 'TribeEvents' ) ) {
 		if( !$label ) { $label = __('Tags:', 'tribe-events-calendar'); }
 
 		$tribe_ecp = TribeEvents::instance();
-		apply_filters('tribe_meta_event_tags', the_terms( get_the_ID(), 'post_tag', '<dt class="tag-label">'.$label.'</dt><dd class="tag-meta">', $separator, '</dd>' ));
+		apply_filters('tribe_meta_event_tags', the_terms( get_the_ID(), 'post_tag', '<dt class="tribe-tag-label">'.$label.'</dt><dd class="tribe-tag-meta">', $separator, '</dd>' ));
 	}
 
 	/**
