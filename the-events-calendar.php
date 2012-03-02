1cc869996ad05670cff084b816359da32d0bdffd
Joey Kudish <tribe@jkudish.com>
2012-03-02 23:23:52 +0000

    Updates #15383 License adjustments
    git-svn-id: https://central.tri.be/svn/premium-plugins/code/events/trunk@1697 75e66736-2afa-4fbe-8bd5-dce959cd71f9

--- the-events-calendar.php
+++ the-events-calendar.php
@@ -10,6 +10,8 @@ License: GPLv2 or later
 */
 
 /*
+Copyright 2009-2012 by Modern Tribe Inc and the contributors
+
 This program is free software; you can redistribute it and/or
 modify it under the terms of the GNU General Public License
 as published by the Free Software Foundation; either version 2
@@ -24,6 +26,7 @@ You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 */
+
 
 
 require_once( dirname(__FILE__) . '/lib/the-events-calendar.class.php' );
