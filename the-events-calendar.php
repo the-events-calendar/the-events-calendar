903fdd3a954af9a6f60db71169da8ed4b4443562
Nick Ciske <tribe@thoughtrefinery.com>
2012-03-02 19:41:51 +0000

    License
    git-svn-id: https://central.tri.be/svn/premium-plugins/code/events/trunk@1693 75e66736-2afa-4fbe-8bd5-dce959cd71f9

--- the-events-calendar.php
+++ the-events-calendar.php
@@ -1,12 +1,30 @@
 <?php
 /*
- Plugin Name: The Events Calendar
- Description: The Events Calendar is a fully featured event management system with multiple views, 3rd party integrations, and a slew of premium add-ons.
- Version: 2.0.4
- Author: Modern Tribe, Inc.
- Author URI: http://tri.be?ref=tec-plugin
- Text Domain: tribe-events-calendar
- */
+Plugin Name: The Events Calendar
+Description: The Events Calendar is a fully featured event management system with multiple views, 3rd party integrations, and a slew of premium add-ons.
+Version: 2.0.4
+Author: Modern Tribe, Inc.
+Author URI: http://tri.be?ref=tec-plugin
+Text Domain: tribe-events-calendar
+License: GPLv2 or later
+*/
+
+/*
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+
 
 require_once( dirname(__FILE__) . '/lib/the-events-calendar.class.php' );
 
