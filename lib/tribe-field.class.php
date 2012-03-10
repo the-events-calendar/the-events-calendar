cf06c29cc980a276ce3641e462fa9f6b90be8f66
Joey Kudish <tribe@jkudish.com>
2012-03-10 20:31:57 +0000

    Updates #15354 Finished all the field types
    git-svn-id: https://central.tri.be/svn/premium-plugins/code/events/branches/2.0.5@1757 75e66736-2afa-4fbe-8bd5-dce959cd71f9

--- lib/tribe-field.class.php
+++ lib/tribe-field.class.php
@@ -27,9 +27,9 @@ if ( !class_exists('TribeField') ) {
 				'class' => null,
 				'label' => null,
 				'tooltip' => null,
-				'size' => 'small',
+				'size' => 'medium',
 				'html' => null,
-				'error' => null,
+				'error' => false,
 				'value' => null,
 				'options' => null,
 				'display_callback' => null,
@@ -39,11 +39,11 @@ if ( !class_exists('TribeField') ) {
 				'heading',
 				'html',
 				'text',
+				'textarea',
 				'radio',
 				'checkbox_bool',
 				'dropdown',
 				'dropdown_chosen',
-				'textarea',
 				'license_key',
 			);
 
@@ -53,6 +53,20 @@ if ( !class_exists('TribeField') ) {
 			$args = wp_parse_args($field, $this->defaults);
 			extract($args);
 
+			// sanitize the values just to be safe
+			$id = esc_attr($id);
+			$type = esc_attr($type);
+			$name = esc_attr($name);
+			$class = sanitize_html_class($class);
+			$label = esc_attr($label);
+			$tooltip = esc_attr($tooltip);
+			$size = esc_attr($size);
+			$html = esc_html($html);
+			$error = (bool) $error;
+			$display_callback = esc_attr($display_callback);
+
+
+			// set the ID
 			$this->id = apply_filters( 'tribe_field_id', $id );
 
 			// set each instance variable and filter
@@ -69,7 +83,8 @@ if ( !class_exists('TribeField') ) {
 				call_user_func($this->display_callback);
 			} elseif ( in_array($this->type, $this->valid_field_types) ) {
 				$field = call_user_method($this->type, $this);
-				echo apply_filters('tribe_field_'.$this->type.'_'.$this->id, $field, $this->id, $this);
+				$field = apply_filters( 'tribe_field_'.$this->type, $field, $this->id, $this );
+				echo apply_filters( 'tribe_field_'.$this->type.'_'.$this->id, $field, $this->id, $this );
 			} else {
 				TribeEvents::debug( __('Invalid field type specified', 'tribe-events-calendar'), $this->type, 'notice');
 			}
@@ -77,68 +92,92 @@ if ( !class_exists('TribeField') ) {
 
 		public function doFieldStart() {
 			$return = '<fieldset id="tribe-field-'.$this->id.'"';
-			$return .= ($this->class) ? ' class="'.$this->class.'"' : '';
+			$return .= ' class="tribe-field tribe-field-'.$this->type;
+			$return .= ($this->error) ? 'tribe-error' : '';
+			$return .= ($this->class) ? ' '.$this->class.'"' : '';
 			$return .= '>';
-			return apply_filters('tribe_field_start', $return, $this->id, $this->class, $this);
+			return apply_filters( 'tribe_field_start', $return, $this->id, $this->type, $this->error, $this->class, $this );
 		}
 
 		public function doFieldEnd() {
 			$return = '</fieldset>';
-			return apply_filters('tribe_field_end', $return, $this->id, $this);
+			return apply_filters( 'tribe_field_end', $return, $this->id, $this );
 		}
 
 		public function doFieldLabel() {
 			$return = '';
 			if ($this->label)
 				$return = '<legend class="tribe-field-label">'.$this->label.'</legend>';
-			return apply_filters('tribe_field_label', $return, $this->label, $this);
+			return apply_filters( 'tribe_field_label', $return, $this->label, $this );
 		}
 
 		public function doToolTip() {
 			$return = '';
 			if ($this->tooltip)
 				$return = ' title="'.$this->tooltip.'"';
-			return apply_filters('tribe_field_tooltip', $return, $this->tooltip, $this);
+			return apply_filters( 'tribe_field_tooltip', $return, $this->tooltip, $this );
 		}
 
 		public function doScreenReaderLabel() {
 			$return = '';
 			if ($this->tooltip)
 				$return = '<label class="screen-reader-text">'.$this->tooltip.'</label>';
-			return apply_filters('tribe_field_screen_reader_label', $return, $this->tooltip, $this);
+			return apply_filters( 'tribe_field_screen_reader_label', $return, $this->tooltip, $this );
 		}
 
 		public function doFieldValue() {
 			$return = '';
 			if ($this->value)
 				$return = ' value="'.$this->value.'"';
-			return apply_filters('tribe_field_value', $return, $this->value, $this);
+			return apply_filters( 'tribe_field_value', $return, $this->value, $this );
+		}
+
+		public function doFieldName() {
+			$return = '';
+			if ($this->name)
+				$return = ' name="'.$this->name.'"';
+			return apply_filters( 'tribe_field_name', $return, $this->name, $this );
 		}
 
 		public function heading() {
-			$field = '<h3>'.esc_attr($this->label).'</h3>';
+			$field = '<h3>'.$this->label.'</h3>';
 			return $field;
 		}
 
 		public function html() {
-			$field = esc_html($this->html);
+			$field = $this->html;
 			return $field;
 		}
 
 		public function text() {
 			switch ($this->size) {
-				case 'large' : $size = '10'; break;
-				case 'medium' : $size = '6'; break;
+				case 'large' : $size = '118'; break;
+				case 'medium' : $size = '30'; break;
 				case 'small'; default : $size = '4'; break;
 			}
 			$field = $this->doFieldStart();
 			$field .= $this->doFieldLabel();
-			$field .= '<input ';
-			$field .= ( isset($this->error) ) ? 'class="tribe-error"' : '';
-			$field .= 'type="text" name="'.$this->name.'" size="'.$size.'"';
+			$field .= '<input';
+			$field .= ' type="text"';
+			$field .= $this->doFieldName();
+			$field .= ' size="'.$size.'"';
 			$field .= $this->doFieldValue();
 			$field .= $this->doToolTip();
 			$field .= '/>';
+			$field .= $this->doScreenReaderLabel();
+			$field .= $this->doFieldEnd();
+			return $field;
+		}
+
+		public function textarea() {
+			$field = $this->doFieldStart();
+			$field .= $this->doFieldLabel();
+			$field .= '<textarea';
+			$field .= $this->doFieldName();
+			$field .= $this->doToolTip();
+			$field .= '>';
+			$field .= $this->value;
+			$field .= '</textarea>';
 			$field .= $this->doScreenReaderLabel();
 			$field .= $this->doFieldEnd();
 			return $field;
@@ -151,8 +190,8 @@ if ( !class_exists('TribeField') ) {
 				foreach ($this->options as $option_id => $title) {
 					$field .= '<label title="'.$title.'">';
 					$field .= '<input type="radio"';
-					$field .= ( isset($this->error) ) ? 'class="tribe-error"' : '';
- 					$field .= ' name="'.$this->name.'" value="'.$option_id.'" '.checked( $this->value, $option_id, false ).'/>';
+					$field .= $this->doFieldName();
+ 					$field .= ' value="'.$option_id.'" '.checked( $this->value, $option_id, false ).'/>';
 					$field .= $title;
 					$field .= '</label>';
 				}
@@ -166,7 +205,9 @@ if ( !class_exists('TribeField') ) {
 		public function checkbox_bool() {
 			$field = $this->doFieldStart();
 			$field .= $this->doFieldLabel();
-			$field .= '<input type="checkbox" name="'.$this->name.'" value="1" '.checked( $this->value, true, false );
+			$field .= '<input type="checkbox"';
+			$field .= $this->doFieldName();
+			$field .= ' value="1" '.checked( $this->value, true, false );
 			$field .= $this->doToolTip();
 			$field .= '/>';
 			$field .= $this->doScreenReaderLabel();
@@ -174,6 +215,53 @@ if ( !class_exists('TribeField') ) {
 			return $field;
 		}
 
+		public function dropdown() {
+			$field = $this->doFieldStart();
+			$field .= $this->doFieldLabel();
+			$field .= '<select';
+			$field .= $this->doFieldName();
+			$field .= $this->doToolTip();
+			$field .= '>';
+			if ( is_array($this->options) ) {
+				foreach ($this->options as $option_id => $title) {
+					$field .= '<option value="'.$option_id.'"';
+					$field .= selected( $this->value, $option_id, false );
+					$field .= '>'.$title.'</option>';
+				}
+			$field .= '</select>';
+			$field .= $this->doScreenReaderLabel();
+			} else {
+				$field .= '</select>';
+				$field .= $this->doScreenReaderLabel();
+				$field .= '<span class="tribe-error">'.__('No select options specified', 'tribe-events-calendar').'</span>';
+			}
+			$field .= $this->doFieldEnd();
+			return $field;
+		}
+
+		public function dropdown_chosen() {
+			$field = $this->doFieldStart();
+			$field .= $this->doFieldLabel();
+			$field .= '<select';
+			$field .= $this->doFieldName();
+			$field .= $this->doToolTip();
+			$field .= '>';
+			if ( is_array($this->options) ) {
+				foreach ($this->options as $option_id => $title) {
+					$field .= '<option value="'.$option_id.'"';
+					$field .= selected( $this->value, $option_id, false );
+					$field .= '>'.$title.'</option>';
+				}
+			$field .= '</select>';
+			$field .= $this->doScreenReaderLabel();
+			} else {
+				$field .= '</select>';
+				$field .= $this->doScreenReaderLabel();
+				$field .= '<span class="tribe-error">'.__('No select options specified', 'tribe-events-calendar').'</span>';
+			}
+			$field .= $this->doFieldEnd();
+			return $field;
+		}
 
 	} // end class
 
--- tests/tab-test.php
+++ tests/tab-test.php
@@ -6,9 +6,14 @@ function tribe_test_tab() {
 		'fields' => array(
 			'heading_test' => array( 'type' => 'heading', 'label' => 'Heading Test' ),
 			'html_test' => array( 'type' => 'html', 'label' => 'Html Test', 'html' => 'some random html'),
-			'text_test' => array( 'type' => 'text', 'label' => 'This is a text field'),
+			'text_test_small' => array( 'type' => 'text', 'label' => 'This is a small text field', 'size' => 'small'),
+			'text_test_medium' => array( 'type' => 'text', 'label' => 'This is a medium text field'),
+			'text_test_large' => array( 'type' => 'text', 'label' => 'This is a large text field', 'size' => 'large'),
+			'textarea_test' => array( 'type' => 'textarea', 'label' => 'This is a textarea field', 'size' => 'large'),
 			'radio_test' => array( 'type' => 'radio', 'label' => 'This is a radio field', 'options' => array('1' => 'one', '2' => 'two')),
 			'checkbox_text' => array( 'type' => 'checkbox_bool', 'label' => 'This is a checkbox field'),
+			'dropdown_test' => array( 'type' => 'dropdown', 'label' => 'This is a dropdown field', 'options' => array('1' => 'one', '2' => 'two')),
+			'dropdown_chosen_test' => array( 'type' => 'dropdown_chosen', 'label' => 'This is a chosen dropdown field', 'options' => array('1' => 'one', '2' => 'two')),
 		),
 	);
 	new TribeSettingsTab( 'test', __('Test', 'tribe-events-calendar'), $testTabArgs );
