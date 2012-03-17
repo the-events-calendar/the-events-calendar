537756abb40486cdc32ed37a1f91026e802812f9
Paul Hughes <supersmash@gmail.com>
2012-03-17 12:57:59 +0000

    Updates #14782 Fixed for multi-day events such that the event-has-passed message only displays if the event is truly over.
    git-svn-id: https://central.tri.be/svn/premium-plugins/code/events/branches/2.0.5@1884 75e66736-2afa-4fbe-8bd5-dce959cd71f9

--- views/single.php
+++ views/single.php
@@ -8,10 +8,10 @@
 
 // Don't load directly
 if ( !defined('ABSPATH') ) { die('-1'); }
-
 ?>
 <span class="back"><a href="<?php echo tribe_get_events_link(); ?>"><?php _e('&laquo; Back to Events', 'tribe-events-calendar'); ?></a></span>				
-<?php if (strtotime(tribe_get_end_date()) <= time() ) { ?><div class="event-passed"><?php  _e('This event has passed.', 'tribe-events-calendar') ?></div><?php } ?>
+<?php if (tribe_get_end_date(get_the_ID(), false, 'U') <= time() ) { ?><div class="event-passed"><?php  _e('This event has passed.', 'tribe-events-calendar') ?></div><?php } ?>
+<?php echo tribe_get_end_date(); ?>
 <div id="tribe-events-event-meta" itemscope itemtype="http://schema.org/Event">
 	<dl class="column">
 		<dt class="event-label event-label-name"><?php _e('Event:', 'tribe-events-calendar') ?></dt>
