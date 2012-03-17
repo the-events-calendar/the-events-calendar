4ff84396d6778e218723a72292aece618772bb33
Joey Kudish <tribe@jkudish.com>
2012-03-17 05:55:45 +0000

    Updates #15354 Update default/saved fields behaviour
    git-svn-id: https://central.tri.be/svn/premium-plugins/code/events/branches/2.0.5@1878 75e66736-2afa-4fbe-8bd5-dce959cd71f9

--- lib/tribe-field.class.php
+++ lib/tribe-field.class.php
@@ -144,7 +144,9 @@ if ( !class_exists('TribeField') ) {
 
 				} elseif ( $this->display_callback && is_array($this->display_callback) && method_exists($this->display_callback[0], $this->display_callback[1]) ) {
 
-					call_user_method($this->display_callback[1], $this->display_callback[0]);
+					// die(var_dump($this->display_callback));
+
+					call_user_func($this->display_callback[0].'::'.$this->display_callback[1]);
 
 				} elseif ( in_array($this->type, $this->valid_field_types) ) {
 
