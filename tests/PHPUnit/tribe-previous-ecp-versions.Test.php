6a7a21ecd83a7dfcd4090a64073ac46f49823209
Paul Hughes <supersmash@gmail.com>
2012-03-17 03:19:42 +0000

    Updates #13727 Added a unit test to make sure the 'previous_ecp_versions' option is saving correctly.
    git-svn-id: https://central.tri.be/svn/premium-plugins/code/events/branches/2.0.5@1871 75e66736-2afa-4fbe-8bd5-dce959cd71f9

---
+++ tests/PHPUnit/tribe-previous-ecp-versions.Test.php
@@ -0,0 +1,29 @@
+<?php
+/* -- tribe-previous-ecp-versions.Test.php -- */
+
+class WP_Test_TribePreviousEcpVersionsClass extends Tribe_WP_UnitTestCase {
+
+	/**
+	 * Check to make sure that the 'previous_ecp_versions' option exists.
+	 *
+	 * @author Paul Hughes
+	 */
+	function test_previous_ecp_versions_exists() {
+		$this->assertTrue( count( tribe_get_option( 'previous_ecp_versions' ) ) > 0 );
+	}
+	
+	/**
+	 * Check to make sure that 'previous_ecp_versions' is saving correctly.
+	 *
+	 * @author Paul Hughes
+	 */
+	function test_previous_ecp_versions_saving() {
+		$tribe_ecp = TribeEvents::instance();
+		$tribe_ecp->setOption('latest_ecp_version', '1.6.5');
+		$tribe_ecp->init();
+		$previous_ecp_versions = tribe_get_option( 'previous_ecp_versions' );
+		$this->assertEquals( '0', $previous_ecp_versions[0] );
+		$this->assertEquals( '1.6.5', $previous_ecp_versions[count( $previous_ecp_versions ) - 1] );
+	}
+	
+}
