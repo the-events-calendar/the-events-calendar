453985712572efd50b63ba769760f15f7608dbc2
Paul Hughes <supersmash@gmail.com>
2012-03-21 17:15:19 +0000

    Updates #15592 Unit tests for event creation completed (for now). They test the API function and the template tag itself.
    git-svn-id: https://central.tri.be/svn/premium-plugins/code/events/branches/2.0.5@1984 75e66736-2afa-4fbe-8bd5-dce959cd71f9

--- tests/PHPUnit/tribe-event-creation.Test.php
+++ tests/PHPUnit/tribe-event-creation.Test.php
@@ -28,14 +28,14 @@ class WP_Test_TribeEventCreation extends Tribe_WP_UnitTestCase {
 	}
 	
 	function test_tribe_create_event_template_tag_post_object_created() {
-		$post = get_post( tribe_create_event($this->postExampleSettings) );
+		$post = get_post( tribe_create_event( $this->postExampleSettings ) );
 		
 		// Check to make sure the postID is returned and the post object was created from it.
 		$this->assertInternalType( 'object', $post);		
 	}
 	
 	function test_tribe_create_event_template_tag_meta_information() {
-		$post = get_post( tribe_create_event($this->postExampleSettings) );
+		$post = get_post( tribe_create_event( $this->postExampleSettings ) );
 		
 		// Check to make sure all the post information was saved properly.
 		$this->assertEquals( 3, $post->post_author );
@@ -50,4 +50,29 @@ class WP_Test_TribeEventCreation extends Tribe_WP_UnitTestCase {
 		$this->assertEquals( '2012-01-01 01:15:00', get_post_meta( $post->ID, '_EventStartDate', true ) );
 		$this->assertEquals( '2012-01-03 15:25:59', get_post_meta( $post->ID, '_EventEndDate', true ) );
 	}
+	
+	function test_tribe_create_event_API_post_object_created() {
+		$post = get_post( TribeEventsAPI::createEvent($this->postExampleSettings) );
+		
+		// Check to make sure the postID is returned and the post object was created from it.
+		$this->assertInternalType( 'object', $post);		
+	}
+	
+	function test_tribe_create_event_API_meta_information() {
+		$post = get_post( TribeEventsAPI::createEvent( $this->postExampleSettings ) );
+		
+		// Check to make sure all the post information was saved properly.
+		$this->assertEquals( 3, $post->post_author );
+		$this->assertEquals( 'This is event content!', $post->post_content );
+		$this->assertEquals( '', get_post_meta( $post->ID, '_EventAllDay', true ) );
+		$this->assertEquals( 1, get_post_meta( $post->ID, '_EventHideFromUpcoming', true ) );
+		$this->assertEquals( 5, get_post_meta( $post->ID, '_EventOrganizerID', true ) );
+		$this->assertEquals( 8, get_post_meta( $post->ID, '_EventVenueID', true ) );
+		$this->assertEquals( 1, get_post_meta( $post->ID, '_EventShowMapLink', true ) );
+		$this->assertEquals( 1, get_post_meta( $post->ID, '_EventShowMapLink', true ) );
+		$this->assertEquals( 1, get_post_meta( $post->ID, '_EventShowMap', true ) );
+		$this->assertEquals( '2012-01-01 01:15:00', get_post_meta( $post->ID, '_EventStartDate', true ) );
+		$this->assertEquals( '2012-01-03 15:25:59', get_post_meta( $post->ID, '_EventEndDate', true ) );
+	}
+	
 }
