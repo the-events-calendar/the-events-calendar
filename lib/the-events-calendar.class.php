84000f5211d6211fa5a9b23d94cccb1282c11114
Joey Kudish <tribe@jkudish.com>
2012-03-21 05:38:58 +0000

    Updates #15584 Fix province bug
    git-svn-id: https://central.tri.be/svn/premium-plugins/code/events/branches/2.0.5@1980 75e66736-2afa-4fbe-8bd5-dce959cd71f9

--- lib/the-events-calendar.class.php
+++ lib/the-events-calendar.class.php
@@ -1961,7 +1961,7 @@ if ( !class_exists( 'TribeEvents' ) ) {
 
 				}
 
-				if ( isset($_VenueState) ) {
+				if ( isset($_VenueState) && !empty($_VenueState) ) {
 					$_VenueStateProvince = $_VenueState;
 				} elseif ( isset($_VenueProvince) ) {
 					$_VenueStateProvince = $_VenueProvince;
