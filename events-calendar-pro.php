d3eea8e0b7161facd540d010b10c73df92130af3
Joey Kudish <tribe@jkudish.com>
2012-03-21 21:29:20 +0000

    Updates #15605 add "The" to the events calendar pro name
    git-svn-id: https://central.tri.be/svn/premium-plugins/code/events-premium/branches/2.1@1992 75e66736-2afa-4fbe-8bd5-dce959cd71f9

--- events-calendar-pro.php
+++ events-calendar-pro.php
@@ -1,6 +1,6 @@
 <?php
 /*
-Plugin Name: Events Calendar PRO
+Plugin Name: The Events Calendar PRO
 Description: The Events Calendar PRO, a premium add-on to the open source The Events Calendar plugin (required), enables recurring events, custom attributes, venue pages, new widgets and a host of other premium features.
 Version: 2.1
 Author: Modern Tribe, Inc.
