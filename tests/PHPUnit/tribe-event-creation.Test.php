eb17750ea6c95a81fe5e2e8ff59eeeeeeb61ff3b
Joey Kudish <tribe@jkudish.com>
2012-03-21 19:49:02 +0000

    Updates #15592 Minor unit test adjustments
    git-svn-id: https://central.tri.be/svn/premium-plugins/code/events/branches/2.0.5@1989 75e66736-2afa-4fbe-8bd5-dce959cd71f9

--- tests/PHPUnit/tribe-event-creation.Test.php
+++ tests/PHPUnit/tribe-event-creation.Test.php
@@ -8,14 +8,20 @@
  * @author Paul Hughes
  */
 class WP_Test_TribeEventCreation extends Tribe_WP_UnitTestCase {
-	
-	var $postExampleSettings;
-	
+
 	/**
-	 * Add to the setUp() function the assigning of values for the event creation.
+ 	 * @since 2.0.5
+	 * @author Paul Hughes
+	 * @var holds example data for the post
+	 */
+	var $postExampleSettings;
+
+	/**
+	 * Extend the setUp() function by assigning values for the event creation.
 	 *
  	 * @since 2.0.5
 	 * @author Paul Hughes
+	 * @uses $postExampleSettings
 	 */
 	function setUp() {
 		parent::setUp();
@@ -38,19 +44,20 @@ class WP_Test_TribeEventCreation extends Tribe_WP_UnitTestCase {
 			'EventEndMeridian' => 'pm'
 		);
 	}
-	
+
 	/**
 	 * Check to make sure that the post object is created from a returned post ID.
 	 *
  	 * @since 2.0.5
 	 * @author Paul Hughes
+	 * @uses $postExampleSettings
 	 */
 	function test_tribe_create_event_template_tag_post_object_created() {
 		$post = get_post( tribe_create_event( $this->postExampleSettings ) );
-		
-		$this->assertInternalType( 'object', $post);		
+
+		$this->assertInternalType( 'object', $post);
 	}
-	
+
 	/**
 	 * Check to make sure that the event data is saved properly.
 	 *
@@ -59,7 +66,7 @@ class WP_Test_TribeEventCreation extends Tribe_WP_UnitTestCase {
 	 */
 	function test_tribe_create_event_template_tag_meta_information() {
 		$post = get_post( tribe_create_event( $this->postExampleSettings ) );
-		
+
 		$this->assertEquals( 3, $post->post_author );
 		$this->assertEquals( 'This is event content!', $post->post_content );
 		$this->assertEquals( '', get_post_meta( $post->ID, '_EventAllDay', true ) );
@@ -72,17 +79,18 @@ class WP_Test_TribeEventCreation extends Tribe_WP_UnitTestCase {
 		$this->assertEquals( '2012-01-01 01:15:00', get_post_meta( $post->ID, '_EventStartDate', true ) );
 		$this->assertEquals( '2012-01-03 15:25:59', get_post_meta( $post->ID, '_EventEndDate', true ) );
 	}
-	
+
 	/**
 	 * Check to make sure that the post object is created from a returned post ID.
 	 *
  	 * @since 2.0.5
 	 * @author Paul Hughes
+	 * @uses $postExampleSettings
 	 */
 	function test_tribe_create_event_API_post_object_created() {
 		$post = get_post( TribeEventsAPI::createEvent($this->postExampleSettings) );
-		
-		$this->assertInternalType( 'object', $post);		
+
+		$this->assertInternalType( 'object', $post);
 	}
 
 	/**
@@ -90,10 +98,10 @@ class WP_Test_TribeEventCreation extends Tribe_WP_UnitTestCase {
 	 *
  	 * @since 2.0.5
 	 * @author Paul Hughes
-	 */	
+	 */
 	function test_tribe_create_event_API_meta_information() {
 		$post = get_post( TribeEventsAPI::createEvent( $this->postExampleSettings ) );
-		
+
 		$this->assertEquals( 3, $post->post_author );
 		$this->assertEquals( 'This is event content!', $post->post_content );
 		$this->assertEquals( '', get_post_meta( $post->ID, '_EventAllDay', true ) );
@@ -106,5 +114,5 @@ class WP_Test_TribeEventCreation extends Tribe_WP_UnitTestCase {
 		$this->assertEquals( '2012-01-01 01:15:00', get_post_meta( $post->ID, '_EventStartDate', true ) );
 		$this->assertEquals( '2012-01-03 15:25:59', get_post_meta( $post->ID, '_EventEndDate', true ) );
 	}
-	
+
 }
