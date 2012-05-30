5438db2bd342632a924b651e0b6a40a952d83de1
Paul Hughes <supersmash@gmail.com>
2012-05-30 02:52:51 +0000

    Updates #16452 Events list in ugly-links mode now displays events in proper order.
    git-svn-id: https://central.tri.be/svn/premium-plugins/code/events/trunk@2413 75e66736-2afa-4fbe-8bd5-dce959cd71f9

--- lib/tribe-event-query.class.php
+++ lib/tribe-event-query.class.php
@@ -68,29 +68,31 @@ if (!class_exists('TribeEventsQuery')) {
 		}
 
 		public static function setArgsFromDisplayType($query) { 
-         if( !empty($query->query_vars['eventDisplay']) ) {
-            switch ( $query->query_vars['eventDisplay'] ) {
-               case "past":
-                  $query = self::setPastDisplayTypeArgs($query);
-                  break;
-               case "upcoming":
-                  $query = self::setUpcomingDisplayTypeArgs($query);
-                  break;
-               case "day":
-                  $query = self::setDayDisplayTypeArgs($query);
-                  break;
-               case "all":
-                  $query = self::setAllDisplayTypeArgs($query);
-                  break;				
-               case "month":
-                  $query = self::setMonthDisplayTypeArgs($query);				
-            }
-         } else if ( is_single() ) {
+        	if( !empty($query->query_vars['eventDisplay']) ) {
+            	switch ( $query->query_vars['eventDisplay'] ) {
+               		case "past":
+                  		$query = self::setPastDisplayTypeArgs($query);
+                  		break;
+               		case "upcoming":
+                  		$query = self::setUpcomingDisplayTypeArgs($query);
+                  		break;
+               		case "day":
+                  		$query = self::setDayDisplayTypeArgs($query);
+                  		break;
+               		case "all":
+                  		$query = self::setAllDisplayTypeArgs($query);
+                  		break;				
+               		case "month":
+                  		$query = self::setMonthDisplayTypeArgs($query);				
+            	}
+         	} else if ( is_single() ) {
 				$args = &$query->query_vars;
 				if( isset($args['eventDate']) ) {
 					$args['start_date'] = $args['eventDate'];
 					$args['end_date'] = $args['eventDate'];
 				}
+			} else {
+				$query = self::setUpcomingDisplayTypeArgs($query);
 			}
 		
 			return $query;
