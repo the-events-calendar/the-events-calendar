09ecff6ed98688c95a721290112736e04afb14b5
Daniel Dvorkin <daniel@mzaweb.com>
2012-05-31 06:12:12 +0000

    Styling fixes for the Ajaxified calendar widget
    git-svn-id: https://central.tri.be/svn/premium-plugins/code/events-premium/trunk@2421 75e66736-2afa-4fbe-8bd5-dce959cd71f9

--- resources/events-mini-ajax.js
+++ resources/events-mini-ajax.js
@@ -4,17 +4,19 @@ jQuery(document).ready(function($) {
   
   $('#calendar_wrap').delegate('.tribe-mini-ajax', 'click', function(e){ 
     e.preventDefault();    
-    var month_target = $(this).attr("href").split('/').pop();
+    var month_target = $(this).attr("data-month");
     var params = {
       action: 'calendar-mini',
       eventDate: month_target
     };
+    $("#tribe-mini-ajax-month").hide();
     $("#ajax-loading-mini").show();
     $.post(
 	  TribeMiniCalendar.ajaxurl,
       params,
       function ( response ) {
         $("#ajax-loading-mini").hide();
+        $("#tribe-mini-ajax-month").show();
         $("#calendar_wrap").html( response );
       }
       );
