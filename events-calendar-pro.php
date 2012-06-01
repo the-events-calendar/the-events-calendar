b01f1d9b73a7f20bf11f176ba99165370016c547
Paul Hughes <supersmash@gmail.com>
2012-06-01 14:26:25 +0000

    Updates #15716 The Save Settings button is now again displayed on the Additional Fields settings page.
    git-svn-id: https://central.tri.be/svn/premium-plugins/code/events-premium/trunk@2439 75e66736-2afa-4fbe-8bd5-dce959cd71f9

--- events-calendar-pro.php
+++ events-calendar-pro.php
@@ -242,7 +242,8 @@ if ( !class_exists( 'TribeEventsPro' ) ) {
 	  	public function add_settings_tabs() {
 			require_once( $this->pluginPath . 'admin-views/tribe-options-defaults.php' );
 			new TribeSettingsTab( 'defaults', __( 'Defaults', 'tribe-events-calendar-pro' ), $defaultsTab );
-			new TribeSettingsTab( 'additional-fields', __( 'Additional Fields', 'tribe-events-calendar-pro' ), array( 'priority' => 35 ) );
+			// The single-entry array at the end allows for the save settings button to be displayed.
+			new TribeSettingsTab( 'additional-fields', __( 'Additional Fields', 'tribe-events-calendar-pro' ), array( 'priority' => 35, 'fields' => array( '1' ) ) );
 	  	}
 
 
