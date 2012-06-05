52a099c979a00fd6be5ca26135ef7906be58b3e6
Daniel Dvorkin <daniel@mzaweb.com>
2012-06-05 17:24:32 +0000

    [App Shop] Fix boxes height
    git-svn-id: https://central.tri.be/svn/premium-plugins/code/events/trunk@2468 75e66736-2afa-4fbe-8bd5-dce959cd71f9

--- resources/app-shop.js
+++ resources/app-shop.js
@@ -1,10 +1,10 @@
 jQuery( document ).ready( function () {
 
 	var maxHeight = 0;
-	jQuery( "div.tribe-addon" ).each( function () {
+	jQuery( "div.tribe-addon .caption" ).each( function () {
 		var h = jQuery(this).height();
 		maxHeight = h > maxHeight ? h : maxHeight;
 	} );
 
-	jQuery( "div.tribe-addon" ).css('height', maxHeight+100);
+	jQuery( "div.tribe-addon .caption" ).css('height', maxHeight);
 } );
