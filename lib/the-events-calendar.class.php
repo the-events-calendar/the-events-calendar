45396119a12b1d522822bd363e373a56b18559d0
Paul Hughes <supersmash@gmail.com>
2012-06-26 23:43:02 +0000

    Updates #16911 Added 'Settings' and 'Calendar' links to the actions links for the plugin on the plugins page.
    git-svn-id: https://central.tri.be/svn/premium-plugins/code/events/trunk@2605 75e66736-2afa-4fbe-8bd5-dce959cd71f9

--- lib/the-events-calendar.class.php
+++ lib/the-events-calendar.class.php
@@ -257,12 +257,10 @@ if ( !class_exists( 'TribeEvents' ) ) {
 			add_action( 'admin_notices', array( $this, 'checkAddOnCompatibility' ) );
 			
 			add_action( 'wp_before_admin_bar_render', array( $this, 'addToolbarItems' ), 10 );
-		
 			add_action( 'admin_notices', array( $this, 'activationMessage' ) );
-		
 			add_action( 'all_admin_notices', array( $this, 'addViewCalendar' ) );
-			
 			add_action( 'load-nav-menus.php', array( $this, 'setInitialMenuMetaBoxes' ) );
+			add_action( 'plugin_action_links_' . trailingslashit( $this->pluginDir ) . 'the-events-calendar.php', array( $this, 'addLinksToPluginActions' ) );
 		}
 
 		public static function ecpActive( $version = '2.0.7' ) {
@@ -2718,6 +2716,12 @@ if ( !class_exists( 'TribeEvents' ) ) {
 				
 				update_user_option( $user->ID, 'tribe_setDefaultNavMenuBoxes', true, true );
 			}
+		}
+		
+		public function addLinksToPluginActions( $actions ) {
+			$actions['settings'] = '<a href="' . add_query_arg( array( 'post_type' => self::POSTTYPE, 'page' => 'tribe-events-calendar' ), admin_url( 'edit.php' ) ) .'">' . __('Settings', 'tribe-events-calendar') . '</a>';
+			$actions['tribe-calendar'] = '<a href="' . $this->getLink() .'">' . __('Calendar', 'tribe-events-calendar') . '</a>';
+			return $actions;
 		}
 
 	} // end TribeEvents class
