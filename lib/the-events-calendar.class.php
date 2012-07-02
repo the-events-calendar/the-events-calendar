c095dbfd3ea2c1a3480e9401bce68f5f55eeb600
Paul Hughes <supersmash@gmail.com>
2012-07-02 19:36:41 +0000

    Updates #15620 Fixed php notice generated by comments for recurring events fix.
    git-svn-id: https://central.tri.be/svn/premium-plugins/code/events/trunk@2641 75e66736-2afa-4fbe-8bd5-dce959cd71f9

--- lib/the-events-calendar.class.php
+++ lib/the-events-calendar.class.php
@@ -2769,7 +2769,7 @@ if ( !class_exists( 'TribeEvents' ) ) {
 		 * @return string the link
 		 */ 
 		public function newCommentLink( $content, $comment ) {
-			if ( class_exists( 'TribeEventsPro' ) && tribe_is_recurring_event( get_the_ID() ) ) {
+			if ( class_exists( 'TribeEventsPro' ) && tribe_is_recurring_event( get_the_ID() ) && isset( $_REQUEST['eventDate'] ) ) {
 				$link = trailingslashit( $this->getLink( 'single' ) ) . $_REQUEST['eventDate'] . '#comment-' . $comment->comment_ID;
 			} else {
 				$link = $content;
