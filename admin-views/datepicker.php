096e2d11abc28c5f18c43aa7abcc3797d81948a9
Paul Hughes <paul@paulalexanderhughes.com>
2012-08-01 13:02:26 -0400

    Updates #17273 Added hidden labels to the date picker for accessibility reasons.
    Signed-off-by: Paul Hughes <paul@paulalexanderhughes.com>

--- admin-views/datepicker.php
+++ admin-views/datepicker.php
@@ -8,9 +8,11 @@ if ( !defined('ABSPATH') ) { die('-1'); }
 
 ?>
 <form action="<?php echo tribe_get_dropdown_link_prefix(); ?>" method="get" id="<?php echo $prefix; ?>events-picker">
+	<label for="<?php echo $prefix; ?>events-month" style="display: none;">Events Month</label>
 	<select id='<?php echo $prefix; ?>events-month' name='EventJumpToMonth' class='<?php echo $prefix; ?>events-dropdown'>
 		<?php echo $monthOptions; ?>
 	</select>
+	<label for="<?php echo $prefix; ?>events-year" style="display: none;">Events Year</label>
 	<select id='<?php echo $prefix; ?>events-year' name='EventJumpToYear' class='<?php echo $prefix; ?>events-dropdown'>
 		<?php echo $yearOptions; ?>
 	</select>
