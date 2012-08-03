0e0d36889ce20aa0b9570f629e5c7f7270a19704
MZAWeb <daniel@mzaweb.com>
2012-08-03 11:15:34 -0300

    Load TribeSpinJS from the main TEC file

--- lib/the-events-calendar.class.php
+++ lib/the-events-calendar.class.php
@@ -155,6 +155,8 @@ if ( !class_exists( 'TribeEvents' ) ) {
 		 *Load all the required library files.
 		 **/
 		protected function loadLibraries() {
+			// Load spin.js class early so any class can use it
+			require_once ('tribe-spin-js.php');
 
 			// Exceptions Helper
 			require_once( 'tribe-event-exception.class.php' );
