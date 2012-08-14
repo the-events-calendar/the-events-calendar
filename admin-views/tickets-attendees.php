5f22b96eb62536c54ccee18f4f89a50a1487ef9d
MZAWeb <daniel@mzaweb.com>
2012-08-14 14:02:01 -0300

    Remove standard search from attendee list First pass at attendee summary

--- admin-views/tickets-attendees.php
+++ admin-views/tickets-attendees.php
@@ -1,11 +1,67 @@
 <div class="wrap">
 	<div id="icon-edit" class="icon32 icon32-tickets-attendees"><br></div>
 	<h2>Attendees</h2>
-</div>
 
-<form id="topics-filter" method="get">
-	<?php $attendees_table->search_box( "Search", "s" ); ?>
-	<input type="hidden" name="page" value="<?php echo $_REQUEST['page'] ?>"/>
-	<input type="hidden" name="post_type" value="topic"/>
-	<?php $attendees_table->display() ?>
-</form>
+	<div id="tribe-filters" class="metabox-holder">
+		<div id="filters-wrap" class="postbox">
+			<h3 title="Click to toggle">Summary</h3>
+			<table class="eventtable ticket_list">
+				<?php
+				$event_id = isset( $_GET["event_id"] ) ? $_GET["event_id"] : 0;
+				$tickets  = TribeEventsTickets::get_event_tickets( $event_id );
+
+				$provider = null;
+				$count    = 0;
+
+				foreach ( $tickets as $ticket ) {
+
+					$provider     = $ticket->provider_class;
+					$provider_obj = call_user_func( array( $provider,
+					                                       'get_instance' ) );
+
+					if ( ( $ticket->provider_class !== $provider ) || $count == 0 ) {
+						?>
+						<td colspan="3" >
+							<h4><?php echo esc_html( self::$active_modules[$ticket->provider_class] ); ?></h4>
+						</td>
+						<?php } ?>
+					<tr>
+						<td width="40%">
+							<p class="ticket_name"><?php
+								echo $ticket->name;
+								?></p>
+
+						</td>
+						<td nowrap="nowrap">
+							<?php
+							if ( $ticket->stock ) {
+								?>
+								<i>Stock: <?php echo esc_html( $ticket->stock ); ?></i>
+								<?php
+							}
+							?>
+						</td>
+						<td nowrap="nowrap">
+							<?php
+							if ( $ticket->qty_sold ) {
+								?>
+								<i>Sold: <?php echo esc_html( $ticket->qty_sold ); ?></i>
+								<?php
+							}
+							?>
+						</td>
+					</tr>
+					<?php
+					$count++;
+				} ?>
+			</table>
+		</div>
+	</div>
+
+	<form id="topics-filter" method="get">
+		<?php $attendees_table->search_box( "Search", "s" ); ?>
+		<input type="hidden" name="page" value="<?php echo $_REQUEST['page'] ?>"/>
+		<input type="hidden" name="post_type" value="topic"/>
+		<?php $attendees_table->display() ?>
+	</form>
+</div>
