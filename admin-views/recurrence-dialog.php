d8d16051b2212d12f24169debf65a48cbb29a22f
Ryan Urban <ryan@fringewebdesign.com>
2012-08-23 15:19:09 -0400

    Update #17482 - update save/delete helper text

--- admin-views/recurrence-dialog.php
+++ admin-views/recurrence-dialog.php
@@ -8,8 +8,16 @@ if ( !defined('ABSPATH') ) { die('-1'); }
 
 ?>
 <div id="recurring-dialog"  title="Saving Recurring Event" style="display: none;">
-	<?php _e('Which events do you wish to update?','tribe-events-calendar'); ?><br/>
+	<p><?php _e('Would you like to change only this instance of the event, or all events in this series?','tribe-events-calendar'); ?></p>
+	<ul>
+		<li><strong>Only This Event:</strong> <em>All other events in the series will remain the same.</em></li><br/>
+		<li><strong>All Events:</strong> <em>All events in the series will be changed. Any changes made to other events will be kept.</em></li>
+	</ul>
 </div>
 <div id="deletion-dialog"  title="Delete Recurring Event" style="display: none;" data-start="<?php echo (isset($recStart)) ?  $recStart : '' ?>" data-post="<?php echo (isset($recPost)) ?  $recPost : '' ?>">
-	<?php _e('Select your desired action','tribe-events-calendar'); ?><br/>
+	<p><?php _e('Would you like to delete only this instance of the event, or all events in this series?','tribe-events-calendar'); ?></p>
+	<ul>
+		<li><strong>Only This Event:</strong> <em>All other events in the series will not be deleted.</em></li><br/>
+		<li><strong>All Events:</strong> <em>All events in the series will be deleted.</em></li>
+	</ul>
 </div>
