4805da360a941905f4ea9d06f41ffaa63a44bca0
Ryan Urban <ryan@fringewebdesign.com>
2012-08-23 15:55:40 -0400

    Update #16822 - add end date to all day events

--- admin-views/recurrence-dialog.php
+++ admin-views/recurrence-dialog.php
@@ -10,14 +10,14 @@ if ( !defined('ABSPATH') ) { die('-1'); }
 <div id="recurring-dialog"  title="Saving Recurring Event" style="display: none;">
 	<p><?php _e('Would you like to change only this instance of the event, or all events in this series?','tribe-events-calendar'); ?></p>
 	<ul>
-		<li><strong>Only This Event:</strong> <em>All other events in the series will remain the same.</em></li><br/>
-		<li><strong>All Events:</strong> <em>All events in the series will be changed. Any changes made to other events will be kept.</em></li>
+		<li><strong><?php _e('Only This Event:','tribe-events-calendar'); ?></strong> <em><?php _e('All other events in the series will remain the same.','tribe-events-calendar'); ?></em></li><br/>
+		<li><strong><?php _e('All Events:','tribe-events-calendar'); ?></strong> <em><?php _e('All events in the series will be changed. Any changes made to other events will be kept.','tribe-events-calendar'); ?></em></li>
 	</ul>
 </div>
 <div id="deletion-dialog"  title="Delete Recurring Event" style="display: none;" data-start="<?php echo (isset($recStart)) ?  $recStart : '' ?>" data-post="<?php echo (isset($recPost)) ?  $recPost : '' ?>">
 	<p><?php _e('Would you like to delete only this instance of the event, or all events in this series?','tribe-events-calendar'); ?></p>
 	<ul>
-		<li><strong>Only This Event:</strong> <em>All other events in the series will not be deleted.</em></li><br/>
-		<li><strong>All Events:</strong> <em>All events in the series will be deleted.</em></li>
+		<li><strong><?php _e('Only This Event:','tribe-events-calendar'); ?></strong> <em><?php _e('All other events in the series will not be deleted.','tribe-events-calendar'); ?></em></li><br/>
+		<li><strong><?php _e('All Events:','tribe-events-calendar'); ?></strong> <em><?php _e('All events in the series will be deleted.','tribe-events-calendar'); ?></em></li>
 	</ul>
 </div>
--- views/events-list-load-widget-display.php
+++ views/events-list-load-widget-display.php
@@ -41,16 +41,17 @@ ob_end_clean();
 	<div class="when">
 		<?php
 			$space = false;
-			$output = '';
+			$output = ''; 			
 			echo tribe_get_start_date( $post->ID ); 
 
-         if( tribe_is_multiday( $post->ID ) || !$event->AllDay ) {
-            echo ' - ' . tribe_get_end_date($post->ID);
-         }
+         	if( tribe_is_multiday( $post->ID ) ) {
+            	echo '<br/>' . __('Ends', 'tribe-events-calendar-pro') . ' ';
+				echo tribe_get_end_date($post->ID);
+         	}
 
 			if($event->AllDay) {
-				echo ' <small>('.__('All Day','tribe-events-calendar').')</small>';
-         }
+				echo ' <small><em>('.__('All Day','tribe-events-calendar').')</em></small>';
+         	}
       ?> 
 	</div>
 	<div class="event">
