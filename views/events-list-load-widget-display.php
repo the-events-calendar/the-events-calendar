cd8d2a7fd3e397327d9863de851f1cd40cae7afe
Ryan Urban <ryan@fringewebdesign.com>
2012-08-31 16:42:41 -0400

    Update #16822 - update widget list

--- views/events-list-load-widget-display.php
+++ views/events-list-load-widget-display.php
@@ -44,12 +44,12 @@ ob_end_clean();
 			$output = ''; 			
 			echo tribe_get_start_date( $post->ID ); 
 
-         	if( tribe_is_multiday( $post->ID ) ) {
+         	if( tribe_is_multiday( $post->ID ) || !$event->AllDay ) {
             	echo '<br/>' . __('Ends', 'tribe-events-calendar-pro') . ' ';
 				echo tribe_get_end_date($post->ID);
          	}
 
-			if($event->AllDay) {
+			if( $event->AllDay ) {
 				echo ' <small><em>('.__('All Day','tribe-events-calendar').')</em></small>';
          	}
       ?> 
