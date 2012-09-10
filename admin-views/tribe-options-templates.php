e94dbb20c975a1e04f6c0abc3163ea2385887d64
Paul Hughes <paul@paulalexanderhughes.com>
2012-09-10 16:07:50 -0400

    Updates #17960 Updated settings tabs with some new copy.
    Signed-off-by: Paul Hughes <paul@paulalexanderhughes.com>

--- admin-views/tribe-options-templates.php
+++ admin-views/tribe-options-templates.php
@@ -23,7 +23,7 @@ $templatesTab = array(
 		),
 		'info-box-description' => array(
 			'type' => 'html',
-			'html' => '<p>' . __('Having trouble making the calendar fit in your theme? On this page, rectify that by selecting what WordPress "page template" The Events Calendar will use to display your event views. The page template controls the usage and placement of the header, sidebar, main content, footer and any other unique content blocks your site may be using. The dropdown below includes the Default Events Template (bundled with the Events framework), Default Page Template (the theme\'s default page.php template), and any other page templates your theme may have shipped with.</p><p>While we do our best to make sure the plugin plays nicely with as many themes as possible, there will inevitably be situations where none of the templating options available below will look 100% perfect. In that situation you may want to check out <a href="http://tri.be/support/documentation/events-calendar-themers-guide/">our themer\'s guide</a> to figure out what templating approach is best for you.', 'tribe-events-calendar') . '</p>',
+			'html' => '<p>' . __('You can apply different page templates to The Events Calendar. Page templates control the layout of individual pages. The Events Calendar comes with a Default Events Template. However, you can apply any page template that is available in your WordPress Theme. If you are having problems getting your Events Calendar to display correctly, switching the page template may solve the problem.</p><p>We make every effort to ensure that the Plugin is compatible with as many Themes as possible but there may be situations in which none of the below templating options will look 100% perfect. Check out our <a href="http://tri.be/support/documentation/events-calendar-themers-guide/">our themer\'s guide</a> to figure out what approach is best for you.', 'tribe-events-calendar') . '</p>',
 		),
 		'info-end' => array(
 			'type' => 'html',
@@ -41,14 +41,14 @@ $templatesTab = array(
 		'tribeEventsBeforeHTML' => array(
 			'type' => 'textarea',
 		 	'label' => __( 'Add HTML before calendar', 'tribe-events-calendar' ),
-			'tooltip' => __( 'Some themes may require that you add extra divs before the calendar list to help with styling.<br>This is displayed directly after the header.', 'tribe-events-calendar' ) . ' ' . __( 'You may use (x)HTML.', 'tribe-events-calendar' ),
+			'tooltip' => __( 'If you are familiar with CSS you can use this box to add extra divs before the calendar list. Some Themes may require this to help with styling.', 'tribe-events-calendar' ),
 			'validation_type' => 'html',
 			'size' => 'large',
 		),
 		'tribeEventsAfterHTML' => array(
 			'type' => 'textarea',
 		 	'label' => __( 'Add HTML after calendar', 'tribe-events-calendar' ),
-			'tooltip' => __( 'Some themes may require that you add extra divs after the calendar list to help with styling.<br>This is displayed directly above the footer.', 'tribe-events-calendar' ) . ' ' . __( 'You may use (x)HTML.', 'tribe-events-calendar' ),
+			'tooltip' => __( 'If you are familiar with CSS you can use this box to add extra divs after the calendar list. Some Themes may require this to help with styling.', 'tribe-events-calendar' ),
 			'validation_type' => 'html',
 			'size' => 'large',
 		),
