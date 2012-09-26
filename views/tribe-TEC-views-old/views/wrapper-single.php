ec07fd888966d588048ad8beaac6c3e9db68d1e1
Ryan Urban <ryan@fringewebdesign.com>
2012-09-26 12:30:08 -0400

    Add old-views folder for reference / remove deprecated

---
+++ views/tribe-TEC-views-old/views/wrapper-single.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Single Page Wrapper Template
+ * This file loads the single page wrapper template for the single page specific views (single-event.php,
+ * single-venue.php, etc).
+ *
+ * If 'Default Events Template' is selected in Events -> Settings -> Template -> Events Template, 
+ * then this file loads the single page wrapper template for all the single page views. Generally,
+ * this setting should only be used if you want to manually specify all the wrapper markup of
+ * your views in this template file. You can also select one of the other Events Template 
+ * Settings to automatically integrate views into your theme.
+ *
+ * You can recreate an ENTIRELY new single page wrapper template by doing a template override,
+ * and placing a wrapper-single.php file in a tribe-events/ directory within your theme
+ * directory, which will override the /views/wrapper-single.php. 
+ *
+ * @package TribeEventsCalendar
+ * @since  1.0
+ * @author Modern Tribe Inc.
+ *
+ */
+
+if ( !defined('ABSPATH') ) { die('-1'); }
+?>
+
+<?php get_header(); ?>
+	<?php tribe_events_before_html(); ?>
+	
+		<div id="container">
+			<div id="content" class="tribe-events-event widecolumn">
+			<?php the_post(); global $post; ?>
+				<div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+					<h2 class="entry-title"><?php the_title(); ?></h2>
+					<?php include( tribe_get_current_template() ); ?>
+					<?php edit_post_link( __( 'Edit', 'tribe-events-calendar' ), '<span class="edit-link">', '</span>' ); ?>
+				</div><!-- #post -->
+			<?php if( tribe_get_option( 'showComments','no' ) == 'yes' ) { comments_template(); } ?>
+			</div><!-- #content -->
+		</div><!-- #container-->
+		
+		<?php get_sidebar(); ?>
+		
+	<?php tribe_events_after_html(); ?>
+<?php get_footer(); ?>
