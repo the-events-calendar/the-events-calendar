c507909e78692a2ca37d350168f247497f8eb19e
MZAWeb <daniel@mzaweb.com>
2012-10-03 18:20:39 -0300

    Removed loading of WP_Router

--- events-calendar-pro.php
+++ events-calendar-pro.php
@@ -66,9 +66,6 @@ if ( !class_exists( 'TribeEventsPro' ) ) {
 			TribeCommonLibraries::register( 'advanced-post-manager', '1.0.5', $this->pluginPath . 'vendor/advanced-post-manager/tribe-apm.php' );
 			TribeCommonLibraries::register( 'related-posts', '1.1', $this->pluginPath. 'vendor/tribe-related-posts/tribe-related-posts.php' );
 
-			$tec = TribeEvents::instance();
-			TribeCommonLibraries::register( 'wp-router', '0.3', $tec->pluginPath . 'vendor/wp-router/wp-router.php' );
-
 			//TribeCommonLibraries::register( 'tribe-support', '0.1', $this->pluginPath . 'vendor/tribe-support/tribe-support.class.php' );
 
 			// Next Event Widget
