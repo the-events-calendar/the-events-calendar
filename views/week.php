dab49ae48579aa728eccfbdd3f5cb2bd9a1c7065
Timothy Wood <tim@imaginesimplicity.com>
2012-10-08 22:17:55 -0400

    dynamic day headers
    Signed-off-by: Timothy Wood <tim@imaginesimplicity.com>

--- views/week.php
+++ views/week.php
@@ -43,10 +43,6 @@ if ( !defined('ABSPATH') ) { die('-1'); }
 	-Event Excerpt Tag
 	-Event Time Duration Tag (Just like for calendar view)
 	-Tag For URL To Event
-*/
-?>
-
-<?php /*
 
 	// Separate skeleton / full styles
 	// Add tags (id's, classes, tooltip shit, etc update to micro formats? query bits)
@@ -55,165 +51,18 @@ if ( !defined('ABSPATH') ) { die('-1'); }
 	
 	// Cut into hooks/filters last
 
-*/ ?>
+*/ 
 
-<div id="tribe-events-content" class="tribe-events-week-grid">
-	
-    <!-- This title is here for ajax loading – do not remove if you want ajax switching between month views -->
-    <title><?php wp_title(); ?></title>
-      	
-	
-	
-	<?php 
+echo apply_filters( 'tribe_events_week_before_template', '');
 
 	// weekly header (navigation)
 	echo apply_filters( 'tribe_events_week_the_header', '');
 
-	?>
+	echo apply_filters('tribe_events_week_the_grid', '');
 	
-	
-	<?php
-		// Tooltips (see how implemented in calendar.php, /hooks/calendar.php, public/template-tags/calendar.php)
-		// jQuery bits for placing event height/top & all day centering (basically all styles that are hardcoded
-		// in this view need to be done by jQuery)
-		// Think about classes/design for recurring events like in prev/next week, etc, highlight today, etc
-		// Thinking the "th" bookmarks could link to their dayview counterparts
-	 ?>
-
-	<table cellspacing="0" cellpadding="0" class="tribe-events-grid">
-	
-		<thead>
-			<tr>
-				<th scope="col" class="tribe-grid-first"><span>Hours</span></th>
-				<th title="2012-09-08" scope="col" class="tribe-grid-today"><a href="" rel="bookmark">Sun 9/7</a></th>
-				<th title="2012-09-08" scope="col"><a href="" rel="bookmark">Mon 9/8</a></th>
-				<th title="2012-09-08" scope="col"><a href="" rel="bookmark">Tue 9/9</a></th>
-				<th title="2012-09-08" scope="col"><a href="" rel="bookmark">Wed 9/10</a></th>
-				<th title="2012-09-08" scope="col"><a href="" rel="bookmark">Thu 9/11</a></th>
-				<th title="2012-09-08" scope="col"><a href="" rel="bookmark">Fri 9/12</a></th>
-				<th title="2012-09-08" scope="col"><a href="" rel="bookmark">Sat 9/13</a></th>
-			</tr>
-		</thead>
-
-		<tbody>
-		
-			<?php // our dummy row for all day events ?>
-			<tr class="tribe-week-dummy-row" style="height: 72px;">
-				<td></td>
-				<td class="tribe-week-today"></td>
-				<td></td>
-				<td></td>
-				<td></td>
-				<td></td>
-				<td></td>
-				<td></td>
-			</tr><!-- .tribe-week-dummy-row -->
-		
-			<?php // our all day events row ?>
-			<tr class="tribe-week-allday-row">
-				<td class="tribe-week-allday-th"><div style="margin-top: -72px;">All Day</div></td>
-				<td colspan="7">
-					<div style="margin-top: -72px;">
-						<table cellpadding="0" cellspacing="0">
-							<tbody class="hfeed">
-								<tr>
-									<td colspan="2">
-										<div class="hentry vevent"><a href="" class="entry-title summary url" rel="bookmark">Multiday all day event</a></div>
-									</td>
-									<td colspan="4">
-										<div class="hentry vevent"><a href="" class="entry-title summary url" rel="bookmark">Multiday all day event</a></div>
-									</td>
-									<td>
-										<div class="hentry vevent"><a href="" class="entry-title summary url" rel="bookmark">All day event</a></div>
-									</td>
-								</tr>
-							</tbody><!-- .hfeed -->
-						</table>
-					</div>
-				</td>
-			</tr><!-- .tribe-week-allday-row -->
-		
-			<?php // our week grid ?>
-			<tr class="tribe-week-grid-bgd">
-				<td></td>
-				<td colspan="7">
-					<div class="tribe-week-grid-outer-wrap">
-						<div class="tribe-week-grid-inner-wrap">
-							<div class="tribe-week-grid-block"><div></div></div>
-							<div class="tribe-week-grid-block"><div></div></div>
-							<div class="tribe-week-grid-block"><div></div></div>
-							<div class="tribe-week-grid-block"><div></div></div>
-						</div><!-- .tribe-week-grid-inner-wrap -->
-					</div><!-- .tribe-week-grid-outer-wrap -->
-				</td>
-			</tr><!-- .tribe-week-grid-bgd -->
-			
-			<?php // our actual week grid columns, hours, and events ?>
-			<tr class="tribe-week-events-row hfeed">
-				<td class="tribe-week-grid-hours">
-					<div>7am</div>
-					<div>8am</div>
-					<div>9am</div>
-					<div>10am</div>
-				</td><!-- .tribe-week-grid-hours -->
-				<td class="tribe-week-grid-col tribe-week-today"></td><!-- .tribe-week-grid-col -->
-				<td class="tribe-week-grid-col">
-					<div class="tribe-week-grid-col-inner-wrap">
-						<div class="tribe-week-grid-event-wrap hentry vevent">
-							<div>
-								<a href="" class="entry-title summary url" rel="bookmark">MIT Theme Structure Long title</a>
-							</div>
-						</div><!-- .tribe-week-grid-event-wrap -->
-					</div><!-- .tribe-week-grid-col-inner-wrap -->
-				</td><!-- .tribe-week-grid-col -->
-				<td class="tribe-week-grid-col"></td><!-- .tribe-week-grid-col -->
-				<td class="tribe-week-grid-col">
-					<div class="tribe-week-grid-col-inner-wrap">
-						<div class="tribe-week-grid-event-wrap hentry vevent">
-							<div style="height: 60px;">
-								<a href="" class="entry-title summary url" rel="bookmark">MIT Theme Structure Long title</a>
-							</div>
-						</div><!-- .tribe-week-grid-event-wrap -->
-						
-						<div style="width: 90%; top: 28px; right: 0; left: auto;" class="tribe-week-grid-event-wrap tribe-event-overlapping tribe-event-o-1 hentry vevent">
-							<div style="height: 50px;">
-								<a href="" class="entry-title summary url" rel="bookmark">Short Title</a>
-							</div>
-						</div><!-- .tribe-week-grid-event-wrap -->
-						
-						<div style="width: 80%; top: 48px; right: 0; left: auto;" class="tribe-week-grid-event-wrap tribe-event-overlapping tribe-event-o-2 hentry vevent">
-							<div style="height: 50px;">
-								<a href="" class="entry-title summary url" rel="bookmark">Short Title</a>
-							</div>
-						</div><!-- .tribe-week-grid-event-wrap -->
-					</div><!-- .tribe-week-grid-col-inner-wrap -->
-				</td><!-- .tribe-week-grid-col -->
-				<td class="tribe-week-grid-col"></td><!-- .tribe-week-grid-col -->
-				<td class="tribe-week-grid-col"></td><!-- .tribe-week-grid-col -->
-				<td class="tribe-week-grid-col">
-					<div class="tribe-week-grid-col-inner-wrap">
-						<div style="width: 80%;" class="tribe-week-grid-event-wrap tribe-event-same-time hentry vevent">
-							<div style="height: 60px;">
-								<a href="" class="entry-title summary url" rel="bookmark">MIT Theme Structure Long title</a>
-							</div>
-						</div><!-- .tribe-week-grid-event-wrap -->
-						
-						<div style="width: 80%; right: 0; left: auto;" class="tribe-week-grid-event-wrap tribe-event-same-time tribe-event-st-1 hentry vevent">
-							<div style="height: 60px;">
-								<a href="" class="entry-title summary url" rel="bookmark">Short Title</a>
-							</div>
-						</div><!-- .tribe-week-grid-event-wrap -->
-					</div><!-- .tribe-week-grid-col-inner-wrap -->
-				</td><!-- .tribe-week-grid-col -->
-			</tr><!-- .tribe-week-events-row -->
-					
-		</tbody>
-		
-	</table><!-- .tribe-events-grid -->
-		
-    <?php // iCal Import
+	// iCal Import
     if( function_exists( 'tribe_get_ical_link' ) ): ?>
        	<a class="tribe-events-ical" title="<?php esc_attr_e( 'iCal Import', 'tribe-events-calendar' ); ?>" href="<?php echo tribe_get_ical_link(); ?>"><?php _e( 'iCal Import', 'tribe-events-calendar' ); ?></a>
-    <?php endif; ?>
-		
-</div><!-- #tribe-events-content -->
+    <?php endif;
+
+echo apply_filters('tribe_events_week_after_template', '');
