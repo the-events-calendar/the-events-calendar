87b1ac0eb0bc598db17f95306f3b9eeacdd34366
Kyle Unzicker <kyle@wndfrm.com>
2012-10-16 16:23:59 -0500

    Added Event Schedule template tag

--- public/template-tags/general.php
+++ public/template-tags/general.php
@@ -417,5 +417,24 @@ if( class_exists( 'TribeEvents' ) ) {
 		return apply_filter('tribe_event_featured_image', $featured_image);
 	}
 
+	function tribe_event_schedule_details( $post_id = null){
+		if (is_null( $post_id ))
+			$post_id = get_the_ID();
+			$schedule = '';
+			 if ( tribe_is_multiday( $post_id ) ) { // multi-date event 
+				$schedule .= '<h2><span class="dtstart">'. tribe_get_start_date() .'</span> - <span class="dtend">'. tribe_get_end_date() .'</span></h2>';
+			 } elseif ( tribe_get_all_day( $post_id ) ) {  // all day event
+				$schedule .= '<h2><span class="dtstart">'. tribe_get_start_date() .'</span></h2>';
+			} else { // single day event
+					if ( tribe_get_start_date( $post_id, false, 'g:i A' ) == tribe_get_end_date( $post_id, false, 'g:i A' ) ) { // Same start/end time 
+						$schedule .= '<h2><span class="dtstart">'. tribe_get_start_date($post_id, false) .'</span> @ <span class="starttime">'. tribe_get_start_date( $post_id, false, 'g:i A' ) .'</span></h2>';
+					 } else {  // defined start/end time
+					  $schedule .= '<h2><span class="dtstart">'. tribe_get_start_date( $post_id, false ) .'</span> | <span class="starttime">'. tribe_get_start_date( $post_id, false, 'g:i A' ) .' - <span class="starttime">'. tribe_get_end_date( $post_id, false, 'g:i A' ) .'</h2>';	
+					 } 					
+			}			 
+			return $schedule;
+			return apply_filters('tribe_event_recurring_info_tooltip', $schedule);
+	} 
+
 }
 ?>
