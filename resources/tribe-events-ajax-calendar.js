5742c8afd7f0bf48ca64c088970bc781ca193be8
samuel estok <sam@faction23.com>
2012-10-25 19:31:18 -0700

    rough pass at implementing pushstate for ajax

--- resources/tribe-events-ajax-calendar.js
+++ resources/tribe-events-ajax-calendar.js
@@ -1,25 +1,38 @@
 //alert( TribeCalendar.ajaxurl );
 
 
-jQuery( document ).ready( function ( $ ) {
+jQuery( document ).ready( function ( $ ) {	
+	
+	window.onpopstate = function(event) {	
+		
+		var tribe_pop = false;
+		var pop_date = event.state.date;
+		var pop_target = event.state.target;
+		tribe_events_calendar_ajax_post( pop_date, pop_target, tribe_pop );
+	};
 
 	$( '.tribe-events-calendar .tribe-events-nav a' ).live( 'click', function ( e ) {
 
 		e.preventDefault();
+		var tribe_pop = true;
 		var month_target = $( this ).attr( "data-month" );
-		tribe_events_calendar_ajax_post( month_target );
+		var href_target = $( this ).attr( "href" );
+		tribe_events_calendar_ajax_post( month_target, href_target, tribe_pop );
 	} );
 
 	$( '.tribe-events-calendar select.tribe-events-events-dropdown' ).live( 'change', function ( e ) {
 
-		var date = $( '#tribe-events-events-year' ).val() + '-' + $( '#tribe-events-events-month' ).val();
-		tribe_events_calendar_ajax_post( date );
+		var tribe_pop = true;
+		var baseUrl = $(this).parent().attr('action');
+		var date = $( '#tribe-events-events-year' ).val() + '-' + $( '#tribe-events-events-month' ).val();		
+		var href_target = baseUrl + date + '/';		
+		tribe_events_calendar_ajax_post( date, href_target, tribe_pop );
 	} );
 
-	function tribe_events_calendar_ajax_post( date ) {
+	function tribe_events_calendar_ajax_post( date, href_target, tribe_pop ) {
 
-		$( '.ajax-loading' ).show();
-
+		$( '.ajax-loading' ).show();	
+                
 		var params = {
 			action:'tribe_calendar',
 			eventDate:date
@@ -32,6 +45,12 @@ jQuery( document ).ready( function ( $ ) {
 				$( "#ajax-loading" ).hide();
 				if ( response !== '' ) {
 					$( '#tribe-events-content.tribe-events-calendar' ).html( response );
+					if( tribe_pop ) {
+						history.pushState({
+							"date": date, 
+							"target": href_target
+						}, null, href_target);
+					}
 				}
 			}
 		);
