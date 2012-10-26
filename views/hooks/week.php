d982e938aa986e8fede14ac642fb13100312d61e
Ryan Urban <ryan@fringewebdesign.com>
2012-10-26 00:17:12 -0400

    Update week view and styles

--- views/hooks/week.php
+++ views/hooks/week.php
@@ -93,30 +93,45 @@ if( !class_exists('Tribe_Events_Week_Template')){
 
 			ob_start();
 ?>
-<div class="tribe-week-wrapper">
-	<div class="tribe-grid-allday">
-		<div scope="col" class="column tribe-grid-first"><span><?php _e('Hours', 'tribe-events-calendar-pro'); ?></span></div>
+<div class="tribe-events-grid">
+
+	<!-- Table Header -->
+	<div class="tribe-grid-header">
+		<div class="column first"><span class="tribe-events-visuallyhidden"><?php _e('Hours', 'tribe-events-calendar-pro'); ?></span></div>
+		
+		<div class="test">
+		
 		<?php
 			for( $n = 0; $n < $week_length; $n++ ) {
 				$day = Date('Y-m-d', strtotime($start_of_week . " +$n days"));
 				$header_class = ($day == $today) ? 'tribe-week-today' : '';
-				printf('<div title="%s" scope="col" class="column %s"><a href="%s" rel="bookmark">%s</a></div>',
+				printf('<div title="%s" class="column %s"><a href="%s" rel="bookmark">%s</a></div>',
 					$day,
 					$header_class,
 					trailingslashit( get_site_url() ) . trailingslashit( $tribe_ecp->rewriteSlug ) . trailingslashit( Date('Y-m-d', strtotime($start_of_week . " +$n days") ) ),
-					Date('l jS', strtotime($start_of_week . " +$n days"))
+					Date('D jS', strtotime($start_of_week . " +$n days"))
 					);
 			}
 		?>
-	</div>
+		
+		</div>
+		
+	</div><!-- .tribe-grid-header -->
+	
+	
+	<!-- All Day -->
 	<div class="tribe-grid-allday">
-		<div scope="col" class="column tribe-grid-first"><span><?php _e('All Day', 'tribe-events-calendar-pro'); ?></span></div>
+		<div class="column first"><?php _e('All Day', 'tribe-events-calendar-pro'); ?></div>
+		
+		
+		<div class="test">
+		
 		<?php
 			$placeholder = 0;
 			for( $n = 0; $n < $week_length; $n++ ) {
 				$day = Date('Y-m-d', strtotime($start_of_week . " +$n days"));
 				$header_class = ($day == $today) ? 'tribe-week-today' : '';
-				printf('<div title="%s" scope="col" class="column %s">',
+				printf('<div title="%s" class="column %s">',
 					Date('Y-m-d', strtotime($start_of_week . " +$n days")),
 					$header_class
 					);
@@ -144,8 +159,47 @@ if( !class_exists('Tribe_Events_Week_Template')){
 				echo '</div>';
 			}
 		?>
-	</div>
-	<div class="tribe-grid-daily">
+		
+		</div>
+	</div><!-- .tribe-grid-allday -->
+	
+	<div class="tribe-week-grid-bgd">
+		<div></div>
+		<div>
+			<div class="tribe-week-grid-outer-wrap">
+				<div class="tribe-week-grid-inner-wrap">
+					<div class="tribe-week-grid-block"><div></div></div>
+					<div class="tribe-week-grid-block"><div></div></div>
+					<div class="tribe-week-grid-block"><div></div></div>
+					<div class="tribe-week-grid-block"><div></div></div>
+					<div class="tribe-week-grid-block"><div></div></div>
+					<div class="tribe-week-grid-block"><div></div></div>
+					<div class="tribe-week-grid-block"><div></div></div>
+					<div class="tribe-week-grid-block"><div></div></div>
+					<div class="tribe-week-grid-block"><div></div></div>
+					<div class="tribe-week-grid-block"><div></div></div>
+					<div class="tribe-week-grid-block"><div></div></div>
+					<div class="tribe-week-grid-block"><div></div></div>
+					<div class="tribe-week-grid-block"><div></div></div>
+					<div class="tribe-week-grid-block"><div></div></div>
+					<div class="tribe-week-grid-block"><div></div></div>
+					<div class="tribe-week-grid-block"><div></div></div>
+					<div class="tribe-week-grid-block"><div></div></div>
+					<div class="tribe-week-grid-block"><div></div></div>
+					<div class="tribe-week-grid-block"><div></div></div>
+					<div class="tribe-week-grid-block"><div></div></div>
+					<div class="tribe-week-grid-block"><div></div></div>
+					<div class="tribe-week-grid-block"><div></div></div>
+					<div class="tribe-week-grid-block"><div></div></div>
+				</div><!-- .tribe-week-grid-inner-wrap -->
+			</div><!-- .tribe-week-grid-outer-wrap -->
+		</div>
+	</div><!-- .tribe-week-grid-bgd -->
+	
+	<!-- Days of the week & hours & events -->
+	<div class="tribe-grid-body">
+	
+		<!-- hours -->
 		<div class="column tribe-week-grid-hours">
 			<?php 
 
@@ -154,12 +208,16 @@ if( !class_exists('Tribe_Events_Week_Template')){
 			}
 
 			?>
-		</div>
-		<?php
+		</div><!-- tribe-week-grid-hours -->
+		
+		
+		<div class="test">
+		
+		<?php // Our day columns?
 			for( $n = 0; $n < $week_length; $n++ ) {
 				$day = Date('Y-m-d', strtotime($start_of_week . " +$n days"));
 				$header_class = ($day == $today) ? 'tribe-week-today' : '';
-				printf('<div title="%s" scope="col" class="column %s">',
+				printf('<div title="%s" class="column hfeed %s">',
 					Date('Y-m-d', strtotime($start_of_week . " +$n days")),
 					$header_class
 					);
@@ -177,163 +235,12 @@ if( !class_exists('Tribe_Events_Week_Template')){
 				echo '</div>';
 			}
 		?>
-	</div>
-</div>
-	<table cellspacing="0" cellpadding="0" class="tribe-events-grid">
-		<thead>
-			<tr>
-				<th scope="col" class="tribe-grid-first"><span><?php _e('Hours', 'tribe-events-calendar-pro'); ?></span></th>
-				<?php
-					for( $n = 0; $n < $week_length; $n++ ) {
-						$header_class = (Date('Y-m-d',strtotime($start_of_week . " +$n days")) == $today) ? 'tribe-week-today' : '';
-						printf('<th title="%s" scope="col" class="%s"><a href="%s" rel="bookmark">%s</a></th>',
-							Date('Y-m-d', strtotime($start_of_week . " +$n days")),
-							$header_class,
-							trailingslashit( get_site_url() ) . trailingslashit( $tribe_ecp->rewriteSlug ) . trailingslashit( Date('Y-m-d', strtotime($start_of_week . " +$n days") ) ),
-							Date('l jS', strtotime($start_of_week . " +$n days"))
-							);
-					}
-				?>
-			</tr>
-		</thead>
-		<tbody>
-			<?php // our dummy row for all day events ?>
-			<tr class="tribe-week-dummy-row" style="height: 72px;">
-				<?php 
-				
-				// For our hours
-				echo '<td></td>';
-				
-				for( $n = 0; $n < $week_length; $n++ ) {
-					$header_class = (Date('Y-m-d',strtotime($start_of_week . " +$n days")) == $today) ? 'tribe-week-today' : '';
-					printf( '<td class="%s"></td>', $header_class );
-				}
-
-				?>
-			</tr><!-- .tribe-week-dummy-row -->
 		
-			<?php // our all day events row ?>
-			<tr class="tribe-week-allday-row">
-				<td class="tribe-week-allday-th"><div style="margin-top: -72px;">All Day</div></td>
-				<td colspan="7">
-					<div style="margin-top: -72px;">
-						<table cellpadding="0" cellspacing="0">
-							<tbody class="hfeed">
-								<tr>
-									<td colspan="2">
-										<div class="hentry vevent"><h3 class="entry-title summary"><a href="" class="url" rel="bookmark">Multiday all day event</a></h3></div>
-									</td>
-									<td colspan="4">
-										<div class="hentry vevent"><h3 class="entry-title summary"><a href="" class="entry-title summary url" rel="bookmark">Multiday all day event</a></h3></div>
-									</td>
-									<td>
-										<div class="hentry vevent"><h3 class="entry-title summary"><a href="" class="entry-title summary url" rel="bookmark">All day event</a></h3></div>
-									</td>
-								</tr>
-							</tbody><!-- .hfeed -->
-						</table>
-					</div>
-				</td>
-			</tr><!-- .tribe-week-allday-row -->
-		
-			<?php // our week grid ?>
-			<tr class="tribe-week-grid-bgd">
-				<td></td>
-				<td colspan="7">
-					<div class="tribe-week-grid-outer-wrap">
-						<div class="tribe-week-grid-inner-wrap">
-							<div class="tribe-week-grid-block"><div></div></div>
-							<div class="tribe-week-grid-block"><div></div></div>
-							<div class="tribe-week-grid-block"><div></div></div>
-							<div class="tribe-week-grid-block"><div></div></div>
-						</div><!-- .tribe-week-grid-inner-wrap -->
-					</div><!-- .tribe-week-grid-outer-wrap -->
-				</td>
-			</tr><!-- .tribe-week-grid-bgd -->
-			
-			<?php // our actual week grid columns, hours, and events ?>
-			<tr class="tribe-week-events-row hfeed">
-				<td class="tribe-week-grid-hours">
-					<div>7am</div>
-					<div>8am</div>
-					<div>9am</div>
-					<div>10am</div>
-				</td><!-- .tribe-week-grid-hours -->
-				<td class="tribe-week-grid-col tribe-week-today"></td><!-- .tribe-week-grid-col -->
-				<td class="tribe-week-grid-col">
-					<div class="tribe-week-grid-col-inner-wrap">
-						<div class="tribe-week-grid-event-wrap hentry vevent">
-							<div id="tribe-events-event-1">
-								<h3 class="entry-title summary"><a href="" class="url" rel="bookmark">MIT Theme Structure Long title</a></h3>
-								
-								
-								<div id="tribe-events-tooltip-1" class="tribe-events-tooltip">
-									<h4 class="entry-title summary">MIT Theme Structure Long title</h4>
-									<div class="tribe-events-event-body">
-										<div class="duration">
-											<abbr class="tribe-events-abbr updated published dtstart" title="">
-												Start Date 
-											</abbr><!-- .dtstart -->
-											<abbr class="tribe-events-abbr dtend" title="">
-												End Date
-											</abbr><!-- .dtend -->
-										</div><!-- .duration -->
-										<p class="entry-summary description">Test Description</p>
-									</div><!-- .tribe-events-event-body -->
-									<span class="tribe-events-arrow"></span>
-								</div><!-- .tribe-events-tooltip -->
-							
-								
-								
-								
-							</div>
-						</div><!-- .tribe-week-grid-event-wrap -->
-					</div><!-- .tribe-week-grid-col-inner-wrap -->
-				</td><!-- .tribe-week-grid-col -->
-				<td class="tribe-week-grid-col"></td><!-- .tribe-week-grid-col -->
-				<td class="tribe-week-grid-col">
-					<div class="tribe-week-grid-col-inner-wrap">
-						<div class="tribe-week-grid-event-wrap hentry vevent">
-							<div  id="tribe-events-event-2" style="height: 60px;">
-								<h3 class="entry-title summary"><a href="" class="entry-title summary url" rel="bookmark">MIT Theme Structure Long title</a></h3>
-							</div>
-						</div><!-- .tribe-week-grid-event-wrap -->
-						
-						<div style="width: 90%; top: 28px; right: 0; left: auto;" class="tribe-week-grid-event-wrap tribe-event-overlapping tribe-event-o-1 hentry vevent">
-							<div  id="tribe-events-event-3" style="height: 50px;">
-								<h3 class="entry-title summary"><a href="" class="entry-title summary url" rel="bookmark">Short Title</a></h3>
-							</div>
-						</div><!-- .tribe-week-grid-event-wrap -->
-						
-						<div style="width: 80%; top: 48px; right: 0; left: auto;" class="tribe-week-grid-event-wrap tribe-event-overlapping tribe-event-o-2 hentry vevent">
-							<div  id="tribe-events-event-4" style="height: 50px;">
-								<h3 class="entry-title summary"><a href="" class="entry-title summary url" rel="bookmark">Short Title</a></h3>
-							</div>
-						</div><!-- .tribe-week-grid-event-wrap -->
-					</div><!-- .tribe-week-grid-col-inner-wrap -->
-				</td><!-- .tribe-week-grid-col -->
-				<td class="tribe-week-grid-col"></td><!-- .tribe-week-grid-col -->
-				<td class="tribe-week-grid-col"></td><!-- .tribe-week-grid-col -->
-				<td class="tribe-week-grid-col">
-					<div class="tribe-week-grid-col-inner-wrap">
-						<div style="width: 80%;" class="tribe-week-grid-event-wrap tribe-event-same-time hentry vevent">
-							<div  id="tribe-events-event-5" style="height: 60px;">
-								<h3 class="entry-title summary"><a href="" class="entry-title summary url" rel="bookmark">MIT Theme Structure Long title</a></h3>
-							</div>
-						</div><!-- .tribe-week-grid-event-wrap -->
-						
-						<div style="width: 80%; right: 0; left: auto;" class="tribe-week-grid-event-wrap tribe-event-same-time tribe-event-st-1 hentry vevent">
-							<div  id="tribe-events-event-6" style="height: 60px;">
-								<h3 class="entry-title summary"><a href="" class="entry-title summary url" rel="bookmark">Short Title</a></h3>
-							</div>
-						</div><!-- .tribe-week-grid-event-wrap -->
-					</div><!-- .tribe-week-grid-col-inner-wrap -->
-				</td><!-- .tribe-week-grid-col -->
-			</tr><!-- .tribe-week-events-row -->
-					
-		</tbody>
-		
-	</table><!-- .tribe-events-grid -->
+		</div>
+	</div><!-- .tribe-grid-body -->
+	
+	
+</div><!-- .tribe-events-grid -->
 		
     <?php 
 			$html = ob_get_clean();
@@ -365,7 +272,7 @@ if( !class_exists('Tribe_Events_Week_Template')){
 			$dropdown = ob_get_clean();
 
 			// Display Week Navigation
-			$html = sprintf('<div id="tribe-events-header"><h3 class="tribe-events-visuallyhidden">%s</h3><ul class="tribe-events-sub-nav"><li class="tribe-events-nav-prev"><a href="%s" rel="pref">%s</a></li><li>%s</li><li class="tribe-events-nav-next"><a href="%s" rel="next">%s</a><img src="%s" class="ajax-loading" id="ajax-loading" alt="Loading events" /></li></ul></div>',
+			$html = sprintf('<div id="tribe-events-header"><h3 class="tribe-events-visuallyhidden">%s</h3><ul class="tribe-events-sub-nav"><li class="tribe-events-nav-prev"><a href="%s" rel="prev">%s</a></li><li>%s</li><li class="tribe-events-nav-next"><a href="%s" rel="next">%s</a><img src="%s" class="ajax-loading" id="ajax-loading" alt="Loading events" /></li></ul></div>',
 								__( 'Week Navigation', 'tribe-events-calendar' ),
 								tribe_get_last_week_permalink( $current_week ),
 								'&#x2190;' . tribe_get_previous_month_text(),
