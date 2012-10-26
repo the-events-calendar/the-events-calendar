0c67ad4d4ac3b59daf01055e372d1040c4370809
samuel estok <sam@faction23.com>
2012-10-26 13:29:49 -0700

    added title to ajax events

--- resources/tribe-events-ajax-calendar.js
+++ resources/tribe-events-ajax-calendar.js
@@ -9,30 +9,30 @@ jQuery( document ).ready( function ( $ ) {
 	
 		window.onpopstate = function(event) {	
 
-			var tribe_pop = false;
+			var tribe_nopop = false;
 			var pop_date = event.state.date;		
-			tribe_events_calendar_ajax_post( pop_date, null, tribe_pop );
+			tribe_events_calendar_ajax_post( pop_date, null, tribe_nopop );
 		};
 
 		$( '.tribe-events-calendar .tribe-events-nav a' ).live( 'click', function ( e ) {
 
 			e.preventDefault();
-			var tribe_pop = true;
+			var tribe_nopop = true;
 			var month_target = $( this ).attr( "data-month" );
 			var href_target = $( this ).attr( "href" );
-			tribe_events_calendar_ajax_post( month_target, href_target, tribe_pop );
+			tribe_events_calendar_ajax_post( month_target, href_target, tribe_nopop );
 		} );
 
 		$( '.tribe-events-calendar select.tribe-events-events-dropdown' ).live( 'change', function ( e ) {
 
-			var tribe_pop = true;
+			var tribe_nopop = true;
 			var baseUrl = $(this).parent().attr('action');
 			var date = $( '#tribe-events-events-year' ).val() + '-' + $( '#tribe-events-events-month' ).val();		
 			var href_target = baseUrl + date + '/';		
-			tribe_events_calendar_ajax_post( date, href_target, tribe_pop );
+			tribe_events_calendar_ajax_post( date, href_target, tribe_nopop );
 		} );
 
-		function tribe_events_calendar_ajax_post( date, href_target, tribe_pop ) {
+		function tribe_events_calendar_ajax_post( date, href_target, tribe_nopop ) {
 
 			$( '.ajax-loading' ).show();	
 
@@ -48,10 +48,12 @@ jQuery( document ).ready( function ( $ ) {
 					$( "#ajax-loading" ).hide();
 					if ( response !== '' ) {
 						$( '#tribe-events-content.tribe-events-calendar' ).html( response );
-						if( tribe_pop ) {
+						var page_title = $(response).find("#tribe-events-header").attr('data-title');	
+						$(document).attr('title', page_title);
+						if( tribe_nopop ) {
 							history.pushState({
 								"date": date
-							}, null, href_target);
+							}, page_title, href_target);
 						}
 					}
 				}
