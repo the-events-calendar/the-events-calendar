4761b5f3ea026cacdd9e71c82a0f4bc4871bf54f
Kyle Unzicker <kyle@wndfrm.com>
2012-11-03 15:11:19 -0500

    markup/skeleton styles

--- views/hooks/single-organizer.php
+++ views/hooks/single-organizer.php
@@ -62,23 +62,17 @@ if ( !class_exists( 'Tribe_Events_Pro_Single_organizer_Template' ) ) {
 		public static function the_meta( $post_id ) {
 			ob_start();
 ?>
-
-			<dt><?php echo __( 'Name:', 'tribe-events-calendar-pro' ); ?></dt>
-			<dd class="vcard fn org"><?php the_title(); ?></dd>
-
-			<?php if ( tribe_get_organizer_phone() ) : // organizer phone ?>
-				<dt><?php echo __( 'Phone:', 'tribe-events-calendar-pro' ); ?></dt>
- 				<dd class="vcard tel"><?php echo tribe_get_organizer_phone(); ?></dd>
- 			<?php endif; ?>
- 			<?php if ( tribe_get_organizer_link( get_the_ID(), false, false ) ) : // organizer phone ?>
-				<dt><?php echo __( 'Email:', 'tribe-events-calendar-pro' ); ?></dt>
- 				<dd class="vcard author fn org"><?php echo tribe_get_organizer_link(); ?></dd>
- 			<?php endif; ?>
- 			<?php if ( tribe_get_phone() ) : // organizer phone ?>
-				<dt><?php echo __( 'Website:', 'tribe-events-calendar-pro' ); ?></dt>
- 				<dd class="vcard email"><a href="mailto:<?php echo tribe_get_organizer_email(); ?>"><?php echo tribe_get_organizer_email(); ?></a></dd>
- 			<?php endif; ?>
-
+			<address class="organizer-address">
+				<?php if ( tribe_get_organizer_phone() ) : // organizer phone ?>
+	 				<span class="vcard tel"><?php echo tribe_get_organizer_phone(); ?></span>
+	 			<?php endif; ?>
+	 			<?php if ( tribe_get_organizer_link( get_the_ID(), false, false ) ) : // organizer phone ?>
+	 				<span class="vcard author fn org"><?php echo tribe_get_organizer_link(); ?></span>
+	 			<?php endif; ?>
+	 			<?php if ( tribe_get_organizer_email() ) : // organizer phone ?>
+	 				<span class="vcard email"><a href="mailto:<?php echo tribe_get_organizer_email(); ?>"><?php echo tribe_get_organizer_email(); ?></a></span>
+	 			<?php endif; ?>
+ 			</address>
 
 <?php
 			$html = ob_get_clean();
