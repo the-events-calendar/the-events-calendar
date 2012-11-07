ac6d973c03bef0d74dca8a643472280094c19eb2
Kyle Unzicker <kyle@wndfrm.com>
2012-11-07 11:37:14 -0600

    QAing wordpress default themes

--- views/wrapper-single.php
+++ views/wrapper-single.php
@@ -24,6 +24,7 @@ if ( !defined('ABSPATH') ) { die('-1'); }
 ?>
 
 <?php get_header(); ?>
+<div id="tribe-events-pg-template">
 	<?php tribe_events_before_html(); ?>
 
 		<?php the_post(); global $post; ?>
@@ -37,4 +38,5 @@ if ( !defined('ABSPATH') ) { die('-1'); }
 		<?php /* get_sidebar(); */ ?> 
 		
 	<?php tribe_events_after_html(); ?>
+</div> <!-- #tribe-events-pg-template -->	
 <?php get_footer(); ?>
