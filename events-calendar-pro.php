609f45cb44ec6da0638c301e5d091be0ccfdeeb4
Paul Hughes <paul@paulalexanderhughes.com>
2012-11-08 11:00:07 -0500

    Updated version numbers to 2.0.10, properly.
    Signed-off-by: Paul Hughes <paul@paulalexanderhughes.com>

--- events-calendar-pro.php
+++ events-calendar-pro.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: The Events Calendar PRO
 Description: The Events Calendar PRO, a premium add-on to the open source The Events Calendar plugin (required), enables recurring events, custom attributes, venue pages, new widgets and a host of other premium features.
-Version: 2.0.9
+Version: 2.0.10-alpha
 Author: Modern Tribe, Inc.
 Author URI: http://tri.be/?ref=ecp-plugin
 Text Domain: tribe-events-calendar-pro
@@ -40,8 +40,8 @@ if ( !class_exists( 'TribeEventsPro' ) ) {
 		public $pluginSlug;
 		public $licenseKey;
 		public static $updateUrl = 'http://tri.be/';
-		const REQUIRED_TEC_VERSION = '2.0.9';
-		const VERSION = '2.0.9';
+		const REQUIRED_TEC_VERSION = '2.0.10';
+		const VERSION = '2.0.10';
 
 	    private function __construct() {
 			$this->pluginDir = trailingslashit( basename( dirname( __FILE__ ) ) );
