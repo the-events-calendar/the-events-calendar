308bf87e5e4762fc5d18fbf53fcc4969a20f3b35
Paul Hughes <paul@paulalexanderhughes.com>
2012-11-08 15:59:18 -0500

    Fixes advanced list widget.
    Signed-off-by: Paul Hughes <paul@paulalexanderhughes.com>

--- views/events-advanced-list-load-widget-display.php
+++ views/events-advanced-list-load-widget-display.php
@@ -46,11 +46,10 @@ ob_end_clean();
 			echo tribe_get_start_date( $post->ID ); 
 			
 			if( $end && $event->EndDate != '' ) {
-				if( tribe_get_all_day( $post->ID ) != 'yes' || ( tribe_get_all_day( $post->ID ) == 'yes' &&
-					tribe_is_multiday( $post->ID ) ) ) { 
+				if ( ( tribe_get_all_day( $post->ID ) != 'yes' && tribe_is_multiday( $post->ID ) ) || ( tribe_get_all_day( $post->ID ) == 'yes' && tribe_is_multiday( $post->ID ) ) ) { 
 					echo ' – <br/>'. tribe_get_end_date($post->ID);
 				} elseif ( tribe_get_all_day( $post->ID ) != 'yes' && !tribe_is_multiday( $post->ID ) ) {
-					echo ' – <br/>'. tribe_get_end_date($post->ID, 'g:i a');
+					echo ' – <br/>'. tribe_get_end_date($post->ID, false, 'g:i a');
 				}
 			}
 
