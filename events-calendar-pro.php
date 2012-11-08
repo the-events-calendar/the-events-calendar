3b1c9e574e542e5826a7ccdf93cbd24c7d8d4ec8
Paul Hughes <paul@paulalexanderhughes.com>
2012-11-08 16:33:58 -0500

    Updates #18574 Updated version numbers from 2.0.10-alpha to 2.0.10. Language files did not require update.
    Signed-off-by: Paul Hughes <paul@paulalexanderhughes.com>

--- events-calendar-pro.php
+++ events-calendar-pro.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: The Events Calendar PRO
 Description: The Events Calendar PRO, a premium add-on to the open source The Events Calendar plugin (required), enables recurring events, custom attributes, venue pages, new widgets and a host of other premium features.
-Version: 2.0.10-alpha
+Version: 2.0.10
 Author: Modern Tribe, Inc.
 Author URI: http://tri.be/?ref=ecp-plugin
 Text Domain: tribe-events-calendar-pro
