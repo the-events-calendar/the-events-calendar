c7983a7a5b16ec896e50bf7bd1f5c7df3daff303
Timothy Wood <tim@imaginesimplicity.com>
2012-11-14 08:27:39 -0500

    updates #18255: hooked in photoview
    Signed-off-by: Timothy Wood <tim@imaginesimplicity.com>

---
+++ views/hooks/photo.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * @for Photo Template
+ * This file contains the hook logic required to create an effective day grid view.
+ *
+ * @package TribeEventsCalendarPro
+ * @since  3.0
+ * @author Modern Tribe Inc.
+ *
+ */
+
+if ( !defined('ABSPATH') ) { die('-1'); }
+
+
+if( !class_exists('Tribe_Events_Day_Template')){
+	class Tribe_Events_Day_Template extends Tribe_Template_Factory {
+
+		static $timeslots = array();
+
+		public static function init(){
+
+			wp_enqueue_script('isotope', get_template_directory_uri() . '/js/jquery.isotope.min.js', array( 'jquery' ), false , true);
+			wp_enqueue_style('photo-view', get_template_directory_uri() . '/css/photo-view.css', array(), '0.1', 'screen');
+
+			// override list methods
+			add_filter( 'tribe_events_list_before_loop', array( __CLASS__, 'before_loop'), 20, 1);
+			add_filter( 'tribe_events_list_inside_before_loop', array( __CLASS__, 'inside_before_loop'), 20, 1);
+
+		}
+
+		public static function before_loop( $pass_through ){
+			$html = '<div class="tribe-events-loop hfeed" id="tribe-event-photo-events">';
+			return apply_filters('tribe_template_factory_debug', $pass_through . $html, 'tribe_events_photo_before_loop');
+		}
+
+		public static function inside_before_loop( $pass_through ){
+			$post_id = get_the_ID();
+			// Get our wrapper classes (for event categories, organizer, venue, and defaults)
+			$tribe_string_classes = '';
+			$tribe_cat_ids = tribe_get_event_cat_ids( $post_id ); 
+			foreach( $tribe_cat_ids as $tribe_cat_id ) { 
+				$tribe_string_classes .= 'tribe-events-category-'. $tribe_cat_id .' '; 
+			}
+			$tribe_string_wp_classes = '';
+			$allClasses = get_post_class(); 
+			foreach ($allClasses as $class) { 
+				$tribe_string_wp_classes .= $class . ' '; 
+			}
+			$tribe_classes_default = 'hentry vevent '. $tribe_string_wp_classes;
+			$tribe_classes_venue = tribe_get_venue_id() ? 'tribe-events-venue-'. tribe_get_venue_id() : '';
+			$tribe_classes_organizer = tribe_get_organizer_id() ? 'tribe-events-organizer-'. tribe_get_organizer_id() : '';
+			$tribe_classes_categories = $tribe_string_classes;
+			$class_string = $tribe_classes_default .' '. $tribe_classes_venue .' '. $tribe_classes_organizer .' '. $tribe_classes_categories;		
+			$html = '<div id="post-'. $post_id .'" class="'. $class_string .' tribe-events-photo-event clearfix">';
+			return apply_filters('tribe_template_factory_debug', $html , 'tribe_events_day_inside_before_loop');
+		}
+
+	}
+	Tribe_Events_Day_Template::init();
+}
