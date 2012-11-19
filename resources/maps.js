f5e3b50625f7366b7d9e02598014ae16054a5560
samuel estok <sam@faction23.com>
2012-11-19 12:45:33 -0800

    initialize geo bug fix
    Updates: #18289

--- resources/maps.js
+++ resources/maps.js
@@ -4,6 +4,29 @@ tribe_map_bounds = new google.maps.LatLngBounds();
 
 var tribe_map_paged = 1;
 
+function tribe_process_geocoding( location, callback ) {
+
+	var request = {
+		address:location
+	};
+
+	tribe_map_geocoder.geocode( request, function ( results, status ) {
+		if ( status == google.maps.GeocoderStatus.OK ) {
+			callback( results );
+			return results;
+		}
+
+		if ( status == google.maps.GeocoderStatus.ZERO_RESULTS ) {
+			if( GeoLoc.map_view ) {
+				spin_end();
+			}
+			return status;
+		}
+
+		return status;
+	} );
+}
+
 jQuery( document ).ready( function ( $ ) {	
 
 	$( '#tribe-geo-location' ).placeholder();	
