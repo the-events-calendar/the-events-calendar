b0f4206802f0b21c687bbbc60064974e168ba74b
Kyle Unzicker <kyle@wndfrm.com>
2012-11-19 14:49:05 -0500

    setup of template
    - added necessary wrapper classes, filter overrides

--- views/hooks/photo.php
+++ views/hooks/photo.php
@@ -9,7 +9,8 @@
  *
  */
 
-if ( !defined('ABSPATH') ) { die('-1'); }
+if ( !defined('ABSPATH') ) { die('-1');
+}
 
 
 if( !class_exists('Tribe_Events_Day_Template')){
@@ -20,17 +21,18 @@ if( !class_exists('Tribe_Events_Day_Template')){
 		public static function init(){
 
 			Tribe_PRO_Template_Factory::asset_package('isotope');
-			wp_enqueue_script('tribe-events-pro-photo', trailingslashit( $tec_pro->pluginUrl ) . 'resources/' .  'tribe-events-photo-view.js', array('jquery'));
+			wp_enqueue_script('tribe-events-pro-photo', trailingslashit( TribeEventsPro::instance()->pluginUrl ) . 'resources/' .  'tribe-events-photo-view.js', array('jquery'));
 			
 
 			// override list methods
 			add_filter( 'tribe_events_list_before_loop', array( __CLASS__, 'before_loop'), 20, 1);
 			add_filter( 'tribe_events_list_inside_before_loop', array( __CLASS__, 'inside_before_loop'), 20, 1);
+			add_filter( 'tribe_events_list_the_content', array( __CLASS__, 'the_content'), 20, 1);
 
 		}
 
 		public static function before_loop( $pass_through ){
-			$html = '<div class="tribe-events-loop hfeed" id="tribe-event-photo-events">';
+			$html = '<div class="tribe-events-loop hfeed" id="tribe-events-photo-events">';
 			return apply_filters('tribe_template_factory_debug', $pass_through . $html, 'tribe_events_photo_before_loop');
 		}
 
@@ -52,8 +54,17 @@ if( !class_exists('Tribe_Events_Day_Template')){
 			$tribe_classes_organizer = tribe_get_organizer_id() ? 'tribe-events-organizer-'. tribe_get_organizer_id() : '';
 			$tribe_classes_categories = $tribe_string_classes;
 			$class_string = $tribe_classes_default .' '. $tribe_classes_venue .' '. $tribe_classes_organizer .' '. $tribe_classes_categories;		
-			$html = '<div id="post-'. $post_id .'" class="'. $class_string .' tribe-events-photo-event clearfix">';
+			$html = '<div id="post-'. $post_id .'" class="'. $class_string .' tribe-events-photo-event tribe-clearfix">';
 			return apply_filters('tribe_template_factory_debug', $html , 'tribe_events_day_inside_before_loop');
+		}
+
+		public static function the_content( $post_id ){
+			$html = '';
+			if (has_excerpt())
+				$html .= '<p>'. get_the_excerpt() .'</p>';
+			else
+				$html .= '<p>'. TribeEvents::truncate(get_the_content(), 20) .'</p>';	
+			return apply_filters('tribe_template_factory_debug', $html, 'tribe_events_photo_the_content');
 		}
 
 	}
