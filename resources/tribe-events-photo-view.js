373e4631b3b4c5571657438167da4d0e8a9ad4b6
Kyle Unzicker <kyle@wndfrm.com>
2012-11-19 14:49:14 -0500

    setup necessary js

--- resources/tribe-events-photo-view.js
+++ resources/tribe-events-photo-view.js
@@ -1,1 +1,12 @@
-// tribe-events-photo-view.js
+jQuery( document ).ready( function ( $ ) {
+		var $container = $('#tribe-events-photo-events');
+		 $(window).load(function(){ 
+		  $container.isotope();
+		 }); 
+
+ 
+		// update columnWidth on window resize
+		$(window).smartresize(function(){
+		  $container.isotope();
+		}).smartresize();	
+});
