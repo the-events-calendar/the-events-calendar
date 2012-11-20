9a389f95a7a683da46c7f7c5d73b2e66963cda04
Kyle Unzicker <kyle@wndfrm.com>
2012-11-20 09:06:57 -0500

    modified js options to show recurring tooltips
    changed overflow: hidden to overflow: visible

--- resources/tribe-events-photo-view.js
+++ resources/tribe-events-photo-view.js
@@ -1,12 +1,16 @@
 jQuery( document ).ready( function ( $ ) {
 		var $container = $('#tribe-events-photo-events');
 		 $(window).load(function(){ 
-		  $container.isotope();
+		  $container.isotope({
+		  	containerStyle: { position: 'relative', overflow: 'visible' }
+		  });
 		 }); 
 
  
 		// update columnWidth on window resize
 		$(window).smartresize(function(){
-		  $container.isotope();
+		  $container.isotope({
+		  	containerStyle: { position: 'relative', overflow: 'visible' }
+		  });
 		}).smartresize();	
 });
