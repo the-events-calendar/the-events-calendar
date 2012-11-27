8ea44964228f514271de6db2e23852fdf4f85b9b
Paul Hughes <paul@paulalexanderhughes.com>
2012-11-27 07:16:50 -0500

    Fixed the Network Settings notices that were showing up. Updates #17993
    Signed-off-by: Paul Hughes <paul@paulalexanderhughes.com>

--- lib/tribe-settings.class.php
+++ lib/tribe-settings.class.php
@@ -139,6 +139,8 @@ if ( !class_exists( 'TribeSettings' ) ) {
 			$this->major_error = get_option( 'tribe_settings_major_error', false );
 			$this->sent_data = get_option( 'tribe_settings_sent_data', array() );
 			$this->validated = array();
+			$this->defaultTab = null;
+			$this->currentTab = null;
 
 			// run actions & filters
 			add_action( 'admin_menu', array( $this, 'addPage' ) );
@@ -181,16 +183,17 @@ if ( !class_exists( 'TribeSettings' ) ) {
 		public function initTabs() {
 			if ( isset( $_GET['page'] ) && $_GET['page'] == $this->adminSlug ) {
 				do_action( 'tribe_settings_do_tabs' ); // this is the hook to use to add new tabs
-				$this->tabs = (array) apply_filters( 'tribe_settings_tabs', array() );
-				$this->allTabs = (array) apply_filters( 'tribe_settings_all_tabs', array() );
 				if ( is_network_admin() )
-					$this->defaultTab = apply_filters( 'tribe_settings_default_tab', 'network' );
+					$this->defaultTab = apply_filters( 'tribe_settings_default_tab_network', 'network' );
+					$this->currentTab = apply_filters( 'tribe_settings_current_tab', ( isset( $_GET['tab'] ) && $_GET['tab'] ) ? esc_attr( $_GET['tab'] ) : $this->defaultTab );
 					$this->url = apply_filters( 'tribe_settings_url', add_query_arg( array( 'page' => $this->adminSlug, 'tab' => $this->currentTab ), network_admin_url( 'settings.php' ) ) );
 				if( !is_network_admin() ) {
 					$this->defaultTab = apply_filters( 'tribe_settings_default_tab', 'general' );
+					$this->currentTab = apply_filters( 'tribe_settings_current_tab', ( isset( $_GET['tab'] ) && $_GET['tab'] ) ? esc_attr( $_GET['tab'] ) : $this->defaultTab );
 					$this->url = apply_filters( 'tribe_settings_url', add_query_arg( array( 'page' => $this->adminSlug, 'tab' => $this->currentTab ), add_query_arg( array( 'post_type' => TribeEvents::POSTTYPE ), admin_url( 'edit.php' ) ) ) );
 				}
-				$this->currentTab = apply_filters( 'tribe_settings_current_tab', ( isset( $_GET['tab'] ) && $_GET['tab'] ) ? esc_attr( $_GET['tab'] ) : $this->defaultTab );
+				$this->tabs = (array) apply_filters( 'tribe_settings_tabs', array() );
+				$this->allTabs = (array) apply_filters( 'tribe_settings_all_tabs', array() );
 				$this->noSaveTabs = (array) apply_filters( 'tribe_settings_no_save_tabs', array() );
 				$this->fields_for_save = (array) apply_filters( 'tribe_settings_fields', array() );
 				do_action( 'tribe_settings_after_do_tabs' );
