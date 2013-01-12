4f29a33ff9b2fd4273a6266d636b31b63ddec830
samuel estok <sam@faction23.com>
2013-01-12 09:45:03 -0800

    update calendar js to new namespace
    updates #19377

--- resources/tribe-events-ajax-calendar.js
+++ resources/tribe-events-ajax-calendar.js
@@ -1,78 +1,62 @@
 jQuery( document ).ready( function ( $ ) {
-
-	// Add select2 to our month/year selects
+	
+	var base_url = $('#tribe-events-header .tribe-nav-next a').attr('href').slice(0, -8);
 	
 	if($('.tribe-events-calendar').length) {
 		$( '.tribe-events-events-dropdown' ).select2({
 			minimumResultsForSearch: 9999
 		});
 	}
-	
-	function tribe_get_if_day_is_set(){
-		var dp_day = '';
-		if( $('#tribe-bar-date').length ) {
-			var dp_date = $('#tribe-bar-date').val();
-			if( dp_date.length )
-				dp_day = dp_date.slice(-3);
-		}
-		return dp_day;
-	}
-	
-	var tribe_base_url = $('#tribe-events-events-picker').attr('action');	
-	
-	if( typeof GeoLoc === 'undefined' ) 
-		var GeoLoc = {"map_view":""};
 
-	if( tribe_ev.tests.pushstate && !GeoLoc.map_view ) {	
+	if( tribe_ev.tests.pushstate && !tribe_ev.tests.map_view() ) {		
+					
+		var params = 'action=tribe_calendar&eventDate=' + $('#tribe-events-header').attr('data-date');
+
+		if( tribe_ev.data.params.length ) 
+			params = params + '&' + tribe_ev.data.params;	
 		
-		var initial_url = location.href;
+		history.replaceState({									
+			"tribe_params": params
+		}, '', location.href);	
 		
-		if( tribe_storage )
-			tribe_storage.setItem( 'tribe_initial_load', 'true' );	
+		$(window).on('popstate', function(event) {		
 		
-		$(window).on('popstate', function(event) {
-		
-		var initial_load = '';
-		if( tribe_storage )
-			initial_load = tribe_storage.getItem( 'tribe_initial_load' );	
-		
-			var state = event.originalEvent.state;
+			var state = event.originalEvent.state;				
 
 			if( state ) {			
-				tribe_do_string = false;
-				tribe_pushstate = false;	
-				tribe_popping = true;
-				tribe_params = state.tribe_params;
+				tribe_ev.state.do_string = false;
+				tribe_ev.state.pushstate = false;	
+				tribe_ev.state.popping = true;
+				tribe_ev.state.params = state.tribe_params;
 				tribe_ev.fn.pre_ajax( function() {
-					tribe_events_calendar_ajax_post( '', '', tribe_pushstate, tribe_do_string, tribe_popping, tribe_params );
+					tribe_events_calendar_ajax_post();	
 				});
-			} else if( tribe_storage && initial_load !== 'true' ){				
-				window.location = initial_url;
-			}
+				
+				tribe_ev.fn.set_form( tribe_ev.state.params );	
+			} 
 		} );
 	}
 
 	$( '#tribe-events-content' ).on( 'click', '.tribe-events-sub-nav a', function ( e ) {
-		e.preventDefault();		
-		tribe_date = $( this ).attr( "data-month" );
-		$( '#tribe-bar-date' ).val(tribe_date + tribe_get_if_day_is_set());
-		tribe_href_target = $( this ).attr( "href" );
-		tribe_pushstate = true;
-		tribe_do_string = false;
+		e.preventDefault();
+		var $this = $(this);
+		tribe_ev.state.date = $this.attr( "data-month" );
+		$( '#tribe-bar-date' ).val(tribe_ev.state.date + tribe_ev.fn.get_day());			
+		tribe_ev.data.cur_url = $this.attr( "href" );
+		tribe_ev.state.popping = false;
 		tribe_ev.fn.pre_ajax( function() { 		
-			tribe_events_calendar_ajax_post( tribe_date, tribe_href_target, tribe_pushstate, tribe_do_string );	
+			tribe_events_calendar_ajax_post();	
 		});
 	} );	
 
 	$( '#tribe-events-bar' ).on( 'change', '#tribe-bar-dates select', function ( e ) {
-		e.preventDefault();			
-		tribe_date = $( '#tribe-events-events-year' ).val() + '-' + $( '#tribe-events-events-month' ).val();		
-		$( '#tribe-bar-date' ).val(tribe_date + tribe_get_if_day_is_set());
-		tribe_href_target = tribe_base_url + tribe_date + '/';		
-		tribe_pushstate = true;
-		tribe_do_string = false;
+		e.preventDefault();				
+		tribe_ev.state.date = $( '#tribe-events-events-year' ).val() + '-' + $( '#tribe-events-events-month' ).val();	
+		$( '#tribe-bar-date' ).val(tribe_ev.state.date + tribe_ev.fn.get_day());		
+		tribe_ev.data.cur_url = base_url + tribe_ev.state.date + '/';		
+		tribe_ev.state.popping = false;
 		tribe_ev.fn.pre_ajax( function() { 
-			tribe_events_calendar_ajax_post( tribe_date, tribe_href_target, tribe_pushstate, tribe_do_string );	
+			tribe_events_calendar_ajax_post();	
 		});
 	} );	
 
@@ -82,17 +66,12 @@ jQuery( document ).ready( function ( $ ) {
 	
 	function tribe_events_bar_calajax_actions(e) {
 		if( tribe_events_bar_action != 'change_view' ) {
-
-			e.preventDefault();			
-
-			tribe_date = $('#tribe-events-header').attr('data-date');
-			tribe_href_target = tribe_ev.data.cur_url;		
-
-			tribe_pushstate = false;
-			tribe_do_string = true;			
-			
+			e.preventDefault();	
+			tribe_ev.state.date = $('#tribe-events-header').attr('data-date');
+			tribe_ev.data.cur_url = tribe_ev.data.initial_url;
+			tribe_ev.state.popping = false;
 			tribe_ev.fn.pre_ajax( function() { 
-				tribe_events_calendar_ajax_post( tribe_date, tribe_href_target, tribe_pushstate, tribe_do_string );
+				tribe_events_calendar_ajax_post();
 			});		
 		}
 	}
@@ -103,10 +82,7 @@ jQuery( document ).ready( function ( $ ) {
 	
 	$( '.tribe-bar-settings button[name="settingsUpdate"]' ).on( 'click', function (e) {		
 		tribe_events_bar_calajax_actions(e);
-		$( '#tribe-events-bar [class^="tribe-bar-button-"]' )
-			.removeClass( 'open' )
-			.next( '.tribe-bar-drop-content' )
-			.hide();
+		tribe_ev.fn.hide_settings();
 	} );
 
 	// if advanced filters active intercept submit
@@ -119,26 +95,23 @@ jQuery( document ).ready( function ( $ ) {
 			
 			$form.find('input[type="submit"]').remove();
 			
+			function run_filtered_month_ajax() {				
+				tribe_ev.state.date = $( '#tribe-events-header' ).attr( 'data-date' );	
+				tribe_ev.data.cur_url = tribe_ev.data.initial_url;
+				tribe_ev.state.popping = false;
+				tribe_ev.fn.pre_ajax( function() { 
+					tribe_events_calendar_ajax_post();
+				});				
+			}
+			
 			$( "#tribe_events_filters_form" ).on( "slidechange", ".ui-slider", function() {
-				if( !$('body').hasClass('tribe-reset-on') ){
-					tribe_date = $( '#tribe-events-header' ).attr( 'data-date' );					
-					tribe_href_target = tribe_ev.data.cur_url;
-					tribe_pushstate = false;
-					tribe_do_string = true;
-					tribe_ev.fn.pre_ajax( function() { 
-						tribe_events_calendar_ajax_post( tribe_date, tribe_href_target, tribe_pushstate, tribe_do_string );
-					});
-				}			
+				if( !tribe_ev.tests.reset_on() ){
+					run_filtered_month_ajax();
+				}
 			} );
 			$("#tribe_events_filters_form").on("change", "input, select", function(){
-				if( !$('body').hasClass('tribe-reset-on') ){
-					tribe_date = $( '#tribe-events-header' ).attr( 'data-date' );					
-					tribe_href_target = tribe_ev.data.cur_url;
-					tribe_pushstate = false;
-					tribe_do_string = true;
-					tribe_ev.fn.pre_ajax( function() { 
-						tribe_events_calendar_ajax_post( tribe_date, tribe_href_target, tribe_pushstate, tribe_do_string );
-					});
+				if( !tribe_ev.tests.reset_on() ){
+					run_filtered_month_ajax();
 				}
 			});			
 		}		
@@ -146,69 +119,66 @@ jQuery( document ).ready( function ( $ ) {
 		$form.on( 'submit', function ( e ) {
 			if ( tribe_events_bar_action != 'change_view' ) {
 				e.preventDefault();
-				tribe_date = $( '#tribe-events-header' ).attr( 'data-date' );					
-				tribe_href_target = tribe_ev.data.cur_url;
-				tribe_pushstate = false;
-				tribe_do_string = true;
+				tribe_ev.state.date = $( '#tribe-events-header' ).attr( 'data-date' );	
+				tribe_ev.data.cur_url = tribe_ev.data.initial_url;
+				tribe_ev.state.popping = false;
 				tribe_ev.fn.pre_ajax( function() { 
-					tribe_events_calendar_ajax_post( tribe_date, tribe_href_target, tribe_pushstate, tribe_do_string );
-				});
+					tribe_events_calendar_ajax_post();
+				});	
 			}
 		} );
 	}	
 
 
-	function tribe_events_calendar_ajax_post( tribe_date, tribe_href_target, tribe_pushstate, tribe_do_string, tribe_popping, tribe_params ) {
+	function tribe_events_calendar_ajax_post() {
 
-		$( '#tribe-events-footer, #tribe-events-header' ).find('.tribe-ajax-loading').show();
+		tribe_ev.fn.spin_show();
+		tribe_ev.state.pushcount = 0;
 		
-		if( !tribe_popping ) {		
+		if( !tribe_ev.state.popping ) {		
 
-			tribe_params = {
+			tribe_ev.state.params = {
 				action:'tribe_calendar',
-				eventDate:tribe_date
+				eventDate:tribe_ev.state.date
 			};
 			
-			tribe_url_params = {};
-
-			// add any set values from event bar to params. want to use serialize but due to ie bug we are stuck with second	
+			tribe_ev.state.url_params = {};
 
 			$( 'form#tribe-bar-form :input[value!=""]' ).each( function () {
 				var $this = $( this );
 				if( $this.val().length && !$this.hasClass('tribe-no-param') ) {
 					if( $this.is(':checkbox') ) {
 						if( $this.is(':checked') ) {
-							tribe_params[$this.attr('name')] = $this.val();
-							tribe_url_params[$this.attr('name')] = $this.val();
-							tribe_push_counter++;
+							tribe_ev.state.params[$this.attr('name')] = $this.val();
+							tribe_ev.state.url_params[$this.attr('name')] = $this.val();
+							tribe_ev.state.pushcount++;
 						}
 					} else {
-						tribe_params[$this.attr('name')] = $this.val();
-						tribe_url_params[$this.attr('name')] = $this.val();
-						tribe_push_counter++;
+						tribe_ev.state.params[$this.attr('name')] = $this.val();
+						tribe_ev.state.url_params[$this.attr('name')] = $this.val();
+						tribe_ev.state.pushcount++;
 					}					
 				}			
 			} );
 
-			tribe_params = $.param(tribe_params);
-			tribe_url_params = $.param(tribe_url_params);
-
-			// check if advanced filters plugin is active
+			tribe_ev.state.params = $.param(tribe_ev.state.params);
+			tribe_ev.state.url_params = $.param(tribe_ev.state.url_params);
 
 			if( $('#tribe_events_filters_form').length ) {
 
-				// serialize any set values and add to params
-
-				tribe_filter_params = $('form#tribe_events_filters_form :input[value!=""]').serialize();				
+				var tribe_filter_params = $('form#tribe_events_filters_form :input[value!=""]').serialize();				
 				if( tribe_filter_params.length ) {
-					tribe_params = tribe_params + '&' + tribe_filter_params;
-					tribe_url_params = tribe_url_params + '&' + tribe_filter_params;
+					tribe_ev.state.params = tribe_ev.state.params + '&' + tribe_filter_params;
+					tribe_ev.state.url_params = tribe_ev.state.url_params + '&' + tribe_filter_params;
 				}
 			}			
 			
-			if ( tribe_push_counter > 0 || tribe_filter_params != '' ) {
-				tribe_pushstate = false;
-				tribe_do_string = true;				
+			if ( tribe_ev.state.pushcount > 0 || tribe_filter_params != '' ) {
+				tribe_ev.state.do_string = true;
+				tribe_ev.state.pushstate = false;			
+			} else {
+				tribe_ev.state.do_string = false;
+				tribe_ev.state.pushstate = true;
 			}
 			
 			
@@ -218,11 +188,12 @@ jQuery( document ).ready( function ( $ ) {
 
 			$.post(
 				TribeCalendar.ajaxurl,
-				tribe_params,
+				tribe_ev.state.params,
 				function ( response ) {
-					$( '#tribe-events-footer, #tribe-events-header' ).find('.tribe-ajax-loading').hide();
-					if( tribe_storage )
-						tribe_storage.setItem( 'tribe_initial_load', 'false' );
+					
+					tribe_ev.fn.spin_hide();
+					tribe_ev.state.initial_load = false;	
+					
 					if ( response !== '' ) {
 						
 						tribe_ev.data.ajax_response = {
@@ -246,32 +217,29 @@ jQuery( document ).ready( function ( $ ) {
 						
 						$(document).attr('title', page_title);
 						
-						if( tribe_do_string ) {							
-							tribe_href_target = tribe_href_target + '?' + tribe_url_params;								
+						if( tribe_ev.state.do_string ) {							
+							tribe_ev.data.cur_url = tribe_ev.data.cur_url + '?' + tribe_ev.state.url_params;								
 							history.pushState({
-								"tribe_date": tribe_date,
-								"tribe_params": tribe_params
-							}, page_title, tribe_href_target);															
+								"tribe_date": tribe_ev.state.date,
+								"tribe_params": tribe_ev.state.params
+							}, page_title, tribe_ev.data.cur_url);															
 						}						
 
-						if( tribe_pushstate ) {								
+						if( tribe_ev.state.pushstate ) {								
 							history.pushState({
-								"tribe_date": tribe_date,
-								"tribe_params": tribe_params
-							}, page_title, tribe_href_target);
+								"tribe_date": tribe_ev.state.date,
+								"tribe_params": tribe_ev.state.params
+							}, page_title, tribe_ev.data.cur_url);
 						}
 					}
 				}
 			);
 				
-		} else {
-			
-			if( tribe_do_string ) {
-				tribe_href_target = tribe_href_target + '?' + tribe_url_params;													
-			}			
-			
-			window.location = tribe_href_target;			
+		} else {			
+			if( tribe_ev.state.do_string ) 
+				window.location = tribe_ev.data.cur_url + '?' + tribe_ev.state.url_params;													
+			else			
+				window.location = tribe_ev.data.cur_url;			
 		}
-	}
-	
+	}	
 } );
