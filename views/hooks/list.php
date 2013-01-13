8cda7606c80e1adb019c71595adee60be6b361a8
Ryan Urban <ryan@fringewebdesign.com>
2013-01-13 16:46:36 -0500

    Update address conditionals

--- views/hooks/list.php
+++ views/hooks/list.php
@@ -288,12 +288,11 @@ if( !class_exists('Tribe_Events_List_Template')){
 				$venue_name = tribe_get_meta( 'tribe_event_venue_name' );
 				$venue_address = tribe_get_meta('tribe_event_venue_address');
 				
-				if( !empty($venue_name) )
-					printf('<h3 class="tribe-venue-details">%s%s%s</h3>',
-						$venue_name,
-						!empty($venue_address) ? ', ' : '',
-						!empty($venue_address) ? $venue_address : ''
-					);
+				printf('<h3 class="tribe-venue-details">%s%s%s</h3>',
+					'|'.$venue_name.'|',
+					!empty($venue_name) && !empty($venue_address) ? ', ' : '',
+					!empty($venue_address) ? $venue_address : ''
+				);
 
 				?>
 			</div><!-- .tribe-events-event-meta -->
