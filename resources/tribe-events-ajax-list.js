8a21df75ea77f107ba5573fba840ed0ca6f8892b
samuel estok <sam@faction23.com>
2013-01-21 09:59:09 -0800

    fix bug with jq 1.9 and parama filtering in ajax

--- resources/tribe-events-ajax-list.js
+++ resources/tribe-events-ajax-list.js
@@ -152,7 +152,7 @@ jQuery( document ).ready( function ( $ ) {
 				tribe_ev.state.params['hash'] = tribe_hash_string;
 			}
 
-			$( 'form#tribe-bar-form :input[value!=""]' ).each( function () {					
+			$( 'form#tribe-bar-form input[value!=""]' ).each( function () {					
 				var $this = $( this );
 				if( $this.val().length && !$this.hasClass('tribe-no-param') ) {
 					if( $this.is(':checkbox') ) {
@@ -173,7 +173,7 @@ jQuery( document ).ready( function ( $ ) {
 			if( $('#tribe_events_filters_form').length ) {
 
 				tribe_ev.fn.enable_inputs( '#tribe_events_filters_form', 'input, select' );
-				var tribe_filter_params = $('form#tribe_events_filters_form :input[value!=""]').serialize();
+				var tribe_filter_params = $('form#tribe_events_filters_form input[value!=""]').serialize();
 				tribe_ev.fn.disable_inputs( '#tribe_events_filters_form', 'input, select' );			
 				if( tribe_filter_params.length ) {
 					tribe_ev.state.params = tribe_ev.state.params + '&' + tribe_filter_params;
