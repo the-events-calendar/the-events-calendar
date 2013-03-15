c507646c91921c0ece14bab21289fdcf0555903e
Jonathan Brinley <jbrinley@adeliedesign.com>
2013-03-15 17:58:03 -0400

    add view param to list view ajax response

--- lib/the-events-calendar.class.php
+++ lib/the-events-calendar.class.php
@@ -3542,7 +3542,9 @@ if ( !class_exists( 'TribeEvents' ) ) {
 			                   'max_pages'       => $query->max_num_pages,
 			                   'hash'            => $hash_str,
 			                   'tribe_paged'     => $tribe_paged,
-			                   'total_count'	 => $query->found_posts );
+			                   'total_count'     => $query->found_posts,
+			                   'view'            => 'list',
+			);
 
 
 			remove_action( 'pre_get_posts', array( $this, 'list_ajax_call_set_date' ), -10 );
