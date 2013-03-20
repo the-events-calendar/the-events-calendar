a7b43891367b8d824b4224df8c8b5ee95d0cb34d
Ryan Urban <ryan@fringewebdesign.com>
2013-03-20 13:57:53 -0400

    update typo

--- lib/tribe-pro-template-factory.class.php
+++ lib/tribe-pro-template-factory.class.php
@@ -12,14 +12,14 @@ if( !class_exists('Tribe_PRO_Template_Factory') ) {
 			$prefix = 'tribe-events-pro';
 
 			// setup plugin resources & 3rd party vendor urls
-			$resouces_url = trailingslashit( $tec_pro->pluginUrl ) . 'resources/';
+			$resources_url = trailingslashit( $tec_pro->pluginUrl ) . 'resources/';
 			$vendor_url = trailingslashit( $tec_pro->pluginUrl ) . 'vendor/';
 
 			switch( $name ) {
 				case 'ajax-weekview' :					
 					$ajax_data = array( "ajaxurl"     => admin_url( 'admin-ajax.php', ( is_ssl() ? 'https' : 'http' ) ),
 					                    'post_type' => TribeEvents::POSTTYPE );
-					wp_enqueue_script('tribe-events-pro-week', $resouces_url . 'tribe-events-week.js', array('jquery'), false, false);
+					wp_enqueue_script('tribe-events-pro-week', $resources_url . 'tribe-events-week.js', array('jquery'), false, false);
 					wp_enqueue_script( 'tribe-events-pro-slimscroll', $vendor_url . 'jquery-slimscroll/jquery.slimscroll.min.js', array('jquery-ui-draggable'), NULL );	
 					wp_localize_script( 'tribe-events-pro-week', 'TribeWeek', $ajax_data );
 					break;					
@@ -28,13 +28,13 @@ if( !class_exists('Tribe_PRO_Template_Factory') ) {
 					$ajax_data = array( "ajaxurl"     => admin_url( 'admin-ajax.php', ( is_ssl() ? 'https' : 'http' ) ),
 					                    'tribe_paged' => $tribe_paged );
 					wp_enqueue_script( 'tribe-events-pro-isotope', $vendor_url . 'isotope/jquery.isotope.min.js', array(), NULL );	
-					wp_enqueue_script('tribe-events-pro-photo', $resouces_url . 'tribe-events-photo-view.js', array('jquery'));
+					wp_enqueue_script('tribe-events-pro-photo', $resources_url . 'tribe-events-photo-view.js', array('jquery'));
 					wp_localize_script( 'tribe-events-pro-photo', 'TribePhoto', $ajax_data );
 					break;					
 				case 'ajax-dayview':
 					$ajax_data = array( "ajaxurl"   => admin_url( 'admin-ajax.php', ( is_ssl() ? 'https' : 'http' ) ),
 					                    'post_type' => TribeEvents::POSTTYPE );
-					wp_enqueue_script( 'tribe-events-pro-ajax-day', $resouces_url . 'tribe-events-ajax-day.js' );
+					wp_enqueue_script( 'tribe-events-pro-ajax-day', $resources_url . 'tribe-events-ajax-day.js' );
 					wp_localize_script( 'tribe-events-pro-ajax-day', 'TribeCalendar', $ajax_data );
 					break;
 			}
