7c3d9e1d8ba1a6f1ad84d7834cb9f6e99e29f76c
Daniel Dvorkin <daniel@mzaweb.com>
2013-03-21 02:04:12 -0300

    [Tickets PRO]
    Tweaks to the email template for the attendee list

--- views/tickets-attendees-email.php
+++ views/tickets-attendees-email.php
@@ -14,28 +14,35 @@
 	$head_style  = 'background:#444444; color:#ffffff; padding:15px;';
 	$odd_style  = 'background:#eeeeee; color:#222222; padding:15px; border-bottom:1px solid #ccc;';
 	$even_style = 'background:#ffffff; color:#222222; padding:15px; border-bottom:1px solid #ccc;';
-	$cell_type = 'th';
+
 
 	foreach ( $items as $item ) {
 		$count ++;
-		if ( $count === 1 ) echo '<thead><tr>';
-		if ( $count === 2 ) echo '<tbody>';
-		if ( $count > 1 ) {
-			echo '<tr>';
+		if ( $count === 1 ) {
+			$cell_type = 'th';
+			$style = $head_style;
+			echo '<thead>';
+		}
+		if ( $count === 2 ) {
 			$cell_type = 'td';
+			echo '<tbody>';
+		}
+
+		if ( $count > 1 ) {
 			if ( $count % 2 == 0 )
 				$style = $odd_style;
 			else
 				$style = $even_style;
-		} else {
-			$style = $head_style;
 		}
+
+		echo '<tr>';
 
 		foreach ( $item as $field ) {
 			echo sprintf( '<%1$s valign="top" style="%2$s">%3$s</%1$s>', esc_attr( $cell_type ), esc_attr( $style ), esc_html( $field ) );
 		}
 
 		echo '</tr>';
+
 		if ( $count === 1 ) echo '</thead>';
 		if ( $count === count( $items ) ) echo '</tbody>';
 
