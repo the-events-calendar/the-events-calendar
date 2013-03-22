67524de39d0ea684d216e20ed6a7b70ed8b5548a
Samuel Estok <drone@faction23.com>
2013-03-21 19:11:52 -0700

    fix select2 width in admin

--- resources/events-admin.js
+++ resources/events-admin.js
@@ -2,7 +2,7 @@ jQuery(document).ready(function($) {
 
 	// Load the Chosen JQuery plugin for all select elements with the class 'chosen'.
 	$('.chosen, .tribe-field-dropdown_chosen select').chosen();
-	$('.select2, .tribe-field-dropdown_select2 select').select2();
+	$('.select2, .tribe-field-dropdown_select2 select').select2({width: '250px'});
 
 	//not done by default on front end
 	$('.hide-if-js').hide();
