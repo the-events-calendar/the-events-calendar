b16141f0db81c9db5b8c676cc556afd815b3f3e4
Samuel Estok <drone@faction23.com>
2013-03-22 10:43:46 -0700

    hack to get select2 to display text for empty value

--- resources/events-admin.js
+++ resources/events-admin.js
@@ -4,6 +4,12 @@ jQuery(document).ready(function($) {
 	$('.chosen, .tribe-field-dropdown_chosen select').chosen();
 	$('.select2, .tribe-field-dropdown_select2 select').select2({width: '250px'});
 
+	if($('select[name="tribeEventsTemplate"]' ).length && $('select[name="tribeEventsTemplate"]' ).val() === '' ){
+		$templates = $('select[name="tribeEventsTemplate"]' );
+		var name = $templates.find("option:selected" ).text();
+		$templates.prev('.select2-container' ).children().children('span').text(name);
+	}
+
 	//not done by default on front end
 	$('.hide-if-js').hide();
 
