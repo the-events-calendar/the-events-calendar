ce1189b9086b796c814611c131889493220b800d
Samuel Estok <drone@faction23.com>
2013-03-26 14:07:31 -0700

    forgot line break in venue meta

--- views/tickets-email.php
+++ views/tickets-email.php
@@ -329,7 +329,7 @@
 													</td>
 													<td class="ticket-venue-child" valign="top" align="left" width="100" style="padding: 0 !important; width:140px; margin:0 !important;">
 														<span style="color:#0a0a0e !important; font-family: 'Helvetica Neue', Helvetica, sans-serif; font-size:13px; display:block; margin-bottom:5px;"><?php echo $venue_phone;?></span>
-														<a href="#" style="color:#006caa !important; display:block; margin:0; font-family: 'Helvetica Neue', Helvetica, sans-serif; font-size:13px; text-decoration:underline;"><?php echo $venue_email;?></a>
+														<a href="#" style="color:#006caa !important; display:block; margin:0; font-family: 'Helvetica Neue', Helvetica, sans-serif; font-size:13px; text-decoration:underline;"><?php echo $venue_email;?></a><br />
 														<a href="#" style="color:#006caa !important; display:block; margin:0; font-family: 'Helvetica Neue', Helvetica, sans-serif; font-size:13px; text-decoration:underline;"><?php echo $venue_web;?></a>
 													</td>
 												</tr>
