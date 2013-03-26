8c9fab4f5383f0049493dfc9429c2c1cec7fd73f
Ryan Urban <ryan@fringewebdesign.com>
2013-03-26 16:02:30 -0400

    Update #19363 - updated styles for updated map view

--- views/hooks/map.php
+++ views/hooks/map.php
@@ -21,9 +21,13 @@ if( !class_exists('Tribe_Events_Map_Template')){
 			add_filter( 'tribe_events_list_before_template', array( __CLASS__, 'before_template' ), 20, 1 );
 			add_filter( 'tribe_events_list_before_template', array( __CLASS__, 'the_map' ), 20, 1 );
 			add_filter( 'tribe_events_list_before_template', array( __CLASS__, 'the_options' ), 20, 1 );
+			add_filter( 'tribe_events_list_the_title', array( __CLASS__, 'the_title' ), 20, 1 );
 	
 			// Results
 			add_filter( 'tribe_events_list_before_loop', array( __CLASS__, 'before_the_results' ), 20, 1 );
+
+			// End list template
+			add_filter( 'tribe_events_list_after_template', array( __CLASS__, 'after_template' ), 20, 1 );
 
 		}
 		// Start Map Template
@@ -48,10 +52,24 @@ if( !class_exists('Tribe_Events_Map_Template')){
 			return apply_filters('tribe_template_factory_debug', $html, 'tribe_events_map_the_options');
 		}
 
+		public static function the_title( $html ){
+			$html = '<div id="tribe-events-geo-content">';
+			$html .= sprintf( '<h2 class="tribe-events-page-title">%s</h2>',
+				tribe_get_events_title()
+				);
+			return apply_filters('tribe_template_factory_debug', $html, 'tribe_events_list_the_title');
+		}
+
 		// Start Results
 		public static function before_the_results( $html ){
 			$html = '<div id="tribe-geo-results">';
 			return apply_filters('tribe_template_factory_debug', $html, 'tribe_events_map_before_the_results');
+		}
+
+		// End List Template
+		public static function after_template( $html ){
+			$html .= '</div><!-- #tribe-events-geo-content -->';
+			return apply_filters('tribe_template_factory_debug', $html, 'tribe_events_list_after_template');		
 		}
 
 	}
