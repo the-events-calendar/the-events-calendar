26cb4836d6f1e97c83211c9b57ad9be900a10a86
jessica <jessica@exobi.com>
2013-04-10 23:56:32 -0400

    Cleaned up calendar/nav-header.php
    See ticket #20696

--- views/calendar/nav-header.php
+++ views/calendar/nav-header.php
@@ -14,25 +14,16 @@
  */
 ?>
 
-<?php 
-
-$current_month_date = tribe_get_month_view_date()
-$prev_month_date = $tribe_ecp->previousMonth($current_month_view);
-$prev_month_link = tribe_get_previous_month_link();
-$prev_month_text = tribe_get_previous_month_text();
-
-?>
-
 <?php do_action('tribe_events_calendar_before_header_nav') ?>
 
 <h3 class="tribe-events-visuallyhidden"><?php _e( 'Calendar Month Navigation', 'tribe-events-calendar' ) ?></h3>
 
 <ul class="tribe-events-sub-nav">
 	<li class="tribe-events-nav-previous">
-			<?php tribe_previous_month_link(); ?>
+			<?php tribe_events_previous_month_link(); ?>
 	</li><!-- .tribe-events-nav-previous -->
 	<li class="tribe-events-nav-next">
-		<?php tribe_next_month_link(); ?>
+		<?php tribe_events_next_month_link(); ?>
 		<img class="tribe-events-ajax-loading tribe-events-spinner-medium" src="<?php tribe_events_resource_url('images/tribe-loading.gif') ?>" alt="<?php _e('Loading Events', 'tribe-events') ?>" />	
 	</li><!-- .tribe-events-nav-next -->
 </ul><!-- .tribe-events-sub-nav -->
