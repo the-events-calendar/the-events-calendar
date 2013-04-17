928b3ba382ec5ec641cc21150271baba224ada0a
jessica <jessica@exobi.com>
2013-04-16 22:39:10 -0400

    New photo view using new template structure
    See ticket #20696

--- views/hooks/photo.php
+++ views/hooks/photo.php
@@ -9,17 +9,59 @@
  *
  */
 
-if ( !defined('ABSPATH') ) { die('-1');
-}
+if ( !defined('ABSPATH') ) 
+	die('-1');
 
 if( !class_exists('Tribe_Events_Photo_Template')){
-	class Tribe_Events_Photo_Template extends Tribe_Template_Factory {
+	class Tribe_Events_Photo_Template extends Tribe_PRO_Template_Factory {
 
-		static $timeslots = array();
+		/**
+		 * Array of asset packages needed for this template
+		 *
+		 * @var array
+		 **/
+		protected $asset_packages = array( 'ajax-list', 'ajax-photoview' );
 
-		public static function init(){
-			
-			Tribe_PRO_Template_Factory::asset_package('ajax-photoview');			
+		protected function hooks() {
+			parent::hooks();
+			add_filter( 'tribe_events_event_classes', array( $this, 'event_classes') );
+			add_filter( 'tribe_events_header_attributes',  array( $this, 'header_attributes') );
+		}
+
+		/**
+		 * Add header attributes for map view
+		 *
+		 * @return string
+		 * @since 3.0
+		 **/
+		function header_attributes($attrs) {
+			$attrs['data-view'] = 'photo';
+			$attrs['data-baseurl'] = tribe_get_photo_permalink( false );
+			return apply_filters('tribe_events_pro_header_attributes', $attrs);
+		}
+
+
+		/**
+		 * Add event classes specific to photo view
+		 *
+		 * @param $classes
+		 * @return array
+		 * @since 3.0
+		 **/
+		public function event_classes($classes) {
+			$classes[] = 'tribe-events-photo-event';
+			return $classes;
+		}
+
+		/**
+		 * Set up the notices for this template
+		 *
+		 * @return void
+		 * @since 3.0
+		 **/
+		protected function set_notices() {
+
+			parent::set_notices();
 
 			// if no events for photo view (similar to day view)
 			if ( empty($search_term) && empty( $wp_query->query_vars['s'] ) && !have_posts() ) { // Messages if currently no events, and no search term
@@ -33,55 +75,7 @@ if( !class_exists('Tribe_Events_Photo_Template')){
 				TribeEvents::setNotice( 'events-not-found', sprintf( __( 'No events scheduled for <strong>%s</strong>. Please try another day.', 'tribe-events-calendar' ), date_i18n( 'F d, Y', strtotime( get_query_var( 'eventDate' ) ) ) ) );
 			}
 
-			// Set excerpt length & more
-			add_action( 'tribe_events_list_before_loop', array(__CLASS__, 'setup_excerpt') );
-
-			// Reset excerpt length & more
-			add_action( 'tribe_events_list_after_loop', array(__CLASS__, 'reset_excerpt') );
 		}
-
-		/**
-		 * Set up excerpt to how we like it in list view
-		 *
-		 * @return void
-		 * @since 3.0
-		 **/
-		public static function setup_excerpt() {
-			add_filter( 'excerpt_length', array(__CLASS__, 'excerpt_length') );
-			add_filter( 'excerpt_more', array(__CLASS__, 'excerpt_more') );
-		}
-
-		/**
-		 * Reset excerpt to how it used to be
-		 *
-		 * @return void
-		 * @since 3.0
-		 **/
-		public static function reset_excerpt() {
-			remove_filter( 'excerpt_length', array(__CLASS__, 'excerpt_length') );
-			remove_filter( 'excerpt_more', array(__CLASS__, 'excerpt_more') );
-		}
-
-		/**
-		 * Set excerpt length used in list view
-		 *
-		 * @return void
-		 * @since 
-		 **/
-		public static function excerpt_length() {
-			return 30;
-		}
-
-		/**
-		 * Set excerpt more used in list view
-		 *
-		 * @return void
-		 * @since 
-		 **/
-		public static function excerpt_more() {
-			return '&hellip;';
-		}
-
 	}
-	Tribe_Events_Photo_Template::init();
+	Tribe_Events_Photo_Template::instance();
 }
