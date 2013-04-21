131193b38211ced99a96810549184204ad398977
Samuel Estok <sam@faction23.com>
2013-04-20 18:14:43 -0700

    Complete removing all pro js from core.

--- resources/tribe-events-pro.js
+++ resources/tribe-events-pro.js
@@ -45,6 +45,87 @@
                 }
             }
         },
+        pro_tooltips: function () {
+
+            $('#tribe-events').on('mouseenter', 'div[id*="tribe-events-event-"], div[id*="tribe-events-daynum-"]:has(a), div.event-is-recurring',function () {
+
+                var bottomPad = 0;
+                var $this = $(this);
+
+                if ($('body').hasClass('tribe-events-week')) {
+
+                    var $tip = $this.find('.tribe-events-tooltip');
+
+                    if (!$this.parents('.tribe-grid-allday').length) {
+
+                        var $wrapper = $('.tribe-week-grid-wrapper');
+                        var $parent = $this.parent();
+                        var $container = $parent.parent();
+
+                        var pwidth = Math.ceil($container.width());
+                        var cwidth = Math.ceil($this.width());
+                        var twidth = Math.ceil($tip.outerWidth());
+                        var gheight = $wrapper.height();
+
+                        var scroll = $wrapper.scrollTop();
+                        var coffset = $parent.position();
+                        var poffset = $this.position();
+                        var ptop = Math.ceil(poffset.top);
+                        var toffset = scroll - ptop;
+
+                        var isright = $parent.hasClass('tribe-events-right');
+                        var wcheck;
+                        var theight;
+                        var available;
+                        var cssmap = {};
+
+                        if (!$tip.hasClass('hovered')) {
+                            $tip.attr('data-ow', twidth).addClass('hovered');
+                        }
+
+                        if (isright)
+                            wcheck = Math.ceil(coffset.left) - 20;
+                        else
+                            wcheck = pwidth - cwidth - Math.ceil(coffset.left);
+
+                        if (twidth >= wcheck)
+                            twidth = wcheck;
+                        else if ($tip.attr('data-ow') > wcheck)
+                            twidth = wcheck;
+                        else
+                            twidth = $tip.attr('data-ow');
+
+                        if (isright)
+                            cssmap = { "right": cwidth + 20, "bottom": "auto", "width": twidth + "px"};
+                        else
+                            cssmap = { "left": cwidth + 20, "bottom": "auto", "width": twidth + "px"};
+
+                        $tip.css(cssmap);
+
+                        theight = $tip.height();
+
+                        if (toffset >= 0) {
+                            toffset = toffset + 5;
+                        } else {
+                            available = toffset + gheight;
+                            if (theight > available)
+                                toffset = available - theight - 8;
+                            else
+                                toffset = 5;
+                        }
+
+                        $tip.css("top", toffset).show();
+
+                    } else {
+                        bottomPad = $this.outerHeight() + 6;
+                        $tip.css('bottom', bottomPad).show();
+                    }
+
+
+                }
+
+            });
+        },
         print_geo_options: function (){
             $("#tribe-geo-links").empty();
             $("#tribe-geo-options").show();
@@ -82,6 +163,102 @@
                 return status;
             });
         },
+        set_form: function (params) {
+            $('body').addClass('tribe-reset-on');
+
+            var has_sliders = false;
+            var has_select2 = false;
+
+            if ($('#tribe_events_filters_form').length) {
+                var $form = $('#tribe_events_filters_form');
+
+                $form.tribe_clear_form();
+
+                if ($form.find('.select2-container').length) {
+
+                    has_select2 = true;
+
+                    $('#tribe_events_filters_form .select2-container').select2("val", {});
+                }
+
+                if ($form.find('.ui-slider').length) {
+
+                    has_sliders = true;
+
+                    $('#tribe_events_filters_form .ui-slider').each(function () {
+                        var s_id = $(this).attr('id');
+                        var $this = $('#' + s_id);
+                        var $input = $this.prev();
+                        var $display = $input.prev();
+                        var settings = $this.slider("option");
+
+                        $this.slider("values", 0, settings.min);
+                        $this.slider("values", 1, settings.max);
+                        $display.text(settings.min + " - " + settings.max);
+                        $input.val('');
+                    });
+                }
+            }
+
+            if ($('#tribe-bar-form').length) {
+                $('#tribe-bar-form').tribe_clear_form();
+            }
+
+            params = tribe_ev.fn.parse_string(params);
+
+            $.each(params, function (key, value) {
+                if (key !== 'action') {
+                    var name = decodeURI(key);
+                    var $target = '';
+                    if (value.length === 1) {
+                        if ($('[name="' + name + '"]').is('input[type="text"], input[type="hidden"]')) {
+                            $('[name="' + name + '"]').val(value);
+                        } else if ($('[name="' + name + '"][value="' + value + '"]').is(':checkbox, :radio')) {
+                            $('[name="' + name + '"][value="' + value + '"]').prop("checked", true);
+                        } else if ($('[name="' + name + '"]').is('select')) {
+                            $('select[name="' + name + '"] option[value="' + value + '"]').attr('selected', true);
+                        }
+                    } else {
+                        for (var i = 0; i < value.length; i++) {
+                            $target = $('[name="' + name + '"][value="' + value[i] + '"]');
+                            if ($target.is(':checkbox, :radio')) {
+                                $target.prop("checked", true);
+                            } else {
+                                $('select[name="' + name + '"] option[value="' + value[i] + '"]').attr('selected', true);
+                            }
+                        }
+                    }
+                }
+            });
+
+            if (has_sliders) {
+                $('#tribe_events_filters_form .ui-slider').each(function () {
+                    var s_id = $(this).attr('id');
+                    var $this = $('#' + s_id);
+                    var $input = $this.prev();
+                    var range = $input.val().split('-');
+
+                    if (range[0] !== '') {
+                        var $display = $input.prev();
+
+                        $this.slider("values", 0, range[0]);
+                        $this.slider("values", 1, range[1]);
+                        $display.text(range[0] + " - " + range[1]);
+                        $this.slider('refresh');
+                    }
+                });
+            }
+
+            if (has_select2) {
+                $('#tribe_events_filters_form .select2-container').each(function () {
+                    var s2_id = $(this).attr('id');
+                    var $this = $('#' + s2_id);
+                    $this.next().trigger("change");
+                });
+            }
+
+            $('body').removeClass('tribe-reset-on');
+        },
         set_recurrence: function (recurrence_on) {
             if (recurrence_on) {
                 tribe_ev.state.recurrence = true;
@@ -103,17 +280,19 @@
         }
     });
 
+    tribe_ev.geoloc = {
+        map: [],
+        geocoder: [],
+        geocodes: [],
+        bounds: [],
+        markers: []
+    };
+
 })( jQuery );
 
-tribe_ev.geoloc = {
-    map: [],
-    geocoder: [],
-    geocodes: [],
-    bounds: [],
-    markers: []
-};
-
 jQuery(document).ready(function ($) {
+
+    tribe_ev.fn.pro_tooltips();
 
     var recurrence_on = false;
 
