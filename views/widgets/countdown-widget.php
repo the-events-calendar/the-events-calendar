74e005a66495e73bcde6731b26fca329b1d0547b
Kyle Unzicker <kyle@wndfrm.com>
2013-05-02 15:15:01 -0500

    created countdown widget view file

---
+++ views/widgets/countdown-widget.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * Events Pro Countdown Widget Template
+ * This is the template for the output of the event countdown widget. 
+ * All the items are turned on and off through the widget admin.
+ * There is currently no default styling, which is highly needed.
+ *
+ * You can customize this view by putting a replacement file of the same name
+ * (/widgets/countdown-widget.php) in the tribe-events/pro/widgets/ directory of your theme.
+ *
+ *
+ * @return string
+ *
+ * @package TribeEventsCalendarPro
+ * @since  3.0
+ * @author Modern Tribe Inc.
+ *
+ */
+
+// Don't load directly
+if ( !defined('ABSPATH') ) { die('-1'); }
+?>
+
+<?php if ($show_seconds) { ?>
+	<div class="tribe-countdown-timer tribe-clearfix">
+		<div class="tribe-countdown-days tribe-countdown-number">DD<br />
+			<span class="tribe-countdown-under"><?php _e('days', 'tribe-events-calendar-pro'); ?></span>
+		</div>
+		<div class="tribe-countdown-colon">:</div>
+		<div class="tribe-countdown-hours tribe-countdown-number">HH<br />
+			<span class="tribe-countdown-under"><?php _e('hours', 'tribe-events-calendar-pro'); ?></span>
+		</div>
+		<div class="tribe-countdown-colon">:</div>
+		<div class="tribe-countdown-minutes tribe-countdown-number">MM<br />
+			<span class="tribe-countdown-under"><?php _e('min', 'tribe-events-calendar-pro'); ?></span>
+		</div>
+		<div class="tribe-countdown-colon">:</div>
+		<div class="tribe-countdown-seconds tribe-countdown-number tribe-countdown-right">SS<br />
+			<span class="tribe-countdown-under"><?php _e('sec', 'tribe-events-calendar-pro'); ?></span>
+		</div>
+	</div>
+<?php } else { ?>
+	<div class="tribe-countdown-timer">
+		<div class="tribe-countdown-days tribe-countdown-number">DD<br />
+			<span class="tribe-countdown-under"><?php _e('days', 'tribe-events-calendar-pro'); ?></span>
+		</div>
+		<div class="tribe-countdown-colon">:</div>
+		<div class="tribe-countdown-hours tribe-countdown-number">HH<br />
+			<span class="tribe-countdown-under"><?php _e('hours', 'tribe-events-calendar-pro'); ?></span>
+		</div>
+		<div class="tribe-countdown-colon">:</div>
+		<div class="tribe-countdown-minutes tribe-countdown-number tribe-countdown-right">MM<br />
+			<span class="tribe-countdown-under"><?php _e('min', 'tribe-events-calendar-pro'); ?></span>
+		</div>
+	</div>
+<?php } ?>	
