7586e6c52723e15cb0370e40d004b64f991d8766
Kyle Unzicker <kyle@wndfrm.com>
2013-05-06 08:37:58 -0500

    cleaned up countdown widget a bit

--- views/widgets/countdown-widget.php
+++ views/widgets/countdown-widget.php
@@ -21,7 +21,7 @@
 if ( !defined('ABSPATH') ) { die('-1'); }
 ?>
 
-<?php if ($show_seconds) { ?>
+
 	<div class="tribe-countdown-timer tribe-clearfix">
 		<div class="tribe-countdown-days tribe-countdown-number">DD<br />
 			<span class="tribe-countdown-under"><?php _e('days', 'tribe-events-calendar-pro'); ?></span>
@@ -34,23 +34,12 @@ if ( !defined('ABSPATH') ) { die('-1'); }
 		<div class="tribe-countdown-minutes tribe-countdown-number">MM<br />
 			<span class="tribe-countdown-under"><?php _e('min', 'tribe-events-calendar-pro'); ?></span>
 		</div>
+		<?php if ($show_seconds) { ?>
 		<div class="tribe-countdown-colon">:</div>
 		<div class="tribe-countdown-seconds tribe-countdown-number tribe-countdown-right">SS<br />
 			<span class="tribe-countdown-under"><?php _e('sec', 'tribe-events-calendar-pro'); ?></span>
 		</div>
+		<?php } ?>	
 	</div>
-<?php } else { ?>
-	<div class="tribe-countdown-timer">
-		<div class="tribe-countdown-days tribe-countdown-number">DD<br />
-			<span class="tribe-countdown-under"><?php _e('days', 'tribe-events-calendar-pro'); ?></span>
-		</div>
-		<div class="tribe-countdown-colon">:</div>
-		<div class="tribe-countdown-hours tribe-countdown-number">HH<br />
-			<span class="tribe-countdown-under"><?php _e('hours', 'tribe-events-calendar-pro'); ?></span>
-		</div>
-		<div class="tribe-countdown-colon">:</div>
-		<div class="tribe-countdown-minutes tribe-countdown-number tribe-countdown-right">MM<br />
-			<span class="tribe-countdown-under"><?php _e('min', 'tribe-events-calendar-pro'); ?></span>
-		</div>
-	</div>
-<?php } ?>	
+
+
