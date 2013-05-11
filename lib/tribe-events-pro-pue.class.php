b0e9d717ef3d452a603c66fefed2c626c491e6c8
Peter Chester <peter@tri.be>
2013-05-11 13:51:08 -0700

    Abstract PUE in events pro
    Signed-off-by: Peter Chester <peter@tri.be>

---
+++ lib/tribe-events-pro-pue.class.php
@@ -0,0 +1,70 @@
+<?php
+/**
+ * Manage upgrades for the Tribe Events Pro plugin
+ *
+ * @author Peter Chester
+ */
+
+// Don't load directly
+if( !defined( 'ABSPATH' ) ) die( '-1' );
+
+if( !class_exists( 'TribeEventsProPUE' ) ) {
+	class TribeEventsProPUE {
+
+		/**
+		 * @var string slug used for the plugin update engine
+		 */
+		private static $pue_slug = 'events-calendar-pro';
+
+		/**
+		 * @var string plugin update url
+		 */
+		private static $update_url = 'http://tri.be/';
+
+		/**
+		 * @var string plugin file name
+		 */
+		private static $plugin_file;
+
+		/**
+		 * Constructor function. a.k.a. Let's get this party started!
+		 *
+		 * @param string $plugin_file file path.
+		 */
+		public function __construct( $plugin_file ) {
+			self::$plugin_file = $plugin_file;
+			if ( !class_exists( 'TribeCommonLibraries' ) ) {
+				require_once( dirname( self::$plugin_file ) . '/vendor/tribe-common-libraries/tribe-common-libraries.class.php' );
+			}
+			TribeCommonLibraries::register( 'tribe-pue-client', '1.3', dirname( self::$plugin_file ) . '/vendor/pue-client/pue-client.php' );
+			add_action( 'tribe_helper_activation_complete', array( $this, 'load_plugin_update_engine' ) );
+			register_activation_hook( self::$plugin_file, array( $this, 'register_uninstall_hook' ) );
+		}
+
+		/**
+		 * Load the Plugin Update Engine
+		 */
+		public function load_plugin_update_engine() {
+			if( apply_filters( 'tribe_enable_pue', TRUE, self::$pue_slug ) ) {
+				$this->pue_instance = new TribePluginUpdateEngineChecker( self::$update_url, self::$pue_slug, array(), plugin_basename( self::$plugin_file ) );
+			}
+		}
+
+		/**
+		 * Register the uninstall hook on activation
+		 */
+		public function register_uninstall_hook() {
+			register_uninstall_hook( self::$plugin_file , array( get_class($this), 'uninstall' ) );
+		}
+
+		/**
+		 * The uninstall hook for the pue option.
+		 */
+		public function uninstall() {
+			$slug = str_replace( '-', '_', self::$pue_slug );
+			delete_option( 'pue_install_key_' . $slug );
+			delete_option( 'pu_dismissed_upgrade_' . $slug );
+		}
+	}
+}
+?>
