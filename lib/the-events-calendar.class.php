f639394c5731fa81f5763dba5e7d1daeb95a57b5
Peter Chester <peter@tri.be>
2013-05-19 10:04:39 -0600

    Add language structure.
    Signed-off-by: Peter Chester <peter@tri.be>

--- lib/the-events-calendar.class.php
+++ lib/the-events-calendar.class.php
@@ -3540,12 +3540,12 @@ if ( !class_exists( 'TribeEvents' ) ) {
 		}
 
 		public function setup_listview_in_bar( $views ) {
-			$views[] = array( 'displaying' => 'upcoming', 'event_bar_hook' => 'tribe_events_list_before_template', 'anchor' => 'List', 'url' => tribe_get_listview_link() );
+			$views[] = array( 'displaying' => 'upcoming', 'event_bar_hook' => 'tribe_events_list_before_template', 'anchor' => __( 'List', 'tribe-events-calendar' ), 'url' => tribe_get_listview_link() );
 			return $views;
 		}
 
 		public function setup_gridview_in_bar( $views ) {
-			$views[] = array( 'displaying' => 'month', 'event_bar_hook' => 'tribe_events_month_before_template', 'anchor' => 'Month', 'url' => tribe_get_gridview_link() );
+			$views[] = array( 'displaying' => 'month', 'event_bar_hook' => 'tribe_events_month_before_template', 'anchor' => __( 'Month', 'tribe-events-calendar' ), 'url' => tribe_get_gridview_link() );
 			return $views;
 		}
 
