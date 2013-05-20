636aa29cb11ef8b2786081675694318305dbf204
Paul Hughes <paul@paulalexanderhughes.com>
2013-05-20 10:30:53 -0400

    Added quotes around a href attribute value. Updates #20820
    Signed-off-by: Paul Hughes <paul@paulalexanderhughes.com>

--- lib/the-events-calendar.class.php
+++ lib/the-events-calendar.class.php
@@ -3202,7 +3202,7 @@ if ( !class_exists( 'TribeEvents' ) ) {
 	            } elseif ( strpos( $anchor, '%title%' ) !== false ) {
 					$anchor = preg_replace( '|%title%|', $results->post_title, $anchor );
 				}
-				return apply_filters('tribe_events_get_event_link', '<a href='.tribe_get_event_link($results).'>'.$anchor.'</a>');
+				return apply_filters('tribe_events_get_event_link', '<a href="'.tribe_get_event_link($results).'">'.$anchor.'</a>');
 			}
 		}
 
