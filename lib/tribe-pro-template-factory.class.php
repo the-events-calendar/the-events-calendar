491149474127b786286372ae2546965873238a69
Paul Hughes <paul@paulalexanderhughes.com>
2013-05-28 11:55:25 -0700

    Fixed so PRO template loader uses the core function to check for minified files. Updates #20951
    Signed-off-by: Paul Hughes <paul@paulalexanderhughes.com>

--- lib/tribe-pro-template-factory.class.php
+++ lib/tribe-pro-template-factory.class.php
@@ -24,22 +24,27 @@ if( !class_exists('Tribe_PRO_Template_Factory') ) {
 				case 'ajax-weekview' :					
 					$ajax_data = array( "ajaxurl"     => admin_url( 'admin-ajax.php', ( is_ssl() ? 'https' : 'http' ) ),
 					                    'post_type' => TribeEvents::POSTTYPE );
-					wp_enqueue_script( 'tribe-events-pro-slimscroll', $vendor_url . 'jquery-slimscroll/jquery.slimscroll.min.js', array('tribe-events-pro', 'jquery-ui-draggable'), null, true );
-					wp_enqueue_script('tribe-events-pro-week', $resources_url . 'tribe-events-week.js', array('tribe-events-pro-slimscroll'), false, true);
+					$path1 = Tribe_Template_Factory::getMinFile( $vendor_url . 'jquery-slimscroll/jquery.slimscroll.js', true );
+					$path2 = Tribe_Template_Factory::getMinFile( $resources_url . 'tribe-events-week.js', true );
+					wp_enqueue_script( 'tribe-events-pro-slimscroll', $path1, array('tribe-events-pro', 'jquery-ui-draggable'), null, true );
+					wp_enqueue_script('tribe-events-pro-week', $path2, array('tribe-events-pro-slimscroll'), false, true);
 					wp_localize_script( 'tribe-events-pro-week', 'TribeWeek', $ajax_data );
 					break;					
 				case 'ajax-photoview' :				
 					$tribe_paged = ( !empty( $_REQUEST['tribe_paged'] ) ) ? $_REQUEST['tribe_paged'] : 0;
 					$ajax_data = array( "ajaxurl"     => admin_url( 'admin-ajax.php', ( is_ssl() ? 'https' : 'http' ) ),
 					                    'tribe_paged' => $tribe_paged );
-					wp_enqueue_script( 'tribe-events-pro-isotope', $vendor_url . 'isotope/jquery.isotope.min.js', array('tribe-events-pro'), null, true );
-					wp_enqueue_script('tribe-events-pro-photo', $resources_url . 'tribe-events-photo-view.js', array('tribe-events-pro-isotope'), null, true);
+					$path1 = Tribe_Template_Factory::getMinFile( $vendor_url . 'isotope/jquery.isotope.js', true );
+					$path2 = Tribe_Template_Factory::getMinFile( $resources_url . 'tribe-events-photo-view.js', true );
+					wp_enqueue_script( 'tribe-events-pro-isotope', $path1, array('tribe-events-pro'), null, true );
+					wp_enqueue_script('tribe-events-pro-photo', $path2, array('tribe-events-pro-isotope'), null, true);
 					wp_localize_script( 'tribe-events-pro-photo', 'TribePhoto', $ajax_data );
 					break;					
 				case 'ajax-dayview':
 					$ajax_data = array( "ajaxurl"   => admin_url( 'admin-ajax.php', ( is_ssl() ? 'https' : 'http' ) ),
 					                    'post_type' => TribeEvents::POSTTYPE );
-					wp_enqueue_script( 'tribe-events-pro-ajax-day', $resources_url . 'tribe-events-ajax-day.js', array('tribe-events-pro'), null, true );
+					$path = Tribe_Template_Factory::getMinFile( $resources_url . 'tribe-events-ajax-day.js', true );
+					wp_enqueue_script( 'tribe-events-pro-ajax-day', $path, array('tribe-events-pro'), null, true );
 					wp_localize_script( 'tribe-events-pro-ajax-day', 'TribeCalendar', $ajax_data );
 					break;
 
@@ -47,7 +52,8 @@ if( !class_exists('Tribe_PRO_Template_Factory') ) {
 					$http = is_ssl() ? 'https' : 'http';
 
 					wp_register_script( 'gmaps', $http . '://maps.google.com/maps/api/js?sensor=false', array( 'tribe-events-pro' ) );
-					wp_register_script( 'tribe-events-pro-geoloc', $resources_url . 'tribe-events-ajax-maps.js', array( 'gmaps' ) );
+					$path = Tribe_Template_Factory::getMinFile( $resources_url . 'tribe-events-ajax-maps.js', true );
+					wp_register_script( 'tribe-events-pro-geoloc', $path, array( 'gmaps' ) );
 					wp_enqueue_script( 'tribe-events-pro-geoloc' );
 
 					$geoloc = TribeEventsGeoLoc::instance();
