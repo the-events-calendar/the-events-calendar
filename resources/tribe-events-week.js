bb79572ef167e998e1a9884a73f823dde31143a8
Samuel Estok <sam@faction23.com>
2013-06-20 02:57:43 -0700

    Complete week view docs

--- resources/tribe-events-week.js
+++ resources/tribe-events-week.js
@@ -256,6 +256,13 @@
 			});
 		});
 
+		/**
+		 * @function tribe_events_bar_weekajax_actions
+		 * @since 3.0
+		 * @desc On events bar submit, this function collects the current state of the bar and sends it to the week view ajax handler.
+		 * @param {event} e The event object.
+		 */
+
 		function tribe_events_bar_weekajax_actions(e) {
 			if (tribe_events_bar_action != 'change_view') {
 				e.preventDefault();
@@ -295,6 +302,14 @@
 			tribe_events_week_ajax_post();
 		});
 
+		/**
+		 * @function tribe_events_week_ajax_post
+		 * @since 3.0
+		 * @desc The ajax handler for week view.
+		 * Fires the custom event 'tribe_ev_serializeBar' at start, then 'tribe_ev_collectParams' to gather any additional parameters before actually launching the ajax post request.
+		 * As post begins 'tribe_ev_ajaxStart' and 'tribe_ev_weekView_AjaxStart' are fired, and then 'tribe_ev_ajaxSuccess' and 'tribe_ev_weekView_ajaxSuccess' are fired on success.
+		 * Various functions in the events plugins hook into these events. They are triggered on the tribe_ev.events object.
+		 */
 
 		function tribe_events_week_ajax_post() {
 
