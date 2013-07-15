c8ffa8ceaa1a03acf1097d5ebbe54c23944029a4
Kyle Unzicker <kyle@wndfrm.com>
2013-07-15 11:30:52 -0500

    changed inline comment for style override

--- lib/tribe-template-factory.class.php
+++ lib/tribe-template-factory.class.php
@@ -550,11 +550,11 @@ if( !class_exists('Tribe_Template_Factory') ) {
 							break;
 					}
 
-					// Is there a core override file in the theme?
 					$styleUrl = trailingslashit( $tec->pluginUrl ) . 'resources/' . $event_file_option;
 					$styleUrl = self::getMinFile( $styleUrl, true );
 					$styleUrl = apply_filters( 'tribe_events_stylesheet_url', $styleUrl );
 
+					// Is there a core override file in the theme?
 					$styleOverrideUrl = TribeEventsTemplates::locate_stylesheet('tribe-events/'.$event_file, $styleUrl);
 
 					// Load up stylesheet from theme or plugin
