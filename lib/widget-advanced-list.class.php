83c5731941ef1dc8e8edb9a409396b4e5a687688
jessica <jessica@exobi.com>
2013-07-19 19:07:39 -0400

    Fix notices in advanced list widget Updates #22254

--- lib/widget-advanced-list.class.php
+++ lib/widget-advanced-list.class.php
@@ -11,7 +11,7 @@ if ( !defined('ABSPATH') ) { die('-1'); }
 if( !class_exists( 'TribeEventsAdvancedListWidget' ) ) {
 	class TribeEventsAdvancedListWidget extends TribeEventsListWidget {
 
-		static $params;
+		static $params = array();
 
 		function TribeEventsAdvancedListWidget() {
 			/* Widget settings. */
