52a5e642ed99cc53edc6a581d57a2aeb07663f44
Jonathan Brinley <jbrinley@flightless.us>
2013-09-22 22:29:27 -0400

    limit range of recurrence instances added to the DB, update them on a rolling basis - updates #20041

--- lib/tribe-recurrence.class.php
+++ lib/tribe-recurrence.class.php
@@ -6,9 +6,13 @@
 class TribeRecurrence {
 	private $start_date;
 	private $end;
+	/** @var DateSeriesRules */
 	private $series_rules;
 	private $by_occurrence_count;
 	private $event;
+	private $minDate = 0;
+	private $maxDate = PHP_INT_MAX;
+	private $last_request_constrained = FALSE;
 
 	public function  __construct($start_date, $end, $series_rules, $by_occurrence_count = false, $event = null) {
 		$this->start_date = $start_date;
@@ -18,14 +22,23 @@ class TribeRecurrence {
 		$this->event = $event;
 	}
 
+	public function setMinDate( $timestamp ) {
+		$this->minDate = (int)$timestamp;
+	}
+
+	public function setMaxDate( $timestamp ) {
+		$this->maxDate = (int)$timestamp;
+	}
+
 	/**
 	 * Using the rules engine, find all dates in the series 
 	 *
 	 * @param bool $all_events Return ALL instances?
 	 * @param array $old_start_dates The old start dates for an event.
-	 * @return An array of all dates in the series
+	 * @return array An array of all dates in the series
 	 */
 	public function getDates( $all_events = true, $old_start_dates = null ) {
+		$this->last_request_constrained = FALSE;
 		if( $this->series_rules ) {
 			$dates = array();
 			$cur_date = $this->start_date;
@@ -36,16 +49,25 @@ class TribeRecurrence {
 					$cur_date = $this->series_rules->getNextDate($cur_date);
 					// Makes sure to assign the proper hours to the date.
 					$cur_date = mktime (date("H", $this->start_date), date("i", $this->start_date), date("s", $this->start_date), date('n', $cur_date),  date('j', $cur_date), date('Y', $cur_date));
-					$dates[] = $cur_date;
-				}				
+					if ( $cur_date >= $this->minDate && $cur_date <= $this->maxDate ) {
+						$dates[] = $cur_date;
+					} elseif ( $cur_date > $this->maxDate ) {
+						$this->last_request_constrained = $cur_date;
+						break;
+					}
+				}
 			} else {
 				// date driven
-				while($cur_date <= $this->end) {
+				while( $cur_date <= $this->end && $cur_date < $this->maxDate ) {
 					$cur_date = $this->series_rules->getNextDate($cur_date);
 					// Makes sure to assign the proper hours to the date.
 					$cur_date = mktime (date("H", $this->start_date), date("i", $this->start_date), date("s", $this->start_date), date('n', $cur_date),  date('j', $cur_date), date('Y', $cur_date));
-					if($cur_date <= $this->end)
+					if ( $cur_date <= $this->end && $cur_date >= $this->minDate && $cur_date <= $this->maxDate ) {
 						$dates[] = $cur_date;
+					} elseif ( $cur_date > $this->maxDate ) {
+						$this->last_request_constrained = $cur_date;
+						break;
+					}
 				}
 			}
 
@@ -61,8 +83,18 @@ class TribeRecurrence {
 					}
 				}
 			}
-			
+
 			return $dates;
 		}
+		return array();
+	}
+
+	/**
+	 * Flag indicating if the last getDates() request was constrained
+	 * by the max date setting
+	 * @return bool
+	 */
+	public function constrainedByMaxDate() {
+		return $this->last_request_constrained;
 	}
 }
