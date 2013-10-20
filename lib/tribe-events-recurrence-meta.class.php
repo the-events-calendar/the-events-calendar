f37c3934a10b57e1f8235420a7c7ff6bee87c23b
jessica <jessica@exobi.com>
2013-10-19 23:16:37 -0400

    Make explanation for "Recurring event instances" setting clearer Updates #23269

--- lib/tribe-events-recurrence-meta.class.php
+++ lib/tribe-events-recurrence-meta.class.php
@@ -807,7 +807,7 @@ class TribeEventsRecurrenceMeta {
 				'hideSubsequentRecurrencesDefault' => array(
 					'type' => 'checkbox_bool',
 					'label' => __( 'Recurring event instances', 'tribe-events-calendar-pro' ),
-					'tooltip' => __( 'Show only the first instance of each recurring event.', 'tribe-events-calendar-pro' ),
+					'tooltip' => __( 'Show only the first instance of each recurring event (only affects list-style views).', 'tribe-events-calendar-pro' ),
 					'default' => false,
 					'validation_type' => 'boolean',
 				),
