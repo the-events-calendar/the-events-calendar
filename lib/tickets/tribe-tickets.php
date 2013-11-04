f612af55b091b755a4049327be8f439af0a03237
Daniel Dvorkin <daniel@mzaweb.com>
2013-11-04 13:48:09 -0300

    Update Tickets API to use the new getTemplateHierarchy

--- lib/tickets/tribe-tickets.php
+++ lib/tickets/tribe-tickets.php
@@ -569,7 +569,8 @@ if ( ! class_exists( 'TribeEventsTickets' ) ) {
 		 */
 		public function generate_tickets_email_content( $tickets ) {
 			ob_start();
-			include TribeEventsTemplates::getTemplateHierarchy( 'email.php', array('subfolder'=>'tickets') );
+			include TribeEventsTemplates::getTemplateHierarchy( 'tickets/email.php', array( 'namespace' => 'tickets' ) );
+
 			return ob_get_clean();
 		}
 
