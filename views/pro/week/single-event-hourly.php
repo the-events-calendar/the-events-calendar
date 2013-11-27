4736d37491d183f164df9f94456733aa9137ff33
Samuel Estok <sam@faction23.com>
2013-11-27 14:37:11 -0800

    updated week to use global template tag

--- views/pro/week/single-event-hourly.php
+++ views/pro/week/single-event-hourly.php
@@ -14,7 +14,7 @@
 if ( !defined('ABSPATH') ) { die('-1'); } ?>
 
 <?php $event = tribe_events_week_get_event(); ?>
-<div id='tribe-events-event-<?php echo $event->ID; ?>' <?php tribe_events_the_header_attributes( 'week-hourly' ); ?> class='<?php tribe_events_event_classes() ?> tribe-week-event' data-tribejson='<?php echo tribe_events_week_template_data( $event ); ?>'>
+<div id='tribe-events-event-<?php echo $event->ID; ?>' <?php tribe_events_the_header_attributes( 'week-hourly' ); ?> class='<?php tribe_events_event_classes() ?> tribe-week-event' data-tribejson='<?php echo tribe_events_template_data( $event ); ?>'>
 	<div class="hentry vevent">
 		<h3 class="entry-title summary"><a href="<?php tribe_event_link( $event ); ?>" class="url" rel="bookmark"><?php echo $event->post_title; ?></a></h3>
 	</div>
