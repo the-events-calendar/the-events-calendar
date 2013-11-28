a48cc0b33daed64e697e11c50602d2a42b326073
barryhughes <barry@codingkills.me>
2013-11-28 08:10:50 -0800

    Removed unnecessary additional filter call in tribe_events_before_html()

--- public/template-tags/general.php
+++ public/template-tags/general.php
@@ -406,7 +406,6 @@ if ( class_exists( 'TribeEvents' ) ) {
 		$before = wpautop( $before );
 		$before = '<div class="tribe-events-before-html">'. stripslashes( shortcode_unautop( $before  ) ) .'</div>';
 		$before = $before.'<span class="tribe-events-ajax-loading"><img class="tribe-events-spinner-medium" src="'.tribe_events_resource_url('images/tribe-loading.gif').'" alt="'.__('Loading Events', 'tribe-events').'" /></span>';
-		$before = apply_filters( 'tribe_events_before_html', $before );
 
 		echo apply_filters( 'tribe_events_before_html', $before );
 	}
