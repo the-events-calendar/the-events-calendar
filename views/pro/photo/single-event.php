4b4a7f7f127b24502c45bce75db95410b28ec8e9
Ryan Urban <ryan@fringewebdesign.com>
2013-12-18 14:30:41 -0600

    Update responsive issues

--- views/pro/photo/single-event.php
+++ views/pro/photo/single-event.php
@@ -17,38 +17,42 @@ if ( !defined('ABSPATH') ) { die('-1'); } ?>
 
 global $post;
 
- ?>
+?>
 
-<?php echo tribe_event_featured_image( null, 'medium' ); ?>
+<div class="tribe-events-photo-event-wrap">
 
- <div class="tribe-events-event-details tribe-clearfix">
+	<?php echo tribe_event_featured_image( null, 'medium' ); ?>
 
-	<!-- Event Title -->
-	<?php do_action( 'tribe_events_before_the_event_title' ); ?>
-	<h2 class="tribe-events-list-event-title entry-title summary">
-		<a class="url" href="<?php echo tribe_get_event_link() ?>" title="<?php the_title() ?>" rel="bookmark">
-			<?php the_title(); ?>
-		</a>
-	</h2>
-	<?php do_action( 'tribe_events_after_the_event_title' ); ?>
+	<div class="tribe-events-event-details tribe-clearfix">
 
-	<!-- Event Meta -->
-	<?php do_action( 'tribe_events_before_the_meta' ); ?>
-		<div class="tribe-events-event-meta">
-			<div class="updated published time-details">
-				<?php if ( ! empty( $post->distance ) ) : ?>
-				<strong>[<?php echo tribe_get_distance_with_unit( $post->distance ); ?>]</strong>
-				<?php endif; ?>
-				<?php echo tribe_events_event_schedule_details(), tribe_events_event_recurring_info_tooltip(); ?>
-			</div>
-		</div><!-- .tribe-events-event-meta -->
-	<?php do_action( 'tribe_events_after_the_meta' ); ?>
+		<!-- Event Title -->
+		<?php do_action( 'tribe_events_before_the_event_title' ); ?>
+		<h2 class="tribe-events-list-event-title entry-title summary">
+			<a class="url" href="<?php echo tribe_get_event_link() ?>" title="<?php the_title() ?>" rel="bookmark">
+				<?php the_title(); ?>
+			</a>
+		</h2>
+		<?php do_action( 'tribe_events_after_the_event_title' ); ?>
 
-	<!-- Event Content -->
-	<?php do_action( 'tribe_events_before_the_content' ); ?>
-	<div class="tribe-events-list-photo-description tribe-events-content entry-summary description">
-		<?php the_excerpt(); ?>
-	</div>
-	<?php do_action( 'tribe_events_after_the_content' ) ?>
+		<!-- Event Meta -->
+		<?php do_action( 'tribe_events_before_the_meta' ); ?>
+			<div class="tribe-events-event-meta">
+				<div class="updated published time-details">
+					<?php if ( ! empty( $post->distance ) ) : ?>
+					<strong>[<?php echo tribe_get_distance_with_unit( $post->distance ); ?>]</strong>
+					<?php endif; ?>
+					<?php echo tribe_events_event_schedule_details(), tribe_events_event_recurring_info_tooltip(); ?>
+				</div>
+			</div><!-- .tribe-events-event-meta -->
+		<?php do_action( 'tribe_events_after_the_meta' ); ?>
 
-</div><!-- /.tribe-events-event-details -->
+		<!-- Event Content -->
+		<?php do_action( 'tribe_events_before_the_content' ); ?>
+		<div class="tribe-events-list-photo-description tribe-events-content entry-summary description">
+			<?php the_excerpt(); ?>
+		</div>
+		<?php do_action( 'tribe_events_after_the_content' ) ?>
+
+	</div><!-- /.tribe-events-event-details -->
+
+</div><!-- /.tribe-events-photo-event-wrap -->
