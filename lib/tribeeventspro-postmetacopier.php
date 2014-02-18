8eed834d7ffc0e6f7d64dd695d2c495d06f45804
Jonathan Brinley <jbrinley@flightless.us>
2014-02-17 22:14:09 -0500

    add filter to the meta copier blacklist array

--- lib/tribeeventspro-postmetacopier.php
+++ lib/tribeeventspro-postmetacopier.php
@@ -4,11 +4,17 @@
  * Class TribeEventsPro_PostMetaCopier
  */
 class TribeEventsPro_PostMetaCopier {
+	/**
+	 * @param int $original_post
+	 * @param int $destination_post
+	 *
+	 * @return void
+	 */
 	public function copy_meta( $original_post, $destination_post ) {
 		$this->clear_meta( $destination_post );
 		$post_meta_keys = get_post_custom_keys( $original_post );
 		if (empty($post_meta_keys)) return;
-		$meta_blacklist = $this->get_meta_key_blacklist();
+		$meta_blacklist = $this->get_meta_key_blacklist( $original_post );
 		$meta_keys = array_diff($post_meta_keys, $meta_blacklist);
 
 		foreach ($meta_keys as $meta_key) {
@@ -22,21 +28,22 @@ class TribeEventsPro_PostMetaCopier {
 
 	private function clear_meta( $post_id ) {
 		$post_meta_keys = get_post_custom_keys( $post_id );
-		$blacklist = $this->get_meta_key_blacklist();
+		$blacklist = $this->get_meta_key_blacklist( $post_id );
 		$post_meta_keys = array_diff( $post_meta_keys, $blacklist );
 		foreach ( $post_meta_keys as $key ) {
 			delete_post_meta( $post_id, $key );
 		}
 	}
 
-	private function get_meta_key_blacklist() {
-		return array(
+	private function get_meta_key_blacklist( $post_id ) {
+		$list = array(
 			'_edit_lock',
 			'_edit_last',
 			'_EventStartDate',
 			'_EventEndDate',
 			'_EventDuration',
 		);
+		return apply_filters( 'tribe_events_meta_copier_blacklist', $list );
 	}
 }
  
