c3449d308a46b257e2b686d7b6427cdc26609632
Ryan Urban <ryan@fringewebdesign.com>
2014-02-21 21:34:18 -0600

    Finish port to agenda

--- lib/template-classes/week.php
+++ lib/template-classes/week.php
@@ -203,7 +203,7 @@ if ( !class_exists( 'Tribe_Events_Pro_Week_Template' ) ) {
 				$date = date( 'Y-m-d', strtotime( self::$start_of_week_date . " +$day_offset days" ) );
 				$week_days[ $n ] = array(
 					'date' => $date,
-					'display' => date( 'D', strtotime( self::$start_of_week_date . " +$day_offset days" ) ) .' <span class="tribe-events-daynum">'. date( 'jS', strtotime( self::$start_of_week_date . " +$day_offset days" ) ) .'</span>',
+					'display' => '<span data-full-date="'. date( 'l, M j, Y', strtotime( self::$start_of_week_date . " +$day_offset days" ) ) .'">'. date( 'D jS', strtotime( self::$start_of_week_date . " +$day_offset days" ) ) .'</span>',
 					'is_today' => ( $date == self::$today ) ? true : false,
 					'is_past' => ( $date < self::$today ) ? true : false,
 					'is_future' => ( $date > self::$today ) ? true : false,
