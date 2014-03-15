0c89e94b8363d69b070b47d975304c780c3e8a8f
jessica <jessica@exobi.com>
2014-03-14 19:26:18 -0700

    Fix csv importer venue & organizer meta save Updates #22304

--- lib/io/csv/classes/TribeEventsImporter_FileImporter_Venues.php
+++ lib/io/csv/classes/TribeEventsImporter_FileImporter_Venues.php
@@ -21,6 +21,7 @@ class TribeEventsImporter_FileImporter_Venues extends TribeEventsImporter_FileIm
 	protected function create_post( array $record ) {
 		$venue = $this->build_venue_array( $record );
 		$id = TribeEventsAPI::createVenue( $venue );
+		TribeEventsAPI::updateVenue( $id, $venue );
 		return $id;
 	}
 
