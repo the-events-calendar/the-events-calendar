ef268c15b98a33d34cf1a85547aefff20a3f6d7a
Casey <caseypicker@gmail.com>
2014-04-17 22:36:05 -0500

    Issue #21085 - Splitting out Related events into a separate template file

--- public/template-tags/general.php
+++ public/template-tags/general.php
@@ -737,29 +737,8 @@ if( class_exists( 'TribeEventsPro' ) ) {
 	 * @param int $count The number of related events to find.
 	 * @return void.
 	 */
-	function tribe_single_related_events( $count = 3 ) {
-		$posts = tribe_get_related_posts( $count );
-		if ( is_array( $posts ) && !empty( $posts ) ) {
-			echo '<h3 class="tribe-events-related-events-title">'.  __( 'Related Events', 'tribe-events-calendar-pro' ) .'</h3>';
-			echo '<ul class="tribe-related-events tribe-clearfix hfeed vcalendar">';
-			foreach ( $posts as $post ) {
-				echo '<li>';
-
-					$thumb = ( has_post_thumbnail( $post->ID ) ) ? get_the_post_thumbnail( $post->ID, 'large' ) : '<img src="'. trailingslashit( TribeEventsPro::instance()->pluginUrl ) . 'resources/images/tribe-related-events-placeholder.png" alt="'. get_the_title( $post->ID ) .'" />';;
-					echo '<div class="tribe-related-events-thumbnail">';
-					echo '<a href="'. tribe_get_event_link( $post ) .'" class="url" rel="bookmark">'. $thumb .'</a>';
-					echo '</div>';
-					echo '<div class="tribe-related-event-info">';
-						echo '<h3 class="tribe-related-events-title summary"><a href="'. tribe_get_event_link( $post ) .'" class="url" rel="bookmark">'. get_the_title( $post->ID ) .'</a></h3>';
-
-						if ( $post->post_type == TribeEvents::POSTTYPE ) {
-							echo tribe_events_event_schedule_details( $post );
-						}
-					echo '</div>';
-				echo '</li>';
-			}
-			echo '</ul>';
-		}
+	function tribe_single_related_events( ) {
+		tribe_get_template_part( 'pro/related-events' );
 	}
 
 	/**
