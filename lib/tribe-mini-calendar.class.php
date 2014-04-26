f0c38b708bbf63ba06a750a38ece274559f39fe2
barryhughes <barry@codingkills.me>
2014-04-25 17:17:43 -0700

    Break out get_tax_query_from_widget_options() (updates #26308)

--- lib/tribe-mini-calendar.class.php
+++ lib/tribe-mini-calendar.class.php
@@ -256,32 +256,6 @@ class TribeEventsMiniCalendar {
 		}
 	}
 
-	public function get_tax_query_from_widget_options( $filters, $operand ) {
-
-		if ( empty( $filters ) )
-			return null;
-
-		$tax_query = array();
-
-		foreach ( $filters as $tax => $terms ) {
-			if ( empty( $terms ) )
-				continue;
-
-			$tax_operand = 'AND';
-			if ( $operand == 'OR' )
-				$tax_operand = 'IN';
-
-			$arr         = array( 'taxonomy' => $tax, 'field' => 'id', 'operator' => $tax_operand, 'terms' => $terms );
-			$tax_query[] = $arr;
-		}
-
-		if ( count( $tax_query ) > 1 ) {
-			$tax_query['relation'] = $operand;
-		}
-
-		return $tax_query;
-	}
-
 	/* Query Filters */
 
 	public function set_count( $query ) {
