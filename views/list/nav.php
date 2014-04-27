the-events-calendar/the-events-calendar

commit 30abfa807a728f81381a1631ca8b20b84bc54234

Issue #25965 - Wrapped &laquo; and &raquo; in SPANs for responsive positioning

views/list/nav.php
@@ -18,18 +18,18 @@ if ( !defined('ABSPATH') ) { die('-1'); } ?>
 <ul class="tribe-events-sub-nav">
 	<!-- Left Navigation -->
 	<?php if( tribe_is_past() ) : ?>
-		<li class="tribe-events-nav-next tribe-events-nav-left tribe-events-past">
+		<li class="tribe-events-nav-previous tribe-events-nav-left tribe-events-past">
 		<?php if( get_next_posts_link() ) : ?>
-			<a href="<?php tribe_get_past_link() ?>"><?php _e( '&laquo; Previous Events', 'tribe-events-calendar' ) ?></a>
+			<a href="<?php tribe_get_past_link() ?>"><?php _e( '<span>&laquo;</span> Previous Events', 'tribe-events-calendar' ) ?></a>
 		<?php endif; ?>
 		</li><!-- .tribe-events-nav-previous -->
 	<?php elseif ( tribe_is_upcoming() ) : ?>
 		<?php if( get_previous_posts_link() ) : ?>
 			<li class="tribe-events-nav-previous tribe-events-nav-left">
-			<a href="<?php echo tribe_get_upcoming_link() ?>" rel="pref"><?php _e( '&laquo; Previous Events', 'tribe-events-calendar' ) ?></a>
+			<a href="<?php echo tribe_get_upcoming_link() ?>" rel="pref"><?php _e( '<span>&laquo;</span> Previous Events', 'tribe-events-calendar' ) ?></a>
 		<?php elseif ( tribe_has_past_events() ) : ?>
 			<li class="tribe-events-nav-previous tribe-events-nav-left tribe-events-past">
-			<a href="<?php echo tribe_get_past_link() ?>" rel="pref"><?php _e( '&laquo; Previous Events', 'tribe-events-calendar' ) ?></a>
+			<a href="<?php echo tribe_get_past_link() ?>" rel="pref"><?php _e( '<span>&laquo;</span> Previous Events', 'tribe-events-calendar' ) ?></a>
 		<?php endif; ?>
 		</li><!-- .tribe-events-nav-previous -->
 	<?php endif; ?>
@@ -37,17 +37,17 @@ if ( !defined('ABSPATH') ) { die('-1'); } ?>
 	<!-- Right Navigation -->
 	<?php if( tribe_is_past() ) : ?>
 		<?php if( get_query_var( 'paged' ) > 1 ) : ?>
-			<li class="tribe-events-nav-previous tribe-events-nav-right tribe-events-past">
-				<a href="<?php echo tribe_get_past_link() ?>" rel="pref"><?php _e( 'Next Events &raquo;', 'tribe-events-calendar' ) ?></a>
+			<li class="tribe-events-nav-next tribe-events-nav-right tribe-events-past">
+				<a href="<?php echo tribe_get_past_link() ?>" rel="pref"><?php _e( 'Next Events <span>&raquo;</span>', 'tribe-events-calendar' ) ?></a>
 		<?php elseif( !get_previous_posts_link() ) : ?>
-			<li class="tribe-events-nav-previous tribe-events-nav-right">
-				<a href="<?php echo tribe_get_upcoming_link() ?>" rel="next"><?php _e( 'Next Events &raquo;', 'tribe-events-calendar' ) ?></a>
+			<li class="tribe-events-nav-next tribe-events-nav-right">
+				<a href="<?php echo tribe_get_upcoming_link() ?>" rel="next"><?php _e( 'Next Events <span>&raquo;</span>', 'tribe-events-calendar' ) ?></a>
 		<?php endif; ?>
 		</li><!-- .tribe-events-nav-previous -->
 	<?php elseif ( tribe_is_upcoming() ) : ?>
 		<li class="tribe-events-nav-next tribe-events-nav-right">
 		<?php if( get_next_posts_link() ) : ?> 
-			<a href="<?php echo tribe_get_upcoming_link() ?>" rel="next"><?php _e( 'Next Events &raquo;', 'tribe-events-calendar' ) ?></a>
+			<a href="<?php echo tribe_get_upcoming_link() ?>" rel="next"><?php _e( 'Next Events <span>&raquo;</span>', 'tribe-events-calendar' ) ?></a>
 		<?php endif; ?>
 		</li><!-- .tribe-events-nav-previous -->
 	<?php endif; ?>

resources/tribe-events-full.css
@@ -1753,11 +1753,29 @@ input[name*="tribe-bar-"]:-moz-placeholder {
 	}
 	.tribe-events-sub-nav li a {
 		display: inline-block;
-		padding: 10px 15px;
+		padding: 10px 20px;
 		background: #666;
 		color: #fff;
 		white-space: normal;
+		position:relative;
 	}
+	
+	.tribe-events-sub-nav .tribe-events-nav-previous a span{
+		display:block;
+		content:"\ab";
+		position:absolute;
+		left:10px;
+		top:10px;
+	}
+	
+	.tribe-events-sub-nav .tribe-events-nav-next a span{
+		display:block;
+		content: "\00BB";
+		position:absolute;
+		right:10px;
+		top:10px;
+	}
+	
 	.tribe-events-sub-nav li a:visited,
 	.tribe-events-sub-nav li a:hover {
 		color: #fff;

commit 7296dc138af59a53cac906e4a307f5ecd45b4a58

Bug #27075 Fix past view and pagination broken in 30abfa807a728f81381a1631ca8b20b84bc54234

views/list/nav.php
@@ -18,7 +18,7 @@ if ( !defined('ABSPATH') ) { die('-1'); } ?>
 <ul class="tribe-events-sub-nav">
 	<!-- Left Navigation -->
 	<?php if( tribe_is_past() ) : ?>
-		<li class="tribe-events-nav-previous tribe-events-nav-left tribe-events-past">
+		<li class="tribe-events-nav-next tribe-events-nav-left tribe-events-past">
 		<?php if( get_next_posts_link() ) : ?>
 			<a href="<?php tribe_get_past_link() ?>"><?php _e( '<span>&laquo;</span> Previous Events', 'tribe-events-calendar' ) ?></a>
 		<?php endif; ?>
@@ -37,10 +37,10 @@ if ( !defined('ABSPATH') ) { die('-1'); } ?>
 	<!-- Right Navigation -->
 	<?php if( tribe_is_past() ) : ?>
 		<?php if( get_query_var( 'paged' ) > 1 ) : ?>
-			<li class="tribe-events-nav-next tribe-events-nav-right tribe-events-past">
+			<li class="tribe-events-nav-previous tribe-events-nav-right tribe-events-past">
 				<a href="<?php echo tribe_get_past_link() ?>" rel="next"><?php _e( 'Next Events <span>&raquo;</span>', 'tribe-events-calendar' ) ?></a>
 		<?php elseif( !get_previous_posts_link() ) : ?>
-			<li class="tribe-events-nav-next tribe-events-nav-right">
+			<li class="tribe-events-nav-previous tribe-events-nav-right">
 				<a href="<?php echo tribe_get_upcoming_link() ?>" rel="next"><?php _e( 'Next Events <span>&raquo;</span>', 'tribe-events-calendar' ) ?></a>
 		<?php endif; ?>
 		</li><!-- .tribe-events-nav-previous -->