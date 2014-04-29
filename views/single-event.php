1d259301e3a69d533b40ce22e4e708c02c4952a0
barryhughes <barry@codingkills.me>
2014-04-29 16:42:05 -0700

    Ensure single event posts are hCalendar compliant #24914

--- views/single-event.php
+++ views/single-event.php
@@ -18,14 +18,14 @@ $event_id = get_the_ID();
 
 ?>
 
-<div id="tribe-events-content" class="tribe-events-single">
+<div id="tribe-events-content" class="tribe-events-single vevent hentry">
 
 	<p class="tribe-events-back"><a href="<?php echo tribe_get_events_link() ?>"> <?php _e( '&laquo; All Events', 'tribe-events-calendar' ) ?></a></p>
 
 	<!-- Notices -->
 	<?php tribe_events_the_notices() ?>
 
-	<?php the_title( '<h2 class="tribe-events-single-event-title summary">', '</h2>' ); ?>
+	<?php the_title( '<h2 class="tribe-events-single-event-title summary entry-title">', '</h2>' ); ?>
 
 	<div class="tribe-events-schedule updated published tribe-clearfix">
 		<?php echo tribe_events_event_schedule_details( $event_id, '<h3>', '</h3>'); ?>
@@ -46,7 +46,7 @@ $event_id = get_the_ID();
 	</div><!-- #tribe-events-header -->
 
 	<?php while ( have_posts() ) :  the_post(); ?>
-		<div id="post-<?php the_ID(); ?>" <?php post_class('vevent'); ?>>
+		<div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 			<!-- Event featured image, but exclude link -->
 			<?php echo tribe_event_featured_image($event_id, 'full', false); ?>
 
@@ -61,7 +61,7 @@ $event_id = get_the_ID();
 			<?php do_action( 'tribe_events_single_event_before_the_meta' ) ?>
 				<?php echo tribe_events_single_event_meta() ?>
 			<?php do_action( 'tribe_events_single_event_after_the_meta' ) ?>
-			</div><!-- .hentry .vevent -->
+			</div> <!-- #post-x -->
 		<?php if( get_post_type() == TribeEvents::POSTTYPE && tribe_get_option( 'showComments', false ) ) comments_template() ?>
 	<?php endwhile; ?>
 
