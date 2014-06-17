9ade9d67d11d1307c5907d84c406db38cce6cfe8
barryhughes <barry@codingkills.me>
2014-06-17 11:56:40 -0700

    Relabel single event iCal btn for consistency (updates #24401)

--- lib/tribe-ical.class.php
+++ lib/tribe-ical.class.php
@@ -72,7 +72,7 @@ class TribeiCal {
 
 		echo '<div class="tribe-events-cal-links">';
 		echo '<a class="tribe-events-gcal tribe-events-button" href="' . tribe_get_gcal_link() . '" title="' . __( 'Add to Google Calendar', 'tribe-events-calendar' ) . '">+ ' . __( 'Google Calendar', 'tribe-events-calendar-pro' ) . '</a>';
-		echo '<a class="tribe-events-ical tribe-events-button" href="' . tribe_get_single_ical_link() . '">+ ' . __( 'iCal Import', 'tribe-events-calendar' ) . '</a>';
+		echo '<a class="tribe-events-ical tribe-events-button" href="' . tribe_get_single_ical_link() . '">+ ' . __( 'iCal Export', 'tribe-events-calendar' ) . '</a>';
 		echo '</div><!-- .tribe-events-cal-links -->';
 	}
 
