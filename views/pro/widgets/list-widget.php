96ae391d28efd56ef537f035c93af94617eb472b
jessica <jessica@exobi.com>
2014-07-09 13:58:38 -0400

    Fix incorrect text domain Updates #28639

--- views/pro/widgets/list-widget.php
+++ views/pro/widgets/list-widget.php
@@ -113,7 +113,7 @@ if ( $posts ):
 
 	<p class="tribe-events-widget-link">
 		<a href="<?php esc_attr_e( esc_url( $link_to_all ) ) ?>" rel="bookmark">
-			<?php _e( 'View More&hellip;', 'tribe-events-calendar' ) ?>
+			<?php _e( 'View More&hellip;', 'tribe-events-calendar-pro' ) ?>
 		</a>
 	</p>
 <?php
