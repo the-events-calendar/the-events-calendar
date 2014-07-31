6a84ea5d02ce09960067306ba524c3f112859c50
jessica <jessica@exobi.com>
2014-07-31 14:28:53 -0400

    Cleanup complex code in attendee list template

--- admin-views/tickets/attendees.php
+++ admin-views/tickets/attendees.php
@@ -1,92 +1,111 @@
 <?php
 $this->attendees_table->prepare_items();
 
-$event_id = isset( $_GET["event_id"] ) ? intval( $_GET["event_id"] ) : 0;
-$event    = get_post( $event_id );
+$event_id = isset( $_GET['event_id'] ) ? intval( $_GET['event_id'] ) : 0;
+$event = get_post( $event_id );
 $tickets  = TribeEventsTickets::get_event_tickets( $event_id );
 ?>
 
 <div class="wrap">
 	<div id="icon-edit" class="icon32 icon32-tickets-attendees"><br></div>
-	<h2><?php _e( 'Attendees', 'tribe-events-calendar' );?></h2>
+	<h2><?php _e( 'Attendees', 'tribe-events-calendar' ); ?></h2>
+
 	<h2><?php echo $event->post_title; ?></h2>
 
 	<div id="tribe-filters" class="metabox-holder">
 		<div id="filters-wrap" class="postbox">
-			<h3 title="Click to toggle"><?php _e( 'Event Summary', 'tribe-events-calendar' );?></h3>
-
+			<h3 title="Click to toggle"><?php _e( 'Event Summary', 'tribe-events-calendar' ); ?></h3>
 
 
 			<table class="eventtable ticket_list">
-				<tr>
+			<tr>
 					<td width="33%" valign="top">
+						<h4><?php _e( 'Event Details', 'tribe-events-calendar' ); ?></h4>
+
+						<strong><?php _e( 'Start Date / Time:', 'tribe-events-calendar' ) ?></strong>
+						<?php echo tribe_get_start_date( $event_id, false, tribe_get_datetime_format( true ) ) ?>
+						<br/>
+
+						<strong><?php _e( 'End Date / Time:', 'tribe-events-calendar' ) ?></strong>
+						<?php echo tribe_get_end_date( $event_id, false, tribe_get_datetime_format( true ) ) ?>
+
 						<?php
-						echo sprintf( '<h4>%s</h4>', esc_html( __( 'Event Details', 'tribe-events-calendar' ) ) );
-
-						echo sprintf( '<strong>%s </strong> %s', esc_html( __( 'Start Date / Time:', 'tribe-events-calendar' ) ), tribe_get_start_date( $event_id, false, tribe_get_datetime_format(true) ) );
-						echo "<br/>";
-						echo sprintf( '<strong>%s </strong> %s', esc_html( __( 'End Date / Time:', 'tribe-events-calendar' ) ), tribe_get_end_date( $event_id, false, tribe_get_datetime_format(true) ) );
-
+						// venue
 						$venue_id = tribe_get_venue_id( $event_id );
 						if ( ! empty( $venue_id ) )
 							$venue = get_post( $venue_id );
 
-						if ( ! empty( $venue ) ) {
-							echo "<br/>";
-							echo sprintf( '<strong>%s: </strong> %s', esc_html( __( tribe_get_venue_label_singular(), 'tribe-events-calendar' ) ), esc_html( $venue->post_title ) );
+						if ( ! empty( $venue ) ) : ?>
+							<br/>
+							<strong>
+								<?php echo tribe_get_venue_label_singular() ?>
+							</strong>
+							<?php echo $venue->post_title; ?>
 
+							<?php
+							// phone
 							$phone = get_post_meta( $venue_id, '_VenuePhone', true );
-							if ( ! empty( $phone ) ) {
-								echo "<br/>";
-								echo sprintf( '<strong>%s </strong> %s', esc_html( __( 'Phone:', 'tribe-events-calendar' ) ), esc_html( $phone ) );
-							}
 
+							if ( ! empty( $phone ) ) ?>
+								<br/>
+								<strong>
+									<?php _e( 'Phone:', 'tribe-events-calendar' ) ?>
+								</strong>
+								<?php echo esc_html( $phone ); ?>
+							<?php endif; ?>
+
+							<?php
+							// website
 							$website = get_post_meta( $venue_id, '_VenueURL', true );
-							if ( ! empty( $website ) ) {
-								echo "<br/>";
-								echo sprintf( '<strong>%s </strong> <a target="_blank" href="%s">%s</a>', esc_html( __( 'Website:', 'tribe-events-calendar' ) ), esc_url( $website ), esc_html( $website ) );
-							}
-						}
-						?>
+							if ( ! empty( $website ) ) : ?>
+								<br/>
+								<strong>
+									<?php _e( 'Website:', 'tribe-events-calendar' ) ?>
+									<a target="_blank" href="<?php echo esc_url( $website ) ?>"><?php echo esc_html( $website ) ?></a>
+								</strong>
+							<?php endif; ?>
+
+						<?php endif; // if ( $venue ) ?>
+
 					</td>
 					<td width="33%" valign="top">
+						<h4><?php _e( 'Ticket Sales', 'tribe-events-calendar' ); ?></h4>
+
 						<?php
-						echo sprintf( '<h4>%s</h4>', esc_html( __( 'Ticket Sales', 'tribe-events-calendar' ) ) );
 
 						$total_sold = 0;
 
-						foreach ( $tickets as $ticket ) {
+						foreach ( $tickets as $ticket ) : ?>
 
-							echo sprintf( '<strong>%s: </strong>', esc_html( $ticket->name ) );
-
+						<strong><?php echo esc_html( $ticket->name ) ?>: </strong>
+							<?php
 							$stock = $ticket->stock;
 							$sold  = ! empty ( $ticket->qty_sold ) ? $ticket->qty_sold : 0;
 
-							if ( empty( $stock ) && $stock !== 0 ) {
-								echo sprintf( __( "Sold %d", 'tribe-events-calendar' ), esc_html( $sold ) );
-							} else {
-								echo sprintf( __( "Sold %d of %d", 'tribe-events-calendar' ), esc_html( $sold ), esc_html( $sold + $stock ) );
-							}
+							if ( empty( $stock ) && $stock !== 0 ) : ?>
+								<?php echo sprintf( __( "Sold %d", 'tribe-events-calendar' ), esc_html( $sold ) ); ?>
+							<?php else : ?>
+								<?php echo sprintf( __( "Sold %d of %d", 'tribe-events-calendar' ), esc_html( $sold ), esc_html( $sold + $stock ) ); ?>
+							<?php endif; ?>
+							<br/>
 
-							$total_sold += $sold;
+							<?php $total_sold += $sold;
 
-							echo "<br/>";
-						}
-						?>
+						endforeach; ?>
 					</td>
 					<td width="33%" valign="middle">
 						<div class="totals">
 							<?php
 
-							$checkedin = TribeEventsTickets::get_event_checkedin_attendees_count( $event_id );
-							echo '<span id="total_tickets_sold_wrapper">';
-							echo sprintf( '%s <span id="total_tickets_sold">%d</span>', esc_html( __( 'Tickets sold:', 'tribe-events-calendar' ) ), $total_sold );
-							echo '</span>';
-							echo '<span id="total_checkedin_wrapper">';
-							echo "<br/>";
-							echo sprintf( '%s <span id="total_checkedin">%d</span>', esc_html( __( 'Checked in:', 'tribe-events-calendar' ) ), $checkedin );
-							echo '</span>';
-							?>
+							$checkedin = TribeEventsTickets::get_event_checkedin_attendees_count( $event_id ); ?>
+
+							<span id="total_tickets_sold_wrapper">
+								<?php _e( 'Tickets sold:', 'tribe-events-calendar' ) ?> <span id="total_tickets_sold"><?php echo $total_sold ?></span>
+							</span>
+							<span id="total_checkedin_wrapper">
+								<br/>
+								<?php _e( 'Checked in:', 'tribe-events-calendar' ); ?> <span id="total_checkedin"><?php echo $checkedin ?></span>
+							</span>
 						</div>
 					</td>
 				</tr>
