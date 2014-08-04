43b2a213c15e4eba038bda72fd85c54aeb6ad3f7
jessica <jessica@exobi.com>
2014-08-04 16:30:43 -0400

    Reformat tribe-event-exception.class.php to WP coding standards Updates #28316

--- lib/tribe-event-exception.class.php
+++ lib/tribe-event-exception.class.php
@@ -4,17 +4,24 @@
  */
 
 // Don't load directly
-if ( !defined('ABSPATH') ) { die('-1'); }
+if ( ! defined( 'ABSPATH' ) ) {
+	die( '-1' );
+}
 
-if( !class_exists( 'TribeEventsPostException' ) ) {
+if ( ! class_exists( 'TribeEventsPostException' ) ) {
 	class TribeEventsPostException extends Exception {
 		/**
-		* Display the exception message in the div #tribe-events-post-error
-		* @param int $post->ID
-		*/
+		 * Display the exception message in the div #tribe-events-post-error
+		 *
+		 * @param int $post ->ID
+		 */
 		public static function displayMessage( $postId ) {
-			if( $error = get_post_meta( $postId, TribeEvents::EVENTSERROROPT, true ) ) : ?>
-				<script type="text/javascript">jQuery('#tribe-events-post-error').append('<h3><?php _e( 'Error', 'tribe-events-calendar' ); ?></h3><p>' + '<?php echo $error; ?>' + '</p>').show();</script>
+			if ( $error = get_post_meta( $postId, TribeEvents::EVENTSERROROPT, true ) ) : ?>
+				<script type="text/javascript">
+					jQuery('#tribe-events-post-error')
+						.append('<h3><?php _e( 'Error', 'tribe-events-calendar' ); ?></h3><p>' + '<?php echo $error; ?>' + '</p>')
+						.show();
+				</script>
 			<?php endif;
 		}
 	} // end TribeEventsPostException
@@ -23,16 +30,16 @@ if( !class_exists( 'TribeEventsPostException' ) ) {
 /**
  * Exception handling for third-party plugins dealing with the WordPress options view.
  */
-if( !class_exists( 'TribeEventsOptionsException' ) ) {
+if ( ! class_exists( 'TribeEventsOptionsException' ) ) {
 	class TribeEventsOptionsException extends Exception {
 		/**
-		* Display the exception message in the div #tribe-events-options-error
-		*/
+		 * Display the exception message in the div #tribe-events-options-error
+		 */
 		public static function displayMessage() {
-			$eventsOptions = get_option(TribeEvents::OPTIONNAME, array() );
-			if( isset($eventsOptions['error']) && $eventsOptions['error'] ) : ?>
+			$eventsOptions = get_option( TribeEvents::OPTIONNAME, array() );
+			if ( isset( $eventsOptions['error'] ) && $eventsOptions['error'] ) : ?>
 				<script type="text/javascript">jQuery('#tribe-events-options-error').append('<h3><?php _e( 'Error', 'tribe-events-calendar' ); ?></h3><p>' + '<?php echo $eventsOptions['error']; ?>' + '</p>').show();</script>
 			<?php endif;
-	    }
+		}
 	} // end TribeEventsOptionsException
 } // end if !class_exists TribeEventsOptionsException
