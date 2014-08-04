4386bfbbf41a0f28a43343e9d7e08c8f372e9db4
jessica <jessica@exobi.com>
2014-08-04 16:18:48 -0400

    Reformat tribe-debug-bar.class.php to WP coding standards Updates #28316

--- lib/tribe-debug-bar.class.php
+++ lib/tribe-debug-bar.class.php
@@ -1,13 +1,16 @@
 <?php
 /**
  * Integrate Tribe debugging with debug bar plugin.
+ * @todo move to a plugin
  */
 
 // Don't load directly
-if ( !defined('ABSPATH') ) { die('-1'); }
+if ( ! defined( 'ABSPATH' ) ) {
+	die( '-1' );
+}
 
-function load_tribe_debug_bar($panels) {
-	if (!class_exists('TribeDebugBar') && class_exists('Debug_Bar_Panel')) {
+function load_tribe_debug_bar( $panels ) {
+	if ( ! class_exists( 'TribeDebugBar' ) && class_exists( 'Debug_Bar_Panel' ) ) {
 
 		/**
 		 * Debug bar class for the Events Calendar to support debugging.
@@ -25,7 +28,7 @@ function load_tribe_debug_bar($panels) {
 			 * Initialize the class and place hooks and styling.
 			 */
 			function init() {
-				$this->title( __('Tribe', 'tribe-events-calendar') );
+				$this->title( __( 'Tribe', 'tribe-events-calendar' ) );
 				remove_action( 'tribe_debug', array( TribeEvents::instance(), 'renderDebug' ), 10, 2 );
 				add_action( 'tribe_debug', array( $this, 'logDebug' ), 8, 3 );
 				wp_enqueue_style( 'tribe-debugger', TribeEvents::instance()->pluginUrl . 'resources/debugger.css', array(), apply_filters( 'tribe_events_css_version', TribeEvents::VERSION ) );
@@ -44,14 +47,14 @@ function load_tribe_debug_bar($panels) {
 			 */
 			function render() {
 				echo '<div id="debug-bar-tribe">';
-				if (count(self::$debug_log)) {
+				if ( count( self::$debug_log ) ) {
 					echo '<ul>';
-					foreach(self::$debug_log as $k => $logentry) {
+					foreach ( self::$debug_log as $k => $logentry ) {
 						echo "<li class='tribe-debug-{$logentry['format']}'>";
 						echo "<div class='tribe-debug-entry-title'>{$logentry['title']}</div>";
-						if (isset($logentry['data']) && $logentry['data']) {
+						if ( isset( $logentry['data'] ) && $logentry['data'] ) {
 							echo '<div class="tribe-debug-entry-data"><pre>';
-							print_r($logentry['data']);
+							print_r( $logentry['data'] );
 							echo '</pre></div>';
 						}
 						echo '</li>';
@@ -64,21 +67,24 @@ function load_tribe_debug_bar($panels) {
 			/**
 			 * log debug statements for display in debug bar
 			 *
-			 * @param string $title Message to display in log
-			 * @param string|bool $data Optional data to display
-			 * @param string $format Optional format (log|warning|error|notice)
+			 * @param string      $title  Message to display in log
+			 * @param string|bool $data   Optional data to display
+			 * @param string      $format Optional format (log|warning|error|notice)
+			 *
 			 * @return void
 			 */
 			public function logDebug( $title, $data = false, $format = 'log' ) {
 				self::$debug_log[] = array(
-					'title' => $title,
-					'data' => $data,
+					'title'  => $title,
+					'data'   => $data,
 					'format' => $format,
 				);
 			}
 		}
+
 		$panels[] = new TribeDebugBar;
 	}
+
 	return $panels;
 }
 
