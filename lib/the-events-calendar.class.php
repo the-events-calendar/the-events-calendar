a96feadf76783f2188eef62bd8a584d890933949
jessica <jessica@exobi.com>
2014-08-31 14:25:00 -0400

    Add correct textdomain to 5-star rating plug Updates #29495

--- lib/the-events-calendar.class.php
+++ lib/the-events-calendar.class.php
@@ -485,7 +485,7 @@ if ( ! class_exists( 'TribeEvents' ) ) {
 
 			// only display custom text on Tribe Admin Pages
 			if ( isset( $current_screen->id ) && strpos( $current_screen->id, 'tribe' ) !== false ) {
-				return sprintf( __( 'Rate <strong>The Events Calendar</strong> <a href="%1$s" target="_blank">&#9733;&#9733;&#9733;&#9733;&#9733;</a> on <a href="%1$s" target="_blank">WordPress.org</a> to keep this plugin free.  Thanks from the friendly folks at Modern Tribe.' ), __( 'http://wordpress.org/support/view/plugin-reviews/the-events-calendar?filter=5' ) );
+				return sprintf( __( 'Rate <strong>The Events Calendar</strong> <a href="%1$s" target="_blank">&#9733;&#9733;&#9733;&#9733;&#9733;</a> on <a href="%1$s" target="_blank">WordPress.org</a> to keep this plugin free.  Thanks from the friendly folks at Modern Tribe.', 'tribe-events-calendar' ), __( 'http://wordpress.org/support/view/plugin-reviews/the-events-calendar?filter=5', 'tribe-events-calendar' ) );
 			} else {
 				return $footer_text;
 			}
