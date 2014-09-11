2fcd5296fe0c11a2b7d92d55eacfc147666f9935
Casey <caseypicker@gmail.com>
2014-09-11 07:56:49 -0500

    Displaying Venue even if no address exists - updates #28985

--- views/modules/meta.php
+++ views/modules/meta.php
@@ -30,14 +30,12 @@ tribe_get_template_part( 'modules/meta/details' );
 // If we have no map to embed and no need to keep the venue separate...
 if ( ! $set_venue_apart && ! tribe_embed_google_map() ) {
 	tribe_get_template_part( 'modules/meta/venue' );
-} elseif ( ! $set_venue_apart && ! tribe_has_organizer() && tribe_address_exists() && tribe_embed_google_map() ) {
+} elseif ( ! $set_venue_apart && ! tribe_has_organizer() && tribe_embed_google_map() ) {
 	// If we have no organizer, no need to separate the venue but we have a map to embed...
 	tribe_get_template_part( 'modules/meta/venue' );
 	echo '<div class="tribe-events-meta-group tribe-events-meta-group-gmap">';
 	tribe_get_template_part( 'modules/meta/map' );
-	?>
-	</div>
-<?php
+	echo '</div>';
 } else {
 	// If the venue meta has not already been displayed then it will be printed separately by default
 	$set_venue_apart = true;
@@ -56,7 +54,7 @@ do_action( 'tribe_events_single_event_meta_primary_section_end' );
 <?php endif; ?>
 
 
-<?php if ( $set_venue_apart && tribe_address_exists() ) : ?>
+<?php if ( $set_venue_apart ) : ?>
 	<?php if ( $not_skeleton ) : ?>
 		<div class="tribe-events-single-section tribe-events-event-meta secondary tribe-clearfix">
 	<?php endif; ?>
