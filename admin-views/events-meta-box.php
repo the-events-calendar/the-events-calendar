84fca2de2c39826b1645227d084bc45594727a10
barryhughes <barry@codingkills.me>
2014-09-26 09:35:44 -0700

    Minor correction: missing angle bracket

--- admin-views/events-meta-box.php
+++ admin-views/events-meta-box.php
@@ -149,7 +149,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 						}
 						?>
 						<option value="prefix"> <?php _ex( 'Before cost', 'Currency symbol position', 'tribe-events-calendar' ) ?> </option>
-						<option value="suffix"<?php if ( $suffix ) {
+						<option value="suffix"> <?php if ( $suffix ) {
 							echo ' selected="selected"';
 							} ?><?php _ex( 'After cost', 'Currency symbol position', 'tribe-events-calendar' ) ?> </option>
 					</select>
