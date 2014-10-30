ba899ca92f1cce8d08ef0a5baf56392fa1154950
barryhughes <barry@codingkills.me>
2014-10-30 10:34:32 -0700

    Hook up new ticketing-specific Google Event Data class (updated #31486)

--- lib/the-events-calendar.class.php
+++ lib/the-events-calendar.class.php
@@ -289,6 +289,7 @@ if ( ! class_exists( 'TribeEvents' ) ) {
 			require_once 'tickets/tribe-ticket-object.php';
 			require_once 'tickets/tribe-tickets.php';
 			require_once 'tickets/tribe-tickets-metabox.php';
+			require_once 'tickets/google-event-data.php';
 
 			// CSV Importer
 			require_once 'io/csv/ecp-events-importer.php';
