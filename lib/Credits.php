fc9f1ccc86e8284d6ef94b410596aaa9d8183333
jessica <jessica@exobi.com>
2014-11-20 21:33:45 -0500

    Show html comment credit only after tribe views Updates #32207

--- lib/Credits.php
+++ lib/Credits.php
@@ -18,7 +18,7 @@ class Tribe__Events__Credits {
 	 * Hook the functionality of this class into the world
 	 */
 	public function hook() {
-		add_action( 'wp_footer', array( __CLASS__, 'html_comment_credit' ) );
+		add_action( 'tribe_events_after_html', array( __CLASS__, 'html_comment_credit' ) );
 		add_filter( 'admin_footer_text', array( $this, 'rating_nudge' ), 1, 2 );
 	}
 
@@ -27,9 +27,8 @@ class Tribe__Events__Credits {
 	 *
 	 * @return void
 	 **/
-	public static function html_comment_credit() {
+	public function html_comment_credit() {
 		$html_credit = "\n<!--\n" . __( 'This calendar is powered by The Events Calendar.', 'tribe-events-calendar' ) . "\nhttp://eventscalendarpro.com/\n-->\n";
-
 		echo apply_filters( 'tribe_html_credit', $html_credit );
 	}
 
