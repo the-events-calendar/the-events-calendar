ded22379393dce09aabcecdb1c50a881cd988f36
barryhughes <barry@codingkills.me>
2014-12-30 13:20:15 -0800

    Revert "Support network deactivation cleanup (+specifically handle recurrence cron removal - updates #30254)"
    This reverts commit 0a9a3da90903ca7da4f23757fe6f0b301df1c039.

--- lib/Abstract_Deactivation.php
+++ lib/Abstract_Deactivation.php
@@ -43,15 +43,12 @@ abstract class Tribe__Events__Abstract_Deactivation {
 		$site = get_current_site();
 		$blog_ids = $wpdb->get_col( $wpdb->prepare( 'SELECT blog_id FROM {$wpdb->blogs} WHERE site_id=%d', $site->id ) );
 		$large = wp_is_large_network();
-
 		foreach ( $blog_ids as $blog ) {
 			set_time_limit( 30 );
 			switch_to_blog( $blog );
 			$large ? $this->short_blog_deactivate() : $this->blog_deactivate();
 			restore_current_blog();
 		}
-
-		do_action( 'tribe_events_pro_multisite_deactivate' );
 	}
 
 	/**
