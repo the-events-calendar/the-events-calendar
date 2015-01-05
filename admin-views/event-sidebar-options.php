745b2aff5a374ed4ec47506ab695a8a8e17ca453
Casey <caseypicker@gmail.com>
2015-01-05 10:50:28 -0600

    Update text to use tribe_get_event_label_singular() - updates #28697

--- admin-views/event-sidebar-options.php
+++ admin-views/event-sidebar-options.php
@@ -10,7 +10,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 ?>
 <?php global $post; ?>
-<label class="selectit"><input value="yes" type="checkbox" <?php checked( tribe_get_event_meta( $post->ID, '_EventHideFromUpcoming' ) == "yes" ) ?> name="EventHideFromUpcoming"> <?php _e( "Hide From Event Listings", 'tribe-events-calendar' ); ?>
+<label class="selectit"><input value="yes" type="checkbox" <?php checked( tribe_get_event_meta( $post->ID, '_EventHideFromUpcoming' ) == "yes" ) ?> name="EventHideFromUpcoming"> <?php printf( __( "Hide From %s Listings", 'tribe-events-calendar' ), tribe_get_event_label_singular() ); ?>
 </label><br /><br />
 <label class="selectit"><input value="yes" type="checkbox" <?php checked( $post->menu_order == "-1" ) ?> name="EventShowInCalendar"> <?php _e( "Sticky in Calendar View", 'tribe-events-calendar' ); ?>
 </label>
