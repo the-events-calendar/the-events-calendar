d52bc161c12b02a01eb373094b51f6afdad1686c
jessica <jessica@exobi.com>
2015-01-15 12:25:26 -0500

    Bump version to 3.9.1

--- events-calendar-pro.php
+++ events-calendar-pro.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: The Events Calendar PRO
 Description: The Events Calendar PRO, a premium add-on to the open source The Events Calendar plugin (required), enables recurring events, custom attributes, venue pages, new widgets and a host of other premium features.
-Version: 3.9
+Version: 3.9.1
 Author: Modern Tribe, Inc.
 Author URI: http://m.tri.be/20
 Text Domain: tribe-events-calendar-pro
@@ -59,8 +59,8 @@ if ( !class_exists( 'TribeEventsPro' ) ) {
 		 */
 		public $mini_calendar_shortcode;
 
-		const REQUIRED_TEC_VERSION = '3.9';
-		const VERSION = '3.9';
+		const REQUIRED_TEC_VERSION = '3.9.1';
+		const VERSION = '3.9.1';
 
         /**
          * Class constructor.
