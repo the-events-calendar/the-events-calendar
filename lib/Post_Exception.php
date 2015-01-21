df21f8ad01cedf4bc6f7b8adf2c8540bd4f1f229
Jonathan Brinley <jbrinley@flightless.us>
2015-01-21 11:12:23 -0500

    move Tribe__Events__Options_Exception to its own file

--- lib/Post_Exception.php
+++ lib/Post_Exception.php
@@ -26,20 +26,3 @@ if ( ! class_exists( 'TribeEventsPostException' ) ) {
 		}
 	} // end Tribe__Events__Post_Exception
 } // end if !class_exists Tribe__Events__Post_Exception
-
-/**
- * Exception handling for third-party plugins dealing with the WordPress options view.
- */
-if ( ! class_exists( 'TribeEventsOptionsException' ) ) {
-	class TribeEventsOptionsException extends Exception {
-		/**
-		 * Display the exception message in the div #tribe-events-options-error
-		 */
-		public static function displayMessage() {
-			$eventsOptions = get_option( Tribe__Events__Events::OPTIONNAME, array() );
-			if ( isset( $eventsOptions['error'] ) && $eventsOptions['error'] ) : ?>
-				<script type="text/javascript">jQuery('#tribe-events-options-error').append('<h3><?php _e( 'Error', 'tribe-events-calendar' ); ?></h3><p>' + '<?php echo $eventsOptions['error']; ?>' + '</p>').show();</script>
-			<?php endif;
-		}
-	} // end TribeEventsOptionsException
-} // end if !class_exists TribeEventsOptionsException
