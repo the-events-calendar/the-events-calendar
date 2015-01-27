a22d769374048297d6086b9761df52805dec3d09
theAverageDev (Luca Tumedei) <luca@theaveragedev.com>
2015-01-28 07:44:13 +0800

    fixed wrong "list" case - 33457

--- views/list.php
+++ views/list.php
@@ -4,7 +4,7 @@
  * The wrapper template for a list of events. This includes the Past Events and Upcoming Events views
  * as well as those same views filtered to a specific category.
  *
- * Override this template in your own theme by creating a file at [your-theme]/tribe-events/List.php
+ * Override this template in your own theme by creating a file at [your-theme]/tribe-events/list.php
  *
  * @package TribeEventsCalendar
  *
