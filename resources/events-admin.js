f39aa86e2eb69d9ad3079b3dc5e6d02d0cbe520c
george <george@momnt.co>
2015-01-27 21:24:39 -0500

    use snake_case for functions and vars – updates #31776

--- resources/events-admin.js
+++ resources/events-admin.js
@@ -5,7 +5,7 @@ jQuery( document ).ready( function( $ ) {
 		viewCalLinkHTML   = $( '#view-calendar-link-div' ).html(),
 		$template_select  = $( 'select[name="tribeEventsTemplate"]' ),
 		$event_pickers    = $( '#tribe-event-datepickers' ),
-		isCommunityEdit   = $( 'body' ).is( '.tribe_community_edit' ),
+		is_community_edit   = $( 'body' ).is( '.tribe_community_edit' ),
 		datepicker_format = 0;
 
 	// Modified from tribe_ev.data to match jQuery UI formatting.
@@ -36,8 +36,8 @@ jQuery( document ).ready( function( $ ) {
 
 	//not done by default on front end
 
-	function getDatepickerNumMonths() {
-		return ( isCommunityEdit && $(window).width() < 768 ) ? 1 : 3;
+	function get_datepicker_num_months() {
+		return ( is_community_edit && $(window).width() < 768 ) ? 1 : 3;
 	}
 
 	$( '.hide-if-js' )
@@ -76,11 +76,11 @@ jQuery( document ).ready( function( $ ) {
 			showAnim       : 'fadeIn',
 			changeMonth    : true,
 			changeYear     : true,
-			numberOfMonths : getDatepickerNumMonths(),
+			numberOfMonths : get_datepicker_num_months(),
 			firstDay       : startofweek,
 			showButtonPanel: true,
 			beforeShow     : function( element, object ) {
-				object.input.datepicker( 'option', 'numberOfMonths', getDatepickerNumMonths() );
+				object.input.datepicker( 'option', 'numberOfMonths', get_datepicker_num_months() );
 				object.input.data( 'prevDate', object.input.datepicker( "getDate" ) );
 			},
 			onSelect       : function( selectedDate ) {
