bf55f3117a37f87dc550e447732c4a86b0b6908e
elimn <brook@eli.mn>
2015-02-12 08:26:12 -0700

    Cleaned up tribe_event_featured_image() -- updates #32719

--- public/template-tags/general.php
+++ public/template-tags/general.php
@@ -950,19 +950,18 @@ if ( class_exists( 'Tribe__Events__Events' ) ) {
 		if ( is_null( $post_id ) ) {
 			$post_id = get_the_ID();
 		}
-		$image_src      = wp_get_attachment_image_src( get_post_thumbnail_id( $post_id ), $size );
-		$image_html     = get_the_post_thumbnail( $post_id, $size );
 
+		$image_html     = get_the_post_thumbnail( $post_id, $size );
 		$featured_image = '';
 
 		//if link is not specifically excluded, then include <a>
-		if ( ! empty( $image_src ) && $link ) {
+		if ( ! empty( $image_html ) && $link ) {
 			$featured_image .= '<div class="tribe-events-event-image"><a href="' . tribe_get_event_link() . '">' . $image_html . '</a></div>';
-		} elseif ( ! empty( $image_src ) ) {
+		} elseif ( ! empty( $image_html ) ) {
 			$featured_image .= '<div class="tribe-events-event-image">' . $image_html . '</div>';
 		}
 
-		return apply_filters( 'tribe_event_featured_image', $featured_image, $post_id, $size, $image_src );
+		return apply_filters( 'tribe_event_featured_image', $featured_image, $post_id, $size );
 	}
 
 	/**
