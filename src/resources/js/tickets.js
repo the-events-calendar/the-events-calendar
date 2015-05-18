f7fcbca994d996015bee6a79b2c0b4227843034a
Evan Mattson <me@aaemnnost.tv>
2015-05-18 17:12:27 -0400

    cache #tribetickets query

--- src/resources/js/tickets.js
+++ src/resources/js/tickets.js
@@ -131,6 +131,8 @@ var ticketHeaderImage = window.ticketHeaderImage || {};
 
 		} );
 
+		var $tribetickets = $('#tribetickets');
+
 		/* "Save Ticket" button action */
 		$( '#ticket_form_save' ).click( function() {
 
@@ -166,7 +168,7 @@ var ticketHeaderImage = window.ticketHeaderImage || {};
 
 		/* "Delete Ticket" link action */
 
-		$( '#tribetickets' ).on( 'click', '.ticket_delete', function( e ) {
+		$tribetickets.on( 'click', '.ticket_delete', function( e ) {
 
 			e.preventDefault();
 
@@ -199,7 +201,7 @@ var ticketHeaderImage = window.ticketHeaderImage || {};
 
 		/* "Edit Ticket" link action */
 
-		$( '#tribetickets' )
+		$tribetickets
 			.on( 'click', '.ticket_edit', function( e ) {
 
 				e.preventDefault();
@@ -380,7 +382,7 @@ var ticketHeaderImage = window.ticketHeaderImage || {};
 		}
 
 		function tribe_fix_image_width() {
-			if ( $( '#tribetickets' ).width() < $tiximg.width() ) {
+			if ( $tribetickets.width() < $tiximg.width() ) {
 				$tiximg.css( "width", '95%' );
 			}
 		}
