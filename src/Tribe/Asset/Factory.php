704f59a01f4223cef17920007260f23ff831789c
Matthew Batchelder <borkweb@gmail.com>
2015-05-18 16:52:21 -0400

    Adding fixes to fully support rewritten Template_Factory
    See: https://central.tri.be/issues/36164 and https://central.tri.be/issues/33500

---
+++ src/Tribe/Asset/Factory.php
@@ -0,0 +1,9 @@
+<?php
+class Tribe__Events__Pro__Asset__Factory extends Tribe__Events__Asset__Factory {
+		/**
+		 * @return string
+		 */
+		protected function get_asset_class_name_prefix() {
+			return 'Tribe__Events__Pro__Asset__';
+		}
+}
