dfaad198f2893a2d06bf1bed4b2f61cb90982a29
Matthew Batchelder <borkweb@gmail.com>
2015-06-01 14:20:19 -0400

    Abstract out date meta logic. If missing dates, don't update dates.
    See: https://central.tri.be/issues/34636

--- src/Tribe/API.php
+++ src/Tribe/API.php
@@ -91,34 +91,11 @@ if ( ! class_exists( 'Tribe__Events__API' ) ) {
 		public static function saveEventMeta( $event_id, $data, $event = null ) {
 			$tec = Tribe__Events__Main::instance();
 
-			if ( isset( $data['EventAllDay'] ) && ( $data['EventAllDay'] == 'yes' || $data['EventAllDay'] == true || ! isset( $data['EventStartDate'] ) ) ) {
-				$data['EventStartDate'] = tribe_event_beginning_of_day( $data['EventStartDate'] );
-				$data['EventEndDate']   = tribe_event_end_of_day( $data['EventEndDate'] );
-			} else {
-				delete_post_meta( $event_id, '_EventAllDay' );
-				if ( isset( $data['EventStartMeridian'] ) ) {
-					$data['EventStartDate'] = date( Tribe__Events__Date_Utils::DBDATETIMEFORMAT, strtotime( $data['EventStartDate'] . " " . $data['EventStartHour'] . ":" . $data['EventStartMinute'] . ":00 " . $data['EventStartMeridian'] ) );
-					$data['EventEndDate']   = date( Tribe__Events__Date_Utils::DBDATETIMEFORMAT, strtotime( $data['EventEndDate'] . " " . $data['EventEndHour'] . ":" . $data['EventEndMinute'] . ":00 " . $data['EventEndMeridian'] ) );
+			$data = self::prepare_event_date_meta( $event_id, $data );
 
-				} else {
-					$data['EventStartDate'] = date( Tribe__Events__Date_Utils::DBDATETIMEFORMAT, strtotime( $data['EventStartDate'] . " " . $data['EventStartHour'] . ":" . $data['EventStartMinute'] . ":00" ) );
-					$data['EventEndDate']   = date( Tribe__Events__Date_Utils::DBDATETIMEFORMAT, strtotime( $data['EventEndDate'] . " " . $data['EventEndHour'] . ":" . $data['EventEndMinute'] . ":00" ) );
-				}
-			}
-
-			if ( empty ( $data['EventHideFromUpcoming'] ) ) {
+			if ( empty( $data['EventHideFromUpcoming'] ) ) {
 				delete_post_meta( $event_id, '_EventHideFromUpcoming' );
 			}
-
-			// sanity check that start date < end date
-			$startTimestamp = strtotime( $data['EventStartDate'] );
-			$endTimestamp   = strtotime( $data['EventEndDate'] );
-
-			if ( $startTimestamp > $endTimestamp ) {
-				$data['EventEndDate'] = $data['EventStartDate'];
-			}
-
-			$data['EventDuration'] = strtotime( $data['EventEndDate'] ) - $startTimestamp;
 
 			update_post_meta( $event_id, '_EventShowMapLink', isset( $data['venue']['EventShowMapLink'] ) );
 			update_post_meta( $event_id, '_EventShowMap', isset( $data['venue']['EventShowMap'] ) );
@@ -129,21 +106,21 @@ if ( ! class_exists( 'Tribe__Events__API' ) ) {
 				$post_status = get_post_status( $event_id );
 			}
 
-			if ( isset( $data["Organizer"] ) ) {
-				if ( ! empty( $data["Organizer"]["OrganizerID"] ) ) {
-					$organizer_post_status = get_post( $data["Organizer"]['OrganizerID'] )->post_status;
+			if ( isset( $data['Organizer'] ) ) {
+				if ( ! empty( $data['Organizer']['OrganizerID'] ) ) {
+					$organizer_post_status = get_post( $data['Organizer']['OrganizerID'] )->post_status;
 				} else {
 					$organizer_post_status = $post_status;
 				}
-				$data['EventOrganizerID'] = Tribe__Events__API::saveEventOrganizer( $data["Organizer"], $event, $organizer_post_status );
+				$data['EventOrganizerID'] = self::saveEventOrganizer( $data['Organizer'], $event, $organizer_post_status );
 			}
-			if ( isset( $data["Venue"] ) ) {
-				if ( ! empty( $data['Venue']["VenueID"] ) ) {
+			if ( isset( $data['Venue'] ) ) {
+				if ( ! empty( $data['Venue']['VenueID'] ) ) {
 					$venue_post_status = get_post( $data['Venue']['VenueID'] )->post_status;
 				} else {
 					$venue_post_status = $post_status;
 				}
-				$data['EventVenueID'] = Tribe__Events__API::saveEventVenue( $data["Venue"], $event, $venue_post_status );
+				$data['EventVenueID'] = self::saveEventVenue( $data['Venue'], $event, $venue_post_status );
 			}
 
 			// Ordinarily there is a single cost value for each event, but addons (ie, ticketing plugins) may need
@@ -156,20 +133,20 @@ if ( ! class_exists( 'Tribe__Events__API' ) ) {
 			//update meta fields
 			foreach ( $tec->metaTags as $tag ) {
 				$htmlElement = ltrim( $tag, '_' );
-				if ( isset( $data[$htmlElement] ) && $tag != Tribe__Events__Main::EVENTSERROROPT ) {
-					if ( is_string( $data[$htmlElement] ) ) {
-						$data[$htmlElement] = filter_var( $data[$htmlElement], FILTER_SANITIZE_STRING );
+				if ( isset( $data[ $htmlElement ] ) && $tag != Tribe__Events__Main::EVENTSERROROPT ) {
+					if ( is_string( $data[ $htmlElement ] ) ) {
+						$data[ $htmlElement ] = filter_var( $data[ $htmlElement ], FILTER_SANITIZE_STRING );
 					}
 					// Fields with multiple values per key
-					if ( is_array( $data[$htmlElement] ) ) {
+					if ( is_array( $data[ $htmlElement ] ) ) {
 						delete_post_meta( $event_id, $tag );
-						foreach ( $data[$htmlElement] as $value ) {
+						foreach ( $data[ $htmlElement ] as $value ) {
 							add_post_meta( $event_id, $tag, $value );
 						}
 					}
 					// Fields with a single value per key
 					else {
-						update_post_meta( $event_id, $tag, $data[$htmlElement] );
+						update_post_meta( $event_id, $tag, $data[ $htmlElement ] );
 					}
 				}
 			}
@@ -191,6 +168,68 @@ if ( ! class_exists( 'Tribe__Events__API' ) ) {
 
 			do_action( 'tribe_events_update_meta', $event_id, $data );
 		}
+
+		/**
+		 * given a set of meta data, prepare date data if it exists
+		 *
+		 * @param $data array Associative array of event meta data
+		 *
+		 * @return array
+		 */
+		protected static function prepare_event_date_meta( $event_id, $data ) {
+			$date_provided = false;
+
+			if (
+				isset( $data['EventAllDay'] )
+				&& (
+					'yes' === $data['EventAllDay']
+					|| true === $data['EventAllDay']
+					|| ! isset( $data['EventStartDate'] )
+				)
+			) {
+				$date_provided = true;
+				$data['EventStartDate'] = tribe_event_beginning_of_day( $data['EventStartDate'] );
+				$data['EventEndDate']   = tribe_event_end_of_day( $data['EventEndDate'] );
+			} elseif (
+				isset( $data['EventStartDate'] )
+				&& isset( $data['EventEndDate'] )
+			) {
+				$date_provided = true;
+				delete_post_meta( $event_id, '_EventAllDay' );
+
+				$start_date_string = "{$data['EventStartDate']} {$data['EventStartHour']}:{$data['EventStartMinute']}:00";
+				$end_date_string = "{$data['EventEndDate']} {$data['EventEndHour']}:{$data['EventEndMinute']}:00";
+
+				if ( isset( $data['EventStartMeridian'] ) ) {
+					$start_date_string .= " {$data['EventStartMeridian']}";
+				}
+
+				if ( isset( $data['EventEndMeridian'] ) ) {
+					$end_date_string .= " {$data['EventEndMeridian']}";
+				}
+
+				$data['EventStartDate'] = date( Tribe__Events__Date_Utils::DBDATETIMEFORMAT, strtotime( $start_date_string ) );
+				$data['EventEndDate'] = date( Tribe__Events__Date_Utils::DBDATETIMEFORMAT, strtotime( $end_date_string ) );
+			}
+
+			if ( ! $date_provided ) {
+				$data['EventStartDate'] = get_post_meta( $event_id, '_EventStartDate', true );
+				$data['EventEndDate'] = get_post_meta( $event_id, '_EventEndDate', true );
+				return $data;
+			}
+
+			// sanity check that start date < end date
+			$start_timestamp = strtotime( $data['EventStartDate'] );
+			$end_timestamp   = strtotime( $data['EventEndDate'] );
+
+			if ( $start_timestamp > $end_timestamp ) {
+				$data['EventEndDate'] = $data['EventStartDate'];
+			}
+
+			$data['EventDuration'] = strtotime( $data['EventEndDate'] ) - $start_timestamp;
+
+			return $data;
+		}//end prepare_event_date_meta
 
 		/**
 		 * Triggers an update of the cost meta data (min <-> max) for an event.
