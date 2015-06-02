2e61c9b3aa94d25d2786815d80ab549d42618812
Matthew Batchelder <borkweb@gmail.com>
2015-06-02 15:04:08 -0400

    Consolidate logic for get min/max

--- src/Tribe/Cost_Utils.php
+++ src/Tribe/Cost_Utils.php
@@ -39,13 +39,14 @@ class Tribe__Events__Cost_Utils {
 	}//end get_all_costs
 
 	/**
-	 * Returns a maximum cost in a list of costs. If an array of costs is not passed in, the array of costs is fetched via query.
+	 * Returns a particular cost within an array of costs
 	 *
 	 * @param $costs mixed Cost(s) to review for max value
+	 * @param $function string Function to use to determine which cost to return from range. Valid values: max, min
 	 *
 	 * @return float
 	 */
-	public function get_maximum_cost( $costs = null ) {
+	protected function get_cost_by_func( $costs = null, $function = 'max' ) {
 		if ( null === $costs ) {
 			$costs = $this->get_all_costs();
 		} elseif ( ! is_array( $costs ) ) {
@@ -61,13 +62,32 @@ class Tribe__Events__Cost_Utils {
 			return 0;
 		}
 
-		$min = max( $costs );
+		switch ( $function ) {
+			case 'min':
+				$cost = min( $costs );
+				break;
+			case 'max':
+			default:
+				$cost = max( $costs );
+				break;
+		}//end switch
 
-		if ( ! is_numeric( $min ) ) {
+		if ( ! is_numeric( $cost ) ) {
 			return 0;
 		}
 
-		return $min;
+		return $cost;
+	}//end get_cost_by_func
+
+	/**
+	 * Returns a maximum cost in a list of costs. If an array of costs is not passed in, the array of costs is fetched via query.
+	 *
+	 * @param $costs mixed Cost(s) to review for max value
+	 *
+	 * @return float
+	 */
+	public function get_maximum_cost( $costs = null ) {
+		return $this->get_cost_by_func( $costs, 'max' );
 	}//end get_maximum_cost
 
 	/**
@@ -78,28 +98,7 @@ class Tribe__Events__Cost_Utils {
 	 * @return float
 	 */
 	public function get_minimum_cost( $costs = null ) {
-		if ( null === $costs ) {
-			$costs = $this->get_all_costs();
-		} elseif ( ! is_array( $costs ) ) {
-			$costs = array( $costs );
-		}
-
-		foreach ( $costs as $index => $value ) {
-			$values = $this->parse_cost_range( $value );
-			$costs = array_merge( $costs, $values );
-		}
-
-		if ( empty( $costs ) ) {
-			return 0;
-		}
-
-		$min = min( $costs );
-
-		if ( ! is_numeric( $min ) ) {
-			return 0;
-		}
-
-		return $min;
+		return $this->get_cost_by_func( $costs, 'min' );
 	}//end get_minimum_cost
 
 	/**
