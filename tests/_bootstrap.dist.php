fce6be227942ec5610cf3b873be836b529641cc3
Luca Tumedei <luca@theaveragedev.com>
2015-06-05 08:01:20 +0200

    implemented word around to load active plugins
    when running tests and not backing up globals all tests but the first one will fail; this fixes it

--- tests/_bootstrap.dist.php
+++ tests/_bootstrap.dist.php
@@ -25,6 +25,28 @@ function tribe_events_codeception_bootstrap( $config ) {
 	}
 }
 
+/**
+ * Requires the active plugins main files set in the Codeception configuration file.@deprecated
+ *
+ * Use in set up method of a test case to get around the globals not being backed up for testing purposes.
+ *
+ * @throws \Codeception\Exception\Configuration
+ */
+function tribe_load_active_plugins() {
+	$config = \Codeception\Configuration::config();
+	if ( isset( $config['php']['globals']['wp_tests_options']['active_plugins'] ) && is_array( $config['php']['globals']['wp_tests_options']['active_plugins'] ) ) {
+
+		$wp_plugins_dir = dirname( __FILE__ );
+		while ( basename( $wp_plugins_dir ) != 'plugins' ) {
+			$wp_plugins_dir = dirname( $wp_plugins_dir );
+		}
+
+		foreach ( $config['php']['globals']['wp_tests_options']['active_plugins'] as $plugin ) {
+			require_once $wp_plugins_dir . '/' . $plugin;
+		}
+	}
+}
+
 Codeception\Util\Autoload::registerSuffix( 'Tribe__Events__WP_UnitTestCase', __DIR__ . '/helpers' );
 
 tribe_events_codeception_bootstrap( \Codeception\Configuration::config() );
--- tests/helpers/Tribe__Events__WP_UnitTestCase.php
+++ tests/helpers/Tribe__Events__WP_UnitTestCase.php
@@ -2,16 +2,22 @@
 
 
 class Tribe__Events__WP_UnitTestCase extends WP_UnitTestCase {
+
 	// avoid errors with singletons and closures
-	protected $backupGlobals = FALSE;
+	protected $backupGlobals = false;
 
 	/**
 	 * Gets the path to the _data folder without trailing slash.
 	 *
 	 * @return string
 	 */
-	public function get_data_folder_path(){
-		return dirname(dirname(__FILE__)) . DIRECTORY_SEPARATOR . '_data';
+	public function get_data_folder_path() {
+		return dirname( dirname( __FILE__ ) ) . DIRECTORY_SEPARATOR . '_data';
+	}
+
+	public function setUp() {
+		parent::setUp();
+		tribe_load_active_plugins();
 	}
 
 }
