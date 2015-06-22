de9d2dc7b49e0f714d969bf259a9fa5c9816473e
Matthew Batchelder <borkweb@gmail.com>
2015-06-22 09:52:29 -0400

    Changing deprecation notice to 3.10.1

--- src/Tribe/Query.php
+++ src/Tribe/Query.php
@@ -700,7 +700,7 @@ if ( ! class_exists( 'Tribe__Events__Query' ) ) {
 		 * @return array The counts array.
 		 */
 		public static function getEventCounts( $args = array() ) {
-			_deprecated_function( __METHOD__, '3.11' );
+			_deprecated_function( __METHOD__, '3.10.1' );
 			global $wpdb;
 			$date     = date( 'Y-m-d' );
 			$defaults = array(
