0e5eca1a9ad7141aafd4d42cb4c2ad31188f7cfe
barryhughes <barry@codingkills.me>
2015-06-26 10:27:05 -0700

    Break out license tab logic and register even in mu environment (#36063)

--- src/Tribe/Main.php
+++ src/Tribe/Main.php
@@ -722,81 +722,57 @@ if ( ! class_exists( 'Tribe__Events__Main' ) ) {
 		 * @return void
 		 */
 		public function doSettingTabs() {
-
 			include_once( $this->pluginPath . 'src/admin-views/tribe-options-general.php' );
 			include_once( $this->pluginPath . 'src/admin-views/tribe-options-display.php' );
 
 			$showNetworkTabs = $this->getNetworkOption( 'showSettingsTabs', false );
 
-			$link = add_query_arg(
-				array(
-					'utm_campaign' => 'in-app',
-					'utm_medium'   => 'plugin-tec',
-					'utm_source'   => 'notice'
-				), self::$tecUrl . 'license-keys/'
-			);
-
-			$link = esc_url( $link );
-
-			$tribe_licences_tab_fields = array(
-				'info-start'               => array(
-					'type' => 'html',
-					'html' => '<div id="modern-tribe-info">'
-				),
-				'info-box-title'           => array(
-					'type' => 'html',
-					'html' => '<h2>' . __( 'Licenses', 'tribe-events-calendar' ) . '</h2>',
-				),
-				'info-box-description'     => array(
-					'type' => 'html',
-					'html' => sprintf(
-						__( '<p>The license key you received when completing your purchase from %s will grant you access to support and updates until it expires. You do not need to enter the key below for the plugins to work, but you will need to enter it to get automatic updates. <strong>Find your license keys at <a href="%s" target="_blank">%s</a></strong>.</p> <p>Each paid add-on has its own unique license key. Simply paste the key into its appropriate field on below, and give it a moment to validate. You know you\'re set when a green expiration date appears alongside a "valid" message.</p> <p>If you\'re seeing a red message telling you that your key isn\'t valid or is out of installs, visit <a href="%s" target="_blank">%s</a> to manage your installs or renew / upgrade your license.</p><p>Not seeing an update but expecting one? In WordPress, go to <a href="%s">Dashboard > Updates</a> and click "Check Again".</p>', 'tribe-events-calendar' ),
-						self::$tecUrl,
-						$link,
-						self::$tecUrl . 'license-keys/',
-						$link,
-						self::$tecUrl . 'license-keys/',
-						admin_url( '/update-core.php' )
-					),
-				),
-				'info-end'                 => array(
-					'type' => 'html',
-					'html' => '</div>'
-				),
-				'tribe-form-content-start' => array(
-					'type' => 'html',
-					'html' => '<div class="tribe-settings-form-wrap">'
-				),
-				// TODO: Figure out how properly close this wrapper after the license content
-				'tribe-form-content-end'   => array(
-					'type' => 'html',
-					'html' => '</div>'
-				)
-			);
 			new Tribe__Events__Settings_Tab( 'general', __( 'General', 'tribe-events-calendar' ), $generalTab );
 			new Tribe__Events__Settings_Tab( 'display', __( 'Display', 'tribe-events-calendar' ), $displayTab );
-			// If none of the addons are activated, do not show the licenses tab.
 
-			$addons = apply_filters( 'tribe_licensable_addons', array() );
-			if ( ! empty( $addons ) ) {
-				$license_fields = apply_filters( 'tribe_license_fields', $tribe_licences_tab_fields );
-				if ( is_multisite() ) {
-					new Tribe__Events__Settings_Tab( 'licenses', __( 'Licenses', 'tribe-events-calendar' ), array(
-						'priority'      => '40',
-						'network_admin' => true,
-						'fields'        => $license_fields
-					) );
-				} else {
-					new Tribe__Events__Settings_Tab( 'licenses', __( 'Licenses', 'tribe-events-calendar' ), array(
-						'priority' => '40',
-						'fields'   => $license_fields
-					) );
-				}
-			}
+			$this->do_licenses_tab();
+
 			new Tribe__Events__Settings_Tab( 'help', __( 'Help', 'tribe-events-calendar' ), array(
 				'priority'  => 60,
 				'show_save' => false
 			) );
+		}
+
+		/**
+		 * Registers the license key management tab in the Events > Settings screen,
+		 * only if premium addons are detected.
+		 */
+		protected function do_licenses_tab() {
+			if ( ! $this->have_addons() ) {
+				return;
+			}
+
+			/**
+			 * @var $licenses_tab
+			 */
+			include $this->pluginPath . 'src/admin-views/tribe-options-licenses.php';
+
+			/**
+			 * Allows the fields displayed in the licenses tab to be modified.
+			 *
+			 * @var array
+			 */
+			$license_fields = apply_filters( 'tribe_license_fields', $licenses_tab );
+
+			new Tribe__Events__Settings_Tab( 'licenses', __( 'Licenses', 'tribe-events-calendar' ), array(
+				'priority'      => '40',
+				'fields'        => $license_fields
+			) );
+		}
+
+		/**
+		 * Tries to discover if licensable addons are activated on the same site.
+		 *
+		 * @return bool
+		 */
+		protected function have_addons() {
+			$addons = apply_filters( 'tribe_licensable_addons', array() );
+			return ! empty( $addons );
 		}
 
 		/**
