5797247d2a6e0392e33db7974bcfa7ce2dcaedea
barryhughes <barry@codingkills.me>
2015-06-26 10:39:13 -0700

    Add messaging about key usage in mu networks (#36063)

--- src/admin-views/tribe-options-licenses.php
+++ src/admin-views/tribe-options-licenses.php
@@ -10,6 +10,17 @@ $link = add_query_arg(
 $link = esc_url( $link );
 $tec = Tribe__Events__Main::instance();
 
+$html = __( '<p>The license key you received when completing your purchase from %s will grant you access to support and updates until it expires. You do not need to enter the key below for the plugins to work, but you will need to enter it to get automatic updates. <strong>Find your license keys at <a href="%s" target="_blank">%s</a></strong>.</p> <p>Each paid add-on has its own unique license key. Simply paste the key into its appropriate field on below, and give it a moment to validate. You know you\'re set when a green expiration date appears alongside a "valid" message.</p> <p>If you\'re seeing a red message telling you that your key isn\'t valid or is out of installs, visit <a href="%s" target="_blank">%s</a> to manage your installs or renew / upgrade your license.</p><p>Not seeing an update but expecting one? In WordPress, go to <a href="%s">Dashboard > Updates</a> and click "Check Again".</p>', 'tribe-events-calendar' );
+
+if ( is_multisite() ) {
+	$html .= '<p>'
+		. __( '
+			<strong> Using our plugins in a multisite network? </strong>
+			Please note that your license key will be applied to the entire network, not just this site.
+		  ', 'tribe-events-calendar' )
+		. '</p>';
+}
+
 $licenses_tab = array(
 	'info-start' => array(
 		'type' => 'html',
@@ -22,7 +33,7 @@ $licenses_tab = array(
 	'info-box-description' => array(
 		'type' => 'html',
 		'html' => sprintf(
-			__( '<p>The license key you received when completing your purchase from %s will grant you access to support and updates until it expires. You do not need to enter the key below for the plugins to work, but you will need to enter it to get automatic updates. <strong>Find your license keys at <a href="%s" target="_blank">%s</a></strong>.</p> <p>Each paid add-on has its own unique license key. Simply paste the key into its appropriate field on below, and give it a moment to validate. You know you\'re set when a green expiration date appears alongside a "valid" message.</p> <p>If you\'re seeing a red message telling you that your key isn\'t valid or is out of installs, visit <a href="%s" target="_blank">%s</a> to manage your installs or renew / upgrade your license.</p><p>Not seeing an update but expecting one? In WordPress, go to <a href="%s">Dashboard > Updates</a> and click "Check Again".</p>', 'tribe-events-calendar' ),
+			$html,
 			self::$tecUrl,
 			$link,
 			self::$tecUrl . 'license-keys/',
