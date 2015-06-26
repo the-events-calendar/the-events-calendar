6f8149bff3068da948b177f16b2285b722291966
barryhughes <barry@codingkills.me>
2015-06-26 10:41:24 -0700

    Formatting etc (#36063)

--- src/admin-views/tribe-options-licenses.php
+++ src/admin-views/tribe-options-licenses.php
@@ -10,13 +10,15 @@ $link = add_query_arg(
 $link = esc_url( $link );
 $tec = Tribe__Events__Main::instance();
 
+// Explanatory text about license settings for the tab information box
 $html = __( '<p>The license key you received when completing your purchase from %s will grant you access to support and updates until it expires. You do not need to enter the key below for the plugins to work, but you will need to enter it to get automatic updates. <strong>Find your license keys at <a href="%s" target="_blank">%s</a></strong>.</p> <p>Each paid add-on has its own unique license key. Simply paste the key into its appropriate field on below, and give it a moment to validate. You know you\'re set when a green expiration date appears alongside a "valid" message.</p> <p>If you\'re seeing a red message telling you that your key isn\'t valid or is out of installs, visit <a href="%s" target="_blank">%s</a> to manage your installs or renew / upgrade your license.</p><p>Not seeing an update but expecting one? In WordPress, go to <a href="%s">Dashboard > Updates</a> and click "Check Again".</p>', 'tribe-events-calendar' );
 
+// Expand with extra information for mu network users
 if ( is_multisite() ) {
 	$html .= '<p>'
 		. __( '
-			<strong> Using our plugins in a multisite network? </strong>
-			Please note that your license key will be applied to the entire network, not just this site.
+		       <strong> Using our plugins in a multisite network? </strong>
+		       Please note that your license key will be applied to the entire network, not just this site.
 		  ', 'tribe-events-calendar' )
 		. '</p>';
 }
