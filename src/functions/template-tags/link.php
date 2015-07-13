05b6a35f6c30266eef5ea718a8e444c6f1535a17
Matthew Batchelder <borkweb@gmail.com>
2015-07-13 14:43:23 -0400

    Addressing code review

--- src/functions/template-tags/link.php
+++ src/functions/template-tags/link.php
@@ -184,13 +184,27 @@ if ( class_exists( 'Tribe__Events__Main' ) ) {
 
 		// if a page isn't passed in, attempt to fetch it from a get var
 		if ( ! $page ) {
-			$page = empty( $_REQUEST['tribe_paged'] ) ? 1 : absint( $_REQUEST['tribe_paged'] );
+			// set a default
+			$page = 1;
+
+			// if one was passed in via GET, use it
+			if ( ! empty( $_GET['tribe_paged'] ) ) {
+				$page = absint( $_GET['tribe_paged'] );
+			}
+
+			// unless of course, one was passed in via POST, let's use that instead
+			if ( ! empty( $_POST['tribe_paged'] ) ) {
+				$page = absint( $_POST['tribe_paged'] );
+			}
 		}
 
 		// if what we are currently displaying is not passed in, let's set a default and check $_GET
 		if ( ! $currently_displaying ) {
 			$currently_displaying = 'list';
-			if ( ! empty( $_REQUEST['tribe_event_display'] ) && 'past' === $_REQUEST['tribe_event_display'] ) {
+			if (
+				( ! empty( $_GET['tribe_event_display'] ) && 'past' === $_GET['tribe_event_display'] )
+				|| ( ! empty( $_POST['tribe_event_display'] ) && 'past' === $_POST['tribe_event_display'] )
+			) {
 				$currently_displaying = 'past';
 			}
 		}
