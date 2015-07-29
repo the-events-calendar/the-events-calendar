bd9211af272cab5c0a371700aa5d432fa44bdc67
Matthew Batchelder <borkweb@gmail.com>
2015-07-29 14:18:29 -0400

    Make sure we export ALL events when exporting from month view
    And by "All", I mean all events that aren't hidden. Without doing `posts_per_page = -1`, the query was only retrieving events up to the "Number of events to show per page" setting.
    
    See: https://central.tri.be/issues/38182

--- src/Tribe/iCal.php
+++ src/Tribe/iCal.php
@@ -165,8 +165,10 @@ class Tribe__Events__iCal {
 
 		$args = array(
 			'eventDisplay' => 'custom',
-			'start_date'   => Tribe__Events__Template__Month::calculate_first_cell_date( $month ),
-			'end_date'     => Tribe__Events__Template__Month::calculate_final_cell_date( $month ),
+			'start_date' => Tribe__Events__Template__Month::calculate_first_cell_date( $month ),
+			'end_date' => Tribe__Events__Template__Month::calculate_final_cell_date( $month ),
+			'posts_per_page' => -1,
+			'hide_upcoming' => true,
 		);
 
 		/**
