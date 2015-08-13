fca01cbb7c804f095cbc7419dd511a3b21d50850
Matthew Batchelder <borkweb@gmail.com>
2015-08-13 15:46:45 -0400

    Upping to 3.12a2

--- src/Tribe/Main.php
+++ src/Tribe/Main.php
@@ -41,8 +41,8 @@
 			public $widget_wrappers;
 
 
-			const REQUIRED_TEC_VERSION = '3.11.1';
-			const VERSION = '3.11.1';
+			const REQUIRED_TEC_VERSION = '3.11';
+			const VERSION = '3.12a2';
 
 			private function __construct() {
 				$this->pluginDir = trailingslashit( basename( EVENTS_CALENDAR_PRO_DIR ) );
