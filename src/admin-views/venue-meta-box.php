2878f05dd14d0df2b484feb261cf88c88c11ed6f
Tapan Kumer Das <tapan@innovativebd.net>
2015-09-10 00:00:45 +0600

    Msg for Empty field on event post
    Signed-off-by: Tapan Kumer Das <tapan@innovativebd.net>

--- src/admin-views/venue-meta-box.php
+++ src/admin-views/venue-meta-box.php
@@ -186,12 +186,18 @@ if ( $post->post_type != Tribe__Events__Main::VENUE_POST_TYPE ) {
 				name  : jQuery('[name=venue\\[Venue\\]]').get(0).value
 			},
 			function (result) {
-				if (result == 1) {
+				if (jQuery('[name=venue\\[Venue\\]]').get(0).value == "") {
 					jQuery('.tribe-venue-error').remove();
+					jQuery( '[name=venue\\[Venue\\]]' ).after('<div class="tribe-venue-error error form-invalid"><?php printf( __( '%s Name Can Not be Empty', 'tribe-events-calendar' ), tribe_get_venue_label_singular() ); ?></div>');
 				} else {
-					jQuery('.tribe-venue-error').remove();
-					jQuery( '[name=venue\\[Venue\\]]' ).after('<div class="tribe-venue-error error form-invalid"><?php printf( __( '%s Name Already Exists', 'tribe-events-calendar' ), tribe_get_venue_label_singular() ); ?></div>');
+					if (result == 1) {
+						jQuery('.tribe-venue-error').remove();
+					} else {
+						jQuery('.tribe-venue-error').remove();
+						jQuery( '[name=venue\\[Venue\\]]' ).after('<div class="tribe-venue-error error form-invalid"><?php printf( __( '%s Name Already Exists', 'tribe-events-calendar' ), tribe_get_venue_label_singular() ); ?></div>');
+					}
 				}
+
 			}
 		);
 	});
