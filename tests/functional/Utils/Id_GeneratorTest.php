eb339074132d43e2890058c2b9d9edd0b5b47313
Luca Tumedei <luca@theaveragedev.com>
2015-09-23 10:33:16 +0200

    first test and fail as method is missing

--- tests/functional/Utils/Id_GeneratorTest.php
+++ tests/functional/Utils/Id_GeneratorTest.php
@@ -10,4 +10,16 @@ class Id_GeneratorTest extends \Tribe__Events__WP_UnitTestCase {
 	public function it_should_be_instantiatable() {
 		$this->assertInstanceOf( 'Tribe__Events__Utils__Id_Generator', new \Tribe__Events__Utils__Id_Generator() );
 	}
+
+	/**
+	 * @test
+	 * it should generate unique ids
+	 */
+	public function it_should_generate_unique_ids() {
+		$count = 5;
+		for ( $i = 0; $i < $count; $i ++ ) {
+			$generated_ids[] = \Tribe__Events__Utils__Id_Generator::generate_id( 'tec' );
+		}
+		$this->assertCount( $count, array_unique( $generated_ids ) );
+	}
 }
