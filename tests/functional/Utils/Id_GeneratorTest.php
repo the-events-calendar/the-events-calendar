1d8e9930ec84e9f59740cd3c2fb82cde61b0b170
Luca Tumedei <luca@theaveragedev.com>
2015-09-23 10:39:18 +0200

    second failing test

--- tests/functional/Utils/Id_GeneratorTest.php
+++ tests/functional/Utils/Id_GeneratorTest.php
@@ -22,4 +22,13 @@ class Id_GeneratorTest extends \Tribe__Events__WP_UnitTestCase {
 		}
 		$this->assertCount( $count, array_unique( $generated_ids ) );
 	}
+
+	/**
+	 * @test
+	 * it should allow for the definition of an id group
+	 */
+	public function it_should_allow_for_the_definition_of_an_id_group() {
+		$this->assertEquals( 'foo-0', \Tribe__Events__Utils__Id_Generator::generate_id( 'foo', 'group-1' ) );
+		$this->assertEquals( 'baz-0', \Tribe__Events__Utils__Id_Generator::generate_id( 'baz', 'group-2' ) );
+	}
 }
