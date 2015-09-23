f97005896a5195c80fdcb9f611a9e912e0bd9bc9
Luca Tumedei <luca@theaveragedev.com>
2015-09-23 10:36:21 +0200

    first test pass

--- src/Tribe/Utils/Id_Generator.php
+++ src/Tribe/Utils/Id_Generator.php
@@ -3,4 +3,12 @@
 
 class Tribe__Events__Utils__Id_Generator {
 
+	protected static $count;
+
+	public static function generate_id( $string ) {
+		$out = $string . '-' . self::$count;
+		self::$count ++;
+
+		return $out;
+	}
 }
