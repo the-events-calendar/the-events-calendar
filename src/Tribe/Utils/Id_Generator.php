8f242a2e6290fab817b684828e57a6dbe57c496f
Luca Tumedei <luca@theaveragedev.com>
2015-09-23 10:48:50 +0200

    passed expected exception test

--- src/Tribe/Utils/Id_Generator.php
+++ src/Tribe/Utils/Id_Generator.php
@@ -1,11 +1,15 @@
 <?php
 
-
 class Tribe__Events__Utils__Id_Generator {
 
 	protected static $count = array();
 
 	public static function generate_id( $string, $group = 'default' ) {
+
+		if (!is_string($string)) {
+			throw new \InvalidArgumentException('First argument must be a string');
+		}
+
 		if ( ! isset( self::$count[ $group ] ) ) {
 			self::$count[ $group ] = 0;
 		}
