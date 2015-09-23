ec12f736dd4d56b581a3f2b971877487ed983d5a
Luca Tumedei <luca@theaveragedev.com>
2015-09-23 10:42:32 +0200

    passed second test

--- src/Tribe/Utils/Id_Generator.php
+++ src/Tribe/Utils/Id_Generator.php
@@ -3,11 +3,15 @@
 
 class Tribe__Events__Utils__Id_Generator {
 
-	protected static $count;
+	protected static $count = array();
 
-	public static function generate_id( $string ) {
-		$out = $string . '-' . self::$count;
-		self::$count ++;
+	public static function generate_id( $string, $group = 'default' ) {
+		if ( ! isset( self::$count[ $group ] ) ) {
+			self::$count[ $group ] = 0;
+		}
+
+		$out = $string . '-' . self::$count[ $group ];
+		self::$count[ $group ] ++;
 
 		return $out;
 	}
