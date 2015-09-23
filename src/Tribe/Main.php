5c0712beb937063030ad70a6150e77a9abb3fdfe
barryhughes <barry@codingkills.me>
2015-09-23 15:18:49 -0700

    Version #s -> 3.12.1 (#39633)

--- src/Tribe/Main.php
+++ src/Tribe/Main.php
@@ -42,7 +42,7 @@
 
 
 			const REQUIRED_TEC_VERSION = '3.12';
-			const VERSION = '3.12';
+			const VERSION = '3.12.1';
 
 			private function __construct() {
 				$this->pluginDir = trailingslashit( basename( EVENTS_CALENDAR_PRO_DIR ) );
