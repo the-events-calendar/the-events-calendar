580b772003b5f73c555dfeb4d424782e29f4af50
Matthew Batchelder <borkweb@gmail.com>
2015-09-29 13:32:01 -0400

    Twiddle the JS, too

--- src/resources/js/events-recurrence.js
+++ src/resources/js/events-recurrence.js
@@ -549,6 +549,7 @@ tribe_events_pro_admin.recurrence = {
 				key += '-unfiltered';
 			}
 		} else {
+			key = 'simple-' + key;
 			key += '-' + end_type;
 		}
 
@@ -607,6 +608,12 @@ tribe_events_pro_admin.recurrence = {
 		}
 
 		switch ( key ) {
+			case 'simple-every-day-on':
+			case 'simple-every-week-on':
+			case 'simple-every-month-on':
+			case 'simple-every-year-on':
+				text = text.replace( '%1$s', end );
+				break;
 			case 'every-day-on':
 			case 'every-week-on':
 			case 'every-month-on':
