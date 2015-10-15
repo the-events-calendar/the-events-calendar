308daa6f74732e2f2a7c954f06cca3999f2e3936
barryhughes <barry@codingkills.me>
2015-10-15 15:17:35 -0700

    Mimic PR174 and extend the number of exclusions from the final build (#40441)

--- dev/grunt_options/copy.js
+++ dev/grunt_options/copy.js
@@ -4,7 +4,7 @@
  * Documentation: https://npmjs.org/package/grunt-contrib-copy
  * Example:
  *
- 	main: {
+ main: {
 		files: [
 		  // includes files within path
 		  {expand: true, src: ['path/*'], dest: 'dest/', filter: 'isFile'},
@@ -26,20 +26,39 @@ module.exports = {
 
 	dist: {
 		files: [{
-				expand: true,
-				src: ['**',
-				      '!**/dev/**',
-				      '!**/tests/**',
-				      '!**/.git/**',
-				      '!**/vendor/isotope/index.html',
-				      '!**/vendor/isotope/_includes/**',
-				      '!**/vendor/isotope/_layouts/**',
-				      '!**/vendor/isotope/_posts/**',
-				      '!**/vendor/isotope/js/**',
-				      '!**/vendor/jquery-slimscroll/examples/**'
-				],
-				dest: '<%= pkg._zipfoldername %>/'
-			}]
+			expand: true,
+			src: ['**',
+				'!**/dev/**',
+				'!**/tests/**',
+				'!**/.git/**',
+				'!**/phpunit.xml',
+				'!**/codeception.yml',
+				'!**/composer.json',
+				'!**/composer.lock',
+				'!**/vendor/bacon/**',
+				'!**/vendor/badcow/**',
+				'!**/vendor/bin/**',
+				'!**/vendor/codeception/**',
+				'!**/vendor/composer/**',
+				'!**/vendor/doctrine/**',
+				'!**/vendor/facebook/**',
+				'!**/vendor/guzzlehttp/**',
+				'!**/vendor/isotope/index.html',
+				'!**/vendor/isotope/_includes/**',
+				'!**/vendor/isotope/_layouts/**',
+				'!**/vendor/isotope/_posts/**',
+				'!**/vendor/isotope/js/**',
+				'!**/vendor/lucatume/**',
+				'!**/vendor/phpdocumentor/**',
+				'!**/vendor/phpspec/**',
+				'!**/vendor/phpunit/**',
+				'!**/vendor/psr/**',
+				'!**/vendor/sebastian/**',
+				'!**/vendor/symfony/**',
+				'!**/vendor/autoload.php'
+			],
+			dest: '<%= pkg._zipfoldername %>/'
+		}]
 	}
 
 };
