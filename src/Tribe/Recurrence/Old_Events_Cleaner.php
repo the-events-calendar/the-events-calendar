03d8946f7d8c7f16262c293c0deaf1500457d3da
Luca Tumedei <luca@theaveragedev.com>
2015-10-21 18:33:17 +0200

    added the Old Events Cleaner class

---
+++ src/Tribe/Recurrence/Old_Events_Cleaner.php
@@ -0,0 +1,49 @@
+<?php
+
+
+class Tribe__Events__Pro__Recurrence__Old_Events_Cleaner {
+
+	/**
+	 * @var Tribe__Events__Pro__Recurrence__Old_Events_Cleaner
+	 */
+	protected static $instance;
+	/**
+	 * @var
+	 */
+	private $scheduler;
+
+	/**
+	 * @return Tribe__Events__Pro__Recurrence__Old_Events_Cleaner
+	 */
+	public static function instance() {
+		if ( empty( self::$instance ) ) {
+			self::$instance = new self();
+		}
+
+		return self::$instance;
+	}
+
+	public function __construct( Tribe__Events__Pro__Recurrence_Scheduler $scheduler = null ) {
+		$this->scheduler = $scheduler ? $scheduler : new Tribe__Events__Pro__Recurrence_Scheduler();
+	}
+
+	public function clean_up_old_recurring_events( array $old_value, array $new_value ) {
+		$old_value = empty( $old_value['recurrenceMaxMonthsBefore'] ) ? 24 : $old_value['recurrenceMaxMonthsBefore'];
+		$new_value = empty( $new_value['recurrenceMaxMonthsBefore'] ) ? 24 : $new_value['recurrenceMaxMonthsBefore'];
+
+		if ( $new_value == $old_value ) {
+			return;
+		}
+		if ( $new_value > $old_value ) {
+			return;
+		}
+
+		$this->scheduler->set_before_range( $new_value );
+
+		$this->scheduler->clean_up_old_recurring_events();
+	}
+
+	public function get_scheduler() {
+		return $this->scheduler;
+	}
+}
