cdc485bceebaa4ef05ad7678fb0d8ebaede125d4
Luca Tumedei <luca@theaveragedev.com>
2015-10-23 17:28:50 +0200

    modified Validator to allow for filtering
    thanks @bordoni

--- src/Tribe/Recurrence/Validator.php
+++ src/Tribe/Recurrence/Validator.php
@@ -1,6 +1,4 @@
 <?php
-
-
 class Tribe__Events__Pro__Recurrence__Validator {
 
 	/**
@@ -13,29 +11,33 @@ class Tribe__Events__Pro__Recurrence__Validator {
 	 * @return bool
 	 */
 	public static function is_valid( $event_id, array $recurrence_meta ) {
-		$valid    = true;
-		$errorMsg = '';
+		$response = (object) array(
+			'valid' => true,
+			'message' => '',
+		);
 
 		if ( isset( $recurrence_meta['type'] ) && 'Custom' === $recurrence_meta['type'] ) {
 			if ( ! isset( $recurrence_meta['custom']['type'] ) ) {
-				$valid    = false;
-				$errorMsg = __( 'Custom recurrences must have a type selected.', 'tribe-events-calendar-pro' );
+				$response->valid   = false;
+				$response->message = __( 'Custom recurrences must have a type selected.', 'tribe-events-calendar-pro' );
 			} elseif ( ! isset( $recurrence_meta['custom']['start-time'] ) && ! isset( $recurrence_meta['custom']['day'] ) && ! isset( $recurrence_meta['custom']['week'] ) && ! isset( $recurrence_meta['custom']['month'] ) && ! isset( $recurrence_meta['custom']['year'] ) ) {
-				$valid    = false;
-				$errorMsg = __( 'Custom recurrences must have all data present.', 'tribe-events-calendar-pro' );
+				$response->valid   = false;
+				$response->message = __( 'Custom recurrences must have all data present.', 'tribe-events-calendar-pro' );
 			} elseif ( 'Monthly' === $recurrence_meta['custom']['type'] && ( empty( $recurrence_meta['custom']['month']['day'] ) || empty( $recurrence_meta['custom']['month']['number'] ) || '-' === $recurrence_meta['custom']['month']['day'] || '' === $recurrence_meta['custom']['month']['number'] ) ) {
-				$valid    = false;
-				$errorMsg = __( 'Monthly custom recurrences cannot have a dash set as the day to occur on.', 'tribe-events-calendar-pro' );
+				$response->valid   = false;
+				$response->message = __( 'Monthly custom recurrences cannot have a dash set as the day to occur on.', 'tribe-events-calendar-pro' );
 			} elseif ( 'Yearly' === $recurrence_meta['custom']['type'] && ( empty( $recurrence_meta['custom']['year']['month-day'] ) || '-' === $recurrence_meta['custom']['year']['month-day'] ) ) {
-				$valid    = false;
-				$errorMsg = __( 'Yearly custom recurrences cannot have a dash set as the day to occur on.', 'tribe-events-calendar-pro' );
+				$response->valid   = false;
+				$response->message = __( 'Yearly custom recurrences cannot have a dash set as the day to occur on.', 'tribe-events-calendar-pro' );
 			}
 		}
 
-		if ( ! $valid ) {
-			do_action( 'tribe_recurring_event_error', $event_id, $errorMsg );
+		$response = apply_filters( 'tribe_recurring_pre_event_error', $event_id, $response, $recurrence_meta );
+
+		if ( ! $response->valid ) {
+			do_action( 'tribe_recurring_event_error', $event_id, $response, $recurrence_meta );
 		}
 
-		return $valid;
+		return $response->valid;
 	}
 }
