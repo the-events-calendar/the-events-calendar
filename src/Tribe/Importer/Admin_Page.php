19b91f41993418ad566a8256230de329600249f6
bordoni <bordoni.dev@gmail.com>
2015-10-25 19:37:44 -0200

    Now include a get_url on the Admin Importer Page class, allowing better control over these links

--- src/Tribe/Importer/Admin_Page.php
+++ src/Tribe/Importer/Admin_Page.php
@@ -9,13 +9,48 @@ class Tribe__Events__Importer__Admin_Page {
 	private $messages = array();
 	private $errors = array();
 
+
+	/**
+	 * Admin page for the importer URL, relative to `admin_url()`
+	 * @var null|string
+	 */
+	public $admin_page_url = null;
+
+	/**
+	 * The actual Page Slug used
+	 * @var null|string
+	 */
+	public $admin_page_slug = null;
+
+	private function __construct() {
+		$this->admin_page_url = 'edit.php?post_type=' . Tribe__Events__Main::POSTTYPE;
+		$this->admin_page_slug = 'events-importer';
+	}
+
+	/**
+	 * Returns the main admin settings URL.
+	 *
+	 * @return string
+	 */
+	public function get_url( array $args = array() ) {
+		$defaults = array(
+			'page' => $this->admin_page_slug,
+		);
+
+		// Allow the link to be "changed" on the fly
+		$args = wp_parse_args( $args, $defaults );
+		$url = admin_url( $this->admin_page_url );
+
+		return esc_url( apply_filters( 'tribe_importer_url', add_query_arg( $args, $url ), $args, $url ) );
+	}
+
 	public function register_admin_page() {
 		add_submenu_page(
-			'edit.php?post_type='.Tribe__Events__Main::POSTTYPE,
+			$this->admin_page_url,
 			esc_html__( 'Import', 'the-events-calendar' ),
 			esc_html__( 'Import', 'the-events-calendar' ),
 			'import',
-			'events-importer',
+			$this->admin_page_slug,
 			array( $this, 'render_admin_page_contents' )
 		);
 	}
