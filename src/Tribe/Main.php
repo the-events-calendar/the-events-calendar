c843b4e7862e3bbefca010384f83a00ce34d0283
Matthew Batchelder <borkweb@gmail.com>
2015-10-27 10:34:53 -0400

    Upping version to 4.0

--- src/Tribe/Main.php
+++ src/Tribe/Main.php
@@ -32,8 +32,8 @@ if ( ! class_exists( 'Tribe__Events__Main' ) ) {
 		const VENUE_POST_TYPE     = 'tribe_venue';
 		const ORGANIZER_POST_TYPE = 'tribe_organizer';
 
-		const VERSION           = '3.12.4';
-		const MIN_ADDON_VERSION = '3.12';
+		const VERSION           = '4.0';
+		const MIN_ADDON_VERSION = '4.0';
 		const INFO_API_URL      = 'http://wpapi.org/api/plugin/the-events-calendar.php';
 		const WP_PLUGIN_URL     = 'http://wordpress.org/extend/plugins/the-events-calendar/';
 
