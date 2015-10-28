2e10b103acd128bf78c26513a7dbd4ad1f62cd4a
Luca Tumedei <luca@theaveragedev.com>
2015-10-28 15:26:16 +0100

    refactored Validator class and added first test

--- tests/functional/Tribe/Events/Pro/Recurrence/ValidatorTest.php
+++ tests/functional/Tribe/Events/Pro/Recurrence/ValidatorTest.php
@@ -1,5 +1,8 @@
 <?php
 namespace Tribe\Events\Pro\Recurrence;
+
+use Tribe__Events__Pro__Recurrence__Validator;
+
 class ValidatorTest extends \WP_UnitTestCase {
 
 	protected $backupGlobals = false;
@@ -18,8 +21,16 @@ class ValidatorTest extends \WP_UnitTestCase {
 		parent::tearDown();
 	}
 
-	// tests
-	public function testMe() {
-	}
+	/**
+	 * @test
+	 * it should return false if post is not event
+	 */
+	public function it_should_return_false_if_post_is_not_event() {
+		$post_id = $this->factory->post->create();
 
+		$sut   = new Tribe__Events__Pro__Recurrence__Validator;
+		$valid = $sut->is_valid( $post_id, array() );
+
+		$this->assertFalse( $valid );
+	}
 }
