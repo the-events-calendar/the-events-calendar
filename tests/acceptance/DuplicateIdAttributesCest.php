2e5111a86b0912553f5c000d1ec6b42728d9b3c5
Luca Tumedei <luca@theaveragedev.com>
2015-11-19 11:37:26 +0100

    scaffold test

---
+++ tests/acceptance/DuplicateIdAttributesCest.php
@@ -0,0 +1,18 @@
+<?php
+use \AcceptanceTester;
+
+class DuplicateIdAttributesCest
+{
+    public function _before(AcceptanceTester $I)
+    {
+    }
+
+    public function _after(AcceptanceTester $I)
+    {
+    }
+
+    // tests
+    public function tryToTest(AcceptanceTester $I)
+    {
+    }
+}
