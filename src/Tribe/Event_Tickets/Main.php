699e09f41ace659c5644a7bc506950daf6352bd2
Matthew Batchelder <borkweb@gmail.com>
2015-12-09 11:30:09 -0500

    Don't do as many ugly things with the PHPDoc blocks

--- src/Tribe/Event_Tickets/Main.php
+++ src/Tribe/Event_Tickets/Main.php
@@ -18,14 +18,18 @@ class Tribe__Events__Event_Tickets__Main {
 
 	/**
 	 * Contains an instance of the Attendees Report integration class
+	 *
 	 * @since 4.0.1
+	 *
 	 * @var Tribe__Events__Event_Tickets__Attendees_Report
 	 */
 	private $attendees_report;
 
 	/**
 	 * Contains an instance of the Ticket Email integration class
+	 *
 	 * @since 4.0.2
+	 *
 	 * @var Tribe__Events__Event_Tickets__Ticket_Email
 	 */
 	private $ticket_email;
@@ -34,6 +38,7 @@ class Tribe__Events__Event_Tickets__Main {
 	 * Method to return the private instance of the class
 	 *
 	 * @since 4.0.1
+	 *
 	 * @return Tribe__Events__Event_Tickets__Main
 	 */
 	public static function instance() {
@@ -56,6 +61,9 @@ class Tribe__Events__Event_Tickets__Main {
 	 * Attendees Report integration class object accessor method
 	 *
 	 * @since 4.0.1
+	 *
+	 * @param object $object Override Attendees Report object
+	 * @return Tribe__Events__Event_Tickets__Attendees_Report
 	 */
 	public function attendees_report( $object = null ) {
 		if ( $object ) {
@@ -71,6 +79,9 @@ class Tribe__Events__Event_Tickets__Main {
 	 * Ticket email integration class object accessor method
 	 *
 	 * @since 4.0.2
+	 *
+	 * @param object $object Override Ticket Email object
+	 * @return Tribe__Events__Event_Tickets__Ticket_Email
 	 */
 	public function ticket_email( $object = null ) {
 		if ( $object ) {
