6a25d984b6a2a5c3c3c63b290bdf0896a1055569
Luca Tumedei <luca@theaveragedev.com>
2015-12-22 17:26:38 +0100

    use `tribe_get_date_option` to get Month title

--- src/functions/template-tags/loop.php
+++ src/functions/template-tags/loop.php
@@ -151,7 +151,7 @@ if ( class_exists( 'Tribe__Events__Main' ) ) {
 			$title = sprintf(
 				esc_html__( '%1$s for %2$s', 'the-events-calendar' ),
 				$events_label_plural,
-				date_i18n( tribe_get_option( 'monthAndYearFormat', 'F Y' ), strtotime( tribe_get_month_view_date() ) )
+				date_i18n( tribe_get_date_option( 'monthAndYearFormat', 'F Y' ), strtotime( tribe_get_month_view_date() ) )
 			);
 		}
 
