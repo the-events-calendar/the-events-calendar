46ac11e08bb9b6e77c99f7bff51b2943c752a470
barryhughes <barry@codingkills.me>
2016-01-14 15:59:27 -0800

    Prevent fatal if import/continue screen is refreshed | 37186

--- src/Tribe/Importer/Admin_Page.php
+++ src/Tribe/Importer/Admin_Page.php
@@ -232,7 +232,11 @@ class Tribe__Events__Importer__Admin_Page {
 				break;
 
 			case 'continue':
-				$this->continue_import();
+				// This test guards against a fatal error that can occur if the action=continue
+				// screen is refreshed after the import completes
+				if ( Tribe__Events__Importer__File_Uploader::has_valid_csv_file() ) {
+					$this->continue_import();
+				}
 				break;
 
 			default:
