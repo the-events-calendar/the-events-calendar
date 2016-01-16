dbaa560807dd2474310006d2792802acc7c97176
Matthew Batchelder <borkweb@gmail.com>
2016-01-15 19:33:57 -0600

    Escaping JSON+LD output values and venue/organizer titles
    See: https://central.tri.be/issues/43133

--- src/Tribe/Google_Data_Markup/Organizer.php
+++ src/Tribe/Google_Data_Markup/Organizer.php
@@ -18,9 +18,9 @@ class Tribe__Events__Pro__Google_Data_Markup__Organizer extends Tribe__Events__G
 		$organizer_data = parent::build_data();
 
 		$organizer_data[ $id ]->{'@type'}    = 'Person';
-		$organizer_data[ $id ]->telephone = tribe_get_event_meta( $id, '_OrganizerPhone', true );
-		$organizer_data[ $id ]->url       = tribe_get_event_meta( $id, '_OrganizerWebsite', true );
-		$organizer_data[ $id ]->email     = tribe_get_event_meta( $id, '_OrganizerEmail', true );
+		$organizer_data[ $id ]->telephone = esc_js( tribe_get_event_meta( $id, '_OrganizerPhone', true ) );
+		$organizer_data[ $id ]->url       = esc_js( tribe_get_event_meta( $id, '_OrganizerWebsite', true ) );
+		$organizer_data[ $id ]->email     = esc_js( tribe_get_event_meta( $id, '_OrganizerEmail', true ) );
 
 		return $organizer_data;
 	}
