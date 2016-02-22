2a4efd058a27aefbda53dc3f471467ef9447a7ff
Luca Tumedei <luca@theaveragedev.com>
2016-02-22 13:48:58 +0100

    testing a category not a tag

--- tests/acceptance/CreateACategoryCept.php
+++ tests/acceptance/CreateACategoryCept.php
@@ -9,14 +9,14 @@ $I->wantTo("verify that a category can be created");
 // arrange
 $I->activate_tec();
 $term_slug = 'some-event-term';
-$term = get_term_by('slug', $term_slug, 'post_tag');
+$term = get_term_by('slug', $term_slug, 'tribe_events_cat');
 if ($term) {
-    wp_delete_term($term->term_id, 'post_tag');
+    wp_delete_term($term->term_id, 'tribe_events_cat');
 }
 
 // act
 $I->loginAsAdmin();
-$I->amOnAdminPage('/edit-tags.php?taxonomy=post_tag&post_type=tribe_events');
+$I->amOnAdminPage('/edit-tags.php?taxonomy=tribe_events_cat&post_type=tribe_events');
 $I->fillField('tag-name', 'Some event term');
 $I->fillField('slug', $term_slug);
 $I->fillField('description', 'Yet another event term');
@@ -24,7 +24,7 @@ $I->click('#submit');
 
 // assert
 $I->waitForJqueryAjax(10);
-$term = get_term_by('slug', $term_slug, 'post_tag');
+$term = get_term_by('slug', $term_slug, 'tribe_events_cat');
 $I->assertNotEmpty($term);
 $I->assertEquals('Some event term', $term->name);
 $I->assertEquals('Yet another event term', $term->description);
