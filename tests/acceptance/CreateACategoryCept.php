e84329e90187ff13c7dd2a4df7e8df9052b1f44b
Luca Tumedei <luca@theaveragedev.com>
2016-02-22 13:47:31 +0100

    updated tag creation test

--- tests/acceptance/CreateACategoryCept.php
+++ tests/acceptance/CreateACategoryCept.php
@@ -1,16 +1,30 @@
 <?php
 
 // @group: settings
-$I = new AcceptanceTester( $scenario );
+$I = new AcceptanceTester($scenario);
 
-//Activate TEC Calendar
-$I->am( 'administrator' );
-$I->wantTo( "verify that a category can be created" );
+$I->am('administrator');
+$I->wantTo("verify that a category can be created");
 
+// arrange
 $I->activate_tec();
-$I->set_pretty_permalinks();
+$term_slug = 'some-event-term';
+$term = get_term_by('slug', $term_slug, 'post_tag');
+if ($term) {
+    wp_delete_term($term->term_id, 'post_tag');
+}
 
-//Create a tag and test it exists
-$I->createTag( array( 'tagName' => 'New Tag Name', 'tagSlug' => 'New Slug', 'tagDescription' => 'The tag descripton' ) );
+// act
+$I->loginAsAdmin();
+$I->amOnAdminPage('/edit-tags.php?taxonomy=post_tag&post_type=tribe_events');
+$I->fillField('tag-name', 'Some event term');
+$I->fillField('slug', $term_slug);
+$I->fillField('description', 'Yet another event term');
+$I->click('#submit');
 
-//Delete Tag
+// assert
+$I->waitForJqueryAjax(10);
+$term = get_term_by('slug', $term_slug, 'post_tag');
+$I->assertNotEmpty($term);
+$I->assertEquals('Some event term', $term->name);
+$I->assertEquals('Yet another event term', $term->description);
