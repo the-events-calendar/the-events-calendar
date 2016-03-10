ff7ca1c180fa461e9957d57aaf151371c24cd96b
Matthew Batchelder <borkweb@gmail.com>
2016-03-10 11:04:49 -0500

    Set product-taskmaster as a npm dependency

--- gulpfile.js
+++ gulpfile.js
@@ -2,4 +2,6 @@
  * This is a simple gulp file that loads gulp tasks from a tasks directory.
  * Whee.
  */
-require( 'require-dir' )( './dev/tasks', { recurse: true } );
+var gulp = require( 'gulp' );
+
+require( 'product-taskmaster' )( gulp );
