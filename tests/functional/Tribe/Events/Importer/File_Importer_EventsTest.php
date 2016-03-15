926803fd0429c9ed8f0cacc3645ab136e7e4f7b9
Luca Tumedei <luca@theaveragedev.com>
2016-03-15 10:21:52 +0100

    added inner workings to import event excerpt

--- tests/functional/Tribe/Events/Importer/File_Importer_EventsTest.php
+++ tests/functional/Tribe/Events/Importer/File_Importer_EventsTest.php
@@ -8,6 +8,23 @@ use Tribe__Events__Importer__File_Importer_Events as Events_Importer;
 
 class File_Importer_EventsTest extends \Codeception\TestCase\WPTestCase {
 
+	protected $field_map = [
+		'event_name',
+		'event_description',
+		'event_start_date',
+		'event_start_time',
+		'event_end_date',
+		'event_end_time',
+		'event_all_day',
+		'event_venue_name',
+		'event_organizer_name',
+		'event_show_map_link',
+		'event_show_map',
+		'event_cost',
+		'event_category',
+		'event_website',
+	];
+
 	/**
 	 * @var \Tribe__Events__Importer__File_Reader
 	 */
@@ -105,27 +122,105 @@ class File_Importer_EventsTest extends \Codeception\TestCase\WPTestCase {
 		$this->assertFalse( $has_thumbnail );
 	}
 
+	/**
+	 * @test
+	 * it should not mark record as invalid if excerpt entry is missing
+	 */
+	public function it_should_not_mark_record_as_invalid_if_excerpt_entry_is_missing() {
+		$sut = $this->make_instance( 'excerpt' );
+
+		$post_id = $sut->import_next_row();
+
+		$this->assertNotFalse( $post_id );
+	}
+
+	/**
+	 * @test
+	 * it should import the excerpt if defined in file
+	 */
+	public function it_should_import_the_excerpt_if_defined_in_file() {
+		$this->data        = [
+			'excerpt_1' => 'Excerpt 1',
+		];
+		$this->field_map[] = 'event_excerpt';
+
+		$sut     = $this->make_instance( 'excerpt' );
+		$post_id = $sut->import_next_row();
+
+		$this->assertEquals( 'Excerpt 1', get_post( $post_id )->post_excerpt );
+	}
+
+	/**
+	 * @test
+	 * it should import the excerpt if set on file and not set on existing post
+	 */
+	public function it_should_import_the_excerpt_if_set_on_file_and_not_set_on_existing_post() {
+		$this->data        = [
+			'excerpt_1' => '',
+		];
+		$this->field_map[] = 'event_excerpt';
+
+		$sut     = $this->make_instance( 'excerpt' );
+		$post_id = $sut->import_next_row();
+
+		$this->assertEquals( '', get_post( $post_id )->post_excerpt );
+
+		$this->data       = [
+			'excerpt_1' => 'The post excerpt',
+		];
+		$sut              = $this->make_instance( 'excerpt' );
+		$reimport_post_id = $sut->import_next_row();
+
+		$this->assertEquals( $post_id, $reimport_post_id );
+		$this->assertEquals( 'The post excerpt', get_post( $reimport_post_id )->post_excerpt );
+	}
+
+	/**
+	 * @test
+	 * it should not import the excerpt if defined in file but already set on post
+	 */
+	public function it_should_not_import_the_excerpt_if_defined_in_file_but_already_set_on_post() {
+		$this->data        = [
+			'excerpt_1' => 'A',
+		];
+		$this->field_map[] = 'event_excerpt';
+
+		$sut     = $this->make_instance( 'excerpt' );
+		$post_id = $sut->import_next_row();
+
+		$this->assertEquals( 'A', get_post( $post_id )->post_excerpt );
+
+		$this->data       = [
+			'excerpt_1' => 'B',
+		];
+		$sut              = $this->make_instance( 'excerpt' );
+		$reimport_post_id = $sut->import_next_row();
+
+		$this->assertEquals( $post_id, $reimport_post_id );
+		$this->assertEquals( 'A', get_post( $reimport_post_id )->post_excerpt );
+	}
+
+	/**
+	 * @test
+	 * it should not import the excerpt if not defined in file
+	 */
+	public function it_should_not_import_the_excerpt_if_not_defined_in_file() {
+		$this->data        = [
+			'excerpt_1' => '',
+		];
+		$this->field_map[] = 'event_excerpt';
+
+		$sut     = $this->make_instance( 'excerpt' );
+		$post_id = $sut->import_next_row();
+
+		$this->assertEquals( '', get_post( $post_id )->post_excerpt );
+	}
+
 	private function make_instance( $template_dir = null ) {
 		$this->setup_file( $template_dir );
 
 		$sut = new Events_Importer( $this->file_reader, $this->featured_image_uploader->reveal() );
-		$sut->set_map( [
-			'event_name',
-			'event_description',
-			'event_excerpt',
-			'event_start_date',
-			'event_start_time',
-			'event_end_date',
-			'event_end_time',
-			'event_all_day',
-			'event_venue_name',
-			'event_organizer_name',
-			'event_show_map_link',
-			'event_show_map',
-			'event_cost',
-			'event_category',
-			'event_website',
-		] );
+		$sut->set_map( $this->field_map );
 		$sut->set_type( 'events' );
 
 		return $sut;
