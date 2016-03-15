34b30526dd0fd97e6867589b8133a5596493879a
Luca Tumedei <luca@theaveragedev.com>
2016-03-15 14:20:31 +0100

    set up emtpy tests

---
+++ tests/functional/Tribe/Events/Importer/File_Importer_Events_TimezoneTest.php
@@ -0,0 +1,11 @@
+<?php
+namespace Tribe\Events\Importer;
+require_once 'File_Importer_EventsTest.php';
+
+use Handlebars\Handlebars;
+use Handlebars\Loader\FilesystemLoader;
+use org\bovigo\vfs\vfsStream;
+use Tribe__Events__Importer__File_Importer_Events as Events_Importer;
+
+class File_Importer_Events_TimezoneTest extends File_Importer_EventsTest {
+}
