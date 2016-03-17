1eeecf859b96ddb0a46d55d562667dfea566b54a
trishasalas <trisha@trishasalas.com>
2016-03-16 23:05:03 -0500

    trying to fix that paging nav, first attempt with not a ton of detail

--- src/functions/template-tags/link.php
+++ src/functions/template-tags/link.php
@@ -271,6 +271,95 @@ if ( class_exists( 'Tribe__Events__Main' ) ) {
 	}
 
 	/**
+	 * Link to a nearby Condensed List View page
+	 *
+	 * Returns a link to the next/previous condensed list view page
+	 *
+	 * @param string $direction 'next' or 'prev'
+	 * @param int|null $term Term ID
+	 * @param string $currently_displaying Type of listview page that is currently being displayed ('past' or 'list')
+	 * @param int $page Current page number being displayed
+	 *
+	 * @return string URL
+	 */
+	function tribe_get_condensed_listview_dir_link( $direction = 'next', $term = null, $currently_displaying = null, $page = null ) {
+		$link = tribe_get_condensed_listview_link( $term );
+
+		// if a page isn't passed in, attempt to fetch it from a get var
+		if ( ! $page ) {
+			if ( ! empty( $_POST['tribe_paged'] ) ) {
+				$page = absint( $_POST['tribe_paged'] );
+			} elseif ( ! empty( $_GET['tribe_paged'] ) ) {
+				$page = absint( $_GET['tribe_paged'] );
+			} else {
+				$page = 1;
+			}
+		}
+
+		// if what we are currently displaying is not passed in, let's set a default and check $_GET
+		if ( ! $currently_displaying ) {
+			$currently_displaying = 'condensed-list';
+			if (
+				( ! empty( $_GET['tribe_event_display'] ) && 'past' === $_GET['tribe_event_display'] )
+				|| ( ! empty( $_POST['tribe_event_display'] ) && 'past' === $_POST['tribe_event_display'] )
+			) {
+				$currently_displaying = 'past';
+			}
+		}
+
+		// assume we want to display what we're currently displaying (until we discover otherwise)
+		$display = $currently_displaying;
+
+		if (
+			( 'next' === $direction && 'condensed-list' === $currently_displaying )
+			|| ( 'prev' === $direction && 'past' === $currently_displaying )
+		) {
+			$page++;
+		} elseif ( 'condensed-list' === $currently_displaying && 1 === $page ) {
+			$display = 'past';
+		} elseif ( 'past' === $currently_displaying && 1 === $page ) {
+			$display = 'condensed-list';
+		} else {
+			$page--;
+		}
+
+		$link = add_query_arg( array(
+			'tribe_event_display' => $display,
+			'tribe_paged' => $page,
+		), $link );
+
+		return apply_filters( 'tribe_get_condensed_listview_dir_link', $link, $term );
+	}
+
+	/**
+	 * Link to prev Condensed List View
+	 *
+	 * Returns a link to the previous list view page
+	 *
+	 * @param int|null $term Term ID
+	 *
+	 * @return string URL
+	 */
+	function tribe_get_condensed_listview_prev_link( $term = null ) {
+		$link = tribe_get_condensed_listview_dir_link( 'prev', $term );
+		return apply_filters( 'tribe_get_condensed_listview_prev_link', $link, $term );
+	}
+
+	/**
+	 * Link to next Condensed List View
+	 *
+	 * Returns a link to the next list view page
+	 *
+	 * @param int|null $term Term ID
+	 *
+	 * @return string URL
+	 */
+	function tribe_get_condensed_listview_next_link( $term = null ) {
+		$link = tribe_get_condensed_listview_dir_link( 'next', $term );
+		return apply_filters( 'tribe_get_condensed_listview_next_link', $link, $term );
+	}
+
+	/**
 	 * Single Event Link
 	 *
 	 * Get link to a single event
