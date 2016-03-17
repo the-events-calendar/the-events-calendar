3497c7fce7880d152343a69e1bf1da26ab689c6b
Luca Tumedei <luca@theaveragedev.com>
2016-03-17 14:06:08 +0100

    cleaned use statements

--- tests/functional/Tribe/Events/Importer/File_Importer_Events_FeaturedImageTest.php
+++ tests/functional/Tribe/Events/Importer/File_Importer_Events_FeaturedImageTest.php
@@ -6,7 +6,6 @@ require_once 'File_Importer_EventsTest.php';
 use Handlebars\Handlebars;
 use Handlebars\Loader\FilesystemLoader;
 use org\bovigo\vfs\vfsStream;
-use Tribe__Events__Importer__File_Importer_Events as Events_Importer;
 
 class File_Importer_Events_FeaturedImageTest extends File_Importer_EventsTest {
 
