624db125fd3e109902bfce287ff7f247c3d6d72e
bordoni <bordoni.dev@gmail.com>
2016-03-24 19:59:30 -0300

    Remove the Duplication

--- src/views/default-template.php
+++ src/views/default-template.php
@@ -17,9 +17,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 get_header();
 ?>
 <div id="tribe-events-pg-template">
-	<?php tribe_events_before_html(); ?>
 	<?php tribe_get_view(); ?>
-	<?php tribe_events_after_html(); ?>
 </div> <!-- #tribe-events-pg-template -->
 <?php
 get_footer();
