f0149973db9093578a5bbdd04e2093d269936916
bordoni <bordoni.dev@gmail.com>
2016-03-28 22:22:13 -0300

    Im glad we have CR

--- src/Tribe/Admin/Organizer_Chooser_Meta_Box.php
+++ src/Tribe/Admin/Organizer_Chooser_Meta_Box.php
@@ -37,7 +37,7 @@ class Tribe__Events__Admin__Organizer_Chooser_Meta_Box {
 		}
 
 		if ( $event instanceof stdClass || is_array( $event ) ) {
-			$event = new WP_Post( $event );
+			$event = new WP_Post( (object) $event );
 		}
 
 		if ( ! $event instanceof WP_Post ) {
