80596609fa97346d0819f2ff9b0620005c426a30
Luca Tumedei <luca@theaveragedev.com>
2016-03-30 11:50:55 +0200

    fixed code style

--- src/Tribe/API.php
+++ src/Tribe/API.php
@@ -449,9 +449,9 @@ if ( ! class_exists( 'Tribe__Events__API' ) ) {
 		public static function createVenue( $data, $post_status = 'publish' ) {
 
 			if ( ( isset( $data['Venue'] ) && $data['Venue'] ) || self::someVenueDataSet( $data ) ) {
-				$title = isset($data['Venue']) ? $data['Venue'] : esc_html__( 'Unnamed Venue', 'the-events-calendar' );
-				$content = isset($data['Description']) ? $data['Description'] : '';
-				$slug  = sanitize_title( $title );
+				$title   = isset( $data['Venue'] ) ? $data['Venue'] : esc_html__( 'Unnamed Venue', 'the-events-calendar' );
+				$content = isset( $data['Description'] ) ? $data['Description'] : '';
+				$slug    = sanitize_title( $title );
 
 				$postdata = array(
 					'post_title'  => $title,
