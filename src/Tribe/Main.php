a5add14e286340411efff699ed477b0669bf9546
bordoni <bordoni.dev@gmail.com>
2016-04-04 01:39:34 -0300

    Load L10N files using Helper from Common

--- src/Tribe/Main.php
+++ src/Tribe/Main.php
@@ -1352,7 +1352,7 @@
 			 * @return void
 			 */
 			public function loadTextDomain() {
-				load_plugin_textdomain( 'tribe-events-calendar-pro', false, $this->pluginDir . 'lang/' );
+				Tribe__Main::instance()->load_text_domain( 'tribe-events-calendar-pro', $this->pluginDir . 'lang/' );
 			}
 
 			/**
