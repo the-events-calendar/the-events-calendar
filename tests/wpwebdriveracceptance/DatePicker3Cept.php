e89191503a80524e8ef1f70f1dfdcbd20118e4af
Ron MacDonald <rmacdon465@yahoo.com>
2016-04-21 12:16:36 -0600

    Fixed and removed commented code

--- tests/wpwebdriveracceptance/DatePicker3Cept.php
+++ tests/wpwebdriveracceptance/DatePicker3Cept.php
@@ -10,8 +10,6 @@
 
 $I = new WpwebdriveracceptanceTester($scenario);
 $I->wantTo('Test the datepicker cases');
-//$I->resizeWindow(1280, 1024);  ?experimenting here to see if window size has to do with Element not visible error
-//$I->resizeWindow(800, 600);
 
 //Login to backend
 $I->amOnPage("/wp-login.php");
@@ -21,160 +19,152 @@ $I->fillField('Password','test_user');
 //$I->fillField("#user_pass", "admin");
 $I->click('Log In');
 
-//Goto the Event Setting tab and click Display tab
-//$I->amOnPage('/wp-admin/edit.php?page=tribe-common&tab=display&post_type=tribe_events');
-//$I->click('#display');
-
-//Get Date Format Backend
-//$I->scrollTo(["css" => "#tribe-field-weekDayFormat"], 50, 200);
-//$I->wait(5);
-//$I->waitForElement(['name' => 'datepickerFormat'], 5); // secs
-//$I->executeJS('jQuery("datepickerFormat").show()');
-//$I->executeJS('jQuery("#datepickerFormat").css("display", "inline")');
-
-//$I->click('#s2id_autogen3');
-//$I->click('form select[name=datepickerFormat]');
-//$format = $I->grabTextFrom(['name' => 'datepickerFormat']);
-//$text = $I->grabTextFrom('option', ['value' => $format ]);
-
 $I->wait(5);
-//$text = $I->executeJS('jQuery("option", ["value" => $format ]).text()');
-
-
 
 $output = '';
 
+$result = '';
 
-
-
-//for ($i=1; $i <=9 ; $i++) { 
-//$I->grabTextFrom('select[name=datepickerFormat] option:nth-child(' . $i .')');
-//$option = $I->select('select[name=datepickerFormat]', 'value ='.$i);
-//$option = $I->grabTextFrom('select[name=datepickerFormat] option:nth-child(' . $i .')');
-//$option = $I->executeJS('return $("select[name=datepickerFormat] option:nth-child(' . $i .')").val()');
 $array = array("2016-01-15","1/15/2016","01/15/2016","15/1/2016","15/01/2016","1-15-2016","01-15-2016","15-1-2016","15-01-2016");
+$i = 0;
 
 foreach($array as $ar){
 	$option = $ar;
-$I->amOnPage('/wp-admin/edit.php?page=tribe-common&tab=display&post_type=tribe_events');
-$I->scrollTo(["css" => "#tribe-field-weekDayFormat"], 50, 200);
-$I->wait(5);
-$I->waitForElement(['name' => 'datepickerFormat'], 5); // secs
 
-$I->click('#s2id_autogen3');
+	//Goto the Event Setting tab and click Display tab
+	$I->amOnPage('/wp-admin/edit.php?page=tribe-common&tab=display&post_type=tribe_events');
+	$I->waitForElement(['name' => 'datepickerFormat'], 5); // secs
+	$I->scrollTo(["css" => "#tribe-field-weekDayFormat"]);
+	$jQueryDatepicker = 'jQuery(\'select[name="datepickerFormat"]\').val(' . $i  . ').prop("selected","selected").change();';
+	$I->executeJS($jQueryDatepicker);
+	$I->wait(2);
+	$I->click('Save Changes');
 
-//script is failing here
-$I->selectOption("select[name=datepickerFormat]", $option); 
-//End fail
+	//Increment $i
+	$i = $i + 1;
 
-$I->click('Save Changes');
+	//Move to Frontend event date
+	$I->amOnPage('/events/today/');
 
+	$jQueryDay = 'jQuery(\'#tribe-bar-date\').val()';
 
-//$I->wantTo('Test the datepicker cases ' . $option . '' );
+	$frontdate = $I->executeJS('return jQuery("#tribe-bar-date").val()');
+	
+	$I->wait(2);
 
-//Move to Frontend event date
-$I->amOnPage('/events');
-
-$frontdate = $I->grabValueFrom('#tribe-bar-date');
-
-$result = '';
-
-//For Date Format Y-m-d
-if(validateDate($option, 'Y-m-d') == 1){
-	$front_status = validateDate($frontdate, 'Y-m-d');
-	if($front_status == 1){
-		$result = 'Matched!';
-	}else{
-		$result = 'Not Matched!!';
+	//For Date Format Y-m-d
+	if(validateDate($option, 'Y-m-d') == 1){
+		$front_status = validateDate($frontdate, 'Y-m-d');
+		
+		if($front_status == 1){
+			$result = '1. Y-m-d Matched!';
+			$I->comment('1. Frontdate is format Matched');
+		}else{
+			$result = '1. Y-m-d Not Matched!!';
+			throw new \InvalidArgumentException('1. Not Matched!!');
+		}
 	}
-}
 
-//For Date Format n/d/Y
-if(validateDate($option, 'n/d/Y') == 1){
-	$front_status = validateDate($frontdate, 'n/d/Y');
-	if($front_status == 1){
-		$result = 'Matched!';
-	}else{
-		$result = 'Not Matched!!';
+	//For Date Format n/d/Y
+	if(validateDate($option, 'n/d/Y') == 1){
+		$front_status = validateDate($frontdate, 'n/d/Y');
+		if($front_status == 1){
+			$result = '2. n/d/Y Matched!';
+			$I->comment('2. Frontdate is format Matched');
+		}else{
+			$result = '2. n/d/Y Not Matched!!';
+			throw new \InvalidArgumentException('2. Not Matched!!');
+		}
 	}
-}
 
-//For Date Format m/d/Y
-if(validateDate($option, 'm/d/Y') == 1){
-	$front_status = validateDate($frontdate, 'm/d/Y');
-	if($front_status == 1){
-		$result = 'Matched!';
-	}else{
-		$result = 'Not Matched!!';
+	//For Date Format m/d/Y
+	if(validateDate($option, 'm/d/Y') == 1){
+		
+		$I->comment('Frontdate is format m/d/Y');
+		if($front_status == 1){
+			$result = '3. m/d/Y Matched!';
+			$I->comment('3. Frontdate is format Matched');
+		}else{
+			$result = '3. m/d/Y Not Matched!!';
+			throw new \InvalidArgumentException('3. Not Matched!!');
+		}
 	}
-}
 
-//For Date Format d/n/Y
-if(validateDate($option, 'd/n/Y') == 1){
-	$front_status = validateDate($frontdate, 'd/n/Y');
-	if($front_status == 1){
-		$result = 'Matched!';
-	}else{
-		$result = 'Not Matched!!';
+	//For Date Format d/n/Y
+	if(validateDate($option, 'd/n/Y') == 1){
+	
+		$I->comment('Frontdate is format d/n/Y');
+		if($front_status == 1){
+			$result = '4. d/n/Y Matched!';
+			$I->comment('4. Frontdate is format Matched');
+		}else{
+			$result = '4. d/n/Y Not Matched!!';
+			throw new \InvalidArgumentException('4. Not Matched!!');
+		}
 	}
-}
 
-//For Date Format d/m/Y
-if(validateDate($option, 'd/m/Y') == 1){
-	$front_status = validateDate($frontdate, 'd/m/Y');
-	if($front_status == 1){
-		$result = 'Matched!';
-	}else{
-		$result = 'Not Matched!!';
+	//For Date Format d/m/Y
+	if(validateDate($option, 'd/m/Y') == 1){
+		$front_status = validateDate($frontdate, 'd/m/Y');
+		if($front_status == 1){
+			$result = '5. d/m/Y Matched!';
+			$I->comment('5. Frontdate is format Matched');
+		}else{
+			$result = '5. d/m/Y Not Matched!!';
+			throw new \InvalidArgumentException('5. Not Matched!!');
+		}
 	}
-}
 
-//For Date Format n-d-Y
-if(validateDate($option, 'n-d-Y') == 1){
-	$front_status = validateDate($frontdate, 'n-d-Y');
-	if($front_status == 1){
-		$result = 'Matched!';
-	}else{
-		$result = 'Not Matched!!';
+	//For Date Format n-d-Y
+	if(validateDate($option, 'n-d-Y') == 1){
+		$front_status = validateDate($frontdate, 'n-d-Y');
+		if($front_status == 1){
+			$result = '6. Matched!';
+			$I->comment('6. Frontdate is format Matched');
+		}else{
+			$result = '6. Not Matched!!';
+			throw new \InvalidArgumentException('6. Not Matched!!');
+		}
 	}
-}
 
-//For Date Format m-d-Y
-if(validateDate($option, 'm-d-Y') == 1){
-	$front_status = validateDate($frontdate, 'm-d-Y');
-	if($front_status == 1){
-		$result = 'Matched!';
-	}else{
-		$result = 'Not Matched!!';
+	//For Date Format m-d-Y
+	if(validateDate($option, 'm-d-Y') == 1){
+		$front_status = validateDate($frontdate, 'm-d-Y');
+		if($front_status == 1){
+			$result = '7. Matched!';
+			$I->comment('7. Frontdate is format Matched');
+		}else{
+			$result = '7. Not Matched!!';
+			throw new \InvalidArgumentException('7. Not Matched!!');
+		}
 	}
-}
 
-//For Date Format d-n-Y
-if(validateDate($option, 'd-n-Y') == 1){
-	$front_status = validateDate($frontdate, 'd-n-Y');
-	if($front_status == 1){
-		$result = 'Matched!';
-	}else{
-		$result = 'Not Matched!!';
+	//For Date Format d-n-Y
+	if(validateDate($option, 'd-n-Y') == 1){
+		$front_status = validateDate($frontdate, 'd-n-Y');
+		if($front_status == 1){
+			$result = '8. Matched!';
+			$I->comment('8. Frontdate is format Matched');
+		}else{
+			$result = '8. Not Matched!!';
+			throw new \InvalidArgumentException('8. Not Matched!!');
+		}
 	}
-}
 
-//For Date Format d-m-Y
-if(validateDate($option, 'd-m-Y') == 1){
-	$front_status = validateDate($frontdate, 'd-m-Y');
-	if($front_status == 1){
-		$result = 'Matched!';
-	}else{
-		$result = 'Not Matched!!';
+	//For Date Format d-m-Y
+	if(validateDate($option, 'd-m-Y') == 1){
+		$front_status = validateDate($frontdate, 'd-m-Y');
+		if($front_status == 1){
+			$result = '9. Matched!';
+			$I->comment('9. Frontdate is format Matched');
+		}else{
+			$result = '9. Not Matched!!';
+			throw new \InvalidArgumentException('9. Not Matched!!');
+		}	
 	}
-}
-
-
 
 $output .= ' ' . $option . ' and ' . $frontdate . ' => ' . $result . ' || ';
 
-$I->amOnPage('/wp-admin/edit.php?page=tribe-common&tab=display&post_type=tribe_events');
-$I->click('#display');
 }
 
 $I->wantTo('Testing: ' . $output . '' );
@@ -188,23 +178,5 @@ function validateDate($date, $format)
     return $d && $d->format($format) == $date;
 }
 
-/*//Get Date Format Backend
-$format = $I->grabValueFrom(['name' => 'dateWithYearFormat']);
-//$I->wantTo('Test the datepicker cases ' . $format . '' );
 
-//Get Date frontend
-$I->amOnPage('/event/test-event/');
-$date = $I->grabTextFrom('.tribe-event-date-start');
-//$I->wantTo('Test the datepicker cases ' . $date . '' );
-
-$status = validateDate($date, $format);
-
-//Display Message according to conditions
-if($status == 1){
-	$I->wantTo('Format matched!!(' . $date . ' = ' . $format . ')' );
-}else{
-	$I->wantTo('Format not matched!(' . $date . ' != ' . $format . ')' );
-}*/
 ?>
-DatePickerCept.php
-Displaying DatePickerCept.php.
