c3865903add333c7ce20ef2c6101b7c193893602
Matthew Batchelder <borkweb@gmail.com>
2016-05-02 16:38:37 -0700

    Support oembeds for recurring event /all/ pages
    See: https://central.tri.be/issues/46516

--- src/Tribe/Main.php
+++ src/Tribe/Main.php
@@ -191,6 +191,9 @@
 				// Event CSV import additions
 				add_filter( 'tribe_events_importer_venue_column_names', array( Tribe__Events__Pro__CSV_Importer__Fields::instance(), 'filter_venue_column_names' ), 10, 1 );
 				add_filter( 'tribe_events_importer_venue_array', array( Tribe__Events__Pro__CSV_Importer__Fields::instance(), 'filter_venue_array' ), 10, 4 );
+
+				add_filter( 'oembed_discovery_links', array( $this, 'oembed_discovery_links_for_recurring_events' ) );
+				add_filter( 'oembed_request_post_id', array( $this, 'oembed_request_post_id_for_recurring_events' ), 10, 2 );
 			}
 
 			/**
@@ -1710,6 +1713,58 @@
 				return self::$instance;
 			}
 
+			/**
+			 * Outputs oembed resource links on the /all/ pages for recurring events
+			 *
+			 * @since 4.2
+			 *
+			 * @param string $output Resource links to output
+			 *
+			 * @return string
+			 */
+			public function oembed_discovery_links_for_recurring_events( $output ) {
+				global $wp_query;
 
+				if ( $output ) {
+					return $output;
+				}
+
+				if ( ! tribe_is_showing_all() ) {
+					return $output;
+				}
+
+				if ( empty( $wp_query->posts[0] ) ) {
+					return $output;
+				}
+
+				$post = $wp_query->posts[0];
+				$post_id = $post->ID;
+
+				$output = '<link rel="alternate" type="application/json+oembed" href="' . esc_url( get_oembed_endpoint_url( add_query_arg( 'post_id', $post_id, get_permalink( $post_id ) ) ) ) . '" />' . "\n";
+
+				if ( class_exists( 'SimpleXMLElement' ) ) {
+					$output .= '<link rel="alternate" type="text/xml+oembed" href="' . esc_url( get_oembed_endpoint_url( add_query_arg( 'post_id', $post_id, get_permalink( $post_id ) ), 'xml' ) ) . '" />' . "\n";
+				}
+
+				return $output;
+			}
+
+			/**
+			 * Convert a /all/ URL to an upcoming post id for oembeds
+			 *
+			 * @since 4.2
+			 *
+			 * @param int $post_id Post ID of the event
+			 * @param string $url URL of the oembed resource
+			 *
+			 * @return int
+			 */
+			public function oembed_request_post_id_for_recurring_events( $post_id, $url ) {
+				if ( $post_id ) {
+					return $post_id;
+				}
+
+				return tribe_get_upcoming_recurring_event_id_from_url( $url );
+			}
 		} // end Class
 	}
