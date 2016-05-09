98597bf849d711edf803f5e3f379796bc35f2953
bordoni <bordoni.dev@gmail.com>
2016-05-09 12:25:28 -0300

    Conditionally use Common methods

--- src/Tribe/Main.php
+++ src/Tribe/Main.php
@@ -1357,7 +1357,16 @@
 			 * @return void
 			 */
 			public function loadTextDomain() {
-				Tribe__Main::instance()->load_text_domain( 'tribe-events-calendar-pro', $this->pluginDir . 'lang/' );
+				$mopath = $this->pluginDir . 'lang/';
+				$domain = 'tribe-events-calendar-pro';
+
+				// If we don't have Common classes load the old fashioned way
+				if ( ! class_exists( 'Tribe__Main' ) ) {
+					load_plugin_textdomain( $domain, false, $mopath )
+				} else {
+					// This will load `wp-content/languages/plugins` files first
+					Tribe__Main::instance()->load_text_domain( $domain, $mopath );
+				}
 			}
 
 			/**
