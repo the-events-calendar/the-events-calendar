59d973cfa8e0cefe605f02b2ad533c31e3539ad2
barryhughes <barry@codingkills.me>
2016-05-31 15:28:54 -0700

    Bump version # to 4.2rc1 | #44413

--- src/Tribe/Main.php
+++ src/Tribe/Main.php
@@ -46,8 +46,8 @@
 			 */
 			public $widget_wrappers;
 
-			const REQUIRED_TEC_VERSION = '4.2RC1';
-			const VERSION = '4.2RC1';
+			const REQUIRED_TEC_VERSION = '4.2rc1';
+			const VERSION = '4.2rc1';
 
 
 			private function __construct() {
