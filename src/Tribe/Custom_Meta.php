b85c0ceb609a159e453e7ca933a6d498160c6fbe
barryhughes <barry@codingkills.me>
2016-06-15 15:22:14 -0700

    Cleanup, avoid notices | #45674

--- src/Tribe/Custom_Meta.php
+++ src/Tribe/Custom_Meta.php
@@ -254,11 +254,15 @@ class Tribe__Events__Pro__Custom_Meta {
 	private static function get_value_to_save( $name, $data ) {
 		$value = '';
 
-		// We make an exception for (str) '0' because PHP would otherwise deem it to be empty
-		if ( ! empty( $data ) && ( ! empty( $data[ $name ] ) || $data[ $name ] === '0' ) ) {
+		// $data takes precedence over $_POST but we want to check both
+		if ( isset( $_POST ) ) {
+			$data = array_merge( $data, $_POST );
+		}
+
+		// Is the field set and non-empty? Note that we make an exception for (string) '0'
+		// which in this case we don't want to treat as being empty
+		if ( isset( $data[ $name ] ) && $data[ $name ] === '0' || ! empty( $data[ $name ] ) ) {
 			$value = $data[ $name ];
-		} elseif ( ! empty( $_POST ) && ! empty( $_POST[ $name ] ) ) {
-			$value = $_POST[ $name ];
 		}
 
 		return $value;
