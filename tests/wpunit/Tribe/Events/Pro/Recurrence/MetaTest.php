3e467185181f2083337d42d6312a345d47173844
Luca Tumedei <luca@theaveragedev.com>
2016-06-16 10:30:39 +0200

    added test for well-formed recurrence information

--- tests/wpunit/Tribe/Events/Pro/Recurrence/MetaTest.php
+++ tests/wpunit/Tribe/Events/Pro/Recurrence/MetaTest.php
@@ -66,6 +66,39 @@ class MetaTest extends \Codeception\TestCase\WPTestCase {
 	}
 
 	/**
+	 * @test
+	 * it should return all recurrence rules if none is malformed when getting an event recurrence meta
+	 */
+	public function it_should_return_all_recurrence_rules_if_none_is_malformed_when_getting_an_event_recurrence_meta() {
+		$sut        = $this->make_instance();
+		$meta_input = [
+			'_EventRecurrence' => [
+				'rules'      => [
+					0 => [
+						'type'      => 'Every Week',
+						'end-type'  => 'After',
+						'end'       => null,
+						'end-count' => 5,
+					],
+					1 => [
+						'type'      => 'Every Day',
+						'end-type'  => 'After',
+						'end'       => null,
+						'end-count' => 5,
+					],
+				],
+				'exclusions' => [ ]
+			]
+		];
+		$event_id   = $this->factory()->post->create( [ 'post_type' => Main::POSTTYPE, 'meta_input' => $meta_input ] );
+
+		$recurrence_for_event = $sut->get_recurrence_for_event( $event_id );
+		$this->assertArrayHasKey( 'rules', $recurrence_for_event );
+		$this->assertArrayHasKey( 'exclusions', $recurrence_for_event );
+		$this->assertCount( 2, $recurrence_for_event['rules'] );
+	}
+
+	/**
 	 * @return Meta
 	 */
 	private function make_instance() {
