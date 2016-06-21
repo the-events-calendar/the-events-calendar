2b331f32c96b259075272eaa3ce9ac65907a5893
Luca Tumedei <luca@theaveragedev.com>
2016-06-21 15:04:53 +0200

    fixed last not passing acceptance tests

--- tests/acceptance/Issue_38042_Dropping_CategoryCest.php
+++ tests/acceptance/Issue_38042_Dropping_CategoryCest.php
@@ -10,8 +10,7 @@ class Issue_38042_Dropping_CategoryCest {
 		$I->setTribeOption( 'tribeEventsTemplate', '' );
 		$I->setTribeOption( 'viewOption', 'month' );
 
-		// let's resize the window not to incur in mobile breakpoints
-		$I->resizeWindow( 1200, 1000 );
+		$I->haveTermInDatabase( $this->term_slug, 'tribe_events_cat', [ 'slug' => $this->term_slug ] );
 
 		$I->amOnPage( "/events/category/{$this->term_slug}" );
 
@@ -19,14 +18,13 @@ class Issue_38042_Dropping_CategoryCest {
 		//This issue *should* be independent of posts and categories in the database.
 	}
 
-	public function _after( AcceptanceTester $I ) {
-	}
-
 	/**
 	 * @test
 	 * it should not drop the category when using the Tribe search bar
 	 */
 	public function it_should_not_drop_the_category_when_using_the_tribe_search_bar( AcceptanceTester $I ) {
+		$I->click( '#tribe-bar-collapse-toggle:not(.tribe-bar-filters-open)' );
+
 		$I->fillField( 'input[name="tribe-bar-search"]', 'foo' );
 		$I->click( 'input[name="submit-bar"]' );
 
@@ -39,12 +37,16 @@ class Issue_38042_Dropping_CategoryCest {
 
 	/**
 	 * @test
-	 * it should it should not drop the category when unsing the month selector
+	 * it should not drop the category when using the month selector
 	 */
-	public function it_should_it_should_not_drop_the_category_when_unsing_the_month_selector( AcceptanceTester $I ) {
-		$I->click( [ 'css' => 'input#tribe-bar-date' ] );
+	public function it_should_not_drop_the_category_when_using_the_month_selector( AcceptanceTester $I ) {
+		$I->click( '#tribe-bar-collapse-toggle:not(.tribe-bar-filters-open)' );
+
+		$I->waitForElement( 'input#tribe-bar-date' );
+
+		$I->click( 'input#tribe-bar-date' );
 		// I click the 5th month in the datepicker
-		$I->click( [ 'css' => 'body > .datepicker > div.datepicker-months span:nth-child(5)' ] );
+		$I->click( 'body > .datepicker > div.datepicker-months span:nth-child(5)' );
 
 		$I->waitForJqueryAjax( 10 );
 
@@ -57,10 +59,12 @@ class Issue_38042_Dropping_CategoryCest {
 	 * @test
 	 * it should not drop the category when using the month selector then the search
 	 */
-	public function it_should_not_drop_the_category_when_using_the_month_selector_and_the_search( AcceptanceTester $I ) {
-		$I->click( [ 'css' => 'input#tribe-bar-date' ] );
+	public function it_should_not_drop_the_category_when_using_the_month_selector_then_the_search( AcceptanceTester $I ) {
+		$I->click( '#tribe-bar-collapse-toggle:not(.tribe-bar-filters-open)' );
+
+		$I->click( 'input#tribe-bar-date' );
 		// I click the 5th month in the datepicker, this will submit
-		$I->click( [ 'css' => 'body > .datepicker > div.datepicker-months span:nth-child(11)' ] );
+		$I->click( 'body > .datepicker > div.datepicker-months span:nth-child(11)' );
 
 		$I->waitForJqueryAjax( 10 );
 
@@ -80,15 +84,16 @@ class Issue_38042_Dropping_CategoryCest {
 	 * it should not drop category when using the search then the month selector
 	 */
 	public function it_should_not_drop_category_when_using_the_search_then_the_month_selector( AcceptanceTester $I ) {
+		$I->click( '#tribe-bar-collapse-toggle:not(.tribe-bar-filters-open)' );
 
 		$I->fillField( 'input[name="tribe-bar-search"]', 'foo' );
 		$I->click( 'input[name="submit-bar"]' );
 
 		$I->waitForJqueryAjax( 10 );
 
-		$I->click( [ 'css' => 'input#tribe-bar-date' ] );
+		$I->click( 'input#tribe-bar-date' );
 		// I click the 5th month in the datepicker, this will submit
-		$I->click( [ 'css' => 'body > .datepicker > div.datepicker-months span:nth-child(5)' ] );
+		$I->click( 'body > .datepicker > div.datepicker-months span:nth-child(5)' );
 
 		$I->waitForJqueryAjax( 10 );
 
