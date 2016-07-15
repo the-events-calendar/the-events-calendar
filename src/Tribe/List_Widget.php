077adcf4c9350fe5b00c671e38b842371c07d4b3
Luca Tumedei <luca@theaveragedev.com>
2016-07-15 17:49:08 +0200

    refactroed JSON-LD code into own method

--- src/Tribe/List_Widget.php
+++ src/Tribe/List_Widget.php
@@ -132,8 +132,7 @@ class Tribe__Events__List_Widget extends WP_Widget {
 		echo $after_widget;
 		wp_reset_query();
 
-		// print JSON-LD data about the widget events
-		Tribe__Events__JSON_LD__Event::instance()->markup(self::$posts);
+		$this->print_jsonld_markup_for(self::$posts);
 
 		// Reinstate the tribe bar params
 		if ( ! empty( $hold_tribe_bar_args ) ) {
@@ -190,5 +189,13 @@ class Tribe__Events__List_Widget extends WP_Widget {
 			'limit'              => '5',
 			'no_upcoming_events' => false,
 		) );
+	}
+
+	protected function print_jsonld_markup_for( $posts ) {
+		if ( empty( $posts ) ) {
+			return;
+		}
+
+		Tribe__Events__JSON_LD__Event::instance()->markup(self::$posts);
 	}
 }
