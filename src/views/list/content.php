4c5eee5930e1b2437e168855bc4013aec9578262
Luca Tumedei <luca@theaveragedev.com>
2016-07-19 15:37:27 +0200

    added missing doc block and fixed space issue

--- src/views/list/content.php
+++ src/views/list/content.php
@@ -16,7 +16,13 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 <div id="tribe-events-content" class="tribe-events-list">
 
-	<?php do_action('tribe_events_list_before_the_content'); ?>
+
+	<?php
+	/**
+	 * Fires before any content is printed inside the list view.
+	 */
+	do_action( 'tribe_events_list_before_the_content' );
+	?>
 
 	<!-- List Title -->
 	<?php do_action( 'tribe_events_before_the_title' ); ?>
