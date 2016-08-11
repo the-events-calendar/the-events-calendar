dd3005e321902c70cf60eade53e9d10d612362a3
Zach <zbtirrell@gmail.com>
2016-08-11 19:43:53 -0400

    tweaks to refine fields (#814)
    * tweaks to refine fields
    
    See feedback doc
    
    * add additional helper text for categories

--- src/admin-views/aggregator/origins/refine.php
+++ src/admin-views/aggregator/origins/refine.php
@@ -1,8 +1,8 @@
 <?php
 $keywords                = new stdClass;
-$keywords->placeholder   = __( 'Add Keyword', 'the-events-calendar' );
+$keywords->placeholder   = __( 'Keyword(s)', 'the-events-calendar' );
 $location                = new stdClass;
-$location->placeholder   = __( 'Add Location', 'the-events-calendar' );
+$location->placeholder   = __( 'Location', 'the-events-calendar' );
 $start_date              = new stdClass;
 $start_date->placeholder = __( 'Start Date', 'the-events-calendar' );
 $radius                  = new stdClass;
@@ -20,47 +20,53 @@ if ( 'ics' === $origin_slug ) {
 		<label for="tribe-ea-field-refine_keywords"><?php echo __( 'Refine:', 'the-events-calendar' ); ?></label>
 	</th>
 	<td>
-		<input
-			name="aggregator[<?php echo esc_attr( $origin_slug ); ?>][keywords]"
-			type="text"
-			id="tribe-ea-field-ics_keywords"
-			class="tribe-ea-field tribe-ea-size-large"
-			placeholder="<?php echo esc_attr( $keywords->placeholder ); ?>"
-			value="<?php echo esc_attr( empty( $record->meta['keywords'] ) ? '' : $record->meta['keywords'] ); ?>"
-		>
-		<input
-			name="aggregator[<?php echo esc_attr( $origin_slug ); ?>][start]"
-			type="text"
-			id="tribe-ea-field-ics_start"
-			class="tribe-ea-field tribe-ea-size-large"
-			placeholder="<?php echo esc_attr( $start_date->placeholder ); ?>"
-			value="<?php echo esc_attr( empty( $record->meta['start'] ) ? '' : $record->meta['start'] ); ?>"
-		>
-		<input
-			name="aggregator[<?php echo esc_attr( $origin_slug ); ?>][location]"
-			type="text"
-			id="tribe-ea-field-ics_location"
-			class="tribe-ea-field tribe-ea-size-large"
-			placeholder="<?php echo esc_attr( $location->placeholder ); ?>"
-			value="<?php echo esc_attr( empty( $record->meta['location'] ) ? '' : $record->meta['location'] ); ?>"
-		>
-		<select
-			name="aggregator[<?php echo esc_attr( $origin_slug ); ?>][radius]"
-			id="tribe-ea-field-ics_radius"
-			class="tribe-ea-field tribe-ea-dropdown tribe-ea-size-large"
-			placeholder="<?php echo esc_attr( $radius->placeholder ); ?>"
-			data-hide-search
-			data-prevent-clear
-		>
-			<option value=""></option>
-			<?php
-			foreach ( Tribe__Events__Utils__Radius::get_radii() as $name => $value ) {
-				?>
-				<option value="<?php echo esc_attr( $value ); ?>" <?php selected( $value, empty( $record->meta['radius'] ) ? '' : $record->meta['radius'] ); ?>><?php esc_html_e( $name ); ?></option>
+		<div class="tribe-refine">
+			<input
+				name="aggregator[<?php echo esc_attr( $origin_slug ); ?>][keywords]"
+				type="text"
+				id="tribe-ea-field-ics_keywords"
+				class="tribe-ea-field tribe-ea-size-xlarge"
+				placeholder="<?php echo esc_attr( $keywords->placeholder ); ?>"
+				value="<?php echo esc_attr( empty( $record->meta['keywords'] ) ? '' : $record->meta['keywords'] ); ?>"
+			>
+			<span class="tribe-bumpdown-trigger tribe-bumpdown-permanent tribe-ea-help dashicons dashicons-editor-help" data-bumpdown="<?php echo esc_attr( $radius->help ); ?>"></span>
+		</div>
+		<div class="tribe-refine">
+			<input
+				name="aggregator[<?php echo esc_attr( $origin_slug ); ?>][start]"
+				type="text"
+				id="tribe-ea-field-ics_start"
+				class="tribe-ea-field tribe-ea-size-medium"
+				placeholder="<?php echo esc_attr( $start_date->placeholder ); ?>"
+				value="<?php echo esc_attr( empty( $record->meta['start'] ) ? '' : $record->meta['start'] ); ?>"
+			>
+		</div>
+		<div class="tribe-refine">
+			<input
+				name="aggregator[<?php echo esc_attr( $origin_slug ); ?>][location]"
+				type="text"
+				id="tribe-ea-field-ics_location"
+				class="tribe-ea-field tribe-ea-size-large"
+				placeholder="<?php echo esc_attr( $location->placeholder ); ?>"
+				value="<?php echo esc_attr( empty( $record->meta['location'] ) ? '' : $record->meta['location'] ); ?>"
+			>
+			<select
+				name="aggregator[<?php echo esc_attr( $origin_slug ); ?>][radius]"
+				id="tribe-ea-field-ics_radius"
+				class="tribe-ea-field tribe-ea-dropdown tribe-ea-size-medium"
+				placeholder="<?php echo esc_attr( $radius->placeholder ); ?>"
+				data-hide-search
+				data-prevent-clear
+			>
+				<option value=""></option>
 				<?php
-			}
-			?>
-		</select>
-		<span class="tribe-bumpdown-trigger tribe-bumpdown-permanent tribe-ea-help dashicons dashicons-editor-help" data-bumpdown="<?php echo esc_attr( $radius->help ); ?>"></span>
+				foreach ( Tribe__Events__Utils__Radius::get_radii() as $name => $value ) {
+					?>
+					<option value="<?php echo esc_attr( $value ); ?>" <?php selected( $value, empty( $record->meta['radius'] ) ? '' : $record->meta['radius'] ); ?>><?php esc_html_e( $name ); ?></option>
+					<?php
+				}
+				?>
+			</select>
+		</div>
 	</td>
 </tr>
