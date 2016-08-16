4da238759f263787588662ad54ab1dd5aad3ded1
Luca Tumedei <luca@theaveragedev.com>
2016-08-16 15:36:32 +0200

    add an isset check

--- src/Tribe/API.php
+++ src/Tribe/API.php
@@ -159,7 +159,7 @@ if ( ! class_exists( 'Tribe__Events__API' ) ) {
 					else {
 						update_post_meta( $event_id, $tag, $data[ $htmlElement ] );
 					}
-
+	//here
 					if ( self::is_meta_value_changed( $tag, $data, $post_meta ) ) {
 						$modified[ $tag ] = $now;
 					}
@@ -231,7 +231,7 @@ if ( ! class_exists( 'Tribe__Events__API' ) ) {
 				$data_value = null;
 			}
 
-			if ( $data_value !== $old[ $prefixed_field ] ) {
+			if ( ! isset( $old[ $prefixed_field ] ) || $data_value !== $old[ $prefixed_field ] ) {
 				return true;
 			}
 
