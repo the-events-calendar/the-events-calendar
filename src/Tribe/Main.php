d76b9c28f6b672207c734e215dd39e6070d83009
Luca Tumedei <luca@theaveragedev.com>
2016-08-23 18:33:47 +0200

    fix version numbers

--- src/Tribe/Main.php
+++ src/Tribe/Main.php
@@ -32,7 +32,7 @@ if ( ! class_exists( 'Tribe__Events__Main' ) ) {
 		const VENUE_POST_TYPE     = 'tribe_venue';
 		const ORGANIZER_POST_TYPE = 'tribe_organizer';
 
-		const VERSION           = '4.2.5';
+		const VERSION           = '4.2.6';
 		const MIN_ADDON_VERSION = '4.2';
 		const WP_PLUGIN_URL     = 'http://wordpress.org/extend/plugins/the-events-calendar/';
 
