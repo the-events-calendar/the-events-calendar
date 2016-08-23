b64794c7833c1b23a6ca0f919121c65178ab9534
Zach <zbtirrell@gmail.com>
2016-08-23 07:40:13 -0400

    import settings need to be available for non-Aggregator (#873)
    Also some reorganization of the code there to make it more scannable

--- src/admin-views/aggregator/settings.php
+++ src/admin-views/aggregator/settings.php
@@ -35,13 +35,6 @@ $show_map_options = array(
 $origin_show_map_options = array( '' => $use_global_settings_phrase ) + $show_map_options;
 
 $change_authority = array(
-	'overwrite' => __( 'Overwrite my event with any changes from the original source.', 'the-events-calendar' ),
-	'retain' => __( 'Do not re-import events. Changes made locally will be preserved.', 'the-events-calendar' ),
-	'preserve_changes' => __( 'Preserve the most recent change in each event field, whether that change occurred on my site or at the original source.', 'the-events-calendar' ),
-);
-
-// if there's an Event Aggregator license key, add the Facebook API fields
-$internal = array(
 	'import-defaults-update_authority' => array(
 		'type' => 'html',
 		'html' => '<h3>' . esc_html__( 'Event Update Authority', 'the-events-calendar' ) . '</h3>',
@@ -56,45 +49,15 @@ $internal = array(
 		'validation_type' => 'options',
 		'default' => 'retain',
 		'parent_option' => Tribe__Events__Main::OPTIONNAME,
-		'options' => $change_authority,
+		'options' => array(
+			'overwrite' => __( 'Overwrite my event with any changes from the original source.', 'the-events-calendar' ),
+			'retain' => __( 'Do not re-import events. Changes made locally will be preserved.', 'the-events-calendar' ),
+			'preserve_changes' => __( 'Preserve the most recent change in each event field, whether that change occurred on my site or at the original source.', 'the-events-calendar' ),
+		),
 	),
-	'import-defaults' => array(
-		'type' => 'html',
-		'html' => '<h3>' . esc_html__( 'Global Import Settings', 'the-events-calendar' ) . '</h3>',
-	),
-	'tribe_aggregator_default_post_status' => array(
-		'type' => 'dropdown',
-		'label' => esc_html__( 'Default Status', 'the-events-calendar' ),
-		'tooltip' => esc_html__( 'The default post status for events', 'the-events-calendar' ),
-		'size' => 'medium',
-		'validation_type' => 'options',
-		'default' => 'publish',
-		'can_be_empty' => true,
-		'parent_option' => Tribe__Events__Main::OPTIONNAME,
-		'options' => $post_statuses,
-	),
-	'tribe_aggregator_default_category' => array(
-		'type' => 'dropdown',
-		'label' => esc_html__( 'Default Event Category', 'the-events-calendar' ),
-		'tooltip' => esc_html__( 'The default event category for events', 'the-events-calendar' ),
-		'size' => 'medium',
-		'validation_type' => 'options',
-		'default' => '',
-		'can_be_empty' => true,
-		'parent_option' => Tribe__Events__Main::OPTIONNAME,
-		'options' => $categories,
-	),
-	'tribe_aggregator_default_show_map' => array(
-		'type' => 'dropdown',
-		'label' => esc_html__( 'Show Google Map', 'the-events-calendar' ),
-		'tooltip' => esc_html__( 'Show Google Map by default on imported venues', 'the-events-calendar' ),
-		'size' => 'medium',
-		'validation_type' => 'options',
-		'default' => 'no',
-		'can_be_empty' => true,
-		'parent_option' => Tribe__Events__Main::OPTIONNAME,
-		'options' => $show_map_options,
-	),
+);
+
+$csv = array(
 	'csv-defaults' => array(
 		'type' => 'html',
 		'html' => '<h3>' . esc_html__( 'CSV Import Settings', 'the-events-calendar' ) . '</h3>',
@@ -121,176 +84,242 @@ $internal = array(
 		'parent_option' => Tribe__Events__Main::OPTIONNAME,
 		'options' => $origin_categories,
 	),
-	'ical-defaults' => array(
-		'type' => 'html',
-		'html' => '<h3>' . esc_html__( 'iCal and .ics File Import Settings', 'the-events-calendar' ) . '</h3>',
-	),
-	'tribe_aggregator_default_ical_post_status' => array(
-		'type' => 'dropdown',
-		'label' => esc_html__( 'Default Status', 'the-events-calendar' ),
-		'tooltip' => esc_html__( 'The default post status for events imported via iCal, Google Calendar, and .ics files', 'the-events-calendar' ),
-		'size' => 'medium',
-		'validation_type' => 'options',
-		'default' => '',
-		'can_be_empty' => true,
-		'parent_option' => Tribe__Events__Main::OPTIONNAME,
-		'options' => $origin_post_statuses,
-	),
-	'tribe_aggregator_default_ical_category' => array(
-		'type' => 'dropdown',
-		'label' => esc_html__( 'Default Event Category', 'the-events-calendar' ),
-		'tooltip' => esc_html__( 'The default event category for events imported via iCal, Google Calendar, and .ics files', 'the-events-calendar' ),
-		'size' => 'medium',
-		'validation_type' => 'options',
-		'default' => '',
-		'can_be_empty' => true,
-		'parent_option' => Tribe__Events__Main::OPTIONNAME,
-		'options' => $origin_categories,
-	),
-	'tribe_aggregator_default_ical_show_map' => array(
-		'type' => 'dropdown',
-		'label' => esc_html__( 'Show Google Map', 'the-events-calendar' ),
-		'tooltip' => esc_html__( 'Show Google Map by default on imported venues', 'the-events-calendar' ),
-		'size' => 'medium',
-		'validation_type' => 'options',
-		'default' => 'no',
-		'can_be_empty' => true,
-		'parent_option' => Tribe__Events__Main::OPTIONNAME,
-		'options' => $origin_show_map_options,
-	),
-	'facebook-defaults' => array(
-		'type' => 'html',
-		'html' => '<h3>' . esc_html__( 'Facebook Import Settings', 'the-events-calendar' ) . '</h3>',
-	),
-	'facebook-defaults-info' => array(
-		'type' => 'html',
-		'html' => '<p>' . sprintf(
-			esc_html__(
-				'To import Facebook events, please be sure to add your Facebook API key and secret on %1$sEvents > Settings > APIs%2$s',
-				'the-events-calendar'
-			),
-			'<a href="' . admin_url( Tribe__Settings::$parent_page . '&page=tribe-common&tab=addons' ) . '">',
-			'</a>'
-		). '</p>',
-	),
-	'tribe_aggregator_default_facebook_post_status' => array(
-		'type' => 'dropdown',
-		'label' => esc_html__( 'Default Status', 'the-events-calendar' ),
-		'tooltip' => esc_html__( 'The default post status for events imported via Facebook', 'the-events-calendar' ),
-		'size' => 'medium',
-		'validation_type' => 'options',
-		'default' => '',
-		'can_be_empty' => true,
-		'parent_option' => Tribe__Events__Main::OPTIONNAME,
-		'options' => $origin_post_statuses,
-	),
-	'tribe_aggregator_default_facebook_category' => array(
-		'type' => 'dropdown',
-		'label' => esc_html__( 'Default Event Category', 'the-events-calendar' ),
-		'tooltip' => esc_html__( 'The default event category for events imported via Facebook', 'the-events-calendar' ),
-		'size' => 'medium',
-		'validation_type' => 'options',
-		'default' => '',
-		'can_be_empty' => true,
-		'parent_option' => Tribe__Events__Main::OPTIONNAME,
-		'options' => $origin_categories,
-	),
-	'tribe_aggregator_default_facebook_show_map' => array(
-		'type' => 'dropdown',
-		'label' => esc_html__( 'Show Google Map', 'the-events-calendar' ),
-		'tooltip' => esc_html__( 'Show Google Map by default on imported venues', 'the-events-calendar' ),
-		'size' => 'medium',
-		'validation_type' => 'options',
-		'default' => 'no',
-		'can_be_empty' => true,
-		'parent_option' => Tribe__Events__Main::OPTIONNAME,
-		'options' => $origin_show_map_options,
-	),
-	'gcal-defaults' => array(
-		'type' => 'html',
-		'html' => '<h3>' . esc_html__( 'Google Calendar Import Settings', 'the-events-calendar' ) . '</h3>',
-	),
-	'tribe_aggregator_default_gcal_post_status' => array(
-		'type' => 'dropdown',
-		'label' => esc_html__( 'Default Status', 'the-events-calendar' ),
-		'tooltip' => esc_html__( 'The default post status for events imported via Google Calendar', 'the-events-calendar' ),
-		'size' => 'medium',
-		'validation_type' => 'options',
-		'default' => '',
-		'can_be_empty' => true,
-		'parent_option' => Tribe__Events__Main::OPTIONNAME,
-		'options' => $origin_post_statuses,
-	),
-	'tribe_aggregator_default_gcal_category' => array(
-		'type' => 'dropdown',
-		'label' => esc_html__( 'Default Event Category', 'the-events-calendar' ),
-		'tooltip' => esc_html__( 'The default event category for events imported via Google Calendar', 'the-events-calendar' ),
-		'size' => 'medium',
-		'validation_type' => 'options',
-		'default' => '',
-		'can_be_empty' => true,
-		'parent_option' => Tribe__Events__Main::OPTIONNAME,
-		'options' => $origin_categories,
-	),
-	'tribe_aggregator_default_gcal_show_map' => array(
-		'type' => 'dropdown',
-		'label' => esc_html__( 'Show Google Map', 'the-events-calendar' ),
-		'tooltip' => esc_html__( 'Show Google Map by default on imported venues', 'the-events-calendar' ),
-		'size' => 'medium',
-		'validation_type' => 'options',
-		'default' => 'no',
-		'can_be_empty' => true,
-		'parent_option' => Tribe__Events__Main::OPTIONNAME,
-		'options' => $origin_show_map_options,
-	),
-	'meetup-defaults' => array(
-		'type' => 'html',
-		'html' => '<h3>' . esc_html__( 'Meetup Import Settings', 'the-events-calendar' ) . '</h3>',
-	),
-	'meetup-defaults-info' => array(
-		'type' => 'html',
-		'html' => '<p>' . sprintf(
-			esc_html__(
-				'To import Meetup events, please be sure to add your Meetup API key on %1$sEvents > Settings > APIs%2$s',
-				'the-events-calendar'
-			),
-			'<a href="' . admin_url( Tribe__Settings::$parent_page . '&page=tribe-common&tab=addons' ) . '">',
-			'</a>'
-		). '</p>',
-	),
-	'tribe_aggregator_default_meetup_post_status' => array(
-		'type' => 'dropdown',
-		'label' => esc_html__( 'Default Status', 'the-events-calendar' ),
-		'tooltip' => esc_html__( 'The default post status for events imported via Meetup', 'the-events-calendar' ),
-		'size' => 'medium',
-		'validation_type' => 'options',
-		'default' => '',
-		'can_be_empty' => true,
-		'parent_option' => Tribe__Events__Main::OPTIONNAME,
-		'options' => $origin_post_statuses,
-	),
-	'tribe_aggregator_default_meetup_category' => array(
-		'type' => 'dropdown',
-		'label' => esc_html__( 'Default Event Category', 'the-events-calendar' ),
-		'tooltip' => esc_html__( 'The default event category for events imported via Meetup', 'the-events-calendar' ),
-		'size' => 'medium',
-		'validation_type' => 'options',
-		'default' => '',
-		'can_be_empty' => true,
-		'parent_option' => Tribe__Events__Main::OPTIONNAME,
-		'options' => $origin_categories,
-	),
-	'tribe_aggregator_default_meetup_show_map' => array(
-		'type' => 'dropdown',
-		'label' => esc_html__( 'Show Google Map', 'the-events-calendar' ),
-		'tooltip' => esc_html__( 'Show Google Map by default on imported venues', 'the-events-calendar' ),
-		'size' => 'medium',
-		'validation_type' => 'options',
-		'default' => 'no',
-		'can_be_empty' => true,
-		'parent_option' => Tribe__Events__Main::OPTIONNAME,
-		'options' => $origin_show_map_options,
-	),
+);
+
+$global = $ical = $facebook = $gcal = $meetup = array();
+// if there's an Event Aggregator license key, add the Global settings, Facebook, iCal, and Meetup fields
+if ( Tribe__Events__Aggregator::is_service_active() ) {
+	$global = array(
+		'import-defaults' => array(
+			'type' => 'html',
+			'html' => '<h3>' . esc_html__( 'Global Import Settings', 'the-events-calendar' ) . '</h3>',
+		),
+		'tribe_aggregator_default_post_status' => array(
+			'type' => 'dropdown',
+			'label' => esc_html__( 'Default Status', 'the-events-calendar' ),
+			'tooltip' => esc_html__( 'The default post status for events', 'the-events-calendar' ),
+			'size' => 'medium',
+			'validation_type' => 'options',
+			'default' => 'publish',
+			'can_be_empty' => true,
+			'parent_option' => Tribe__Events__Main::OPTIONNAME,
+			'options' => $post_statuses,
+		),
+		'tribe_aggregator_default_category' => array(
+			'type' => 'dropdown',
+			'label' => esc_html__( 'Default Event Category', 'the-events-calendar' ),
+			'tooltip' => esc_html__( 'The default event category for events', 'the-events-calendar' ),
+			'size' => 'medium',
+			'validation_type' => 'options',
+			'default' => '',
+			'can_be_empty' => true,
+			'parent_option' => Tribe__Events__Main::OPTIONNAME,
+			'options' => $categories,
+		),
+		'tribe_aggregator_default_show_map' => array(
+			'type' => 'dropdown',
+			'label' => esc_html__( 'Show Google Map', 'the-events-calendar' ),
+			'tooltip' => esc_html__( 'Show Google Map by default on imported venues', 'the-events-calendar' ),
+			'size' => 'medium',
+			'validation_type' => 'options',
+			'default' => 'no',
+			'can_be_empty' => true,
+			'parent_option' => Tribe__Events__Main::OPTIONNAME,
+			'options' => $show_map_options,
+		),
+	);
+
+	$ical = array(
+		'ical-defaults' => array(
+			'type' => 'html',
+			'html' => '<h3>' . esc_html__( 'iCal and .ics File Import Settings', 'the-events-calendar' ) . '</h3>',
+		),
+		'tribe_aggregator_default_ical_post_status' => array(
+			'type' => 'dropdown',
+			'label' => esc_html__( 'Default Status', 'the-events-calendar' ),
+			'tooltip' => esc_html__( 'The default post status for events imported via iCal, Google Calendar, and .ics files', 'the-events-calendar' ),
+			'size' => 'medium',
+			'validation_type' => 'options',
+			'default' => '',
+			'can_be_empty' => true,
+			'parent_option' => Tribe__Events__Main::OPTIONNAME,
+			'options' => $origin_post_statuses,
+		),
+		'tribe_aggregator_default_ical_category' => array(
+			'type' => 'dropdown',
+			'label' => esc_html__( 'Default Event Category', 'the-events-calendar' ),
+			'tooltip' => esc_html__( 'The default event category for events imported via iCal, Google Calendar, and .ics files', 'the-events-calendar' ),
+			'size' => 'medium',
+			'validation_type' => 'options',
+			'default' => '',
+			'can_be_empty' => true,
+			'parent_option' => Tribe__Events__Main::OPTIONNAME,
+			'options' => $origin_categories,
+		),
+		'tribe_aggregator_default_ical_show_map' => array(
+			'type' => 'dropdown',
+			'label' => esc_html__( 'Show Google Map', 'the-events-calendar' ),
+			'tooltip' => esc_html__( 'Show Google Map by default on imported venues', 'the-events-calendar' ),
+			'size' => 'medium',
+			'validation_type' => 'options',
+			'default' => 'no',
+			'can_be_empty' => true,
+			'parent_option' => Tribe__Events__Main::OPTIONNAME,
+			'options' => $origin_show_map_options,
+		),
+	);
+
+	$facebook = array(
+		'facebook-defaults' => array(
+			'type' => 'html',
+			'html' => '<h3>' . esc_html__( 'Facebook Import Settings', 'the-events-calendar' ) . '</h3>',
+		),
+		'facebook-defaults-info' => array(
+			'type' => 'html',
+			'html' => '<p>' . sprintf(
+				esc_html__(
+					'To import Facebook events, please be sure to add your Facebook API key and secret on %1$sEvents > Settings > APIs%2$s',
+					'the-events-calendar'
+				),
+				'<a href="' . admin_url( Tribe__Settings::$parent_page . '&page=tribe-common&tab=addons' ) . '">',
+				'</a>'
+			). '</p>',
+		),
+		'tribe_aggregator_default_facebook_post_status' => array(
+			'type' => 'dropdown',
+			'label' => esc_html__( 'Default Status', 'the-events-calendar' ),
+			'tooltip' => esc_html__( 'The default post status for events imported via Facebook', 'the-events-calendar' ),
+			'size' => 'medium',
+			'validation_type' => 'options',
+			'default' => '',
+			'can_be_empty' => true,
+			'parent_option' => Tribe__Events__Main::OPTIONNAME,
+			'options' => $origin_post_statuses,
+		),
+		'tribe_aggregator_default_facebook_category' => array(
+			'type' => 'dropdown',
+			'label' => esc_html__( 'Default Event Category', 'the-events-calendar' ),
+			'tooltip' => esc_html__( 'The default event category for events imported via Facebook', 'the-events-calendar' ),
+			'size' => 'medium',
+			'validation_type' => 'options',
+			'default' => '',
+			'can_be_empty' => true,
+			'parent_option' => Tribe__Events__Main::OPTIONNAME,
+			'options' => $origin_categories,
+		),
+		'tribe_aggregator_default_facebook_show_map' => array(
+			'type' => 'dropdown',
+			'label' => esc_html__( 'Show Google Map', 'the-events-calendar' ),
+			'tooltip' => esc_html__( 'Show Google Map by default on imported venues', 'the-events-calendar' ),
+			'size' => 'medium',
+			'validation_type' => 'options',
+			'default' => 'no',
+			'can_be_empty' => true,
+			'parent_option' => Tribe__Events__Main::OPTIONNAME,
+			'options' => $origin_show_map_options,
+		),
+	);
+
+	$gcal = array(
+		'gcal-defaults' => array(
+			'type' => 'html',
+			'html' => '<h3>' . esc_html__( 'Google Calendar Import Settings', 'the-events-calendar' ) . '</h3>',
+		),
+		'tribe_aggregator_default_gcal_post_status' => array(
+			'type' => 'dropdown',
+			'label' => esc_html__( 'Default Status', 'the-events-calendar' ),
+			'tooltip' => esc_html__( 'The default post status for events imported via Google Calendar', 'the-events-calendar' ),
+			'size' => 'medium',
+			'validation_type' => 'options',
+			'default' => '',
+			'can_be_empty' => true,
+			'parent_option' => Tribe__Events__Main::OPTIONNAME,
+			'options' => $origin_post_statuses,
+		),
+		'tribe_aggregator_default_gcal_category' => array(
+			'type' => 'dropdown',
+			'label' => esc_html__( 'Default Event Category', 'the-events-calendar' ),
+			'tooltip' => esc_html__( 'The default event category for events imported via Google Calendar', 'the-events-calendar' ),
+			'size' => 'medium',
+			'validation_type' => 'options',
+			'default' => '',
+			'can_be_empty' => true,
+			'parent_option' => Tribe__Events__Main::OPTIONNAME,
+			'options' => $origin_categories,
+		),
+		'tribe_aggregator_default_gcal_show_map' => array(
+			'type' => 'dropdown',
+			'label' => esc_html__( 'Show Google Map', 'the-events-calendar' ),
+			'tooltip' => esc_html__( 'Show Google Map by default on imported venues', 'the-events-calendar' ),
+			'size' => 'medium',
+			'validation_type' => 'options',
+			'default' => 'no',
+			'can_be_empty' => true,
+			'parent_option' => Tribe__Events__Main::OPTIONNAME,
+			'options' => $origin_show_map_options,
+		),
+	);
+
+	$meetup = array(
+		'meetup-defaults' => array(
+			'type' => 'html',
+			'html' => '<h3>' . esc_html__( 'Meetup Import Settings', 'the-events-calendar' ) . '</h3>',
+		),
+		'meetup-defaults-info' => array(
+			'type' => 'html',
+			'html' => '<p>' . sprintf(
+				esc_html__(
+					'To import Meetup events, please be sure to add your Meetup API key on %1$sEvents > Settings > APIs%2$s',
+					'the-events-calendar'
+				),
+				'<a href="' . admin_url( Tribe__Settings::$parent_page . '&page=tribe-common&tab=addons' ) . '">',
+				'</a>'
+			). '</p>',
+		),
+		'tribe_aggregator_default_meetup_post_status' => array(
+			'type' => 'dropdown',
+			'label' => esc_html__( 'Default Status', 'the-events-calendar' ),
+			'tooltip' => esc_html__( 'The default post status for events imported via Meetup', 'the-events-calendar' ),
+			'size' => 'medium',
+			'validation_type' => 'options',
+			'default' => '',
+			'can_be_empty' => true,
+			'parent_option' => Tribe__Events__Main::OPTIONNAME,
+			'options' => $origin_post_statuses,
+		),
+		'tribe_aggregator_default_meetup_category' => array(
+			'type' => 'dropdown',
+			'label' => esc_html__( 'Default Event Category', 'the-events-calendar' ),
+			'tooltip' => esc_html__( 'The default event category for events imported via Meetup', 'the-events-calendar' ),
+			'size' => 'medium',
+			'validation_type' => 'options',
+			'default' => '',
+			'can_be_empty' => true,
+			'parent_option' => Tribe__Events__Main::OPTIONNAME,
+			'options' => $origin_categories,
+		),
+		'tribe_aggregator_default_meetup_show_map' => array(
+			'type' => 'dropdown',
+			'label' => esc_html__( 'Show Google Map', 'the-events-calendar' ),
+			'tooltip' => esc_html__( 'Show Google Map by default on imported venues', 'the-events-calendar' ),
+			'size' => 'medium',
+			'validation_type' => 'options',
+			'default' => 'no',
+			'can_be_empty' => true,
+			'parent_option' => Tribe__Events__Main::OPTIONNAME,
+			'options' => $origin_show_map_options,
+		),
+	);
+}
+
+$internal = array_merge(
+	$change_authority,
+	$global,
+	$csv,
+	$ical,
+	$facebook,
+	$gcal,
+	$meetup
 );
 
 $internal = apply_filters( 'tribe_aggregator_fields', $internal );
