5d4079ea2044daea3f85e2b0ecab78c810cb9b5a
Zach Tirrell <zbtirrell@gmail.com>
2016-08-24 13:42:54 -0400

    crank up the timeout when talking to Event Aggregator
    This is important for slow connections and big file transfers

--- src/Tribe/Aggregator/Tabs/New.php
+++ src/Tribe/Aggregator/Tabs/New.php
@@ -329,6 +329,10 @@ class Tribe__Events__Aggregator__Tabs__New extends Tribe__Events__Aggregator__Ta
 
 		$result = $record->get_import_data();
 
+		if ( is_wp_error( $result ) ) {
+			wp_send_json_error( $result );
+		}
+
 		// if we've received a source name, let's set that in the record as soon as possible
 		if ( ! empty( $result->data->source_name ) ) {
 			$record->update_meta( 'source_name', $result->data->source_name );
