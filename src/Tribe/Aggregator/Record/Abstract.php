155aa06ed966f4e8f8b96cbbaa3d4c2baddd6647
Matthew Batchelder <borkweb@gmail.com>
2016-08-26 08:42:46 -0400

    Move ignored post query to the Ignored_Events class

--- src/Tribe/Aggregator/Record/Abstract.php
+++ src/Tribe/Aggregator/Record/Abstract.php
@@ -894,7 +894,7 @@ abstract class Tribe__Events__Aggregator__Record__Abstract {
 
 		$unique_field = $this->get_unique_field();
 		$existing_ids = $this->get_existing_ids_from_import_data( $items );
-		$ignored_ids  = $this->get_ignored_ids( $existing_ids );
+		$ignored_ids  = Tribe__Events__Ignored_Events::instance()->get_ignored_posts_by_id( array( 'fields' => 'ids' ), $existing_ids );
 
 		$count_scanned_events = 0;
 
@@ -1136,36 +1136,6 @@ abstract class Tribe__Events__Aggregator__Record__Abstract {
 		$existing_ids = $event_object->get_existing_ids( $this->meta['origin'], $selected_ids );
 
 		return $existing_ids;
-	}
-
-	/**
-	 * Gets all ids for events that have been ignored
-	 *
-	 * @param array $check Array of event IDs to check
-	 *
-	 * @return array
-	 */
-	protected function get_ignored_ids( $check ) {
-		if ( empty( $check ) ) {
-			return array();
-		}
-
-		$first = reset( $check );
-
-		if ( ! is_numeric( $first ) ) {
-			$check = wp_list_pluck( $check, 'post_id' );
-		}
-
-		$args = array(
-			'fields' => 'ids',
-			'post_type' => Tribe__Events__Main::POSTTYPE,
-			'post_status' => Tribe__Events__Ignored_Events::$ignored_status,
-			'post__in' => $check,
-		);
-
-		$query = new WP_Query( $args );
-
-		return $query->posts;
 	}
 
 	protected function filter_data_by_selected( $import_data ) {
