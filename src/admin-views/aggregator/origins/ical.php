a563e03e4610401a8a17a7a395ae774c8f8db38a
Matthew Batchelder <borkweb@gmail.com>
2016-09-01 08:47:55 -0400

    Remove the data preview when the Import Type changes
    See: Findings doc

--- src/admin-views/aggregator/origins/ical.php
+++ src/admin-views/aggregator/origins/ical.php
@@ -26,7 +26,7 @@ $frequencies = $cron->get_frequency();
 			<select
 				name="aggregator[ical][import_type]"
 				id="tribe-ea-field-ical_import_type"
-				class="tribe-ea-field tribe-ea-dropdown tribe-ea-size-large"
+				class="tribe-ea-field tribe-ea-dropdown tribe-ea-size-large tribe-import-type"
 				placeholder="<?php echo esc_attr( $field->placeholder ); ?>"
 				data-hide-search
 				data-prevent-clear
