ebf03f407f3c55c364bc521290c1dd531e5fa0df
Matthew Batchelder <borkweb@gmail.com>
2016-09-04 11:04:07 -0400

    Versioning beta

--- src/Tribe/Main.php
+++ src/Tribe/Main.php
@@ -32,8 +32,8 @@ if ( ! class_exists( 'Tribe__Events__Main' ) ) {
 		const VENUE_POST_TYPE     = 'tribe_venue';
 		const ORGANIZER_POST_TYPE = 'tribe_organizer';
 
-		const VERSION           = '4.3dev';
-		const MIN_ADDON_VERSION = '4.3dev';
+		const VERSION           = '4.3beta1';
+		const MIN_ADDON_VERSION = '4.3beta1';
 		const WP_PLUGIN_URL     = 'http://wordpress.org/extend/plugins/the-events-calendar/';
 
 		/**
