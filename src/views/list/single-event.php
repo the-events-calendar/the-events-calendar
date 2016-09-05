064760154629c12c7a09dedec1190400b0a089c5
bordoni <bordoni.dev@gmail.com>
2016-09-05 04:34:58 -0300

    We need to change these Checks, to be a little bit smarter for Views

--- src/views/list/single-event.php
+++ src/views/list/single-event.php
@@ -6,6 +6,7 @@
  * Override this template in your own theme by creating a file at [your-theme]/tribe-events/list/single-event.php
  *
  * @package TribeEventsCalendar
+ * @version  4.3beta1
  *
  */
 if ( ! defined( 'ABSPATH' ) ) {
