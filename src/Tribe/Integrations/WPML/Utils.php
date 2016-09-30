7ed2431f5b218607297204271c01f312b0598838
Luca Tumedei <luca@theaveragedev.com>
2016-09-30 12:27:45 +0200

    fixed odd spacing

--- src/Tribe/Integrations/WPML/Utils.php
+++ src/Tribe/Integrations/WPML/Utils.php
@@ -18,11 +18,11 @@ class Tribe__Events__Integrations__WPML__Utils {
 	public static function get_wpml_i18n_strings( array $strings ) {
 		$tec = Tribe__Events__Main::instance();
 
-		$domains = apply_filters(
+		$domains = apply_filters (
 			'tribe_events_rewrite_i18n_domains', array(
-			'default'             => true, // Default doesn't need file path
-			'the-events-calendar' => $tec->pluginDir . 'lang/',
-		)
+				'default'             => true, // Default doesn't need file path
+				'the-events-calendar' => $tec->pluginDir . 'lang/',
+			)
 		);
 
 		global $sitepress;
