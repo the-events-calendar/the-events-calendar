5f4a638b4a44ede985ef9dabeda1b5e0954e8a60
Luca Tumedei <luca@theaveragedev.com>
2016-10-04 12:01:29 +0200

    removed lingering todos

--- src/Tribe/Main.php
+++ src/Tribe/Main.php
@@ -2563,7 +2563,6 @@ if ( ! class_exists( 'Tribe__Events__Main' ) ) {
 				$event_url = str_replace( '?' . $url_query, '', $event_url );
 			}
 
-			// @todo @luca: allow filtering here to translate
 			// Append Events structure
 			$event_url .= trailingslashit( sanitize_title( Tribe__Settings_Manager::get_option( 'eventsSlug', 'events' ) ) );
 
