0560eab9cdd9e1883f3bdad58c76fb262931600d
Luca Tumedei <luca@theaveragedev.com>
2016-10-07 13:41:43 +0200

    handle more language negotiations types for linked post permalinks

--- src/Tribe/Integrations/WPML/Permalinks.php
+++ src/Tribe/Integrations/WPML/Permalinks.php
@@ -42,7 +42,7 @@ class Tribe__Events__Integrations__WPML__Permalinks {
 	}
 
 	/**
-	 * Filters the post type link to remove WPML language query arg on non translated posts.
+	 * Filters the post type link to remove WPML language query arg/frags on non translated posts.
 	 *
 	 * @param string  $post_link The post's permalink.
 	 * @param WP_Post $post      The post in question.
@@ -55,10 +55,71 @@ class Tribe__Events__Integrations__WPML__Permalinks {
 		/** @var SitePress $sitepress */
 		global $sitepress;
 
+		// append the language as a query argument
+		$language_negotiation_type = $sitepress->get_setting( 'language_negotiation_type' );
+
+		switch ( $language_negotiation_type ) {
+			case 1;
+			case 2;
+				$post_link = $this->get_post_permalink( $post );
+				break;
+			case 3:
+				$post_link = $this->update_language_query_arg( $post_link, $post );
+				break;
+			default:
+				break;
+		}
+
+		return $post_link;
+	}
+
+	/**
+	 * Returns the post link withe the language query arg removed or updated to the post language.
+	 *
+	 * @param         $post_link
+	 * @param WP_Post $post
+	 *
+	 * @return string
+	 */
+	protected function update_language_query_arg( $post_link, WP_Post $post, $sitepress ) {
+		/** @var SitePress $sitepress */
+		global $sitepress;
+
 		$post_language = $sitepress->get_language_for_element( $post->ID, 'post_' . $post->post_type );
 		if ( $post_language !== ICL_LANGUAGE_CODE ) {
-			$post_link = remove_query_arg( 'lang', $post_link );
+			if ( $post_language === $sitepress->get_default_language() ) {
+				$post_link = remove_query_arg( 'lang', $post_link );
+
+				return $post_link;
+			} else {
+				$post_link = remove_query_arg( 'lang', $post_link );
+				$post_link = add_query_arg( array( 'lang' => $post_language ), $post_link );
+
+				return $post_link;
+			}
 		}
+
+		return $post_link;
+	}
+
+	/**
+	 * @param WP_Post $post
+	 *
+	 * @return false|string
+	 */
+	protected function get_post_permalink( WP_Post $post ) {
+		/** @var SitePress $sitepress */
+		global $sitepress;
+
+		$post_language = $sitepress->get_language_for_element( $post->ID, 'post_' . $post->post_type );
+
+		$sitepress->switch_lang( $post_language );
+		remove_filter( 'post_type_link', array( $this, 'filter_post_type_link' ), 20 );
+
+		$post_link = get_permalink( $post->ID );
+
+		add_filter( 'post_type_link', array( $this, 'filter_post_type_link' ), 20, 2 );
+		$sitepress->switch_lang( ICL_LANGUAGE_CODE );
 
 		return $post_link;
 	}
