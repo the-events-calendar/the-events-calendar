6a7b27ab042b1d7988ab0ea7d2c99f097ca55a9f
theAverageDev (Luca Tumedei) <luca@theaveragedev.com>
2016-10-10 19:12:12 +0200

    take slug translation of post slug into account (#1045)
    * take slug translation of post slug into account
    
    * leaner check
    
    * refactor some common code to Utils
    
    * update var name

--- src/Tribe/Integrations/WPML/Rewrites.php
+++ src/Tribe/Integrations/WPML/Rewrites.php
@@ -13,11 +13,11 @@ class Tribe__Events__Integrations__WPML__Rewrites {
 	 */
 	protected static $instance;
 	/**
-	 * @var string The English version of the venue slug
+	 * @var string The English version of the venue slug.
 	 */
 	protected $venue_slug = 'venue';
 	/**
-	 * @var string The English version of the organizer slug
+	 * @var string The English version of the organizer slug.
 	 */
 	protected $organizer_slug = 'organizer';
 	/**
@@ -50,6 +50,14 @@ class Tribe__Events__Integrations__WPML__Rewrites {
 		return self::$instance;
 	}
 
+	/**
+	 * Filters the rewrite rules array to add support for translated versions of
+	 * venue and organizer slugs in their rules.
+	 *
+	 * @param array $rewrite_rules
+	 *
+	 * @return array
+	 */
 	public function filter_rewrite_rules_array( array $rewrite_rules ) {
 		$this->prepare_venue_slug_translations();
 		$this->prepare_organizer_slug_translations();
@@ -62,14 +70,16 @@ class Tribe__Events__Integrations__WPML__Rewrites {
 
 	protected function prepare_venue_slug_translations() {
 		$wpml_i18n_strings             = Tribe__Events__Integrations__WPML__Utils::get_wpml_i18n_strings( array( $this->venue_slug ) );
-		$candidate_slug_translations   = $wpml_i18n_strings[0];
-		$this->venue_slug_translations = array_map( 'esc_attr', $candidate_slug_translations );
+		$post_slug_translations        = Tribe__Events__Integrations__WPML__Utils::get_post_slug_translations_for( Tribe__Events__Venue::POSTTYPE );
+		$slug_translations             = array_merge( $wpml_i18n_strings[0], array_values( $post_slug_translations ) );
+		$this->venue_slug_translations = array_map( 'esc_attr', array_unique( $slug_translations ) );
 	}
 
 	protected function prepare_organizer_slug_translations() {
 		$wpml_i18n_strings                 = Tribe__Events__Integrations__WPML__Utils::get_wpml_i18n_strings( array( $this->organizer_slug ) );
-		$candidate_slug_translations       = $wpml_i18n_strings[0];
-		$this->organizer_slug_translations = array_map( 'esc_attr', $candidate_slug_translations );
+		$post_slug_translations            = Tribe__Events__Integrations__WPML__Utils::get_post_slug_translations_for( Tribe__Events__Organizer::POSTTYPE );
+		$slug_translations                 = array_merge( $wpml_i18n_strings[0], array_values( $post_slug_translations ) );
+		$this->organizer_slug_translations = array_map( 'esc_attr', array_unique( $slug_translations ) );
 	}
 
 	protected function replace_rules_with_translations( array $rewrite_rules ) {
@@ -87,6 +97,16 @@ class Tribe__Events__Integrations__WPML__Rewrites {
 		return array_combine( $replaced_keys, $values );
 	}
 
+	/**
+	 * Adds support for the translated version of the venue slug in all venues rewrite
+	 * rules regular expressions.
+	 *
+	 * E.g. `venue/then-some` becomes `(?:venue|luogo|lieu)/then-some`; the match uses
+	 * non-capturing groups not to mess up the match keys.
+	 *
+	 * @param string $rule  A rewrite rule scheme assigning pattern matches to vars.
+	 * @param string $regex A rewrite rule regular expression.
+	 */
 	public function translate_venue_rules( $rule, $regex ) {
 		if ( ! $this->is_venue_rule( $regex ) ) {
 			return;
@@ -103,6 +123,16 @@ class Tribe__Events__Integrations__WPML__Rewrites {
 		return preg_match( '/^' . $this->venue_slug . '/', $candidate_rule );
 	}
 
+	/**
+	 * Adds support for the translated version of the organizer slug in all organizers rewrite
+	 * rules regular expressions.
+	 *
+	 * E.g. `organizer/then-some` becomes `(?:organizer|organizzatore|organisateur)/then-some`;
+	 * the match uses non-capturing groups not to mess up the match keys.
+	 *
+	 * @param string $rule  A rewrite rule scheme assigning pattern matches to vars.
+	 * @param string $regex A rewrite rule regular expression.
+	 */
 	public function translate_organizer_rules( $rule, $regex ) {
 		if ( ! $this->is_organizer_rule( $regex ) ) {
 			return;
