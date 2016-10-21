84d73c9bb07ead9adef90091985649bde797c24c
bordoni <bordoni.dev@gmail.com>
2016-10-21 16:24:30 -0200

    Bump version to 4.3.2

--- src/Tribe/Main.php
+++ src/Tribe/Main.php
@@ -32,7 +32,7 @@ if ( ! class_exists( 'Tribe__Events__Main' ) ) {
 		const VENUE_POST_TYPE     = 'tribe_venue';
 		const ORGANIZER_POST_TYPE = 'tribe_organizer';
 
-		const VERSION           = '4.3.1.1';
+		const VERSION           = '4.3.2';
 		const MIN_ADDON_VERSION = '4.3';
 		const WP_PLUGIN_URL     = 'http://wordpress.org/extend/plugins/the-events-calendar/';
 
