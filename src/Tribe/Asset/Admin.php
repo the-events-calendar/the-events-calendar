a4a4f916f28bcbde7f5772965b5cc059c1d39244
Brian Jessee <brian@jesseeproductions.com>
2016-10-28 12:10:23 -0400

    #66484 - more tab stuff

--- src/Tribe/Asset/Admin.php
+++ src/Tribe/Asset/Admin.php
@@ -5,15 +5,15 @@ class Tribe__Events__Asset__Admin extends Tribe__Events__Asset__Abstract_Asset {
 
 	public function handle() {
 		$deps = array_merge(
-					$this->deps,
-					array(
-						'jquery',
-						'jquery-ui-datepicker',
-						'jquery-ui-sortable',
-						'tribe-bumpdown',
-						'underscore',
-						'wp-util',
-					)
+			$this->deps,
+			array(
+				'jquery',
+				'jquery-ui-datepicker',
+				'jquery-ui-sortable',
+				'tribe-bumpdown',
+				'underscore',
+				'wp-util',
+			)
 		);
 
 		$path = Tribe__Events__Template_Factory::getMinFile( tribe_events_resource_url( 'events-admin.js' ), true );
