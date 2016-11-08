17ee21783990766865443a2eb677d7dfdc37a725
Matthew Batchelder <borkweb@gmail.com>
2016-11-08 15:45:58 -0500

    Styling featured events for List View
    See: https://central.tri.be/issues/65918

--- src/views/list/loop.php
+++ src/views/list/loop.php
@@ -35,7 +35,18 @@ $more = false;
 		}
 		?>
 		<div id="post-<?php the_ID() ?>" class="<?php tribe_events_event_classes() ?>" <?php echo $post_parent; ?>>
-			<?php tribe_get_template_part( 'list/single', 'event' ) ?>
+			<?php
+			$event_type = tribe( 'tec.featured_events' )->is_featured( $post->ID ) ? 'featured' : 'event';
+
+			/**
+			 * Filters the event type used when selecting a template to render
+			 *
+			 * @param $event_type
+			 */
+			$event_type = apply_filters( 'tribe_events_list_view_event_type', $event_type );
+
+			tribe_get_template_part( 'list/single', $event_type );
+			?>
 		</div>
 
 
