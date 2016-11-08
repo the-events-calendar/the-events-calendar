99726770cae9ab5457960d2839740dcd829860fa
Matthew Batchelder <borkweb@gmail.com>
2016-11-08 15:54:37 -0500

    Add featured event styles for Day View
    NOTE: this PR builds off of #1124 and 1127 and should be reviewed after those have been reviewed/merged.
    
    See: https://central.tri.be/issues/65922

--- src/views/day/loop.php
+++ src/views/day/loop.php
@@ -38,7 +38,18 @@ $current_timeslot = null;
 
 		<!-- Event  -->
 		<div id="post-<?php the_ID() ?>" class="<?php tribe_events_event_classes() ?>">
-			<?php tribe_get_template_part( 'day/single', 'event' ) ?>
+			<?php
+			$event_type = tribe( 'tec.featured_events' )->is_featured( $post->ID ) ? 'featured' : 'event';
+
+			/**
+			 * Filters the event type used when selecting a template to render
+			 *
+			 * @param $event_type
+			 */
+			$event_type = apply_filters( 'tribe_events_day_view_event_type', $event_type );
+
+			tribe_get_template_part( 'day/single', $event_type );
+			?>
 		</div>
 
 		<?php do_action( 'tribe_events_inside_after_loop' ); ?>
