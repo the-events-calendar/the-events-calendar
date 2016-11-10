07e995f1e76913fbcb4f039b078bb30d047a6c96
bordoni <bordoni.dev@gmail.com>
2016-11-10 04:21:40 -0200

    Add a good array_merge to prevent overwriting

--- src/Tribe/Query.php
+++ src/Tribe/Query.php
@@ -348,6 +348,13 @@ if ( ! class_exists( 'Tribe__Events__Query' ) ) {
 			if ( $query->tribe_is_event_query && $query->get( 'hide_upcoming' ) && ! $query->get( 'suppress_filters' ) ) {
 				$hide_upcoming_ids = self::getHideFromUpcomingEvents();
 				if ( ! empty( $hide_upcoming_ids ) ) {
+					$post__not_in = $query->get( 'post__not_in' );
+
+					// Merge if there is any items
+					if ( is_array( $post__not_in ) ) {
+						$hide_upcoming_ids = array_merge( $hide_upcoming_ids, $post__not_in );
+					}
+
 					$query->set( 'post__not_in', $hide_upcoming_ids );
 				}
 			}
@@ -409,7 +416,7 @@ if ( ! class_exists( 'Tribe__Events__Query' ) ) {
 			// and event query on the tribe_events edit page
 			return ( is_admin()
 						&& $query->tribe_is_event_query
-						&& Tribe__Admin__Helpers::instance()->is_screen( 'edit-' . Tribe__Events__Main::POSTTYPE ) ) 
+						&& Tribe__Admin__Helpers::instance()->is_screen( 'edit-' . Tribe__Events__Main::POSTTYPE ) )
 			       || true === $query->get( 'tribe_remove_date_filters', false );
 		}
 
@@ -527,7 +534,7 @@ if ( ! class_exists( 'Tribe__Events__Query' ) ) {
 		 */
 		public static function posts_where( $where_sql, $query ) {
 			global $wpdb;
-			
+
 			// if it's a true event query then we to setup where conditions
 			// but only if we aren't grabbing a specific post
 			if (
