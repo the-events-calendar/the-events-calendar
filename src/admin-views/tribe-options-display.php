290d4c4357fce85b22398898f70337c06f4d1830
bordoni <bordoni.dev@gmail.com>
2016-11-10 05:06:04 -0200

    Fix the settings page Domain to Core

--- src/admin-views/tribe-options-display.php
+++ src/admin-views/tribe-options-display.php
@@ -64,16 +64,16 @@ $display_tab_fields = Tribe__Main::array_insert_before_key(
 	array(
 		'dateWithYearFormat'                 => array(
 			'type'            => 'text',
-			'label'           => esc_html__( 'Date with year', 'tribe-common' ),
-			'tooltip'         => esc_html__( 'Enter the format to use for displaying dates with the year. Used when displaying a date in a future year.', 'tribe-common' ),
+			'label'           => esc_html__( 'Date with year', 'the-events-calendar' ),
+			'tooltip'         => esc_html__( 'Enter the format to use for displaying dates with the year. Used when displaying a date in a future year.', 'the-events-calendar' ),
 			'default'         => get_option( 'date_format' ),
 			'size'            => 'medium',
 			'validation_type' => 'html',
 		),
 		'dateTimeSeparator'                  => array(
 			'type'            => 'text',
-			'label'           => esc_html__( 'Date time separator', 'tribe-common' ),
-			'tooltip'         => esc_html__( 'Enter the separator that will be placed between the date and time, when both are shown.', 'tribe-common' ),
+			'label'           => esc_html__( 'Date time separator', 'the-events-calendar' ),
+			'tooltip'         => esc_html__( 'Enter the separator that will be placed between the date and time, when both are shown.', 'the-events-calendar' ),
 			'default'         => ' @ ',
 			'size'            => 'small',
 			'validation_type' => 'html',
