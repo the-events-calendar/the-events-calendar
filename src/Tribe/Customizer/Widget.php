0f2cd7369f22ab933c9dd801a3547b1ca3187125
Matthew Batchelder <borkweb@gmail.com>
2016-11-28 08:34:13 -0500

    Drive Featured Event colors with two Customizer options
    See: https://central.tri.be/issues/69755

--- src/Tribe/Customizer/Widget.php
+++ src/Tribe/Customizer/Widget.php
@@ -32,50 +32,6 @@ final class Tribe__Events__Customizer__Widget extends Tribe__Customizer__Section
 	public function get_css_template( $template ) {
 		$customizer = Tribe__Customizer::instance();
 
-		if ( $customizer->has_option( $this->ID, 'featured_bg_color' ) ) {
-			$template .= '
-				.tribe-events-list-widget .tribe-event-featured,
-				.tribe-events-venue-widget .tribe-event-featured,
-				.tribe-mini-calendar-list-wrapper .tribe-event-featured,
-				.tribe-events-adv-list-widget .tribe-event-featured {
-					background-color: <%= widget.featured_bg_color %>;
-				}
-			';
-		}
-
-		if ( $customizer->has_option( $this->ID, 'featured_title_color' ) ) {
-			$template .= '
-				.tribe-events-list-widget .tribe-event-featured a,
-				.tribe-events-venue-widget .tribe-event-featured a,
-				.tribe-mini-calendar-list-wrapper .tribe-event-featured .tribe-events-title a,
-				.tribe-events-adv-list-widget .tribe-event-featured .tribe-events-title a {
-					color: <%= widget.featured_title_color %>;
-				}
-
-				.tribe-events-list-widget .tribe-event-featured .tribe-events-title a:active,
-				.tribe-events-list-widget .tribe-event-featured .tribe-events-title a:hover,
-				.tribe-events-venue-widget .tribe-event-featured .tribe-events-title a:active,
-				.tribe-events-venue-widget .tribe-event-featured .tribe-events-title a:hover,
-				.tribe-mini-calendar-list-wrapper .tribe-event-featured .tribe-events-title a:active,
-				.tribe-mini-calendar-list-wrapper .tribe-event-featured .tribe-events-title a:hover,
-				.tribe-events-adv-list-widget .tribe-event-featured .tribe-events-title a:active,
-				.tribe-events-adv-list-widget .tribe-event-featured .tribe-events-title a:hover {
-					color: <%= widget.featured_title_color_active %>;
-				}
-			';
-		}
-
-		if ( $customizer->has_option( $this->ID, 'featured_text_color' ) ) {
-			$template .= '
-				.tribe-events-list-widget .tribe-event-featured,
-				.tribe-events-venue-widget .tribe-event-featured,
-				.tribe-mini-calendar-list-wrapper .tribe-event-featured,
-				.tribe-events-adv-list-widget .tribe-event-featured {
-					color: <%= widget.featured_text_color %>;
-				}
-			';
-		}
-
 		if ( ! $customizer->get_option( array( 'widget', 'featured_show_images' ) ) ) {
 			$template .= '
 				.tribe-events-list-widget .tribe-event-featured .tribe-event-image,
@@ -88,30 +44,6 @@ final class Tribe__Events__Customizer__Widget extends Tribe__Customizer__Section
 		}
 
 		return $template;
-	}
-
-	public function create_ghost_settings( $settings = array() ) {
-
-		if ( ! empty( $settings['featured_bg_color'] ) ) {
-			$featured_bg_color = new Tribe__Utils__Color( $settings['featured_bg_color'] );
-
-			$settings['featured_bg_color'] = '#' . $featured_bg_color->getHex();
-		}
-
-		if ( ! empty( $settings['featured_title_color'] ) ) {
-			$featured_title_color = new Tribe__Utils__Color( $settings['featured_title_color'] );
-
-			$settings['featured_title_color'] = '#' . $featured_title_color->getHex();
-			$settings['featured_title_color_active'] = '#' . $featured_title_color->lighten( 8 );
-		}
-
-		if ( ! empty( $settings['featured_text_color'] ) ) {
-			$featured_text_color = new Tribe__Utils__Color( $settings['featured_text_color'] );
-
-			$settings['featured_text_color'] = '#' . $featured_text_color->getHex();
-		}
-
-		return $settings;
 	}
 
 	public function setup() {
@@ -185,72 +117,6 @@ final class Tribe__Events__Customizer__Widget extends Tribe__Customizer__Section
 		);
 
 		$manager->add_setting(
-			$customizer->get_setting_name( 'featured_bg_color', $section ),
-			array(
-				'default'              => $this->get_default( 'featured_bg_color' ),
-				'type'                 => 'option',
-
-				'sanitize_callback'    => 'sanitize_hex_color',
-				'sanitize_js_callback' => 'maybe_hash_hex_color',
-			)
-		);
-
-		$manager->add_control(
-			new WP_Customize_Color_Control(
-				$manager,
-				$customizer->get_setting_name( 'featured_bg_color', $section ),
-				array(
-					'label'   => __( 'Featured Background Color' ),
-					'section' => $section->id,
-				)
-			)
-		);
-
-		$manager->add_setting(
-			$customizer->get_setting_name( 'featured_title_color', $section ),
-			array(
-				'default'              => $this->get_default( 'featured_title_color' ),
-				'type'                 => 'option',
-
-				'sanitize_callback'    => 'sanitize_hex_color',
-				'sanitize_js_callback' => 'maybe_hash_hex_color',
-			)
-		);
-
-		$manager->add_control(
-			new WP_Customize_Color_Control(
-				$manager,
-				$customizer->get_setting_name( 'featured_title_color', $section ),
-				array(
-					'label'   => __( 'Featured Title Color' ),
-					'section' => $section->id,
-				)
-			)
-		);
-
-		$manager->add_setting(
-			$customizer->get_setting_name( 'featured_text_color', $section ),
-			array(
-				'default'              => $this->get_default( 'featured_text_color' ),
-				'type'                 => 'option',
-
-				'sanitize_callback'    => 'sanitize_hex_color',
-				'sanitize_js_callback' => 'maybe_hash_hex_color',
-			)
-		);
-
-		$manager->add_control(
-			new WP_Customize_Color_Control(
-				$manager,
-				$customizer->get_setting_name( 'featured_text_color', $section ),
-				array(
-					'label'   => __( 'Featured Text Color' ),
-					'section' => $section->id,
-				)
-			)
-		);
-
-		$manager->add_setting(
 			$customizer->get_setting_name( 'featured_show_images', $section ),
 			array(
 				'default'              => $this->get_default( 'featured_show_images' ),
@@ -273,9 +139,6 @@ final class Tribe__Events__Customizer__Widget extends Tribe__Customizer__Section
 		// Introduced to make Selective Refresh have less code duplication
 		$customizer->add_setting_name( $customizer->get_setting_name( 'calendar_header_color', $section ) );
 		$customizer->add_setting_name( $customizer->get_setting_name( 'calendar_datebar_color', $section ) );
-		$customizer->add_setting_name( $customizer->get_setting_name( 'featured_bg_color', $section ) );
-		$customizer->add_setting_name( $customizer->get_setting_name( 'featured_title_color', $section ) );
-		$customizer->add_setting_name( $customizer->get_setting_name( 'featured_text_color', $section ) );
 		$customizer->add_setting_name( $customizer->get_setting_name( 'featured_show_images', $section ) );
 	}
 }
