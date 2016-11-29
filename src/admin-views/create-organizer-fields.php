1249167c046aa406b7b38b73f7e03059f5c55835
Trisha Salas <trisha@trishasalas.com>
2016-11-29 00:42:18 -0600

    fix indentation and tabs

--- src/admin-views/create-organizer-fields.php
+++ src/admin-views/create-organizer-fields.php
@@ -5,18 +5,17 @@
 	</td>
 </tr>
 <tr class="linked-post organizer">
-    <td><label for="organizer-website"><?php esc_html_e( 'Website:', 'the-events-calendar' ); ?></label></td>
+	<td><label for="organizer-website"><?php esc_html_e( 'Website:', 'the-events-calendar' ); ?></label></td>
 	<td>
 		<input id="organizer-website" type='text' name='organizer[Website][]' class='organizer-website' size='25' value='' />
 	</td>
 </tr>
 <tr class="linked-post organizer">
 	<td>
-        <label for="organizer-email"><?php esc_html_e( 'Email:', 'the-events-calendar' ); ?></label>
+		<label for="organizer-email"><?php esc_html_e( 'Email:', 'the-events-calendar' ); ?></label>
 	</td>
 	<td class="organizer-email">
 		<input id="organizer-email" type='text' name='organizer[Email][]' class='organizer-email' size='25' value='' />
-        <small><?php esc_html_e( 'The e-mail address will be obfuscated on your site to avoid it getting harvested by spammers.', 'the-events-calendar' ); ?></small>
+		<small><?php esc_html_e( 'The e-mail address will be obfuscated on your site to avoid it getting harvested by spammers.', 'the-events-calendar' ); ?></small>
 	</td>
-
 </tr>
