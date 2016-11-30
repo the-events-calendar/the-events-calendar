6e291c72e98c84ceb1acd2fb39efe48796df9f2d
Luca Tumedei <luca@theaveragedev.com>
2016-11-30 14:06:06 +0100

    rework the syntax to use built in methods

--- src/Tribe/Main.php
+++ src/Tribe/Main.php
@@ -2336,17 +2336,15 @@ if ( ! class_exists( 'Tribe__Events__Main' ) ) {
 				) {
 					$using_permalinks = get_option( 'permalink_structure' );
 
-					if($using_permalinks){
-						$this->displaying = isset( $wp_query->query_vars['eventDisplay'] ) ?
-							$wp_query->query_vars['eventDisplay']
-							: tribe_get_option( 'viewOption', 'list' );
+					if ( $using_permalinks ) {
+						$this->displaying = $wp_query->get( 'eventDisplay', tribe_get_option( 'viewOption', 'list' ) );
 					} else {
-						$event_display_arg = ! empty( $_GET['tribe_event_display'] ) ?
-							$_GET['tribe_event_display']
-							: tribe_get_option( 'viewOption', 'list' );
-						$this->displaying = isset( $wp_query->query_vars['eventDisplay'] ) ?
-							$wp_query->query_vars['eventDisplay']
-							: $event_display_arg;
+						$this->displaying = $wp_query->get( 'eventDisplay',false );
+						if ( false === $this->displaying ) {
+							$this->displaying = ! empty( $_GET['tribe_event_display'] ) ?
+								filter_var( $_GET['tribe_event_display'], FILTER_SANITIZE_STRING )
+								: tribe_get_option( 'viewOption', 'list' );
+						}
 					}
 
 
