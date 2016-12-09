b9141eafd78ca6beb0e3e9e9039759c1c166440d
Barry Hughes <barry@codingkills.me>
2016-12-09 22:31:12 +0000

    Add @version tags

--- src/views/month/tooltip.php
+++ src/views/month/tooltip.php
@@ -1,11 +1,9 @@
 <?php
-
 /**
- *
  * Please see single-event.php in this directory for detailed instructions on how to use and modify these templates.
  *
+ * @version 4.4
  */
-
 ?>
 
 <script type="text/html" id="tribe_tmpl_tooltip">
