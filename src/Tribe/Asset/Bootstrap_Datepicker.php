6888c3ae5da0d67f4aacc147157f005efba1e93f
bordoni <bordoni.dev@gmail.com>
2016-12-23 18:54:04 -0200

    Makes sure we don't have any notices on Bootstrap Datepicker

--- src/Tribe/Asset/Bootstrap_Datepicker.php
+++ src/Tribe/Asset/Bootstrap_Datepicker.php
@@ -7,17 +7,24 @@ class Tribe__Events__Asset__Bootstrap_Datepicker extends Tribe__Events__Asset__A
 		$path = Tribe__Events__Template_Factory::getMinFile( $this->vendor_url . 'bootstrap-datepicker/js/bootstrap-datepicker.min.js', true );
 		wp_enqueue_style( $this->prefix . '-bootstrap-datepicker-css', $css_path );
 
+		$months_full     = Tribe__Date_Utils::get_localized_months_full();
+		$months_short    = Tribe__Date_Utils::get_localized_months_short();
+		$days_week       = Tribe__Date_Utils::get_localized_weekdays_full();
+		$days_week_short = Tribe__Date_Utils::get_localized_months_short();
+		$days_week_min   = Tribe__Date_Utils::get_localized_weekdays_initial();
+
 		$handle = $this->prefix . '-bootstrap-datepicker';
-		wp_enqueue_script( $handle, $path, 'jquery', '3.2' );
+		wp_enqueue_script( $handle, $path, array( 'jquery' ), '3.2' );
 		Tribe__Events__Template_Factory::add_vendor_script( $handle );
+
 		$localized_datepicker_array = array(
-			'days' => array_merge( $this->tec->daysOfWeek, array( $this->tec->daysOfWeek[0] ) ),
-			'daysShort' => array_merge( $this->tec->daysOfWeekShort, array( $this->tec->daysOfWeekShort[0] ) ),
-			'daysMin' => array_merge( $this->tec->daysOfWeekMin, array( $this->tec->daysOfWeekMin[0] ) ),
-			'months' => array_values( $this->tec->monthsFull ),
-			'monthsShort' => array_values( $this->tec->monthsShort ),
-			'clear' => __( 'Clear', 'the-events-calendar' ),
-			'today' => __( 'Today', 'the-events-calendar' ),
+			'days'        => $days_week,
+			'daysShort'   => $days_week_short,
+			'daysMin'     => $days_week_min,
+			'months'      => array_values( $months_full ),
+			'monthsShort' => array_values( $months_short ),
+			'clear'       => esc_attr__( 'Clear', 'the-events-calendar' ),
+			'today'       => esc_attr__( 'Today', 'the-events-calendar' ),
 		);
 		wp_localize_script( $handle, 'tribe_bootstrap_datepicker_strings', array( 'dates' => $localized_datepicker_array ) );
 	}
