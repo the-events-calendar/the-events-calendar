6954f05efde03d61f2b7e621d13cb0769a023307
Luca Tumedei <luca@theaveragedev.com>
2017-01-02 09:18:57 +0100

    we really need a REST based suite...

---
+++ tests/restv1/DiscoveryCest.php
@@ -0,0 +1,18 @@
+<?php
+
+
+class DiscoveryCest
+{
+    public function _before(Restv1Tester $I)
+    {
+    }
+
+    public function _after(Restv1Tester $I)
+    {
+    }
+
+    // tests
+    public function tryToTest(Restv1Tester $I)
+    {
+    }
+}
