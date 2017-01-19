745885017b4367a493307294f0023a421b2f1ddc
Luca Tumedei <luca@theaveragedev.com>
2017-01-19 12:25:51 +0100

    add bad data ea message

--- src/Tribe/REST/V1/EA_Messages.php
+++ src/Tribe/REST/V1/EA_Messages.php
@@ -8,6 +8,7 @@ class Tribe__Events__REST__V1__EA_Messages extends Tribe__Events__REST__V1__Mess
 			'not-tec-rest-api-site' => __( 'The Events Calendar is not active or is not at least version 4.5 on the requested URL.', 'the-events-calendar' ),
 			'tec-rest-api-unsupported' => __( 'The requested URL does not support The Events Calendar REST API.', 'the-events-calendar' ),
 			'tec-rest-api-disabled' => __( 'The Events Calendar REST API is disabled on the requested URL.', 'the-events-calendar' ),
+			'tec-rest-api-bad-data' => __( 'The URL provided could be reached and has The Events Calendar REST API enabled but returned malformed data.', 'the-events-calendar' ),
 			'no_results' => __( 'The requested URL does not have any upcoming and published events.', 'the-events-calendar' ),
 		);
 	}
