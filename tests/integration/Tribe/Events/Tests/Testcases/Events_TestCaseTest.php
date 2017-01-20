efe8c3fd227156a4f2c634c118a2fef71032839d
Luca Tumedei <luca@theaveragedev.com>
2017-01-20 10:02:37 +0100

    add testing testcase

---
+++ tests/integration/Tribe/Events/Tests/Testcases/Events_TestCaseTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Tribe\Events\Tests\Testcases;
+
+class Events_TestCaseTest extends Events_TestCase {
+
+	public function setUp() {
+		// before
+		parent::setUp();
+
+		// your set up methods here
+	}
+
+	public function tearDown() {
+		// your tear down methods here
+
+		// then
+		parent::tearDown();
+	}
+
+	// tests
+	public function testMe() {
+	}
+
+}
