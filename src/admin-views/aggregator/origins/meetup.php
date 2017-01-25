fa726a742c0615d69f5b634c68ca60b0e63ab270
bordoni <bordoni.dev@gmail.com>
2017-01-24 22:08:50 -0200

    Add Schedule field template for required origins

--- src/admin-views/aggregator/origins/meetup.php
+++ src/admin-views/aggregator/origins/meetup.php
@@ -125,6 +125,8 @@ $missing_meetup_credentials = ! $meetup_api_key;
 	</td>
 </tr>
 
+<?php $this->template( 'fields/schedule', array( 'record' => $record, 'origin' => 'meetup', 'aggregator_action' => $aggregator_action ) ); ?>
+
 <?php
 $field              = (object) array();
 $field->label       = __( 'URL:', 'the-events-calendar' );
