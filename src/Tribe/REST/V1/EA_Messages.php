eb9ee9c86fece444c00b153e69461a5ebb333b5c
Luca Tumedei <luca@theaveragedev.com>
2017-02-21 13:33:27 +0100

    update the error messages

--- src/Tribe/REST/V1/EA_Messages.php
+++ src/Tribe/REST/V1/EA_Messages.php
@@ -7,12 +7,18 @@ class Tribe__Events__REST__V1__EA_Messages extends Tribe__Events__REST__V1__Mess
 		parent::__construct();
 
 		$ea_messages = array(
-			'not-tec-rest-api-site'    => __( 'The Events Calendar is not active or is not at least version 4.5 on the requested URL.', 'the-events-calendar' ),
-			'tec-rest-api-unsupported' => __( 'The requested URL does not support The Events Calendar REST API.', 'the-events-calendar' ),
-			'tec-rest-api-disabled'    => __( 'The Events Calendar REST API is disabled on the requested URL.', 'the-events-calendar' ),
-			'tec-rest-api-bad-data'    => __( 'The URL provided could be reached and has The Events Calendar REST API enabled but returned malformed data.', 'the-events-calendar' ),
-			'tec-rest-api-timeout'     => __( 'The URL provided could be reached and has The Events Calendar REST API enabled but took too long to respond.', 'the-events-calendar' ) . ' ' . sprintf( '<a href="%s" target="_blank">%s</a> ', esc_attr( admin_url( '/edit.php?post_type=tribe_events&page=tribe-common&tab=imports#tribe-field-tribe_aggregator_default_url_import_events_count' ) ), esc_html__( 'Please adjust your import amount and try again.', 'the-events-calendar' ) ),
-			'no-results'               => __( 'The requested URL does not have any upcoming and published events matching the search criteria.', 'the-events-calendar' ),
+			'not-tec-rest-api-site'              => __( 'The Events Calendar is not active or is not at least version 4.5 on the requested URL.', 'the-events-calendar' ),
+			'tec-rest-api-unsupported'           => __( 'The requested URL does not support The Events Calendar REST API.', 'the-events-calendar' ),
+			'tec-rest-api-disabled'              => __( 'The Events Calendar REST API is disabled on the requested URL.', 'the-events-calendar' ),
+			'tec-rest-api-bad-data'              => __( 'The URL provided could be reached and has The Events Calendar REST API enabled but returned malformed data.', 'the-events-calendar' ),
+			'tec-rest-api-archive-header-error'  => __( 'The URL provided could be reached and has The Events Calendar REST API enabled but there was an error while fetching the archive control data.', 'the-events-calendar' ),
+			'tec-rest-api-archive-missing-total' => __( 'The URL provided could be reached and has The Events Calendar REST API enabled but there was an error while fetching the total number of events.', 'the-events-calendar' ),
+			'tec-rest-api-archive-bad-total'     => __( 'The URL provided could be reached and has The Events Calendar REST API enabled but returned malformed data in regard to the total number of events.', 'the-events-calendar' ),
+			'tec-rest-api-archive-page-error'    => __( 'The URL provided could be reached and has The Events Calendar REST API enabled but there was an error while fetching an archive page.', 'the-events-calendar' ),
+			'tec-rest-api-archive-page-empty'    => __( 'The URL provided could be reached and has The Events Calendar REST API enabled but returned an empty archive page.', 'the-events-calendar' ),
+			'tec-rest-api-single-event-error'    => __( 'The URL provided could be reached and has The Events Calendar REST API enabled but there was an error while fetching the event data.', 'the-events-calendar' ),
+			'tec-rest-api-single-event-empty'    => __( 'The URL provided could be reached and has The Events Calendar REST API enabled but returned empty event data.', 'the-events-calendar' ),
+			'no-results'                         => __( 'The requested URL does not have any upcoming and published events matching the search criteria.', 'the-events-calendar' ),
 		);
 
 		$this->messages = array_merge( $this->messages, $ea_messages );
