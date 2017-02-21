26e4b5f0d9f70a75f834bdf9a4b41b0cdadc35cd
Luca Tumedei <luca@theaveragedev.com>
2017-02-21 13:46:16 +0100

    attach the link to any “adjustable” message

--- src/Tribe/REST/V1/EA_Messages.php
+++ src/Tribe/REST/V1/EA_Messages.php
@@ -1,9 +1,7 @@
 <?php
 
-class Tribe__Events__REST__V1__EA_Messages extends Tribe__Events__REST__V1__Messages implements Tribe__REST__Messages_Interface
-{
-	public function __construct()
-	{
+class Tribe__Events__REST__V1__EA_Messages extends Tribe__Events__REST__V1__Messages implements Tribe__REST__Messages_Interface {
+	public function __construct() {
 		parent::__construct();
 
 		$ea_messages = array(
@@ -20,6 +18,25 @@ class Tribe__Events__REST__V1__EA_Messages extends Tribe__Events__REST__V1__Mess
 			'tec-rest-api-single-event-empty'    => __( 'The URL provided could be reached and has The Events Calendar REST API enabled but returned empty event data.', 'the-events-calendar' ),
 			'no-results'                         => __( 'The requested URL does not have any upcoming and published events matching the search criteria.', 'the-events-calendar' ),
 		);
+
+		$adjustable_map = array(
+			'tec-rest-api-bad-data',
+			'tec-rest-api-archive-header-error',
+			'tec-rest-api-archive-missing-total',
+			'tec-rest-api-archive-bad-total',
+			'tec-rest-api-archive-page-error',
+			'tec-rest-api-archive-page-empty',
+			'tec-rest-api-single-event-error',
+			'tec-rest-api-single-event-empty',
+		);
+
+		$link = esc_attr( admin_url( '/edit.php?post_type=tribe_events&page=tribe-common&tab=imports#tribe-field-tribe_aggregator_default_url_import_events_count' ) );
+		$message = esc_html__( 'Please try to adjust your import amount and try again.', 'the-events-calendar' );
+		$adjust_message = sprintf( '<a href="%s" target="_blank">%s</a> ', $link, $message );
+
+		foreach ( $adjustable_map as $message_code ) {
+			$ea_messages[ $message_code ] .= ' ' . $adjust_message;
+		}
 
 		$this->messages = array_merge( $this->messages, $ea_messages );
 	}
