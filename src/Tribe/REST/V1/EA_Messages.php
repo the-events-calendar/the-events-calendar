80e6b0ec67ff2fc0bd25d26917cd3281de4e17eb
Luca Tumedei <luca@theaveragedev.com>
2017-02-24 09:28:36 +0100

    updated rest v1 ea client messages

--- src/Tribe/REST/V1/EA_Messages.php
+++ src/Tribe/REST/V1/EA_Messages.php
@@ -6,18 +6,40 @@ class Tribe__Events__REST__V1__EA_Messages extends Tribe__Events__REST__V1__Mess
 
 		$ea_messages = array(
 			'not-tec-rest-api-site'              => __( 'The Events Calendar is not active or is not at least version 4.5 on the requested URL.', 'the-events-calendar' ),
-			'tec-rest-api-unsupported'           => __( 'The requested URL does not support The Events Calendar REST API.', 'the-events-calendar' ),
-			'tec-rest-api-disabled'              => __( 'The Events Calendar REST API is disabled on the requested URL.', 'the-events-calendar' ),
-			'tec-rest-api-bad-data'              => __( 'The URL provided could be reached and has The Events Calendar REST API enabled but returned malformed data.', 'the-events-calendar' ),
-			'tec-rest-api-archive-header-error'  => __( 'The URL provided could be reached and has The Events Calendar REST API enabled but there was an error while fetching the archive control data.', 'the-events-calendar' ),
-			'tec-rest-api-archive-missing-total' => __( 'The URL provided could be reached and has The Events Calendar REST API enabled but there was an error while fetching the total number of events.', 'the-events-calendar' ),
-			'tec-rest-api-archive-bad-total'     => __( 'The URL provided could be reached and has The Events Calendar REST API enabled but returned malformed data in regard to the total number of events.', 'the-events-calendar' ),
-			'tec-rest-api-archive-page-error'    => __( 'The URL provided could be reached and has The Events Calendar REST API enabled but there was an error while fetching an archive page.', 'the-events-calendar' ),
-			'tec-rest-api-archive-page-empty'    => __( 'The URL provided could be reached and has The Events Calendar REST API enabled but returned an empty archive page.', 'the-events-calendar' ),
-			'tec-rest-api-single-event-error'    => __( 'The URL provided could be reached and has The Events Calendar REST API enabled but there was an error while fetching the event data.', 'the-events-calendar' ),
-			'tec-rest-api-single-event-empty'    => __( 'The URL provided could be reached and has The Events Calendar REST API enabled but returned empty event data.', 'the-events-calendar' ),
+			'tec-rest-api-unsupported'           => __( 'Events could not be imported. The requested URL does not support The Events Calendar REST API.', 'the-events-calendar' ),
+			'tec-rest-api-disabled'              => __( 'Events could not be imported. The Events Calendar REST API is disabled on the requested URL.', 'the-events-calendar' ),
+			'tec-rest-api-bad-data'              => __( 'Events could not be imported. The URL provided could be reached and has The Events Calendar REST API enabled, but returned malformed data.', 'the-events-calendar' ),
+			'tec-rest-api-archive-header-error'  => __( 'Events could not be imported. The URL provided could be reached and has The Events Calendar REST API enabled, but there was an error while fetching the archive control data.', 'the-events-calendar' ),
+			'tec-rest-api-archive-missing-total' => __( 'Events could not be imported. The URL provided could be reached and has The Events Calendar REST API enabled, but there was an error while fetching the total number of events.', 'the-events-calendar' ),
+			'tec-rest-api-archive-bad-total'     => __( 'Events could not be imported. The URL provided could be reached and has The Events Calendar REST API enabled, but returned malformed data in regard to the total number of events.', 'the-events-calendar' ),
+			'tec-rest-api-archive-page-error'    => __( 'Events could not be imported. The URL provided could be reached and has The Events Calendar REST API enabled, but there was an error while fetching an archive page.', 'the-events-calendar' ),
+			'tec-rest-api-archive-page-empty'    => __( 'Events could not be imported. The URL provided could be reached and has The Events Calendar REST API enabled, but returned an empty archive page.', 'the-events-calendar' ),
+			'tec-rest-api-single-event-error'    => __( 'Events could not be imported. The URL provided could be reached and has The Events Calendar REST API enabled, but there was an error while fetching the event data.', 'the-events-calendar' ),
+			'tec-rest-api-single-event-empty'    => __( 'Events could not be imported. The URL provided could be reached and has The Events Calendar REST API enabled, but returned empty event data.', 'the-events-calendar' ),
 			'no-results'                         => __( 'The requested URL does not have any upcoming and published events matching the search criteria.', 'the-events-calendar' ),
 		);
+
+		$learn_more_map = array(
+			'tec-rest-api-unsupported',
+			'tec-rest-api-disabled',
+			'tec-rest-api-bad-data',
+			'tec-rest-api-archive-header-error',
+			'tec-rest-api-archive-missing-total',
+			'tec-rest-api-archive-bad-total',
+			'tec-rest-api-archive-page-error',
+			'tec-rest-api-archive-page-empty',
+			'tec-rest-api-single-event-error',
+			'tec-rest-api-single-event-empty',
+		);
+
+		// @todo @luca add the Knowledgebase link here!
+		$learn_more_link = esc_attr( admin_url( 'http://theeventscalendar.com' ) );
+		$learn_more_message = esc_html__( 'Learn more.', 'the-events-calendar' );
+		$learn_more_message_html = sprintf( '<a href="%s" target="_blank">%s</a> ', $learn_more_link, $learn_more_message );
+
+		foreach ( $learn_more_map as $message_code ) {
+			$ea_messages[ $message_code ] .= ' ' . $learn_more_message_html;
+		}
 
 		$adjustable_map = array(
 			'tec-rest-api-bad-data',
@@ -30,12 +52,12 @@ class Tribe__Events__REST__V1__EA_Messages extends Tribe__Events__REST__V1__Mess
 			'tec-rest-api-single-event-empty',
 		);
 
-		$link = esc_attr( admin_url( '/edit.php?post_type=tribe_events&page=tribe-common&tab=imports#tribe-field-tribe_aggregator_default_url_import_events_count' ) );
-		$message = esc_html__( 'Please try to adjust your import amount and try again.', 'the-events-calendar' );
-		$adjust_message = sprintf( '<a href="%s" target="_blank">%s</a> ', $link, $message );
+		$adjust_link = esc_attr( admin_url( '/edit.php?post_type=tribe_events&page=tribe-common&tab=imports#tribe-field-tribe_aggregator_default_url_import_events_count' ) );
+		$adjust_message = esc_html__( 'Try to adjust your import settings and try again.', 'the-events-calendar' );
+		$adjust_message_html = sprintf( '<a href="%s" target="_blank">%s</a> ', $adjust_link, $adjust_message );
 
 		foreach ( $adjustable_map as $message_code ) {
-			$ea_messages[ $message_code ] .= ' ' . $adjust_message;
+			$ea_messages[ $message_code ] .= ' ' . $adjust_message_html;
 		}
 
 		$this->messages = array_merge( $this->messages, $ea_messages );
