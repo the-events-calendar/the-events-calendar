631a0a07058c1cad22a37f4e4a81a8716edd400f
Trisha Salas <trisha@trishasalas.com>
2017-03-20 10:46:05 -0500

    icons

--- src/resources/js/tribe-events-bar.js
+++ src/resources/js/tribe-events-bar.js
@@ -139,14 +139,6 @@ var tribe_events_bar_action;
 			$( '#tribe-events-bar' ).addClass( 'tribe-has-datepicker' );
 		}
 
-		// Implement views links
-		function format( view ) {
-			return '<span class="tribe-icon-' + $.trim( view.text.toLowerCase() ) + '">' + view.text + '</span>';
-		}
-
-		// Implement placeholder
-		$( 'input[name*="tribe-bar-"]' ).placeholder();
-
 
 		// Trigger Mobile Change
 		tf.maybe_default_view_change();
@@ -341,15 +333,15 @@ var tribe_events_bar_action;
 
 })( window, document, jQuery, tribe_ev.data, tribe_ev.events, tribe_ev.fn, tribe_ev.state, tribe_ev.tests, tribe_debug );
 
-(function (factory) {
-	if (typeof define === 'function' && define.amd) {
-		define(['jquery'], factory);
-	} else if (typeof exports === 'object') {
-		factory(require('jquery'));
+(function( factory ) {
+	if ( typeof define === 'function' && define.amd ) {
+		define( ['jquery'], factory );
+	} else if ( typeof exports === 'object' ) {
+		factory( require( 'jquery' ) );
 	} else {
-		factory(jQuery);
+		factory( jQuery );
 	}
-})(function ($) {
+})( function( $ ) {
 	'use strict';
 
 	var KEY_ENTER = 13,
@@ -361,30 +353,30 @@ var tribe_events_bar_action;
 		KEY_ESCAPE = 27;
 
 	$.fn.selectability = function() {
-		this.each(function() {
-			var $this = $(this);
+		this.each( function() {
+			var $this = $( this );
 
-			if (!$this.data('selectability')) {
-				$this.data('selectability', new Selectability($this));
+			if ( ! $this.data( 'selectability' ) ) {
+				$this.data( 'selectability', new Selectability( $this ) );
 			}
-		});
+		} );
 
 		return this;
 	};
 
-	var idgen = (function () {
+	var idgen = (function() {
 		var i = 0;
-		return function () {
+		return function() {
 			var id;
 			do {
-				id = "selectability#id" + (i++);
-			} while (document.getElementById(id));
+				id = "selectability#id" + (i ++);
+			} while ( document.getElementById( id ) );
 
 			return id;
 		}
 	})();
 
-	function Selectability(element) {
+	function Selectability( element ) {
 		this.element = element;
 
 		this.buildElements();
@@ -393,128 +385,121 @@ var tribe_events_bar_action;
 		this.populateText();
 		this.registerHandlers();
 
-		element
-		.attr('tabindex', -1)
-		.addClass('selectability-offscreen');
+		element.attr( 'tabindex', - 1 ).addClass( 'selectability-offscreen' );
 	}
 
-	Selectability.prototype.buildElements = function () {
-		this.textbox = $('<div></div>')
-		.attr({
+	Selectability.prototype.buildElements = function() {
+		this.textbox = $( '<div></div>' ).attr( {
 			role: 'textbox',
-			tabindex: -1,
+			tabindex: - 1,
 			'aria-readonly': 'true'
-		});
+		} );
 
-		this.listbox = $('<div></div>')
-		.attr({
+		this.listbox = $( '<div></div>' ).attr( {
 			role: 'listbox',
-			tabindex: -1,
+			tabindex: - 1,
 			'aria-multiselectable': 'false'
-		});
-		console.log(this.listbox);
+		} );
 
-		this.combobox = $('<div></div>')
-		.addClass('selectability')
-		.attr({
+		this.combobox = $( '<div></div>' ).addClass( 'selectability' ).attr( {
 			role: 'combobox application',
 			tabindex: 0,
 			'aria-expanded': 'false'
-		});
+		} );
 
-		this.combobox
-		.append(this.textbox)
-		.append(this.listbox);
+		this.combobox.append( this.textbox ).append( this.listbox );
 
-		this.element
-		.before(this.combobox);
+		this.element.before( this.combobox );
 
 	};
 
-	Selectability.prototype.stealLabel = function () {
-		var id = this.element.attr('id'),
-			label = this.element.attr('aria-label');
+	Selectability.prototype.stealLabel = function() {
+		var id = this.element.attr( 'id' ),
+			label = this.element.attr( 'aria-label' ),
+			view = this.element.attr( 'data-view' );
 
-		if (label) {
-			this.combobox.attr('aria-label', label);
+		if ( label ) {
+			this.combobox.attr( 'aria-label', label );
 			return;
 		}
 
-		if (!id) {
+		if ( ! id ) {
 			return;
 		}
 
 		var ids = [];
-		$('label[for]')
-		.filter(function () { return $(this).attr('for') === id })
-		.each(function () {
-			var $this = $(this),
+		$( 'label[for]' ).filter( function() {
+			return $( this ).attr( 'for' ) === id
+		} ).each( function() {
+			var $this = $( this ),
 				autogen = idgen();
 
-			$this.removeAttr('for');
-			$this.attr('id', autogen);
-			ids.push(autogen);
-		});
+			$this.removeAttr( 'for' );
+			$this.attr( 'id', autogen );
+			ids.push( autogen );
+		} );
 
-		this.combobox.attr('aria-labelledby', ids.join(' '));
+		this.combobox.attr( 'aria-labelledby', ids.join( ' ' ) );
 	};
 
-	Selectability.prototype.populateText = function (event) {
-		if (event && event.selectability) {
+	Selectability.prototype.populateText = function( event ) {
+		if ( event && event.selectability ) {
 			return;
 		}
 
-		var selected = this.element.find(':selected');
-		if (selected.length) {
-			this.textbox.text(selected.attr('label') || selected.text());
+		var selected = this.element.find( ':selected' );
+		if ( selected.length ) {
+			this.textbox.text(
+				selected.attr( 'label' ) || selected.text()
+			).prepend( '<span class="tribe-icon ' + selected.text().toLowerCase()  + '"></span>' )
 		}
 	}
 
-	Selectability.prototype.synchronizeAttributes = function () {
-		if (this.element.prop('multiple')) {
-			throw new Error('Can only bind to single selection widgets');
+	Selectability.prototype.synchronizeAttributes = function() {
+		if ( this.element.prop( 'multiple' ) ) {
+			throw new Error( 'Can only bind to single selection widgets' );
 		}
 
-		this.disabled = !!this.element.prop('disabled');
-		this.combobox.attr({
+		this.disabled = ! ! this.element.prop( 'disabled' );
+		this.combobox.attr( {
 			'aria-disabled': this.disabled,
-			'aria-required': !!this.element.prop('required')
-		});
+			'aria-required': ! ! this.element.prop( 'required' )
+		} );
 	};
 
-	Selectability.prototype.registerHandlers = function () {
+	Selectability.prototype.registerHandlers = function() {
 		this.observeProperties();
 		this.registerEvents();
 	};
 
-	Selectability.prototype.observeProperties = function () {
-		var synchronize = $.proxy(this.synchronizeAttributes, this),
+	Selectability.prototype.observeProperties = function() {
+		var synchronize = $.proxy( this.synchronizeAttributes, this ),
 			observer = window.MutationObserver
 				|| window.MozMutationObserver
 				|| window.WebkitMutationObserver;
 
-		if (observer) {
-			this.observer = new observer(synchronize);
-			this.observer.observe(this.element[0], {
+		if ( observer ) {
+			this.observer = new observer( synchronize );
+			this.observer.observe( this.element[0], {
 				attributes: true,
 				attributeFilter: ['multiple', 'disabled', 'required']
-			});
+			} );
 		} else {
-			this.element.on({
+			this.element.on( {
 				'propertychange.selectability': synchronize,
 				'focus.selectability': synchronize
-			});
+			} );
 		}
 
-		this.element.change($.proxy(this.populateText, this));
+		this.element.change( $.proxy( this.populateText, this ) );
 	};
 
-	Selectability.prototype.registerEvents = function () {
+	Selectability.prototype.registerEvents = function() {
 		var selectability = this;
-		this.combobox.on({
-			focusout: function (event) {
+		this.combobox.on( {
+			focusout: function( event ) {
 				var elt = this;
-				setTimeout(function () {
+				setTimeout( function() {
 					/*
 					 * So *after* the focusout event chain has run its course, we look to
 					 * see what happens with document.activeElement.
@@ -527,36 +512,36 @@ var tribe_events_bar_action;
 					 *
 					 * sigh.
 					 */
-					if (!$.contains(elt, document.activeElement)) {
+					if ( ! $.contains( elt, document.activeElement ) ) {
 						selectability.closeCombobox();
 					}
-				}, 0);
+				}, 0 );
 			},
-			click: $.proxy(this.comboboxClick, this),
-			keydown: $.proxy(this.comboboxKeydown, this)
-		});
+			click: $.proxy( this.comboboxClick, this ),
+			keydown: $.proxy( this.comboboxKeydown, this )
+		} );
 
-		this.listbox.on({
-			click: $.proxy(this.listboxClick, this),
-			keydown: $.proxy(this.listboxKeydown, this)
-		});
+		this.listbox.on( {
+			click: $.proxy( this.listboxClick, this ),
+			keydown: $.proxy( this.listboxKeydown, this )
+		} );
 	}
 
 	Selectability.prototype.comboboxClick = function() {
-		if (!this.disabled) {
+		if ( ! this.disabled ) {
 			this.toggleCombobox();
 		}
 	};
 
-	Selectability.prototype.comboboxKeydown = function(event) {
-		if (this.disabled) {
+	Selectability.prototype.comboboxKeydown = function( event ) {
+		if ( this.disabled ) {
 			return;
 		}
 
-		switch (event.which) {
+		switch ( event.which ) {
 		case KEY_ENTER:
 		case KEY_SPACE:
-			if (this.combobox.attr('aria-expanded') === 'true') {
+			if ( this.combobox.attr( 'aria-expanded' ) === 'true' ) {
 				this.closeCombobox();
 				this.combobox.focus();
 				event.preventDefault();
@@ -567,7 +552,7 @@ var tribe_events_bar_action;
 		case KEY_DOWN:
 		case KEY_LEFT:
 		case KEY_RIGHT:
-			if (this.combobox.attr('aria-expanded') !== 'true') {
+			if ( this.combobox.attr( 'aria-expanded' ) !== 'true' ) {
 				this.openCombobox();
 			}
 			this.active.focus();
@@ -582,62 +567,61 @@ var tribe_events_bar_action;
 		}
 	};
 
-	Selectability.prototype.listboxClick = function(event) {
-		var $target = $(event.target);
-		if ($target.attr('role') !== 'option') {
+	Selectability.prototype.listboxClick = function( event ) {
+		var $target = $( event.target );
+		if ( $target.attr( 'role' ) !== 'option' ) {
 			return;
 		}
 
-		if ($target.attr('aria-disabled') === 'true') {
+		if ( $target.attr( 'aria-disabled' ) === 'true' ) {
 			return;
 		}
 
-		this.setActive($(event.target));
+		this.setActive( $( event.target ) );
 		this.closeCombobox();
-
 
 		event.preventDefault();
 		return false;
 	};
 
-	Selectability.prototype.setActive = function(active) {
-		var index = this.listbox.find('[role=option]').index(active),
-			value = this.element.find('option').eq(index).val(),
+	Selectability.prototype.setActive = function( active ) {
+		var index = this.listbox.find( '[role=option]' ).index( active ),
+			value = this.element.find( 'option' ).eq( index ).val(),
 			prev = this.element.val(),
-			event = $.Event('change', {
+			event = $.Event( 'change', {
 				val: value,
 				selectability: true
-			});
+			} );
 
 		// nothing to do
-		if (this.element.val() === value) {
+		if ( this.element.val() === value ) {
 			return;
 		}
 
 		// some frameworks read element.val() instead of the event value
 		// so we populate the value and restore it (see below) if the event is canceled
-		this.element.val(value);
+		this.element.val( value );
 
 		try {
 			// work around event handlers throwing exceptions
-			this.element.trigger(event);
+			this.element.trigger( event );
 		} finally {
 			// promote 'change' to a cancelable event
-			if (!event.isDefaultPrevented()) {
+			if ( ! event.isDefaultPrevented() ) {
 				this.active = active;
-				this.textbox.text(active.attr('label') || active.text());
+				this.textbox.text( active.attr( 'label' ) || active.text() );
 			} else {
 				// if the event is prevented, restore the old value
-				this.element.val(prev);
+				this.element.val( prev );
 			}
 		}
 	};
 
-	Selectability.prototype.listboxKeydown = function(event) {
-		switch (event.which) {
+	Selectability.prototype.listboxKeydown = function( event ) {
+		switch ( event.which ) {
 		case KEY_ENTER:
 		case KEY_SPACE:
-			this.setActive($(event.target));
+			this.setActive( $( event.target ) );
 			this.closeCombobox();
 			this.combobox.focus();
 
@@ -659,14 +643,14 @@ var tribe_events_bar_action;
 	};
 
 	Selectability.prototype.toggleCombobox = function() {
-		if (this.combobox.attr('aria-expanded') === 'true') {
+		if ( this.combobox.attr( 'aria-expanded' ) === 'true' ) {
 			this.closeCombobox();
 		} else {
 			this.openCombobox();
 
 			// We may have an empty select widget, so we can't always depend on
 			// `active' being defined
-			if (this.active) {
+			if ( this.active ) {
 				this.active.focus();
 			}
 		}
@@ -675,109 +659,100 @@ var tribe_events_bar_action;
 	Selectability.prototype.closeCombobox = function() {
 		this.active = null;
 		this.listbox.empty();
-		this.combobox.attr('aria-expanded', false);
+		this.combobox.attr( 'aria-expanded', false );
 	};
 
 	Selectability.prototype.openCombobox = function() {
 		this.populateListbox();
-		this.combobox.attr('aria-expanded', true);
+		this.combobox.attr( 'aria-expanded', true );
 	};
 
 	Selectability.prototype.populateListbox = function() {
 		this.populateText();
 
-		var children = walk.call(this, this.element.children()).children();
-		this.listbox.append(children);
+		var children = walk.call( this, this.element.children() ).children();
+		this.listbox.append( children );
 		return;
 
-		function walk (elements) {
-			var node = $('<div></div>'),
+		function walk( elements ) {
+			var node = $( '<div></div>' ),
 				This = this;
 
-			$.each(elements, function (i, element) {
-				var inner = $('<div></div>');
-				element = $(element);
+			$.each( elements, function( i, element ) {
+				var inner = $( '<div></div>' );
+				element = $( element );
 
-				if (element.is('option')) {
-					inner
-					.text(element.attr('label') || element.text())
-					.attr({
+				if ( element.is( 'option' ) ) {
+					inner.text(
+						element.attr( 'label' ) ||
+						element.text()
+					).prepend( '<span class="tribe-icon ' + element.text().toLowerCase()  + '"></span>' )
+
+					.attr( {
 						role: 'option',
-						tabindex: -1,
-						'aria-disabled': !!element.prop('disabled'),
+						tabindex: - 1,
+						'aria-disabled': ! ! element.prop( 'disabled' ),
 						'aria-selected': element.val() === This.element.val()
-					})
-					.appendTo(node);
+					} ).appendTo( node );
 
-					if (element.prop('disabled')) {
-						inner.removeAttr('tabindex');
-					} else if (element.val() === This.element.val()) {
+					if ( element.prop( 'disabled' ) ) {
+						inner.removeAttr( 'tabindex' );
+					} else if ( element.val() === This.element.val() ) {
 						This.active = inner;
 					}
-				} else if (element.is('optgroup')) {
-					var children = walk.call(This, element.children());
-					if (children.children().length) {
-						var label = $('<div></div>')
-						.text(element.attr('label'))
-						.attr({
+				} else if ( element.is( 'optgroup' ) ) {
+					var children = walk.call( This, element.children() );
+					if ( children.children().length ) {
+						var label = $( '<div></div>' ).text( element.attr( 'label' ) ).attr( {
 							role: 'heading',
 							id: idgen()
-						});
+						} );
 
-						inner
-						.attr({
+						inner.attr( {
 							role: 'group',
-							'aria-labelledby': label.attr('id')
-						})
-						.append(children.prepend(label).children())
-						.appendTo(node);
+							'aria-labelledby': label.attr( 'id' )
+						} ).append( children.prepend( label ).children() ).appendTo( node );
 					}
 				}
-			});
+			} );
 
 			return node;
 		}
 	};
 
 	Selectability.prototype.moveFocusUp = function() {
-		var options = this.listbox.find('[role=option]');
+		var options = this.listbox.find( '[role=option]' );
 
-		for (var i = options.index(this.active[0]) - 1; i >= 0; i--) {
-			var option = $(options[i]);
+		for ( var i = options.index( this.active[0] ) - 1; i >= 0; i -- ) {
+			var option = $( options[i] );
 
-			if (option.attr('aria-disabled') === 'false') {
-				this.active.attr('aria-selected', 'false');
+			if ( option.attr( 'aria-disabled' ) === 'false' ) {
+				this.active.attr( 'aria-selected', 'false' );
 
 				this.active = option;
-				this.active
-				.attr('aria-selected', true)
-				.focus();
+				this.active.attr( 'aria-selected', true ).focus();
 				break;
 			}
 		}
 	};
 
 	Selectability.prototype.moveFocusDown = function() {
-		var options = this.listbox.find('[role=option]');
+		var options = this.listbox.find( '[role=option]' );
 
-		for (var i = options.index(this.active[0]) + 1; i < options.length; i++) {
-			var option = $(options[i]);
+		for ( var i = options.index( this.active[0] ) + 1; i < options.length; i ++ ) {
+			var option = $( options[i] );
 
-			if (option.attr('aria-disabled') === 'false') {
-				this.active.attr({
+			if ( option.attr( 'aria-disabled' ) === 'false' ) {
+				this.active.attr( {
 					'aria-selected': 'false'
-				});
+				} );
 
 				this.active = option;
-				this.active
-				.attr('aria-selected', true)
-				.focus();
+				this.active.attr( 'aria-selected', true ).focus();
 				break;
 			}
 		}
 	};
 
-
-
-});
+} );
 
