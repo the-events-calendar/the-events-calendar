18de2f341e916acfab02aef546d620c5e44de604
Zach Tirrell <zbtirrell@gmail.com>
2017-03-21 08:47:33 -0400

    fixed bad merge

--- src/views/list/single-event.php
+++ src/views/list/single-event.php
@@ -1,92 +1,87 @@
 <?php
 /**
- * Single Event Template
- * A single event. This displays the event title, description, meta, and
- * optionally, the Google map for the event.
+ * List View Single Event
+ * This file contains one event in the list view
  *
- * Override this template in your own theme by creating a file at [your-theme]/tribe-events/single-event.php
+ * Override this template in your own theme by creating a file at [your-theme]/tribe-events/list/single-event.php
  *
  * @package TribeEventsCalendar
- * @version  4.3
+ * @version  4.5
  *
  */
-
 if ( ! defined( 'ABSPATH' ) ) {
 	die( '-1' );
 }
 
-$events_label_singular = tribe_get_event_label_singular();
-$events_label_plural = tribe_get_event_label_plural();
+// Setup an array of venue details for use later in the template
+$venue_details = tribe_get_venue_details();
 
-$event_id = get_the_ID();
+// Venue
+$has_venue_address = ( ! empty( $venue_details['address'] ) ) ? ' location' : '';
+
+// Organizer
+$organizer = tribe_get_organizer();
 
 ?>
 
-<div id="tribe-events-content" class="tribe-events-single">
+<!-- Event Cost -->
+<?php if ( tribe_get_cost() ) : ?>
+	<div class="tribe-events-event-cost">
+		<span class="ticket-cost"><?php echo tribe_get_cost( null, true ); ?></span>
+		<?php
+		/**
+		 * Runs after cost is displayed in list style views
+		 *
+		 * @since 4.5
+		 */
+		do_action( 'tribe_events_inside_cost' )
+		?>
+	</div>
+<?php endif; ?>
 
-	<p class="tribe-events-back">
-		<a href="<?php echo esc_url( tribe_get_events_link() ); ?>"> <?php printf( '&laquo; ' . esc_html_x( 'All %s', '%s Events plural label', 'the-events-calendar' ), $events_label_plural ); ?></a>
-	</p>
+<!-- Event Title -->
+<?php do_action( 'tribe_events_before_the_event_title' ) ?>
+<h2 class="tribe-events-list-event-title">
+	<a class="tribe-event-url" href="<?php echo esc_url( tribe_get_event_link() ); ?>" title="<?php the_title_attribute() ?>" rel="bookmark">
+		<?php the_title() ?>
+	</a>
+</h2>
+<?php do_action( 'tribe_events_after_the_event_title' ) ?>
 
-	<!-- Notices -->
-	<?php tribe_the_notices() ?>
+<!-- Event Meta -->
+<?php do_action( 'tribe_events_before_the_meta' ) ?>
+<div class="tribe-events-event-meta">
+	<div class="author <?php echo esc_attr( $has_venue_address ); ?>">
 
-	<?php the_title( '<h1 class="tribe-events-single-event-title">', '</h1>' ); ?>
+		<!-- Schedule & Recurrence Details -->
+		<div class="tribe-event-schedule-details">
+			<?php echo tribe_events_event_schedule_details() ?>
+		</div>
 
-	<div class="tribe-events-schedule tribe-clearfix">
-		<?php echo tribe_events_event_schedule_details( $event_id, '<h2>', '</h2>' ); ?>
-		<?php if ( tribe_get_cost() ) : ?>
-			<span class="tribe-events-cost"><?php echo tribe_get_cost( null, true ) ?></span>
-			<?php
-			/** This action is documented in the-events-calendar/src/views/list/single-event.php */
-			do_action( 'tribe_events_inside_cost' )
-			?>
+		<?php if ( $venue_details ) : ?>
+			<!-- Venue Display Info -->
+			<div class="tribe-events-venue-details">
+				<?php echo implode( ', ', $venue_details ); ?>
+				<?php
+				if ( tribe_get_map_link() ) {
+					echo tribe_get_map_link_html();
+				}
+				?>
+			</div> <!-- .tribe-events-venue-details -->
 		<?php endif; ?>
+
 	</div>
+</div><!-- .tribe-events-event-meta -->
+<?php do_action( 'tribe_events_after_the_meta' ) ?>
 
-	<!-- Event header -->
-	<div id="tribe-events-header" <?php tribe_events_the_header_attributes() ?>>
-		<!-- Navigation -->
-		<h3 class="tribe-events-visuallyhidden"><?php printf( esc_html__( '%s Navigation', 'the-events-calendar' ), $events_label_singular ); ?></h3>
-		<ul class="tribe-events-sub-nav">
-			<li class="tribe-events-nav-previous"><?php tribe_the_prev_event_link( '<span>&laquo;</span> %title%' ) ?></li>
-			<li class="tribe-events-nav-next"><?php tribe_the_next_event_link( '%title% <span>&raquo;</span>' ) ?></li>
-		</ul>
-		<!-- .tribe-events-sub-nav -->
-	</div>
-	<!-- #tribe-events-header -->
+<!-- Event Image -->
+<?php echo tribe_event_featured_image( null, 'medium' ) ?>
 
-	<?php while ( have_posts() ) :  the_post(); ?>
-		<div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-			<!-- Event featured image, but exclude link -->
-			<?php echo tribe_event_featured_image( $event_id, 'full', false ); ?>
-
-			<!-- Event content -->
-			<?php do_action( 'tribe_events_single_event_before_the_content' ) ?>
-			<div class="tribe-events-single-event-description tribe-events-content">
-				<?php the_content(); ?>
-			</div>
-			<!-- .tribe-events-single-event-description -->
-			<?php do_action( 'tribe_events_single_event_after_the_content' ) ?>
-
-			<!-- Event meta -->
-			<?php do_action( 'tribe_events_single_event_before_the_meta' ) ?>
-			<?php tribe_get_template_part( 'modules/meta' ); ?>
-			<?php do_action( 'tribe_events_single_event_after_the_meta' ) ?>
-		</div> <!-- #post-x -->
-		<?php if ( get_post_type() == Tribe__Events__Main::POSTTYPE && tribe_get_option( 'showComments', false ) ) comments_template() ?>
-	<?php endwhile; ?>
-
-	<!-- Event footer -->
-	<div id="tribe-events-footer">
-		<!-- Navigation -->
-		<h3 class="tribe-events-visuallyhidden"><?php printf( esc_html__( '%s Navigation', 'the-events-calendar' ), $events_label_singular ); ?></h3>
-		<ul class="tribe-events-sub-nav">
-			<li class="tribe-events-nav-previous"><?php tribe_the_prev_event_link( '<span>&laquo;</span> %title%' ) ?></li>
-			<li class="tribe-events-nav-next"><?php tribe_the_next_event_link( '%title% <span>&raquo;</span>' ) ?></li>
-		</ul>
-		<!-- .tribe-events-sub-nav -->
-	</div>
-	<!-- #tribe-events-footer -->
-
-</div><!-- #tribe-events-content -->
+<!-- Event Content -->
+<?php do_action( 'tribe_events_before_the_content' ) ?>
+<div class="tribe-events-list-event-description tribe-events-content">
+	<?php echo tribe_events_get_the_excerpt( null, wp_kses_allowed_html( 'post' ) ); ?>
+	<a href="<?php echo esc_url( tribe_get_event_link() ); ?>" class="tribe-events-read-more" rel="bookmark"><?php esc_html_e( 'Find out more', 'the-events-calendar' ) ?> &raquo;</a>
+</div><!-- .tribe-events-list-event-description -->
+<?php
+do_action( 'tribe_events_after_the_content' );
