6defe2e6dd53077478c6cf039d1543961fb4839b
Luca Tumedei <luca@theaveragedev.com>
2017-04-17 12:37:24 +0200

    Allow for series save

--- src/Tribe/Meta/Save.php
+++ src/Tribe/Meta/Save.php
@@ -132,7 +132,7 @@ class Tribe__Events__Meta__Save {
 	 */
 	public function maybe_save() {
 		// only continue if it's an event post
-		if ( ! $this->is_event() ) {
+		if ( ! ( $this->is_event() || $this->is_series() ) ) {
 			return false;
 		}
 
