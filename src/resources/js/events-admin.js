01b8920d5869b8745a3fc9f11656152d7560ce01
bordoni <bordoni.dev@gmail.com>
2017-04-24 15:26:02 -0300

    Use tfoot instead of the tbody that can be deleted

--- src/resources/js/events-admin.js
+++ src/resources/js/events-admin.js
@@ -326,8 +326,10 @@ jQuery( document ).ready( function( $ ) {
 				fields = $( create_template({}) );
 			}
 
+			console.log( fields );
+
 			// The final <tbody> contains the add new post link, we should add this new selector before that
-			section.find( 'tbody:last' ).before( fields );
+			section.find( 'tfoot' ).before( fields );
 			fields.prepend( dropdown );
 
 			if ( section.find( 'tbody' ).length > 1 ) {
