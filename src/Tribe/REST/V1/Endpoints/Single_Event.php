fc61e004db3c0e70a3734a1531a9e673be808335
Luca Tumedei <luca@theaveragedev.com>
2017-06-07 16:59:42 +0200

    code style

--- src/Tribe/REST/V1/Endpoints/Single_Event.php
+++ src/Tribe/REST/V1/Endpoints/Single_Event.php
@@ -105,13 +105,13 @@ class Tribe__Events__REST__V1__Endpoints__Single_Event
 						),
 					),
 					'400' => array(
-						'description' => __( 'The event post ID is missing.', 'the-events-calendar' )
+						'description' => __( 'The event post ID is missing.', 'the-events-calendar' ),
 					),
 					'403' => array(
-						'description' => __( 'The event with the specified ID is not accessible.', 'the-events-calendar' )
+						'description' => __( 'The event with the specified ID is not accessible.', 'the-events-calendar' ),
 					),
 					'404' => array(
-						'description' => __( 'An event with the specified ID does not exist.', 'the-events-calendar' )
+						'description' => __( 'An event with the specified ID does not exist.', 'the-events-calendar' ),
 					),
 				),
 			),
