50e7cd18593a933193fab9106920fa35801564b2
David Garcia Watkins <david.g@onthegosystems.com>
2017-06-20 10:53:49 +0200

    We are already doing this in WPML__Permalinks::get_post_permalink. Fixes https://github.com/Yoast/wordpress-seo/issues/7117

--- src/Tribe/Integrations/WPML/WPML.php
+++ src/Tribe/Integrations/WPML/WPML.php
@@ -51,8 +51,6 @@ class Tribe__Events__Integrations__WPML__WPML {
 	}
 
 	protected function hook_filters() {
-		add_filter( 'tribe_events_post_type_permalink', 'wpml_permalink_filter' );
-
 		$filters = Tribe__Events__Integrations__WPML__Filters::instance();
 		add_filter( 'tribe_events_rewrite_i18n_slugs_raw', array( $filters, 'filter_tribe_events_rewrite_i18n_slugs_raw' ), 10, 3 );
 
