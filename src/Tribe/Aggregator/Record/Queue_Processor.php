543d2a9d529b323039dcefc85b67d020b51759ed
bordoni <bordoni.dev@gmail.com>
2017-07-21 17:08:15 -0300

    Typo on the method

--- src/Tribe/Aggregator/Record/Queue_Processor.php
+++ src/Tribe/Aggregator/Record/Queue_Processor.php
@@ -143,7 +143,7 @@ class Tribe__Events__Aggregator__Record__Queue_Processor {
 			}
 		}
 
-		$queue_items = get_post_meta( $this->current_record_id, Tribe__Events__Aggregator__Records::meta_prefix( Tribe__Events__Aggregator__Record__Queue::$queue_key ), true );
+		$queue_items = get_post_meta( $this->current_record_id, Tribe__Events__Aggregator__Records::prefix_meta( Tribe__Events__Aggregator__Record__Queue::$queue_key ), true );
 
 		// We only get here if we done processing this batch.
 		if ( ! empty( $queue_items ) ) {
