6c95a60a0eca0065a5b35f22955a7ebce4012539
theAverageDev (Luca Tumedei) <luca@theaveragedev.com>
2017-08-23 10:59:17 +0200

    added first EA response mock file and sanity check test

---
+++ tests/wpunit/Tribe/Events/Aggregator/Record/FeedResponseTest.php
@@ -0,0 +1,42 @@
+<?php
+
+
+namespace Tribe\Events\Aggregator\Record;
+
+use Tribe\Events\Tests\Testcases\Events_TestCase;
+
+class FeedResponseTest extends Events_TestCase {
+
+	/**
+	 * Test the feed responses does not contain duplicates
+	 */
+	public function test_the_feed_response_does_not_contain_duplicates() {
+		$source_dir = codecept_data_dir( 'ea-responses' );
+
+		foreach ( glob( $source_dir . '/*.json' ) as $response_file ) {
+			$response_json         = file_get_contents( $response_file );
+			$response_basename     = basename( $response_file );
+			$decoded_response_json = json_decode( $response_json );
+			$events                = $decoded_response_json->data->events;
+
+			$uids = [];
+
+			foreach ( $events as $event ) {
+				$event_uid = $event->uid;
+				if ( array_key_exists( $event_uid, $uids ) ) {
+					throw new \RuntimeException( "File {$response_basename}: an event with UID {$event_uid} already exists ({$uids[$event_uid]})" );
+				}
+
+				$uids[ $event_uid ] = "{$event->title}, {$event->start_date} to {$event->end_date}";
+			}
+
+			$events_count = count( $events );
+			$uids_count   = count( $uids );
+			$this->assertEquals(
+				$events_count,
+				$uids_count,
+				"File {$response_basename}: the number of UIDs in the feed ({$uids_count}) does not match the number of events in the feed ({$events_count})"
+			);
+		}
+	}
+}
