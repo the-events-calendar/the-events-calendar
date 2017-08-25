88dff8229d16ca42abce54227b9fe75f9db5c2a9
bordoni <bordoni.dev@gmail.com>
2017-08-25 09:34:02 -0400

    Prevent Notice from Non-Existing Variable on Column Categories

--- src/admin-views/list/column-categories.php
+++ src/admin-views/list/column-categories.php
@@ -1,7 +1,7 @@
 <?php
 $categories = get_the_term_list( $this->get( 'event' )->ID, Tribe__Events__Main::TAXONOMY, '', ', ', '' );
 if ( $categories ) {
-	echo strip_tags( $event_cats );
+	echo strip_tags( $categories );
 } else {
 	echo '&mdash;';
 }
