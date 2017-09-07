e68fcaafd735e10d119d39540297cfa7f2457a4d
theAverageDev (Luca Tumedei) <luca@theaveragedev.com>
2017-09-07 15:58:40 +0200

    do not set the import ID on the parent record

--- src/Tribe/Aggregator/Tabs/Scheduled.php
+++ src/Tribe/Aggregator/Tabs/Scheduled.php
@@ -329,7 +329,6 @@ class Tribe__Events__Aggregator__Tabs__Scheduled extends Tribe__Events__Aggregat
 			}
 
 			$record->update_meta( 'last_import_status', 'success:queued' );
-			$record->update_meta( 'import_id', $status->data->import_id );
 			$child->update_meta( 'import_id', $status->data->import_id );
 
 			$child->finalize();
