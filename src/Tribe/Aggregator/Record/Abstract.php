4c9ff2d8837004c0c25db382fb61e983b0cdc222
theAverageDev (Luca Tumedei) <luca@theaveragedev.com>
2017-09-28 17:11:31 +0200

    corrections per code review

--- src/Tribe/Aggregator/Record/Abstract.php
+++ src/Tribe/Aggregator/Record/Abstract.php
@@ -1034,8 +1034,8 @@ abstract class Tribe__Events__Aggregator__Record__Abstract {
 
 		// Prevents Pending that is younger than 1 hour to be pruned
 		if (
-			Tribe__Events__Aggregator__Records::$status->pending === $this->post->post_status &&
-			$current < $created + HOUR_IN_SECONDS
+			Tribe__Events__Aggregator__Records::$status->pending === $this->post->post_status
+			&& $current < $created + HOUR_IN_SECONDS
 		) {
 			return false;
 		}
