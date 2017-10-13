b3238b1a5888712e8b0fb4122ade595cdda5f0ee
jentheo <jennifert@tri.be>
2017-10-13 12:26:25 -0400

    Update Event.php for Bug #42125
    Setting dates to local time instead of UTC for Google

--- src/Tribe/JSON_LD/Event.php
+++ src/Tribe/JSON_LD/Event.php
@@ -59,8 +59,18 @@ class Tribe__Events__JSON_LD__Event extends Tribe__JSON_LD__Abstract {
 			$event_tz_string = get_post_meta( $post_id, '_EventTimezone', true );
 			$tz_mode         = tribe_get_option( 'tribe_events_timezone_mode', 'event' );
 			$tz_string       = $event_tz_string && $tz_mode === 'event' ? $event_tz_string : Tribe__Events__Timezones::wp_timezone_string();
-			$data->startDate = Tribe__Events__Timezones::to_utc( tribe_get_start_date( $post_id, true, Tribe__Date_Utils::DBDATETIMEFORMAT ), $tz_string, 'c' );
-			$data->endDate   = Tribe__Events__Timezones::to_utc( tribe_get_end_date( $post_id, true, Tribe__Date_Utils::DBDATETIMEFORMAT ), $tz_string, 'c' );
+
+			// Convert to local timezone
+			function local_timezone( $date, $event_tz_string ) {
+				$localTime = new DateTime( $date, new DateTimeZone( 'UTC' ) );
+				$localTime->setTimezone( new DateTimeZone( $event_tz_string ) );
+				$localTime->startDate = $localTime->format('c');
+
+				return $localTime->startDate;
+			}
+
+			$data->startDate = local_timezone( Tribe__Events__Timezones::to_utc( tribe_get_start_date( $post_id, true, Tribe__Date_Utils::DBDATETIMEFORMAT ), $tz_string, 'c' ), $event_tz_string );
+			$data->endDate   = local_timezone( Tribe__Events__Timezones::to_utc( tribe_get_end_date( $post_id, true, Tribe__Date_Utils::DBDATETIMEFORMAT ), $tz_string, 'c' ), $event_tz_string );
 
 			if ( tribe_has_venue( $post_id ) ) {
 				$venue_id       = tribe_get_venue_id( $post_id );
