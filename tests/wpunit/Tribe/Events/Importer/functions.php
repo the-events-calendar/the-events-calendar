3832535d3e2227c8b162ff4a80640741feb7dd4a
theAverageDev (Luca Tumedei) <luca@theaveragedev.com>
2017-10-13 11:22:54 +0200

    correct the path to the test image file

--- tests/wpunit/Tribe/Events/Importer/functions.php
+++ tests/wpunit/Tribe/Events/Importer/functions.php
@@ -2,7 +2,13 @@
 namespace Tribe\Events\Importer;
 
 function get_image_url( $extension = 'jpg' ) {
-	return plugins_url( 'tests/_data/csv-import-test-files/featured-image/images/featured-image.' . $extension, codecept_root_dir( 'the-events-calendar.php' ) );
+	if ( true === getenv( 'CI' ) ) {
+		return getenv( 'WP_URL' ) . '/wp-contents/plugins/the-events-calendar/tests/_data/csv-import-test-files/featured-image/images/featured-image.' . $extension;
+	}
+
+	return plugins_url(
+		'tests/_data/csv-import-test-files/featured-image/images/featured-image.' . $extension,
+		codecept_root_dir( 'the-events-calendar.php' ) );
 }
 
 function get_image_path( $extension = 'jpg' ) {
