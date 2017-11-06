1b41d330adad44cd0cf2b2c3eca34d8fef92c99b
George Gecewicz <gecewicz.george@gmail.com>
2017-11-06 13:23:00 -0500

    Initial commit

--- src/Tribe/Asset/Calendar_Script.php
+++ src/Tribe/Asset/Calendar_Script.php
@@ -15,6 +15,10 @@ class Tribe__Events__Asset__Calendar_Script extends Tribe__Events__Asset__Abstra
 			'events_base'        => tribe_get_events_link(),
 		);
 
+		if ( apply_filters( 'tribe_force_filtered_ical_link', false ) ) {
+			$js_config_array['force_filtered_ical_link'] = true;
+		}
+
 		wp_localize_script( $handle, 'tribe_js_config', $js_config_array );
 	}
 }
