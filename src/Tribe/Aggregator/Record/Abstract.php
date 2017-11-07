2f8cf302fd507de8379f2f499c1736d3b3825ce3
theAverageDev (Luca Tumedei) <luca@theaveragedev.com>
2017-11-07 11:40:39 -0800

    set the queue status to `fetch` in place of an empty array

--- src/Tribe/Aggregator/Record/Abstract.php
+++ src/Tribe/Aggregator/Record/Abstract.php
@@ -577,8 +577,8 @@ abstract class Tribe__Events__Aggregator__Record__Abstract {
 			$post['meta_input'][ self::$meta_key_prefix . $key ] = $value;
 		}
 
-		// add the queue meta information, empty for the time being, to the child record; it will be filled in later
-		$post['meta_input'][ self::$meta_key_prefix . Tribe__Events__Aggregator__Record__Queue::$queue_key ] = array();
+		// initialize the queue meta entry and set its status to fetching
+		$post['meta_input'][ self::$meta_key_prefix . Tribe__Events__Aggregator__Record__Queue::$queue_key ] = 'fetch';
 
 		$frequency = Tribe__Events__Aggregator__Cron::instance()->get_frequency( array( 'id' => $this->meta['frequency'] ) );
 		if ( ! $frequency ) {
