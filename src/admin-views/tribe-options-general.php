6fcfd20f046ebb76724ac9587c074cbf2ef3e72e
George Gecewicz <gecewicz.george@gmail.com>
2017-11-09 20:49:56 -0500

    Committing code solution for 92314

--- src/admin-views/tribe-options-general.php
+++ src/admin-views/tribe-options-general.php
@@ -64,11 +64,11 @@ $general_tab_fields = Tribe__Main::array_insert_before_key(
 			'validation_type' => 'boolean',
 		),
 		'unprettyPermalinksUrl'         => array(
-			'type'  => 'html',
+			'type'  => 'wrapped_html',
 			'label' => esc_html__( 'Events URL slug', 'the-events-calendar' ),
 			'html'  => '<p>'
 				. sprintf(
-					__( 'You cannot edit the slug for your events page as you do not have pretty permalinks enabled. The current URL for your events page is %1$s. In order to edit the slug here, <a href="%2$s">enable pretty permalinks</a>.', 'the-events-calendar' ),
+					__( 'The current URL for your events page is %1$s. <br><br> You cannot edit the slug for your events page as you do not have pretty permalinks enabled. In order to edit the slug here, <a href="%2$s">enable pretty permalinks</a>.', 'the-events-calendar' ),
 					sprintf (
 						'<a href="%1$s">%2$s</a>',
 						esc_url( $tec->getLink( 'home' ) ),
