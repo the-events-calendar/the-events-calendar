8e27b7e16f470f6ea05d63c4a8a006c98a6a742a
George Gecewicz <gecewicz.george@gmail.com>
2017-12-13 18:28:21 -0500

    Tweaking the docblock moment

--- src/Tribe/REST/V1/Endpoints/Archive_Event.php
+++ src/Tribe/REST/V1/Endpoints/Archive_Event.php
@@ -65,11 +65,18 @@ class Tribe__Events__REST__V1__Endpoints__Archive_Event
 		$args['s'] = $request['search'];
 
 		/**
-		 * Allows users to make the REST API return events from a more specific, localized date range.
+		 * Allows users to override "inclusive" start and end dates and  make the REST API use a 
+		 * timezone-adjusted date range.
 		 *
-		 * With this enabled, "inclusive" start dates and end dates are supplied: the first second
-		 * of the specified start date, and the last second of the specified end date. With this
-		 * disabled, the localized but otherwise-unmodified start and dates are used, which may yield fewer results.
+		 * Example: wp-json/tribe/events/v1/events?start_date=2017-12-21&end_date=2017-12-22
+		 *
+		 * - The "inclusive" behavior, which is the default here, would set start_date to
+		 *   2017-12-21 00:00:00 and end_date to 2017-12-22 23:59:59. Events within this range will
+		 *   be retrieved.
+		 *
+		 * - If you set this filter to false on a site whose timezone is America/New_York, then the
+		 *   REST API would set start_date to 2017-12-20 19:00:00 and end_date to
+		 *   2017-12-21 19:00:00. A different range of events to draw from.
 		 *
 		 * @since TBD
 		 *
