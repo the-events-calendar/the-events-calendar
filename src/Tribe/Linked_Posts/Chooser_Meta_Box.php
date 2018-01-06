a789829494db7946a9b500040ea989a7e70ad059
Clifford Paulick <tko@tourkick.com>
2018-01-06 06:55:54 -0600

    fix indentation

--- src/Tribe/Linked_Posts/Chooser_Meta_Box.php
+++ src/Tribe/Linked_Posts/Chooser_Meta_Box.php
@@ -162,6 +162,7 @@ class Tribe__Events__Linked_Posts__Chooser_Meta_Box {
 		?>
 		<tr class="saved-linked-post">
             <td class="saved-<?php echo esc_attr( $linked_post_type_container ); ?>-table-cell">
+			<td class="saved-<?php echo esc_attr( $linked_post_type_container ); ?>-table-cell">
 				<?php $this->move_handle(); ?>
 				<label
 					data-l10n-create-<?php echo esc_attr( $this->post_type ); ?>="<?php printf( esc_attr__( 'Create New %s', 'the-events-calendar' ), $this->singular_name ); ?>">
