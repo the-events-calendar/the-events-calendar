7eb5666d8ce7b7add4fe80a9e42ee573355695a1
barryhughes <barry@codingkills.me>
2018-01-25 12:56:26 -0800

    Prepare 4.6.10.1 | M18.02.1

--- src/Tribe/Main.php
+++ src/Tribe/Main.php
@@ -32,7 +32,7 @@ if ( ! class_exists( 'Tribe__Events__Main' ) ) {
 		const VENUE_POST_TYPE     = 'tribe_venue';
 		const ORGANIZER_POST_TYPE = 'tribe_organizer';
 
-		const VERSION             = '4.6.10';
+		const VERSION             = '4.6.10.1';
 		const MIN_ADDON_VERSION   = '4.4';
 		const MIN_COMMON_VERSION  = '4.7.3';
 
