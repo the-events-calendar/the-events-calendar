8bf58f0291415b5b8ec93e1932a3a536f23d3b4f
jesseeproductions <info@jesseeproductions.com>
2018-02-28 08:02:18 -0500

    #99056 - add back authorize coding due to changes

--- src/Tribe/Aggregator/Record/Eventbrite.php
+++ src/Tribe/Aggregator/Record/Eventbrite.php
@@ -41,15 +41,11 @@ class Tribe__Events__Aggregator__Record__Eventbrite extends Tribe__Events__Aggre
 	public static function get_auth_url( $args = array() ) {
 		$service = tribe( 'events-aggregator.service' );
 
-		$eb_api = (object) $service->api;
-		if ( defined( 'EVENT_AGGREGATOR_API_BASE_URL' ) ) {
-			$eb_api->domain = EVENT_AGGREGATOR_API_BASE_URL;
+		if ( $service->api() instanceof WP_Error ) {
+			return '';
 		}
 
-		$eb_api->key = tribe( 'eventbrite.pue' )->pue_instance->get_key();
-
-
-		$url = $eb_api->domain . 'eventbrite/' . $eb_api->key;
+		$url = $service->api()->domain . 'eventbrite/' . $service->api()->key;
 		$defaults = array(
 			'referral' => urlencode( home_url() ),
 			'admin_url' => urlencode( get_admin_url() ),
