6fe7613e36d47edd6b523c987083ed86e30c51f9
jesseeproductions <info@jesseeproductions.com>
2018-02-28 14:51:04 -0500

    #79126 - add ability to sort the order of the organizers

--- src/functions/template-tags/organizer.php
+++ src/functions/template-tags/organizer.php
@@ -61,6 +61,11 @@ if ( class_exists( 'Tribe__Events__Main' ) ) {
 				$organizer_ids = array_filter( (array) $organizer_ids );
 			}
 		}
+		// if there are linked post order use that instead of the current linked post to change the order
+		if ( ! empty( $organizer_ids_order = get_post_meta( $event_id, '_EventOrganizerID_Order', true ) ) ) {
+			$organizer_ids = $organizer_ids_order;
+		}
+
 		return apply_filters( 'tribe_get_organizer_ids', $organizer_ids, $event_id );
 	}
 
