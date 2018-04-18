2f499610556df99b0e5d3e3dfd1550a6951e61a3
mitogh <mitogh@gmail.com>
2018-04-18 15:50:54 -0500

    Format code follow styleguide

--- tests/wpunit/functions/template-tags/organizerTest.php
+++ tests/wpunit/functions/template-tags/organizerTest.php
@@ -66,21 +66,21 @@ class organizerTest extends Events_TestCase {
 	 *
 	 * @since TBD
 	 */
-	public function test_tribe_sanitize_organizers( ) {
+	public function test_tribe_sanitize_organizers() {
 		// Test empty values
-		$this->assertEquals( [], tribe_sanitize_organizers());
-		$this->assertEquals( [], tribe_sanitize_organizers([], []));
+		$this->assertEquals( [], tribe_sanitize_organizers() );
+		$this->assertEquals( [], tribe_sanitize_organizers( [], [] ) );
 		// Return original value if no order value is present
-		$this->assertEquals( [1,2,3], tribe_sanitize_organizers([1,2,3]));
+		$this->assertEquals( [ 1, 2, 3 ], tribe_sanitize_organizers( [ 1, 2, 3 ] ) );
 		// Test only values that are not ordered
-		$this->assertEquals( [1,2,3], tribe_sanitize_organizers([1,2,3], []));
+		$this->assertEquals( [ 1, 2, 3 ], tribe_sanitize_organizers( [ 1, 2, 3 ], [] ) );
 		// Same values on current and ordered side
-		$this->assertEquals( [1,2,3], tribe_sanitize_organizers([1,2,3], [1,2,3]));
+		$this->assertEquals( [ 1, 2, 3 ], tribe_sanitize_organizers( [ 1, 2, 3 ], [ 1, 2, 3 ] ) );
 		// Test values that are not part of the ordered list
-		$this->assertEquals( [4], tribe_sanitize_organizers([4], [1,2,3]));
+		$this->assertEquals( [ 4 ], tribe_sanitize_organizers( [ 4 ], [ 1, 2, 3 ] ) );
 		// Make sure order is respected if a new values are present
-		$this->assertEquals( [1,2,3,4,5,6], tribe_sanitize_organizers([4, 2, 5, 1, 3, 6], [1,2,3]));
+		$this->assertEquals( [ 1, 2, 3, 4, 5, 6 ], tribe_sanitize_organizers( [ 4, 2, 5, 1, 3, 6 ], [ 1, 2, 3 ] ) );
 		// Test only a single ordered value the remaining are placed are entered
-		$this->assertEquals( [2,1,3,4,5,6], tribe_sanitize_organizers([1, 2, 3, 4, 5, 6], [2]));
+		$this->assertEquals( [ 2, 1, 3, 4, 5, 6 ], tribe_sanitize_organizers( [ 1, 2, 3, 4, 5, 6 ], [ 2 ] ) );
 	}
 }
