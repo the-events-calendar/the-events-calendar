cbf4c37e159fc7d408922b68b27d1527a9b0b8ce
jesseeproductions <info@jesseeproductions.com>
2018-04-19 12:17:07 -0400

    #104641 - code formatting fix

--- src/Tribe/Aggregator/Record/Eventbrite.php
+++ src/Tribe/Aggregator/Record/Eventbrite.php
@@ -129,7 +129,7 @@ class Tribe__Events__Aggregator__Record__Eventbrite extends Tribe__Events__Aggre
 			return $args;
 		}
 
-		$args['site'] = urlencode( site_url() );
+		$args['site']     = urlencode( site_url() );
 		$args['security'] = tribe( 'events-aggregator.settings' )->get_eb_security_key()->security_key;
 
 		return $args;
