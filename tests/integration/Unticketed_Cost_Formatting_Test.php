de6c18d1a63deb604c6eb295597f12609fda43da
George Gecewicz <gecewicz.george@gmail.com>
2018-05-15 11:24:16 -0400

    implement luca's code review feedback

--- tests/integration/Unticketed_Cost_Formatting_Test.php
+++ tests/integration/Unticketed_Cost_Formatting_Test.php
@@ -2,7 +2,9 @@
 
 namespace Tribe\Events;
 
-class Unticketed_Cost_Formatting_Test extends \Codeception\TestCase\WPTestCase {
+use Tribe\Events\Test\Testcases\Events_TestCase;
+
+class Unticketed_Cost_Formatting_Test extends Events_TestCase {
 
 	/**
 	 * Container for our test events. Each event will be stored as an array of
@@ -25,67 +27,7 @@ class Unticketed_Cost_Formatting_Test extends \Codeception\TestCase\WPTestCase {
 	 * @since TBD
 	 */
 	public function setUp() {
-		// This needs to come first so that the post_example_settings template is created
 		parent::setUp();
-
-		$this->post_example_settings = array(
-			'post_author'           => 3,
-			'post_title'            => 'Unticketed Cost Formatting Test Event ',
-			'post_content'          => 'Testing out cost formatting on events when neither Event Tickets nor Event Tickets Plus are active.',
-			'post_status'           => 'publish',
-			'EventAllDay'           => false,
-			'EventHideFromUpcoming' => true,
-			'EventOrganizerID'      => 5,
-			'EventVenueID'          => 8,
-			'EventShowMapLink'      => true,
-			'EventShowMap'          => true,
-			'EventStartDate'        => '2012-01-01',
-			'EventEndDate'          => '2012-01-03',
-			'EventStartHour'        => '01',
-			'EventStartMinute'      => '15',
-			'EventStartMeridian'    => 'am',
-			'EventEndHour'          => '03',
-			'EventEndMinute'        => '25',
-			'EventEndMeridian'      => 'pm',
-			'EventCurrencySymbol'   => '$'
-		);
-
-		$costs = [
-			null, // A null value means the event has no cost, as distinct from being "Free".
-			0,    // Should output "Free" string.
-			'4,92',
-			'4,999',
-			'5',
-			'5.99',
-			'25',
-			'100',
-			'180.067',
-			'3.00 8.00 125.95', // Represents a range of values.
-			'Free',             // The cost field should not manipulate this, and just output it literally.
-			'*&^$@#%@',         // Ditto: Should not be manipulated, just output literally.
-			'東京は都会です',     // Ditto: Should not be manipulated, just output literally.
-			'1995.95',
-		];
-
-		$iterations = 0;
-
-		foreach ( $costs as $event_cost ) {
-
-			$iterations ++;
-
-			$new_event                    = $this->post_example_settings;
-			$new_event['post_title']     .= uniqid();
-			$new_event['EventStartDate']  = date_i18n( 'Y-m-d', strtotime( "+$iterations days" ) );
-
-			if ( null !== $event_cost ) {
-				$new_event['EventCost'] = $event_cost;
-			}
-
-			$this->test_events[] = [
-				$event_cost,
-				tribe_create_event( $new_event )
-			];
-		}
 	}
 
 	/**
@@ -112,33 +54,44 @@ class Unticketed_Cost_Formatting_Test extends \Codeception\TestCase\WPTestCase {
 	 * @since TBD
 	 */
 	public function test_single_numeric_event_cost_formatting() {
-		$settings = $this->post_example_settings;
 
 		// "$4,92"
-		$settings['EventCost']             = '4,92';
-		$settings['EventCurrencyPosition'] = 'prefix';
-		$post_id_1                         = tribe_create_event( $settings );
+		$post_id_1 = $this->factory()->event->create( [
+			'meta_input' => [
+				'_EventCost'             => '4,92',
+				'_EventCurrencyPosition' => 'prefix',
+			]
+		] );
 
 		$this->assertEquals( '$4,92', tribe_get_cost( $post_id_1, true ), 'Simply add the currency symbol in the correct location.' );
 
-		// "4,92$"
-		$settings['EventCost']             = '4,92';
-		$settings['EventCurrencyPosition'] = 'suffix';
-		$post_id_2                         = tribe_create_event( $settings );
+		// // "4,92$"
+		$post_id_2 = $this->factory()->event->create( [
+			'meta_input' => [
+				'_EventCost'             => '4,92',
+				'_EventCurrencyPosition' => 'suffix',
+			]
+		] );
 
 		$this->assertEquals( '4,92$', tribe_get_cost( $post_id_2, true ), 'Simply add the currency symbol in the correct location.' );
 
 		// "$180.067"
-		$settings['EventCost']             = '180.067';
-		$settings['EventCurrencyPosition'] = 'prefix';
-		$post_id_3                         = tribe_create_event( $settings );
+		$post_id_3 = $this->factory()->event->create( [
+			'meta_input' => [
+				'_EventCost'             => '180.067',
+				'_EventCurrencyPosition' => 'prefix',
+			]
+		] );
 
 		$this->assertEquals( '$180.067', tribe_get_cost( $post_id_3, true ), 'Simply add the currency symbol in the correct location.'  );
 
 		// "180.067$"
-		$settings['EventCost']             = '180.067';
-		$settings['EventCurrencyPosition'] = 'suffix';
-		$post_id_4                         = tribe_create_event( $settings );
+		$post_id_4 = $this->factory()->event->create( [
+			'meta_input' => [
+				'_EventCost'             => '180.067',
+				'_EventCurrencyPosition' => 'suffix',
+			]
+		] );
 
 		$this->assertEquals( '180.067$', tribe_get_cost( $post_id_4, true ), 'Simply add the currency symbol in the correct location.' );
 	}
@@ -149,26 +102,34 @@ class Unticketed_Cost_Formatting_Test extends \Codeception\TestCase\WPTestCase {
 	 * @since TBD
 	 */
 	public function test_non_numeric_non_null_event_costs_formatting() {
-		$settings = $this->post_example_settings;
 
 		// "Free"
-		$settings['EventCost']             = 'Free';
-		$settings['EventCurrencyPosition'] = 'prefix';
-		$post_id_1                         = tribe_create_event( $settings );
+		$post_id_1 = $this->factory()->event->create( [
+			'meta_input' => [
+				'_EventCost'             => 'Free',
+				'_EventCurrencyPosition' => 'prefix',
+			]
+		] );
 
 		$this->assertEquals( 'Free', tribe_get_cost( $post_id_1, true ), 'The string "Free" should be escaped and rendered as-is, with no added currency symbol.' );
 
-		// "東京は都会です"
-		$settings['EventCost']             = '*&^$@#%@';
-		$settings['EventCurrencyPosition'] = 'suffix';
-		$post_id_2                         = tribe_create_event( $settings );
+		// "*&^$@#%@"
+		$post_id_2 = $this->factory()->event->create( [
+			'meta_input' => [
+				'_EventCost'             => '*&^$@#%@',
+				'_EventCurrencyPosition' => 'suffix',
+			]
+		] );
 
 		$this->assertEquals( '*&amp;^$@#%@', tribe_get_cost( $post_id_2, true ), 'The string "*&^$@#%@" should be escaped and rendered as-is, with no added currency symbol.' );
 
 		// "東京は都会です"
-		$settings['EventCost']             = '東京は都会です';
-		$settings['EventCurrencyPosition'] = 'suffix';
-		$post_id_3                         = tribe_create_event( $settings );
+		$post_id_3 = $this->factory()->event->create( [
+			'meta_input' => [
+				'_EventCost'             => '東京は都会です',
+				'_EventCurrencyPosition' => 'suffix',
+			]
+		] );
 
 		$this->assertEquals( '東京は都会です', tribe_get_cost( $post_id_3, true ), 'The string "東京は都会です" should be escaped and rendered as-is, with no added currency symbol.' );
 	}
