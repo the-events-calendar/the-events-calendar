c1de08e51c73102cd750f0a01900ecfbf72ab96a
George Gecewicz <gecewicz.george@gmail.com>
2018-05-16 11:20:24 -0400

    saving progress with implementing dataProvider

--- tests/integration/Unticketed_Cost_Formatting_Test.php
+++ tests/integration/Unticketed_Cost_Formatting_Test.php
@@ -7,21 +7,6 @@ use Tribe\Events\Test\Testcases\Events_TestCase;
 class Unticketed_Cost_Formatting_Test extends Events_TestCase {
 
 	/**
-	 * Container for our test events. Each event will be stored as an array of
-	 * [ cost, post_id ] - in other words, this is an array of arrays.
-	 *
-	 * @var array
-	 */
-	protected $test_events = [];
-
-	/**
-	 * Data and meta data for the test posts we'll be using.
-	 *
-	 * @var array
-	 */
-	protected $post_example_settings;
-
-	/**
 	 * Set some default/common event meta to be used across test events.
 	 *
 	 * @since TBD
@@ -39,6 +24,33 @@ class Unticketed_Cost_Formatting_Test extends Events_TestCase {
 		parent::tearDown();
 	}
 
+	public function costs_and_currency_positions() {
+		return [
+			[ '4,92',             'prefix', '$4,92' ],
+			[ '4,92',             'suffix', '4,92$' ],
+			[ '180.067',          'prefix', '$180.067' ],
+			[ '180.067',          'suffix', '180.067$' ],
+			[ 'Free',             'prefix', 'Free' ],
+			[ 'Free',             'suffix', 'Free' ],
+			// [ 'd0 l33t sp35k',    'prefix', 'd0 l33t sp35k' ], @todo Will currently return "Free - $35", need to fix.
+			// [ 'd0 l33t sp35k',    'suffix', 'd0 l33t sp35k' ], @todo Will currently return "Free - 35$", need to fix.
+			[ '*&^$@#%@',         'prefix', '*&amp;^$@#%@' ],
+			[ '*&^$@#%@',         'suffix', '*&amp;^$@#%@' ],
+			[ '東京は都会です',     'prefix', '東京は都会です' ],
+			[ '東京は都会です',     'suffix', '東京は都会です' ],
+			[ '3.00 8.00 125.95', 'prefix', '$3.00 - $125.95' ],
+			[ '3.00 8.00 125.95', 'suffix', '3.00$ - 125.95$' ],
+			[ '125.95 3 8.00',    'prefix', '$3 - $125.95' ],
+			[ '125.95 3 8.00',    'suffix', '3$ - 125.95$' ],
+			[ null,               'prefix', '' ],
+			[ null,               'suffix', '' ],
+			[ 0,                  'prefix', 'Free' ],
+			[ 0,                  'suffix', 'Free' ],
+			[ '0',                'prefix', 'Free' ],
+			[ '0',                'suffix', 'Free' ],
+		];
+	}
+
 	/**
 	 * Formatting methods rely on Cost Utils heavily, so ensure it exists.
 	 *
@@ -49,111 +61,22 @@ class Unticketed_Cost_Formatting_Test extends Events_TestCase {
 	}
 
 	/**
-	 * Test for single numeric event costs.
+	 * @param mixed $cost_input
+	 * @param string $currency_position
 	 *
-	 * @since TBD
+	 * @dataProvider costs_and_currency_positions
 	 */
-	public function test_single_numeric_event_cost_formatting() {
+	public function test_unticketed_event_cost_formatting( $cost_input, $currency_position, $output ) {
 
-		// "$4,92"
-		$post_id_1 = $this->factory()->event->create( [
+		$event_id = $this->factory()->event->create( [
 			'meta_input' => [
-				'_EventCost'             => '4,92',
-				'_EventCurrencyPosition' => 'prefix',
+				'_EventCost'             => $cost_input,
+				'_EventCurrencyPosition' => $currency_position,
 			]
 		] );
 
-		$this->assertEquals( '$4,92', tribe_get_cost( $post_id_1, true ), 'Simply add the currency symbol in the correct location.' );
-
-		// // "4,92$"
-		$post_id_2 = $this->factory()->event->create( [
-			'meta_input' => [
-				'_EventCost'             => '4,92',
-				'_EventCurrencyPosition' => 'suffix',
-			]
-		] );
-
-		$this->assertEquals( '4,92$', tribe_get_cost( $post_id_2, true ), 'Simply add the currency symbol in the correct location.' );
-
-		// "$180.067"
-		$post_id_3 = $this->factory()->event->create( [
-			'meta_input' => [
-				'_EventCost'             => '180.067',
-				'_EventCurrencyPosition' => 'prefix',
-			]
-		] );
-
-		$this->assertEquals( '$180.067', tribe_get_cost( $post_id_3, true ), 'Simply add the currency symbol in the correct location.'  );
-
-		// "180.067$"
-		$post_id_4 = $this->factory()->event->create( [
-			'meta_input' => [
-				'_EventCost'             => '180.067',
-				'_EventCurrencyPosition' => 'suffix',
-			]
-		] );
-
-		$this->assertEquals( '180.067$', tribe_get_cost( $post_id_4, true ), 'Simply add the currency symbol in the correct location.' );
+		$this->assertEquals( $output, tribe_get_cost( $event_id, true ) );
 	}
-
-	/**
-	 * Test for non-numeric event cost values (that aren't "null" either).
-	 *
-	 * @since TBD
-	 */
-	public function test_non_numeric_non_null_event_costs_formatting() {
-
-		// "Free"
-		$post_id_1 = $this->factory()->event->create( [
-			'meta_input' => [
-				'_EventCost'             => 'Free',
-				'_EventCurrencyPosition' => 'prefix',
-			]
-		] );
-
-		$this->assertEquals( 'Free', tribe_get_cost( $post_id_1, true ), 'The string "Free" should be escaped and rendered as-is, with no added currency symbol.' );
-
-		// "*&^$@#%@"
-		$post_id_2 = $this->factory()->event->create( [
-			'meta_input' => [
-				'_EventCost'             => '*&^$@#%@',
-				'_EventCurrencyPosition' => 'suffix',
-			]
-		] );
-
-		$this->assertEquals( '*&amp;^$@#%@', tribe_get_cost( $post_id_2, true ), 'The string "*&^$@#%@" should be escaped and rendered as-is, with no added currency symbol.' );
-
-		// "東京は都会です"
-		$post_id_3 = $this->factory()->event->create( [
-			'meta_input' => [
-				'_EventCost'             => '東京は都会です',
-				'_EventCurrencyPosition' => 'suffix',
-			]
-		] );
-
-		$this->assertEquals( '東京は都会です', tribe_get_cost( $post_id_3, true ), 'The string "東京は都会です" should be escaped and rendered as-is, with no added currency symbol.' );
-	}
-
-	/**
-	 * Test for when the event cost is null.
-	 *
-	 * @todo
-	 */
-	// public function test_null_event_cost_formatting() {}
-
-	/**
-	 * Test for when the event cost is the int 0.
-	 *
-	 * @todo should output "Free" string
-	 */
-	// public function test_zero_int_event_cost_formatting() {}
-
-	/**
-	 * Test for when there are a number of costs entered into the event cost field.
-	 *
-	 * @todo When something like "3.00 8.00 125.95", should output "$3.00 - $125.95" or "3.00$ - 125.95$" depending on post meta option, test both.
-	 */
-	// public function test_cost_range_event_cost_formatting() {}
 
 	/**
 	 * Test to ensure the "currency follows value" option in Tribe General Settings is the default
