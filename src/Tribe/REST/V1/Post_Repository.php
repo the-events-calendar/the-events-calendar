f67d67d0b730005e9a046b7b8e296937d8ebf941
theAverageDev (Luca Tumedei) <luca@theaveragedev.com>
2018-05-24 15:34:10 +0200

    make the check at the correct time

--- src/Tribe/REST/V1/Post_Repository.php
+++ src/Tribe/REST/V1/Post_Repository.php
@@ -62,11 +62,12 @@ class Tribe__Events__REST__V1__Post_Repository implements Tribe__Events__REST__I
 	 */
 	public function get_event_data( $event_id, $context = '' ) {
 		$event = get_post( $event_id );
-		$event_id = $event->ID;
 
 		if ( empty( $event ) || ! tribe_is_event( $event ) ) {
 			return new WP_Error( 'event-not-found', $this->messages->get_message( 'event-not-found' ) );
 		}
+
+		$event_id = $event->ID;
 
 		$meta = array_map( 'reset', get_post_custom( $event_id ) );
 
