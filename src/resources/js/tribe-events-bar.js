5be90c4023a1c0c996bf50a791c0163048e197a4
Juanfra Aldasoro <juan@nicethemes.com>
2018-06-01 17:36:23 -0300

    Yoda conditionals and remove unnecessary conditional for date when changing from month view.

--- src/resources/js/tribe-events-bar.js
+++ src/resources/js/tribe-events-bar.js
@@ -294,22 +294,16 @@ var tribe_events_bar_action;
 
 			tribe_events_bar_action = 'change_view';
 
-			if ( ts.view === 'month' && $tribedate.length ) {
+			if ( 'month' === ts.view && $tribedate.length ) {
 				var dp_date = $tribedate.val();
 				var day     = tf.get_day();
 
-				if ( ts.datepicker_format !== '0' ) {
-					if ( day.length ) {
-						dp_date = tribeDateFormat( $tribedate.bootstrapDatepicker( 'getDate' ), 'tribeMonthQuery' );
-						$tribedate.val( dp_date + day );
-					}
-					else {
-						$tribedate.val( '' );
-					}
-
+				if ( '0' !== ts.datepicker_format ) {
+					dp_date = tribeDateFormat( $tribedate.bootstrapDatepicker( 'getDate' ), 'tribeMonthQuery' );
+					$tribedate.val( dp_date + day );
 				}
 				else {
-					if ( dp_date.length === 7 ) {
+					if ( 7 === dp_date.length ) {
 						$tribedate.val( dp_date + day );
 					}
 				}
@@ -331,8 +325,8 @@ var tribe_events_bar_action;
 
 			$inputs.each( function() {
 				var $this = $( this );
-				if ( $this.val() && $this.val().length && !$this.hasClass( 'tribe-no-param' ) ) {
-					if ( ts.view !== 'month' && '0' !== ts.datepicker_format && $this.is( $tribedate ) ) {
+				if ( $this.val() && $this.val().length && ! $this.hasClass( 'tribe-no-param' ) ) {
+					if ( 'month' !== ts.view  && '0' !== ts.datepicker_format && $this.is( $tribedate ) ) {
 
 						ts.url_params[ $this.attr( 'name' ) ] = tribeDateFormat( $this.bootstrapDatepicker( 'getDate' ), 'tribeQuery' );
 
