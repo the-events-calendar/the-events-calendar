1e10798f49969016415326897bbdf0248312b7a9
Juanfra Aldasoro <juan@nicethemes.com>
2018-06-18 14:55:24 -0300

    Revert changes from https://github.com/moderntribe/the-events-calendar/pull/1908

--- src/Tribe/Integrations/WPML/WPML.php
+++ src/Tribe/Integrations/WPML/WPML.php
@@ -66,10 +66,6 @@ class Tribe__Events__Integrations__WPML__WPML {
 		$language_switcher = Tribe__Events__Integrations__WPML__Language_Switcher::instance();
 		add_filter( 'icl_ls_languages', array( $language_switcher, 'filter_icl_ls_languages' ), 5 );
 
-		$meta = Tribe__Events__Integrations__WPML__Meta::instance();
-		add_filter( 'get_post_metadata', array( $meta, 'translate_post_id' ), 10, 3 );
-		add_filter( 'pre_get_posts', array( $meta, 'include_all_languages' ) );
-
 		// Disable month view caching when WPML is activated for now, until we
 		// fully implement multilingual support for the month view cache.
 		add_filter( 'tribe_events_enable_month_view_cache', '__return_false' );
