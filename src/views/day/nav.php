b21c32621f31426278e3a1b97c2edacfe8dffb8f
Juanfra Aldasoro <juan@nicethemes.com>
2018-06-19 12:58:46 -0300

    Bump and set views version number.

--- src/views/day/nav.php
+++ src/views/day/nav.php
@@ -6,7 +6,7 @@
  * Override this template in your own theme by creating a file at [your-theme]/tribe-events/day/nav.php
  *
  * @package TribeEventsCalendar
- * @version 4.2
+ * @version 4.6.19
  *
  */
 
