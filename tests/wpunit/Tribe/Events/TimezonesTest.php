7996dbd426978883399c64a6c874a64516b6e6db
Clifford Paulick <tko@tourkick.com>
2018-06-28 21:47:16 -0500

    Test `Timezones::get_event_timezone_string()`

--- tests/wpunit/Tribe/Events/TimezonesTest.php
+++ tests/wpunit/Tribe/Events/TimezonesTest.php
@@ -65,4 +65,48 @@ class TimezonesTest extends \Codeception\TestCase\WPTestCase
         
         $this->assertMatchesSnapshot( $output, $this->driver );
     }
+
+	/**
+	 * Event time zone string should be returned, not site-wide time zone.
+	 *
+	 * @since TBD
+	 */
+	public function test_get_event_timezone_string_should_return_event_meta_time_zone_if_set() {
+		$event_id = $this->factory()->event->create( [
+			'when'       => '2018-07-01 08:00:00',
+			'meta_input' => [
+				'_EventTimezone' => 'America/Chicago'
+			]
+		] );
+
+		update_option( 'timezone_string', 'America/New_York' );
+
+		// Site-wide general event time zone setting
+		tribe_update_option( 'tribe_events_timezone_mode', 'site' );
+
+		$event_tz_string = Timezones::get_event_timezone_string( $event_id );
+
+		$this->assertEquals( $event_tz_string, 'America/Chicago' );
+	}
+
+	/**
+	 * Event time zone string should fallback to site-wide time zone if event's is not set.
+	 *
+	 * @since TBD
+	 */
+	public function test_get_event_timezone_string_should_return_site_time_zone_if_event_meta_time_zone_not_set() {
+		$event_id = $this->factory()->event->create( [
+			'when' => '2018-07-01 08:00:00',
+		] );
+
+		update_option( 'timezone_string', 'America/New_York' );
+
+		// Local event timezone
+		tribe_update_option( 'tribe_events_timezone_mode', 'event' );
+
+		$event_tz_string = Timezones::get_event_timezone_string( $event_id );
+
+		$this->assertEquals( $event_tz_string, 'America/New_York' );
+	}
+
 }
