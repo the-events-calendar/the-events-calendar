b367248da61a301ef1ae152082840373f8e1a57d
Juanfra Aldasoro <juan@nicethemes.com>
2018-07-23 20:41:44 -0300

    Bump common version because we are going to need the updated assets functions.

--- src/Tribe/Main.php
+++ src/Tribe/Main.php
@@ -34,7 +34,7 @@ if ( ! class_exists( 'Tribe__Events__Main' ) ) {
 
 		const VERSION             = '4.6.20';
 		const MIN_ADDON_VERSION   = '4.4';
-		const MIN_COMMON_VERSION  = '4.7.16';
+		const MIN_COMMON_VERSION  = '4.7.18';
 
 		const WP_PLUGIN_URL       = 'https://wordpress.org/extend/plugins/the-events-calendar/';
 
