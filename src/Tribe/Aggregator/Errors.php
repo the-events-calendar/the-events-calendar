7d21f05ea3ca78c1c8d9944824990984f84c4f32
George Gecewicz <gecewicz.george@gmail.com>
2018-09-27 12:52:20 -0400

    removing more references...

--- src/Tribe/Aggregator/Errors.php
+++ src/Tribe/Aggregator/Errors.php
@@ -54,7 +54,6 @@ class Tribe__Events__Aggregator__Errors {
 		tribe_register_error( 'core:aggregator:invalid-edit-record-type', __( 'Only scheduled import records can be edited.', 'the-events-calendar' ) );
 		tribe_register_error( 'core:aggregator:invalid-event-id', __( 'Unable to find an event with the ID of %s.', 'the-events-calendar' ) );
 		tribe_register_error( 'core:aggregator:invalid-event-json', __( 'The Event Aggregator API responded with bad data. Please <a href="https://theeventscalendar.com/support/post/" target="_blank">contact support</a>.', 'the-events-calendar' ) );
-		tribe_register_error( 'core:aggregator:invalid-facebook-token', __( 'We received an invalid Facebook Token from the Service.', 'the-events-calendar' ) );
 		tribe_register_error( 'core:aggregator:invalid-import-id', __( 'Unable to find an import record with the ID of %s.', 'the-events-calendar' ) );
 		tribe_register_error( 'core:aggregator:invalid-image-path', __( 'Unable to attach an image to the event', 'the-events-calendar' ) );
 		tribe_register_error( 'core:aggregator:invalid-record-frequency', __( 'An invalid frequency was used when trying to create this scheduled import.', 'the-events-calendar' ) );
