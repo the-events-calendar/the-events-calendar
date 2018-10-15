3c7c9448744aace808b17617164896ef00bcf0c8
theAverageDev (Luca Tumedei) <luca@theaveragedev.com>
2018-10-15 18:53:45 +0200

    first batch of work for ORM

--- src/Tribe/Repositories/Event.php
+++ src/Tribe/Repositories/Event.php
@@ -31,13 +31,17 @@ class Tribe__Events__Repositories__Event extends Tribe__Repository {
 		parent::__construct();
 
 		$this->default_args = array(
-			'post_type'          => Tribe__Events__Main::POSTTYPE,
-			// Since we'll be handling the dates let's avoid filtering side-effects.
-			'do_not_inject_date' => true,
+			'post_type'              => Tribe__Events__Main::POSTTYPE,
+			// We'll be handling the dates, let's mark the query as a non-filtered one.
+			'tribe_suppress_filters' => true,
 		);
 
 		$this->schema = array_merge( $this->schema, array(
-			'all_day' => array( $this, 'filter_by_all_day' ),
+			'all_day'      => array( $this, 'filter_by_all_day' ),
+			'ends_before'  => array( $this, 'filter_by_ends_before' ),
+			'ends_after'   => array( $this, 'filter_by_ends_after' ),
+			'ends_between' => array( $this, 'filter_by_ends_between' ),
+			'multiday'     => array( $this, 'filter_by_multiday' ),
 		) );
 	}
 
@@ -50,7 +54,7 @@ class Tribe__Events__Repositories__Event extends Tribe__Repository {
 	 *
 	 * @return array|null An array of query arguments or null if modified with internal methods.
 	 */
-	public function filter_by_all_day( $all_day ) {
+	public function filter_by_all_day( $all_day = true ) {
 		if ( (bool) $all_day ) {
 			$this->by( 'meta_equals', '_EventAllDay', 'yes' );
 
@@ -74,5 +78,134 @@ class Tribe__Events__Repositories__Event extends Tribe__Repository {
 				),
 			),
 		);
+	}
+
+	/**
+	 * Filters events whose end date occurs before the provided date; fetch is not inclusive.
+	 *
+	 * @since TBD
+	 *
+	 * @param string|DateTime|int $datetime A `strtotime` parse-able string, a DateTime object or
+	 *                                      a timestamp.
+	 * @param string|DateTimeZone $timezone A timezone string, UTC offset or DateTimeZone object;
+	 *                                      defaults to the site timezone; this parameter is ignored
+	 *                                      if the `$datetime` parameter is a DatTime object.
+	 *
+	 * @return array An array of arguments that should be added to the WP_Query object.
+	 */
+	public function filter_by_ends_before( $datetime, $timezone = null ) {
+		$date = Tribe__Date_Utils::build_date_object( $datetime, $timezone )
+		                         ->setTimezone( new DateTimeZone( 'UTC' ) );
+
+		return array(
+			'meta_query' => array(
+				'ends-before' => array(
+					'key'     => '_EventEndDateUTC',
+					'compare' => '<',
+					'value'   => $date->format( 'Y-m-d H:i:s' ),
+					'type'    => 'DATETIME',
+				),
+			),
+		);
+	}
+
+	/**
+	 * Filters events whose end date occurs after the provided date; fetch is not inclusive.
+	 *
+	 * @since TBD
+	 *
+	 * @param string|DateTime|int $datetime A `strtotime` parse-able string, a DateTime object or
+	 *                                      a timestamp.
+	 * @param string|DateTimeZone $timezone A timezone string, UTC offset or DateTimeZone object;
+	 *                                      defaults to the site timezone; this parameter is ignored
+	 *                                      if the `$datetime` parameter is a DatTime object.
+	 *
+	 * @return array An array of arguments that should be added to the WP_Query object.
+	 */
+	public function filter_by_ends_after( $datetime, $timezone = null ) {
+		$date = Tribe__Date_Utils::build_date_object( $datetime, $timezone )
+		                         ->setTimezone( new DateTimeZone( 'UTC' ) );
+
+		return array(
+			'meta_query' => array(
+				'ends-after' => array(
+					'key'     => '_EventEndDateUTC',
+					'compare' => '>',
+					'value'   => $date->format( 'Y-m-d H:i:s' ),
+					'type'    => 'DATETIME',
+				),
+			),
+		);
+	}
+
+	/**
+	 * Filters events whose end date occurs between a set of dates; fetch is inclusive.
+	 *
+	 * @since TBD
+	 *
+	 * @param string|DateTime|int $start_datetime A `strtotime` parse-able string, a DateTime object or
+	 *                                            a timestamp.
+	 * @param string|DateTime|int $end_datetime   A `strtotime` parse-able string, a DateTime object or
+	 *                                            a timestamp.
+	 * @param string|DateTimeZone $timezone       A timezone string, UTC offset or DateTimeZone object;
+	 *                                            defaults to the site timezone; this parameter is ignored
+	 *                                            if the `$datetime` parameter is a DatTime object.
+	 */
+	public function filter_by_ends_between( $start_datetime, $end_datetime, $timezone = null ) {
+		$utc = new DateTimeZone( 'UTC' );
+
+		$lower = Tribe__Date_Utils::build_date_object( $start_datetime, $timezone )->setTimezone( $utc );
+		$upper = Tribe__Date_Utils::build_date_object( $end_datetime, $timezone )->setTimezone( $utc );
+
+		$this->by( 'meta_between', '_EventEndDateUTC', array(
+			$lower->format( 'Y-m-d H:i:s' ),
+			$upper->format( 'Y-m-d H:i:s' )
+		), 'DATETIME' );
+	}
+
+	/**
+	 * Filters events to include only those that match the provided multi day state.
+	 *
+	 * Please note that an event might be multi-day in its timezone but not in another;
+	 * this filter will make the check on the event times localized to the event timezone.
+	 * Furthermore the end of day cutoff is taken into account so, given a cutoff of 10PM
+	 * an event starting at 10:30PM and ending at 11AM is not multi-day.
+	 *
+	 * @since TBD
+	 *
+	 * @param bool $multiday Whether to filter by events that are or not multi-day.
+	 */
+	public function filter_by_multiday( $multiday = true ) {
+		global $wpdb;
+
+		$this->filter_query->join( "LEFT JOIN {$wpdb->postmeta} multiday_start_date "
+		                           . "ON ( {$wpdb->posts}.ID = multiday_start_date.post_id AND "
+		                           . "multiday_start_date.meta_key = '_EventStartDate' )" );
+		$this->filter_query->join( "LEFT JOIN {$wpdb->postmeta} multiday_end_date "
+		                           . "ON ( {$wpdb->posts}.ID = multiday_end_date.post_id AND "
+		                           . "multiday_end_date.meta_key = '_EventEndDate' )" );
+
+		// We're interested in the time only.
+		$end_of_day_cutoff = tribe_end_of_day( 'today', 'H:i:s' );
+
+		if ( '23:59:59' === $end_of_day_cutoff ) {
+			/*
+			 * An event is considered multi-day when the end date is not the same as the start date when
+			 * using the "natural" end-of-day cutoff.
+			 */
+			$compare = $multiday ? '!=' : '==';
+			$this->filter_query->where(
+				"DATE( multiday_end_date.meta_value ) {$compare} DATE( multiday_start_date.meta_value )"
+			);
+		} else {
+			/*
+			 * An event is considered multi-day when the end date is after the end-of-day cutoff of the start date.
+			 * Since the cut-off moves forward from midnight add 1 day to the start date.
+			 */
+			$compare = $multiday ? '>' : '<=';
+			$this->filter_query->where(
+				"multiday_end_date.meta_value {$compare} DATE_FORMAT( DATE_ADD( multiday_start_date.meta_value, INTERVAL 1 DAY ) , '%Y-%m-%d {$end_of_day_cutoff}' )"
+			);
+		}
 	}
 }
