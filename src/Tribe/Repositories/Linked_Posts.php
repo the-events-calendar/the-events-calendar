4eb9c0406a6c95679e4e83227d03af2a33b3cdfe
Scott Kingsley Clark <lol@scottkclark.com>
2018-10-24 01:22:08 -0500

    Initial Linked Posts work

---
+++ src/Tribe/Repositories/Linked_Posts.php
@@ -0,0 +1,87 @@
+<?php
+/**
+ * The main ORM/Repository class for linked posts.
+ *
+ * @since TBD
+ */
+
+/**
+ * Class Tribe__Events__Repositories__Linked_Posts
+ *
+ *
+ * @since TBD
+ */
+class Tribe__Events__Repositories__Linked_Posts extends Tribe__Repository {
+
+	/**
+	 * The unique fragment that will be used to identify this repository filters.
+	 *
+	 * @var string
+	 */
+	protected $filter_name = 'linked_posts';
+
+	/**
+	 * Tribe__Events__Repositories__Linked_Posts constructor.
+	 *
+	 * Sets up the repository default parameters and schema.
+	 *
+	 * @since TBD
+	 */
+	public function __construct() {
+		parent::__construct();
+
+		$this->default_args = array(
+			'post_type'                    => Tribe__Events__Venue::POSTTYPE,
+			// We'll be handling the dates, let's mark the query as a non-filtered one.
+			'tribe_suppress_query_filters' => true,
+		);
+
+		$this->schema = array_merge( $this->schema, array(
+			'event' => array( $this, 'filter_by_event' ),
+		) );
+	}
+
+	/**
+	 * Filter by LIKE or REGEX string.
+	 *
+	 * @param string $meta_key Meta key to filter by.
+	 * @param string $value    MySQL compatible LIKE or REGEX string.
+	 */
+	public function filter_by_like_regex( $meta_key, $value ) {
+		if ( tribe_is_regex( $value ) ) {
+			$this->by( 'meta_regexp', $meta_key, tribe_unfenced_regex( $value ) );
+
+			return;
+		}
+
+		$this->by( 'meta_like', $meta_key, $value );
+	}
+
+	/**
+	 * Filters linked post types by a specific event of set of events.
+	 *
+	 * @since TBD
+	 *
+	 * @param int|WP_Post|array $event Post ID, Post Object, or an array of Post IDs or Objects.
+	 */
+	public function filter_by_event( $event ) {
+		$events = (array) $event;
+
+		$post_ids = array();
+
+		foreach ( $events as $event_id_or_object ) {
+			$post_id = Tribe__Events__Main::postIdHelper( $event_id_or_object );
+
+			if ( ! $post_id ) {
+				continue;
+			}
+
+			$post_ids[] = $post_id;
+		}
+
+		$post_ids = array_unique( $post_ids );
+
+		// @todo Figure out logic.
+	}
+
+}
--- src/functions/template-tags/orm.php
+++ src/functions/template-tags/orm.php
@@ -37,3 +37,32 @@ if ( ! function_exists( 'tribe_events' ) ) {
 	}
 }
 
+if ( ! function_exists( 'tribe_linked_posts' ) ) {
+	/**
+	 * Builds and returns the correct linked posts repository.
+	 *
+	 * @since TBD
+	 *
+	 * @param string $repository The slug of the repository to build/return.
+	 *
+	 * @return Tribe__Repository__Interface An instance of the requested repository
+	 *                                      class.
+	 */
+	function tribe_linked_posts( $repository = 'default' ) {
+		$map = array(
+			'default' => 'events.linked-posts-repository',
+		);
+
+		/**
+		 * Filters the map relating linked posts repository slugs to service container bindings.
+		 *
+		 * @since TBD
+		 *
+		 * @param array  $map        A map in the shape [ <repository_slug> => <service_name> ]
+		 * @param string $repository The currently requested implementation.
+		 */
+		$map = apply_filters( 'tribe_events_linked_posts_repository_map', $map, $repository );
+
+		return tribe( Tribe__Utils__Array::get( $map, $repository, $map['default'] ) );
+	}
+}
