9ef75801268a195c91f0525d13f1693f9c305288
theAverageDev (Luca Tumedei) <luca@theaveragedev.com>
2018-11-01 17:45:32 +0100

    add test and delete/update suppport to events

--- tests/wpunit/Tribe/Events/ORM/Events/DeleteTest.php
+++ tests/wpunit/Tribe/Events/ORM/Events/DeleteTest.php
@@ -3,6 +3,7 @@
 namespace Tribe\Events\ORM\Events;
 
 use Tribe\Events\Test\Factories\Event;
+use Tribe__Promise as Promise;
 
 class DeleteTest extends \Codeception\TestCase\WPTestCase {
 
@@ -27,5 +28,28 @@ class DeleteTest extends \Codeception\TestCase\WPTestCase {
 		$deleted = tribe_events()->where( 'starts_after', 'today' )->delete();
 
 		$this->assertEqualSets($after_today, $deleted);
+	}
+
+	/**
+	 * It should allow deleting events and getting a promise
+	 *
+	 * @test
+	 */
+	public function should_allow_deleting_events_and_getting_a_promise() {
+		$filter_name = tribe_events()->get_filter_name();
+		add_filter( "tribe_repository_{$filter_name}_delete_background_activated", '__return_true' );
+		add_filter( "tribe_repository_{$filter_name}_delete_background_threshold", function () {
+			return 1;
+		} );
+		$after_today  = array_map( function ( $i ) {
+			return $this->factory()->event->create( [ 'when' => "+{$i} weeks" ] );
+		}, range( 1, 3 ) );
+		$before_today = array_map( function ( $i ) {
+			return $this->factory()->event->create( [ 'when' => "-{$i} weeks" ] );
+		}, range( 1, 3 ) );
+
+		$promise = tribe_events()->where( 'starts_after', 'today' )->delete( true );
+
+		$this->assertInstanceOf( Promise::class, $promise );
 	}
 }
