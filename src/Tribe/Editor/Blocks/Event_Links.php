6736c0d89952e169d9e7ee8b51be77fdd3313c78
mitogh <hello@crisoforo.com>
2018-11-13 23:14:25 -0600

    Update location of the blocks

--- src/Tribe/Editor/Blocks/Event_Links.php
+++ src/Tribe/Editor/Blocks/Event_Links.php
@@ -49,7 +49,7 @@ extends Tribe__Editor__Blocks__Abstract {
 		// Add the rendering attributes into global context
 		tribe( 'events.editor.template' )->add_template_globals( $args );
 
-		$html = tribe( 'events.editor.template' )->template( array( 'blocks', $this->slug() ), $args, false );
+		$html = tribe( 'events.editor.template' )->template( array( 'editor', 'blocks', $this->slug() ), $args, false );
 
 		remove_filter( 'the_content', 'strip_dynamic_blocks', 1 );
 
