079243790aaac90fbceaddeb49faf5559300921b
bordoni <bordoni.dev@gmail.com>
2018-11-15 15:22:15 -0200

    Bump version number to 4.7-beta1

--- src/Tribe/Main.php
+++ src/Tribe/Main.php
@@ -32,9 +32,9 @@ if ( ! class_exists( 'Tribe__Events__Main' ) ) {
 		const VENUE_POST_TYPE     = 'tribe_venue';
 		const ORGANIZER_POST_TYPE = 'tribe_organizer';
 
-		const VERSION             = '4.6.26';
+		const VERSION             = '4.7-beta1';
 		const MIN_ADDON_VERSION   = '4.4';
-		const MIN_COMMON_VERSION  = '4.7.20';
+		const MIN_COMMON_VERSION  = '4.8-beta';
 
 		const WP_PLUGIN_URL       = 'https://wordpress.org/extend/plugins/the-events-calendar/';
 
