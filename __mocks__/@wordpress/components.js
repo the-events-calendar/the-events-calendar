48e51c91600a674f261284220541e2a440b3df76
Neal Fennimore <hi@neal.codes>
2018-11-16 12:19:49 -0500

    Fix tests

---
+++ __mocks__/@wordpress/components.js
@@ -0,0 +1,17 @@
+/**
+ * External dependencies
+ */
+import { noop } from 'lodash';
+
+export const withAPIData = () => noop;
+export const Spinner = () => "🏃‍♂️";
+export const Modal = ( { title, children } ) => (
+	<div>
+		<span>{ title }</span>
+		<span>{ children }</span>
+	</div>
+);
+export const Dashicon = ( { className, icon } ) => <span className={ className }>{ icon }</span>;
+export const Dropdown = () => <span>Dropdown</span>;
+export const Tooltip = () => <span>Tooltip</span>;
+export const PanelBody = ({ children }) => <span className="PanelBody">{ children }</span>
