e4f58f5cd1b569f7242e61fc94c5556036fc6958
Neal Fennimore <hi@neal.codes>
2018-11-19 12:41:10 -0500

    Update start and end time date changes to automatically adjust time

--- src/modules/data/blocks/datetime/sagas.js
+++ src/modules/data/blocks/datetime/sagas.js
@@ -13,7 +13,14 @@ import {
 	actions,
 	thunks,
 } from '@moderntribe/events/data/blocks/datetime';
-import { date as dateUtil } from '@moderntribe/common/utils';
+import { date as dateUtil, moment as momentUtil, time as timeUtil } from '@moderntribe/common/utils';
+
+const {
+	HALF_HOUR_IN_SECONDS,
+	DAY_IN_SECONDS,
+	HOUR_IN_SECONDS,
+	MINUTE_IN_SECONDS,
+} = timeUtil;
 
 /**
  * Set the human readable label into the store, based on a start and end date to generate a new label based on those
@@ -111,6 +118,135 @@ export function* onTimeZoneChange( action ) {
 }
 
 /**
+ * Convert current start and end date into seconds
+ *
+ * @export
+ * @returns {Object} {start, end}
+ */
+export function* deriveMomentsFromDates() {
+	const dates = yield all( {
+		start: select( selectors.getStart ),
+		end: select( selectors.getEnd ),
+	} );
+
+	return yield all( {
+		start: yield call( momentUtil.toMoment, dates.start ),
+		end: yield call( momentUtil.toMoment, dates.end ),
+	} );
+}
+
+/**
+ * Convert current start and end date into seconds
+ *
+ * @export
+ * @returns {Object} {start, end}
+ */
+export function* deriveSecondsFromDates() {
+	const moments = yield call( deriveMomentsFromDates );
+
+	return yield all( {
+		start: yield call( timeUtil.toSeconds, moments.start.format( timeUtil.TIME_FORMAT_HH_MM_SS ) ),
+		end: yield call( timeUtil.toSeconds, moments.end.format( timeUtil.TIME_FORMAT_HH_MM_SS ) ),
+	} );
+}
+
+/**
+ * Convert current start and end seconds into time object
+ *
+ * @export
+ * @returns {Object} {start, end}
+ */
+export function* deriveTimeFromSeconds( { start, end } ) {
+	const startTime = yield call( timeUtil.fromSeconds, start, timeUtil.TIME_FORMAT_HH_MM_SS );
+	const endTime = yield call( timeUtil.fromSeconds, end, timeUtil.TIME_FORMAT_HH_MM_SS );
+
+	const [ startHour, startMinute, startSecond ] = yield call( [ startTime, 'split' ], ':' );
+	const [ endHour, endMinute, endSecond ] = yield call( [ endTime, 'split' ], ':' );
+
+	return {
+		start: {
+			hour: startHour,
+			minute: startMinute,
+			second: startSecond,
+		},
+		end: {
+			hour: endHour,
+			minute: endMinute,
+			second: endSecond,
+		},
+	};
+}
+
+/**
+ * Prevents end time from being before start time.
+ * Should only prevent when not a multi-day event.
+ *
+ * @export
+ * @param {Object} { actions } Actions for syncing
+ * @param {Object} { startTime, endTime } Start and end time
+ * @param {Object} action Action received
+ */
+export function* preventEndTimeBeforeStartTime() {
+	const seconds = yield call( deriveSecondsFromDates );
+
+	// 	// If end time is earlier than start time, fix time
+	if ( seconds.end <= seconds.start ) {
+		// If there is less than half an hour left in the day, roll back one hour
+		if ( seconds.start + HALF_HOUR_IN_SECONDS >= DAY_IN_SECONDS ) {
+			seconds.start -= HOUR_IN_SECONDS;
+		}
+
+		seconds.end = seconds.start + HALF_HOUR_IN_SECONDS;
+
+		const time = yield call( deriveTimeFromSeconds, seconds );
+		const moments = yield call( deriveMomentsFromDates );
+		const dates = {
+			start: moments.start.set( time.start ),
+			end: moments.end.set( time.end ),
+		};
+
+		yield put( thunks.setDateTime( dates ) );
+	}
+}
+
+/**
+ * Prevents start time from appearing ahead of end time.
+ * Should only prevent when not a multi-day event.
+ *
+ * @export
+ * @param {Object} { actions } Actions for syncing
+ * @param {Object} { startTime, endTime } Start and end time
+ * @param {Object} action Action received
+ */
+export function* preventStartTimeAfterEndTime() {
+	const seconds = yield call( deriveSecondsFromDates );
+
+	if ( seconds.start >= seconds.end ) {
+		seconds.start = Math.max( seconds.end - HALF_HOUR_IN_SECONDS, 0 );
+		seconds.end = Math.max( seconds.start + MINUTE_IN_SECONDS, seconds.end );
+
+		const time = yield call( deriveTimeFromSeconds, seconds );
+		const moments = yield call( deriveMomentsFromDates );
+		const dates = {
+			start: moments.start.set( time.start ),
+			end: moments.end.set( time.end ),
+		};
+
+		yield put( thunks.setDateTime( dates ) );
+	}
+}
+
+export function* handleStartDateTimeChange( action ) {
+	yield call( setHumanReadableFromDate, action );
+	yield call( preventEndTimeBeforeStartTime );
+}
+
+export function* handleEndDateTimeChange( action ) {
+	yield call( setHumanReadableFromDate, action );
+	yield call( preventStartTimeAfterEndTime );
+}
+
+/**
  * Watchers of actions and act accordingly to each.
  *
  * @since 0.3.1-alpha
@@ -118,8 +254,8 @@ export function* onTimeZoneChange( action ) {
  * @returns {IterableIterator<*|ForkEffect>}
  */
 export default function* watchers() {
-	yield takeEvery( types.SET_START_DATE_TIME, setHumanReadableFromDate );
-	yield takeEvery( types.SET_END_DATE_TIME, setHumanReadableFromDate );
+	yield takeEvery( types.SET_START_DATE_TIME, handleStartDateTimeChange );
+	yield takeEvery( types.SET_END_DATE_TIME, handleEndDateTimeChange );
 	yield takeEvery( types.SET_TIME_ZONE, onTimeZoneChange );
 	yield takeLatest( types.SET_NATURAL_LANGUAGE_LABEL, onHumanReadableChange );
 }
