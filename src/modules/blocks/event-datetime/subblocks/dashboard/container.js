a07b54903623a319a2f7958d7c7410fa669ac855
Neal Fennimore <hi@neal.codes>
2018-11-20 09:05:28 -0500

    Cleanup unused

--- src/modules/blocks/event-datetime/subblocks/dashboard/container.js
+++ src/modules/blocks/event-datetime/subblocks/dashboard/container.js
@@ -37,37 +37,9 @@ const onSelectDay = ( stateProps, dispatchProps ) => ( { from, to } ) => {
 const onStartTimePickerChange = ( stateProps, dispatchProps ) => ( e ) => {
 	const seconds = time.toSeconds( e.target.value );
 	dispatchProps.setStartTime( seconds );
-	// const { start, end } = stateProps;
-	// const { setStartTime } = dispatchProps;
-	// const [ hour, minute ] = e.target.value.split( ':' );
-
-	// const startMoment = moment.toMoment( start );
-	// const max = moment.toMoment( end ).clone().subtract( 1, 'minutes' );
-
-	// const startMomentCopy = startMoment.clone();
-	// startMomentCopy.set( 'hour', parseInt( hour, 10 ) );
-	// startMomentCopy.set( 'minute', parseInt( minute, 10 ) );
-	// startMomentCopy.set( 'second', 0 );
-
-	// // if ( startMomentCopy.isAfter( max ) ) {
-	// // 	return;
-	// // }
-
-	// const seconds = startMomentCopy.diff( startMoment.clone().startOf( 'day' ), 'seconds' );
-	// setStartTime( { start, seconds } );
 };
 
 const onStartTimePickerClick = ( stateProps, dispatchProps ) => ( value, onClose ) => {
-	// const { start, end } = stateProps;
-	// const { setStartTime, setAllDay } = dispatchProps;
-
-	// const isAllDay = value === 'all-day';
-
-	// if ( ! isAllDay ) {
-	// 	setStartTime( { start, seconds: value } );
-	// }
-
-	// setAllDay( { start, end, isAllDay } );
 	dispatchProps.setStartTime( value );
 	onClose();
 };
@@ -75,33 +47,9 @@ const onStartTimePickerClick = ( stateProps, dispatchProps ) => ( value, onClose
 const onEndTimePickerChange = ( stateProps, dispatchProps ) => ( e ) => {
 	const seconds = time.toSeconds( e.target.value );
 	dispatchProps.setEndTime( seconds );
-
-	// const { start, end } = stateProps;
-	// const { setEndTime } = dispatchProps;
-
-	// const endMoment = moment.toMoment( end );
-	// const min = moment.toMoment( start ).clone().add( 1, 'minutes' );
-
-	// const endMomentCopy = endMoment.clone();
-	// endMomentCopy.set( 'hour', parseInt( hour, 10 ) );
-	// endMomentCopy.set( 'minute', parseInt( minute, 10 ) );
-	// endMomentCopy.set( 'second', 0 );
-
-	// const seconds = endMomentCopy.diff( endMoment.clone().startOf( 'day' ), 'seconds' );
-	// setEndTime( { end, seconds } );
 };
 
 const onEndTimePickerClick = ( stateProps, dispatchProps ) => ( value, onClose ) => {
-	// const { start, end } = stateProps;
-	// const { setEndTime, setAllDay } = dispatchProps;
-
-	// const isAllDay = value === 'all-day';
-
-	// if ( ! isAllDay ) {
-	// 	setEndTime( { end, seconds: value } );
-	// }
-
-	// setAllDay( { start, end, isAllDay } );
 	dispatchProps.setEndTime( value );
 	onClose();
 };
