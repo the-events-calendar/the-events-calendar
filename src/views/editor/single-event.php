fa4fea08471c59dea1915fd55118541611fac58e
bordoni <bordoni.dev@gmail.com>
2018-11-26 11:46:34 -0200

    Start using template parts for the single-event.php view

--- src/views/editor/single-event.php
+++ src/views/editor/single-event.php
@@ -1,50 +1,28 @@
 <?php
 /**
  * Single Event Template
- * A single event. This displays the event title, description, meta, and
- * optionally, the Google map for the event, when the blocks editor is used
  *
- * Override this template in your own theme by creating a file at [your-theme]/tribe-events/blocks/single-event.php
+ * A single event complete template, divided in smaller template parts.
  *
- * @package TribeEventsCalendar
+ * Override this template in your own theme by creating a file at:
+ * [your-theme]/tribe/events/editor/single-event.php
+ *
+ * See more documentation about our Blocks Editor templating system.
+ *
+ * @link {INSERT_ARTCILE_LINK_HERE}
+ *
  * @version TBD
  *
  */
 
-if ( ! defined( 'ABSPATH' ) ) {
-	die( '-1' );
-}
-
-$events_label_singular = tribe_get_event_label_singular();
-$events_label_plural   = tribe_get_event_label_plural();
 $event_id = $this->get( 'post_id' );
 ?>
 
 <div id="tribe-events-content" class="tribe-events-single tribe-blocks-editor">
-
-	<p class="tribe-events-back">
-		<a href="<?php echo esc_url( tribe_get_events_link() ); ?>"> <?php printf( '&laquo; ' . esc_html_x( 'All %s', '%s Events plural label', 'the-events-calendar' ), $events_label_plural ); ?></a>
-	</p>
-
-	<?php tribe_the_notices() ?>
-
-	<?php the_title( '<h1 class="tribe-events-single-event-title">', '</h1>' ); ?>
-
-	<div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-		<?php the_content(); ?>
-	</div>
-
-	<?php
-	if ( tribe_get_option( 'showComments', false ) ) {
-		comments_template();
-	}
-	?>
-
-	<div id="tribe-events-footer">
-		<h3 class="tribe-events-visuallyhidden"><?php printf( esc_html__( '%s Navigation', 'the-events-calendar' ), $events_label_singular ); ?></h3>
-		<ul class="tribe-events-sub-nav">
-			<li class="tribe-events-nav-previous"><?php tribe_the_prev_event_link( '<span>&laquo;</span> %title%' ) ?></li>
-			<li class="tribe-events-nav-next"><?php tribe_the_next_event_link( '%title% <span>&raquo;</span>' ) ?></li>
-		</ul>
-	</div>
+	<?php $this->template( 'editor/parts/back-link' ); ?>
+	<?php $this->template( 'editor/parts/notices' ); ?>
+	<?php $this->template( 'editor/parts/title' ); ?>
+	<?php $this->template( 'editor/parts/content' ); ?>
+	<?php $this->template( 'editor/parts/comments' ); ?>
+	<?php $this->template( 'editor/parts/footer' ); ?>
 </div>
