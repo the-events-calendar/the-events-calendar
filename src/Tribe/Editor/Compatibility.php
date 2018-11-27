96fe7771686e714de235315cc9117aa5da764f48
bordoni <bordoni.dev@gmail.com>
2018-11-27 15:21:19 -0200

    Toggle with good future Proofing for WP 5.0 release

---
+++ src/Tribe/Editor/Compatibility.php
@@ -0,0 +1,81 @@
+
+<?php
+/**
+ * Events Editor Compatibility
+ *
+ * @since TBD
+ */
+class Tribe__Events__Editor__Compatibility {
+
+	/**
+	 * Gets if user toggled blocks editor on the settings
+	 *
+	 * @since TBD
+	 *
+	 * @return bool
+	 */
+	public function is_blocks_editor_toggled_on() {
+		$option = tribe_get_option( $this->get_toggle_blocks_editor_key(), false );
+
+		return tribe_is_truthy( $option );
+	}
+
+	/**
+	 * Gets the option key for toggling Blocks Editor active
+	 *
+	 * @since TBD
+	 *
+	 * @return string
+	 */
+	public function get_toggle_blocks_editor_key() {
+		return 'toggle_blocks_editor';
+	}
+
+	/**
+	 * Gets the option key for the Hidden Field of toggling blocks editor
+	 *
+	 * @since TBD
+	 *
+	 * @return string
+	 */
+	public function get_toggle_blocks_editor_hidden_key() {
+		return 'toggle_blocks_editor_hidden_field';
+	}
+
+	/**
+	 * Inserts the Toggle and Hidden Field for the Activation of Blocks Editor
+	 *
+	 * @since TBD
+	 *
+	 * @param array $fields Fields from Options General
+	 *
+	 * @return array
+	 */
+	public function insert_toggle_blocks_editor_field( $fields = array() ) {
+		if ( ! tribe( 'editor' )->is_wp_version() ) {
+			return $fields;
+		}
+
+		$insert_after = 'liveFiltersUpdate';
+		$insert_data = array(
+			$this->get_toggle_blocks_editor_key() => array(
+				'type'            => 'checkbox_bool',
+				'label'           => esc_html__( 'Activate Blocks Editor', 'the-events-calendar' ),
+				'tooltip'         => esc_html__( 'Check this box to use the Blocks Editor (aka: Gutenberg) to manage Events single Page.', 'the-events-calendar' ),
+				'default'         => false,
+				'validation_type' => 'boolean',
+				'attributes'      => array( 'id' => 'tribe-blocks-editor-toggle-field' ),
+			),
+			$this->get_toggle_blocks_editor_hidden_key() => array(
+				'type'            => 'checkbox_bool',
+				'label'           => esc_html__( 'Hidden Blocks Editor Config', 'the-events-calendar' ),
+				'default'         => false,
+				'validation_type' => 'boolean',
+				'attributes'      => array( 'id' => 'tribe-blocks-editor-toggle-hidden-field' ),
+			),
+		);
+
+		return Tribe__Main::array_insert_after_key( $insert_after, $fields, $insert_data );
+	}
+
+}
--- src/resources/js/tribe-settings.js
+++ src/resources/js/tribe-settings.js
@@ -15,3 +15,40 @@ jQuery( document ).ready( function( $ ) {
 	}
 
 } );
+
+( function( $, data ) {
+	"use strict";
+	var $document = $( document );
+
+	/**
+	 * Check hidden field when Unchecked when the base field is checked first
+	 */
+	$document.ready( function() {
+		// Verify that all WP variables exists
+		if ( -1 !== [ typeof pagenow, typeof typenow, typeof adminpage ].indexOf( 'undefined' ) ) {
+			return false;
+		}
+
+		var $container = $( '#tribe-field-toggle_blocks_editor' );
+		var $hiddenContainer = $( '#tribe-field-toggle_blocks_editor_hidden_field' );
+		var $field = $container.find( '#tribe-blocks-editor-toggle-field' );
+		var $hiddenField = $hiddenContainer.find( '#tribe-blocks-editor-toggle-hidden-field' );
+
+		var isFieldChecked = $field.is( ':checked' );
+		var isHiddenFieldChecked = $hiddenField.is( ':checked' );
+
+		// Once this field is check we bail forever
+		if ( isHiddenFieldChecked ) {
+			return;
+		}
+
+		// Only check the hidden field when we change the Field was checked
+		if ( isFieldChecked ) {
+			$field.one( 'change', function() {
+				// Check the hidden field
+				$hiddenField.prop( 'checked', true );
+			} );
+		}
+
+	} );
+}( jQuery, {} ) );
