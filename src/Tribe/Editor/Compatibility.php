45b279d6bbee3f29e566fdc32f053746b4948342
bordoni <bordoni.dev@gmail.com>
2018-11-28 16:17:44 -0200

    Add readmore Link

--- src/Tribe/Editor/Compatibility.php
+++ src/Tribe/Editor/Compatibility.php
@@ -100,12 +100,15 @@ class Tribe__Events__Editor__Compatibility {
 			return $fields;
 		}
 
+		$read_more_url = 'https://theeventscalendar.com/gutenberg-block-editor-news/?utm_source=tec&utm_medium=eventscalendarapp&utm_term=adminnotice&utm_campaign=gutenbergrelease&utm_content=ebook-gutenberg&cid=tec_eventscalendarapp_adminnotice_gutenbergrelease_ebook-gutenberg';
+		$read_more_link = sprintf( ' <a href="%2$s" target="_blank">%1$s</a>', esc_html__( 'Read More', 'the-events-calendar' ), esc_url( $read_more_url ) );
+
 		$insert_after = 'liveFiltersUpdate';
 		$insert_data = array(
 			$this->get_toggle_blocks_editor_key() => array(
 				'type'            => 'checkbox_bool',
 				'label'           => esc_html__( 'Activate Block Editor for Events', 'the-events-calendar' ),
-				'tooltip'         => esc_html__( 'Enable early access to the new Gutenberg block editor interface for creating events.', 'the-events-calendar' ),
+				'tooltip'         => esc_html__( 'Enable early access to the new Gutenberg block editor interface for creating events.', 'the-events-calendar' ) . $read_more_link,
 				'default'         => false,
 				'validation_type' => 'boolean',
 				'attributes'      => array( 'id' => 'tribe-blocks-editor-toggle-field' ),
