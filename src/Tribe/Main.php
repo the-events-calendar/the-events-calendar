9d443c6ab308cea628b50213e93a3efdbfb36a38
mitogh <hello@crisoforo.com>
2018-11-29 18:33:36 -0600

    Don't update MIN_COMMON_VERSION

--- src/Tribe/Main.php
+++ src/Tribe/Main.php
@@ -34,7 +34,7 @@ if ( ! class_exists( 'Tribe__Events__Main' ) ) {
 
 		const VERSION             = '4.7.1';
 		const MIN_ADDON_VERSION   = '4.4';
-		const MIN_COMMON_VERSION  = '4.8.1';
+		const MIN_COMMON_VERSION  = '4.8';
 
 		const WP_PLUGIN_URL       = 'https://wordpress.org/extend/plugins/the-events-calendar/';
 
