ffc431e5f26e3c4bef8b77054caaadc18fdf8574
jesseeproductions <info@jesseeproductions.com>
2018-12-04 21:23:58 -0500

    #116838 - add-on checking from parent

--- src/Tribe/Plugin_Register.php
+++ src/Tribe/Plugin_Register.php
@@ -6,8 +6,7 @@ class Tribe__Events__Plugin_Register extends Tribe__Abstract_Plugin_Register {
 	protected $main_class         = 'Tribe__Events__Main';
 	protected $dependencies = array(
 		'addon-dependencies' => array(
-			'Tribe__Events__Pro__Main'       => '4.5.6',
-			'Tribe__Events__Community__Main' => '4.4.14',
+			'Tribe__Events__Pro__Main'       => '4.4.35',
 		),
 	);
 
