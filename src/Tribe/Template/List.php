8ee212529267cad306cd17fdf497f17d76e62df6
theAverageDev (Luca Tumedei) <luca@theaveragedev.com>
2019-01-18 17:06:06 +0100

    use the start of day for past filtering

--- src/Tribe/Template/List.php
+++ src/Tribe/Template/List.php
@@ -94,7 +94,7 @@ if ( ! class_exists( 'Tribe__Events__Template__List' ) ) {
 				$args['start_date'] = $date;
 				$args['order']      = 'ASC';
 			} elseif ( 'past' === $display ) {
-				$args['starts_before'] = $date;
+				$args['starts_before'] = tribe_beginning_of_day( $date );
 				$args['order']         = 'DESC';
 			} elseif ( 'all' === $display ) {
 				$args['start_date'] = tribe_beginning_of_day( $date );
