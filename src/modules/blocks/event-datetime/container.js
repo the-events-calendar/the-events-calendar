d3db8ca65b808d5895aa553508c242ac7e13874d
Paul <kim.paul.ms@gmail.com>
2019-01-22 20:53:27 +0800

    remove unneeded props in date time block

--- src/modules/blocks/event-datetime/container.js
+++ src/modules/blocks/event-datetime/container.js
@@ -2,7 +2,7 @@
  * External dependencies
  */
 import { connect } from 'react-redux';
-import { compose, bindActionCreators } from 'redux';
+import { compose } from 'redux';
 
 /**
  * Internal dependencies
@@ -20,7 +20,6 @@ import {
 	selectors as priceSelectors,
 	actions as priceActions,
 } from '@moderntribe/events/data/blocks/price';
-import { moment } from '@moderntribe/common/utils';
 import { withStore, withSaveData, withBlockCloser } from '@moderntribe/common/hoc';
 import EventDateTime from './template';
 
@@ -28,67 +27,36 @@ import EventDateTime from './template';
  * Module Code
  */
 
-const onClose = ( dispatchProps ) => ( e ) => {
-	const { setDateInputVisibility, closeDashboardDateTime } = dispatchProps;
-	setDateInputVisibility( false );
-	closeDashboardDateTime();
-};
-
-
-const onTimeZoneVisibilityChange = ( dispatch ) => ( checked ) => (
-	dispatch( dateTimeActions.setTimeZoneVisibility( checked ) )
-);
-
-const onDateTimeLabelClick = ( dispatch ) => () => {
-	dispatch( dateTimeActions.setDateInputVisibility( true ) );
-	dispatch( UIActions.openDashboardDateTime() );
-};
-
 const mapStateToProps = ( state ) => ( {
-	isDashboardOpen: UISelectors.getDashboardDateTimeOpen( state ),
 	isOpen: UISelectors.getDashboardDateTimeOpen( state ),
-	visibleMonth: UISelectors.getVisibleMonth( state ),
-	isEditable: dateTimeSelectors.isEditable( state ),
 	start: dateTimeSelectors.getStart( state ),
 	end: dateTimeSelectors.getEnd( state ),
-	naturalLanguageLabel: dateTimeSelectors.getNaturalLanguageLabel( state ),
-	multiDay: dateTimeSelectors.getMultiDay( state ),
 	allDay: dateTimeSelectors.getAllDay( state ),
 	separatorDate: dateTimeSelectors.getDateSeparator( state ),
 	separatorTime: dateTimeSelectors.getTimeSeparator( state ),
 	showTimeZone: dateTimeSelectors.getTimeZoneVisibility( state ),
 	timeZone: dateTimeSelectors.getTimeZone( state ),
 	timeZoneLabel: dateTimeSelectors.getTimeZoneLabel( state ),
-	showDateInput: dateTimeSelectors.getDateInputVisibility( state ),
 	cost: priceSelectors.getPrice( state ),
 	currencySymbol: priceSelectors.getSymbol( state ),
 	currencyPosition: priceSelectors.getPosition( state ),
 } );
 
 const mapDispatchToProps = ( dispatch ) => ( {
-	...bindActionCreators( dateTimeActions, dispatch ),
-	...bindActionCreators( dateTimeThunks, dispatch ),
-	...bindActionCreators( UIActions, dispatch ),
-	...bindActionCreators( priceActions, dispatch ),
 	setInitialState: ( props ) => {
 		dispatch( priceActions.setInitialState( props ) );
 		dispatch( dateTimeThunks.setInitialState( props ) );
 		dispatch( UIActions.setInitialState( props ) );
 	},
-	onTimeZoneVisibilityChange: onTimeZoneVisibilityChange( dispatch ),
-	onDateTimeLabelClick: onDateTimeLabelClick( dispatch ),
-} );
-
-const mergeProps = ( stateProps, dispatchProps, ownProps ) => ( {
-	...ownProps,
-	...stateProps,
-	...dispatchProps,
-	onClose: onClose( dispatchProps ),
+	onClose: () => {
+		dispatch( dateTimeActions.setDateInputVisibility( false ) );
+		dispatch( UIActions.closeDashboardDateTime() );
+	},
 } );
 
 export default compose(
 	withStore(),
-	connect( mapStateToProps, mapDispatchToProps, mergeProps ),
+	connect( mapStateToProps, mapDispatchToProps ),
 	withSaveData(),
 	withBlockCloser,
 )( EventDateTime );
