2dcf25c8b552bdf972f1f5cfa128499a96092616
bordoni <bordoni.dev@gmail.com>
2019-02-27 11:10:24 -0300

    Modify behavior for the UTC notice

--- src/Tribe/Admin/Notice/Timezones.php
+++ src/Tribe/Admin/Notice/Timezones.php
@@ -1,23 +1,74 @@
 <?php
+use Tribe__Date_Utils as Date;
 /**
  * Shows an admin notice for Timezones
  * (When using UTC and on TEC Pages or WordPress > General Settings)
  */
 class Tribe__Events__Admin__Notice__Timezones {
 
+	/**
+	 * Notice Slug on the user options
+	 *
+	 * @since  TBD
+	 * @var string
+	 */
+	private $slug = 'events-utc-timezone';
+
 	public function hook() {
+		$date = $this->get_current_reset_date();
+		$slug = $this->slug;
+
+		// If we have a date append to the Slug
+		if ( $date ) {
+			$slug .= '-' . $date;
+		}
 
 		tribe_notice(
-			'tribe-events-utc-timezone',
-			array( $this, 'notice' ),
-			array(
+			$slug,
+			[ $this, 'notice' ],
+			[
 				'type'    => 'warning',
 				'dismiss' => 1,
 				'wrap'    => 'p',
-			),
-			array( $this, 'should_display' )
+			],
+			[ $this, 'should_display' ]
 		);
 
+	}
+
+	/**
+	 * Fetches the date in which the Notice had it's reset
+	 *
+	 * @since  TBD
+	 *
+	 * @return string|null
+	 */
+	public function get_current_reset_date() {
+		$dates = $this->get_reset_dates();
+		$today = date( Date::DBDATEFORMAT );
+
+		foreach ( $dates as $key => $date ) {
+			if ( $date <= $today ) {
+				return $date;
+			}
+		}
+
+		return null;
+	}
+
+	/**
+	 * Which dates this Notice gets reset
+	 *
+	 * @since  TBD
+	 *
+	 * @return array
+	 */
+	public function get_reset_dates() {
+		$dates[] = date( Date::DBDATEFORMAT, strtotime( 'last sunday of february' ) );
+		$dates[] = date( Date::DBDATEFORMAT, strtotime( 'third sunday of march' ) );
+		$dates[] = date( Date::DBDATEFORMAT, strtotime( 'third sunday of october' ) );
+		$dates[] = date( Date::DBDATEFORMAT, strtotime( 'second sunday of november' ) );
+		return $dates;
 	}
 
 	/**
@@ -49,7 +100,6 @@ class Tribe__Events__Admin__Notice__Timezones {
 	 * @return boolean
 	 */
 	public function is_utc_timezone() {
-
 		// If the site is using UTC or UTC manual offset
 		return strpos( Tribe__Timezones::wp_timezone_string(), 'UTC' ) !== false;
 	}
@@ -67,15 +117,20 @@ class Tribe__Events__Admin__Notice__Timezones {
 			return false;
 		}
 
-		$url = 'https://theeventscalendar.com/knowledgebase/time-zones/';
+		$text = [];
+		$current_utc = Tribe__Timezones::wp_timezone_string();
+
+		$url = 'http://m.tri.be/1acz';
 		$link = sprintf(
 			'<a href="%1$s" target="_blank">%2$s</a>',
 			esc_url( $url ),
 			esc_html__( 'Read more', 'the-events-calendar' )
 		);
-		$text = __( 'When using The Events Calendar, we recommend that you choose a city in your timezone and avoid using a UTC timezone offset. Choosing a UTC timezone may cause problems when importing events or with Day Light Savings time. %1$s', 'the-events-calendar' );
 
-		return sprintf( $text, $link );
+		$text[] = __( 'Daylight Saving Time could impact your events! To show the right time for your location, be sure to correctly configure each event as well as WordPress itself.', 'the-events-calendar' );
+		$text[] = __( 'For best results, we recommend you use a geographic timezone such as "America/Los Angeles" instead of an offset such as "%2$s". %1$s', 'the-events-calendar' );
+
+		return sprintf( implode( '<br />', $text ), $link, $current_utc );
 
 	}
 
