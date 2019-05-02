f1465bed12514e56fb2b698a7c3765c5d0363f63
theAverageDev (Luca Tumedei) <luca@theaveragedev.com>
2019-05-02 15:57:01 +0200

    test(Views v2): regenerate snapshots

---
+++ src/Tribe/Views/V2/Query/Controller.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Controls a query connecting it with the Repository and Context.
+ *
+ * @package Tribe\Events\Views\V2\Query
+ * @since TBD
+ */
+
+namespace Tribe\Events\Views\V2\Query;
+
+/**
+ * Class Controller
+ *
+ * @package Tribe\Events\Views\V2\Query
+ * @since TBD
+ */
+class Controller {
+
+}
--- tests/views_integration/Tribe/Events/Views/V2/__snapshots__/TestCaseTest__should_allow_making_a_simple_snapshot_assertion__1.php
+++ tests/views_integration/Tribe/Events/Views/V2/__snapshots__/TestCaseTest__should_allow_making_a_simple_snapshot_assertion__1.php
@@ -1,6 +1,5 @@
 <?php return '<script type="text/javascript">const dump = {
     "posts_per_page": "10",
-    "event_display": "test",
     "view": "test",
     "view_data": {
         "venue": 23,
