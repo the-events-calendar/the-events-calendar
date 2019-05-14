1855f76d6a113420de595ca34efb298d65b02bf7
Juanfra Aldasoro <juanfraa@gmail.com>
2019-05-14 10:35:44 -0300

    Don't use camelcase

--- src/views/v2/events-bar/views.php
+++ src/views/v2/events-bar/views.php
@@ -16,19 +16,19 @@
 <div class="tribe-events__events-bar-views">
 	<h3 class="tribe-common-a11y-visual-hide"><?php printf( esc_html__( '%s Views Navigation', 'the-events-calendar' ), tribe_get_event_label_singular() ); ?></h3>
 	<div class="tribe-common-form-control-tabs">
-		<button id="tabButton" aria-haspopup="listbox" aria-labelledby="tabButton" aria-expanded="true"><?php esc_html_e( 'Views', 'the-events-calendar' ); ?></button>
+		<button id="tribe-views-button" aria-haspopup="listbox" aria-labelledby="tribe-views-button" aria-expanded="true"><?php esc_html_e( 'Views', 'the-events-calendar' ); ?></button>
 		<ul tabindex="-1" role="listbox" aria-activedescendant="tabOneLabel">
 			<li role="presentation">
-				<input id="tabList" name="tabGroup" type="radio" value="tabList" checked="checked" />
-				<label id="tabListLabel" for="tabList" role="option" aria-selected="true"><?php esc_html_e( 'List', 'the-events-calendar' ); ?></label>
+				<input id="tribe-views-list" name="tribe-views" type="radio" value="tribe-views-list" checked="checked" />
+				<label id="tribe-views-list-label" for="tribe-views-list" role="option" aria-selected="true"><?php esc_html_e( 'List', 'the-events-calendar' ); ?></label>
 			</li>
 			<li role="presentation">
-				<input id="tabMonth" name="tabGroup" type="radio" value="tabMonth" />
-				<label id="tabMonthLabel" for="tabMonth" role="option"><?php esc_html_e( 'Month', 'the-events-calendar' ); ?></label>
+				<input id="tribe-views-month" name="tribe-views" type="radio" value="tribe-views-month" />
+				<label id="tribe-views-month-label" for="tribe-views-month" role="option"><?php esc_html_e( 'Month', 'the-events-calendar' ); ?></label>
 			</li>
 			<li role="presentation">
-				<input id="tabWeek" name="tabGroup" type="radio" value="tabWeek" />
-				<label id="tabWeekLabel" for="tabWeek" role="option"><?php esc_html_e( 'Week', 'the-events-calendar' ); ?></label>
+				<input id="tribe-views-week" name="tribe-views" type="radio" value="tribe-views-week" />
+				<label id="tribe-views-week-label" for="tribe-views-week" role="option"><?php esc_html_e( 'Week', 'the-events-calendar' ); ?></label>
 			</li>
 		</ul>
 	</div>
