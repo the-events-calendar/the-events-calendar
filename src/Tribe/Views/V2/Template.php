5d8cf10b429b0c1f6bfc6993dd1f50bc5adfcfed
bordoni <bordoni.dev@gmail.com>
2019-05-14 06:51:40 -0300

    Include the methods back, and make use of that.

--- src/Tribe/Views/V2/Template.php
+++ src/Tribe/Views/V2/Template.php
@@ -34,6 +34,7 @@ class Template extends Base_Template {
 		return parent::template( $this->slug, $context, false );
 	}
 
+
 	/**
 	 * The slug the template should use to build its path.
 	 *
@@ -55,5 +56,55 @@ class Template extends Base_Template {
 		$this->set_template_origin( tribe( 'tec.main' ) )
 		     ->set_template_folder( 'src/views/v2' )
 		     ->set_template_folder_lookup( true );
+	}
+
+	/**
+	 * Returns the template file the View will use to render.
+	 *
+	 * If a template cannot be found for the view then the base template for the view will be returned.
+	 *
+	 * @param string|null $name Either a specific name to check or `null` to let the view pick the
+	 *                          template according to the template override rules.
+	 *
+	 * @return string The path to the template file the View will use to render its contents.
+	 * @since TBD
+	 *
+	 */
+	public function get_template_file( $name = null ) {
+		$name = null !== $name ? $name : $this->slug;
+
+		$template = parent::get_template_file( $name );
+
+		return false !== $template
+			? $template
+			: $this->get_base_template_file();
+	}
+
+	/**
+	 * Returns the absolute path to the view base template file.
+	 *
+	 * @since TBD
+	 *
+	 * @return string The absolute path to the Views base template.
+	 */
+	public function get_base_template_file() {
+		// Print the lookup folders as relative paths.
+		$this->set( 'lookup_folders', array_map( function ( array $folder ) {
+			$folder['path'] = str_replace( WP_CONTENT_DIR, '', $folder['path'] );
+			return $folder;
+		}, $this->get_template_path_list() ) );
+
+		return parent::get_template_file( 'base' );
+	}
+
+	/**
+	 * Returns the absolute path to the view "not found" template file.
+	 *
+	 * @since TBD
+	 *
+	 * @return string The absolute path to the Views "not found" template.
+	 */
+	public function get_not_found_template() {
+		return parent::get_template_file( 'not-found' );
 	}
 }
