c15774c7d23ea69133e630e0bffaa77f0f890dd4
bordoni <bordoni.dev@gmail.com>
2019-05-14 05:30:02 -0300

    use proper html and prep for work with luca

--- src/views/kitchen-sink/manager.php
+++ src/views/kitchen-sink/manager.php
@@ -10,35 +10,6 @@ tribe_asset_enqueue( 'tribe-events-views-v2-manager' );
 	method="get"
 	data-rest-url="<?php echo esc_url( tribe( Rest_Endpoint::class )->get_url() ); ?>"
 >
-	<?php // @todo remove this css ?>
-	<style type="text/css">
-		.tribe-events-container {
-			position: relative;
-		}
-
-		.tribe-events-view-loader {
-			align-content: center;
-			justify-content: center;
-			display: flex;
-			position: absolute;
-			top: 0;
-			right: 0;
-			bottom: 0;
-			left: 0;
-			background: rgba( 255, 255, 255, 0.6 );
-		}
-
-		.tribe-events-view-loader.tribe-hidden {
-			display: none;
-		}
-
-		.tribe-events-view-loader .tribe-events-view-loader-spinner {
-			align-self: center;
-		}
-	</style>
-	<?php // @todo remove this css ?>
-
-
 	<div class="tribe-events-view-loader tribe-hidden">
 		<div class="tribe-events-view-loader-spinner">Loading...</div>
 	</div>
@@ -47,16 +18,71 @@ tribe_asset_enqueue( 'tribe-events-views-v2-manager' );
 	<br />
 
 	<a
-		href="<?php echo esc_url( home_url( '/events/list' ) ); ?>"
+		href="<?php echo esc_url( home_url( '/events/list/page/2' ) ); ?>"
 		class="tribe-events-navigation-link"
 	>
-		Developers, Developers, Developers
+		Page 2
 	</a>
 
 	<br />
 
 	<button>
-		Button from Developers
+		Search?
 	</button>
 
 </form>
+<form
+	class="tribe-events-container"
+	action=""
+	method="get"
+	data-rest-url="<?php echo esc_url( tribe( Rest_Endpoint::class )->get_url() ); ?>"
+>
+	<div class="tribe-events-view-loader tribe-hidden">
+		<div class="tribe-events-view-loader-spinner">Loading...</div>
+	</div>
+	<input type="text" name="tribe-events-views[view]" value="default" />
+
+	<br />
+
+	<a
+		href="<?php echo esc_url( home_url( '/events/list/page/2' ) ); ?>"
+		class="tribe-events-navigation-link"
+	>
+		Page 2
+	</a>
+
+	<br />
+
+	<button>
+		Search?
+	</button>
+
+</form>
+
+<?php // @todo remove this css ?>
+<style type="text/css">
+	.tribe-events-container {
+		position: relative;
+	}
+
+	.tribe-events-view-loader {
+		align-content: center;
+		justify-content: center;
+		display: flex;
+		position: absolute;
+		top: 0;
+		right: 0;
+		bottom: 0;
+		left: 0;
+		background: rgba( 255, 255, 255, 0.6 );
+	}
+
+	.tribe-events-view-loader.tribe-hidden {
+		display: none;
+	}
+
+	.tribe-events-view-loader .tribe-events-view-loader-spinner {
+		align-self: center;
+	}
+</style>
+<?php // @todo remove this css ?>
--- src/views/v2/dev-list/list.php
+++ src/views/v2/dev-list/list.php
@@ -10,39 +10,7 @@ tribe_asset_enqueue( 'tribe-events-views-v2-manager' );
 	method="get"
 	data-rest-url="<?php echo esc_url( tribe( Rest_Endpoint::class )->get_url() ); ?>"
 >
-	<?php // @todo remove this css ?>
-	<style type="text/css">
-		.tribe-events-container {
-			position: relative;
-		}
-
-		.tribe-events-view-loader {
-			align-content: center;
-			justify-content: center;
-			display: flex;
-			position: absolute;
-			top: 0;
-			right: 0;
-			bottom: 0;
-			left: 0;
-			background: rgba( 255, 255, 255, 0.6 );
-		}
-
-		.tribe-events-view-loader.tribe-hidden {
-			display: none;
-		}
-
-		.tribe-events-view-loader .tribe-events-view-loader-spinner {
-			align-self: center;
-		}
-	</style>
-	<?php // @todo remove this css ?>
-
-	<?php
-
-	var_dump( $_GET );
-
-	?>
+	<?php var_dump( $_GET ); ?>
 
 
 	<div class="tribe-events-view-loader tribe-hidden">
@@ -53,16 +21,16 @@ tribe_asset_enqueue( 'tribe-events-views-v2-manager' );
 	<br />
 
 	<a
-		href="<?php echo esc_url( home_url( '/events/list' ) ); ?>"
+		href="<?php echo esc_url( home_url( '/events/list/page/3' ) ); ?>"
 		class="tribe-events-navigation-link"
 	>
-		Developers, Developers, Developers
+		Page 3!
 	</a>
 
 	<br />
 
 	<button>
-		Button from Developers
+		Searched!
 	</button>
 
 </form>
