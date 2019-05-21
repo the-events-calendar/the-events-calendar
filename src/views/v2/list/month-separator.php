cdd56c350d85ccda215c2a52a9ffbc5f71af79cc
Paul <kim.paul.ms@gmail.com>
2019-05-21 13:16:15 +0800

    clean up styles for list view month separator

--- src/views/v2/list/month-separator.php
+++ src/views/v2/list/month-separator.php
@@ -15,5 +15,5 @@
 $month = $this->get( 'month' );
 ?>
 <div class="tribe-events-calendar-list__separator-month">
-	<span><?php echo esc_html( $month ); ?></span>
+	<span class="tribe-events-calendar-list__separator-month-text"><?php echo esc_html( $month ); ?></span>
 </div>
