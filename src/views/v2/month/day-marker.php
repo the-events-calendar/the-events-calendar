3d676020bbc62de160da29a03c4ecbdb9ed6c447
Paul <kim.paul.ms@gmail.com>
2019-05-21 14:16:01 +0800

    styles for day marker component

--- src/views/v2/month/day-marker.php
+++ src/views/v2/month/day-marker.php
@@ -14,5 +14,5 @@
  */
 ?>
 <div class="tribe-events-c-day-marker">
-	<time class="tribe-events-c-day-marker__date" datetime="2019-05-12">May 12</time>
+	<time class="tribe-events-c-day-marker__date tribe-common-b1 tribe-common-b1--bold" datetime="2019-05-12">May 12</time>
 </div>
