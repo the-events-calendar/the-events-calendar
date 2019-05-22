25a6b8e3be5860457b0744f8611554d56599cf88
bordoni <bordoni.dev@gmail.com>
2019-05-22 15:12:24 -0400

    Version bump

--- src/Tribe/Main.php
+++ src/Tribe/Main.php
@@ -34,7 +34,7 @@ if ( ! class_exists( 'Tribe__Events__Main' ) ) {
 		const VENUE_POST_TYPE     = 'tribe_venue';
 		const ORGANIZER_POST_TYPE = 'tribe_organizer';
 
-		const VERSION             = '4.9.2';
+		const VERSION             = '4.9.3';
 
 		/**
 		 * Min Pro Addon
