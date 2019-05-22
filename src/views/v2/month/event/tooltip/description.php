c9eb131bc2e82031e805200b9f010c983d64f02e
Paul <kim.paul.ms@gmail.com>
2019-05-23 02:56:24 +0800

    tooltip styles

--- src/views/v2/month/event/tooltip/description.php
+++ src/views/v2/month/event/tooltip/description.php
@@ -13,6 +13,6 @@
  *
  */
 ?>
-<p class="tribe-events-calendar-month__event-tooltip-description">
+<p class="tribe-events-calendar-month__event-tooltip-description tribe-common-b3">
 	Lorem ipsum dolor sit amet, consectetur adipiscing elit. In consectetur metus tortor, et maximus libero aliquet in. Praesent rhoncus ultricies neque in imperdiet. Aenean at ex luctus, tempor justo eget, pharetra magna. Nullam ut odio maximus nisi mollis gravida. Fusce dictum vestibulum pretium. In faucibus aliquet hendrerit. Sed ultrices tristique odio id bibendum. Donec at porta ante, sit amet vehicula erat. Quisque non est dapibus neque faucibus faucibus. Nulla ac justo ornare, facilisis mi id, vulputate lorem.
 </p>
