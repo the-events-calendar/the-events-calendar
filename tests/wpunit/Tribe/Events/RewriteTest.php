4b19a6e531abfea22832b1f36a88193646faf084
theAverageDev (Luca Tumedei) <luca@theaveragedev.com>
2019-05-23 16:52:08 +0200

    test(rewrite): add missing trailing slash

--- tests/wpunit/Tribe/Events/RewriteTest.php
+++ tests/wpunit/Tribe/Events/RewriteTest.php
@@ -64,43 +64,43 @@ class RewriteTest extends \Codeception\TestCase\WPTestCase {
 			],
 			'list_page_1'             => [
 				'/?post_type=tribe_events&eventDisplay=list',
-				'/events/list',
+				'/events/list/',
 			],
 			'list_page_2'             => [
 				'/?post_type=tribe_events&eventDisplay=list&paged=2',
-				'/events/list/page/2',
+				'/events/list/page/2/',
 			],
 			'list_page_1_w_extra'     => [
 				'/?post_type=tribe_events&eventDisplay=list&foo=bar',
-				'/events/list?foo=bar',
+				'/events/list/?foo=bar',
 			],
 			'tag_page_1'              => [
 				'/?post_type=tribe_events&eventDisplay=list&tag=test',
-				'/events/tag/test/list',
+				'/events/tag/test/list/',
 			],
 			'tag_page_1_w_extra'      => [
 				'/?post_type=tribe_events&eventDisplay=list&tag=test&foo=bar',
-				'/events/tag/test/list?foo=bar',
+				'/events/tag/test/list/?foo=bar',
 			],
 			'tag_page_2'              => [
 				'/?post_type=tribe_events&eventDisplay=list&tag=test&paged=2',
-				'/events/tag/test/list/page/2',
+				'/events/tag/test/list/page/2/',
 			],
 			'tag_page_2_w_extra'      => [
 				'/?post_type=tribe_events&eventDisplay=list&tag=test&paged=2&foo=bar',
-				'/events/tag/test/list/page/2?foo=bar',
+				'/events/tag/test/list/page/2/?foo=bar',
 			],
 			'category_page_1'         => [
 				'/?post_type=tribe_events&eventDisplay=list&tribe_events_cat=test',
-				'/events/category/test/list',
+				'/events/category/test/list/',
 			],
 			'category_page_1_w_extra' => [
 				'/?post_type=tribe_events&eventDisplay=list&tribe_events_cat=test&foo=bar',
-				'/events/category/test/list?foo=bar',
+				'/events/category/test/list/?foo=bar',
 			],
 			'category_page_2'         => [
 				'/?post_type=tribe_events&eventDisplay=list&tribe_events_cat=test&paged=2',
-				'/events/category/test/list/page/2',
+				'/events/category/test/list/page/2/',
 			],
 			'category_page_2_w_extra' => [
 				'/?post_type=tribe_events&eventDisplay=list&tribe_events_cat=test&paged=2&foo=bar',
@@ -108,19 +108,19 @@ class RewriteTest extends \Codeception\TestCase\WPTestCase {
 			],
 			'day_page'                => [
 				'/?post_type=tribe_events&eventDisplay=day&eventDate=2018-12-01',
-				'/events/2018-12-01',
+				'/events/2018-12-01/',
 			],
 			'month_page'              => [
 				'/?post_type=tribe_events&eventDisplay=month&eventDate=2018-12',
-				'/events/2018-12',
+				'/events/2018-12/',
 			],
 			'feed_page'               => [
 				'/?post_type=tribe_events&tag=test&feed=rss2',
-				'/events/tag/test/feed/rss2',
+				'/events/tag/test/feed/rss2/',
 			],
 			'ical_page'               => [
 				'/?post_type=tribe_events&tag=test&ical=1',
-				'/events/tag/test/ical',
+				'/events/tag/test/ical/',
 			],
 		];
 	}
