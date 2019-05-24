0eef8a4b233414ca70dc0c8e6bbdd83e45325c24
bordoni <bordoni.dev@gmail.com>
2019-05-24 11:52:33 -0400

    Get local context to avoid warning

--- src/views/v2/data.php
+++ src/views/v2/data.php
@@ -1,1 +1,1 @@
-<script class="tribe-events-views-data" type="application/json"><?php echo json_encode( $this->get() ); ?></script>
+<script class="tribe-events-views-data" type="application/json"><?php echo json_encode( $this->get_template_context() ); ?></script>
