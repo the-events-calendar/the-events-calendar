a5489c1395ce84a8ae1e55ef67fcfa19a555bfef
Juanfra Aldasoro <juanfraa@gmail.com>
2019-05-24 10:59:37 -0300

    Multiday first markup

---
+++ src/views/v2/month/event-multiday.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * View: Month Event Multiday
+ *
+ * Override this template in your own theme by creating a file at:
+ * [your-theme]/tribe/events/views/v2/month/event-multiday.php
+ *
+ * See more documentation about our views templating system.
+ *
+ * @link {INSERT_ARTCILE_LINK_HERE}
+ *
+ * @version TBD
+ *
+ */
+
+$event    = $this->get( 'event' );
+$event_id = $event->ID;
+
+$classes = [ 'tribe-events-calendar-month__event-multiday' ];
+
+if ( $is_featured = tribe( 'tec.featured_events' )->is_featured( $event_id ) ) {
+	$classes[] = 'tribe-events-calendar-month__event-multiday--featured';
+}
+?>
+<div class="<?php echo esc_attr( implode( ' ', $classes ) ); ?>">
+	<?php if ( $is_featured ) : ?>
+		<span class="tribe-events-calendar-month__event-multiday-featured-icon"></span>
+	<?php endif; ?>
+	<h3 class="tribe-events-calendar-month__event-multiday-title tribe-common-h8">
+		Lorem ipsum
+	</h3>
+</div>
