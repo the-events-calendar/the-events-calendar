8dfe685aa97f3087d8b96d6de3d68a59725927f6
Juanfra Aldasoro <juanfraa@gmail.com>
2019-05-24 18:15:12 -0300

    Use a heading for the day number. Move classes

--- src/views/v2/month/day.php
+++ src/views/v2/month/day.php
@@ -15,22 +15,23 @@
 // @todo: This is just for presentation purposes, while building the view.
 $day_number  = ( $week < 1 ) ? ( $day + 1 ) * ( $week + 1 ) : ( $day + 1 ) + $week * 7;
 
-$day_classes = [ 'tribe-events-calendar-month__day-date' ];
+$day_title_classes = [ 'tribe-events-calendar-month__day-date' ];
 $day_id = 'tribe-events-calendar-day-' . $day_number;
 
 // @todo: check if we use classes here or if we wrap the day block directly with the classes (we also have `.tribe-events-calendar-month__day-date--current`).
 if ( $day_number == date( 'd', time() ) ) {
-	$day_classes[] = 'tribe-events-calendar-month__day-date--current';
+	$day_title_classes[] = 'tribe-events-calendar-month__day-date--current';
 }
 ?>
 
 <div class="tribe-events-calendar-month__day" role="gridcell" aria-labelledby="<?php echo esc_attr( $day_id ); ?>">
 	<div
 		id="<?php echo esc_attr( $day_id ); ?>"
-		class="<?php echo esc_attr( implode( ' ', $day_classes ) ); ?>"
 		aria-label="Weekday, Month Daynumber"
 	>
-		<time datetime="YYYY-MM-DD"><?php echo esc_html( $day_number ); ?></time>
+		<h4 class="<?php echo esc_attr( implode( ' ', $day_title_classes ) ); ?>">
+			<time datetime="YYYY-MM-DD"><?php echo esc_html( $day_number ); ?></time>
+		</h4>
 	</div>
 	<!-- Events for this day will be listed here -->
 </div>
