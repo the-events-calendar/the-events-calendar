63767bab2b29b65da66454a35325ce1be34f7fd8
Juanfra Aldasoro <juanfraa@gmail.com>
2019-05-27 10:14:33 -0300

    Add a11y to day cell, to describe the number of events that are taking place

--- src/views/v2/month/day.php
+++ src/views/v2/month/day.php
@@ -27,6 +27,7 @@ if ( $day_number == date( 'd', time() ) ) {
 <div class="tribe-events-calendar-month__day" role="gridcell" aria-labelledby="<?php echo esc_attr( $day_id ); ?>">
 	<div id="<?php echo esc_attr( $day_id ); ?>">
 		<h4 class="<?php echo esc_attr( implode( ' ', $day_title_classes ) ); ?>">
+			<span class="tribe-common-a11y-hidden">X events, </span>
 			<time datetime="YYYY-MM-DD">
 				<a href="#link-to-day-view-if-it-has-events">
 					<?php echo esc_html( $day_number ); ?>
