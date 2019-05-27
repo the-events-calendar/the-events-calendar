caf5a0700a7403c0df8332ea7a95b095d160a6bf
Juanfra Aldasoro <juanfraa@gmail.com>
2019-05-27 10:58:03 -0300

    Changes in markup

--- src/views/v2/month/event-multiday.php
+++ src/views/v2/month/event-multiday.php
@@ -23,14 +23,16 @@ if ( $is_featured = tribe( 'tec.featured_events' )->is_featured( $event_id ) ) {
 }
 ?>
 <div class="<?php echo esc_attr( implode( ' ', $classes ) ); ?>">
-	<?php if ( $is_featured ) : ?>
-		<em
-			class="tribe-events-calendar-month__event-multiday-featured-icon"
-			aria-label="<?php esc_attr_e( 'Featured', 'the-events-calendar' ); ?>"
-			title="<?php esc_attr_e( 'Featured', 'the-events-calendar' ); ?>"
-		></em>
-	<?php endif; ?>
-	<h3 class="tribe-events-calendar-month__event-multiday-title tribe-common-h8">
-		Lorem ipsum
-	</h3>
+	<a class="tribe-events-calendar-month__event-multiday-inner tribe-events-calendar-month__event-multiday-inner--border-left tribe-events-calendar-month__event-multiday-inner--border-right">
+			<?php if ( $is_featured ) : ?>
+				<em
+					class="tribe-events-calendar-month__event-multiday-featured-icon tribe-common-svgicon tribe-common-svgicon--featured"
+					aria-label="<?php esc_attr_e( 'Featured', 'the-events-calendar' ); ?>"
+					title="<?php esc_attr_e( 'Featured', 'the-events-calendar' ); ?>"
+				></em>
+			<?php endif; ?>
+			<h3 class="tribe-events-calendar-month__event-multiday-title tribe-common-h8">
+				Lorem ipsum
+			</h3>
+	</a>
 </div>
