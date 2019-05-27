fa792cc12e99c544a77cd106b55935f5048f1661
Juanfra Aldasoro <juanfraa@gmail.com>
2019-05-27 15:37:48 -0300

    Fix indentation

--- src/views/v2/month/event-multiday.php
+++ src/views/v2/month/event-multiday.php
@@ -25,15 +25,15 @@ if ( $is_featured = tribe( 'tec.featured_events' )->is_featured( $event_id ) ) {
 <div class="<?php echo esc_attr( implode( ' ', $classes ) ); ?>">
 	<time datetime="the-date-and-or-duration" class="tribe-common-a11y-visual-hide">The date and duration</time>
 	<a class="tribe-events-calendar-month__event-multiday-inner tribe-events-calendar-month__event-multiday-inner--border-left tribe-events-calendar-month__event-multiday-inner--border-right">
-			<?php if ( $is_featured ) : ?>
-				<em
-					class="tribe-events-calendar-month__event-multiday-featured-icon tribe-common-svgicon tribe-common-svgicon--featured"
-					aria-label="<?php esc_attr_e( 'Featured', 'the-events-calendar' ); ?>"
-					title="<?php esc_attr_e( 'Featured', 'the-events-calendar' ); ?>"
-				></em>
-			<?php endif; ?>
-			<h3 class="tribe-events-calendar-month__event-multiday-title tribe-common-h8">
-				Lorem ipsum
-			</h3>
+		<?php if ( $is_featured ) : ?>
+			<em
+				class="tribe-events-calendar-month__event-multiday-featured-icon tribe-common-svgicon tribe-common-svgicon--featured"
+				aria-label="<?php esc_attr_e( 'Featured', 'the-events-calendar' ); ?>"
+				title="<?php esc_attr_e( 'Featured', 'the-events-calendar' ); ?>"
+			></em>
+		<?php endif; ?>
+		<h3 class="tribe-events-calendar-month__event-multiday-title tribe-common-h8">
+			Lorem ipsum
+		</h3>
 	</a>
 </div>
