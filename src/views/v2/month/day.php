32091f75f0778ea9319f24a3e652dd863b6813d9
Juanfra Aldasoro <juanfraa@gmail.com>
2019-05-27 16:03:19 -0300

    Hardcode month for the ID.

--- src/views/v2/month/day.php
+++ src/views/v2/month/day.php
@@ -13,10 +13,11 @@
  *
  */
 // @todo: This is just for presentation purposes, while building the view.
-$day_number  = ( $week < 1 ) ? ( $day + 1 ) * ( $week + 1 ) : ( $day + 1 ) + $week * 7;
+$day_number   = ( $week < 1 ) ? ( $day + 1 ) * ( $week + 1 ) : ( $day + 1 ) + $week * 7;
+$month_number = 5;
 
 $day_title_classes = [ 'tribe-events-calendar-month__day-date' ];
-$day_id = 'tribe-events-calendar-day-' . $day_number;
+$day_id = 'tribe-events-calendar-day-' . $month_number . '-' . $day_number;
 
 // @todo: check if we use classes here or if we wrap the day block directly with the classes (we also have `.tribe-events-calendar-month__day-date--current`).
 if ( $day_number == date( 'd', time() ) ) {
@@ -27,7 +28,7 @@ if ( $day_number == date( 'd', time() ) ) {
 <div class="tribe-events-calendar-month__day" role="gridcell" aria-labelledby="<?php echo esc_attr( $day_id ); ?>">
 	<div id="<?php echo esc_attr( $day_id ); ?>">
 		<h3 class="<?php echo esc_attr( implode( ' ', $day_title_classes ) ); ?>">
-			<span class="tribe-common-a11y-hidden">X events, </span>
+			<span class="tribe-common-a11y-visual-hide">X events, </span>
 			<time datetime="YYYY-MM-DD">
 				<a href="#link-to-day-view-if-it-has-events">
 					<?php echo esc_html( $day_number ); ?>
