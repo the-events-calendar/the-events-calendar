64e88bb2a14e482ccffaa2e1eb547e19d64c6b7b
Juanfra Aldasoro <juanfraa@gmail.com>
2019-05-28 15:26:27 -0300

    Add tests

---
+++ tests/views_integration/Tribe/Events/Views/V2/HTML/MonthViewSmallCTATest.php
@@ -0,0 +1,32 @@
+<?php
+namespace Tribe\Events\Views\V2\Views\HTML;
+
+use Tribe\Events\Views\V2\TestHtmlCase;
+
+class MonthViewSmallCTATest extends TestHtmlCase {
+
+	/**
+	 * @test
+	 */
+	public function it_should_contain_correct_html_classes() {
+		$template = $this->template->template( 'month/tooltip/cta' );
+		$html = $this->document->html( $template );
+
+		$this->assertEquals(
+			$html->find( '.tribe-events-c-small-cta' )->count(),
+			1,
+			'Month Tooltip CTA HTML needs to contain one ".tribe-events-c-small-cta" element'
+		);
+
+		$this->assertTrue(
+			$html->find( '.tribe-events-c-small-cta' )->children()->is( '.tribe-events-c-small-cta__link' ),
+			'Month Tooltip CTA HTML needs to contain ".tribe-events-c-small-cta__link" element'
+		);
+
+		$this->assertTrue(
+			$html->find( '.tribe-events-c-small-cta' )->children()->is( '.tribe-events-c-small-cta__price' ),
+			'Month Tooltip CTA HTML needs to contain ".tribe-events-c-small-cta__price" element'
+		);
+	}
+
+}
