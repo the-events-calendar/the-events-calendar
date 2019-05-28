eaaa5000d1018b124229e5ef56057645f07e74cd
Juanfra Aldasoro <juanfra@tri.be>
2019-05-28 14:21:48 -0300

    Update cta.php

--- src/views/v2/month/tooltip/cta.php
+++ src/views/v2/month/tooltip/cta.php
@@ -14,5 +14,6 @@
  */
 ?>
 <div class="tribe-events-c-small-cta">
-	<a class="tribe-events-c-small-cta__link tribe-common-cta tribe-common-cta--alt">Buy Now</a><span class="tribe-events-c-small-cta__price">$10</span>
+	<a class="tribe-events-c-small-cta__link tribe-common-cta tribe-common-cta--alt">Buy Now</a>
+	<span class="tribe-events-c-small-cta__price">$10</span>
 </div>
