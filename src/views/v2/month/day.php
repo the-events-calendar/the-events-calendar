7110d5f36ee386c671fceb01d9a873f3a4a4c44e
Juanfra Aldasoro <juanfraa@gmail.com>
2019-05-29 17:05:01 -0300

    BEM BEM BEM

--- src/views/v2/month/day.php
+++ src/views/v2/month/day.php
@@ -16,12 +16,14 @@
 $day_number   = ( $week < 1 ) ? ( $day + 1 ) * ( $week + 1 ) : ( $day + 1 ) + $week * 7;
 $month_number = 5;
 
-$day_title_classes = [ 'tribe-events-calendar-month__day-date' ];
+$day_title_classes      = [ 'tribe-events-calendar-month__day-date' ];
+$day_title_link_classes = [ 'tribe-events-calendar-month__day-date-link' ];
 $day_id = 'tribe-events-calendar-day-' . $month_number . '-' . $day_number;
 
 // @todo: check if we use classes here or if we wrap the day block directly with the classes (we also have `.tribe-events-calendar-month__day-date--current`).
 if ( $day_number == date( 'd', time() ) ) {
 	$day_title_classes[] = 'tribe-events-calendar-month__day-date--current';
+	$day_title_link_classes[] = 'tribe-events-calendar-month__day-date-link--current';
 }
 ?>
 
@@ -30,7 +32,10 @@ if ( $day_number == date( 'd', time() ) ) {
 		<h3 class="<?php echo esc_attr( implode( ' ', $day_title_classes ) ); ?>">
 			<span class="tribe-common-a11y-visual-hide">X events, </span>
 			<time datetime="YYYY-MM-DD">
-				<a href="#link-to-day-view-if-it-has-events">
+				<a
+					href="#link-to-day-view-if-it-has-events"
+					class="<?php echo esc_attr( implode( ' ', $day_title_link_classes ) ); ?>"
+				>
 					<?php echo esc_html( $day_number ); ?>
 				</a>
 			</time>
