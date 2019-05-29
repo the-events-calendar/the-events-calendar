bca00625afacfacc079cf53cdda2f9c3ae718fe4
Paul <kim.paul.ms@gmail.com>
2019-05-29 15:37:56 +0800

    set up mobile event in month view

--- src/views/v2/month.php
+++ src/views/v2/month.php
@@ -15,12 +15,24 @@
 
 $events = $this->get( 'events' );
 
-$this->template( 'events-bar' );
-
-$this->template( 'top-bar' );
-
 ?>
 
-<div class="tribe-events-calendar-month">
-	<h1>Welcome to the month view</h1>
+<div class="tribe-common-l-container tribe-events-l-container">
+
+	<?php $this->template( 'events-bar' ); ?>
+
+	<?php $this->template( 'top-bar' ); ?>
+
+	<div class="tribe-events-calendar-month">
+
+		<h1>Welcome to the month view</h1>
+
+		<?php foreach ( $events as $event ) : ?>
+
+			<?php $this->template( 'month/mobile-event', [ 'event' => $event ] ); ?>
+
+		<?php endforeach; ?>
+
+	</div>
+
 </div>
---
+++ src/views/v2/month/mobile-event.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * View: Month View Mobile Event
+ *
+ * Override this template in your own theme by creating a file at:
+ * [your-theme]/tribe/events/views/v2/month/mobile-event.php
+ *
+ * See more documentation about our views templating system.
+ *
+ * @link {INSERT_ARTCILE_LINK_HERE}
+ *
+ * @version TBD
+ *
+ */
+?>
