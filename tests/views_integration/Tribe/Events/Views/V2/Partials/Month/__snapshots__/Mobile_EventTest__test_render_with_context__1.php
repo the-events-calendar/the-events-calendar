f3da931b5ba0a9ad7b1b2eadc8ee6e5350700c0b
Paul <kim.paul.ms@gmail.com>
2019-05-29 20:33:50 +0800

    add tests for mobile event

---
+++ tests/views_integration/Tribe/Events/Views/V2/Partials/Month/__snapshots__/Mobile_EventTest__test_render_with_context__1.php
@@ -0,0 +1,43 @@
+<?php return '
+<article class="tribe-events-calendar-month__mobile-event tribe-events-calendar-month__mobile-event--featured">
+
+	<div class="tribe-events-calendar-month__mobile-event-featured-image-wrapper">
+	<div class="tribe-events-calendar-month__mobile-event-featured-image tribe-common-c-image tribe-common-c-image--bg">
+		<div class="tribe-common-c-image__bg"></div>
+	</div>
+</div>
+
+	<div class="tribe-events-calendar-month__mobile-event-details">
+
+		<div class="tribe-events-calendar-month__mobile-event-datetime tribe-common-b2">
+	<time datetime="14:00">2pm</time>
+	<span class="tribe-events-calendar-month__mobile-event-datetime-separator"> - </span>
+	<time datetime="18:00">6pm</time>
+	<em
+		class="tribe-events-calendar-month__mobile-event-datetime-featured tribe-common-svgicon tribe-common-svgicon--featured"
+		aria-label="Featured"
+		title="Featured"
+	>
+	</em>
+	<em
+		class="tribe-events-calendar-month__mobile-event-datetime-recurring tribe-common-svgicon tribe-common-svgicon--recurring"
+		aria-label="Recurring"
+		title="Recurring"
+	>
+	</em>
+</div>
+		<h3 class="tribe-events-calendar-month__mobile-event-title tribe-common-h6">
+	<a
+		href="#"
+		title="Lorem Ipsum"
+		rel="bookmark"
+		class="tribe-events-calendar-month__mobile-event-title-link tribe-common-anchor"
+	>
+		Lorem Ipsum
+	</a>
+</h3>
+		
+	</div>
+
+</article>
+';
