ac337ae18fbf2788d7b7a857142490b506d5b41b
Juanfra Aldasoro <juanfraa@gmail.com>
2019-06-05 17:26:47 -0300

    Update snapshot

--- tests/views_integration/Tribe/Events/Views/V2/Partials/Month/__snapshots__/Calendar_EventTest__test_render_with_context__1.php
+++ tests/views_integration/Tribe/Events/Views/V2/Partials/Month/__snapshots__/Calendar_EventTest__test_render_with_context__1.php
@@ -1,52 +1,40 @@
 <?php return '<article class="tribe-events-calendar-month__calendar-event tribe-events-calendar-month__calendar-event--featured">
 
-	<div class="tribe-events-calendar-month__calendar-event-featured-image-wrapper">
-	<div class="tribe-events-calendar-month__calendar-event-featured-image tribe-common-c-image tribe-common-c-image--bg">
-		<div class="tribe-common-c-image__bg"></div>
-	</div>
-</div>
-
+	
 	<div class="tribe-events-calendar-month__calendar-event-details">
 
 		<div class="tribe-events-calendar-month__calendar-event-datetime">
 	<time datetime="14:00">2pm</time>
 	<span class="tribe-events-calendar-month__calendar-event-datetime-separator"> - </span>
 	<time datetime="18:00">6pm</time>
-	<em
-		class="tribe-events-calendar-month__calendar-event-datetime-featured tribe-common-svgicon tribe-common-svgicon--featured"
-		aria-label="Featured"
-		title="Featured"
-	>
-	</em>
-	<em
-		class="tribe-events-calendar-month__calendar-event-datetime-recurring tribe-common-svgicon tribe-common-svgicon--recurring"
-		aria-label="Recurring"
-		title="Recurring"
-	>
-	</em>
-</div>
-		<h3 class="tribe-events-calendar-month__calendar-event-title tribe-common-h8">
+		</div>
+		<h3 class="tribe-events-calendar-month__calendar-event-title tribe-common-h8 tribe-events-tooltip"
+	data-tooltip-content="#tooltip_content-4"
+	aria-describedby="#tooltip_content-4">
 	<a
-		href="#"
-		title="Lorem Ipsum"
+		title=""
 		rel="bookmark"
 		class="tribe-events-calendar-month__calendar-event-title-link tribe-common-anchor-thin"
 	>
-		Lorem Ipsum
-	</a>
+			</a>
 </h3>
 
-	</div>
-
-	<div class="tribe-events-calendar-month__calendar-event-tooltip">
-	<div class="tribe-events-calendar-month__calendar-event-tooltip-featured-image-wrapper">
-	<div class="tribe-events-calendar-month__calendar-event-tooltip-featured-image tribe-common-c-image tribe-common-c-image--bg">
-		<div class="tribe-common-c-image__bg"></div>
+		<div class="tribe-events-tooltip__content">
+	<div
+		id="tooltip_content-4"
+		class="tribe-events-calendar-month__calendar-event-tooltip"
+		role="tooltip"
+	>
+				<p class="tribe-events-calendar-month__calendar-event-tooltip-description tribe-common-b3">
+	Lorem ipsum dolor sit amet, consectetur adipiscing elit. In consectetur metus tortor, et maximus libero aliquet in. Praesent rhoncus ultricies neque in imperdiet. Aenean at ex luctus, tempor justo eget, pharetra magna. Nullam ut odio maximus nisi mollis gravida.
+</p>
+		<div class="tribe-events-c-small-cta tribe-events-calendar-month__calendar-event-tooltip-cta">
+	<a class="tribe-events-c-small-cta__link tribe-common-cta tribe-common-cta--alt">Buy Now</a>
+	<span class="tribe-events-c-small-cta__price">$10</span>
+</div>
 	</div>
 </div>
-	<p class="tribe-events-calendar-month__calendar-event-tooltip-description tribe-common-b3">
-	Lorem ipsum dolor sit amet, consectetur adipiscing elit. In consectetur metus tortor, et maximus libero aliquet in. Praesent rhoncus ultricies neque in imperdiet. Aenean at ex luctus, tempor justo eget, pharetra magna. Nullam ut odio maximus nisi mollis gravida. Fusce dictum vestibulum pretium. In faucibus aliquet hendrerit. Sed ultrices tristique odio id bibendum. Donec at porta ante, sit amet vehicula erat. Quisque non est dapibus neque faucibus faucibus. Nulla ac justo ornare, facilisis mi id, vulputate lorem.
-</p>
+
 	</div>
 
 </article>
