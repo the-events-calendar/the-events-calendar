eadeadc1219b930da3394c1c88bf269a8a2c94de
Paul <kim.paul.ms@gmail.com>
2019-06-05 16:03:50 +0800

    update nav markup to show disabled button if no link

--- src/views/v2/list/nav.php
+++ src/views/v2/list/nav.php
@@ -15,14 +15,6 @@
  * @version TBD
  *
  */
-
-/**
- * @todo make sure we are checking if there is a previous or next
- *
-if ( ! tribe_has_previous_event() || ! tribe_has_next_event() ) {
-	return;
-}
-*/
 ?>
 <nav class="tribe-common-c-nav">
 	<ul class="tribe-common-c-nav__list">
--- src/views/v2/list/nav/next.php
+++ src/views/v2/list/nav/next.php
@@ -14,17 +14,22 @@
  * @version TBD
  *
  */
-if ( empty( $link ) ) {
-	return;
-}
 ?>
 <li class="tribe-common-c-nav__list-item">
-	<a
-		href="<?php echo esc_url( $link ); ?>"
-		rel="next"
-		class="tribe-common-c-nav__next"
-		data-js="tribe-events-view-link"
-	>
+	<?php if ( empty( $link ) ) : ?>
+		<button class="tribe-common-c-nav__next" disabled>
+	<?php else : ?>
+		<a
+			href="<?php echo esc_url( $link ); ?>"
+			rel="next"
+			class="tribe-common-c-nav__next"
+			data-js="tribe-events-view-link"
+		>
+	<?php endif; ?>
 		<?php echo esc_html( sprintf( __( 'Next %s', 'the-events-calendar' ), tribe_get_event_label_plural() ) ); ?>
-	</a>
+	<?php if ( empty( $link ) ) : ?>
+		</button>
+	<?php else : ?>
+		</a>
+	<?php endif; ?>
 </li>
