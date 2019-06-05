1b342dac582f9af079f44762a0b70e053905d6a6
Paul <kim.paul.ms@gmail.com>
2019-06-05 15:45:02 +0800

    add hover, focus, and blur event handlers for hidden multiday events

--- src/resources/js/views/multiday-events.js
+++ src/resources/js/views/multiday-events.js
@@ -40,7 +40,95 @@ tribe.events.views.multiday_events = {};
 	 * @type {PlainObject}
 	 */
 	obj.selectors = {
-		hiddenEvent: '.tribe-events-calendar-month__multiday-event--hidden',
+		day: '.tribe-events-calendar-month__day',
+		multidayEvent: '.tribe-events-calendar-month__multiday-event',
+		multidayEventFocus: '.tribe-events-calendar-month__multiday-event--focus',
+		multidayEventHover: '.tribe-events-calendar-month__multiday-event--hover',
+		hiddenMultidayEvent: '.tribe-events-calendar-month__multiday-event--hidden',
+	};
+
+	/**
+	 * Find visible multiday event that relates to the hidden multiday event
+	 *
+	 * @since 4.9.3
+	 *
+	 * @param {jQuery} $hiddenMultidayEvent jQuery object of hidden multiday event
+	 *
+	 * @return {(jQuery\|boolean)} jQuery object of visible multiday event or false if none found
+	 */
+	obj.findVisibleMultidayEvent = function( $hiddenMultidayEvent ) {
+		var $prevDay = $hiddenMultidayEvent.closest( obj.selectors.day ).prev();
+
+		var $visibleMultidayEvent;
+		while ( $prevDay.length ) {
+			$visibleMultidayEvent = $prevDay.find( obj.selectors.multidayEvent ).not( obj.selectors.hiddenMultidayEvent );
+
+			if ( $visibleMultidayEvent.length ) {
+				return $visibleMultidayEvent;
+			}
+		}
+
+		return false;
+	};
+
+	/**
+	 * Add class to visible multiday event on hidden multiday event hover
+	 *
+	 * @since 4.9.3
+	 *
+	 * @param {jQuery} $visibleMultidayEvent jQuery object of visible multiday event
+	 *
+	 * @return {function} event handler for on hover in
+	 */
+	obj.onHoverIn = function( $visibleMultidayEvent ) {
+		return function() {
+			$visibleMultidayEvent.addClass( obj.selectors.multidayEventHover.className() );
+		};
+	};
+
+	/**
+	 * Remove class to visible multiday event on hidden multiday event hover
+	 *
+	 * @since 4.9.3
+	 *
+	 * @param {jQuery} $visibleMultidayEvent jQuery object of visible multiday event
+	 *
+	 * @return {function} event handler for on hover out
+	 */
+	obj.onHoverOut = function( $visibleMultidayEvent ) {
+		return function() {
+			$visibleMultidayEvent.removeClass( obj.selectors.multidayEventHover.className() );
+		};
+	};
+
+	/**
+	 * Add class to visible multiday event on hidden multiday event focus
+	 *
+	 * @since 4.9.3
+	 *
+	 * @param {jQuery} $visibleMultidayEvent jQuery object of visible multiday event
+	 *
+	 * @return {function} event handler for on focus
+	 */
+	obj.onFocus = function( $visibleMultidayEvent ) {
+		return function() {
+			$visibleMultidayEvent.addClass( obj.selectors.multidayEventFocus.className() );
+		};
+	};
+
+	/**
+	 * Remove class to visible multiday event on hidden multiday event blur
+	 *
+	 * @since 4.9.3
+	 *
+	 * @param {jQuery} $visibleMultidayEvent jQuery object of visible multiday event
+	 *
+	 * @return {function} event handler for on blur
+	 */
+	obj.onBlur = function( $visibleMultidayEvent ) {
+		return function() {
+			$visibleMultidayEvent.removeClass( obj.selectors.multidayEventFocus.className() );
+		};
 	};
 
 	/**
@@ -48,14 +136,20 @@ tribe.events.views.multiday_events = {};
 	 *
 	 * @since  4.9.3
 	 *
+	 * @param {number} index index of hidden multiday events from jQuery selector
+	 * @param {HTMLElement} hiddenMultidayEvent HTML element of hidden multiday event
+	 *
 	 * @return {void}
 	 */
-	obj.bindEvents = function() {
-		// bind events for
-		// hidden multiday hover
-		// hidden multiday focus
-		// recursively look for non-hidden multiday, add class to hover/focus
-		// find day, add hover class
+	obj.bindEvents = function( index, hiddenMultidayEvent ) {
+		var $hiddenMultidayEvent = $( hiddenMultidayEvent );
+		var $visibleMultidayEvent = obj.findVisibleMultidayEvent( $hiddenMultidayEvent );
+
+		if ( $visibleMultidayEvent ) {
+			$hiddenMultidayEvent.hover( obj.onHoverIn( $visibleMultidayEvent ), obj.onHoverOut( $visibleMultidayEvent ) );
+			$hiddenMultidayEvent.focus( obj.onFocus( $visibleMultidayEvent ) );
+			$hiddenMultidayEvent.blur( obj.onBlur( $visibleMultidayEvent ) );
+		}
 	};
 
 	/**
@@ -66,8 +160,13 @@ tribe.events.views.multiday_events = {};
 	 * @return {void}
 	 */
 	obj.ready = function() {
-		obj.$hiddenEvents = $( obj.selectors.hiddenEvent );
-		obj.$hiddenEvents.each( obj.bindEvents );
+		obj.$hiddenMultidayEvents = $( obj.selectors.hiddenMultidayEvent );
+		obj.$hiddenMultidayEvents.each( obj.bindEvents );
+		/**
+		 * @todo: do below for ajax events
+		 */
+		// on 'beforeAjaxBeforeSend.tribeEvents' event, remove all listeners
+		// on 'afterAjaxSuccess.tribeEvents' or 'afterAjaxError.tribeEvents', add all listeners
 	};
 
 	// Configure on document ready
