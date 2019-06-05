66eaf05d68a9d4c3d6d20dfc638212376251294b
Juanfra Aldasoro <juanfraa@gmail.com>
2019-06-05 16:38:30 -0300

    Moar tests fixes

--- tests/views_integration/Tribe/Events/Views/V2/HTML/Month/CalendarEvent/MonthCalendarEventDateTest.php
+++ tests/views_integration/Tribe/Events/Views/V2/HTML/Month/CalendarEvent/MonthCalendarEventDateTest.php
@@ -25,7 +25,7 @@ class MonthCalendarEventDateTest extends TestHtmlCase {
 	 */
 	public function it_should_contain_a11y_attributes() {
 		$event = [ 'ID' => 0, 'title' => 'Lorem Ipsum', 'image' => 'test.jpg', 'featured' => true, 'recurring' => true ];
-		$template = $this->template->template( 'month/calendar-event/date', [ 'event' => $event ] );
+		$template = $this->template->template( 'month/calendar-event/date', [ 'event' => (object) $event ] );
 		$html = $this->document->html( $template );
 
 		$this->markTestSkipped( 'The month calendar event date event does not receive data yet' );
