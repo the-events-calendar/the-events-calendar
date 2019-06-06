e3fc39cb048ff7af5740a19eccb3cc08f11898fc
Juanfra Aldasoro <juanfraa@gmail.com>
2019-06-06 16:19:53 -0300

    Add links to featured images (request from design)

--- src/views/v2/month/calendar-event/tooltip/featured-image.php
+++ src/views/v2/month/calendar-event/tooltip/featured-image.php
@@ -20,12 +20,18 @@ if ( ! isset( $event->image ) ) { // @todo: use template tags for images here
 ?>
 <div class="tribe-events-calendar-month__calendar-event-tooltip-featured-image-wrapper">
 	<div class="tribe-events-calendar-month__calendar-event-tooltip-featured-image tribe-common-c-image tribe-common-c-image--bg">
-		<div
-			class="tribe-common-c-image__bg"
-			style="background-image: url('<?php echo esc_attr( $event->image ); ?>');"
-			role="img"
-			aria-label="alt text here"
+		<a
+			href="#"
+			title="<?php echo esc_attr( $event->title ); ?>"
+			rel="bookmark"
 		>
-		</div>
+			<div
+				class="tribe-common-c-image__bg"
+				style="background-image: url('<?php echo esc_attr( $event->image ); ?>');"
+				role="img"
+				aria-label="alt text here"
+			>
+			</div>
+		</a>
 	</div>
 </div>
