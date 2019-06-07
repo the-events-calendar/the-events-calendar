b921620122ba7b7546c8343d697eaefba0781a5e
Juanfra Aldasoro <juanfraa@gmail.com>
2019-06-07 16:06:45 -0300

    Update tooltip test

--- tests/views_integration/Tribe/Events/Views/V2/HTML/Month/CalendarEvent/MonthCalendarEventTooltipTest.php
+++ tests/views_integration/Tribe/Events/Views/V2/HTML/Month/CalendarEvent/MonthCalendarEventTooltipTest.php
@@ -14,9 +14,27 @@ class MonthCalendarEventTooltipTest extends TestHtmlCase {
 		$html = $this->document->html( $template );
 
 		$this->assertEquals(
-			$html->find( '.tribe-events-tooltip__content' )->count(),
+			$html->find( '.tribe-events-calendar-month__calendar-event-tooltip' )->count(),
 			1,
 			'Month Calendar Event Tooltip HTML needs to contain one ".tribe-events-tooltip__content" element'
+		);
+	}
+
+	public function it_should_contain_correct_html_attributes() {
+		$event = [ 'ID' => 0, 'title' => 'Lorem Ipsum', 'image' => 'test.jpg', 'featured' => true, 'recurring' => true ];
+		$template = $this->template->template( 'month/calendar-event/tooltip', [ 'event' => (object) $event ] );
+		$html = $this->document->html( $template );
+
+		$tooltip = $html->find( '.tribe-events-calendar-month__calendar-event-tooltip' );
+
+		$this->assertTrue(
+			$tooltip->is( '[data-js="tribe-events-tooltip-content"]' ),
+			'Month calendar tooltip needs to be data-js="tribe-events-tooltip-content"'
+		);
+
+		$this->assertTrue(
+			$tooltip->is( '[role="tooltip"]' ),
+			'Month calendar tooltip needs to be role="tooltip"'
 		);
 	}
 
