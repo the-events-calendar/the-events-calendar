4d6c0b23d38879d4a25cbc090902116733655b53
Juanfra Aldasoro <juanfraa@gmail.com>
2019-06-07 18:57:45 -0300

    Update snapshot

--- tests/views_integration/Tribe/Events/Views/V2/Partials/Month/__snapshots__/Calendar_EventTest__test_render_with_context__1.php
+++ tests/views_integration/Tribe/Events/Views/V2/Partials/Month/__snapshots__/Calendar_EventTest__test_render_with_context__1.php
@@ -8,23 +8,24 @@
 	<span class="tribe-events-calendar-month__calendar-event-datetime-separator"> - </span>
 	<time datetime="18:00">6pm</time>
 		</div>
-		<h3 class="tribe-events-calendar-month__calendar-event-title tribe-common-h8 tribe-events-tooltip"
-	data-tooltip-content="#tooltip_content-4"
-	aria-describedby="#tooltip_content-4">
+		<h3 class="tribe-events-calendar-month__calendar-event-title tribe-common-h8">
 	<a
 		title=""
 		rel="bookmark"
 		class="tribe-events-calendar-month__calendar-event-title-link tribe-common-anchor-thin"
+		data-js="tribe-events-tooltip"
+		data-tooltip-content="#tooltip_content-4"
+		aria-describedby="#tooltip_content-4"
 	>
 			</a>
 </h3>
 
-		<div class="tribe-events-tooltip__content">
-	<div
-		id="tooltip_content-4"
-		class="tribe-events-calendar-month__calendar-event-tooltip"
-		role="tooltip"
-	>
+		<div
+	class="tribe-events-calendar-month__calendar-event-tooltip"
+	data-js="tribe-events-tooltip-content"
+	role="tooltip"
+>
+	<div id="tooltip_content-4">
 				<p class="tribe-events-calendar-month__calendar-event-tooltip-description tribe-common-b3">
 	Lorem ipsum dolor sit amet, consectetur adipiscing elit. In consectetur metus tortor, et maximus libero aliquet in. Praesent rhoncus ultricies neque in imperdiet. Aenean at ex luctus, tempor justo eget, pharetra magna. Nullam ut odio maximus nisi mollis gravida.
 </p>
