49b367dbe1a54719fae4e19ff577606219b9d557
Juanfra Aldasoro <juanfraa@gmail.com>
2019-06-10 19:06:07 -0300

    Add test

---
+++ tests/views_integration/Tribe/Events/Views/V2/HTML/DayTest.php
@@ -0,0 +1,21 @@
+<?php
+namespace Tribe\Events\Views\V2\Views\HTML;
+
+use Tribe\Events\Views\V2\TestHtmlCase;
+
+class DayTest extends TestHtmlCase {
+
+	/**
+	 * @test
+	 */
+	public function it_should_contain_correct_html_classes() {
+		$template = $this->template->template( 'day', [ 'events' => (object) [] ] );
+		$html = $this->document->html( $template );
+
+		$this->assertEquals(
+			$html->find( '.tribe-events-calendar-day' )->count(),
+			1,
+			'Day HTML needs to contain one ".tribe-events-calendar-day" element'
+		);
+	}
+}
