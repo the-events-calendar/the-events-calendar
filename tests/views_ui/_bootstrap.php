2f29ca945ae7ca718720cdfce5522a5bc3aeaa11
Matthew Batchelder <borkweb@gmail.com>
2019-06-14 12:06:19 -0400

    Manually set the option

--- tests/views_ui/_bootstrap.php
+++ tests/views_ui/_bootstrap.php
@@ -3,5 +3,9 @@ use Tribe\Events\Views\V2\Service_Provider;
 use Tribe\Events\Views\V2\View;
 
 // Let's  make sure Views v2 are activated if not.
-tribe_update_option( View::$option_enabled, true );
+$options                          = get_option( \Tribe__Main::OPTIONNAME, [] );
+$options[ View::$option_enabled ] = true;
+
+update_option( \Tribe__Main::OPTIONNAME, $options );
+
 tribe_register_provider( Service_Provider::class );
