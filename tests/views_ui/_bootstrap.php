2a50b596af73fc54127972e803b1d3f3039b3a02
Matthew Batchelder <borkweb@gmail.com>
2019-06-14 13:17:11 -0400

    Removing damaging lines in bootstrap

--- tests/views_ui/_bootstrap.php
+++ tests/views_ui/_bootstrap.php
@@ -1,11 +1,1 @@
 <?php
-use Tribe\Events\Views\V2\Service_Provider;
-use Tribe\Events\Views\V2\View;
-
-// Let's  make sure Views v2 are activated if not.
-$options                          = get_option( \Tribe__Main::OPTIONNAME, [] );
-$options[ View::$option_enabled ] = true;
-
-update_option( \Tribe__Main::OPTIONNAME, $options );
-
-tribe_register_provider( Service_Provider::class );
