197b4ffbf7f26465a497e4f87e7003022d7f5a5a
Paul <kim.paul.ms@gmail.com>
2019-06-18 20:01:55 +0800

    styles for tabs

--- src/views/kitchen-sink/page.php
+++ src/views/kitchen-sink/page.php
@@ -153,6 +153,49 @@ tribe_asset_enqueue( 'tribe-common-style' );
 		</ul>
 	</div>
 	<br />
+	<div class="tribe-events-c-view-selector tribe-events-c-view-selector--tabs" style="margin-top: 150px; margin-left: 200px">
+		<button
+			class="tribe-events-c-view-selector__button"
+			aria-controls="container-id"
+			aria-selected="false"
+		>
+			<span class="tribe-events-c-view-selector__button-icon tribe-common-svgicon tribe-common-svgicon--month"></span>
+			<span class="tribe-events-c-view-selector__button-text">
+				Month
+			</span>
+		</button>
+		<ul
+			class="tribe-events-c-view-selector__list"
+			id="container-id"
+			aria-hidden="true"
+		>
+			<li class="tribe-events-c-view-selector__list-item">
+				<a href="#" class="tribe-events-c-view-selector__list-item-link">
+					<span class="tribe-events-c-view-selector__list-item-icon tribe-common-svgicon tribe-common-svgicon--month"></span>
+					<span class="tribe-events-c-view-selector__list-item-text">
+						Month
+					</span>
+				</a>
+			</li>
+			<li class="tribe-events-c-view-selector__list-item">
+				<a href="#" class="tribe-events-c-view-selector__list-item-link tribe-events-c-view-selector__list-item-link--active">
+					<span class="tribe-events-c-view-selector__list-item-icon tribe-common-svgicon tribe-common-svgicon--list"></span>
+					<span class="tribe-events-c-view-selector__list-item-text">
+						List
+					</span>
+				</a>
+			</li>
+			<li class="tribe-events-c-view-selector__list-item">
+				<a href="#" class="tribe-events-c-view-selector__list-item-link">
+					<span class="tribe-events-c-view-selector__list-item-icon tribe-common-svgicon tribe-common-svgicon--day"></span>
+					<span class="tribe-events-c-view-selector__list-item-text">
+						Day
+					</span>
+				</a>
+			</li>
+		</ul>
+	</div>
+	<br />
 	<div class="tribe-common-form-control-tabs" style="margin-top: 100px;">
 		<button class="tribe-common-form-control-tabs__button" id="tabButton" aria-haspopup="listbox" aria-labelledby="tabButton" aria-expanded="true">Tab One</button>
 		<ul class="tribe-common-form-control-tabs__list" tabindex="-1" role="listbox" aria-activedescendant="tabOneLabel">
