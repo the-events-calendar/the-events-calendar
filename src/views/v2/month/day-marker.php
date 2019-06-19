6d19d665180c62e549f019d8f311801a59ed6556
Juanfra Aldasoro <juanfraa@gmail.com>
2019-06-19 13:31:45 -0300

    Update classes

--- src/views/v2/month/day-marker.php
+++ src/views/v2/month/day-marker.php
@@ -14,5 +14,5 @@
  */
 ?>
 <div class="tribe-events-c-day-marker">
-	<time class="tribe-events-c-day-marker__date tribe-common-h7 tribe-common-h6--alt" datetime="2019-05-12">May 12</time>
+	<time class="tribe-events-c-day-marker__date tribe-common-h7 tribe-common-h7--alt tribe-common-h6--min-medium" datetime="2019-05-12">May 12</time>
 </div>
