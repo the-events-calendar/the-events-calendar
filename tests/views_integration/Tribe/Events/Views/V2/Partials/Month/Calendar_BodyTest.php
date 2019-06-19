e63b48c4bf26303a08aa23b660ae9d41561533a5
Paul <kim.paul.ms@gmail.com>
2019-06-19 15:04:49 +0800

    add calendar body test

--- tests/views_integration/Tribe/Events/Views/V2/Partials/Month/Calendar_BodyTest.php
+++ tests/views_integration/Tribe/Events/Views/V2/Partials/Month/Calendar_BodyTest.php
@@ -4,10 +4,10 @@ namespace Tribe\Events\Views\V2\Partials\Month;
 
 use Tribe\Events\Views\V2\Partials\TestCase;
 
-class Calendar_HeaderTest extends TestCase
+class Calendar_BodyTest extends TestCase
 {
 
-	protected $partial_path = 'month/calendar-header';
+	protected $partial_path = 'month/calendar-body';
 
 	/**
 	 * Test static render
