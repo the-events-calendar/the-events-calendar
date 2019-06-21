5657c90911086bfc518ecc78424306b9489d671e
Juanfra Aldasoro <juanfraa@gmail.com>
2019-06-21 12:53:27 -0300

    Fix docblock

--- src/views/v2/list/nav/today.php
+++ src/views/v2/list/nav/today.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * View: Month View Nav Today Button
+ * View: List View Nav Today Button
  *
  * Override this template in your own theme by creating a file at:
  * [your-theme]/tribe/events/views/v2/list/nav/today.php
