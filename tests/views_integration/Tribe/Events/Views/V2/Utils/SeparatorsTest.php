79f4ba3fbd1eb92a194e6c9f6367198f8f7806bc
bordoni <bordoni.dev@gmail.com>
2019-06-25 18:17:45 +0100

    better visibility for tests

--- tests/views_integration/Tribe/Events/Views/V2/Utils/SeparatorsTest.php
+++ tests/views_integration/Tribe/Events/Views/V2/Utils/SeparatorsTest.php
@@ -10,10 +10,7 @@ class SeparatorsTest extends \Codeception\TestCase\WPTestCase {
 		static::factory()->event = new Event();
 	}
 
-	/**
-	 * @test
-	 */
-	public function it_should_be_instantiatable() {
+	public function monthSeparatorProvider() {
 		$events[] = $should_have_month_one = static::factory()->event->create( [ 'when' => '2018-09-01' ] );
 		$events[] = $should_have_month_two = static::factory()->event->create( [ 'when' => '2018-10-15' ] );
 		$events[] = $should_not_have_month_one = static::factory()->event->create( [ 'when' => '2018-10-16' ] );
@@ -21,11 +18,46 @@ class SeparatorsTest extends \Codeception\TestCase\WPTestCase {
 		$events[] = $should_have_month_three = static::factory()->event->create( [ 'when' => '2018-11-16' ] );
 		$events[] = $should_not_have_month_three = static::factory()->event->create( [ 'when' => '2018-11-22' ] );
 
-		$this->assertTrue( Separators::should_have_month( $events, $should_have_month_one ) );
-		$this->assertTrue( Separators::should_have_month( $events, $should_have_month_two ) );
-		$this->assertFalse( Separators::should_have_month( $events, $should_not_have_month_one ) );
-		$this->assertFalse( Separators::should_have_month( $events, $should_not_have_month_two ) );
-		$this->assertTrue( Separators::should_have_month( $events, $should_have_month_three ) );
-		$this->assertFalse( Separators::should_have_month( $events, $should_not_have_month_three ) );
+		return [
+			'2018-09-01-should-have' => [
+				$events,
+				$should_have_month_two,
+				true,
+			],
+			'2018-10-15-should-have' => [
+				$events,
+				$should_have_month_one,
+				true,
+			],
+			'2018-10-16-should-not-have' => [
+				$events,
+				$should_not_have_month_one,
+				false,
+			],
+			'2018-10-16(2)-should-not-have' => [
+				$events,
+				$should_not_have_month_two,
+				false,
+			],
+			'2018-11-16-should-have' => [
+				$events,
+				$should_have_month_three,
+				true,
+			],
+			'2018-11-22-should-not-have' => [
+				$events,
+				$should_not_have_month_three,
+				false,
+			],
+		];
+	}
+
+	/**
+	 * @test
+	 * @dataProvider monthSeparatorProvider
+	 */
+	public function it_should_have_month_separator_on_items( $events, $event, $expected ) {
+		$should_have_month = Separators::should_have_month( $events, $event );
+		$this->assertEquals( $should_have_month, $expected );
 	}
 }
