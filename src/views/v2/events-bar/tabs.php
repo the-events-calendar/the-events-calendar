e79b28c33567381b1943a0d51f35fcc302a54215
Paul <kim.paul.ms@gmail.com>
2019-06-27 19:06:04 +0800

    remove tabs markup, move to filter bar

--- src/views/v2/events-bar/tabs.php
+++ src/views/v2/events-bar/tabs.php
@@ -12,34 +12,4 @@
  * @version TBD
  *
  */
-
-/**
- * @todo: show tabs only if filter bar is active
- */
 ?>
-<div class="tribe-events-c-events-bar__tabs" role="tablist" data-js="tribe-events-events-bar-tablist">
-	<button
-		class="tribe-events-c-events-bar__tab tribe-events-c-events-bar__tab--search tribe-events-c-events-bar__tab--active"
-		id="tribe-events-events-bar-search-tab"
-		aria-controls="tribe-events-events-bar-search"
-		data-js="tribe-events-events-bar-tab tribe-events-events-bar-search-tab"
-		role="tab"
-	>
-		<span class="tribe-events-c-events-bar__tab-icon tribe-common-svgicon tribe-common-svgicon--search"></span>
-		<span class="tribe-events-c-events-bar__tab-text tribe-common-b2">
-			<?php esc_html_e( 'Search', 'the-events-calendar' ); ?>
-		</span>
-	</button>
-	<button
-		class="tribe-events-c-events-bar__tab tribe-events-c-events-bar__tab--filters"
-		id="tribe-events-events-bar-filters-tab"
-		aria-controls="tribe-events-events-bar-filters"
-		data-js="tribe-events-events-bar-tab tribe-events-events-bar-filters-tab"
-		role="tab"
-	>
-		<span class="tribe-events-c-events-bar__tab-icon tribe-common-svgicon tribe-common-svgicon--filters"></span>
-		<span class="tribe-events-c-events-bar__tab-text tribe-common-b2">
-			<?php esc_html_e( 'Filter', 'the-events-calendar' ); ?>
-		</span>
-	</button>
-</div>
