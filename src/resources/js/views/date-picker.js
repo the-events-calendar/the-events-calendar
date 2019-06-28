3c0fa4d5f717fbc5e462e80f2091c78adb956f72
Paul <kim.paul.ms@gmail.com>
2019-06-28 14:58:17 +0800

    initial setup of js

---
+++ src/resources/js/views/date-picker.js
@@ -0,0 +1,80 @@
+/**
+ * Makes sure we have all the required levels on the Tribe Object
+ *
+ * @since TBD
+ *
+ * @type   {PlainObject}
+ */
+tribe.events = tribe.events || {};
+tribe.events.views = tribe.events.views || {};
+
+/**
+ * Configures Date Picker Object in the Global Tribe variable
+ *
+ * @since TBD
+ *
+ * @type   {PlainObject}
+ */
+tribe.events.views.datePicker = {};
+
+/**
+ * Initializes in a Strict env the code that manages the Event Views
+ *
+ * @since TBD
+ *
+ * @param  {PlainObject} $   jQuery
+ * @param  {PlainObject} obj tribe.events.views.manager
+ *
+ * @return {void}
+ */
+( function( $, obj ) {
+	'use strict';
+	var $document = $( document );
+
+	/**
+	 * Selectors used for configuration and setup
+	 *
+	 * @since TBD
+	 *
+	 * @type {PlainObject}
+	 */
+	obj.selectors = {
+
+	};
+
+	/**
+	 * Binds events for date picker
+	 *
+	 * @since TBD
+	 *
+	 * @param {Event} event event object for 'afterSetup.tribeEvents' event
+	 * @param {integer} index jQuery.each index param from 'afterSetup.tribeEvents' event
+	 * @param {jQuery} $container jQuery object of view container
+	 * @param {object} data data object passed from 'afterSetup.tribeEvents' event
+	 *
+	 * @return {void}
+	 */
+	obj.bindEvents = function( event, index, $container, data ) {
+
+	};
+
+	/**
+	 * Handles the initialization of the date picker when Document is ready
+	 *
+	 * @since TBD
+	 *
+	 * @return {void}
+	 */
+	obj.ready = function() {
+		$document.on( 'afterSetup.tribeEvents', tribe.events.views.manager.selectors.container, obj.bindEvents );
+
+		/**
+		 * @todo: do below for ajax events
+		 */
+		// on 'beforeAjaxBeforeSend.tribeEvents' event, remove all listeners
+		// on 'afterAjaxError.tribeEvents', add all listeners
+	};
+
+	// Configure on document ready
+	$document.ready( obj.ready );
+} )( jQuery, tribe.events.views.datePicker );
