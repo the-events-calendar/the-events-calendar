e0f20d3fce7901e0c272a1200a1e6beed4690282
Paul <kim.paul.ms@gmail.com>
2019-06-28 15:40:49 +0800

    stay consistent with datepicker, not date-picker

--- src/views/v2/top-bar/datepicker.php
+++ src/views/v2/top-bar/datepicker.php
@@ -3,16 +3,16 @@
  * View: Top Bar - Date Picker
  *
  * Override this template in your own theme by creating a file at:
- * [your-theme]/tribe/events/views/v2/top-bar/date-picker.php
+ * [your-theme]/tribe/events/views/v2/top-bar/datepicker.php
  *
  * See more documentation about our views templating system.
  *
  * @link {INSERT_ARTCILE_LINK_HERE}
  *
- * @version 4.9.3
+ * @version TBD
  *
  */
 ?>
-<button class="tribe-common-h3 tribe-common-h3--alt tribe-events-c-top-bar__date-picker-button">
+<button class="tribe-common-h3 tribe-common-h3--alt tribe-events-c-top-bar__datepicker-button">
 	<?php esc_html_e( 'Now', 'the-events-calendar' ); ?> &mdash; <time datetime="<?php echo esc_attr( date( 'Y-m-d', time() ) ); ?>"><?php echo date( 'F jS, Y', time() ); ?></time>
 </button>
