361bf62e3d21661c585b46666cc27c42372e251a
Paul <kim.paul.ms@gmail.com>
2019-07-01 22:55:11 +0800

    add hidden input to datepicker

--- src/views/v2/top-bar/datepicker.php
+++ src/views/v2/top-bar/datepicker.php
@@ -13,9 +13,18 @@
  *
  */
 ?>
-<button
-	class="tribe-common-h3 tribe-common-h3--alt tribe-events-c-top-bar__datepicker-button"
-	data-js="tribe-events-top-bar-datepicker-button"
->
-	<?php esc_html_e( 'Now', 'the-events-calendar' ); ?> &mdash; <time datetime="<?php echo esc_attr( date( 'Y-m-d', time() ) ); ?>"><?php echo date( 'F jS, Y', time() ); ?></time>
-</button>
+<div class="tribe-events-c-top-bar__datepicker">
+	<button
+		class="tribe-common-h3 tribe-common-h3--alt tribe-events-c-top-bar__datepicker-button"
+		data-js="tribe-events-top-bar-datepicker-button"
+	>
+		<?php esc_html_e( 'Now', 'the-events-calendar' ); ?> &mdash; <time datetime="<?php echo esc_attr( date( 'Y-m-d', time() ) ); ?>"><?php echo date( 'F jS, Y', time() ); ?></time>
+	</button>
+	<input
+		type="hidden"
+		class="tribe-events-c-top-bar__datepicker-input"
+		id="tribe-events-top-bar-date"
+		name="tribe-events-views[tribe-bar-search]"
+		value="<?php echo esc_attr( tribe_events_template_var( [ 'bar', 'date' ], '' ) ); ?>"
+	/>
+</div>
