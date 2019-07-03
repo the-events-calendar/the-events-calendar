23d0662b62f199c662d8396ffae1f3a8ee7326f0
Paul <kim.paul.ms@gmail.com>
2019-07-03 18:44:19 +0800

    fix up class names for month header

--- tests/views_integration/Tribe/Events/Views/V2/Partials/Month/__snapshots__/Calendar_HeaderTest__test_static_render__1.php
+++ tests/views_integration/Tribe/Events/Views/V2/Partials/Month/__snapshots__/Calendar_HeaderTest__test_static_render__1.php
@@ -1,14 +1,14 @@
-<?php return '<header role="rowgroup">
+<?php return '<header class="tribe-events-calendar-month__header" role="rowgroup">
 
 	<h2 class="tribe-common-a11y-visual-hide" id="tribe-events-calendar-header">Calendar of Events</h2>
 
-	<div role="row" class="tribe-events-calendar-month__header">
+	<div role="row" class="tribe-events-calendar-month__header-row">
 					<div
 				class="tribe-events-calendar-month__header-column"
 				role="columnheader"
 				aria-label="Monday"
 			>
-				<h3 class="tribe-events-calendar-month__header-title tribe-common-b3">
+				<h3 class="tribe-events-calendar-month__header-column-title tribe-common-b3">
 					Mon				</h3>
 			</div>
 					<div
@@ -16,7 +16,7 @@
 				role="columnheader"
 				aria-label="Tuesday"
 			>
-				<h3 class="tribe-events-calendar-month__header-title tribe-common-b3">
+				<h3 class="tribe-events-calendar-month__header-column-title tribe-common-b3">
 					Tue				</h3>
 			</div>
 					<div
@@ -24,7 +24,7 @@
 				role="columnheader"
 				aria-label="Wednesday"
 			>
-				<h3 class="tribe-events-calendar-month__header-title tribe-common-b3">
+				<h3 class="tribe-events-calendar-month__header-column-title tribe-common-b3">
 					Wed				</h3>
 			</div>
 					<div
@@ -32,7 +32,7 @@
 				role="columnheader"
 				aria-label="Thursday"
 			>
-				<h3 class="tribe-events-calendar-month__header-title tribe-common-b3">
+				<h3 class="tribe-events-calendar-month__header-column-title tribe-common-b3">
 					Thu				</h3>
 			</div>
 					<div
@@ -40,7 +40,7 @@
 				role="columnheader"
 				aria-label="Friday"
 			>
-				<h3 class="tribe-events-calendar-month__header-title tribe-common-b3">
+				<h3 class="tribe-events-calendar-month__header-column-title tribe-common-b3">
 					Fri				</h3>
 			</div>
 					<div
@@ -48,7 +48,7 @@
 				role="columnheader"
 				aria-label="Saturday"
 			>
-				<h3 class="tribe-events-calendar-month__header-title tribe-common-b3">
+				<h3 class="tribe-events-calendar-month__header-column-title tribe-common-b3">
 					Sat				</h3>
 			</div>
 					<div
@@ -56,7 +56,7 @@
 				role="columnheader"
 				aria-label="Sunday"
 			>
-				<h3 class="tribe-events-calendar-month__header-title tribe-common-b3">
+				<h3 class="tribe-events-calendar-month__header-column-title tribe-common-b3">
 					Sun				</h3>
 			</div>
 			</div>
