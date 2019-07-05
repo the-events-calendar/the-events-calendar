dbf7dc2b19fde601492135d8ca3cbe4e9f01e0b6
Paul <kim.paul.ms@gmail.com>
2019-07-05 14:27:38 +0800

    fix up multiday event spacer comment

--- src/views/v2/month/calendar-body/day/multiday-events/multiday-event-spacer.php
+++ src/views/v2/month/calendar-body/day/multiday-events/multiday-event-spacer.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * View: Month Event Multiday - Hidden Event / Spacer
+ * View: Month View - Multiday Event Spacer
  *
  * Override this template in your own theme by creating a file at:
  * [your-theme]/tribe/events/views/v2/month/calendar-body/day/multiday-events/multiday-event.php
