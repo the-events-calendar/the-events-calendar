972a017c26d800af7ab3e1c02a563b7eed65d321
Juanfra Aldasoro <juanfraa@gmail.com>
2019-07-05 12:13:36 -0300

    Update snapshots

--- tests/views_integration/Tribe/Events/Views/V2/Partials/Day_View/__snapshots__/NavTest__test_render_with_context__1.php
+++ tests/views_integration/Tribe/Events/Views/V2/Partials/Day_View/__snapshots__/NavTest__test_render_with_context__1.php
@@ -4,7 +4,7 @@
 	<a
 		href="#"
 		rel="prev"
-		class="tribe-events-c-nav__prev tribe-common-b2"
+		class="tribe-events-c-nav__prev tribe-common-b2 tribe-common-b1--min-medium"
 		data-js="tribe-events-view-link"
 	>
 		Previous Day	</a>
@@ -23,7 +23,7 @@
 	<a
 		href="#"
 		rel="next"
-		class="tribe-events-c-nav__next tribe-common-b2"
+		class="tribe-events-c-nav__next tribe-common-b2 tribe-common-b1--min-medium"
 		data-js="tribe-events-view-link"
 	>
 		Next Day	</a>
