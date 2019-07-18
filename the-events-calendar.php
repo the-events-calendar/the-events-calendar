7c008ba550b1bd70045968586bff80ecfb7a1c28
Matthew Batchelder <borkweb@gmail.com>
2019-07-17 23:24:55 -0400

    More standards changes

--- the-events-calendar.php
+++ the-events-calendar.php
@@ -1,14 +1,13 @@
 <?php
 /**
-Plugin Name: The Events Calendar
-Description: The Events Calendar is a carefully crafted, extensible plugin that lets you easily share your events. Beautiful. Solid. Awesome.
-Version: 4.9.5
-Author: Modern Tribe, Inc.
-Author URI: http://m.tri.be/1x
-Text Domain: the-events-calendar
-License: GPLv2 or later
-*/
-
+ * Plugin Name: The Events Calendar
+ * Description: The Events Calendar is a carefully crafted, extensible plugin that lets you easily share your events. Beautiful. Solid. Awesome.
+ * Version: 4.9.5
+ * Author: Modern Tribe, Inc.
+ * Author URI: http://m.tri.be/1x
+ * Text Domain: the-events-calendar
+ * License: GPLv2 or later
+ */
 
 /**
  * Copyright 2009-2012 by Modern Tribe Inc and the contributors
