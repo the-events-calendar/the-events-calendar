88b79c768a7e5b9dea9fee8617d20720430f2dd3
Luca Tumedei <luca@theaveragedev.com>
2019-07-23 14:11:00 +0200

    test(Day_View) fix date to use mock

--- tests/views_integration/Tribe/Events/Views/V2/Views/Day_ViewTest.php
+++ tests/views_integration/Tribe/Events/Views/V2/Views/Day_ViewTest.php
@@ -3,9 +3,10 @@
 namespace Tribe\Events\Views\V2\Views;
 
 use Spatie\Snapshots\MatchesSnapshots;
-use tad\FunctionMocker\FunctionMocker as Test;
 use Tribe\Events\Views\V2\View;
 use Tribe\Test\Products\WPBrowser\Views\V2\ViewTestCase;
+use Tribe__Date_Utils as Dates;
+use Tribe__Timezones as Timezones;
 
 class Day_ViewTest extends ViewTestCase {
 
@@ -18,13 +19,20 @@ class Day_ViewTest extends ViewTestCase {
 		// Sanity check
 		$this->assertEmpty( tribe_events()->found() );
 
-		$day_view = View::make( Day_View::class );
+		$context = tribe_context()->alter( [ 'event_date' => $this->mock_date_value ] );
+
+		$day_view = View::make( Day_View::class, $context );
 		$html     = $day_view->get_html();
 
 		$this->assertMatchesSnapshot( $html );
 	}
 
 	public function test_render_multiple_events_on_day() {
+		$timezone_string = 'America/Sao_Paulo';
+		$timezone        = Timezones::build_timezone_object( $timezone_string );
+		$today           = Dates::build_date_object( $this->mock_date_value, $timezone )->format( 'Y-m-d' );
+		update_option( 'timezone_string', $timezone_string );
+
 		$events = [];
 
 		// Create the events.
@@ -33,19 +41,21 @@ class Day_ViewTest extends ViewTestCase {
 				'09:00',
 				'12:00',
 				'12:30',
-			] as $start_date
+			] as $start_time
 		) {
-			$events[] = tribe_events()->set_args( [
-				'start_date' => $start_date . ' today',
-				'timezone'   => 'America/Sao_Paulo',
+			$events[]        = tribe_events()->set_args( [
+				'start_date' => $today . ' ' . $start_time,
+				'timezone'   => $timezone_string,
 				'duration'   => 3 * HOUR_IN_SECONDS,
-				'title'      => 'Test Event - ' . $start_date,
+				'title'      => 'Test Event - ' . $start_time,
 				'status'     => 'publish',
 			] )->create();
 		}
 
 		// Sanity check
-		$this->assertEquals( 3, tribe_events()->by( 'date_overlaps', tribe_beginning_of_day( 'today' ), tribe_end_of_day( 'today' ) )->count() );
+		$day_start = tribe_beginning_of_day( $today );
+		$day_end   = tribe_end_of_day( $today );
+		$this->assertEquals( 3, tribe_events()->where( 'date_overlaps', $day_start, $day_end )->count() );
 
 		$this->remap_posts( $events, [
 			'events/featured/1.json',
@@ -53,10 +63,13 @@ class Day_ViewTest extends ViewTestCase {
 			'events/single/2.json'
 		] );
 
-		$day_view = View::make( Day_View::class );
-		$day_view->set_context( tribe_context()->alter( [
-			'posts_per_page' => 2,
-		] ) );
+		$context = tribe_context()->alter( [
+				'posts_per_page' => 2,
+				'event_date' => $this->mock_date_value,
+			]
+		);
+		$day_view = View::make( Day_View::class, $context );
+
 		$html = $day_view->get_html();
 
 		// Let's make sure the View is displaying what events we expect it to display.
