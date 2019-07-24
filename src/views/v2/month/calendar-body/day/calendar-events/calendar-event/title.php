4ae5dd087632167d2607d12487faa85ac127809a
Juanfra Aldasoro <juanfra@tri.be>
2019-07-24 14:08:37 -0300

    Update title.php

--- src/views/v2/month/calendar-body/day/calendar-events/calendar-event/title.php
+++ src/views/v2/month/calendar-body/day/calendar-events/calendar-event/title.php
@@ -7,9 +7,9 @@
  *
  * See more documentation about our views templating system.
  *
- * @link {INSERT_ARTCILE_LINK_HERE}
+ * @link {INSERT_ARTICLE_LINK_HERE}
  *
- * @version 4.9.4
+ * @version TBD
  *
  */
 $event    = $this->get( 'event' );
@@ -22,8 +22,8 @@ $event_id = $event->ID;
 		rel="bookmark"
 		class="tribe-events-calendar-month__calendar-event-title-link tribe-common-anchor-thin"
 		data-js="tribe-events-tooltip"
-		data-tooltip-content="#tribe-tooltip-content-<?php echo esc_attr( $event_id ); ?>"
-		aria-describedby="tribe-tooltip-content-<?php echo esc_attr( $event_id ); ?>"
+		data-tooltip-content="#tribe-events-tooltip-content-<?php echo esc_attr( $event_id ); ?>"
+		aria-describedby="tribe-events-tooltip-content-<?php echo esc_attr( $event_id ); ?>"
 	>
 		<?php echo esc_html( $event->title ); ?>
 	</a>
