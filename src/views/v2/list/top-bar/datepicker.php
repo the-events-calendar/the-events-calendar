13ffd17c66422c4d44b4af47f7735d6d12f03b4e
Paul <kim.paul.ms@gmail.com>
2019-08-01 01:34:33 +0800

    fix override paths

--- src/views/v2/list/top-bar/datepicker.php
+++ src/views/v2/list/top-bar/datepicker.php
@@ -3,7 +3,7 @@
  * View: Top Bar - Date Picker
  *
  * Override this template in your own theme by creating a file at:
- * [your-theme]/tribe/events/views/v2/top-bar/datepicker.php
+ * [your-theme]/tribe/events/views/v2/list/top-bar/datepicker.php
  *
  * See more documentation about our views templating system.
  *
