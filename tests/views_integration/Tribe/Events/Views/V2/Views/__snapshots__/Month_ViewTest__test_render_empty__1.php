bac8831fd151710f4a32b01a1af6d29a8e896935
Paul <kim.paul.ms@gmail.com>
2019-08-03 02:17:28 +0800

    fix broken tests

--- tests/views_integration/Tribe/Events/Views/V2/Views/__snapshots__/Month_ViewTest__test_render_empty__1.php
+++ tests/views_integration/Tribe/Events/Views/V2/Views/__snapshots__/Month_ViewTest__test_render_empty__1.php
@@ -91,13 +91,10 @@
 	<div class="tribe-events-c-events-bar__views">
 	<h3 class="tribe-common-a11y-visual-hide">
 		Event Views Navigation	</h3>
-	<div class="tribe-events-c-view-selector tribe-events-c-view-selector--tabs" data-js="tribe-events-view-selector">
+	<div  class="tribe-events-c-view-selector tribe-events-c-view-selector--tabs"  data-js="tribe-events-view-selector">
 		<button
 			class="tribe-events-c-view-selector__button"
-			aria-controls="tribe-events-view-selector-content"
-			aria-expanded="false"
-			aria-selected="false"
-			data-js="tribe-events-accordion-trigger"
+			data-js="tribe-events-view-selector-button"
 		>
 			<span class="tribe-events-c-view-selector__button-icon tribe-common-svgicon tribe-common-svgicon--month"></span>
 			<span class="tribe-events-c-view-selector__button-text">
@@ -106,7 +103,7 @@
 		<div
 	class="tribe-events-c-view-selector__content"
 	id="tribe-events-view-selector-content"
-	aria-hidden="true"
+	data-js="tribe-events-view-selector-list-container"
 >
 	<ul class="tribe-events-c-view-selector__list">
 					<li class="tribe-events-c-view-selector__list-item tribe-events-c-view-selector__list-item--list">
@@ -203,6 +200,7 @@
 		name="tribe-events-views[tribe-bar-search]"
 		value="2019-01-01"
 		tabindex="-1"
+		autocomplete="off"
 	/>
 </div>
 
