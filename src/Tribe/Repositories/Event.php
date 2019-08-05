2086351c7e3e94b0822a35ecfdc9429b9271e8a8
Luca Tumedei <luca@theaveragedev.com>
2019-08-05 14:10:14 +0200

    fix(Repository/Event) handle order-by correctly

--- src/Tribe/Repositories/Event.php
+++ src/Tribe/Repositories/Event.php
@@ -1554,6 +1554,8 @@ class Tribe__Events__Repositories__Event extends Tribe__Repository {
 
 		$postmeta_table = "orderby_{$meta_alias}_meta";
 
+		$filter_id = 'order_by_date';
+
 		$this->filter_query->join(
 			$wpdb->prepare(
 				"
@@ -1564,10 +1566,13 @@ class Tribe__Events__Repositories__Event extends Tribe__Repository {
 					)
 				",
 				$meta_key
-			)
+			),
+			$filter_id,
+			true
 		);
-		$this->filter_query->orderby( $meta_alias );
-		$this->filter_query->fields( "MIN( {$postmeta_table}.meta_value ) AS {$meta_alias}" );
+
+		$this->filter_query->orderby( $meta_alias, $filter_id, true );
+		$this->filter_query->fields( "MIN( {$postmeta_table}.meta_value ) AS {$meta_alias}", $filter_id, true );
 	}
 
 	/**
@@ -1597,8 +1602,10 @@ class Tribe__Events__Repositories__Event extends Tribe__Repository {
 				$meta_key
 			)
 		);
-		$this->filter_query->orderby( 'organizer' );
-		$this->filter_query->fields( "{$posts_table}.post_title AS organizer" );
+
+		$filter_id = 'order_by_organizer';
+		$this->filter_query->orderby( 'organizer', $filter_id, true );
+		$this->filter_query->fields( "{$posts_table}.post_title AS organizer", $filter_id, true );
 	}
 
 	/**
@@ -1628,8 +1635,10 @@ class Tribe__Events__Repositories__Event extends Tribe__Repository {
 				$meta_key
 			)
 		);
-		$this->filter_query->orderby( 'venue' );
-		$this->filter_query->fields( "{$posts_table}.post_title AS venue" );
+
+		$filter_id = 'order_by_venue';
+		$this->filter_query->orderby( 'venue', $filter_id, true );
+		$this->filter_query->fields( "{$posts_table}.post_title AS venue", $filter_id, true );
 	}
 
 	/**
