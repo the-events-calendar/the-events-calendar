a66db1702a6c3bdf7b27e2cdc18670a923b7e4f3
Luca Tumedei <luca@theaveragedev.com>
2019-08-15 11:34:57 +0200

    test(views_integration) fix broken tests

--- tests/views_integration/Tribe/Events/Views/V2/Partials/Month/Calendar_Body/Day/__snapshots__/Multiday_EventsTest__test_render_with_context__1.php
+++ tests/views_integration/Tribe/Events/Views/V2/Partials/Month/Calendar_Body/Day/__snapshots__/Multiday_EventsTest__test_render_with_context__1.php
@@ -3,11 +3,11 @@
 	<div class="tribe-events-calendar-month__multiday-event-wrapper">
 	<article  class="tribe-events-calendar-month__multiday-event tribe-events-calendar-month__multiday-event--hidden"  data-event-id="999998">
 		<time
-			datetime="2019-08-14"
+			datetime="2018-01-01"
 			class="tribe-common-a11y-visual-hide"
 		>
-			2019-08-14		</time>
-		<a href="" class="tribe-events-calendar-month__multiday-event-inner">
+			2018-01-01		</time>
+		<a href="http://test.tri.be/?tribe_events=test-event-2018-01-01-10am" class="tribe-events-calendar-month__multiday-event-inner">
 						<h3 class="tribe-events-calendar-month__multiday-event-title tribe-common-h8">
 							</h3>
 		</a>
@@ -17,13 +17,13 @@
 
 
 	<div class="tribe-events-calendar-month__multiday-event-wrapper">
-	<article  class="tribe-events-calendar-month__multiday-event tribe-events-calendar-month__multiday-event--hidden"  data-event-id="99999">
+	<article  class="tribe-events-calendar-month__multiday-event tribe-events-calendar-month__multiday-event--width-"  data-event-id="99999">
 		<time
-			datetime="2019-08-14"
+			datetime="2018-01-02"
 			class="tribe-common-a11y-visual-hide"
 		>
-			2019-08-14		</time>
-		<a href="" class="tribe-events-calendar-month__multiday-event-inner">
+			2018-01-02		</time>
+		<a href="http://test.tri.be/?tribe_events=test-event-2018-01-02-10am" class="tribe-events-calendar-month__multiday-event-inner">
 						<h3 class="tribe-events-calendar-month__multiday-event-title tribe-common-h8">
 							</h3>
 		</a>
