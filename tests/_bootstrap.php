9c5c7b2a33c249b970d1f3be4c57b062ae18106a
Luca Tumedei <luca@theaveragedev.com>
2019-08-20 14:27:05 +0200

    test(_bootstrap.php) move Codeception to snapshot-assertions bridge here

--- tests/_bootstrap.php
+++ tests/_bootstrap.php
@@ -4,3 +4,14 @@ Codeception\Util\Autoload::addNamespace( 'Tribe\Tests', dirname(__DIR__) . '/com
 Codeception\Util\Autoload::addNamespace( 'Tribe\Events\Test', __DIR__ . '/_support' );
 Codeception\Util\Autoload::addNamespace( 'Tribe\Events\Test', __DIR__ . '/_support/classes' );
 Codeception\Util\Autoload::addNamespace( 'Tribe\Events\Test\Acceptance\Steps', __DIR__ . '/acceptance/_steps' );
+
+/**
+ * Codeception will regenerate snapshots on `--debug`, while the `spatie/snapshot-assertions`
+ * library will do the same on `--update-snapshots`.
+ * Since Codeception has strict check on the CLI arguments appending `--update-snapshots` to the
+ * `vendor/bin/codecept run` command will throw an error.
+ * We handle that intention here.
+ */
+if ( Codeception\Util\Debug::isEnabled() ) {
+	$_SERVER['argv'][] = '--update-snapshots';
+}
