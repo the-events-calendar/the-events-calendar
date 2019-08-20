10f52273841f39c562a3272c6f3d24bb9013c09a
Luca Tumedei <luca@theaveragedev.com>
2019-08-20 14:27:05 +0200

    test(_bootstrap.php) move Codeception to snapshot-assertions bridge here

--- tests/views_integration/_bootstrap.php
+++ tests/views_integration/_bootstrap.php
@@ -1,6 +1,5 @@
 <?php
 
-use Codeception\Util\Debug;
 use Tribe\Events\Views\V2\Service_Provider;
 
 // Let's  make sure Views v2 are activated if not.
@@ -11,14 +10,3 @@ tribe_register_provider( Service_Provider::class );
 global $wp_rewrite;
 $wp_rewrite->permalink_structure = '/%postname%/';
 $wp_rewrite->rewrite_rules();
-
-/**
- * Codeception will regenerate snapshots on `--debug`, while the `spatie/snapshot-assertions`
- * library will do the same on `--update-snapshots`.
- * Since Codeception has strict check on the CLI arguments appending `--update-snapshots` to the
- * `vendor/bin/codecept run` command will throw an error.
- * We handle that intention here.
- */
-if ( Debug::isEnabled() ) {
-	$_SERVER['argv'][] = '--update-snapshots';
-}
