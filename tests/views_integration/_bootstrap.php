2fe3d084a0f689b822e99189bd4a879f2be97c51
Luca Tumedei <luca@theaveragedev.com>
2019-08-20 14:23:57 +0200

    test(views_integration/_bootstrap.php) allow snapshot regen on --debug

--- tests/views_integration/_bootstrap.php
+++ tests/views_integration/_bootstrap.php
@@ -1,5 +1,6 @@
 <?php
 
+use Codeception\Util\Debug;
 use Tribe\Events\Views\V2\Service_Provider;
 
 // Let's  make sure Views v2 are activated if not.
@@ -11,3 +12,13 @@ global $wp_rewrite;
 $wp_rewrite->permalink_structure = '/%postname%/';
 $wp_rewrite->rewrite_rules();
 
+/**
+ * Codeception will regenerate snapshots on `--debug`, while the `spatie/snapshot-assertions`
+ * library will do the same on `--update-snapshots`.
+ * Since Codeception has strict check on the CLI arguments appending `--update-snapshots` to the
+ * `vendor/bin/codecept run` command will throw an error.
+ * We handle that intention here.
+ */
+if ( Debug::isEnabled() ) {
+	$_SERVER['argv'][] = '--update-snapshots';
+}
