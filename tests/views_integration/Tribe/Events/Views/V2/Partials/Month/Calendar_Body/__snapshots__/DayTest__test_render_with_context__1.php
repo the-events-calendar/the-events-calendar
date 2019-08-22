d566034a9abe6f5bd12f74bf1d536dbab11d731b
Juanfra Aldasoro <juanfraa@gmail.com>
2019-08-22 12:23:16 +0200

    Update test. Update snapshot.

--- tests/views_integration/Tribe/Events/Views/V2/Partials/Month/Calendar_Body/__snapshots__/DayTest__test_render_with_context__1.php
+++ tests/views_integration/Tribe/Events/Views/V2/Partials/Month/Calendar_Body/__snapshots__/DayTest__test_render_with_context__1.php
@@ -13,8 +13,10 @@
 		tabindex="-1"
 	>
 		<h3 class="tribe-events-calendar-month__day-date tribe-common-h6 tribe-common-h--alt">
-			<span class="tribe-common-a11y-visual-hide">X events, </span>
-			<time datetime="YYYY-MM-DD">
+			<span class="tribe-common-a11y-visual-hide">
+				2 events,
+			</span>
+			<time datetime="2019-07-01">
 				1			</time>
 		</h3>
 					<em
@@ -30,14 +32,16 @@
 		class="tribe-events-calendar-month__day-cell tribe-events-calendar-month__day-cell--desktop tribe-common-a11y-hidden"
 	>
 		<h3 class="tribe-events-calendar-month__day-date tribe-common-h4">
-			<span class="tribe-common-a11y-visual-hide">X events, </span>
-			<time datetime="YYYY-MM-DD">
-				<a
-										href="#link-to-day-view-if-it-has-events"
-					class="tribe-events-calendar-month__day-date-link"
-				>
-					1				</a>
-			</time>
+			<span class="tribe-common-a11y-visual-hide">
+				2 events,
+			</span>
+			<time datetime="2019-07-01">
+									<a
+						href="http://tribe.tests/events/2019-07-01"
+						class="tribe-events-calendar-month__day-date-link"
+					>
+						1					</a>
+							</time>
 		</h3>
 
 		<div class="tribe-events-calendar-month__events">
