fa3bd76f84a32c2e22e6fae5b01a494f29265a3e
Gustavo Bordoni <bordoni.dev@gmail.com>
2019-08-22 16:38:26 -0400

    Remove duplicated.

--- tests/views_integration/Tribe/Events/Views/V2/HTML/MonthTest.php
+++ tests/views_integration/Tribe/Events/Views/V2/HTML/MonthTest.php
@@ -75,7 +75,6 @@ class MonthTest extends HtmlTestCase {
 				'multiday_events'  => [],
 				'found_events'     => 0,
 				'more_events'      => 0,
-				'more_events'      => 0,
 				'day_url'          => tribe_events_get_url( [ 'eventDisplay' => 'day', 'eventDate' => $day_date ] ),
 			];
 		}
