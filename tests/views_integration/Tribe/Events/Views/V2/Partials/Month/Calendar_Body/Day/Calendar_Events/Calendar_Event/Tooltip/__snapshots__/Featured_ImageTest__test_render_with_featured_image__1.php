caa49bb8c53932d8724a6ce47e4cd1fc6d8152cb
Paul <kim.paul.ms@gmail.com>
2019-09-03 15:38:43 +0800

    add tests for tooltip featured image, fix other tests

---
+++ tests/views_integration/Tribe/Events/Views/V2/Partials/Month/Calendar_Body/Day/Calendar_Events/Calendar_Event/Tooltip/__snapshots__/Featured_ImageTest__test_render_with_featured_image__1.php
@@ -0,0 +1,19 @@
+<?php return '<div class="tribe-events-calendar-month__calendar-event-tooltip-featured-image-wrapper">
+	<a
+		href="http://test.tri.be/?tribe_events=test-event-2018-01-01-10am"
+		title="Test Event - 2018-01-01 10am"
+		rel="bookmark"
+		class="tribe-events-calendar-month__calendar-event-tooltip-featured-image-link"
+	>
+		<div class="tribe-events-calendar-month__calendar-event-tooltip-featured-image tribe-common-c-image tribe-common-c-image--bg">
+			<div
+				class="tribe-common-c-image__bg"
+				style="background-image: url(\'http://guttitest.tribe/wp-content/uploads/2019/09/featured-image-16-300x150.jpg\');"
+				role="img"
+				aria-label="Test Event - 2018-01-01 10am"
+			>
+			</div>
+		</div>
+	</a>
+</div>
+';
