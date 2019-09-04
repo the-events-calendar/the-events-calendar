2dba50ae541887bace6f0d04a957bb11438bf0e7
Paul <kim.paul.ms@gmail.com>
2019-09-04 18:42:03 +0800

    move month mobile event recurring to events pro

---
+++ src/views/v2/month/mobile-events/mobile-day/mobile-event/date/recurring.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * View: Month View - Mobile Event Recurring Icon
+ *
+ * Override this template in your own theme by creating a file at:
+ * [your-theme]/tribe/events/views/v2/month/mobile-events/mobile-day/mobile-event/date/recurring.php
+ *
+ * See more documentation about our views templating system.
+ *
+ * @link {INSERT_ARTCILE_LINK_HERE}
+ *
+ * @version TBD
+ */
+?>
