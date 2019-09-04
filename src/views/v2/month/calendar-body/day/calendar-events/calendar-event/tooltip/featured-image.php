ca12bdf332ce7f78aa100e6d07975f402a61b26d
Paul <kim.paul.ms@gmail.com>
2019-09-04 15:56:18 +0800

    fix up styles for tooltip

--- src/views/v2/month/calendar-body/day/calendar-events/calendar-event/tooltip/featured-image.php
+++ src/views/v2/month/calendar-body/day/calendar-events/calendar-event/tooltip/featured-image.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * View: Month View - Calenar Event Tooltip Featured Image
+ * View: Month View - Calendar Event Tooltip Featured Image
  *
  * Override this template in your own theme by creating a file at:
  * [your-theme]/tribe/events/views/v2/month/calendar-body/day/calendar-events/calendar-event/tooltip/featured-image.php
@@ -9,12 +9,11 @@
  *
  * @link {INSERT_ARTCILE_LINK_HERE}
  *
- * @version 4.9.4
+ * @version TBD
  *
  */
-$event = $this->get( 'event' );
 
-if ( ! isset( $event->image ) ) { // @todo: use template tags for images here
+if ( ! $event->thumbnail->exists ) {
 	return;
 }
 ?>
@@ -23,11 +22,12 @@ if ( ! isset( $event->image ) ) { // @todo: use template tags for images here
 		href="<?php echo esc_url( $event->permalink ); ?>"
 		title="<?php echo esc_attr( get_the_title( $event->ID ) ); ?>"
 		rel="bookmark"
+		class="tribe-events-calendar-month__calendar-event-tooltip-featured-image-link"
 	>
 		<div class="tribe-events-calendar-month__calendar-event-tooltip-featured-image tribe-common-c-image tribe-common-c-image--bg">
 			<div
 				class="tribe-common-c-image__bg"
-				style="background-image: url('<?php echo esc_url( $event->image ); ?>');"
+				style="background-image: url('<?php echo esc_url( $event->thumbnail->full->url ); ?>');"
 				role="img"
 				aria-label="<?php echo esc_attr( get_the_title( $event->ID ) ); ?>"
 			>
