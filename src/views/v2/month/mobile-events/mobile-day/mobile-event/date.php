2725376538ac2f62445447dc7d01c1084584d96e
Juanfra Aldasoro <juanfraa@gmail.com>
2019-09-05 17:13:40 +0200

    Update icon class names

--- src/views/v2/month/mobile-events/mobile-day/mobile-event/date.php
+++ src/views/v2/month/mobile-events/mobile-day/mobile-event/date.php
@@ -22,7 +22,7 @@ $time_format = tribe_get_time_format();
 <div class="tribe-events-calendar-month-mobile-events__mobile-event-datetime tribe-common-b2">
 	<?php if ( ! empty( $event->featured ) ) : ?>
 		<em
-			class="tribe-events-calendar-month-mobile-events__mobile-event-datetime-featured tribe-common-svgicon tribe-common-svgicon--featured"
+			class="tribe-events-calendar-month-mobile-events__mobile-event-datetime-featured-icon tribe-common-svgicon tribe-common-svgicon--featured"
 			aria-label="<?php esc_attr_e( 'Featured', 'the-events-calendar' ) ?>"
 			title="<?php esc_attr_e( 'Featured', 'the-events-calendar' ) ?>"
 		>
@@ -32,7 +32,7 @@ $time_format = tribe_get_time_format();
 	<?php // @todo @fe this should be moved to PRO. ?>
 	<?php if ( ! empty( $event->recurring ) ) : ?>
 		<em
-			class="tribe-events-calendar-month-mobile-events__mobile-event-datetime-recurring tribe-common-svgicon tribe-common-svgicon--recurring"
+			class="tribe-events-calendar-month-mobile-events__mobile-event-datetime-recurring-icon tribe-common-svgicon tribe-common-svgicon--recurring"
 			aria-label="<?php esc_attr_e( 'Recurring', 'the-events-calendar' ) ?>"
 			title="<?php esc_attr_e( 'Recurring', 'the-events-calendar' ) ?>"
 		>
