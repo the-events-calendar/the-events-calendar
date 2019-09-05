292d276397c26eb89cf6ca76d7f7c0c8e3fef128
Paul <kim.paul.ms@gmail.com>
2019-09-05 22:16:10 +0800

    make partial test folders and namespace consistent

---
+++ tests/views_integration/Tribe/Events/Views/V2/Partials/Month_View/Mobile_Events/Mobile_Day/Mobile_Event/TitleTest.php
@@ -0,0 +1,27 @@
+<?php
+namespace Tribe\Events\Views\V2\Partials\Month_View\Mobile_Events\Mobile_Day\Mobile_Event;
+
+use Tribe\Test\Products\WPBrowser\Views\V2\HtmlPartialTestCase;
+
+class TitleTest extends HtmlPartialTestCase
+{
+
+	protected $partial_path = 'month/mobile-events/mobile-day/mobile-event/title';
+
+	/**
+	 * Test render with context
+	 */
+	public function test_render_with_context() {
+		$event = tribe_events()->set_args(
+			[
+				'start_date' => '2018-01-01 10am',
+				'timezone'   => 'Europe/Paris',
+				'duration'   => 3 * HOUR_IN_SECONDS,
+				'title'      => 'Test Event - 2018-01-01 10am',
+				'status'     => 'publish',
+			]
+		)->create();
+		$event            = tribe_get_event( $event );
+		$this->assertMatchesSnapshot( $this->get_partial_html( [ 'event' => $event ] ) );
+	}
+}
---
+++ tests/views_integration/Tribe/Events/Views/V2/Partials/Month_View/NavTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Tribe\Events\Views\V2\Partials\Month_View;
+
+use Tribe\Test\Products\WPBrowser\Views\V2\HtmlPartialTestCase;
+
+class NavTest extends HtmlPartialTestCase
+{
+
+	protected $partial_path = 'month/nav';
+
+	/**
+	 * Test render with context
+	 */
+	public function test_render_with_context() {
+		$this->assertMatchesSnapshot( $this->get_partial_html( [
+			'prev_url' => '#',
+			'next_url' => '#',
+			'location' => 'calendar',
+		] ) );
+	}
+}
