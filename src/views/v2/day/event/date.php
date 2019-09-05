ea8789a1c4c3192bfde3da22481b9ec86dbc5b10
Paul <kim.paul.ms@gmail.com>
2019-09-05 21:52:26 +0800

    add recurring template for day view event

--- src/views/v2/day/event/date.php
+++ src/views/v2/day/event/date.php
@@ -9,7 +9,7 @@
  *
  * @link {INSERT_ARTCILE_LINK_HERE}
  *
- * @version 4.9.4
+ * @version TBD
  *
  */
 
@@ -33,4 +33,5 @@ $is_featured = tribe( 'tec.featured_events' )->is_featured( $event_id );
 	<time class="tribe-events-calendar-day__event-datetime tribe-common-b2" datetime="1970-01-01T00:00:00+00:00">
 		<?php echo tribe_events_event_schedule_details( $event ); ?>
 	</time>
+	<?php $this->template( 'day/event/date/recurring' ); ?>
 </div>
---
+++ src/views/v2/day/event/date/recurring.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * View: Day View - Single Event Recurring Icon
+ *
+ * Override this template in your own theme by creating a file at:
+ * [your-theme]/tribe/events/views/v2/day/event/date/recurring.php
+ *
+ * See more documentation about our views templating system.
+ *
+ * @link {INSERT_ARTCILE_LINK_HERE}
+ *
+ * @version TBD
+ *
+ */
