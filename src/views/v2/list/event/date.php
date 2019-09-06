97cca8e97091f1a5813c6c30f7951c920e56d2cf
Paul <kim.paul.ms@gmail.com>
2019-09-06 14:06:21 +0800

    fix up date styles for list view

--- src/views/v2/list/event/date.php
+++ src/views/v2/list/event/date.php
@@ -20,7 +20,7 @@ $is_featured = tribe( 'tec.featured_events' )->is_featured( $event_id );
 $event_date_attr = tribe_get_start_date( $event, true, Dates::DBDATEFORMAT );
 
 ?>
-<div class="tribe-events-calendar-list__event-datetime-wrapper">
+<div class="tribe-events-calendar-list__event-datetime-wrapper tribe-common-b2">
 	<?php if ( $is_featured ) : ?>
 		<em
 			class="tribe-events-calendar-list__event-datetime-featured-icon tribe-common-svgicon tribe-common-svgicon--featured"
@@ -28,11 +28,11 @@ $event_date_attr = tribe_get_start_date( $event, true, Dates::DBDATEFORMAT );
 			title="<?php esc_attr_e( 'Featured', 'the-events-calendar' ); ?>"
 		>
 		</em>
-		<span class="tribe-events-calendar-list__event-datetime-featured-text tribe-common-b2">
+		<span class="tribe-events-calendar-list__event-datetime-featured-text">
 			<?php esc_html_e( 'Featured', 'the-events-calendar' ); ?>
 		</span>
 	<?php endif; ?>
-	<time class="tribe-events-calendar-list__event-datetime tribe-common-b2" datetime="<?php echo esc_attr( $event_date_attr ); ?>">
+	<time class="tribe-events-calendar-list__event-datetime" datetime="<?php echo esc_attr( $event_date_attr ); ?>">
 		<?php echo tribe_events_event_schedule_details( $event ); ?>
 	</time>
 	<?php $this->template( 'list/event/date/recurring' ); ?>
