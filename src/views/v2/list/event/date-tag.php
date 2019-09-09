9179b23f9c2be23e285b7c70fe28fa852830e885
Juanfra Aldasoro <juanfraa@gmail.com>
2019-09-09 16:47:49 +0200

    remove getter, use object properties.

--- src/views/v2/list/event/date-tag.php
+++ src/views/v2/list/event/date-tag.php
@@ -9,15 +9,14 @@
  *
  * @link {INSERT_ARTCILE_LINK_HERE}
  *
- * @version 4.9.4
+ * @version TBD
  */
 
 use Tribe__Date_Utils as Dates;
 
-$event = $this->get( 'event' );
-$event_week_day  = tribe_get_start_date( $event, true, 'D' );
-$event_day_num   = tribe_get_start_date( $event, true, 'j' );
-$event_date_attr = tribe_get_start_date( $event, true, Dates::DBDATEFORMAT );
+$event_week_day  = $event->dates->start->format( 'D' );
+$event_day_num   = $event->dates->start->format( 'j' );
+$event_date_attr = $event->dates->start->format( Dates::DBDATEFORMAT );
 ?>
 <div class="tribe-events-calendar-list__event-date-tag tribe-common-g-col">
 	<time class="tribe-events-calendar-list__event-date-tag-datetime" datetime="<?php echo esc_attr( $event_date_attr ); ?>">
