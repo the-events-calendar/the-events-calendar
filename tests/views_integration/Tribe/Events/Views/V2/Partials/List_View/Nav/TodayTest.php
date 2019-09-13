5f10367d3ff78f9324822f223045aecf4288c5b6
Paul <kim.paul.ms@gmail.com>
2019-09-13 15:49:22 +0800

    add tests for list today, fix other nav tests

---
+++ tests/views_integration/Tribe/Events/Views/V2/Partials/List_View/Nav/TodayTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Tribe\Events\Views\V2\Partials\List_View\Nav;
+
+use Tribe\Test\Products\WPBrowser\Views\V2\HtmlPartialTestCase;
+
+class TodayTest extends HtmlPartialTestCase
+{
+
+	protected $partial_path = 'list/nav/today';
+
+	/**
+	 * Test render with context
+	 */
+	public function test_render_with_context() {
+		$this->assertMatchesSnapshot( $this->get_partial_html( [
+			'link' => '#',
+		] ) );
+	}
+}
--- tests/views_integration/Tribe/Events/Views/V2/Views/__snapshots__/List_ViewTest__test_render_empty__1.php
+++ tests/views_integration/Tribe/Events/Views/V2/Views/__snapshots__/List_ViewTest__test_render_empty__1.php
@@ -226,7 +226,7 @@
 
 		<li class="tribe-events-c-nav__list-item tribe-events-c-nav__list-item--today">
 	<a
-		href="#"
+		href="http://test.tri.be/events/list/"
 		class="tribe-events-c-nav__today tribe-common-b2"
 		data-js="tribe-events-view-link"
 	>
