73399287759ebfda272d735aca5405cd77564d0f
Paul <kim.paul.ms@gmail.com>
2019-09-17 16:02:08 +0800

    add datepicker tests with date

--- tests/views_integration/Tribe/Events/Views/V2/Partials/Day_View/Top_Bar/DatepickerTest.php
+++ tests/views_integration/Tribe/Events/Views/V2/Partials/Day_View/Top_Bar/DatepickerTest.php
@@ -19,4 +19,23 @@ class DatepickerTest extends HtmlPartialTestCase
 			'today' => '2018-01-01',
 		] ) );
 	}
+
+	/**
+	 * Test render with date
+	 */
+	public function test_render_with_date() {
+		$this->markTestSkipped( 'The "today" variable is not working as expected' );
+
+		add_filter( 'tribe_events_template_var', function( $value, $key, $default, $view_slug ) {
+			if ( 'bar-date' === implode( '-', $key ) ) {
+				return '2018-06-01';
+			}
+
+			return $value;
+		}, 10, 4 );
+
+		$this->assertMatchesSnapshot( $this->get_partial_html( [
+			'today' => '2018-01-01',
+		] ) );
+	}
 }
