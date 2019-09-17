87d4426974050629197edd4b8aafb82c4887d861
Paul <kim.paul.ms@gmail.com>
2019-09-17 13:02:15 +0800

    mock nonce in events bar search test

--- tests/views_integration/Tribe/Events/Views/V2/Partials/Components/Events_Bar/SearchTest.php
+++ tests/views_integration/Tribe/Events/Views/V2/Partials/Components/Events_Bar/SearchTest.php
@@ -2,12 +2,21 @@
 
 namespace Tribe\Events\Views\V2\Partials\Components\Events_Bar;
 
+use tad\FunctionMocker\FunctionMocker as Test;
 use Tribe\Test\Products\WPBrowser\Views\V2\HtmlPartialTestCase;
 
 class SearchTest extends HtmlPartialTestCase
 {
 
 	protected $partial_path = 'components/events-bar/search';
+
+	public function setUp() {
+		parent::setUp();
+		// Start Function Mocker.
+		Test::setUp();
+		// Always return the same value when creating nonces.
+		Test::replace( 'wp_create_nonce', '2ab7cc6b39' );
+	}
 
 	/**
 	 * Test render with context
