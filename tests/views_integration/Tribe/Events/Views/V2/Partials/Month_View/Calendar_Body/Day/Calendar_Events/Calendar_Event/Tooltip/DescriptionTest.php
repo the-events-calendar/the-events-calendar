18eed681ecc9fd56093c248072326fc67662268c
Juanfra Aldasoro <juanfraa@gmail.com>
2019-09-24 15:58:13 +0200

    Update description snapshot testing for month view (tooltip).

--- tests/views_integration/Tribe/Events/Views/V2/Partials/Month_View/Calendar_Body/Day/Calendar_Events/Calendar_Event/Tooltip/DescriptionTest.php
+++ tests/views_integration/Tribe/Events/Views/V2/Partials/Month_View/Calendar_Body/Day/Calendar_Events/Calendar_Event/Tooltip/DescriptionTest.php
@@ -2,25 +2,31 @@
 
 namespace Tribe\Events\Views\V2\Partials\Month_View\Calendar_Body\Day\Calendar_Events\Calendar_Event\Tooltip;
 
+use Tribe\Test\PHPUnit\Traits\With_Post_Remapping;
 use Tribe\Test\Products\WPBrowser\Views\V2\HtmlPartialTestCase;
 
 class DescriptionTest extends HtmlPartialTestCase
 {
+	use With_Post_Remapping;
 
 	protected $partial_path = 'month/calendar-body/day/calendar-events/calendar-event/tooltip/description';
 
 	/**
-	 * Test render with context
+	 * Test render with description.
 	 */
-	public function test_render_with_context() {
-		tribe_events()->set_args( [
-			'start_date' => '2018-01-01 10am',
-			'timezone'   => 'Europe/Paris',
-			'duration'   => 3 * HOUR_IN_SECONDS,
-			'title'      => 'Test Event - 2018-01-01 10am',
-			'status'     => 'publish',
-		] )->create();
+	public function test_render_with_description() {
+		$content = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.';
+		$event = $this->get_mock_event( 'events/single/1.template.json', [ 'post_content' => $content ] );
 
-		$this->assertMatchesSnapshot( $this->get_partial_html( [ 'event' => tribe_events()->first() ] ) );
+		$this->assertMatchesSnapshot( $this->get_partial_html( [ 'event' => $event ] ) );
+	}
+
+	/**
+	 * Test render without description.
+	 */
+	public function test_render_without_description() {
+		$event = $this->get_mock_event( 'events/single/1.json', [] );
+
+		$this->assertMatchesSnapshot( $this->get_partial_html( [ 'event' => $event ] ) );
 	}
 }
