3dd6ba5418f7f93387323b0b344c05329cbfb311
Juanfra Aldasoro <juanfraa@gmail.com>
2019-09-24 15:57:46 +0200

    Add description snapshot testing for list view.

---
+++ tests/views_integration/Tribe/Events/Views/V2/Partials/List_View/Event/DescriptionTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Tribe\Events\Views\V2\Partials\List_View\Event;
+
+use Tribe\Test\PHPUnit\Traits\With_Post_Remapping;
+use Tribe\Test\Products\WPBrowser\Views\V2\HtmlPartialTestCase;
+
+class DescriptionTest extends HtmlPartialTestCase
+{
+	use With_Post_Remapping;
+
+	protected $partial_path = 'list/event/description';
+
+	/**
+	 * Test render with description.
+	 */
+	public function test_render_with_description() {
+		$content = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.';
+		$event = $this->get_mock_event( 'events/single/1.template.json', [ 'post_content' => $content ] );
+
+		$this->assertMatchesSnapshot( $this->get_partial_html( [ 'event' => $event ] ) );
+	}
+
+	/**
+	 * Test render without description.
+	 */
+	public function test_render_without_description() {
+		$event = $this->get_mock_event( 'events/single/1.json', [] );
+
+		$this->assertMatchesSnapshot( $this->get_partial_html( [ 'event' => $event ] ) );
+	}
+
+}
