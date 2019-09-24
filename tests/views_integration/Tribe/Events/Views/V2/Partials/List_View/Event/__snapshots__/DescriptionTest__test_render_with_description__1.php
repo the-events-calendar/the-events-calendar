bd3ac524fd178e2f34e1d50b7915cf026220c8c9
Juanfra Aldasoro <juanfraa@gmail.com>
2019-09-24 16:28:56 +0200

    Snapshot cleanup.

--- tests/views_integration/Tribe/Events/Views/V2/Partials/List_View/Event/__snapshots__/DescriptionTest__test_render_with_description__1.php
+++
@@ -1,4 +0,0 @@
-<?php return '<div class="tribe-events-calendar-list__event-description tribe-common-b2">
-	<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-</div>
-';
