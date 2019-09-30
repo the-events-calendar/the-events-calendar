d3fa70de56681e3784fb10c6267a5dfc84dfaa18
Juanfra Aldasoro <juanfraa@gmail.com>
2019-09-30 12:19:20 +0200

    Update snapshots.

--- tests/views_integration/Tribe/Events/Views/V2/Partials/Month_View/Calendar_Body/Day/Calendar_Events/__snapshots__/Calendar_EventTest__test_render_with_event__1.php
+++ tests/views_integration/Tribe/Events/Views/V2/Partials/Month_View/Calendar_Body/Day/Calendar_Events/__snapshots__/Calendar_EventTest__test_render_with_event__1.php
@@ -17,24 +17,11 @@
 		title="Single Event 1"
 		rel="bookmark"
 		class="tribe-events-calendar-month__calendar-event-title-link tribe-common-anchor-thin"
-				data-js="tribe-events-tooltip"
-		data-tooltip-content="#tribe-events-tooltip-content-8"
-		aria-describedby="tribe-events-tooltip-content-8"
-	>
+			>
 		Single Event 1	</a>
 </h3>
 
-		<div
-	class="tribe-events-calendar-month__calendar-event-tooltip"
-	data-js="tribe-events-tooltip-content"
-	role="tooltip"
->
-	<div id="tribe-events-tooltip-content-8">
-				<div class="tribe-events-calendar-month__calendar-event-tooltip-description tribe-common-b3">
-	</div>
-			</div>
-</div>
-
+		
 	</div>
 
 </article>
