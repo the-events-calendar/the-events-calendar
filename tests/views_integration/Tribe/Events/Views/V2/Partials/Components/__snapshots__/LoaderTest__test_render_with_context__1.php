55fae16dee6c8e5d02de4e47df2af07a6f6de2e8
Juanfra Aldasoro <juanfraa@gmail.com>
2019-10-08 12:43:51 +0200

    Update snapshots.

--- tests/views_integration/Tribe/Events/Views/V2/Partials/Components/__snapshots__/LoaderTest__test_render_with_context__1.php
+++ tests/views_integration/Tribe/Events/Views/V2/Partials/Components/__snapshots__/LoaderTest__test_render_with_context__1.php
@@ -3,7 +3,10 @@
 	role="alert"
 	aria-live="assertive"
 >
-	<div class="tribe-events-view-loader__spinner">
-		Loading...	</div>
+		<div class="tribe-events-view-loader__dots tribe-common-c-loader">
+			<div class="tribe-common-c-loader__dot tribe-common-c-loader__dot--first"></div>
+			<div class="tribe-common-c-loader__dot tribe-common-c-loader__dot--second"></div>
+			<div class="tribe-common-c-loader__dot tribe-common-c-loader__dot--third"></div>
+		</div>
 </div>
 ';
