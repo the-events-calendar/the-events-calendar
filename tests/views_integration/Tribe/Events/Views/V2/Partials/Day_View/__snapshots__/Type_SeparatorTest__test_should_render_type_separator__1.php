8cca60e0c846e914d829a538dcae1f7221598d79
Juanfra Aldasoro <juanfraa@gmail.com>
2019-10-10 12:44:29 +0200

    Update snapshot.

--- tests/views_integration/Tribe/Events/Views/V2/Partials/Day_View/__snapshots__/Type_SeparatorTest__test_should_render_type_separator__1.php
+++ tests/views_integration/Tribe/Events/Views/V2/Partials/Day_View/__snapshots__/Type_SeparatorTest__test_should_render_type_separator__1.php
@@ -1,5 +1,4 @@
 <?php return '<div class="tribe-events-calendar-day__type-separator">
 	<span class="tribe-events-calendar-day__type-separator-text tribe-common-h7 tribe-common-h6--min-medium tribe-common-h--alt">
 		Ongoing	</span>
-</div>
-';
+</div>';
