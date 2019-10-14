6f4e9ce9518fa839777f2bf917fac36401cb8ebe
Juanfra Aldasoro <juanfraa@gmail.com>
2019-10-14 13:35:31 +0200

    Update snapshots.

--- tests/views_integration/Tribe/Events/Views/V2/Partials/Day_View/Event/__snapshots__/VenueTest__test_render_with_event_with_venue__1.php
+++ tests/views_integration/Tribe/Events/Views/V2/Partials/Day_View/Event/__snapshots__/VenueTest__test_render_with_event_with_venue__1.php
@@ -2,20 +2,6 @@
 	<span class="tribe-events-calendar-day__event-venue-title tribe-common-b2--bold">
 		Venue 2233 title	</span>
 	<span class="tribe-events-calendar-day__event-venue-address">
-		<span class="tribe-address">
-
-<span class="tribe-street-address">100 Rue de Le Chat</span>
-	
-		<br>
-		<span class="tribe-locality">Paris</span><span class="tribe-delimiter">,</span>
-
-	<abbr class="tribe-region tribe-events-abbr" title="Ile de France">Ile de France</abbr>
-
-	<span class="tribe-postal-code">75019</span>
-
-	<span class="tribe-country-name">France</span>
-
-</span>
-	</span>
+		100 Rue de Le Chat, 					Paris			</span>
 </address>
 ';
--- tests/views_integration/Tribe/Events/Views/V2/Partials/Day_View/Event/__snapshots__/VenueTest__test_render_with_event_without_venue__1.php
+++ tests/views_integration/Tribe/Events/Views/V2/Partials/Day_View/Event/__snapshots__/VenueTest__test_render_with_event_without_venue__1.php
@@ -1,15 +1,1 @@
-<?php return '<address class="tribe-events-calendar-day__event-venue tribe-common-b2">
-	<span class="tribe-events-calendar-day__event-venue-title tribe-common-b2--bold">
-		Venue Name	</span>
-	<span class="tribe-events-calendar-day__event-venue-address">
-		<span class="tribe-address">
-
-
-
-
-
-
-</span>
-	</span>
-</address>
-';
+<?php return '';
