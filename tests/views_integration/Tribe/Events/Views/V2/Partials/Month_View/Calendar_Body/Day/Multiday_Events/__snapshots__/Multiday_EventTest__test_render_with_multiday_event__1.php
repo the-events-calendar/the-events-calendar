5d1862926055febdbb15997377db90ab7cf7b084
Juanfra Aldasoro <juanfraa@gmail.com>
2019-10-16 14:43:10 +0200

    Update snapshots.

--- tests/views_integration/Tribe/Events/Views/V2/Partials/Month_View/Calendar_Body/Day/Multiday_Events/__snapshots__/Multiday_EventTest__test_render_with_multiday_event__1.php
+++ tests/views_integration/Tribe/Events/Views/V2/Partials/Month_View/Calendar_Body/Day/Multiday_Events/__snapshots__/Multiday_EventTest__test_render_with_multiday_event__1.php
@@ -1,14 +1,18 @@
 <?php return '<div class="tribe-events-calendar-month__multiday-event-wrapper">
-	<article  class="tribe-events-calendar-month__multiday-event post-8 tribe_events type-tribe_events status-publish hentry entry tribe-events-calendar-month__multiday-event--hidden"  data-event-id="8">
-		<time
-			datetime="2019-06-20"
-			class="tribe-common-a11y-visual-hide"
-		>
-			2019-06-20		</time>
-		<a href="http://test.tri.be/?tribe_events=single-event-1" class="tribe-events-calendar-month__multiday-event-inner">
-						<h3 class="tribe-events-calendar-month__multiday-event-title tribe-common-h8">
-				Single Event 1			</h3>
-		</a>
-	</article>
+	<article  class="tribe-events-calendar-month__multiday-event post-8 tribe_events type-tribe_events status-publish hentry entry"  data-event-id="8">
+		<div class="tribe-events-calendar-month__multiday-event-hidden">
+			<time
+				datetime="2019-06-20"
+				class="tribe-common-a11y-visual-hide"
+			>
+				2019-06-20			</time>
+			<a
+				href="http://test.tri.be/?tribe_events=single-event-1"
+							>
+								<h3 class="tribe-events-calendar-month__multiday-event-title tribe-common-h8">
+					Single Event 1				</h3>
+			</a>
+		</div>
+			</article>
 </div>
 ';
