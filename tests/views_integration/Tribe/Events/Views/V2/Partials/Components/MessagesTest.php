483e90aa3f08a482cb2898b15798a60b12fb90c4
Juanfra Aldasoro <juanfraa@gmail.com>
2019-10-17 12:54:59 +0200

    Add tests.

---
+++ tests/views_integration/Tribe/Events/Views/V2/Partials/Components/MessagesTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Tribe\Events\Views\V2\Partials\Components;
+
+use Tribe\Test\Products\WPBrowser\Views\V2\HtmlPartialTestCase;
+
+class MessagesTest extends HtmlPartialTestCase
+{
+
+	protected $partial_path = 'components/messages';
+
+	/**
+	 * Test render with context
+	 */
+	public function test_render_with_context() {
+
+		$messages = [
+			'notice' => [
+				'There were no results found',
+			],
+		];
+
+		$this->assertMatchesSnapshot( $this->get_partial_html( [ 'messages' => $messages ] ) );
+	}
+
+	/**
+	 * Test render empty
+	 */
+	public function test_render_no_messages() {
+		$this->assertMatchesSnapshot( $this->get_partial_html( [] ) );
+	}
+}
---
+++ tests/views_integration/Tribe/Events/Views/V2/Partials/Components/__snapshots__/MessagesTest__test_render_with_context__1.php
@@ -0,0 +1,8 @@
+<?php return '
+<div  class="tribe-events-c-messages tribe-common-b2" >
+						<div class="tribe-events-c-messages__inner tribe-events-c-messages__inner--notice">
+				<span>There were no results found</span>
+			</div>
+			</div>
+
+';
