86afe39009896ec6b86ee8ca396d797630190268
Juanfra Aldasoro <juanfraa@gmail.com>
2019-10-22 15:55:09 -0300

    Add test.

--- tests/views_integration/Tribe/Events/Views/V2/Partials/Components/MessagesTest.php
+++ tests/views_integration/Tribe/Events/Views/V2/Partials/Components/MessagesTest.php
@@ -24,6 +24,24 @@ class MessagesTest extends HtmlPartialTestCase
 	}
 
 	/**
+	 * Test render with multiple messages.
+	 */
+	public function test_render_with_multiple_messages_html() {
+
+		$messages = [
+			'notice' => [
+				'There were no results found',
+			],
+			'error' => [
+				'The first rule of fight club is, you do not talk about fight club.',
+				'The second rule of fight club is, <strong>you do not talk about fight club.</strong>',
+			],
+		];
+
+		$this->assertMatchesSnapshot( $this->get_partial_html( [ 'messages' => $messages ] ) );
+	}
+
+	/**
 	 * Test render empty
 	 */
 	public function test_render_no_messages() {
