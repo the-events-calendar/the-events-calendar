099608d095ab6748ecc03d56e4187061ac56a8d8
Juanfra Aldasoro <juanfraa@gmail.com>
2019-10-23 09:27:56 -0300

    Update snapshots.

--- tests/views_integration/Tribe/Events/Views/V2/Partials/Components/__snapshots__/MessagesTest__test_render_with_html__1.php
+++ tests/views_integration/Tribe/Events/Views/V2/Partials/Components/__snapshots__/MessagesTest__test_render_with_html__1.php
@@ -1,8 +1,9 @@
-<?php return '
-	<div class="tribe-events-c-messages tribe-common-b2 tribe-events-c-messages--notice" role="alert">
-		<ul class="tribe-events-c-messages__list">
-							<li class="tribe-events-c-messages__list-message">
+<?php return '<div  class="tribe-events-c-messages tribe-common-b2" >
+	<div class="tribe-events-c-messages__message tribe-events-c-messages__message--notice" role="alert">
+		<ul class="tribe-events-c-messages__message-list">
+							<li class="tribe-events-c-messages__message-list-item">
 					There were no results found for <strong>this amazing search</strong>				</li>
 					</ul>
 	</div>
+</div>
 ';
--- tests/views_integration/Tribe/Events/Views/V2/Partials/Components/__snapshots__/MessagesTest__test_render_with_multiple_messages_html__1.php
+++ tests/views_integration/Tribe/Events/Views/V2/Partials/Components/__snapshots__/MessagesTest__test_render_with_multiple_messages_html__1.php
@@ -1,16 +1,17 @@
-<?php return '
-	<div class="tribe-events-c-messages tribe-common-b2 tribe-events-c-messages--notice" role="alert">
-		<ul class="tribe-events-c-messages__list">
-							<li class="tribe-events-c-messages__list-message">
+<?php return '<div  class="tribe-events-c-messages tribe-common-b2" >
+	<div class="tribe-events-c-messages__message tribe-events-c-messages__message--notice" role="alert">
+		<ul class="tribe-events-c-messages__message-list">
+							<li class="tribe-events-c-messages__message-list-item">
 					There were no results found				</li>
 					</ul>
 	</div>
-	<div class="tribe-events-c-messages tribe-common-b2 tribe-events-c-messages--error" role="alert">
-		<ul class="tribe-events-c-messages__list">
-							<li class="tribe-events-c-messages__list-message">
+	<div class="tribe-events-c-messages__message tribe-events-c-messages__message--error" role="alert">
+		<ul class="tribe-events-c-messages__message-list">
+							<li class="tribe-events-c-messages__message-list-item">
 					The first rule of fight club is, you do not talk about fight club.				</li>
-							<li class="tribe-events-c-messages__list-message">
+							<li class="tribe-events-c-messages__message-list-item">
 					The second rule of fight club is, <strong>you do not talk about fight club.</strong>				</li>
 					</ul>
 	</div>
+</div>
 ';
