c26dd485d81eee6474bf8ada67dc9733acb12dca
Juanfra Aldasoro <juanfraa@gmail.com>
2019-10-23 09:20:24 -0300

    CRs.

--- src/views/v2/components/messages.php
+++ src/views/v2/components/messages.php
@@ -20,16 +20,19 @@ if ( empty( $messages ) ) {
 	return;
 }
 
-?>
+$classes = [ 'tribe-events-c-messages', 'tribe-common-b2' ];
 
+?>
+<div <?php tribe_classes( $classes ); ?>>
 <?php foreach ( $messages as $message_type => $message_group ) : ?>
-	<div class="tribe-events-c-messages tribe-common-b2 tribe-events-c-messages--<?php echo esc_attr( $message_type ); ?>" role="alert">
-		<ul class="tribe-events-c-messages__list">
+	<div class="tribe-events-c-messages__message tribe-events-c-messages__message--<?php echo esc_attr( $message_type ); ?>" role="alert">
+		<ul class="tribe-events-c-messages__message-list">
 			<?php foreach ( $message_group as $message ) : ?>
-				<li class="tribe-events-c-messages__list-message">
+				<li class="tribe-events-c-messages__message-list-item">
 					<?php echo wp_kses_post( $message ); ?>
 				</li>
 			<?php endforeach; ?>
 		</ul>
 	</div>
 <?php endforeach; ?>
+</div>
