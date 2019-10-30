9067ff3f48573249c1a6e405a75b64fc68da23a2
Juanfra Aldasoro <juanfraa@gmail.com>
2019-10-30 19:48:02 -0300

    Update snapshot.

--- tests/views_integration/Tribe/Events/Views/V2/Partials/Components/__snapshots__/Read_MoreTest__test_render_with_context__1.php
+++ tests/views_integration/Tribe/Events/Views/V2/Partials/Components/__snapshots__/Read_MoreTest__test_render_with_context__1.php
@@ -1,7 +1,7 @@
 <?php return '<span class="tribe-events-c-read-more-hellip"> &hellip; </span>
 <div class="tribe-events-c-small-cta tribe-common-b3 tribe-events-c-read-more">
 	<a
-		href="http://theeventscalendar.com"
+		href="http://test.tri.be/?tribe_events=single-event-1"
 		class="tribe-events-c-small-cta__link tribe-common-cta tribe-common-cta--thin-alt"
 	>Continue Reading</a>
 </div>
