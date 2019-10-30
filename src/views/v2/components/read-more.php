8cfe2cbf8ca5b5d580f3a61e22039545ec9456b8
Juanfra Aldasoro <juanfraa@gmail.com>
2019-10-30 18:49:02 -0300

    Update docblock.

--- src/views/v2/components/read-more.php
+++ src/views/v2/components/read-more.php
@@ -11,7 +11,9 @@
  *
  * @version TBD
  *
- * @var string $permalink The permalink.
+ * @var WP_Post $event The event post object with properties added by the `tribe_get_event` function.
+ *
+ * @see tribe_get_event() For the format of the event object.
  */
 ?>
 <span class="tribe-events-c-read-more-hellip"> &hellip; </span>
