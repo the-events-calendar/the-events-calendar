01740e98ecff31dd3c6024b329106772b89588d3
Juanfra Aldasoro <juanfraa@gmail.com>
2019-10-30 10:17:02 -0300

    Add snapshot.

---
+++ tests/views_integration/Tribe/Events/Views/V2/Partials/Components/__snapshots__/Read_MoreTest__test_render_with_context__1.php
@@ -0,0 +1,8 @@
+<?php return '<span class="tribe-events-c-small-cta-hellip"> &hellip; </span>
+<div class="tribe-events-c-small-cta tribe-common-b3 tribe-events-c-small-cta--readmore">
+	<a
+		href="http://theeventscalendar.com"
+		class="tribe-events-c-small-cta__link tribe-common-cta tribe-common-cta--thin-alt"
+	>Continue Reading</a>
+</div>
+';
