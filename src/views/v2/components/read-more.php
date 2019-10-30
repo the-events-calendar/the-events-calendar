f4e99cbf6200d49a9c23ae390fbbab3fb6b6e95c
Juanfra Aldasoro <juanfraa@gmail.com>
2019-10-30 11:41:39 -0300

    CR.

--- src/views/v2/components/read-more.php
+++ src/views/v2/components/read-more.php
@@ -13,8 +13,8 @@
  */
 
 ?>
-<span class="tribe-events-c-small-cta-hellip"> &hellip; </span>
-<div class="tribe-events-c-small-cta tribe-common-b3 tribe-events-c-small-cta--readmore">
+<span class="tribe-events-c-read-more-hellip"> &hellip; </span>
+<div class="tribe-events-c-small-cta tribe-common-b3 tribe-events-c-read-more">
 	<a
 		href="<?php echo esc_url( $permalink ); ?>"
 		class="tribe-events-c-small-cta__link tribe-common-cta tribe-common-cta--thin-alt"
--- tests/views_integration/Tribe/Events/Views/V2/Partials/Components/__snapshots__/Read_MoreTest__test_render_with_context__1.php
+++ tests/views_integration/Tribe/Events/Views/V2/Partials/Components/__snapshots__/Read_MoreTest__test_render_with_context__1.php
@@ -1,5 +1,5 @@
-<?php return '<span class="tribe-events-c-small-cta-hellip"> &hellip; </span>
-<div class="tribe-events-c-small-cta tribe-common-b3 tribe-events-c-small-cta--readmore">
+<?php return '<span class="tribe-events-c-read-more-hellip"> &hellip; </span>
+<div class="tribe-events-c-small-cta tribe-common-b3 tribe-events-c-read-more">
 	<a
 		href="http://theeventscalendar.com"
 		class="tribe-events-c-small-cta__link tribe-common-cta tribe-common-cta--thin-alt"
