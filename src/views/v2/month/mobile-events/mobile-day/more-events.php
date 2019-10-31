3bc4d4691878ed0a4c718699181d5766aeaa4276
Juanfra Aldasoro <juanfraa@gmail.com>
2019-10-31 10:37:34 -0300

    Add styles.

--- src/views/v2/month/mobile-events/mobile-day/more-events.php
+++ src/views/v2/month/mobile-events/mobile-day/more-events.php
@@ -13,8 +13,6 @@
  *
  * @var int $more_events The number of events that's not showing in the day cell or in the multi-day stack.
  * @var string $more_url A string with the URL for more events on that day
- *
- * @see tribe_get_event() For the format of the event object.
  */
 
 // Bail if there are no more events to show.
@@ -23,17 +21,17 @@ if ( empty( $more_events ) || empty( $more_url ) ) {
 }
 ?>
 
-<div class="tribe-events-calendar-month-mobile-events__more-events">
+<div class="tribe-events-calendar-month-mobile-events__more-events tribe-events-c-small-cta tribe-common-b3">
 	<a
 		href="<?php echo esc_url( $more_url ); ?>"
-		class="tribe-events-calendar-month-mobile-events__more-events-link tribe-common-h8 tribe-common-h--alt tribe-common-anchor-thin"
+		class="tribe-events-calendar-month-mobile-events__more-events-link tribe-events-c-small-cta__link tribe-common-cta tribe-common-cta--thin-alt"
 		data-js="tribe-events-view-link"
 	>
 		<?php
 		 echo esc_html(
 			 sprintf(
-				 _n( '+ %d More', '+ %d More', $more_events, 'the-events-calendar' ),
-				 $more_events
+				_n( '+ %d More', '+ %d More', $more_events, 'the-events-calendar' ),
+				$more_events
 			 )
 		 )
 		?>
