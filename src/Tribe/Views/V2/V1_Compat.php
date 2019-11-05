e31d1df7ec44266f277db332538909fa54eb765d
bordoni <bordoni.dev@gmail.com>
2019-11-04 21:36:06 -0500

    Properly disable the assets from V1 at P0

--- src/Tribe/Views/V2/V1_Compat.php
+++ src/Tribe/Views/V2/V1_Compat.php
@@ -59,6 +59,7 @@ class V1_Compat extends \tad_DI52_ServiceProvider {
 	 */
 	public function remove_v1_filters() {
 		$backcompat        = V1_Backcompat::instance();
+		$tec_bar           = tribe( 'tec.bar' );
 
 		$filters_to_remove = [
 			'query_vars'              => [
@@ -85,6 +86,18 @@ class V1_Compat extends \tad_DI52_ServiceProvider {
 				],
 				[ 'callback' => [ $backcompat, 'filter_enabled_views' ], 'priority' => 10 ],
 				[ 'callback' => [ $backcompat, 'filter_default_view' ], 'priority' => 10 ],
+			],
+			'wp_enqueue_scripts' => [
+				[ 'callback' => [ $tec_bar, 'load_script' ], 'priority' => 9 ]
+			],
+			'body_class' => [
+				[ 'callback' => [ $tec_bar, 'body_class' ], 'priority' => 10 ]
+			],
+			'tribe_events_bar_before_template' => [
+				[ 'callback' => [ $tec_bar, 'disabled_bar_before' ], 'priority' => 10 ]
+			],
+			'tribe_events_bar_after_template' => [
+				[ 'callback' => [ $tec_bar, 'disabled_bar_after' ], 'priority' => 10 ]
 			],
 		];
 
