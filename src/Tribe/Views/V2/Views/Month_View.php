5e692bf933759be272a4c13bddbf519484b778ea
Juanfra Aldasoro <juanfraa@gmail.com>
2019-11-05 11:29:44 -0300

    CR.

--- src/Tribe/Views/V2/Views/Month_View.php
+++ src/Tribe/Views/V2/Views/Month_View.php
@@ -188,9 +188,7 @@ class Month_View extends By_Day_View {
 		// The events will be returned in an array with shape `[ <Y-m-d> => [...<events>], <Y-m-d> => [...<events>] ]`.
 		$grid_days = $this->get_grid_days();
 
-		// Get the grid first index to set the grid start date.
-		reset( $grid_days );
-		$grid_start_date = key( $grid_days );
+		$grid_start_date = reset( array_keys( $grid_days ) );
 
 		/*
 		 * The messages set up before will be wrong due to an always empty `$events` array.
