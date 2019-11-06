9dd717d0dcf6f5f9e0139c1154eabd589ef4b48f
Juanfra Aldasoro <juanfraa@gmail.com>
2019-11-06 20:13:18 -0300

    Update snapshots.

--- tests/views_integration/Tribe/Events/Views/V2/Views/__snapshots__/Day_ViewTest__test_render_empty__1.php
+++ tests/views_integration/Tribe/Events/Views/V2/Views/__snapshots__/Day_ViewTest__test_render_empty__1.php
@@ -121,39 +121,6 @@
 	data-js="tribe-events-view-selector-list-container"
 >
 	<ul class="tribe-events-c-view-selector__list">
-					<li class="tribe-events-c-view-selector__list-item tribe-events-c-view-selector__list-item--list">
-	<a
-		href="http://test.tri.be/events/list/"
-		class="tribe-events-c-view-selector__list-item-link"
-		data-js="tribe-events-view-link"
-	>
-		<span class="tribe-events-c-view-selector__list-item-icon tribe-common-svgicon tribe-common-svgicon--list"></span>
-		<span class="tribe-events-c-view-selector__list-item-text">
-			List		</span>
-	</a>
-</li>
-					<li class="tribe-events-c-view-selector__list-item tribe-events-c-view-selector__list-item--month">
-	<a
-		href="http://test.tri.be/events/month/"
-		class="tribe-events-c-view-selector__list-item-link"
-		data-js="tribe-events-view-link"
-	>
-		<span class="tribe-events-c-view-selector__list-item-icon tribe-common-svgicon tribe-common-svgicon--month"></span>
-		<span class="tribe-events-c-view-selector__list-item-text">
-			Month		</span>
-	</a>
-</li>
-					<li class="tribe-events-c-view-selector__list-item tribe-events-c-view-selector__list-item--day tribe-events-c-view-selector__list-item--active">
-	<a
-		href="http://test.tri.be/events/today/"
-		class="tribe-events-c-view-selector__list-item-link"
-		data-js="tribe-events-view-link"
-	>
-		<span class="tribe-events-c-view-selector__list-item-icon tribe-common-svgicon tribe-common-svgicon--day"></span>
-		<span class="tribe-events-c-view-selector__list-item-text">
-			Day		</span>
-	</a>
-</li>
 			</ul>
 </div>
 	</div>
