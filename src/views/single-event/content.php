aca50d6f17bf41813b409bc622575b76abe2e395
bordoni <bordoni.dev@gmail.com>
2019-11-15 18:09:47 -0500

    Use the new methods from Common

--- src/views/single-event/content.php
+++ src/views/single-event/content.php
@@ -12,20 +12,9 @@
  * @version 4.7
  *
  */
-
-$event_id = $this->get( 'post_id' );
-$content = get_the_content( null, false, $event_id );
-
-/**
- * Filters the post content.
- *
- * @since 0.71
- *
- * @param string $content Content of the current post.
- */
-$content = apply_filters( 'the_content', $content );
-$content = str_replace( ']]>', ']]&gt;', $content );
 ?>
+
+<?php $event_id = $this->get( 'post_id' ); ?>
 <div id="post-<?php echo absint( $event_id ); ?>" <?php post_class(); ?>>
-	<?php echo $content; ?>
+	<?php tribe_the_content( null, false, $event_id ); ?>
 </div>
