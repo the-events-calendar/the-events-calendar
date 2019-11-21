0bc015b0f7202fe6469cc9ee380df356f27141b1
Juanfra Aldasoro <juanfraa@gmail.com>
2019-11-21 16:11:35 -0300

    Update snapshots.

--- tests/views_integration/Tribe/Events/Views/V2/Views/__snapshots__/List_ViewTest__test_render_with_upcoming_events__1.php
+++ tests/views_integration/Tribe/Events/Views/V2/Views/__snapshots__/List_ViewTest__test_render_with_upcoming_events__1.php
@@ -351,7 +351,7 @@
 
 		<div class="tribe-events-c-ical tribe-common-b1">
 	<a
-		class="tribe-events-c-ical__link"
+		class="tribe-common-anchor-alt"
 		title="Use this to share calendar data with Google Calendar, Apple iCal and other compatible apps"
 		href="http://test.tri.be/events/?ical=1"
 	>Export Events</a>
