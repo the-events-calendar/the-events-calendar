0c04bc92aec7cc079221c2cd84d2572134a83faa
Juanfra Aldasoro <juanfraa@gmail.com>
2019-11-21 15:43:07 -0300

    Update snapshot.

--- tests/views_integration/Tribe/Events/Views/V2/Partials/Components/__snapshots__/Ical_LinkTest__test_render_with_context__1.php
+++ tests/views_integration/Tribe/Events/Views/V2/Partials/Components/__snapshots__/Ical_LinkTest__test_render_with_context__1.php
@@ -1,6 +1,6 @@
 <?php return '<div class="tribe-events-c-ical tribe-common-b1">
 	<a
-		class="tribe-events-c-ical__link tribe-events-c-small-cta__link tribe-common-cta--thin-alt"
+		class="tribe-events-c-ical__link"
 		title="Use this to share calendar data with Google Calendar, Apple iCal and other compatible apps"
 		href="http://tests.tri.be/?ical=something"
 	>Export Events</a>
