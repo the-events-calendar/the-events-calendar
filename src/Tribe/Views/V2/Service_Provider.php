edb03680254348222de97ae94b53b2342763b4e9
bordoni <bordoni.dev@gmail.com>
2019-11-25 17:13:11 -0500

    Reactivates the Beta Update toggle.

--- src/Tribe/Views/V2/Service_Provider.php
+++ src/Tribe/Views/V2/Service_Provider.php
@@ -24,9 +24,6 @@ class Service_Provider extends \tad_DI52_ServiceProvider {
 		require_once tribe( 'tec.main' )->plugin_path . 'src/functions/views/provider.php';
 
 		if ( ! tribe_events_views_v2_is_enabled() ) {
-			// Deactivates upgrade tab
-			add_filter( 'tribe_events_show_upgrade_tab', '__return_false' );
-
 			return;
 		}
 
