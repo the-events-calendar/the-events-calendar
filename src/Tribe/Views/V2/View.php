89784044e161d240580af64752d699b77343998b
Paul <kim.paul.ms@gmail.com>
2019-12-10 10:58:02 +0800

    add breakpoints to views data

--- src/Tribe/Views/V2/View.php
+++ src/Tribe/Views/V2/View.php
@@ -1194,6 +1194,11 @@ class View implements View_Interface {
 			'container_classes'      => $this->get_html_classes(),
 			'is_past'                => 'past' === $this->context->get( 'event_display_mode', false ),
 			'show_datepicker_submit' => $this->get_show_datepicker_submit(),
+			'breakpoints'            => [
+				'xsmall' => $this->get_xsmall_breakpoint(),
+				'medium' => $this->get_medium_breakpoint(),
+				'full'   => $this->get_full_breakpoint(),
+			],
 		];
 
 		return $template_vars;
@@ -1782,6 +1787,108 @@ class View implements View_Interface {
 		$show_datepicker_submit = apply_filters( "tribe_events_views_v2_view_{$this->slug}_show_datepicker_submit", $show_datepicker_submit, $this );
 
 		return $show_datepicker_submit;
+	}
+
+	/**
+	 * Returns the xsmall breakpoint value
+	 *
+	 * @since TBD
+	 *
+	 * @return int
+	 */
+	protected function get_xsmall_breakpoint() {
+		// Default value for xsmall breakpoint is 500px
+		$bp_xsmall = 500;
+
+		/**
+		 * Filters the xsmall breakpoint value.
+		 *
+		 * @since TBD
+		 *
+		 * @param object $bp_xsmall The xsmall breakpoint value.
+		 * @param View   $this      The current View instance being rendered.
+		 */
+		$bp_xsmall = apply_filters( "tribe_events_views_v2_view_breakpoint_xsmall", $bp_xsmall, $this );
+
+		/**
+		 * Filters the xsmall breakpoint value for a specific view.
+		 *
+		 * @since TBD
+		 *
+		 * @param object $bp_xsmall The xsmall breakpoint value.
+		 * @param View   $this      The current View instance being rendered.
+		 */
+		$bp_xsmall = apply_filters( "tribe_events_views_v2_view_{$this->slug}_breakpoint_xsmall", $bp_xsmall, $this );
+
+		return $bp_xsmall;
+	}
+
+	/**
+	 * Returns the medium breakpoint value
+	 *
+	 * @since TBD
+	 *
+	 * @return int
+	 */
+	protected function get_medium_breakpoint() {
+		// Default value for medium breakpoint is 768px
+		$bp_medium = 768;
+
+		/**
+		 * Filters the medium breakpoint value.
+		 *
+		 * @since TBD
+		 *
+		 * @param object $bp_medium The medium breakpoint value.
+		 * @param View   $this      The current View instance being rendered.
+		 */
+		$bp_medium = apply_filters( "tribe_events_views_v2_view_breakpoint_medium", $bp_medium, $this );
+
+		/**
+		 * Filters the medium breakpoint value for a specific view.
+		 *
+		 * @since TBD
+		 *
+		 * @param object $bp_medium The medium breakpoint value.
+		 * @param View   $this      The current View instance being rendered.
+		 */
+		$bp_medium = apply_filters( "tribe_events_views_v2_view_{$this->slug}_breakpoint_medium", $bp_medium, $this );
+
+		return $bp_medium;
+	}
+
+	/**
+	 * Returns the full breakpoint value
+	 *
+	 * @since TBD
+	 *
+	 * @return int
+	 */
+	protected function get_full_breakpoint() {
+		// Default value for full breakpoint is 960px
+		$bp_full = 960;
+
+		/**
+		 * Filters the full breakpoint value.
+		 *
+		 * @since TBD
+		 *
+		 * @param object $bp_full The full breakpoint value.
+		 * @param View   $this    The current View instance being rendered.
+		 */
+		$bp_full = apply_filters( "tribe_events_views_v2_view_breakpoint_full", $bp_full, $this );
+
+		/**
+		 * Filters the full breakpoint value for a specific view.
+		 *
+		 * @since TBD
+		 *
+		 * @param object $bp_full The full breakpoint value.
+		 * @param View   $this    The current View instance being rendered.
+		 */
+		$bp_full = apply_filters( "tribe_events_views_v2_view_{$this->slug}_breakpoint_full", $bp_full, $this );
+
+		return $bp_full;
 	}
 
 	/**
