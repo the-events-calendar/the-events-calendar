455d61868ab62907f0b728e60be144461e9bd957
Paul <kim.paul.ms@gmail.com>
2019-12-12 15:04:43 +0800

    add breakpoints tests, update snapshots

---
+++ tests/views_integration/Tribe/Events/Views/V2/Partials/Components/__snapshots__/BreakpointsTest__test_render_on_initial_load__1.php
@@ -0,0 +1,5 @@
+<?php return '<script class="tribe-events-views-breakpoints">
+	var scripts = document.getElementsByTagName( \'script\' );
+	window.tribe.events.views.breakpoints.setup( scripts[ scripts.length - 1 ] );
+</script>
+';
