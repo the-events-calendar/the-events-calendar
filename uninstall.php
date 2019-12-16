dd00268d9481d0762bc338463b5fa65ad83474fc
Zach Tirrell <zbtirrell@gmail.com>
2019-12-16 12:08:37 -0500

    no longer sending weird premium flag

--- uninstall.php
+++ uninstall.php
@@ -9,17 +9,11 @@ $tec_freemius = fs_dynamic_init( [
 	'id' => '3069',
 	'slug' => 'the-events-calendar',
 	'public_key' => 'pk_e32061abc28cfedf231f3e5c4e626',
-	[
-		'menu' => [
-			'slug'    => 'edit.php?post_type=tribe_events',
-			'account' => true,
-			'support' => false,
-		],
-		'is_premium'     => false,
-		'has_addons'     => false,
-		'has_paid_plans' => false,
-	]
+	'is_premium'     => false,
+	'has_addons'     => false,
+	'has_paid_plans' => false,
 ] );
+
 define( 'WP_FS__UNINSTALL_MODE', true );
 $tec_freemius->_uninstall_plugin_event();
 $tec_freemius->do_action( 'after_uninstall' );
