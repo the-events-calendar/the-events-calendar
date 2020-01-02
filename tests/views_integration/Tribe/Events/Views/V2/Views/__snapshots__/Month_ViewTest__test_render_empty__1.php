f8b04ab4cf923721dffd37dee549a4ee44ef1ca7
Paul <kim.paul.ms@gmail.com>
2020-01-02 17:42:48 +0800

    update tests

--- tests/views_integration/Tribe/Events/Views/V2/Views/__snapshots__/Month_ViewTest__test_render_empty__1.php
+++ tests/views_integration/Tribe/Events/Views/V2/Views/__snapshots__/Month_ViewTest__test_render_empty__1.php
@@ -196,42 +196,40 @@
 >
 	Today</a>
 
-	
 	<div class="tribe-events-c-top-bar__datepicker">
-		<button
-			class="tribe-common-h3 tribe-common-h--alt tribe-events-c-top-bar__datepicker-button"
-			data-js="tribe-events-top-bar-datepicker-button"
-			type="button"
+	<button
+		class="tribe-common-h3 tribe-common-h--alt tribe-events-c-top-bar__datepicker-button"
+		data-js="tribe-events-top-bar-datepicker-button"
+		type="button"
+	>
+		<time
+			datetime="2019-01"
+			class="tribe-events-c-top-bar__datepicker-time"
 		>
-			<time
-				datetime="2019-01"
-				class="tribe-events-c-top-bar__datepicker-time"
-			>
-				<span class="tribe-events-c-top-bar__datepicker-mobile">
-					1/2019				</span>
-				<span class="tribe-events-c-top-bar__datepicker-desktop tribe-common-a11y-hidden">
-					January 2019				</span>
-			</time>
-		</button>
-		<label
-			class="tribe-events-c-top-bar__datepicker-label tribe-common-a11y-visual-hide"
-			for="tribe-events-top-bar-date"
-		>
-			Select date.		</label>
-		<input
-			type="text"
-			class="tribe-events-c-top-bar__datepicker-input tribe-common-a11y-visual-hide"
-			data-js="tribe-events-top-bar-date"
-			id="tribe-events-top-bar-date"
-			name="tribe-events-views[tribe-bar-date]"
-			value="1/1/2019"
-			tabindex="-1"
-			autocomplete="off"
-			readonly="readonly"
-		/>
-		<div class="tribe-events-c-top-bar__datepicker-container" data-js="tribe-events-top-bar-datepicker-container"></div>
-	</div>
-
+			<span class="tribe-events-c-top-bar__datepicker-mobile">
+				1/2019			</span>
+			<span class="tribe-events-c-top-bar__datepicker-desktop tribe-common-a11y-hidden">
+				January 2019			</span>
+		</time>
+	</button>
+	<label
+		class="tribe-events-c-top-bar__datepicker-label tribe-common-a11y-visual-hide"
+		for="tribe-events-top-bar-date"
+	>
+		Select date.	</label>
+	<input
+		type="text"
+		class="tribe-events-c-top-bar__datepicker-input tribe-common-a11y-visual-hide"
+		data-js="tribe-events-top-bar-date"
+		id="tribe-events-top-bar-date"
+		name="tribe-events-views[tribe-bar-date]"
+		value="1/1/2019"
+		tabindex="-1"
+		autocomplete="off"
+		readonly="readonly"
+	/>
+	<div class="tribe-events-c-top-bar__datepicker-container" data-js="tribe-events-top-bar-datepicker-container"></div>
+</div>
 
 	<div class="tribe-events-c-top-bar__actions tribe-common-a11y-hidden">
 	</div>
