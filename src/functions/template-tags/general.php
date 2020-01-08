790ff39789d2475f6265c29e7e77e42dd59cb2a9
Matthew Batchelder <borkweb@gmail.com>
2020-01-08 13:22:25 -0500

    Fixing some filters

--- src/functions/template-tags/general.php
+++ src/functions/template-tags/general.php
@@ -1054,96 +1054,96 @@ if ( class_exists( 'Tribe__Events__Main' ) ) {
 		static $cache_details = [];
 		$cache_details_key    = "{$event->ID}:{$before}:{$after}:{$html}";
 
-		if ( isset( $cache_details[ $cache_details_key ] ) ) {
-			return $cache_details[ $cache_details_key ];
-		}
+		if ( ! isset( $cache_details[ $cache_details_key ] ) ) {
+			$inner                    = $html ? '<span class="tribe-event-date-start">' : '';
+			$format                   = '';
+			$date_without_year_format = tribe_get_date_format();
+			$date_with_year_format    = tribe_get_date_format( true );
+			$time_format              = get_option( 'time_format' );
+			$datetime_separator       = tribe_get_option( 'dateTimeSeparator', ' @ ' );
+			$time_range_separator     = tribe_get_option( 'timeRangeSeparator', ' - ' );
 
-		$inner                    = $html ? '<span class="tribe-event-date-start">' : '';
-		$format                   = '';
-		$date_without_year_format = tribe_get_date_format();
-		$date_with_year_format    = tribe_get_date_format( true );
-		$time_format              = get_option( 'time_format' );
-		$datetime_separator       = tribe_get_option( 'dateTimeSeparator', ' @ ' );
-		$time_range_separator     = tribe_get_option( 'timeRangeSeparator', ' - ' );
+			$settings = array(
+				'show_end_time' => true,
+				'time'          => true,
+			);
 
-		$settings = array(
-			'show_end_time' => true,
-			'time'          => true,
-		);
+			$settings = wp_parse_args( apply_filters( 'tribe_events_event_schedule_details_formatting', $settings ), $settings );
+			if ( ! $settings['time'] ) {
+				$settings['show_end_time'] = false;
+			}
 
-		$settings = wp_parse_args( apply_filters( 'tribe_events_event_schedule_details_formatting', $settings ), $settings );
-		if ( ! $settings['time'] ) {
-			$settings['show_end_time'] = false;
-		}
+			/**
+			 * @var $show_end_time
+			 * @var $time
+			 */
+			extract( $settings );
 
-		/**
-		 * @var $show_end_time
-		 * @var $time
-		 */
-		extract( $settings );
+			$format = $date_with_year_format;
 
-		$format = $date_with_year_format;
+			/**
+			 * If a yearless date format should be preferred.
+			 *
+			 * By default, this will be true if the event starts and ends in the current year.
+			 *
+			 * @param bool    $use_yearless_format
+			 * @param WP_Post $event
+			 */
+			$use_yearless_format = apply_filters( 'tribe_events_event_schedule_details_use_yearless_format',
+				(
+					tribe_get_start_date( $event, false, 'Y' ) === date_i18n( 'Y' )
+					&& tribe_get_end_date( $event, false, 'Y' ) === date_i18n( 'Y' )
+				),
+				$event
+			);
 
-		/**
-		 * If a yearless date format should be preferred.
-		 *
-		 * By default, this will be true if the event starts and ends in the current year.
-		 *
-		 * @param bool    $use_yearless_format
-		 * @param WP_Post $event
-		 */
-		$use_yearless_format = apply_filters( 'tribe_events_event_schedule_details_use_yearless_format',
-			(
-				tribe_get_start_date( $event, false, 'Y' ) === date_i18n( 'Y' )
-				&& tribe_get_end_date( $event, false, 'Y' ) === date_i18n( 'Y' )
-			),
-			$event
-		);
+			if ( $use_yearless_format ) {
+				$format = $date_without_year_format;
+			}
 
-		if ( $use_yearless_format ) {
-			$format = $date_without_year_format;
-		}
+			if ( tribe_event_is_multiday( $event ) ) { // multi-date event
 
-		if ( tribe_event_is_multiday( $event ) ) { // multi-date event
+				$format2ndday = apply_filters( 'tribe_format_second_date_in_range', $format, $event );
 
-			$format2ndday = apply_filters( 'tribe_format_second_date_in_range', $format, $event );
+				if ( tribe_event_is_all_day( $event ) ) {
+					$inner .= tribe_get_start_date( $event, true, $format );
+					$inner .= ( $html ? '</span>' : '' ) . $time_range_separator;
+					$inner .= $html ? '<span class="tribe-event-date-end">' : '';
 
-			if ( tribe_event_is_all_day( $event ) ) {
-				$inner .= tribe_get_start_date( $event, true, $format );
-				$inner .= ( $html ? '</span>' : '' ) . $time_range_separator;
-				$inner .= $html ? '<span class="tribe-event-date-end">' : '';
+					$end_date_full = tribe_get_end_date( $event, true, Tribe__Date_Utils::DBDATETIMEFORMAT );
+					$end_date_full_timestamp = strtotime( $end_date_full );
 
-				$end_date_full = tribe_get_end_date( $event, true, Tribe__Date_Utils::DBDATETIMEFORMAT );
-				$end_date_full_timestamp = strtotime( $end_date_full );
+					// if the end date is <= the beginning of the day, consider it the previous day
+					if ( $end_date_full_timestamp <= strtotime( tribe_beginning_of_day( $end_date_full ) ) ) {
+						$end_date = tribe_format_date( $end_date_full_timestamp - DAY_IN_SECONDS, false, $format2ndday );
+					} else {
+						$end_date = tribe_get_end_date( $event, false, $format2ndday );
+					}
 
-				// if the end date is <= the beginning of the day, consider it the previous day
-				if ( $end_date_full_timestamp <= strtotime( tribe_beginning_of_day( $end_date_full ) ) ) {
-					$end_date = tribe_format_date( $end_date_full_timestamp - DAY_IN_SECONDS, false, $format2ndday );
+					$inner .= $end_date;
 				} else {
-					$end_date = tribe_get_end_date( $event, false, $format2ndday );
+					$inner .= tribe_get_start_date( $event, false, $format ) . ( $time ? $datetime_separator . tribe_get_start_date( $event, false, $time_format ) : '' );
+					$inner .= ( $html ? '</span>' : '' )  . $time_range_separator;
+					$inner .= $html ? '<span class="tribe-event-date-end">' : '';
+					$inner .= tribe_get_end_date( $event, false, $format2ndday ) . ( $time ? $datetime_separator . tribe_get_end_date( $event, false, $time_format ) : '' );
 				}
+			} elseif ( tribe_event_is_all_day( $event ) ) { // all day event
+				$inner .= tribe_get_start_date( $event, true, $format );
+			} else { // single day event
+				if ( tribe_get_start_date( $event, false, 'g:i A' ) === tribe_get_end_date( $event, false, 'g:i A' ) ) { // Same start/end time
+					$inner .= tribe_get_start_date( $event, false, $format ) . ( $time ? $datetime_separator . tribe_get_start_date( $event, false, $time_format ) : '' );
+				} else { // defined start/end time
+					$inner .= tribe_get_start_date( $event, false, $format ) . ( $time ? $datetime_separator . tribe_get_start_date( $event, false, $time_format ) : '' );
+					$inner .= ( $html ? '</span>' : '' ) . ( $show_end_time ? $time_range_separator : '' );
+					$inner .= $html ? '<span class="tribe-event-time">' : '';
+					$inner .= ( $show_end_time ? tribe_get_end_date( $event, false, $time_format ) : '' );
+				}
+			}
 
-				$inner .= $end_date;
-			} else {
-				$inner .= tribe_get_start_date( $event, false, $format ) . ( $time ? $datetime_separator . tribe_get_start_date( $event, false, $time_format ) : '' );
-				$inner .= ( $html ? '</span>' : '' )  . $time_range_separator;
-				$inner .= $html ? '<span class="tribe-event-date-end">' : '';
-				$inner .= tribe_get_end_date( $event, false, $format2ndday ) . ( $time ? $datetime_separator . tribe_get_end_date( $event, false, $time_format ) : '' );
-			}
-		} elseif ( tribe_event_is_all_day( $event ) ) { // all day event
-			$inner .= tribe_get_start_date( $event, true, $format );
-		} else { // single day event
-			if ( tribe_get_start_date( $event, false, 'g:i A' ) === tribe_get_end_date( $event, false, 'g:i A' ) ) { // Same start/end time
-				$inner .= tribe_get_start_date( $event, false, $format ) . ( $time ? $datetime_separator . tribe_get_start_date( $event, false, $time_format ) : '' );
-			} else { // defined start/end time
-				$inner .= tribe_get_start_date( $event, false, $format ) . ( $time ? $datetime_separator . tribe_get_start_date( $event, false, $time_format ) : '' );
-				$inner .= ( $html ? '</span>' : '' ) . ( $show_end_time ? $time_range_separator : '' );
-				$inner .= $html ? '<span class="tribe-event-time">' : '';
-				$inner .= ( $show_end_time ? tribe_get_end_date( $event, false, $time_format ) : '' );
-			}
+			$inner .= $html ? '</span>' : '';
+
+			$cache_details[ $cache_details_key ] = $inner;
 		}
-
-		$inner .= $html ? '</span>' : '';
 
 		/**
 		 * Provides an opportunity to modify the *inner* schedule details HTML (ie before it is
@@ -1152,7 +1152,7 @@ if ( class_exists( 'Tribe__Events__Main' ) ) {
 		 * @param string $inner_html  the output HTML
 		 * @param int    $event_id    post ID of the event we are interested in
 		 */
-		$inner = apply_filters( 'tribe_events_event_schedule_details_inner', $inner, $event->ID );
+		$inner = apply_filters( 'tribe_events_event_schedule_details_inner', $cache_details[ $cache_details_key ], $event->ID );
 
 		// Wrap the schedule text
 		$schedule = $before . $inner . $after;
@@ -1166,7 +1166,7 @@ if ( class_exists( 'Tribe__Events__Main' ) ) {
 		 * @param string $before    part of the HTML wrapper that was prepended
 		 * @param string $after     part of the HTML wrapper that was appended
 		 */
-		return $cache_details[ $cache_details_key ] = apply_filters( 'tribe_events_event_schedule_details', $schedule, $event->ID, $before, $after );
+		return apply_filters( 'tribe_events_event_schedule_details', $schedule, $event->ID, $before, $after );
 	}
 
 	/**
