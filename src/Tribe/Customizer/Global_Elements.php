18fbf860cb478f824764f82571d8e576f7e8ab9d
Juanfra Aldasoro <juanfraa@gmail.com>
2020-01-22 17:10:56 -0300

    Update customizer to add accent color and move link color for V2

--- src/Tribe/Customizer/Global_Elements.php
+++ src/Tribe/Customizer/Global_Elements.php
@@ -31,6 +31,202 @@ final class Tribe__Events__Customizer__Global_Elements extends Tribe__Customizer
 	 */
 	public function get_css_template( $template ) {
 		$customizer = Tribe__Customizer::instance();
+		$settings   = $customizer->get_option( [ $this->ID ] );
+
+		if ( $customizer->has_option( $this->ID, 'accent_color' ) ) {
+
+			$accent_color     = new Tribe__Utils__Color( $settings['accent_color'] );
+			$accent_color_rgb = $accent_color::hexToRgb( $settings['accent_color'] );
+			$accent_css_rgb   = $accent_color_rgb['R'] . ',' . $accent_color_rgb['G'] . ',' . $accent_color_rgb['B'];
+
+
+			$template .= '
+				.tribe-common .tribe-common-c-btn,
+				.tribe-common a.tribe-common-c-btn,
+				.tribe-theme-twentytwenty .tribe-common .tribe-common-c-btn,
+				.tribe-common .tribe-common-form-control-toggle__input:checked,
+				.tribe-events .tribe-events-c-view-selector__button::before,
+				.tribe-events .tribe-events-c-events-bar__search-button::before,
+				.tribe-events .tribe-events-c-ical__link:hover,
+				.tribe-events .tribe-events-c-ical__link:focus,
+				.tribe-events .tribe-events-c-ical__link:active,
+				.tribe-theme-twentyseventeen .tribe-events .tribe-events-calendar-month__day-cell--selected:hover,
+				.tribe-theme-twentyseventeen .tribe-events .tribe-events-calendar-month__day-cell--selected:focus,
+				.tribe-theme-twentytwenty .tribe-events .tribe-events-calendar-month__day-cell--selected:hover,
+				.tribe-theme-twentytwenty .tribe-events .tribe-events-calendar-month__day-cell--selected:focus {
+					background-color: <%= global_elements.accent_color %>;
+				}
+			';
+
+			$template .= '
+				.tribe-common .tribe-common-c-btn:focus,
+				.tribe-common .tribe-common-c-btn:hover,
+				.tribe-common a.tribe-common-c-btn:focus,
+				.tribe-common a.tribe-common-c-btn:hover,
+				.tribe-events .tribe-events-calendar-month__day-cell--selected,
+				.tribe-events .tribe-events-calendar-month__mobile-events-icon--event,
+				.tribe-theme-twentytwenty .tribe-events .tribe-events-calendar-month__day-cell--selected {
+					background-color: <%= global_elements.accent_color %>;
+				}
+			';
+
+			$template .= '
+				.tribe-theme-twentytwenty .tribe-common .tribe-common-c-btn:hover,
+				.tribe-theme-twentytwenty .tribe-common .tribe-common-c-btn:focus a,
+				.tribe-theme-twentyseventeen .tribe-common .tribe-common-c-btn:hover,
+				.tribe-theme-twentyseventeen .tribe-common .tribe-common-c-btn:focus a {
+					background: rgba( ' . $accent_css_rgb . ', 0.8);
+				}
+			';
+
+			$template .= '
+				.tribe-events .datepicker .day.current,
+				.tribe-events .datepicker .month.current,
+				.tribe-events .datepicker .year.current,
+				.tribe-events .datepicker .day.current:hover,
+				.tribe-events .datepicker .day.current:focus,
+				.tribe-events .datepicker .day.current.focused,
+				.tribe-events .datepicker .month.current:hover,
+				.tribe-events .datepicker .month.current:focus,
+				.tribe-events .datepicker .month.current.focused,
+				.tribe-events .datepicker .year.current:hover,
+				.tribe-events .datepicker .year.current:focus,
+				.tribe-events .datepicker .year.current.focused,
+				.tribe-common .tribe-common-c-loader__dot {
+					background: rgba( ' . $accent_css_rgb . ', 0.07);
+				}
+			';
+
+			$template .= '
+				.tribe-events .tribe-events-calendar-month__day--current .tribe-events-calendar-month__day-date-link:hover,
+				.tribe-events .tribe-events-calendar-month__day--current .tribe-events-calendar-month__day-date-link:focus {
+					color: rgba( ' . $accent_css_rgb . ', 0.8);
+				}
+			';
+
+			$template .= '
+				.tribe-events .datepicker .day.active,
+				.tribe-events .datepicker .month.active,
+				.tribe-events .datepicker .year.active,
+				.tribe-events .datepicker .day.active:hover,
+				.tribe-events .datepicker .day.active:focus,
+				.tribe-events .datepicker .day.active.focused,
+				.tribe-events .datepicker .month.active:hover,
+				.tribe-events .datepicker .month.active:focus,
+				.tribe-events .datepicker .month.active.focused,
+				.tribe-events .datepicker .year.active:hover,
+				.tribe-events .datepicker .year.active:focus,
+				.tribe-events .datepicker .year.active.focused {
+					background: <%= global_elements.accent_color %>;
+				}
+			';
+
+			$template .= '
+				@keyframes tribe-common-c-loader-bounce {
+					0% {}
+					50% { background-color: <%= global_elements.accent_color %>; }
+					100% {}
+				}
+			';
+
+			$template .= '
+				.tribe-common .tribe-common-cta--alt:hover,
+				.tribe-common .tribe-common-cta--alt:focus,
+				.tribe-common .tribe-common-cta--thin-alt:hover,
+				.tribe-common .tribe-common-cta--thin-alt:focus,
+				.tribe-theme-twentyseventeen .tribe-common .tribe-common-cta--alt:hover,
+				.tribe-theme-twentyseventeen .tribe-common .tribe-common-cta--alt:focus,
+				.tribe-theme-twentyseventeen .tribe-common .tribe-common-cta--thin-alt:hover,
+				.tribe-theme-twentyseventeen .tribe-common .tribe-common-cta--thin-alt:focus,
+				.tribe-events .tribe-events-calendar-month__day--current .tribe-events-calendar-month__day-cell:not(.tribe-events-calendar-month__day-cell--selected) .tribe-events-calendar-month__day-date,
+				.tribe-events .tribe-events-calendar-month__day--current .tribe-events-calendar-month__day-cell:not(.tribe-events-calendar-month__day-cell--selected) .tribe-events-calendar-month__day-date-link,
+				.tribe-theme-twentyseventeen .tribe-events .tribe-events-calendar-month__day--current .tribe-events-calendar-month__day-date-link:hover,
+				.tribe-theme-twentyseventeen .tribe-events .tribe-events-calendar-month__day--current .tribe-events-calendar-month__day-date-link:focus {
+					color: <%= global_elements.accent_color %>;
+				}
+			';
+
+			$template .= '
+				.tribe-common .tribe-common-cta--thin-alt,
+				.tribe-common .tribe-common-cta--thin-alt:active,
+				.tribe-common .tribe-common-cta--thin-alt:focus,
+				.tribe-common .tribe-common-cta--thin-alt:hover {
+					border-bottom-color: <%= global_elements.accent_color %>;
+				}
+			';
+
+			$template .= '
+				.tribe-events .tribe-events-c-ical__link {
+					border-color: <%= global_elements.accent_color %>;
+					color: <%= global_elements.accent_color %>;
+				}
+			';
+
+			// Pro styles.
+			$template .= '
+				.tribe-events-pro .tribe-events-pro-week-grid__header-column--current .tribe-events-pro-week-grid__header-column-daynum,
+				.tribe-events-pro .tribe-events-pro-week-grid__header-column--current .tribe-events-pro-week-grid__header-column-daynum-link {
+					color: <%= global_elements.accent_color %>;
+				}
+			';
+
+			$template .= '
+				.tribe-events-pro.tribe-events-view--week .datepicker .day.current::before {
+					background: <%= global_elements.accent_color %>;
+				}
+			';
+
+			$template .= '
+				.tribe-events-pro.tribe-events-view--week .datepicker .active .day,
+				.tribe-events-pro.tribe-events-view--week .datepicker .active .day:hover {
+					background: rgba( ' . $accent_css_rgb . ', 0.07);
+				}
+			';
+
+			$template .= '
+				.tribe-events-pro .tribe-events-pro-week-grid__event-link-inner {
+					background: rgba( ' . $accent_css_rgb . ', 0.1);
+				}
+			';
+			$template .= '
+				.tribe-events-pro .tribe-events-pro-week-grid__event-link:hover .tribe-events-pro-week-grid__event-link-inner,
+				.tribe-events-pro .tribe-events-pro-week-grid__event-link:focus .tribe-events-pro-week-grid__event-link-inner {
+					background: rgba( ' . $accent_css_rgb . ', 0.2);
+				}
+			';
+			$template .= '
+				.tribe-events-pro .tribe-events-pro-week-grid__multiday-event-bar-inner,
+				.tribe-events .tribe-events-calendar-month__multiday-event-bar-inner {
+					background-color: rgba( ' . $accent_css_rgb . ', 0.24);
+				}
+			';
+
+			$template .= '
+				.tribe-events .tribe-events-calendar-month__multiday-event-bar-inner--hover,
+				.tribe-events-pro .tribe-events-pro-week-grid__multiday-event-bar-inner--hover,
+				.tribe-events-pro .tribe-events-pro-week-grid__multiday-event-bar-inner:hover,
+				.tribe-events-pro .tribe-events-pro-week-grid__multiday-event-bar-inner:focus {
+					background-color: rgba( ' . $accent_css_rgb . ', 0.54);
+				}
+			';
+
+			$template .= '
+				.tribe-events-pro .tribe-events-pro-week-day-selector__events-icon {
+					background-color: <%= global_elements.accent_color %>;
+				}
+			';
+
+			$template .= '
+				.tribe-events-pro .tribe-events-pro-map__event-card-wrapper--active .tribe-events-pro-map__event-card-button,
+				.tribe-events-pro .tribe-events-pro-week-day-selector__day--active {
+					border-color: <%= global_elements.accent_color %>;
+				}
+			';
+		}
+
+		if ( tribe_events_views_v2_is_enabled() ) {
+			return $template;
+		}
 
 		if ( $customizer->has_option( $this->ID, 'link_color' ) ) {
 			$template .= '
@@ -39,10 +235,6 @@ final class Tribe__Events__Customizer__Global_Elements extends Tribe__Customizer
 					color: <%= global_elements.link_color %>;
 				}
 			';
-		}
-
-		if ( tribe_events_views_v2_is_enabled() ) {
-			return $template;
 		}
 
 		if ( $customizer->has_option( $this->ID, 'filterbar_color' ) ) {
@@ -135,6 +327,34 @@ final class Tribe__Events__Customizer__Global_Elements extends Tribe__Customizer
 		$customizer = Tribe__Customizer::instance();
 
 		$manager->add_setting(
+			$customizer->get_setting_name( 'accent_color', $section ),
+			array(
+				'default'              => '#334AFF',
+				'type'                 => 'option',
+
+				'sanitize_callback'    => 'sanitize_hex_color',
+				'sanitize_js_callback' => 'maybe_hash_hex_color',
+			)
+		);
+
+		$manager->add_control(
+			new WP_Customize_Color_Control(
+				$manager,
+				$customizer->get_setting_name( 'accent_color', $section ),
+				array(
+					'label'   => esc_html__( 'Accent Color', 'the-events-calendar' ),
+					'section' => $section->id,
+				)
+			)
+		);
+
+		$customizer->add_setting_name( $customizer->get_setting_name( 'accent_color', $section ) );
+
+		if ( tribe_events_views_v2_is_enabled() ) {
+			return;
+		}
+
+		$manager->add_setting(
 			$customizer->get_setting_name( 'link_color', $section ),
 			array(
 				'default'              => $this->get_default( 'link_color' ),
@@ -158,9 +378,6 @@ final class Tribe__Events__Customizer__Global_Elements extends Tribe__Customizer
 
 		$customizer->add_setting_name( $customizer->get_setting_name( 'link_color', $section ) );
 
-		if ( tribe_events_views_v2_is_enabled() ) {
-			return;
-		}
 
 		$manager->add_setting(
 			$customizer->get_setting_name( 'filterbar_color', $section ),
