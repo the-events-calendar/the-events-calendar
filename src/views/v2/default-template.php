49a760ad62dd81e617bd0d734b348be879bffc10
bordoni <bordoni.dev@gmail.com>
2020-01-24 17:44:56 -0500

    Update docblock for file

--- src/views/v2/default-template.php
+++ src/views/v2/default-template.php
@@ -1,5 +1,17 @@
 <?php
-use Tribe\Events\Views\V2\Template_Bootstrap;
+/**
+ * View: Default Template for Events
+ *
+ * Override this template in your own theme by creating a file at:
+ * [your-theme]/tribe/events/v2/default-template.php
+ *
+ * See more documentation about our views templating system.
+ *
+ * @link {INSERT_ARTCILE_LINK_HERE}
+ *
+ * @version 5.0.0
+ */
+
 get_header();
 echo tribe( Template_Bootstrap::class )->get_view_html();
 get_footer();
