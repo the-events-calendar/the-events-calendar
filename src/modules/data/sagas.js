cbcfdc873cbd10e174b2ca7a2ba2034ce5483ced
Paul <kim.paul.ms@gmail.com>
2020-02-06 02:04:51 +0800

    remove price sagas

--- src/modules/data/sagas.js
+++ src/modules/data/sagas.js
@@ -8,9 +8,7 @@ import { store } from '@moderntribe/common/store';
  * Internal dependencies
  */
 import * as datetime from '@moderntribe/events/data/blocks/datetime';
-import * as price from '@moderntribe/events/data/blocks/price';
 
 export default () => [
-	price.sagas,
 	datetime.sagas,
 ].forEach( sagas => store.run( sagas ) );
