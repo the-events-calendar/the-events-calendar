93586c888d20adb55dfd4dd9d8a4d48f3dee9f4d
Dovid Levine <justlevine@gmail.com>
2020-02-14 19:53:30 +0200

    Make list/nav follow same patterns as top-bar/nav

--- src/views/v2/list/nav/next.php
+++ src/views/v2/list/nav/next.php
@@ -14,6 +14,9 @@
  * @version 5.0.1
  *
  */
+
+/* translators: %s: Event (plural or singular). */
+$label = sprintf( __( 'Next %1$s', 'the-events-calendar' ), tribe_get_event_label_plural() );
 ?>
 <li class="tribe-events-c-nav__list-item tribe-events-c-nav__list-item--next">
 	<a
@@ -21,18 +24,13 @@
 		rel="next"
 		class="tribe-events-c-nav__next tribe-common-b2 tribe-common-b1--min-medium"
 		data-js="tribe-events-view-link"
-		aria-label="<?php echo esc_attr( sprintf( __( 'Next %1$s', 'the-events-calendar' ), tribe_get_event_label_plural() ) ); ?>"
-		title="<?php echo esc_attr( sprintf( __( 'Next %1$s', 'the-events-calendar' ), tribe_get_event_label_plural() ) ); ?>"
+		aria-label="<?php echo esc_attr( $label ); ?>"
+		title="<?php echo esc_attr( $label ); ?>"
 	>
 		<span class="tribe-events-c-nav__next-label">
-			<?php
-				$events_label = '<span class="tribe-events-c-nav__next-label-plural tribe-common-a11y-visual-hide">' . tribe_get_event_label_plural() . '</span>';
-				echo wp_kses(
-					/* translators: %s: Event (plural or singular). */
-					sprintf( __( 'Next %1$s', 'the-events-calendar' ), $events_label ),
-					[ 'span' => [ 'class' => [] ] ]
-				);
-			?>
+			<span class="tribe-events-c-nav__next-label-plural tribe-common-a11y-visual-hide">
+				<?php echo esc_attr( $label ); ?>
+			</span>
 		</span>
 	</a>
 </li>
--- src/views/v2/list/nav/prev.php
+++ src/views/v2/list/nav/prev.php
@@ -14,6 +14,9 @@
  * @version 5.0.1
  *
  */
+
+/* translators: %s: Event (plural or singular). */
+$label = sprintf( __( 'Previous %1$s', 'the-events-calendar' ), tribe_get_event_label_plural() );
 ?>
 <li class="tribe-events-c-nav__list-item tribe-events-c-nav__list-item--prev">
 	<a
@@ -21,18 +24,13 @@
 		rel="prev"
 		class="tribe-events-c-nav__prev tribe-common-b2 tribe-common-b1--min-medium"
 		data-js="tribe-events-view-link"
-		aria-label="<?php echo esc_attr( sprintf( __( 'Previous %1$s', 'the-events-calendar' ), tribe_get_event_label_plural() ) ); ?>"
-		title="<?php echo esc_attr( sprintf( __( 'Previous %1$s', 'the-events-calendar' ), tribe_get_event_label_plural() ) ); ?>"
+		aria-label="<?php echo esc_attr( $label ); ?>"
+		title="<?php echo esc_attr( $label ); ?>"
 	>
 		<span class="tribe-events-c-nav__prev-label">
-			<?php
-				$events_label = '<span class="tribe-events-c-nav__prev-label-plural tribe-common-a11y-visual-hide">' . tribe_get_event_label_plural() . '</span>';
-				echo wp_kses(
-					/* translators: %s: Event (plural or singular). */
-					sprintf( __( 'Previous %1$s', 'the-events-calendar' ), $events_label ),
-					[ 'span' => [ 'class' => [] ] ]
-				);
-			?>
+			<span class="tribe-events-c-nav__prev-label-plural tribe-common-a11y-visual-hide">
+				<?php echo esc_attr( $label ); ?>
+			</span>
 		</span>
 	</a>
 </li>
