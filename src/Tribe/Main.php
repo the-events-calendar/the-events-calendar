75cdc03f2f473176f9cc91010aaf3aa9a5fd1978
Gustavo Bordoni <bordoni.dev@gmail.com>
2020-02-24 21:51:31 -0500

    Version bump and base changelog entry

--- src/Tribe/Main.php
+++ src/Tribe/Main.php
@@ -32,7 +32,7 @@ if ( ! class_exists( 'Tribe__Events__Main' ) ) {
 		const VENUE_POST_TYPE     = 'tribe_venue';
 		const ORGANIZER_POST_TYPE = 'tribe_organizer';
 
-		const VERSION             = '5.0.2';
+		const VERSION             = '5.0.2.1';
 
 		/**
 		 * Min Pro Addon
