fae95c5b2e028190351a392a158334e55dc24932
Paul <kim.paul.ms@gmail.com>
2020-03-10 22:11:59 +0800

    use plugin constants in init store

--- src/modules/data/index.js
+++ src/modules/data/index.js
@@ -9,6 +9,8 @@ import { store } from '@moderntribe/common/store';
 import * as blocks from './blocks';
 import initSagas from './sagas';
 
+const { actions, constants } = plugins;
+
 const setInitialState = ( entityRecord ) => {
 
 };
@@ -37,8 +39,8 @@ export const initStore = () => {
 		const { dispatch, injectReducers } = store;
 
 		initSagas();
-		dispatch( plugins.actions.addPlugin( 'events' ) );
-		injectReducers( { events: reducer } );
+		dispatch( actions.addPlugin( constants.EVENTS_PLUGIN ) );
+		injectReducers( { [ constants.EVENTS_PLUGIN ]: reducer } );
 	} );
 };
 
