2f697fbec71046ee5ebfa036a81101ae584e52b3
Paul <kim.paul.ms@gmail.com>
2020-03-10 21:59:05 +0800

    delay init store until editor is ready

--- src/modules/data/index.js
+++ src/modules/data/index.js
@@ -3,17 +3,43 @@
  */
 import reducer from './reducers';
 
-import { plugins } from '@moderntribe/common/data';
+import { globals } from '@moderntribe/common/utils';
+import { editor, plugins } from '@moderntribe/common/data';
 import { store } from '@moderntribe/common/store';
 import * as blocks from './blocks';
 import initSagas from './sagas';
 
-export const initStore = () => {
-	const { dispatch, injectReducers } = store;
+const setInitialState = ( entityRecord ) => {
 
-	initSagas();
-	dispatch( plugins.actions.addPlugin( 'events' ) );
-	injectReducers( { events: reducer } );
+};
+
+export const initStore = () => {
+	const unsubscribe = globals.wpData.subscribe( () => {
+		const coreSelectors = globals.wpData.select( 'core' );
+		const coreEditorSelectors = globals.wpData.select( 'core/editor' );
+
+		/**
+		 * @todo: keep an eye on this, unstable function but is also used in block editor core code.
+		 */
+		if ( ! coreEditorSelectors.__unstableIsEditorReady() ) {
+			return;
+		}
+
+		unsubscribe();
+
+		if ( ! coreEditorSelectors.isCleanNewPost() ) {
+			const postId = coreEditorSelectors.getCurrentPostId();
+			const entityRecord = coreSelectors.getEntityRecord( 'postType', editor.EVENT, postId );
+
+			setInitialState( entityRecord );
+		}
+
+		const { dispatch, injectReducers } = store;
+
+		initSagas();
+		dispatch( plugins.actions.addPlugin( 'events' ) );
+		injectReducers( { events: reducer } );
+	} );
 };
 
 export const getStore = () => store;
