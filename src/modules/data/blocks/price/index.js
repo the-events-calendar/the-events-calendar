7a60b85a00f4713d59b2c33e42d0b539caebc685
Paul <kim.paul.ms@gmail.com>
2020-03-11 19:37:38 +0800

    add price utils

--- src/modules/data/blocks/price/index.js
+++ src/modules/data/blocks/price/index.js
@@ -7,6 +7,7 @@ import * as selectors from './selectors';
 import * as actions from './actions';
 import * as types from './types';
 import sagas from './sagas';
+import * as utils from './utils';
 
 export default reducer;
-export { selectors, actions, types, sagas };
+export { selectors, actions, types, sagas, utils };
