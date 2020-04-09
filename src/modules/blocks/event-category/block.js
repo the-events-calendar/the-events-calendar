572fbb94116d7f294b14fa9e70c2eaff57a1ad90
Paul <kim.paul.ms@gmail.com>
2020-04-09 19:58:17 +0800

    code review suggestions

--- src/modules/blocks/event-category/block.js
+++ src/modules/blocks/event-category/block.js
@@ -14,18 +14,16 @@ import { TermsList } from '@moderntribe/events/elements';
  * Module Code
  */
 
-const EventCategory = ( props ) => {
-	return (
-		<section className="tribe-editor__block">
-			<div className="tribe-editor__event-category">
-				<TermsList
-					slug="tribe_events_cat"
-					label={ __( 'Event Category', 'the-events-calendar' ) }
-					renderEmpty={ __( 'Add Event Categories in document settings', 'the-events-calendar' ) }
-				/>
-			</div>
-		</section>
-	);
-};
+const EventCategory = () => (
+	<section className="tribe-editor__block">
+		<div className="tribe-editor__event-category">
+			<TermsList
+				slug="tribe_events_cat"
+				label={ __( 'Event Category', 'the-events-calendar' ) }
+				renderEmpty={ __( 'Add Event Categories in document settings', 'the-events-calendar' ) }
+			/>
+		</div>
+	</section>
+);
 
 export default EventCategory;
--- src/modules/blocks/event-tags/block.js
+++ src/modules/blocks/event-tags/block.js
@@ -14,18 +14,16 @@ import { TermsList } from '@moderntribe/events/elements';
  * Module Code
  */
 
-const EventTags = ( props ) => {
-	return (
-		<section className="tribe-editor__block">
-			<div className="tribe-editor__event-tags">
-				<TermsList
-					slug="post_tag"
-					label={ __( 'Tags', 'the-events-calendar' ) }
-					renderEmpty={ __( 'Add tags in document settings', 'the-events-calendar' ) }
-				/>
-			</div>
-		</section>
-	);
-};
+const EventTags = () => (
+	<section className="tribe-editor__block">
+		<div className="tribe-editor__event-tags">
+			<TermsList
+				slug="post_tag"
+				label={ __( 'Tags', 'the-events-calendar' ) }
+				renderEmpty={ __( 'Add tags in document settings', 'the-events-calendar' ) }
+			/>
+		</div>
+	</section>
+);
 
 export default EventTags;
