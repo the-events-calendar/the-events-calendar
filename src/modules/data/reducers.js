8ec3eeae1a6f190487c407c262c99f440d2c177a
Paul <kim.paul.ms@gmail.com>
2020-04-17 22:45:12 +0800

    remove ui data from block editor

--- src/modules/data/reducers.js
+++ src/modules/data/reducers.js
@@ -7,13 +7,11 @@ import { combineReducers } from 'redux';
  * Internal dependencies
  */
 import blocks from './blocks';
-import ui from './ui';
 import search from './search';
 import details from './details';
 
 export default combineReducers( {
 	blocks,
-	ui,
 	search,
 	details,
 } );
