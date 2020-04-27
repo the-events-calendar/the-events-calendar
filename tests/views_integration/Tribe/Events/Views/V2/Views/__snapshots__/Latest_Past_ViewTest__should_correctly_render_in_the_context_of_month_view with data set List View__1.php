d58922883f92f48d31450fdf0873975d4dc094b8
Luca Tumedei <luca@theaveragedev.com>
2020-04-27 16:08:07 +0200

    test(views_integration) add coverage for Latest Past Events View

---
+++ tests/views_integration/Tribe/Events/Views/V2/Views/__snapshots__/Latest_Past_ViewTest__should_correctly_render_in_the_context_of_month_view with data set List View__1.php
@@ -0,0 +1,389 @@
+<?php return '<div
+	 class="tribe-common tribe-events tribe-events-view tribe-events-view--list" 	data-js="tribe-events-view"
+	data-view-rest-nonce="2ab7cc6b39"
+	data-view-rest-url="http://test.tri.be/index.php?rest_route=/tribe/views/v2/html"
+	data-view-manage-url="1"
+				data-view-breakpoint-pointer="random-id"
+	>
+	<div class="tribe-common-l-container tribe-events-l-container">
+		<div
+	class="tribe-events-view-loader tribe-common-a11y-hidden"
+	role="alert"
+	aria-live="assertive"
+>
+	<div class="tribe-events-view-loader__dots tribe-common-c-loader">
+		<div class="tribe-common-c-loader__dot tribe-common-c-loader__dot--first"></div>
+		<div class="tribe-common-c-loader__dot tribe-common-c-loader__dot--second"></div>
+		<div class="tribe-common-c-loader__dot tribe-common-c-loader__dot--third"></div>
+	</div>
+</div>
+
+		
+		<script
+	data-js="tribe-events-view-data"
+	type="application/json"
+>
+	{"slug":"list","prev_url":"http:\\/\\/test.tri.be\\/events\\/list\\/?page=1&tribe-bar-date=2020-03-01&tribe_event_display=past","next_url":"http:\\/\\/test.tri.be\\/events\\/list\\/","view_class":"Tribe\\\\Events\\\\Views\\\\V2\\\\Views\\\\List_View","view_slug":"list","view_label":"List","view":{},"title":"Search Results for &#8220;&#8221; &#8211; The Events Calendar Tests","events":[],"url":"http:\\/\\/test.tri.be\\/events\\/list\\/?tribe-bar-date=2020-03-01","url_event_date":"2020-03-01","bar":{"keyword":"","date":"2020-03-01"},"today":"2020-03-01","now":"2020-03-01 11:00:00","request_date":{"date":"2020-03-01 00:00:00.000000","timezone_type":3,"timezone":"UTC"},"rest_url":"http:\\/\\/test.tri.be\\/index.php?rest_route=\\/tribe\\/views\\/v2\\/html","rest_nonce":"2ab7cc6b39","should_manage_url":true,"today_url":"http:\\/\\/test.tri.be\\/events\\/list\\/","prev_label":"2020-03-01","next_label":"","date_formats":{"compact":"n\\/j\\/Y","month_and_year_compact":"n\\/j\\/Y","month_and_year":"F Y","time_range_separator":" - ","date_time_separator":" @ "},"messages":{"notice":["There were no results found."]},"start_of_week":"1","breadcrumbs":[],"before_events":"<div id=\\"tribe-events\\" class=\\"tribe-no-js\\" data-live_ajax=\\"1\\" data-datepicker_format=\\"1\\" data-category=\\"\\" data-featured=\\"\\">","after_events":"<\\/div><!-- #tribe-events -->\\n<!--\\nThis calendar is powered by The Events Calendar.\\nhttp:\\/\\/m.tri.be\\/18wn\\n-->\\n","display_events_bar":true,"disable_event_search":false,"live_refresh":true,"ical":{"display_link":true,"link":{"url":"http:\\/\\/test.tri.be\\/events\\/?ical=1","text":"Export Events","title":"Use this to share calendar data with Google Calendar, Apple iCal and other compatible apps"}},"container_classes":["tribe-common","tribe-events","tribe-events-view","tribe-events-view--list"],"container_data":[],"is_past":false,"breakpoints":{"xsmall":500,"medium":768,"full":960},"breakpoint_pointer":"random-id","is_initial_load":true,"public_views":{"list":{"view_class":"Tribe\\\\Events\\\\Views\\\\V2\\\\Views\\\\List_View","view_url":"http:\\/\\/test.tri.be\\/events\\/list\\/?tribe-bar-date=2020-03-01","view_label":"List"},"month":{"view_class":"Tribe\\\\Events\\\\Views\\\\V2\\\\Views\\\\Month_View","view_url":"http:\\/\\/test.tri.be\\/events\\/month\\/2020-03\\/","view_label":"Month"},"day":{"view_class":"Tribe\\\\Events\\\\Views\\\\V2\\\\Views\\\\Day_View","view_url":"http:\\/\\/test.tri.be\\/events\\/2020-03-01\\/","view_label":"Day"},"latest-past":{"view_class":"Tribe\\\\Events\\\\Views\\\\V2\\\\Views\\\\Latest_Past_View","view_url":"http:\\/\\/test.tri.be\\/?post_type=tribe_events&eventDisplay=latest-past&tribe-bar-date=2020-03-01","view_label":"Latest-past"}},"show_latest_past":true,"show_now":true,"now_label":"Now onwards","now_label_mobile":"Now onwards","show_end":false,"selected_start_datetime":"2020-03-01","selected_start_date_mobile":"3\\/1\\/2020","selected_start_date_label":"March 1","selected_end_datetime":"2020-03-01","selected_end_date_mobile":"3\\/1\\/2020","selected_end_date_label":"Now","datepicker_date":"3\\/1\\/2020","json_ld_data":"","_context":{"slug":"list"},"text":"Loading..."}</script>
+
+		<div class="tribe-events-before-html">
+	<div id="tribe-events" class="tribe-no-js" data-live_ajax="1" data-datepicker_format="1" data-category="" data-featured=""></div>
+
+
+		<header  class="tribe-events-header tribe-events-header--has-event-search" >
+			<div  class="tribe-events-header__messages tribe-events-c-messages tribe-common-b2" >
+			<div class="tribe-events-c-messages__message tribe-events-c-messages__message--notice" role="alert">
+			<ul class="tribe-events-c-messages__message-list">
+									<li class="tribe-events-c-messages__message-list-item">
+						There were no results found.					</li>
+							</ul>
+		</div>
+	</div>
+
+			
+			<div
+	 class="tribe-events-header__events-bar tribe-events-c-events-bar tribe-events-c-events-bar--border" 	data-js="tribe-events-events-bar"
+>
+
+	<h2 class="tribe-common-a11y-visual-hide">
+		Events Search and Views Navigation	</h2>
+
+			<button
+	class="tribe-events-c-events-bar__search-button"
+	aria-controls="tribe-events-search-filter-container"
+	aria-expanded="false"
+	aria-selected="false"
+	data-js="tribe-events-search-button"
+>
+		<span class="tribe-events-c-events-bar__search-button-text tribe-common-a11y-visual-hide">
+		Search	</span>
+</button>
+
+		<div
+			class="tribe-events-c-events-bar__search-filters-container"
+			id="tribe-events-search-filters-container"
+			data-js="tribe-events-search-filters-container"
+		>
+			
+			<div
+	class="tribe-events-c-events-bar__search"
+	id="tribe-events-events-bar-search"
+	data-js="tribe-events-events-bar-tabpanel tribe-events-events-bar-search"
+>
+	<form
+		class="tribe-events-c-search tribe-events-c-events-bar__search-form"
+		method="get"
+		data-js="tribe-events-view-form"
+		role="search"
+	>
+		<input type="hidden" id="tribe-events-views[_wpnonce]" name="tribe-events-views[_wpnonce]" value="2ab7cc6b39" /><input type="hidden" name="_wp_http_referer" value="/events/list/?tribe-bar-date=2020-03-01" />		<input type="hidden" name="tribe-events-views[url]" value="http://test.tri.be/events/list/?tribe-bar-date=2020-03-01" />
+
+		<div class="tribe-events-c-search__input-group">
+			<div
+	class="tribe-common-form-control-text tribe-events-c-search__input-control tribe-events-c-search__input-control--keyword"
+	data-js="tribe-events-events-bar-input-control"
+>
+	<label class="tribe-common-form-control-text__label" for="tribe-events-events-bar-keyword">
+		Enter Keyword. Search for Events by Keyword.	</label>
+	<input
+		class="tribe-common-form-control-text__input tribe-events-c-search__input tribe-events-c-search__input--icon"
+		data-js="tribe-events-events-bar-input-control-input"
+		type="text"
+		id="tribe-events-events-bar-keyword"
+		name="tribe-events-views[tribe-bar-search]"
+		value=""
+		placeholder="Search for events"
+		aria-label="Enter Keyword. Search for events by Keyword."
+	/>
+</div>
+		</div>
+
+		<button
+	class="tribe-common-c-btn tribe-events-c-search__button"
+	type="submit"
+	name="submit-bar"
+>
+	Find Events</button>
+	</form>
+</div>
+
+					</div>
+	
+	<div class="tribe-events-c-events-bar__views">
+	<h3 class="tribe-common-a11y-visual-hide">
+		Event Views Navigation	</h3>
+	<div  class="tribe-events-c-view-selector tribe-events-c-view-selector--labels"  data-js="tribe-events-view-selector">
+		<button
+			class="tribe-events-c-view-selector__button"
+			data-js="tribe-events-view-selector-button"
+		>
+			<span class="tribe-events-c-view-selector__button-icon tribe-common-svgicon tribe-common-svgicon--list"></span>
+			<span class="tribe-events-c-view-selector__button-text tribe-common-a11y-visual-hide">
+				List			</span>
+		</button>
+		<div
+	class="tribe-events-c-view-selector__content"
+	id="tribe-events-view-selector-content"
+	data-js="tribe-events-view-selector-list-container"
+>
+	<ul class="tribe-events-c-view-selector__list">
+					<li class="tribe-events-c-view-selector__list-item tribe-events-c-view-selector__list-item--list tribe-events-c-view-selector__list-item--active">
+	<a
+		href="http://test.tri.be/events/list/?tribe-bar-date=2020-03-01"
+		class="tribe-events-c-view-selector__list-item-link"
+		data-js="tribe-events-view-link"
+	>
+		<span class="tribe-events-c-view-selector__list-item-icon tribe-common-svgicon tribe-common-svgicon--list"></span>
+		<span class="tribe-events-c-view-selector__list-item-text">
+			List		</span>
+	</a>
+</li>
+					<li class="tribe-events-c-view-selector__list-item tribe-events-c-view-selector__list-item--month">
+	<a
+		href="http://test.tri.be/events/month/2020-03/"
+		class="tribe-events-c-view-selector__list-item-link"
+		data-js="tribe-events-view-link"
+	>
+		<span class="tribe-events-c-view-selector__list-item-icon tribe-common-svgicon tribe-common-svgicon--month"></span>
+		<span class="tribe-events-c-view-selector__list-item-text">
+			Month		</span>
+	</a>
+</li>
+					<li class="tribe-events-c-view-selector__list-item tribe-events-c-view-selector__list-item--day">
+	<a
+		href="http://test.tri.be/events/2020-03-01/"
+		class="tribe-events-c-view-selector__list-item-link"
+		data-js="tribe-events-view-link"
+	>
+		<span class="tribe-events-c-view-selector__list-item-icon tribe-common-svgicon tribe-common-svgicon--day"></span>
+		<span class="tribe-events-c-view-selector__list-item-text">
+			Day		</span>
+	</a>
+</li>
+					<li class="tribe-events-c-view-selector__list-item tribe-events-c-view-selector__list-item--latest-past">
+	<a
+		href="http://test.tri.be/?post_type=tribe_events&#038;eventDisplay=latest-past&#038;tribe-bar-date=2020-03-01"
+		class="tribe-events-c-view-selector__list-item-link"
+		data-js="tribe-events-view-link"
+	>
+		<span class="tribe-events-c-view-selector__list-item-icon tribe-common-svgicon tribe-common-svgicon--latest-past"></span>
+		<span class="tribe-events-c-view-selector__list-item-text">
+			Latest-past		</span>
+	</a>
+</li>
+			</ul>
+</div>
+	</div>
+</div>
+
+</div>
+
+			<div class="tribe-events-c-top-bar tribe-events-header__top-bar">
+
+	<nav class="tribe-events-c-top-bar__nav tribe-common-a11y-hidden">
+	<ul class="tribe-events-c-top-bar__nav-list">
+		<li class="tribe-events-c-top-bar__nav-list-item">
+	<a
+		href="http://test.tri.be/events/list/?page=1&#038;tribe-bar-date=2020-03-01&#038;tribe_event_display=past"
+		class="tribe-common-c-btn-icon tribe-common-c-btn-icon--caret-left tribe-events-c-top-bar__nav-link tribe-events-c-top-bar__nav-link--prev"
+		aria-label="Previous Events"
+		title="Previous Events"
+		data-js="tribe-events-view-link"
+	>
+	</a>
+</li>
+
+			</ul>
+</nav>
+
+	<a
+	href="http://test.tri.be/events/list/"
+	class="tribe-common-c-btn-border tribe-events-c-top-bar__today-button tribe-common-a11y-hidden"
+	data-js="tribe-events-view-link"
+	aria-label="Click to select today&#039;s date"
+	title="Click to select today&#039;s date"
+>
+	Today</a>
+
+	<div class="tribe-events-c-top-bar__datepicker">
+	<button
+		class="tribe-common-h3 tribe-common-h--alt tribe-events-c-top-bar__datepicker-button"
+		data-js="tribe-events-top-bar-datepicker-button"
+		type="button"
+		aria-label="Click to toggle datepicker"
+		title="Click to toggle datepicker"
+	>
+		<time
+			datetime="2020-03-01"
+			class="tribe-events-c-top-bar__datepicker-time"
+		>
+							<span class="tribe-events-c-top-bar__datepicker-mobile">
+					Now onwards				</span>
+				<span class="tribe-events-c-top-bar__datepicker-desktop tribe-common-a11y-hidden">
+					Now onwards				</span>
+					</time>
+			</button>
+	<label
+		class="tribe-events-c-top-bar__datepicker-label tribe-common-a11y-visual-hide"
+		for="tribe-events-top-bar-date"
+	>
+		Select date.	</label>
+	<input
+		type="text"
+		class="tribe-events-c-top-bar__datepicker-input tribe-common-a11y-visual-hide"
+		data-js="tribe-events-top-bar-date"
+		id="tribe-events-top-bar-date"
+		name="tribe-events-views[tribe-bar-date]"
+		value="3/1/2020"
+		tabindex="-1"
+		autocomplete="off"
+		readonly="readonly"
+	/>
+	<div class="tribe-events-c-top-bar__datepicker-container" data-js="tribe-events-top-bar-datepicker-container"></div>
+</div>
+
+	<div class="tribe-events-c-top-bar__actions tribe-common-a11y-hidden">
+	</div>
+
+</div>
+		</header>
+
+		
+		<div class="tribe-events-calendar-list">
+
+			
+		</div>
+
+		
+		<div class="tribe-events-calendar-latest-past">
+
+	<h2 class="tribe-events-calendar-latest-past__heading tribe-common-h5 tribe-common-h3--min-medium">
+	Latest Past Events</h2>
+
+			
+		<div  class="tribe-common-g-row tribe-events-calendar-latest-past__event-row" >
+
+	<div class="tribe-events-calendar-latest-past__event-date-tag tribe-common-g-col">
+	<time class="tribe-events-calendar-latest-past__event-date-tag-datetime" datetime="2020-02-20">
+		<span class="tribe-events-calendar-latest-past__event-date-tag-month">
+			Feb		</span>
+		<span class="tribe-events-calendar-latest-past__event-date-tag-daynum tribe-common-h5 tribe-common-h4--min-medium">
+			20		</span>
+		<span class="tribe-events-calendar-latest-past__event-date-tag-year">
+			2020		</span>
+	</time>
+</div>
+
+	<div class="tribe-events-calendar-latest-past__event-wrapper tribe-common-g-col">
+		<article  class="tribe-events-calendar-latest-past__event tribe-common-g-row tribe-common-g-row--gutters post-89 tribe_events type-tribe_events status-publish hentry" >
+			
+			<div class="tribe-events-calendar-latest-past__event-details tribe-common-g-col">
+
+				<header class="tribe-events-calendar-latest-past__event-header">
+					<div class="tribe-events-calendar-latest-past__event-datetime-wrapper tribe-common-b2">
+		<time class="tribe-events-calendar-latest-past__event-datetime" datetime="2020-02-20">
+		<span class="tribe-event-date-start">February 20, 2020 @ 8:00 am</span> - <span class="tribe-event-time">5:00 pm</span>	</time>
+	</div>
+					<h3 class="tribe-events-calendar-latest-past__event-title tribe-common-h6 tribe-common-h4--min-medium">
+	<a
+		href="http://test.tri.be/?tribe_events=single-event-1"
+		title="Single Event 1"
+		rel="bookmark"
+		class="tribe-events-calendar-latest-past__event-title-link tribe-common-anchor-thin"
+	>
+		Single Event 1	</a>
+</h3>
+									</header>
+
+				<div class="tribe-events-calendar-latest-past__event-description tribe-common-b2 tribe-common-a11y-hidden">
+	<p>Snapshot event 89</p>
+</div>
+				
+			</div>
+		</article>
+	</div>
+
+</div>
+
+			
+		<div  class="tribe-common-g-row tribe-events-calendar-latest-past__event-row" >
+
+	<div class="tribe-events-calendar-latest-past__event-date-tag tribe-common-g-col">
+	<time class="tribe-events-calendar-latest-past__event-date-tag-datetime" datetime="2020-02-15">
+		<span class="tribe-events-calendar-latest-past__event-date-tag-month">
+			Feb		</span>
+		<span class="tribe-events-calendar-latest-past__event-date-tag-daynum tribe-common-h5 tribe-common-h4--min-medium">
+			15		</span>
+		<span class="tribe-events-calendar-latest-past__event-date-tag-year">
+			2020		</span>
+	</time>
+</div>
+
+	<div class="tribe-events-calendar-latest-past__event-wrapper tribe-common-g-col">
+		<article  class="tribe-events-calendar-latest-past__event tribe-common-g-row tribe-common-g-row--gutters post-23 tribe_events type-tribe_events status-publish hentry" >
+			
+			<div class="tribe-events-calendar-latest-past__event-details tribe-common-g-col">
+
+				<header class="tribe-events-calendar-latest-past__event-header">
+					<div class="tribe-events-calendar-latest-past__event-datetime-wrapper tribe-common-b2">
+		<time class="tribe-events-calendar-latest-past__event-datetime" datetime="2020-02-15">
+		<span class="tribe-event-date-start">February 15, 2020 @ 8:00 am</span> - <span class="tribe-event-time">5:00 pm</span>	</time>
+	</div>
+					<h3 class="tribe-events-calendar-latest-past__event-title tribe-common-h6 tribe-common-h4--min-medium">
+	<a
+		href="http://test.tri.be/?tribe_events=single-event-1"
+		title="Single Event 1"
+		rel="bookmark"
+		class="tribe-events-calendar-latest-past__event-title-link tribe-common-anchor-thin"
+	>
+		Single Event 1	</a>
+</h3>
+									</header>
+
+				<div class="tribe-events-calendar-latest-past__event-description tribe-common-b2 tribe-common-a11y-hidden">
+	<p>Snapshot event 23</p>
+</div>
+				
+			</div>
+		</article>
+	</div>
+
+</div>
+
+	
+</div>
+
+		
+	</div>
+</div>
+
+<script class="tribe-events-breakpoints">
+	(function(){
+		if ( \'undefined\' === typeof window.tribe ) {
+			return;
+		}
+
+		if ( \'undefined\' === typeof window.tribe.events ) {
+			return;
+		}
+
+		if ( \'undefined\' === typeof window.tribe.events.views ) {
+			return;
+		}
+
+		if ( \'undefined\' === typeof window.tribe.events.views.breakpoints ) {
+			return;
+		}
+
+		if ( \'function\' !== typeof( window.tribe.events.views.breakpoints.setup ) ) {
+			return;
+		}
+
+		var container = document.querySelectorAll( \'[data-view-breakpoint-pointer="random-id"]\' );
+		if ( ! container ) {
+			return;
+		}
+
+		window.tribe.events.views.breakpoints.setup( container );
+	})();
+</script>
+';
