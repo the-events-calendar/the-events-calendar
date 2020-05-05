5dc1d11261daa42573d63a961b21a0916a96d45e
Stephen Page <stephenjpage@gmail.com>
2020-05-05 18:31:08 -0400

    Try renewing snapshots - again.

--- tests/views_integration/Tribe/Events/Views/V2/Partials/Month_View/Calendar_Body/Day/Calendar_Events/Calendar_Event/__snapshots__/DateTest__test_render_with_featured_event__1.php
+++ tests/views_integration/Tribe/Events/Views/V2/Partials/Month_View/Calendar_Body/Day/Calendar_Events/Calendar_Event/__snapshots__/DateTest__test_render_with_featured_event__1.php
@@ -1,10 +1,10 @@
 <?php return '<div class="tribe-events-calendar-month__calendar-event-datetime">
-		<em
-		class="tribe-events-calendar-month__calendar-event-datetime-featured-icon tribe-common-svgicon tribe-common-svgicon--featured"
-		aria-label="Featured"
-		title="Featured"
-	>
-	</em>
+	<em
+	class="tribe-events-calendar-month__calendar-event-datetime-featured-icon tribe-common-svgicon tribe-common-svgicon--featured"
+	aria-label="Featured"
+	title="Featured"
+>
+</em>
 	<time datetime="13:01">
 		1:01 pm	</time>
 			<span class="tribe-events-calendar-month__calendar-event-datetime-separator"> - </span>
