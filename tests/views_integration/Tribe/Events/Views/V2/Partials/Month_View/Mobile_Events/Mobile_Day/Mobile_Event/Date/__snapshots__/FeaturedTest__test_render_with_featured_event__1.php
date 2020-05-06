c5a2cfedd08da68c1f1ac517ea56ab36cedc57d4
Paul <kim.paul.ms@gmail.com>
2020-05-06 19:41:16 +0800

    add tests and snapshots for month view featured

---
+++ tests/views_integration/Tribe/Events/Views/V2/Partials/Month_View/Mobile_Events/Mobile_Day/Mobile_Event/Date/__snapshots__/FeaturedTest__test_render_with_featured_event__1.php
@@ -0,0 +1,9 @@
+<?php return '<em
+	class="tribe-events-calendar-month-mobile-events__mobile-event-datetime-featured-icon tribe-common-svgicon tribe-common-svgicon--featured"
+	aria-label="Featured"
+	title="Featured"
+>
+</em>
+<span class="tribe-events-calendar-month-mobile-events__mobile-event-datetime-featured-text">
+	Featured</span>
+';
