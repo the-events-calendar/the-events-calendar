e019921393f08a239cd57ccfe895782e35a4fa73
Gustavo Bordoni <bordoni.dev@gmail.com>
2020-05-05 21:25:00 -0400

    prevent func_get_args incorrect usage for PHP 7.0 and forward + changelog [TEC-3104]

--- src/Tribe/Templates.php
+++ src/Tribe/Templates.php
@@ -543,8 +543,8 @@ if ( ! class_exists( 'Tribe__Events__Templates' ) ) {
 		 **/
 		public static function getTemplateHierarchy( $template, $args = array() ) {
 			if ( ! is_array( $args ) ) {
+				$passed        = [ $template, $args ];
 				$args          = array();
-				$passed        = func_get_args();
 				$backwards_map = array( 'namespace', 'plugin_path' );
 				$count = count( $passed );
 
