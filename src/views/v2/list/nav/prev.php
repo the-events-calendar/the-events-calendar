3367086641a8de98456c919dd9d28b3fb810d738
Gustavo Bordoni <bordoni.dev@gmail.com>
2020-05-07 02:01:34 -0400

    Update prev.php

--- src/views/v2/list/nav/prev.php
+++ src/views/v2/list/nav/prev.php
@@ -31,14 +31,7 @@ $events_mobile_friendly_label = sprintf( __( 'Previous %1$s', 'the-events-calend
 		title="<?php echo esc_attr( $label ); ?>"
 	>
 		<span class="tribe-events-c-nav__prev-label">
-			<?php
-				$events_label = '<span class="tribe-events-c-nav__prev-label-plural tribe-common-a11y-visual-hide">' . tribe_get_event_label_plural() . '</span>';
-				echo wp_kses(
-					/* translators: %s: Event (plural or singular). */
-					sprintf( __( 'Previous %1$s', 'the-events-calendar' ), $events_label ),
-					[ 'span' => [ 'class' => [] ] ]
-				);
-			?>
+			<?php echo wp_kses( $events_mobile_friendly_label, [ 'span' => [ 'class' => [] ] ] ); ?>
 		</span>
 	</a>
 </li>
