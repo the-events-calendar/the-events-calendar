2363e44fe683b32e499d6b7af0de558129fca152
Luca Tumedei <luca@theaveragedev.com>
2020-06-15 10:28:10 +0200

    fix(src) correctly check condition

--- src/Tribe/Aggregator/Record/CSV.php
+++ src/Tribe/Aggregator/Record/CSV.php
@@ -348,7 +348,7 @@ class Tribe__Events__Aggregator__Record__CSV extends Tribe__Events__Aggregator__
 
 		$lock_key = 'tribe_ea_csv_import_' . $this->id;
 
-		if ( $this->acquire_db_lock( $lock_key ) ) {
+		if ( ! $this->acquire_db_lock( $lock_key ) ) {
 			return $this->meta['activity'];
 		}
 
