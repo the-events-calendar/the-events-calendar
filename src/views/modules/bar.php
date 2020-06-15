f6200e9cf393affa2ed9679098b9a4b8ab8c0db1
Stephen Page <stephenjpage@gmail.com>
2020-06-15 18:24:23 -0400

    Fix a few typos

--- src/views/modules/bar.php
+++ src/views/modules/bar.php
@@ -4,7 +4,7 @@
  * Renders our events navigation bar used across our views
  *
  * $filters and $views variables are loaded in and coming from
- * the show funcion in: lib/Bar.php
+ * the show function in: lib/Bar.php
  *
  * Override this template in your own theme by creating a file at:
  *
