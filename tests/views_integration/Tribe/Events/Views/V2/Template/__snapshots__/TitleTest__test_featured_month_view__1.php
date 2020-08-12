7313d1bd3c4953dac4d58db8d4b3037ce0e03ed4
Stephen Page <stephenjpage@gmail.com>
2020-08-12 11:40:30 -0400

    Update snapshot

--- tests/views_integration/Tribe/Events/Views/V2/Template/__snapshots__/TitleTest__test_featured_month_view__1.php
+++ tests/views_integration/Tribe/Events/Views/V2/Template/__snapshots__/TitleTest__test_featured_month_view__1.php
@@ -1,1 +1,1 @@
-<?php return 'Upcoming Featured Events';
+<?php return 'Featured Events for August 12, 2020 - August 12, 2020';
--- tests/views_integration/Tribe/Events/Views/V2/Template/__snapshots__/TitleTest__test_month_view__1.php
+++ tests/views_integration/Tribe/Events/Views/V2/Template/__snapshots__/TitleTest__test_month_view__1.php
@@ -1,1 +1,1 @@
-<?php return 'Upcoming Events';
+<?php return 'Events for August 12, 2020 - August 12, 2020';
