d754b14862cedc3c77b38e656f72c03f3b7c94d4
Stephen Page <stephenjpage@gmail.com>
2020-08-14 16:29:42 -0400

    botfix whitespace

--- src/Tribe/Importer/File_Importer_Venues.php
+++ src/Tribe/Importer/File_Importer_Venues.php
@@ -46,8 +46,8 @@ class Tribe__Events__Importer__File_Importer_Venues extends Tribe__Events__Impor
 	 * @return array $venue The array of venue data for creation/update.
 	 */
 	private function build_venue_array( $venue_id, array $record ) {
-		$venue          = [];
-		$columns        = [
+		$venue   = [];
+		$columns = [
 			'Venue'       => 'venue_name',
 			'Address'     => 'venue_address',
 			'City'        => 'venue_city',
@@ -61,29 +61,54 @@ class Tribe__Events__Importer__File_Importer_Venues extends Tribe__Events__Impor
 		];
 
 		foreach ( $columns as $name => $key ) {
-			// Don't overwrite unset columns.
+			// Don't overwrite unmapped columns.
 			if ( ! $this->has_value_by_key( $record, $key ) ) {
 				continue;
 			}
+
+			$value = '';
 
 			// Address is a concatenation of two possible values.
 			if ( 'venue_address' === $key ) {
 				$address_1 = trim( $this->get_value_by_key( $record, 'venue_address' ) );
 				$address_2 = trim( $this->get_value_by_key( $record, 'venue_address2' ) );
-				$venue[ $name ] = ( empty( $address_2 ) ) ? $address_1 : $address_1 . ' ' . $address_2;
+				$value     = ( empty( $address_2 ) ) ? $address_1 : $address_1 . ' ' . $address_2;
 
 				continue;
+			} else {
+				$value = $this->get_value_by_key( $record, $key );
 			}
 
-			$venue[ $name ] = $this->get_value_by_key( $record, $key );
+			/**
+			 * Allows filtering of main values before setting.
+			 * Return boolean false to prevent importing that value.
+			 * @since TBD
+			 *
+			 * @param string $key    The key for the value we'll be importing.
+			 * @param string $value   The value we'll be importing.
+			 * @param array  $venue  The array of venue data we're modifying.
+			 * @param array  $record The event record from the import.
+			 */
+			$value = apply_filters(
+				"tribe_events_importer_venue_{$key}_value",
+				$key,
+				$value,
+				$venue,
+				$record,
+				$this
+			);
+
+			if ( false !== $value ) {
+				$venue[ $name ] = $value;
+			}
 		}
 
 		// Handle the manual stuff.
 		$venue['FeaturedImage'] = $this->get_featured_image( $venue_id, $record );
 
-		$show_map_setting       = tribe( 'events-aggregator.settings' )->default_map( 'csv' );
-		$venue['ShowMap']       = $venue_id ? get_post_meta( $venue_id, '_VenueShowMap', true ) : $show_map_setting;
-		$venue['ShowMapLink']   = $venue_id ? get_post_meta( $venue_id, '_VenueShowMapLink', true ) : $show_map_setting;
+		$show_map_setting     = tribe( 'events-aggregator.settings' )->default_map( 'csv' );
+		$venue['ShowMap']     = $venue_id ? get_post_meta( $venue_id, '_VenueShowMap', true ) : $show_map_setting;
+		$venue['ShowMapLink'] = $venue_id ? get_post_meta( $venue_id, '_VenueShowMapLink', true ) : $show_map_setting;
 
 		/**
 		 * Allows triggering using the default values set in the admin for imported venues.
@@ -127,6 +152,8 @@ class Tribe__Events__Importer__File_Importer_Venues extends Tribe__Events__Impor
 
 	/**
 	 * Set default venue values.
+	 * Note this will only set a value if it has been mapped, and it is empty.
+	 * If you are using the importer to erase values, you should not be triggering this.
 	 *
 	 * @since TBD
 	 *
@@ -135,15 +162,15 @@ class Tribe__Events__Importer__File_Importer_Venues extends Tribe__Events__Impor
 	 * @return array        The modified venue data.
 	 */
 	public function set_defaults( $venue, $record ) {
-		$columns        = [
-			'Address'     => 'address',
-			'City'        => 'city',
-			'Country'     => 'country',
-			'Phone'       => 'phone',
-			'Province'    => 'state',
-			'State'       => 'state',
-			'URL'         => 'url',
-			'Zip'         => 'zip',
+		$columns = [
+			'Address'  => 'address',
+			'City'     => 'city',
+			'Country'  => 'country',
+			'Phone'    => 'phone',
+			'Province' => 'state',
+			'State'    => 'state',
+			'URL'      => 'url',
+			'Zip'      => 'zip',
 		];
 
 		foreach ( $columns as $name => $key ) {
