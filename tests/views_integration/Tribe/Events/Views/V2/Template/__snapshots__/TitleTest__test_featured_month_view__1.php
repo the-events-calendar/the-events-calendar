eeea98cf46781fe9266e3cfb037268739f03c5a1
Luca Tumedei <luca@theaveragedev.com>
2020-08-14 14:23:07 +0200

    test(views_inteagration) debug and fix Title tests

--- tests/views_integration/Tribe/Events/Views/V2/Template/__snapshots__/TitleTest__test_featured_month_view__1.php
+++ tests/views_integration/Tribe/Events/Views/V2/Template/__snapshots__/TitleTest__test_featured_month_view__1.php
@@ -1,1 +1,1 @@
-<?php return 'Featured Events for August 12, 2020 - August 12, 2020';
+<?php return 'Featured Events for February 2019';
--- tests/views_integration/Tribe/Events/Views/V2/Template/__snapshots__/TitleTest__test_month_view__1.php
+++ tests/views_integration/Tribe/Events/Views/V2/Template/__snapshots__/TitleTest__test_month_view__1.php
@@ -1,1 +1,1 @@
-<?php return 'Events for August 12, 2020 - August 12, 2020';
+<?php return 'Events for February 2019';
