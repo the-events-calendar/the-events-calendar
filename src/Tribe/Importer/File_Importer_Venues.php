e22954478ede8ba909abff58429da1293b4b43a1
Stephen Page <stephenjpage@gmail.com>
2020-08-14 15:00:58 -0400

    fix(importer): TEC-2962 Don't overwrite unintentionally. [TEC-2962]
    The Venue and Organizer importers were overwriting fields that were not marked for import.
    
    This PR addresses that. And adds a bit of sparkle.
    
    We now loop through the fields and only add them to the update data if they are "active" (marked for import).
    
    Furthermore - we had hardcoded setting the country to 'United States' by default.
    We don't do that anymore.
    
    What we do - is allow the user (via a filter) to turn on using the default data in the admin.
    
    Then we let them filter _individual_ values by key (like...country) so that they can
    1) override defaults from the admin if they desire, and
    2) set default values via filter for imports completely separate from the defaults set in the admin.
    
    :movie_camera: http://p.tri.be/aly8KM
    :movie_camera: (Take 2, fixes bug at end with default values): http://p.tri.be/ZdlWcm (edited)

--- src/Tribe/Importer/File_Importer_Venues.php
+++ src/Tribe/Importer/File_Importer_Venues.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * Class Tribe__Events__Importer__File_Importer_Venues
  */
@@ -36,30 +35,162 @@ class Tribe__Events__Importer__File_Importer_Venues extends Tribe__Events__Impor
 		return $id;
 	}
 
+	/**
+	 * Build a venue array for creation/update of the current imported venue.
+	 *
+	 * @since 3.2
+	 * @since TBD Adjust to prevent overwriting values that aren't mapped.
+	 *
+	 * @param int    $venue_id The ID of the venue we're currently importing
+	 * @param array  $record An event record from the import.
+	 * @return array $venue The array of venue data for creation/update.
+	 */
 	private function build_venue_array( $venue_id, array $record ) {
-		$show_map_setting = tribe( 'events-aggregator.settings' )->default_map( 'csv' );
-		$venue_address    = trim( $this->get_value_by_key( $record, 'venue_address' ) . ' ' . $this->get_value_by_key( $record, 'venue_address2' ) );
-		$venue            = array(
-			'Venue'         => $this->get_value_by_key( $record, 'venue_name' ),
-			'Description'   => $this->get_value_by_key( $record, 'venue_description' ),
-			'Address'       => $venue_address,
-			'City'          => $this->get_value_by_key( $record, 'venue_city' ),
-			'Country'       => $this->get_value_by_key( $record, 'venue_country' ),
-			'Province'      => $this->get_value_by_key( $record, 'venue_state' ),
-			'State'         => $this->get_value_by_key( $record, 'venue_state' ),
-			'Zip'           => $this->get_value_by_key( $record, 'venue_zip' ),
-			'Phone'         => $this->get_value_by_key( $record, 'venue_phone' ),
-			'URL'           => $this->get_value_by_key( $record, 'venue_url' ),
-			'ShowMap'       => $venue_id ? get_post_meta( $venue_id, '_VenueShowMap', true ) : $show_map_setting,
-			'ShowMapLink'   => $venue_id ? get_post_meta( $venue_id, '_VenueShowMapLink', true ) : $show_map_setting,
-			'FeaturedImage' => $this->get_featured_image( $venue_id, $record ),
-		);
+		$venue          = [];
+		$columns        = [
+			'Venue'       => 'venue_name',
+			'Address'     => 'venue_address',
+			'City'        => 'venue_city',
+			'Country'     => 'venue_country',
+			'Description' => 'venue_description',
+			'Phone'       => 'venue_phone',
+			'Province'    => 'venue_state',
+			'State'       => 'venue_state',
+			'URL'         => 'venue_url',
+			'Zip'         => 'venue_zip',
+		];
 
-		if ( empty( $venue['Country'] ) ) {
-			$venue['Country'] = 'United States';
+		foreach ( $columns as $name => $key ) {
+			// Don't overwrite unset columns.
+			if ( ! $this->has_value_by_key( $record, $key ) ) {
+				continue;
+			}
+
+			// Address is a concatenation of two possible values.
+			if ( 'venue_address' === $key ) {
+				$address_1 = trim( $this->get_value_by_key( $record, 'venue_address' ) );
+				$address_2 = trim( $this->get_value_by_key( $record, 'venue_address2' ) );
+				$venue[ $name ] = ( empty( $address_2 ) ) ? $address_1 : $address_1 . ' ' . $address_2;
+
+				continue;
+			}
+
+			$venue[ $name ] = $this->get_value_by_key( $record, $key );
 		}
 
-		$venue = apply_filters( 'tribe_events_importer_venue_array', $venue, $record, $venue_id, $this );
+		// Handle the manual stuff.
+		$venue['FeaturedImage'] = $this->get_featured_image( $venue_id, $record );
+
+		$show_map_setting       = tribe( 'events-aggregator.settings' )->default_map( 'csv' );
+		$venue['ShowMap']       = $venue_id ? get_post_meta( $venue_id, '_VenueShowMap', true ) : $show_map_setting;
+		$venue['ShowMapLink']   = $venue_id ? get_post_meta( $venue_id, '_VenueShowMapLink', true ) : $show_map_setting;
+
+		/**
+		 * Allows triggering using the default values set in the admin for imported venues.
+		 *
+		 * @since TBD
+		 * @param int   $venue_id The ID of the venue we're currently importing.
+		 * @param array $record The event record from the import.
+		 * @param array $venue The array of venue data we're modifying.
+		 */
+		$set_defaults = apply_filters(
+			'tribe_events_importer_set_default_venue_import_values',
+			false,
+			$venue_id,
+			$record,
+			$venue
+		);
+
+		if ( $set_defaults ) {
+			$venue = $this->set_defaults( $venue, $record );
+		}
+
+		/**
+		 * Allows filtering of values before import.
+		 *
+		 * @since 4.2
+		 *
+		 * @param array $venue The array of venue data we're modifying.
+		 * @param array $record The event record from the import.
+		 * @param int   $venue_id The ID of the venue we're currently importing.
+		 */
+		$venue = apply_filters(
+			'tribe_events_importer_venue_array',
+			$venue,
+			$record,
+			$venue_id,
+			$this
+		);
+
+		return $venue;
+	}
+
+	/**
+	 * Set default venue values.
+	 *
+	 * @since TBD
+	 *
+	 * @param array $venue  The array of venue data we're modifying.
+	 * @param array $record The event record from the import.
+	 * @return array        The modified venue data.
+	 */
+	public function set_defaults( $venue, $record ) {
+		$columns        = [
+			'Address'     => 'address',
+			'City'        => 'city',
+			'Country'     => 'country',
+			'Phone'       => 'phone',
+			'Province'    => 'state',
+			'State'       => 'state',
+			'URL'         => 'url',
+			'Zip'         => 'zip',
+		];
+
+		foreach ( $columns as $name => $key ) {
+			// Only fill in empty columns that we're importing.
+			if ( ! isset( $venue[ $name ] ) || ! empty( $venue[ $name ] ) ) {
+				continue;
+			}
+
+			/**
+			 * Allows filtering of default value before setting.
+			 * Also allows setting a value (specifically for imports) by filter
+			 * that is not set in the admin for manually-created venues.
+			 *
+			 * @since TBD
+			 *
+			 * @param string $value  The default value as set in the admin.
+			 * @param string $name   The name for the value.
+			 * @param array  $venue  The array of venue data we're modifying.
+			 * @param array  $record The event record from the import.
+			 */
+			$default_value = apply_filters(
+				"tribe_events_importer_venue_default_{$key}_value",
+				tribe_get_default_value( $key ),
+				$name,
+				$venue,
+				$record
+			);
+
+			/*
+			 * Country comes through as an array: [ 'US', 'United States' ]
+			 * We could handle this with a filter elsewhere, but let's deal with it here
+			 * so we don't break Geolocation functions.
+			 */
+			if (
+				'country' === $key
+				&& is_array( $default_value )
+			) {
+				$default_value = array_pop( $default_value );
+			}
+
+			// Let's not set values that haven't been set in the admin!
+			if ( empty( $default_value ) ) {
+				continue;
+			}
+
+			$venue[ $name ] = $default_value;
+		}
 
 		return $venue;
 	}
