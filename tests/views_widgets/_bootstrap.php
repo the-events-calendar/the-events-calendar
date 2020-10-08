bc4169c7cdbdf03e8645c4bf9579a1a83f685753
Brian <jesseeproductions@users.noreply.github.com>
2020-10-08 11:01:35 -0400

    Apply suggestions from code review
    Co-authored-by: Stephen Page <stephen@tri.be>

--- tests/views_widgets/_bootstrap.php
+++ tests/views_widgets/_bootstrap.php
@@ -2,7 +2,7 @@
 
 use Tribe\Events\Views\V2\Service_Provider;
 
-// Let's  make sure Views v2 are activated if not.
+// Ensure Views v2 are activated.
 putenv( 'TRIBE_EVENTS_V2_VIEWS=1' );
 tribe_register_provider( Service_Provider::class );
 
