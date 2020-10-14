18b5cb325d33b1c8fa80f566f142c0980fcb1e48
jesseeproductions <info@jesseeproductions.com>
2020-10-14 13:07:36 -0400

    TEC-3592 - fix to load latest past events

--- src/Tribe/Views/V2/Template.php
+++ src/Tribe/Views/V2/Template.php
@@ -60,12 +60,13 @@ class Template extends Base_Template {
 		$context = wp_parse_args( $context_overrides, $this->context );
 		$context['_context'] = $context;
 
-		$template_slug = array_filter(
-			[
+		$template_slug = $this->view->get_template_slug();
+		if ( ! empty( $this->view->get_template_path() ) ) {
+			$template_slug = [
 				$this->view->get_template_path(),
-				$this->view->get_template_slug(),
-			]
-		);
+				$template_slug,
+			];
+		}
 
 		return parent::template( $template_slug, $context, false );
 	}
