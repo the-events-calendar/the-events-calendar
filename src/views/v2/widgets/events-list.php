786a26f7382ff676f8222272a881ac3f335f8f8c
Paul <kim.paul.ms@gmail.com>
2020-10-15 01:45:40 +0800

    add view more template, hook up to events list widget

--- src/views/v2/widgets/events-list.php
+++ src/views/v2/widgets/events-list.php
@@ -19,6 +19,8 @@
  * @var array<string,mixed>  $container_data     An additional set of container `data` attributes.
  * @var string               $breakpoint_pointer String we use as pointer to the current view we are setting up with breakpoints.
  * @var array<string,string> $messages           An array of user-facing messages, managed by the View.
+ * @var array<string,bool>   $display            An array of whether to display specific event meta or not.
+ * @var string               $view_more_link     The URL to view all events.
  */
 
 $event = (object) [
@@ -59,6 +61,7 @@ $display = [
 	'phone'     => true,
 	'organizer' => true,
 ];
+$view_more_link = '#';
 ?>
 <div
 	<?php tribe_classes( $container_classes ); ?>
@@ -92,9 +95,19 @@ $display = [
 
 				<div class="tribe-events-widget-events-list__events">
 					<?php foreach ( $events as $event ) : ?>
-						<?php $this->template( 'widgets/events-list/event', [ 'event' => $event, 'display' => $display ] ); ?>
+						<?php
+						$this->template(
+							'widgets/events-list/event',
+							[
+								'event' => $event,
+								'display' => $display,
+							]
+						);
+						?>
 					<?php endforeach; ?>
 				</div>
+
+				<?php $this->template( 'widgets/events-list/view-more', [ 'view_more_link' => $view_more_link ] ); ?>
 
 			<?php else : ?>
 
