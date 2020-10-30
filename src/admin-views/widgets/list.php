9f34370a1dbda366fd4aec29877e75aec7c111c7
Stephen Page <stephenjpage@gmail.com>
2020-10-30 10:17:28 -0400

    remove echo

--- src/admin-views/widgets/list.php
+++ src/admin-views/widgets/list.php
@@ -25,21 +25,19 @@ if ( empty( $admin_fields ) ) {
 foreach ( $admin_fields as $field_id => $field ) {
 	// Handle a section start. May contain a section title.
 	// Using stripos() to allow for multiple occurrences ("section_start_1", "section_start_2" etc).
-	if ( false !== stripos( $field_id, 'section_start' ) ) {
-		echo '<div class="tribe-events-widget-admin-form__input-section">';
-		if ( ! empty( $field['title'] ) ) {
-			// Note: the actual widget title/handle is an <h3>.
-			echo '<h4>' . esc_html( $field['title'] ) . '</h4>';
-		}
-
+	if ( false !== stripos( $field_id, 'section_start' ) ) : ?>
+		<div class="tribe-events-widget-admin-form__input-section">
+			<?php if ( ! empty( $field['title'] ) ) : ?>
+				<h4><?php echo esc_html( $field['title'] ); ?></h4>
+			<?php endif;
 		continue;
-	}
+	endif;
 
 	// Handle a section end.
-	if ( false !== stripos( $field_id, 'section_end' ) ) {
-		echo '</div>';
-		continue;
-	}
+	if ( false !== stripos( $field_id, 'section_end' ) ) : ?>
+		</div>
+		<?php continue;
+	endif;
 
 	$data = [
 		'for'     => $widget_obj->get_field_id( $field_id ),
