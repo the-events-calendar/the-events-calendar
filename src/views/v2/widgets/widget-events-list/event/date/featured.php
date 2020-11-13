6f99fec996719469e45ccc7df01266bb67ee8b52
Luca Allievi <alliuka@gmail.com>
2020-11-13 10:20:05 +0100

    Replace new widget icons

--- src/views/v2/widgets/widget-events-list/event/date/featured.php
+++ src/views/v2/widgets/widget-events-list/event/date/featured.php
@@ -21,10 +21,11 @@ if ( empty( $event->featured ) ) {
 }
 ?>
 <em
-	class="tribe-events-widget-events-list__event-datetime-featured-icon tribe-common-svgicon tribe-common-svgicon--featured"
+	class="tribe-events-widget-events-list__event-datetime-featured-icon"
 	aria-label="<?php esc_attr_e( 'Featured', 'the-events-calendar' ); ?>"
 	title="<?php esc_attr_e( 'Featured', 'the-events-calendar' ); ?>"
 >
+	<?php $this->template( 'components/icons/featured', [ 'classes' => [ 'tribe-events-widget-events-list__event-datetime-featured-icon-svg' ] ] ); ?>
 </em>
 <span class="tribe-events-widget-events-list__event-datetime-featured-text tribe-common-a11y-visual-hide">
 	<?php esc_html_e( 'Featured', 'the-events-calendar' ); ?>
