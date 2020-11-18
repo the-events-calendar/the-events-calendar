249e445062506dd761123c0862eb6a28bb2e89de
Luca Allievi <alliuka@gmail.com>
2020-11-18 10:46:33 +0100

    Update Widget_ListTest snapshot

--- tests/views_widgets/Tribe/Events/Views/V2/Widgets/__snapshots__/Widget_ListTest__test_render_empty__1.php
+++ tests/views_widgets/Tribe/Events/Views/V2/Widgets/__snapshots__/Widget_ListTest__test_render_empty__1.php
@@ -19,7 +19,7 @@
 		
 			<div  class="tribe-events-header__messages tribe-events-c-messages tribe-common-b2" >
 			<div class="tribe-events-c-messages__message tribe-events-c-messages__message--notice" role="alert">
-			<svg  class="tribe-common-c-svgicon tribe-common-c-svgicon--messages-not-found tribe-events-c-messages__message-icon-svg"  viewBox="0 0 21 23" xmlns="http://www.w3.org/2000/svg"><g fill="none" fill-rule="evenodd"><path d="M.5 2.5h20v20H.5z"/><path stroke-linecap="round" d="M7.583 11.583l5.834 5.834m0-5.834l-5.834 5.834" class="tribe-common-c-svgicon__svg-stroke-accent"/><path stroke-linecap="round" d="M4.5.5v4m12-4v4"/><path stroke-linecap="square" d="M.5 7.5h20"/></g></svg>
+			<svg  class="tribe-common-c-svgicon tribe-common-c-svgicon--messages-not-found tribe-events-c-messages__message-icon-svg"  viewBox="0 0 21 23" xmlns="http://www.w3.org/2000/svg"><g fill-rule="evenodd"><path d="M.5 2.5h20v20H.5z"/><path stroke-linecap="round" d="M7.583 11.583l5.834 5.834m0-5.834l-5.834 5.834" class="tribe-common-c-svgicon__svg-stroke"/><path stroke-linecap="round" d="M4.5.5v4m12-4v4"/><path stroke-linecap="square" d="M.5 7.5h20"/></g></svg>
 			<ul class="tribe-events-c-messages__message-list">
 									<li class="tribe-events-c-messages__message-list-item">
 						There are no upcoming events.					</li>
