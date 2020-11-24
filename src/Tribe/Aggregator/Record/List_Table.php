16b3be663a589cf4e9c9a125f1007315743c4e9a
Mike Cotton <codingmusician@gmail.com>
2020-11-24 15:33:39 -0600

    remove $_REQUEST, use tribe_get_request_var()

--- src/Tribe/Aggregator/Record/List_Table.php
+++ src/Tribe/Aggregator/Record/List_Table.php
@@ -97,11 +97,11 @@ class Tribe__Events__Aggregator__Record__List_Table extends WP_List_Table {
 
 		$args['posts_per_page'] = $per_page;
 
-		if ( 'scheduled' === $this->tab->get_slug() && ! empty( $_REQUEST['s'] ) ) {
+		if ( 'scheduled' === $this->tab->get_slug() && ! empty( tribe_get_request_var( 's' ) ) ) {
 			// nonce check if search form submitted.
 			$nonce = isset( $_POST['s'] ) && isset( $_POST['aggregator']['nonce'] ) ? sanitize_text_field( $_POST['aggregator']['nonce'] ) :  '';
 			if ( isset( $_GET['s'] ) || wp_verify_nonce( $nonce, 'aggregator_' . $this->tab->get_slug() . '_request' ) ) {
-				$search_term = filter_var( $_REQUEST['s'], FILTER_VALIDATE_URL ) ? esc_url_raw( $_REQUEST['s'] ) : sanitize_text_field( $_REQUEST['s'] );
+				$search_term = filter_var( tribe_get_request_var( 's' ), FILTER_VALIDATE_URL ) ? esc_url_raw( tribe_get_request_var( 's' ) ) : sanitize_text_field( tribe_get_request_var( 's' ) );
 				$args['meta_query'] = [
 					'relation' => 'OR',
 					[
@@ -747,8 +747,8 @@ class Tribe__Events__Aggregator__Record__List_Table extends WP_List_Table {
 
 		$current_url = remove_query_arg( $removable_query_args, $current_url );
 
-		if ( ! empty( $_REQUEST['s'] ) ) {
-			$current_url = add_query_arg( 's', sanitize_text_field( $_REQUEST['s'] ), $current_url );
+		if ( ! empty( tribe_get_request_var( 's' ) ) ) {
+			$current_url = add_query_arg( 's', sanitize_text_field( tribe_get_request_var( 's' ) ), $current_url );
 		}
 
 		$page_links = [];
