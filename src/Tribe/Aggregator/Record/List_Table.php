aaea3c115074123a21abf2cf934fcaedcdca7989
Mike Cotton <codingmusician@gmail.com>
2020-11-24 16:00:59 -0600

    Update src/Tribe/Aggregator/Record/List_Table.php
    Co-authored-by: Crisoforo Gaspar Hernández <hello@crisoforo.com>

--- src/Tribe/Aggregator/Record/List_Table.php
+++ src/Tribe/Aggregator/Record/List_Table.php
@@ -101,7 +101,9 @@ class Tribe__Events__Aggregator__Record__List_Table extends WP_List_Table {
 			// nonce check if search form submitted.
 			$nonce = isset( $_POST['s'] ) && isset( $_POST['aggregator']['nonce'] ) ? sanitize_text_field( $_POST['aggregator']['nonce'] ) :  '';
 			if ( isset( $_GET['s'] ) || wp_verify_nonce( $nonce, 'aggregator_' . $this->tab->get_slug() . '_request' ) ) {
-				$search_term = filter_var( tribe_get_request_var( 's' ), FILTER_VALIDATE_URL ) ? esc_url_raw( tribe_get_request_var( 's' ) ) : sanitize_text_field( tribe_get_request_var( 's' ) );
+				$search_term = filter_var( tribe_get_request_var( 's' ), FILTER_VALIDATE_URL ) 
+					? esc_url_raw( tribe_get_request_var( 's' ) ) 
+					: sanitize_text_field( tribe_get_request_var( 's' ) );
 				$args['meta_query'] = [
 					'relation' => 'OR',
 					[
