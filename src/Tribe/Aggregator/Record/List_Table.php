7bacc00a1f13bd1791378f2558ddc89e1f288253
Mike Cotton <codingmusician@gmail.com>
2020-11-25 06:47:13 -0600

    fix spacing

--- src/Tribe/Aggregator/Record/List_Table.php
+++ src/Tribe/Aggregator/Record/List_Table.php
@@ -96,12 +96,12 @@ class Tribe__Events__Aggregator__Record__List_Table extends WP_List_Table {
 		}
 
 		$args['posts_per_page'] = $per_page;
-		$search_term = tribe_get_request_var( 's' );
+		$search_term            = tribe_get_request_var( 's' );
 		if ( 'scheduled' === $this->tab->get_slug() && ! empty( $search_term ) ) {
 			// nonce check if search form submitted.
 			$nonce = isset( $_POST['s'] ) && isset( $_POST['aggregator']['nonce'] ) ? sanitize_text_field( $_POST['aggregator']['nonce'] ) :  '';
 			if ( isset( $_GET['s'] ) || wp_verify_nonce( $nonce, 'aggregator_' . $this->tab->get_slug() . '_request' ) ) {
-				$search_term = filter_var( $search_term, FILTER_VALIDATE_URL ) 
+				$search_term        = filter_var( $search_term, FILTER_VALIDATE_URL ) 
 					? esc_url_raw( $search_term ) 
 					: sanitize_text_field( $search_term );
 				$args['meta_query'] = [
