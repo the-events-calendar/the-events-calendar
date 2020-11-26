3a7649a38f3ba32743c61245624b7a8895021425
Luca Tumedei <luca@theaveragedev.com>
2020-11-26 16:51:47 +0100

    test(views_v2_customizer_integration) set up the suite

---
+++ tests/views_v2_customizer_integration/CustomizerSettingsTest.php
@@ -0,0 +1,55 @@
+<?php
+
+use Spatie\Snapshots\MatchesSnapshots;
+use Tribe__Customizer as Customizer;
+
+class CustomizerSettingsTest extends \Codeception\TestCase\WPTestCase {
+	use MatchesSnapshots;
+
+	/**
+	 * @var array<string,string>
+	 */
+	private $collected_inline_styles = [];
+
+	/**
+	 * It should allow taking a CSS template snapshot
+	 *
+	 * @test
+	 */
+	public function should_allow_taking_a_css_template_snapshot() {
+		$css_template = $this->get_css_template_for_settings( [ 'global_elements' => [ 'accent_color' => '#238923', 'link_color' => '#238923' ] ] );
+
+		$this->assertMatchesSnapshot( $css_template );
+	}
+
+	/**
+	 * Emulates a Customizer render where the specified settings, and only those settings, are set to the
+	 * specified values.
+	 *
+	 * @since TBD
+	 *
+	 * @param array<string<array<string,int|string|float>> $settings A map of the customizer Sections, each
+	 *                                                               one a map of customizer Settings.
+	 *
+	 * @return array<string,string> A map from each sheet handle to its rendered content.
+	 */
+	private function get_css_template_for_settings( array $settings = [] ) {
+		add_filter( 'pre_option_tribe_customizer', static function () use ( $settings ) {
+			return $settings;
+		}, PHP_INT_MAX );
+
+		/** @var Customizer $customizer */
+		$customizer       = tribe( 'customizer' );
+		$styles_collector = function ( string $sheet, string $inline_style ) {
+			$this->collected_inline_styles[ $sheet ] = $inline_style;
+		};
+		// All sheets should count as enqueued.
+		uopz_set_return( 'wp_style_is', true );
+		add_action( 'tribe_customizer_before_inline_style', $styles_collector, 100, 2 );
+		$customizer->inline_style( $force = true );
+		remove_action( 'tribe_customizer_before_inline_style', $styles_collector, 100 );
+
+		return $this->collected_inline_styles;
+	}
+}
+
