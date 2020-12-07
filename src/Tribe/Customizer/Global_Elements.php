32425e8ec7f6616493ae343e143c6e29f1928c27
jesseeproductions <info@jesseeproductions.com>
2020-12-07 10:20:49 -0500

    TEC-3276 - Enable changing map pins in V2 customizer

--- src/Tribe/Customizer/Global_Elements.php
+++ src/Tribe/Customizer/Global_Elements.php
@@ -222,6 +222,32 @@ final class Tribe__Events__Customizer__Global_Elements extends Tribe__Customizer
 
 		$customizer->add_setting_name( $customizer->get_setting_name( 'accent_color', $section ) );
 
+		// Custom Map Pins are not supported with basic embeds.
+		if ( ! tribe_is_using_basic_gmaps_api() ) {
+
+			$manager->add_setting(
+				$customizer->get_setting_name( 'map_pin', $section ),
+				[
+					'default'           => $this->get_default( 'map_pin' ),
+					'type'              => 'option',
+					'sanitize_callback' => 'esc_url_raw',
+				]
+			);
+
+			$manager->add_control(
+				new WP_Customize_Image_Control(
+					$manager,
+					$customizer->get_setting_name( 'map_pin', $section ),
+					[
+						'default' => $this->get_default( 'button_color' ),
+						'label'   => esc_html__( 'Map Pin', 'the-events-calendar' ),
+						'section' => $section->id,
+					]
+				)
+			);
+
+		}
+
 		if ( tribe_events_views_v2_is_enabled() ) {
 			return;
 		}
@@ -270,36 +296,8 @@ final class Tribe__Events__Customizer__Global_Elements extends Tribe__Customizer
 			)
 		);
 
-		// Custom Map Pins are not supported with basic embeds.
-		if ( ! tribe_is_using_basic_gmaps_api() ) {
-
-			$manager->add_setting(
-				$customizer->get_setting_name( 'map_pin', $section ),
-				[
-					'default'           => $this->get_default( 'map_pin' ),
-					'type'              => 'option',
-					'sanitize_callback' => 'esc_url_raw',
-				]
-			);
-
-			$manager->add_control(
-				new WP_Customize_Image_Control(
-					$manager,
-					$customizer->get_setting_name( 'map_pin', $section ),
-					[
-						'default' => $this->get_default( 'button_color' ),
-						'label'   => esc_html__( 'Map Pin', 'the-events-calendar' ),
-						'section' => $section->id,
-					]
-				)
-			);
-		}
-
 		// Introduced to make Selective Refresh have less code duplication
 		$customizer->add_setting_name( $customizer->get_setting_name( 'filterbar_color', $section ) );
 		$customizer->add_setting_name( $customizer->get_setting_name( 'button_color', $section ) );
-
-		// To add Live Edit Pins will require some JS refactor to be able to work
-		// $customizer->add_setting_name( $customizer->get_setting_name( 'map_pin', $section ) );
 	}
 }
