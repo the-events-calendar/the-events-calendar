3f7b80b7c765c63396e8a839ba75168307ef0624
Mike Cotton <codingmusician@gmail.com>
2020-12-07 14:04:08 -0600

    Recommended changes

--- src/Tribe/Admin_List.php
+++ src/Tribe/Admin_List.php
@@ -184,12 +184,12 @@ if ( ! class_exists( 'Tribe__Events__Admin_List' ) ) {
 			
 			$table_alias = 'ea_record_' . substr( uniqid( 'ea_record', true ), 0, 10 );
 			// Add the record meta query if it is missing.
-			if ( ! preg_match( '/\\stribe_aggregator_record\\s/', $clauses['join'] ) ) {
+			if ( ! preg_match( '/\\s' . $table_alias . '\\s/', $clauses['join'] ) ) {
 				$clauses['join'] .= " LEFT JOIN {$wpdb->postmeta} AS {$table_alias} ON {$wpdb->posts}.ID = {$table_alias}.post_id AND {$table_alias}.meta_key = '_tribe_aggregator_parent_record' ";
 			}
 
 			// Add the record meta filter if it is missing.
-			if ( ! preg_match( '/\\stribe_aggregator_record\\s/', $clauses['where'] ) ) {
+			if ( ! preg_match( '/\\s' . $table_alias . '\\s/', $clauses['where'] ) ) {
 				$clauses['where'] .= " AND {$table_alias}.meta_value = '{$record_num}' ";
 			}
 
