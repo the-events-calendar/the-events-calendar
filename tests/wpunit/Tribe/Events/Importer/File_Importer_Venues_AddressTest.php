e08a8e43c6fc12fd7263c36700ea1fb8aa97d319
jesseeproductions <info@jesseeproductions.com>
2020-12-08 11:42:55 -0500

    TEC-3637 - fix tests in tric

--- tests/wpunit/Tribe/Events/Importer/File_Importer_Venues_AddressTest.php
+++ tests/wpunit/Tribe/Events/Importer/File_Importer_Venues_AddressTest.php
@@ -11,11 +11,12 @@ class File_Importer_Venues_AddressTest extends File_Importer_VenuesTest {
 	 * it should insert venue address if provided
 	 */
 	public function it_should_insert_venue_post_content_if_address_is_provided() {
-		$this->data        = [
-			'address_1' => '1626 Second Avenue',
+		$this->data = [
+			'address_1'   => '1626 Second Avenue',
+			'address_2_1' => '',
 		];
-		$sut = $this->make_instance( 'venues-address' );
-		$post_id = $sut->import_next_row();
+		$sut        = $this->make_instance( 'venues-address' );
+		$post_id    = $sut->import_next_row();
 
 		$this->assertEquals( '1626 Second Avenue', get_post_meta( $post_id, '_VenueAddress', true ) );
 	}
@@ -25,20 +26,20 @@ class File_Importer_Venues_AddressTest extends File_Importer_VenuesTest {
 	 * it should overwrite address when reimporting
 	 */
 	public function it_should_overwrite_post_content_when_reimporting() {
-		$this->data        = [
-			'address_1' => 'West 55th Street',
+		$this->data = [
+			'address_1'   => 'West 55th Street',
 			'address_2_1' => '259-A',
 		];
-		$sut = $this->make_instance( 'venues-address' );
-		$post_id = $sut->import_next_row();
+		$sut        = $this->make_instance( 'venues-address' );
+		$post_id    = $sut->import_next_row();
 
 		$this->assertEquals( 'West 55th Street 259-A', get_post_meta( $post_id, '_VenueAddress', true ) );
 
-		$this->data = [
-			'address_1' => 'West 55th Street',
+		$this->data       = [
+			'address_1'   => 'West 55th Street',
 			'address_2_1' => '',
 		];
-		$sut = $this->make_instance( 'venues-address' );
+		$sut              = $this->make_instance( 'venues-address' );
 		$reimport_post_id = $sut->import_next_row();
 
 		$this->assertEquals( $post_id, $reimport_post_id );
@@ -50,18 +51,20 @@ class File_Importer_Venues_AddressTest extends File_Importer_VenuesTest {
 	 * it should restore a venue address that has been emptied
 	 */
 	public function it_should_restore_a_venue_address_that_has_been_emptied() {
-		$this->data        = [
-			'address_1' => '',
+		$this->data = [
+			'address_1'   => '',
+			'address_2_1' => '',
 		];
-		$sut = $this->make_instance( 'venues-address' );
-		$post_id = $sut->import_next_row();
+		$sut        = $this->make_instance( 'venues-address' );
+		$post_id    = $sut->import_next_row();
 
 		$this->assertEquals( '', get_post_meta( $post_id, '_VenueAddress', true ) );
 
-		$this->data = [
-			'address_1' => '10 Downing St',
+		$this->data       = [
+			'address_1'   => '10 Downing St',
+			'address_2_1' => '',
 		];
-		$sut = $this->make_instance( 'venues-address' );
+		$sut              = $this->make_instance( 'venues-address' );
 		$reimport_post_id = $sut->import_next_row();
 
 		$this->assertEquals( $post_id, $reimport_post_id );
