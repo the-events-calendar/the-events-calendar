d8c21d49fbddf454a09e348ce483c73a2483bf5c
Luca Allievi <alliuka@gmail.com>
2021-02-02 10:38:52 +0100

    Update snaps

--- tests/views_widgets/Tribe/Events/Views/V2/Widgets/__snapshots__/Widget_ListTest__test_render_json_with_upcoming_events__1.php
+++ tests/views_widgets/Tribe/Events/Views/V2/Widgets/__snapshots__/Widget_ListTest__test_render_json_with_upcoming_events__1.php
@@ -14,8 +14,8 @@
 	{"slug":"widget-events-list","prev_url":"","next_url":"http:\\/\\/test.tri.be\\/?post_type=tribe_events&eventDisplay=widget-events-list&page=2","view_class":"Tribe\\\\Events\\\\Views\\\\V2\\\\Views\\\\Widgets\\\\Widget_List_View","view_slug":"widget-events-list","view_label":"Widget-events-list","title":"The Events Calendar Tests","events":[7,8],"url":"http:\\/\\/test.tri.be\\/?post_type=tribe_events&eventDisplay=widget-events-list","url_event_date":"2019-01-01","bar":{"keyword":"","date":"2019-01-01 09:00:00"},"today":"2019-01-01 09:00:00","now":"2019-01-01 09:00:00","rest_url":"http:\\/\\/test.tri.be\\/index.php?rest_route=\\/tribe\\/views\\/v2\\/html","rest_method":"POST","rest_nonce":"2ab7cc6b39","should_manage_url":true,"today_url":"http:\\/\\/test.tri.be\\/?post_type=tribe_events&eventDisplay=widget-events-list","prev_label":"","next_label":"","date_formats":{"compact":"n\\/j\\/Y","month_and_year_compact":"n\\/j\\/Y","month_and_year":"F Y","time_range_separator":" - ","date_time_separator":" @ "},"messages":[],"start_of_week":"1","breadcrumbs":[],"before_events":"<div id=\\"tribe-events\\" class=\\"tribe-no-js\\" data-live_ajax=\\"1\\" data-datepicker_format=\\"1\\" data-category=\\"\\" data-featured=\\"\\">","after_events":"<\\/div><!-- #tribe-events -->\\n<!--\\nThis calendar is powered by The Events Calendar.\\nhttp:\\/\\/evnt.is\\/18wn\\n-->\\n","display_events_bar":false,"disable_event_search":false,"live_refresh":true,"ical":{"display_link":true,"link":{"url":"http:\\/\\/test.tri.be\\/events\\/?ical=1","text":"Export Events","title":"Use this to share calendar data with Google Calendar, Apple iCal and other compatible apps"}},"container_classes":["tribe-common","tribe-events","tribe-events-view","tribe-events-view--widget-events-list","tribe-events-widget"],"container_data":[],"is_past":false,"breakpoints":{"xsmall":500,"medium":768,"full":960},"breakpoint_pointer":"random-id","is_initial_load":true,"public_views":{"list":{"view_class":"Tribe\\\\Events\\\\Views\\\\V2\\\\Views\\\\List_View","view_url":"http:\\/\\/test.tri.be\\/events\\/list\\/?tribe-bar-date=2019-01-01","view_label":"List"},"month":{"view_class":"Tribe\\\\Events\\\\Views\\\\V2\\\\Views\\\\Month_View","view_url":"http:\\/\\/test.tri.be\\/events\\/month\\/2019-01\\/","view_label":"Month"},"day":{"view_class":"Tribe\\\\Events\\\\Views\\\\V2\\\\Views\\\\Day_View","view_url":"http:\\/\\/test.tri.be\\/events\\/2019-01-01\\/","view_label":"Day"}},"show_latest_past":false,"view_more_link":"http:\\/\\/test.tri.be\\/events\\/","widget_title":null,"hide_if_no_upcoming_events":null,"jsonld_enable":1,"display":[],"_context":{"slug":"widget-events-list"}}</script>
 
 		<header class="tribe-events-widget-events-list__header">
-			<h3 class="tribe-events-widget-events-list__header-title tribe-common-h6 tribe-common-h--alt">
-							</h3>
+			<h2 class="tribe-events-widget-events-list__header-title tribe-common-h6 tribe-common-h--alt">
+							</h2>
 		</header>
 
 		
@@ -49,7 +49,7 @@
 	<time class="tribe-events-widget-events-list__event-datetime" datetime="2019-06-20">
 		<span class="tribe-event-date-start">1:01 pm</span> - <span class="tribe-event-time">5:01 pm</span>	</time>
 	</div>
-					<h4 class="tribe-events-widget-events-list__event-title tribe-common-h7">
+					<h3 class="tribe-events-widget-events-list__event-title tribe-common-h7">
 	<a
 		href="http://test.tri.be/?tribe_events=test-event-9-days-2"
 		title="Test Event &#8211; +9 days"
@@ -57,7 +57,7 @@
 		class="tribe-events-widget-events-list__event-title-link tribe-common-anchor-thin"
 	>
 		Test Event &#8211; +9 days	</a>
-</h4>
+</h3>
 				</header>
 
 				
@@ -86,7 +86,7 @@
 		<time class="tribe-events-widget-events-list__event-datetime" datetime="2019-06-20">
 		<span class="tribe-event-date-start">1:04 pm</span> - <span class="tribe-event-time">5:04 pm</span>	</time>
 	</div>
-					<h4 class="tribe-events-widget-events-list__event-title tribe-common-h7">
+					<h3 class="tribe-events-widget-events-list__event-title tribe-common-h7">
 	<a
 		href="http://test.tri.be/?tribe_events=single-event-1"
 		title="Single Event 1"
@@ -94,7 +94,7 @@
 		class="tribe-events-widget-events-list__event-title-link tribe-common-anchor-thin"
 	>
 		Single Event 1	</a>
-</h4>
+</h3>
 				</header>
 
 				
