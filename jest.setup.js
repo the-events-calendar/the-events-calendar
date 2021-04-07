7e115ea000e996a6a6bbd82395c18477dc0fa0f8
Paul <kim.paul.ms@gmail.com>
2021-04-08 01:02:58 +0800

    Revert "Revert "Fix / classic details block create organizer""
    This reverts commit 9361c4a1423de6b71187d6379532ac48fc5813d2.

--- jest.setup.js
+++ jest.setup.js
@@ -14,10 +14,18 @@ global.jQuery = $;
 global.$ = $;
 global.wp = {
 	element: React,
+	apiRequest: () => $.Deferred(),
+	editor: {},
+	components: {},
 };
 global.shallow = shallow;
 global.render = render;
 global.mount = mount;
 global.renderer = renderer;
+global.console = {
+	error: jest.fn(),
+	log: jest.fn(),
+	warning: jest.fn(),
+};
 
 moment.tz.setDefault( 'UTC' );
