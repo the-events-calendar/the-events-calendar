98403c02a446814e82b43172587c22afe734aaf0
Camwyn <stephen@theeventscalendar.com>
2021-04-07 09:45:23 -0400

    Make them theme overrides.

--- src/Tribe/Views/V2/Customizer.php
+++ src/Tribe/Views/V2/Customizer.php
@@ -363,20 +363,35 @@ class Customizer {
 					background-color: <%= global_elements.accent_color %>;
 				}
 
-				.tribe-common.tribe-events-widget .tribe-events-widget-events-list__view-more-link {
+				.tribe-events-widget .tribe-events-widget-events-list__view-more-link {
 					color: <%= global_elements.accent_color %>;
 				}
+			';
 
+			// overrides for the widget view more link
+			$css_template .= '
 				.tribe-common.tribe-events-widget .tribe-events-widget-events-list__view-more-link:active,
 				.tribe-common.tribe-events-widget .tribe-events-widget-events-list__view-more-link:focus,
 				.tribe-common.tribe-events-widget .tribe-events-widget-events-list__view-more-link:hover {
 					border-bottom-color: <%= global_elements.accent_color %>;
 				}
+			';
 
+			// Theme overrides for widget view more link
+			$css_template .= '
+				.tribe-theme-twentyseventeen .tribe-events-widget .tribe-events-widget-events-list__view-more-link,
+				.tribe-theme-twentytwentyone .tribe-events-widget .tribe-events-widget-events-list__view-more-link,
+				.tribe-theme-twentyseventeen .site-footer .widget-area .tribe-events-widget .tribe-events-widget-events-list__view-more-link,
+				.site-footer .widget-area .tribe-events-widget .tribe-events-widget-events-list__view-more-link {
+					color: <%= global_elements.accent_color %>;
+				}
+			';
+
+			// Widget featured icon color
+			$css_template .= '
 				.tribe-events-widget .tribe-events-widget-events-list__event-row--featured .tribe-events-widget-events-list__event-date-tag-datetime:after {
 					background-color: <%= global_elements.accent_color %>;
 				}
-
 			';
 
 			// overrides for common base/full/typography/_ctas.pcss.
