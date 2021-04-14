59a3cd2ac63da03356c9b8544ee2de8010a3898c
Camwyn <stephen@theeventscalendar.com>
2021-04-14 13:29:00 -0400

    Update snapshots

--- tests/views_integration/Tribe/Events/Views/V2/Partials/Month_View/Mobile_Events/__snapshots__/Mobile_DayTest__test_render_with_no_events_today_date_equals_day_date__1.php
+++ tests/views_integration/Tribe/Events/Views/V2/Partials/Month_View/Mobile_Events/__snapshots__/Mobile_DayTest__test_render_with_no_events_today_date_equals_day_date__1.php
@@ -1,12 +1,5 @@
 <?php return '
 <div  class="tribe-events-calendar-month-mobile-events__mobile-day tribe-events-calendar-month-mobile-events__mobile-day--show"  id="tribe-events-calendar-mobile-day-2018-06-01">
-	<div class="tribe-events-c-day-marker tribe-events-calendar-month-mobile-events__day-marker">
-	<time
-		class="tribe-events-c-day-marker__date tribe-common-h7 tribe-common-h--alt"
-		datetime="2018-06-01"
-	>
-		June 1	</time>
-</div>
 
 	
 	</div>
