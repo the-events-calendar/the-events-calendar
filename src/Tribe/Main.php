e786b546a20f45da0c00634301ccbe70940b7e1d
Gustavo Bordoni <bordoni.dev@gmail.com>
2021-05-05 00:07:52 -0400

    Modify 5.1.6 to 5.1.6.1

--- src/Tribe/Main.php
+++ src/Tribe/Main.php
@@ -35,7 +35,7 @@ if ( ! class_exists( 'Tribe__Events__Main' ) ) {
 		const VENUE_POST_TYPE     = 'tribe_venue';
 		const ORGANIZER_POST_TYPE = 'tribe_organizer';
 
-		const VERSION             = '5.1.6';
+		const VERSION             = '5.1.6.1';
 
 		/**
 		 * Min Pro Addon
