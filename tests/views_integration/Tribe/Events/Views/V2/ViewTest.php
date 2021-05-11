4d83097f75dbae09a35c970f0fc1765e983d12f7
Matthew Batchelder <borkweb@gmail.com>
2021-05-11 09:50:44 -0400

    Fix URL matching

--- tests/views_integration/Tribe/Events/Views/V2/ViewTest.php
+++ tests/views_integration/Tribe/Events/Views/V2/ViewTest.php
@@ -194,7 +194,7 @@ class ViewTest extends \Codeception\TestCase\WPTestCase {
 		$page_1_view->set_has_next_event( true );
 		$page_1_view->setup_the_loop( [ 'posts_per_page' => 2, 'starts_after' => 'now' ] );
 
-		$this->assertEquals( home_url() . '?post_type=tribe_events&eventDisplay=test&page=2', $page_1_view->next_url() );
+		$this->assertEquals( home_url() . '?post_type=tribe_events&eventDisplay=test&paged=2', $page_1_view->next_url() );
 
 		$page_2_view = View::make( 'test' );
 		$page_2_view->set_has_next_event( false );
