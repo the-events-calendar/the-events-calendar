698310f16caf4a7ab1fe787791cba10363219624
Camwyn <stephen@theeventscalendar.com>
2021-05-20 15:37:39 -0400

    Update common and common styles

--- src/Tribe/Views/V2/Customizer/Section/Global_Elements.php
+++ src/Tribe/Views/V2/Customizer/Section/Global_Elements.php
@@ -212,7 +212,11 @@ final class Global_Elements extends \Tribe__Customizer__Section {
 			return $css_template;
 		}
 
-		$css_template = ":root{\n";
+		// These allow us to continue to _not_ target the shortcode.
+		$apply_to_shortcode = apply_filters( 'tribe_customizer_should_print_shortcode_customizer_styles', false );
+		$tribe_events = $apply_to_shortcode ? '.tribe-events' : '.tribe-events:not( .tribe-events-view--shortcode )';
+
+		$css_template = "$tribe_events {\n";
 
 		// Accent color overrides.
 		if ( $this->should_include_setting_css( 'accent_color' ) ) {
