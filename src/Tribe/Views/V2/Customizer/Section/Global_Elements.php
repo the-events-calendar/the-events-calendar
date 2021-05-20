3ff08c3c3681aa6604be46ffc3e60ed3f9605222
Camwyn <stephen@theeventscalendar.com>
2021-05-20 15:55:31 -0400

    Ensure global styles apply to single events.

--- src/Tribe/Views/V2/Customizer/Section/Global_Elements.php
+++ src/Tribe/Views/V2/Customizer/Section/Global_Elements.php
@@ -214,7 +214,7 @@ final class Global_Elements extends \Tribe__Customizer__Section {
 
 		// These allow us to continue to _not_ target the shortcode.
 		$apply_to_shortcode = apply_filters( 'tribe_customizer_should_print_shortcode_customizer_styles', false );
-		$tribe_events = $apply_to_shortcode ? '.tribe-events' : '.tribe-events:not( .tribe-events-view--shortcode )';
+		$tribe_events = $apply_to_shortcode ? '.tribe-events, #tribe-events-pg-template' : '.tribe-events:not( .tribe-events-view--shortcode ), #tribe-events-pg-template';
 
 		$css_template = "$tribe_events {\n";
 
