2170959422822cd0a7b3d7411b0f05231dbebeca
Camwyn <stephen@theeventscalendar.com>
2021-08-05 11:05:00 -0400

    try 3?

--- tests/aggregatorv1/_bootstrap.php
+++ tests/aggregatorv1/_bootstrap.php
@@ -1,6 +1,3 @@
 <?php
-use Tribe\Events\Views\V2\Service_Provider;
-
 // Let's  make sure Views v2 are activated if not.
 putenv( 'TRIBE_EVENTS_V2_VIEWS=1' );
-tribe_register_provider( Service_Provider::class );
