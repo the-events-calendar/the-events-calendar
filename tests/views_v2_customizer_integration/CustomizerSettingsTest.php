ba39674c0b296f75d945fef02246636bd8a5ed17
Camwyn <stephen@theeventscalendar.com>
2021-08-05 11:56:41 -0400

    update snapshots

--- tests/views_v2_customizer_integration/CustomizerSettingsTest.php
+++ tests/views_v2_customizer_integration/CustomizerSettingsTest.php
@@ -33,20 +33,20 @@ class CustomizerSettingsTest extends \Codeception\TestCase\WPTestCase {
 					    --tec-color-link-accent: #238923;
 					    --tec-color-link-accent-hover: rgba(35,137,35, 0.8);
 					    --tec-color-accent-primary: #238923;
-					    --tec-color-accent-primary-hover: rgba(35,137,35,0.8);
-					    --tec-color-accent-primary-multiday: rgba(35,137,35,0.24);
-					    --tec-color-accent-primary-multiday-hover: rgba(35,137,35,0.34);
-					    --tec-color-accent-primary-active: rgba(35,137,35,0.9);
-					    --tec-color-accent-primary-background: rgba(35,137,35,0.07);
-					    --tec-color-background-secondary-datepicker: rgba(35,137,35,0.5);
+					    --tec-color-accent-primary-hover: rgba(35,137,35, 0.8);
+					    --tec-color-accent-primary-multiday: rgba(35,137,35, 0.24);
+					    --tec-color-accent-primary-multiday-hover: rgba(35,137,35, 0.34);
+					    --tec-color-accent-primary-active: rgba(35,137,35, 0.9);
+					    --tec-color-accent-primary-background: rgba(35,137,35, 0.07);
+					    --tec-color-background-secondary-datepicker: rgba(35,137,35, 0.5);
 					    --tec-color-accent-primary-background-datepicker: #238923;
 					    --tec-color-button-primary: #238923;
-					    --tec-color-button-primary-hover: rgba(35,137,35,0.8);
-					    --tec-color-button-primary-active: rgba(35,137,35,0.9);
-					    --tec-color-button-primary-background: rgba(35,137,35,0.07);
+					    --tec-color-button-primary-hover: rgba(35,137,35, 0.8);
+					    --tec-color-button-primary-active: rgba(35,137,35, 0.9);
+					    --tec-color-button-primary-background: rgba(35,137,35, 0.07);
 					    --tec-color-day-marker-current-month: #238923;
-					    --tec-color-day-marker-current-month-hover: rgba(35,137,35,0.8);
-					    --tec-color-day-marker-current-month-active: rgba(35,137,35,0.9);
+					    --tec-color-day-marker-current-month-hover: rgba(35,137,35, 0.8);
+					    --tec-color-day-marker-current-month-active: rgba(35,137,35, 0.9);
 					    --tec-color-background-primary-multiday: rgba(35,137,35, 0.24);
 					    --tec-color-background-primary-multiday-hover: rgba(35,137,35, 0.34);
 					    --tec-color-background-primary-multiday-active: rgba(35,137,35, 0.34);
@@ -75,7 +75,7 @@ class CustomizerSettingsTest extends \Codeception\TestCase\WPTestCase {
 				"font_family" => "theme"
 			],
 			"tec_events_bar" => [
-				"events_bar_border_color_choice" => "custom",
+				"events_bar_border_color_choice" =>"custom",
 				"events_bar_text_color" => "#8224e3",
 				"find_events_button_color_choice" => "custom",
 				"find_events_button_color" => "#dd3333",
