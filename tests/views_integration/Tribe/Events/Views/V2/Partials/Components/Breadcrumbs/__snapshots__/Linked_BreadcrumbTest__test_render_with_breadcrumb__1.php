0a81432e09c8813f2e17d6d6a120a1f88a92445f
eugenekyale <eugene.kyale@gmail.com>
2021-08-25 17:07:24 +0300

    update snapshot for linked breadcrumb

--- tests/views_integration/Tribe/Events/Views/V2/Partials/Components/Breadcrumbs/__snapshots__/Linked_BreadcrumbTest__test_render_with_breadcrumb__1.php
+++ tests/views_integration/Tribe/Events/Views/V2/Partials/Components/Breadcrumbs/__snapshots__/Linked_BreadcrumbTest__test_render_with_breadcrumb__1.php
@@ -1,6 +1,6 @@
 <?php return '<li class="tribe-events-c-breadcrumbs__list-item">
 	<a
-		href="https://test.tri.be/events/list"
+		href="http://test.tri.be/events/"
 		class="tribe-events-c-breadcrumbs__list-item-link"
 		data-js="tribe-events-view-link"
 	>
