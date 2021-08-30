85d208ed575189bc53ffcf5150240b2ca42b50ab
eugenekyale <eugene.kyale@gmail.com>
2021-08-30 13:34:07 +0300

    regenerate snapshot

--- tests/views_integration/Tribe/Events/Views/V2/Partials/List_View/Event/__snapshots__/VenueTest__test_render_with_event_with_venue__1.php
+++ tests/views_integration/Tribe/Events/Views/V2/Partials/List_View/Event/__snapshots__/VenueTest__test_render_with_event_with_venue__1.php
@@ -2,6 +2,8 @@
 	<span class="tribe-events-calendar-list__event-venue-title tribe-common-b2--bold">
 		Venue 2233 title	</span>
 	<span class="tribe-events-calendar-list__event-venue-address">
-		100 Rue de Le Chat, 					Paris			</span>
+		100 Rue de Le Chat, 
+					Paris, 				
+					Ile de France			</span>
 </address>
 ';
