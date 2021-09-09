c54497cd32097cfb7107322194ad65ff6f970ac5
Edwin Daniels <stratease@gmail.com>
2021-09-08 17:10:06 -0700

    was missing http scheme in url, fixing it_should_return_tec_rest_path_as_base_path test failure

--- src/Tribe/REST/V1/Endpoints/Swagger_Documentation.php
+++ src/Tribe/REST/V1/Endpoints/Swagger_Documentation.php
@@ -60,13 +60,14 @@ class Tribe__Events__REST__V1__Endpoints__Swagger_Documentation
 	 * @return array An array description of a Swagger supported component.
 	 */
 	public function get_documentation() {
+		$scheme = is_ssl() ? 'https' : 'http';
 		$documentation = [
 			'openapi'     => $this->swagger_version,
 			'info'        => $this->get_api_info(),
 			'components' => ['schemas' => $this->get_definitions()],
 			'servers'     => [
 				[
-					'url' => parse_url( home_url(), PHP_URL_HOST ).str_replace( home_url(), '', tribe_events_rest_url() ),
+					'url' => $scheme."://".parse_url( home_url(), PHP_URL_HOST ).str_replace( home_url(), '', tribe_events_rest_url() ),
 				]
 			], 
 			'paths'       => $this->get_paths()
