61ae4a6109e94df0f909503bb9f7009afc6b60e8
jesseeproductions <info@jesseeproductions.com>
2021-11-01 09:22:30 -0400

    TEC-3464 - update snapshots

--- tests/event_status/Tribe/Events/Event_Status/Views/__snapshots__/Single_ViewTest__it_should_render_single_canceled_name_reason_and_html__1.php
+++ tests/event_status/Tribe/Events/Event_Status/Views/__snapshots__/Single_ViewTest__it_should_render_single_canceled_name_reason_and_html__1.php
@@ -1,13 +1,12 @@
 <?php return '<div class="tribe-common-b2 tribe-events-status-single__container">
 	<div class="tribe-events-status-single__notice tribe-events-status-single__notice-canceled">
-	<div class="tribe-events-status-text">
+	<div class="tribe-events-status__text">
 
-		<div class="tribe-events-status-single__notice-header tribe-events-status-text--red tribe-events-status-text--bold tribe-events-status-text--alert-icon">
+		<div class="tribe-events-status-single__notice-header tribe-events-status__text-red tribe-events-status__text-bold tribe-events-status__text-alert-icon">
 			Canceled		</div>
-
-		<div class="tribe-events-status-single__notice-description">
-			This event is canceled because.		</div>
-	</div>
+					<div class="tribe-events-status-single__notice-description">
+				This event is canceled because.			</div>
+			</div>
 </div>
 </div>
 ';
