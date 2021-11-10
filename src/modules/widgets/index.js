005c904e5372e5e4ef43f1a8a6854217d93e1092
Eugene Kyale <eugene.kyale@gmail.com>
2021-11-10 18:45:09 +0300

    Update src/modules/widgets/index.js
    Co-authored-by: Paul <kim.paul.ms@gmail.com>

--- src/modules/widgets/index.js
+++ src/modules/widgets/index.js
@@ -5,7 +5,8 @@ import EventsList from '@moderntribe/events/widgets/events-list';
 import './style.pcss';
 
 const { registerBlockType } = wp.blocks;
-const { registerLegacyWidgetBlock } = wp.widgets;
+
+wp.widgets.registerLegacyWidgetBlock();
 
 const blocks = [
 	EventsList,
@@ -13,7 +14,6 @@ const blocks = [
 
 blocks.forEach( block => {
 	const blockName = `tribe/${ block.id }`;
-	registerLegacyWidgetBlock();
 	registerBlockType( blockName, block );
 } );
 
