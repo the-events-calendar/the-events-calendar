1da5b27a76b64f195a223c1c027ea8541141b502
eugenekyale <eugene.kyale@gmail.com>
2021-11-17 18:00:04 +0300

    disable the disable_event_search setting only on admin pages

--- src/Tribe/Views/V2/View.php
+++ src/Tribe/Views/V2/View.php
@@ -1498,7 +1498,7 @@ class View implements View_Interface {
 			'before_events'        => tribe( Advanced_Display::class )->get_before_events_html( $this ),
 			'after_events'         => tribe( Advanced_Display::class )->get_after_events_html( $this ),
 			'display_events_bar'   => $this->filter_display_events_bar( $this->display_events_bar ),
-			'disable_event_search' => tribe_is_truthy( tribe_get_option( 'tribeDisableTribeBar', false ) ),
+			'disable_event_search' => is_admin() ? false : tribe_is_truthy( tribe_get_option( 'tribeDisableTribeBar', false ) ),
 			'live_refresh'         => tribe_is_truthy( 'automatic' === tribe_get_option( 'liveFiltersUpdate', 'automatic' ) ),
 			'ical'                 => $this->get_ical_data(),
 			'container_classes'    => $this->get_html_classes(),
