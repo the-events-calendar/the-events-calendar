a35329fb3276a439ac8fbe9df67c5d4acf2b56b7
Camwyn <stephen@theeventscalendar.com>
2021-12-17 14:13:41 -0500

    Cleaner fix

--- src/views/blocks/event-links.php
+++ src/views/blocks/event-links.php
@@ -20,52 +20,50 @@ if ( post_password_required() ) {
 
 $has_google_cal = $this->attr( 'hasGoogleCalendar' );
 $has_ical       = $this->attr( 'hasiCal' );
-$is_backend = defined('REST_REQUEST') && true === REST_REQUEST && 'edit' === filter_input( INPUT_GET, 'context', FILTER_SANITIZE_STRING );
+$should_render  = $has_google_cal || $has_ical;
 
-$should_render = ! $is_backend && ( $has_google_cal || $has_ical );
+if ( ! $should_render ) {
+	return;
+}
 
-if ( $should_render ) :
+remove_filter( 'the_content', 'do_blocks', 9 );
+$subscribe_links = empty( $this->context['subscribe_links'] ) ? false : $this->context['subscribe_links'];
 
-	remove_filter( 'the_content', 'do_blocks', 9 );
-	$subscribe_links = empty( $this->context['subscribe_links'] ) ? false : $this->context['subscribe_links'];
+if ( $has_google_cal ) {
+	$google_cal_link = $subscribe_links ? $subscribe_links[ 'gcal' ]->get_uri( null ) : Tribe__Events__Main::instance()->esc_gcal_url( tribe_get_gcal_link() );
+}
 
-	if ( $has_google_cal ) {
-		$google_cal_link = $subscribe_links ? $subscribe_links[ 'gcal' ]->get_uri( null ) : Tribe__Events__Main::instance()->esc_gcal_url( tribe_get_gcal_link() );
-	}
-
-	if ( $has_ical ) {
-		$ical_link = $subscribe_links ? $subscribe_links[ 'ical' ]->get_uri( null ) : tribe_get_single_ical_link();
-	}
+if ( $has_ical ) {
+	$ical_link = $subscribe_links ? $subscribe_links[ 'ical' ]->get_uri( null ) : tribe_get_single_ical_link();
+}
 
 ?>
-	<div class="tribe-block tribe-block__events-link">
-		<?php if ( $has_google_cal ) : ?>
-			<div class="tribe-block__btn--link tribe-block__events-gcal">
-				<a
-					href="<?php echo esc_url( $google_cal_link ); ?>"
-					target="_blank"
-					rel="noopener noreferrer nofollow"
-					title="<?php esc_attr_e( 'Add to Google Calendar', 'the-events-calendar' ); ?>"
-				>
-					<img src="<?php echo Tribe__Main::instance()->plugin_url  . 'src/modules/icons/link.svg'; ?>" />
-					<?php echo esc_html( $this->attr( 'googleCalendarLabel' ) ) ?>
-				</a>
-			</div>
-		<?php endif; ?>
-		<?php if ( $has_ical ) : ?>
-			<div class="tribe-block__btn--link tribe-block__-events-ical">
-				<a
-					href="<?php echo esc_url( $ical_link ); ?>"
-					rel="noopener noreferrer nofollow"
-					title="<?php esc_attr_e( 'Add to iCalendar', 'the-events-calendar' ); ?>"
-				>
-					<img src="<?php echo Tribe__Main::instance()->plugin_url  . 'src/modules/icons/link.svg'; ?>" />
-					<?php echo esc_html( $this->attr( 'iCalLabel' ) ) ?>
-				</a>
-			</div>
-		<?php endif; ?>
-	</div>
+<div class="tribe-block tribe-block__events-link">
+	<?php if ( $has_google_cal ) : ?>
+		<div class="tribe-block__btn--link tribe-block__events-gcal">
+			<a
+				href="<?php echo esc_url( $google_cal_link ); ?>"
+				target="_blank"
+				rel="noopener noreferrer nofollow"
+				title="<?php esc_attr_e( 'Add to Google Calendar', 'the-events-calendar' ); ?>"
+			>
+				<img src="<?php echo Tribe__Main::instance()->plugin_url  . 'src/modules/icons/link.svg'; ?>" />
+				<?php echo esc_html( $this->attr( 'googleCalendarLabel' ) ) ?>
+			</a>
+		</div>
+	<?php endif; ?>
+	<?php if ( $has_ical ) : ?>
+		<div class="tribe-block__btn--link tribe-block__-events-ical">
+			<a
+				href="<?php echo esc_url( $ical_link ); ?>"
+				rel="noopener noreferrer nofollow"
+				title="<?php esc_attr_e( 'Add to iCalendar', 'the-events-calendar' ); ?>"
+			>
+				<img src="<?php echo Tribe__Main::instance()->plugin_url  . 'src/modules/icons/link.svg'; ?>" />
+				<?php echo esc_html( $this->attr( 'iCalLabel' ) ) ?>
+			</a>
+		</div>
+	<?php endif; ?>
+</div>
 
-	<?php add_filter( 'the_content', 'do_blocks', 9 );
-
-endif;
+<?php add_filter( 'the_content', 'do_blocks', 9 );
