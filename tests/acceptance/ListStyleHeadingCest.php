c1869393a44ab95a704fc15e23078e350b0d95bc
Camwyn <stephen@theeventscalendar.com>
2021-12-24 10:32:43 -0500

    Change the acceptance tests to space the events in the 1-hour default, so it only breaks on one day of the year.

--- tests/acceptance/ListStyleHeadingCest.php
+++ tests/acceptance/ListStyleHeadingCest.php
@@ -22,7 +22,7 @@ class ListStyleHeadingCest {
 	}
 
 	/**
-	 * should render only the now label when all the events are happening o
+	 * should render only the now label when all the events are happening today
 	 *
 	 * @test
 	 */
@@ -43,8 +43,8 @@ class ListStyleHeadingCest {
 	 * @test
 	 */
 	public function i_do_see_the_dates_header_on_the_first_page_when_there_are_more_than_one_page_of_results( AcceptanceTester $I ) {
-		$ids                           = $I->haveManyEventsInDatabase( 14, [ 'when' => 'tomorrow' ], 24 );
-		$last_occurrence_of_first_page = $ids[11]; // 12 event 0 based index.
+		$ids                           = $I->haveManyEventsInDatabase( 14, [ 'when' => 'tomorrow' ] );
+		$last_occurrence_of_first_page = $ids[11]; // 12th event, 0 based index.
 		$date                          = tribe_get_start_date( $last_occurrence_of_first_page, false, tribe_get_date_option( 'dateWithoutYearFormat', 'F j' ) );
 
 		$I->amOnPage( '/events/list/' );
@@ -63,8 +63,8 @@ class ListStyleHeadingCest {
 	 * @test
 	 */
 	public function should_render_the_onwards_label_on_the_last_page_of_results( AcceptanceTester $I ) {
-		$ids                           = $I->haveManyEventsInDatabase( 14, [ 'when' => 'tomorrow' ], 24 );
-		$last_occurrence_of_first_page = $ids[12]; // 13 event
+		$ids                           = $I->haveManyEventsInDatabase( 14, [ 'when' => 'tomorrow' ] );
+		$last_occurrence_of_first_page = $ids[12]; // 13th event
 		$date                          = tribe_get_start_date( $last_occurrence_of_first_page, false, tribe_get_date_option( 'dateWithoutYearFormat', 'F j' ) );
 
 		$I->amOnPage( '/events/list/page/2/' );
@@ -81,7 +81,7 @@ class ListStyleHeadingCest {
 	 * @test
 	 */
 	public function only_has_a_single_page_of_events_and_filters_by_date( AcceptanceTester $I ) {
-		$ids                           = $I->haveManyEventsInDatabase( 10, [ 'when' => 'tomorrow' ], 24 );
+		$ids                           = $I->haveManyEventsInDatabase( 10, [ 'when' => 'tomorrow' ] );
 		$last_occurrence_of_first_page = reset( $ids );
 		$date                          = tribe_get_start_date( $last_occurrence_of_first_page, false, tribe_get_date_option( 'dateWithoutYearFormat', 'F j' ) );
 
@@ -99,8 +99,8 @@ class ListStyleHeadingCest {
 	 * @test
 	 */
 	public function renders_the_dates_ranges_if_there_are_multiple_pages_of_events_with_date_filtering( AcceptanceTester $I ) {
-		$ids                           = $I->haveManyEventsInDatabase( 14, [ 'when' => 'tomorrow' ], 24 );
-		$last_occurrence_of_first_page = $ids[11]; // Last element on a 12 events per page setup.
+		$ids                           = $I->haveManyEventsInDatabase( 14, [ 'when' => 'tomorrow' ] );
+		$last_occurrence_of_first_page = $ids[11]; // Last element on the first page of a 12 events per page setup.
 		$format                        = tribe_get_date_option( 'dateWithoutYearFormat', 'F j' );
 		$start_date                    = tribe_get_start_date( $ids[0], false, $format );
 		$end_date                      = tribe_get_start_date( $last_occurrence_of_first_page, false, $format );
@@ -120,7 +120,7 @@ class ListStyleHeadingCest {
 	 * @test
 	 */
 	public function display_onwards_when_visiting_the_last_page_of_events_and_the_date_picker_has_been_used( AcceptanceTester $I ) {
-		$ids                          = $I->haveManyEventsInDatabase( 14, [ 'when' => 'tomorrow' ], 24 );
+		$ids                          = $I->haveManyEventsInDatabase( 14, [ 'when' => 'tomorrow' ] );
 		$first_occurrence_second_page = $ids[12]; // First element on the second page of events
 		$format                       = tribe_get_date_option( 'dateWithoutYearFormat', 'F j' );
 		$start_date                   = tribe_get_start_date( $first_occurrence_second_page, false, $format );
