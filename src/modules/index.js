8dbf03e4a46d3d90e83ce2f0021fd206c7ae2fd7
Paul <kim.paul.ms@gmail.com>
2022-01-21 20:58:21 +0800

    add entry point for main js, update asset enqueuing

---
+++ src/modules/index.js
@@ -0,0 +1,15 @@
+import * as blocks from './blocks';
+import * as data from './data';
+import * as editor from './editor';
+import * as elements from './elements';
+import * as hoc from './hoc';
+import * as icons from './icons';
+
+export {
+	blocks,
+	data,
+	editor,
+	elements,
+	hoc,
+	icons,
+};
