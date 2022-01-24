998ec2df5bc4f6261064997d6835761f93cfcd2a
Eugene Kyale <eugene.kyale@gmail.com>
2022-01-24 15:56:06 +0300

    enqueue assets and styling prep

--- src/Tribe/Assets.php
+++ src/Tribe/Assets.php
@@ -393,6 +393,20 @@ class Tribe__Events__Assets {
 				],
 			]
 		);
+
+		tribe_assets(
+			$plugin,
+			[
+				[
+					'tec-admin-filterbar-upsell',
+					'tec-admin-filterbar-upsell.css',
+				],
+			],
+			'admin_enqueue_scripts',
+			[
+				'conditionals' => [ $this, 'should_enqueue_admin' ],
+			]
+		);
 	}
 
 	/**
