4d9e1b2700ec6439071bb9b338663b469c1d515f
Gustavo Bordoni <bordoni.dev@gmail.com>
2022-01-24 11:35:00 -0500

    Add CSS

--- src/views/blocks/archive-events.php
+++ src/views/blocks/archive-events.php
@@ -7,12 +7,16 @@
  *
  * See more documentation about our views templating system.
  *
- * @link http://evnt.is/1aiy
+ * @link    http://evnt.is/1aiy
  *
  * @version 5.13.0
  */
 
+use Tribe\Events\Views\V2\Assets as Event_Assets;
 use Tribe\Events\Views\V2\Template_Bootstrap;
+
+tribe_asset_enqueue_group( Event_Assets::$group_key );
+tribe_asset_enqueue( 'tec-events-iframe-content-resizer' );
 ?>
 
 <div class="tribe-block tribe-block__archive-events">
