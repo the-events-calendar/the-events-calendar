b3cf4a41ee096777661cc8b51b4600417b297116
Eugene Kyale <eugene.kyale@gmail.com>
2022-01-24 15:04:20 +0300

    initial FB upsell

---
+++ src/admin-views/filterbar/banners/filterbar-upsell.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Filter bar upsell banner.
+ * 
+ * @since TBD
+ */
+?>
+<div class="tec-filterbar-upsell">
+    <div class="tec-filterbar-upsell__content">
+
+    </div>
+
+    <div class="tec-filterbar-upsell__icon">
+        
+    </div>
+</div>
