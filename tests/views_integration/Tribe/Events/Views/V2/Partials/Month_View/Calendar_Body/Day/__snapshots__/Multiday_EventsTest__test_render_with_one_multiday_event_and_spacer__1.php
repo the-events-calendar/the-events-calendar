58cb6d84a27c6415708be9ce9536863e899002f6
Camwyn <stephen@theeventscalendar.com>
2022-02-01 13:29:45 -0500

    update snapshots

--- tests/views_integration/Tribe/Events/Views/V2/Partials/Month_View/Calendar_Body/Day/__snapshots__/Multiday_EventsTest__test_render_with_one_multiday_event_and_spacer__1.php
+++ tests/views_integration/Tribe/Events/Views/V2/Partials/Month_View/Calendar_Body/Day/__snapshots__/Multiday_EventsTest__test_render_with_one_multiday_event_and_spacer__1.php
@@ -1,7 +1,7 @@
 <?php return '
 	<div class="tribe-events-calendar-month__multiday-event-wrapper tribe-events-calendar-month__multiday-event--empty"></div>
 	<div class="tribe-events-calendar-month__multiday-event-wrapper">
-	<article  class="tribe-events-calendar-month__multiday-event post-8 tribe_events type-tribe_events status-publish hentry entry"  data-event-id="8">
+	<article  class="tribe-events-calendar-month__multiday-event post-8 tribe_events type-tribe_events status-publish hentry"  data-event-id="8">
 		<div class="tribe-events-calendar-month__multiday-event-hidden">
 	<time
 	datetime="2019-06-20"
