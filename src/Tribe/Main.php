94a328c02d0243475fe87099ff16f3abb147cdef
Gustavo Bordoni <bordoni.dev@gmail.com>
2022-02-15 15:32:20 -0500

    Fix the problem and tag release

--- src/Tribe/Main.php
+++ src/Tribe/Main.php
@@ -32,7 +32,7 @@ if ( ! class_exists( 'Tribe__Events__Main' ) ) {
 		const VENUE_POST_TYPE     = 'tribe_venue';
 		const ORGANIZER_POST_TYPE = 'tribe_organizer';
 
-		const VERSION             = '5.14.0';
+		const VERSION             = '5.14.0.1';
 
 		/**
 		 * Min Pro Addon
