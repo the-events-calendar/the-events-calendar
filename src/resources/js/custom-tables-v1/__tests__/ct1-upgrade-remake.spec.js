94180f21ae41f9b9660189cad5d6a4da92e0f6f8
Luca Tumedei <luca@theaveragedev.com>
2022-03-01 17:08:18 +0100

    test(CT1/js) iterate more on AJAX test
    Something is not working in the code, but it's a good start.

--- src/resources/js/custom-tables-v1/__tests__/ct1-upgrade-remake.spec.js
+++ src/resources/js/custom-tables-v1/__tests__/ct1-upgrade-remake.spec.js
@@ -14,19 +14,18 @@ import {
 
 const upgradeBoxId = selectors.upgradeBox.substr(1);
 
-const open = jest.fn();
-const send = jest.fn();
-const mockXMLHttpRequest = () => ({
-	open: open,
-	send: send,
-});
+const mockOpen = jest.fn();
+const mockSend = jest.fn();
+
+const mockXMLHttpRequest = (mocks) => {
+	window.XMLHttpRequest = jest.fn().mockImplementation(() => mocks);
+};
 
 describe('CT1 Upgrade UI', () => {
 	// Replace setTimeout to control it.
 	jest.useFakeTimers();
 	jest.spyOn(global, 'setTimeout');
 	// Replace the XMLHttpRequest built-in class to intercept requests.
-	window.XMLHttpRequest = jest.fn().mockImplementation(mockXMLHttpRequest);
 	// Setup the DOM as it should should look.
 	document.body.innerHTML = `<div id="${upgradeBoxId}"></div>`;
 	// Mock the localized data.
@@ -42,5 +41,49 @@ describe('CT1 Upgrade UI', () => {
 		expect(setTimeout).toHaveBeenCalledTimes(1);
 		expect(setTimeout).
 				toHaveBeenLastCalledWith(ajaxGet, 2300, onSuccess, onFailure, onError);
+	});
+
+	describe('ajaxGet', () => {
+		it('should not send request on missing URL', () => {
+			mockXMLHttpRequest({
+				open: mockOpen,
+				send: mockSend,
+			});
+			const callback = jest.fn();
+
+			ajaxGet('', callback, callback, callback);
+
+			expect(mockOpen).not.toHaveBeenCalled();
+			expect(callback).not.toHaveBeenCalled();
+		});
+
+		it('should open and send a request when provided URL', () => {
+			mockXMLHttpRequest({
+				open: mockOpen,
+				send: mockSend,
+			});
+			const callback = jest.fn();
+
+			ajaxGet('/some-url.php', callback, callback, callback);
+
+			expect(mockOpen).toHaveBeenCalledWith('GET', '/some-url.php', true);
+			expect(mockSend).toHaveBeenCalled();
+		});
+
+		it('should call onSuccess on success', () => {
+			const successCallback = jest.fn();
+			const failureCallback = jest.fn();
+			const errorCallback = jest.fn();
+			mockXMLHttpRequest({
+				open: mockOpen,
+				send: mockSend,
+				status: 200,
+				response: 'hello there',
+			});
+
+			ajaxGet('/some-url.php', successCallback, failureCallback, errorCallback);
+
+			expect(successCallback).toHaveBeenCalledWith('hello there');
+		});
 	});
 });
