28b0a2e3a9e9f027c0bc6416d34ee38d21924744
Edwin Daniels <stratease@gmail.com>
2022-03-04 17:03:10 -0800

    Removed unnecessary undo event lock query.

--- src/Events/Custom_Tables/V1/Migration/Events.php
+++ src/Events/Custom_Tables/V1/Migration/Events.php
@@ -48,57 +48,32 @@ class Events {
 	 *
 	 * @since TBD
 	 *
-	 * @param int  $limit             The max number of Event post IDs to return.
-	 * @param bool $has_been_migrated Whether to limit results to only those that have been previously touched by
-	 *                                migration.
+	 * @param int $limit The max number of Event post IDs to return.
 	 *
 	 * @return array<numeric> An array of claimed and locked Event post IDs.
 	 */
-	public function get_ids_to_process( $limit, $has_been_migrated = false ) {
+	public function get_ids_to_process( $limit ) {
 		global $wpdb;
 
 		// Batch locking
 		$batch_uid = uniqid( 'tec_ct1_action', true ); // Should be pretty unique.
 
 		// Atomic query.
-		if ( $has_been_migrated ) { // @todo remove - this was for undo, not needed anymore
-			// Fetch only those that were previously touched
-			$lock_query = "INSERT INTO {$wpdb->postmeta} (post_id, meta_key, meta_value)
-	    SELECT p.ID, %s, %s
-	    FROM {$wpdb->posts} p
-			LEFT JOIN {$wpdb->postmeta} pm ON p.ID = pm.post_id AND pm.meta_key = %s
-			INNER JOIN {$wpdb->postmeta} pm_exists ON p.ID = pm_exists.post_id
-	    WHERE p.post_type = %s AND pm.meta_value IS NULL
-	    	AND pm_exists.meta_key =%s
-	    	AND pm_exists.meta_value IN (%s, %s)
-	    LIMIT %d";
-			$lock_query = $wpdb->prepare( $lock_query,
-				Event_Report::META_KEY_MIGRATION_LOCK_HASH,
-				$batch_uid,
-				Event_Report::META_KEY_MIGRATION_LOCK_HASH,
-				TEC::POSTTYPE,
-				Event_Report::META_KEY_MIGRATION_PHASE,
-				Event_Report::META_VALUE_MIGRATION_PHASE_MIGRATION_SUCCESS,
-				Event_Report::META_VALUE_MIGRATION_PHASE_MIGRATION_FAILURE,
-				$limit
-			);
-		} else {
-			//  Fetch only those that were NOT previously touched.
-			$lock_query = "INSERT INTO {$wpdb->postmeta} (post_id, meta_key, meta_value)
+		// Fetch only those that were NOT previously touched.
+		$lock_query = "INSERT INTO {$wpdb->postmeta} (post_id, meta_key, meta_value)
 	    SELECT p.ID, %s,%s
 	    FROM {$wpdb->posts} p
 			LEFT JOIN {$wpdb->postmeta} pm ON p.ID = pm.post_id AND pm.meta_key IN(%s, %s)
 	    WHERE p.post_type = %s AND pm.meta_value IS NULL
 	    LIMIT %d";
-			$lock_query = $wpdb->prepare( $lock_query,
-				Event_Report::META_KEY_MIGRATION_LOCK_HASH,
-				$batch_uid,
-				Event_Report::META_KEY_MIGRATION_LOCK_HASH,
-				Event_Report::META_KEY_MIGRATION_PHASE,
-				TEC::POSTTYPE,
-				$limit
-			);
-		}
+		$lock_query = $wpdb->prepare( $lock_query,
+			Event_Report::META_KEY_MIGRATION_LOCK_HASH,
+			$batch_uid,
+			Event_Report::META_KEY_MIGRATION_LOCK_HASH,
+			Event_Report::META_KEY_MIGRATION_PHASE,
+			TEC::POSTTYPE,
+			$limit
+		);
 
 		$wpdb->query( $lock_query );
 
