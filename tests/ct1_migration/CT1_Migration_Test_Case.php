0e2f77c96670aa73796514a113b0959f4c449393
Luca Tumedei <luca@theaveragedev.com>
2022-03-07 16:15:32 +0100

    test(ct1_migration) set user to admin
    Since we'll need to create posts during the tests, set the user to the
    first available admin to avoid issues with authorization issues.

--- tests/ct1_migration/CT1_Migration_Test_Case.php
+++ tests/ct1_migration/CT1_Migration_Test_Case.php
@@ -5,9 +5,26 @@ use tad\WPBrowser\Module\WPLoader\FactoryStore;
 class CT1_Migration_Test_Case extends \Codeception\Test\Unit {
 	protected $backupGlobals = false;
 
-	public static function setUpBeforeClass(  ) {
+	public static function setUpBeforeClass() {
 		// This will load all the factories.
 		$factories = new FactoryStore();
-		$factories->getThingFactory( 'post' );
+		if ( ! class_exists( '\\WP_UnitTest_Factory_For_Thing' ) ) {
+			$factories->getThingFactory( 'post' );
+		}
+	}
+
+	/**
+	 * @before
+	 */
+	public function set_user_to_admin() {
+		if ( current_user_can( 'manage_options' ) ) {
+			return;
+		}
+
+		$admin_users = get_users( [ 'role' => 'administrator', 'limit' => 1, 'fields' => 'ids' ] );
+		if ( ! count( $admin_users ) ) {
+			throw new RuntimeException( 'No administrator user found!' );
+		}
+		wp_set_current_user( reset( $admin_users ) );
 	}
 }
