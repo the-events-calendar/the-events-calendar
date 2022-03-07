c0f8bf1c835765996696babb0c626a372b2fc818
Luca Tumedei <luca@theaveragedev.com>
2022-03-07 14:02:26 +0100

    fix(CT1/Migration) load Action Scheduler
    Load Action Scheduler at the last possible moment in the context
    of the Migration code.
    
    The approach is robust enough to have Action Scheduler reliably loaded
    in the context of tests too.

--- src/Events/Custom_Tables/V1/Migration/Provider.php
+++ src/Events/Custom_Tables/V1/Migration/Provider.php
@@ -56,6 +56,8 @@ class Provider extends Service_Provider implements Provider_Contract {
 		$this->container->singleton( Ajax::class, Ajax::class );
 		$this->container->singleton( Asset_Loader::class, Asset_Loader::class );
 
+		$this->load_action_scheduler();
+
 		// Action Scheduler will fire this action: on it we'll migrate, or preview the migration of, an Event.
 		add_action( Process::ACTION_PROCESS, [ $this, 'migrate_event' ], 10, 2 );
 		add_action( Process::ACTION_UNDO, [ $this, 'undo_event_migration' ] );
@@ -238,5 +240,47 @@ class Provider extends Service_Provider implements Provider_Contract {
 	 */
 	public function add_phase_callback( $upgrade_fields ) {
 		return $this->container->make( Upgrade_Tab::class )->add_phase_content( $upgrade_fields );
+	}
+
+	/**
+	 * Loads the Action Scheduler library by loading the main plugin file shipped with
+	 * this plugin.
+	 *
+	 * This method would, usually, run on the `plugins_loaded` action and might, in that
+	 * case, further delay the loading of the Action Scheduler library to the `init` action.
+	 *
+	 * @since TBD
+	 */
+	private function load_action_scheduler() {
+		/*
+		 * We do not sense around for of the functions defined by Action Scheduler by design:
+		 * Action Scheduler will take care of loading the most recent version. If we looked
+		 * for some of Action Scheduler API functions, then, we would let a possibly older
+		 * version load instead of ours just because it did init Action Scheduler before
+		 * this plugin.
+		 */
+		if ( did_action( 'plugins_loaded' ) || doing_action( 'plugins_loaded' ) ) {
+			add_action( 'init', [ $this, 'load_action_scheduler_late' ], - 99999 );
+
+			return;
+		}
+
+		$action_scheduler_file = TEC::instance()->plugin_path . '/vendor/woocommerce/action-scheduler/action-scheduler.php';
+		require_once $action_scheduler_file;
+	}
+
+	/**
+	 * Loads Action Scheduler late, after the `plugins_loaded` hook, at the
+	 * start of the `init` one.
+	 *
+	 * Action Scheduler does support loading after the `plugins_loaded` hook, but
+	 * not during it. The provider will register exactly during the `plugins_loaded`
+	 * action, so we need to retry setting up Action Scheduler again.
+	 *
+	 * @since TBD
+	 */
+	public function load_action_scheduler_late() {
+		$action_scheduler_file = TEC::instance()->plugin_path . '/vendor/woocommerce/action-scheduler/action-scheduler.php';
+		require_once $action_scheduler_file;
 	}
 }
