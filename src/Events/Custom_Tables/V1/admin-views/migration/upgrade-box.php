1c16215f9ea308fb64adac27842bc32c378279f4
Edwin Daniels <stratease@gmail.com>
2022-03-10 16:34:02 -0800

    Replacd translation w text dictionary

--- src/Events/Custom_Tables/V1/admin-views/migration/upgrade-box.php
+++ src/Events/Custom_Tables/V1/admin-views/migration/upgrade-box.php
@@ -8,17 +8,17 @@
  * @var string $template_path The absolute path to the Migration template root directory.
  */
 use TEC\Events\Custom_Tables\V1\Migration\State;
-// @todo remove
-/*$state = tribe(State::class);
-$state->set('phase', State::PHASE_PREVIEW_PROMPT);
-$state->save();*/
+use TEC\Events\Custom_Tables\V1\Migration\String_Dictionary;
+
+$text = tribe( String_Dictionary::class );
+ 
 ?>
 <div id="tec-ct1-upgrade-box" >
 	<div id="tec-ct1-upgrade-dynamic" class="tec-ct1-upgrade">
 		<?php // @todo Do we want a spinner here? ?>
 		<div class="tec-ct1-upgrade__row">
 			<div class="content-container">
-				<p><?php echo __( 'Loading...', 'the-events-calendar' )?></p>
+				<p><?php echo esc_html( $text->get( 'loading-message' ) ); ?></p>
 			</div>
 		</div>
 	</div>
