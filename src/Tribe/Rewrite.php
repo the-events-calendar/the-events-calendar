d19db040e0c1ca2d47f1dba20f1320d685f9a9e8
shisho585 <47120757+shisho585@users.noreply.github.com>
2022-04-03 21:27:25 +0300

    Switch localized bases to new function
    In the previous commit i created new i18n function called 'get_i18n_url_strings_for_domains' for sanitizing translated strings as url's instead of as key's.
    In this commit i just replace the old function in the new one in get_localized_bases function.

--- src/Tribe/Rewrite.php
+++ src/Tribe/Rewrite.php
@@ -740,7 +740,7 @@ class Tribe__Events__Rewrite extends Tribe__Rewrite {
 
 		$flags           = I18n::COMPILE_STRTOLOWER;
 		$localized_bases = tribe( 'tec.i18n' )
-			->get_i18n_strings_for_domains( $bases, [ $locale ], $domains, $flags );
+			->get_i18n_url_strings_for_domains( $bases, [ $locale ], $domains, $flags );
 
 		$return = array_filter(
 			array_map(
