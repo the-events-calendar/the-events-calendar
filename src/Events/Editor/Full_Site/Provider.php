9a1af4eecf69851aa534cafc599ab046207f1593
Camwyn <stephen@theeventscalendar.com>
2022-04-21 11:52:16 -0400

    Address CR comments

--- src/Events/Editor/Full_Site/Provider.php
+++ src/Events/Editor/Full_Site/Provider.php
@@ -28,10 +28,10 @@ class Provider extends \tad_DI52_ServiceProvider {
 		$this->register_singletons();
 
 		// Register the Service Provider for Hooks.
-		$this->register_hooks();
+		$this->container->register(Hooks::class);
 
 		// Register the Service Provider for Assets.
-		$this->register_assets();
+		$this->container->register( Assets::class );
 
 		// Register the service provider itself on the container.
 		$this->container->singleton( static::class, $this );
@@ -39,29 +39,5 @@ class Provider extends \tad_DI52_ServiceProvider {
 
 	protected function register_singletons() {
 		$this->container->singleton( Archive_Events::class, Archive_Events::class, [ 'load' ] );
-	}
-
-	/**
-	 * Registers the provider handling all the 1st level filters and actions for this Service Provider
-	 *
-	 * @since TBD
-	 */
-	protected function register_assets() {
-		$assets = $this->container->register(Assets::class);
-		$this->container->singleton( Assets::class, $assets );
-	}
-
-	/**
-	 * Registers the provider handling all the 1st level filters and actions for this Service Provider.
-	 *
-	 * @since TBD
-	 */
-	protected function register_hooks() {
-		$hooks = new Hooks( $this->container );
-		$hooks->register();
-
-		// Allow Hooks to be removed, by having the them registered to the container
-		$this->container->singleton( Hooks::class, $hooks );
-		$this->container->singleton( 'events.editor.full-site.hooks', $hooks );
 	}
 }
