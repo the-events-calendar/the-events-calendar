a84c821ec58e349fff937418e9606118d6b04f4b
Luca Tumedei <luca@theaveragedev.com>
2022-04-27 13:38:53 +0200

    test(ct1_integration) do not activate ECP in TEC tests

--- tests/ct1_integration/_bootstrap.php
+++ tests/ct1_integration/_bootstrap.php
@@ -7,6 +7,5 @@ putenv( 'TEC_CUSTOM_TABLES_V1_DISABLED=0' );
 $_ENV['TEC_CUSTOM_TABLES_V1_DISABLED'] = 0;
 add_filter( 'tec_events_custom_tables_v1_enabled', '__return_true' );
 tribe()->register( TEC\Events\Custom_Tables\V1\Provider::class );
-tribe()->register( TEC\Events_Pro\Custom_Tables\V1\Provider::class );
 // Run the activation routine to ensure the tables will be set up independently of the previous state.
 Activation::activate();
