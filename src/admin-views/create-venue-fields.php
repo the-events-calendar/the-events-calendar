5c96c76fd221fa098fabab61e63f7550cdb2d025
Camwyn <stephen@theeventscalendar.com>
2022-04-27 10:41:09 -0400

    Use get_venue_link so we don't get passed a bunch of HTML when we expect a URL.

--- src/admin-views/create-venue-fields.php
+++ src/admin-views/create-venue-fields.php
@@ -24,7 +24,7 @@ if ( ! $_POST && is_admin() ) {
 
 	} else {
 		$_VenuePhone            = tribe_get_phone();
-		$_VenueURL              = strip_tags( tribe_get_venue_website_link( null, null ) );
+		$_VenueURL              = strip_tags( tribe_get_venue_link( null, false ) );
 		$_VenueAddress          = tribe_get_address();
 		$_VenueCity             = tribe_get_city();
 		$_VenueProvince         = tribe_get_province();
