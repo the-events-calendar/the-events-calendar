90b2f3ccaca3756e7c9c3f0add9ade48d3a47960
Luca Tumedei <luca@theaveragedev.com>
2022-04-27 14:22:10 +0200

    tests(ct1_migration) register table schemas before activation

--- tests/ct1_migration/_bootstrap.php
+++ tests/ct1_migration/_bootstrap.php
@@ -67,6 +67,8 @@ addListener( Codeception\Events::SUITE_BEFORE, static function () {
 	// Ensure the CT1 code branch is enabled.
 	putenv( 'TEC_CUSTOM_TABLES_V1_DISABLED=0' );
 	$_ENV['TEC_CUSTOM_TABLES_V1_DISABLED'] = 0;
+	add_filter( 'tec_events_custom_tables_v1_enabled', '__return_true' );
+	tribe()->register( TEC\Events\Custom_Tables\V1\Provider::class );
 	// Run the activation routine to ensure the tables will be set up independently of the previous state.
 	Activation::activate();
 } );
