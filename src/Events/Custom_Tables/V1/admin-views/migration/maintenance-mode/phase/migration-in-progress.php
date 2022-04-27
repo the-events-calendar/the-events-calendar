2cb6542cca20ef91c374720611b612bba29b7e6a
Edwin Daniels <stratease@gmail.com>
2022-04-26 17:38:02 -0700

    Maintenance mode template directory.

---
+++ src/Events/Custom_Tables/V1/admin-views/migration/maintenance-mode/phase/migration-in-progress.php
@@ -0,0 +1,48 @@
+<?php
+use TEC\Events\Custom_Tables\V1\Migration\String_Dictionary;
+
+/**
+ * @var string            $template_directory The absolute path to the Migration template root directory.
+ * @var String_Dictionary $text               The text dictionary.
+ */
+?>
+<div class="tec-ct1-upgrade__row">
+	<div class="content-container">
+		<h3>
+			<?php include $template_directory . '/upgrade-logo.php'; ?>
+			<?php echo esc_html( $text->get( 'migration-in-progress' ) ); ?>
+		</h3>
+
+		<p>
+			<?php
+			echo sprintf(
+					$text->get( 'migration-in-progress-paragraph' ),
+					'<strong>',
+					'</strong>'
+			);
+
+			if ( $addendum = $text->get( 'migration-prompt-plugin-state-addendum' ) ) {
+				?>
+				<strong><?php echo esc_html( $addendum ); ?></strong>
+				<?php
+			}
+			?>
+			<a href="<?php echo esc_url( $text->get( 'learn-more-button-url' ) ); ?>" target="_blank" rel="noopener">
+				<strong><?php echo esc_html( $text->get( 'learn-more-button' ) ); ?></strong>
+			</a>
+		</p>
+
+		<div class="tec-ct1-upgrade-update-bar-container">
+			<p><?php echo esc_html( $text->get( 'loading-message' ) ); ?></p>
+		</div>
+		<div>
+			<a href="#"
+			   class="tec-ct1-action-container tec-ct1-upgrade-cancel-migration tec-ct1-upgrade__link-danger"><?php echo esc_html( $text->get( 'cancel-migration-button' ) ); ?></a>
+		</div>
+	</div>
+	<div class="image-container">
+		<img class="screenshot"
+			 src="<?php echo esc_url( $text->get( 'completed-screenshot-url' ) ); ?>"
+			 alt="<?php echo esc_attr( $text->get( 'updated-views-screenshot-alt' ) ); ?>"/>
+	</div>
+</div>
---
+++ src/Events/Custom_Tables/V1/admin-views/migration/maintenance-mode/phase/migration-prompt.php
@@ -0,0 +1,104 @@
+<?php
+
+use TEC\Events\Custom_Tables\V1\Migration\Reports\Site_Report;
+use TEC\Events\Custom_Tables\V1\Migration\String_Dictionary;
+
+/**
+ * @var string            $template_directory The absolute path to the Migration template root directory.
+ * @var Site_Report       $report             The report details.
+ * @var String_Dictionary $text               Our text dictionary.
+ */
+?>
+<div class="tec-ct1-upgrade__row">
+	<div class="content-container">
+		<h3>
+			<?php
+			include $template_directory . '/upgrade-logo.php';
+			?>
+			<?php echo esc_html( $text->get( 'preview-complete' ) ); ?>
+		</h3>
+
+		<p>
+			<?php
+			echo sprintf(
+					esc_html( $text->get( 'preview-complete-paragraph' ) ),
+					'<a href="https://evnt.is/2n" rel="noopener" target="_blank">',
+					'</a>'
+			);
+			?>
+		</p>
+
+		<p class="tec-ct1-upgrade__alert">
+			<i class="tec-ct1-upgrade__alert-icon">!</i>
+			<span>
+			<?php
+			if ( $report->has_errors ) {
+				echo esc_html( $text->get( 'migration-is-blocked' ) );
+			} else {
+				echo sprintf(
+						esc_html( $text->get( 'preview-estimate' ) ),
+						'<strong>',
+						'</strong>',
+						$report->estimated_time_in_minutes
+				);
+
+				if ( $addendum = $text->get( 'migration-prompt-plugin-state-addendum' ) ) {
+					?>
+					<strong><?php echo esc_html( $addendum ); ?></strong>
+					<?php
+				}
+			}
+			?>
+			</span>
+			<a href="<?php echo esc_url( $text->get( 'learn-more-button-url' ) ); ?>" target="_blank" rel="noopener">
+				<strong><?php echo esc_html( $text->get( 'learn-more-button' ) ); ?></strong>
+			</a>
+		</p>
+	</div>
+
+	<div class="image-container">
+		<img class="screenshot"
+			 src="<?php echo esc_url( $text->get( 'completed-screenshot-url' ) ); ?>"
+			 alt="<?php echo esc_attr( $text->get( 'updated-views-screenshot-alt' ) ); ?>"/>
+	</div>
+</div>
+
+<div class="tec-ct1-upgrade__row">
+	<?php
+	$datetime_heading = $text->get( 'previewed-date-heading' );
+	$total_heading    = $text->get( 'previewed-total-heading' );
+	ob_start();
+	?>
+	<em title="<?php esc_attr( $text->get( 're-run-preview-button' ) ) ?>">
+		<?php include TEC_CUSTOM_TABLES_V1_ROOT . '/admin-views/migration/icons/rerun.php'; ?>
+	</em>
+	<a class="tec-ct1-upgrade-start-migration-preview"
+	   href="#"><?php echo esc_html( $text->get( 're-run-preview-button' ) ); ?></a>
+	<?php
+	$heading_action = ob_get_clean();
+	include_once __DIR__ . '/report.php';
+	?>
+</div>
+
+<div class="tec-ct1-upgrade__row tec-ct1-action-container">
+	<div class="content-container">
+		<button class="tec-ct1-upgrade-start-migration"
+				<?php
+				if ( $report->has_errors ) { // @todo Do we want to block migration like this?
+					?>
+					disabled="disabled"
+				<?php } ?>
+				type="button"><?php echo esc_html( $text->get( 'start-migration-button' ) ); ?></button>
+		<i>
+			<?php
+			if ( $report->estimated_time_in_minutes === 1 ) {
+				$message = esc_html( $text->get( 'estimated-time-singular' ) );
+			} else {
+				$message = esc_html( $text->get( 'estimated-time-plural' ) );
+			}
+
+			echo sprintf( $message, $report->estimated_time_in_minutes );
+			?>
+		</i>
+	</div>
+</div>
