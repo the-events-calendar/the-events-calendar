77a052f832d76ea66e7f587df960995678e5c34e
Edwin Daniels <stratease@gmail.com>
2022-05-04 12:17:07 -0700

    Fix preview report. Now preview report will only show errors if there are errors.

--- src/Events/Custom_Tables/V1/Migration/Ajax.php
+++ src/Events/Custom_Tables/V1/Migration/Ajax.php
@@ -173,12 +173,16 @@ class Ajax {
 		switch ( $phase ) {
 			case State::PHASE_MIGRATION_COMPLETE:
 			case State::PHASE_MIGRATION_PROMPT:
+				$event_reports = $site_report->get_event_reports( $page, $count, Event_Report::META_VALUE_MIGRATION_PHASE_MIGRATION_FAILURE );
+				if ( ! count( $event_reports ) ) {
+					$site_report->get_event_reports( $page, $count );
+				}
 				$renderer = new Phase_View_Renderer( $phase,
 					"$base_dir/$phase.php",
 					[
 						'state'         => tribe( State::class ),
 						'report'        => $site_report,
-						'event_reports' => $site_report->get_event_reports( $page, $count ),
+						'event_reports' => $event_reports,
 						'text'          => tribe( String_Dictionary::class )
 					]
 				);
