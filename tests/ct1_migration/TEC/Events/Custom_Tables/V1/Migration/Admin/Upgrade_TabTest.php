4148d904ad47d3a67cb0be15d283b1bc24ff0ab5
Edwin Daniels <stratease@gmail.com>
2022-05-09 17:54:43 -0700

    Added missing 30 day upgrade tab expiration. Added test to validate the expiration of the tab.

--- tests/ct1_migration/TEC/Events/Custom_Tables/V1/Migration/Admin/Upgrade_TabTest.php
+++ tests/ct1_migration/TEC/Events/Custom_Tables/V1/Migration/Admin/Upgrade_TabTest.php
@@ -79,7 +79,7 @@ class Upgrade_TabTest extends \CT1_Migration_Test_Case {
 	 *
 	 * @test
 	 */
-	public function should_not_show_if_migration_is_not_required_on_first_check() {
+	public function should_show_if_migration_is_not_required_on_first_check() {
 		$this->given_the_current_migration_phase_is( null );
 		$events = new Events();
 		$this->assertEquals( 0, $events->get_total_events() );
@@ -96,9 +96,38 @@ class Upgrade_TabTest extends \CT1_Migration_Test_Case {
 	 *
 	 * @test
 	 */
-	public function should_() {
+	public function should_not_show_if_migration_is_not_required_on_second_check() {
 		$this->given_the_current_migration_phase_is( State::PHASE_MIGRATION_NOT_REQUIRED );
 		$state = new State( new Events() );
+
+		$tab         = new Upgrade_Tab( $state );
+		$should_show = $tab->should_show();
+
+		$this->assertFalse( $should_show );
+	}
+
+	/**
+	 * It should not show if migration complete and the tab duration has expired.
+	 *
+	 * @test
+	 */
+	public function should_not_show_if_migration_complete_and_expired() {
+		$this->given_the_current_migration_phase_is( State::PHASE_MIGRATION_COMPLETE );
+		$state = new State( new Events() );
+
+		// Should show if 30 days + 1 minute (just over 30 days) have passed.
+		$days_in_seconds = ( 30 * 24 * 60 * 60 ) - 60;
+		$state->set( 'complete_timestamp', time() - $days_in_seconds );
+		$state->save();
+
+		$tab         = new Upgrade_Tab( $state );
+		$should_show = $tab->should_show();
+		$this->assertTrue( $should_show );
+
+		// Should not hide if 30 days - 1 minute (just under 30 days) have passed.
+		$days_in_seconds = ( 30 * 24 * 60 * 60 ) + 60;
+		$state->set( 'complete_timestamp', time() - $days_in_seconds );
+		$state->save();
 
 		$tab         = new Upgrade_Tab( $state );
 		$should_show = $tab->should_show();
