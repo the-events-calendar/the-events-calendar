1429d1b0d0e066c6645a8e0e558c000dc7867db0
Juan Aldasoro <juanfraa@gmail.com>
2022-05-19 14:44:12 +0200

    Update Plugin_Register.php

--- src/Tribe/Plugin_Register.php
+++ src/Tribe/Plugin_Register.php
@@ -8,7 +8,6 @@ class Tribe__Events__Plugin_Register extends Tribe__Abstract_Plugin_Register {
 		'addon-dependencies' => [
 			'Tribe__Events__Pro__Main'                 => '5.14.0-dev',
 			'Tribe__Events__Filterbar__View'           => '5.3.0-dev',
-			'Tribe__Events__Tickets__Eventbrite__Main' => '4.6-dev',
 			'Tribe__Events__Community__Main'           => '4.9.0-dev',
 			'Tribe__Events__Community__Tickets__Main'  => '4.7.2-dev',
 			'Tribe__Events__Tickets__Eventbrite__Main' => '4.6.11-dev',
