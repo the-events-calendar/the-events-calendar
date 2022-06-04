4d51c0f40f5720518ce9287832842e5245df1d40
Gustavo Bordoni <bordoni.dev@gmail.com>
2022-06-03 22:45:39 -0400

    Modified the tribe_events_the_header_attributes to fix the problem around single view

--- src/functions/template-tags/deprecated.php
+++ src/functions/template-tags/deprecated.php
@@ -5,20 +5,6 @@
  *
  */
 
-/**
- * Prints out data attributes used in the template header tags
- *
- * @deprecated 6.0.0 No longer used by templates / views.
- *
- * @param string|null $current_view
- *
- **@category Events
- */
-function tribe_events_the_header_attributes( $current_view = null ) {
-	_deprecated_function( __FUNCTION__, '6.0.0' );
-	return;
-}
-
 if ( ! function_exists( 'tribe_get_next_day_date' ) ) {
 	/**
 	 * Get the next day's date
