a25aec3f271ce07e879c0d103685511289d164b3
Luca Tumedei <luca@theaveragedev.com>
2022-06-13 11:05:49 -0700

    fix(CT1) do not start migration during preview
    The migration should not start if the migration process is in progress
    **and** if the preview process is in progress.

--- src/Events/Custom_Tables/V1/Migration/Process.php
+++ src/Events/Custom_Tables/V1/Migration/Process.php
@@ -62,8 +62,9 @@ class Process {
 	public function start( $dry_run = true ) {
 		// Check if we are already doing this action?
 		if ( in_array( $this->state->get_phase(), [
-			State::PHASE_MIGRATION_IN_PROGRESS
-		] ) ) {
+			State::PHASE_MIGRATION_IN_PROGRESS,
+			State::PHASE_PREVIEW_IN_PROGRESS,
+		], true ) ) {
 			return false;
 		}
 		// Reset our undo state.
