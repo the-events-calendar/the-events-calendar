e9130b385ec848559a310147dee7ef6d4f527957
Gustavo Bordoni <bordoni.dev@gmail.com>
2022-06-24 08:53:57 -0400

    Version required correctly for CE

--- src/Tribe/Plugin_Register.php
+++ src/Tribe/Plugin_Register.php
@@ -8,7 +8,7 @@ class Tribe__Events__Plugin_Register extends Tribe__Abstract_Plugin_Register {
 		'addon-dependencies' => [
 			'Tribe__Events__Pro__Main'                 => '6.0.0-beta4-dev',
 			'Tribe__Events__Filterbar__View'           => '5.5.0-beta4-dev',
-			'Tribe__Events__Community__Main'           => '4.10.0-beta-3.2-dev',
+			'Tribe__Events__Community__Main'           => '4.10.0-beta4-dev',
 			'Tribe__Events__Community__Tickets__Main'  => '4.7.2-dev',
 			// @todo @moraleida update the version below to the actual version of ET that will be released to support RBE
 			'Tribe__Tickets__Main'                     => '5.6.0-beta4-dev',
