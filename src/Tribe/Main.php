fca718e9aa4a0267a2ec9bf5dd9ff4320939450c
Gustavo Bordoni <bordoni.dev@gmail.com>
2022-06-24 08:48:47 -0400

    Version bump to beta4

--- src/Tribe/Main.php
+++ src/Tribe/Main.php
@@ -35,14 +35,14 @@ if ( ! class_exists( 'Tribe__Events__Main' ) ) {
 		const VENUE_POST_TYPE     = 'tribe_venue';
 		const ORGANIZER_POST_TYPE = 'tribe_organizer';
 
-		const VERSION             = '6.0.0-beta3.2';
+		const VERSION             = '6.0.0-beta4';
 
 		/**
 		 * Min Pro Addon
 		 *
 		 * @deprecated 4.8
 		 */
-		const MIN_ADDON_VERSION   = '6.0.0-beta3.2-dev';
+		const MIN_ADDON_VERSION   = '6.0.0-beta4-dev';
 
 		/**
 		 * Min Common
@@ -72,7 +72,7 @@ if ( ! class_exists( 'Tribe__Events__Main' ) ) {
 		 *
 		 * @since 4.8
 		 */
-		protected $min_et_version = '5.6.0-beta3.2-dev';
+		protected $min_et_version = '5.6.0-beta4-dev';
 
 		/**
 		 * Maybe display data wrapper
--- src/Tribe/Plugin_Register.php
+++ src/Tribe/Plugin_Register.php
@@ -6,13 +6,13 @@ class Tribe__Events__Plugin_Register extends Tribe__Abstract_Plugin_Register {
 	protected $main_class   = 'Tribe__Events__Main';
 	protected $dependencies = [
 		'addon-dependencies' => [
-			'Tribe__Events__Pro__Main'                 => '6.0.0-beta3.2-dev',
-			'Tribe__Events__Filterbar__View'           => '5.5.0-beta3.2-dev',
+			'Tribe__Events__Pro__Main'                 => '6.0.0-beta4-dev',
+			'Tribe__Events__Filterbar__View'           => '5.5.0-beta4-dev',
 			'Tribe__Events__Community__Main'           => '4.10.0-beta-3.2-dev',
 			'Tribe__Events__Community__Tickets__Main'  => '4.7.2-dev',
 			// @todo @moraleida update the version below to the actual version of ET that will be released to support RBE
-			'Tribe__Tickets__Main'                     => '5.6.0-beta3.2-dev',
-			'Tribe__Tickets_Plus__Main'                => '5.7.0-beta3.2-dev',
+			'Tribe__Tickets__Main'                     => '5.6.0-beta4-dev',
+			'Tribe__Tickets_Plus__Main'                => '5.7.0-beta4-dev',
 			'Tribe__Events__Tickets__Eventbrite__Main' => '4.6.11-dev',
 		],
 	];
