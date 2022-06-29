dd69abd115ee9a4f762654977a4d3c360f73e8b6
Camwyn <stephen@theeventscalendar.com>
2022-06-29 12:52:13 -0400

    version bump

--- the-events-calendar.php
+++ the-events-calendar.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: The Events Calendar
  * Description: The Events Calendar is a carefully crafted, extensible plugin that lets you easily share your events. Beautiful. Solid. Awesome.
- * Version: 5.16.1.1
+ * Version: 5.16.2
  * Author: The Events Calendar
  * Author URI: https://evnt.is/1x
  * Text Domain: the-events-calendar
