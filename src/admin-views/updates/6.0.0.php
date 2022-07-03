876d3a940647cbbb8447127e600757a6e1da1813
Gustavo Bordoni <bordoni.dev@gmail.com>
2022-07-03 00:39:24 -0400

    Update Admin Update page for 6.0.0

--- src/admin-views/updates/6.0.0.php
+++ src/admin-views/updates/6.0.0.php
@@ -28,18 +28,21 @@ $common_main = Tribe__Main::instance();
 
 	<?php echo tribe( 'tec.admin.notice.update' )->notice(); ?>
 
-	<div class="tec-update-page-intro">
+	<div class="tec-update-page-block">
 		<h3><?php esc_html_e( "What’s New in The Events Calendar 6.0", 'the-events-calendar' ); ?></h3>
-		<p><?php esc_html_e( "We’re excited to bring all of our users faster event creation and editing in the WordPress dashboard as well as quicker loading times for your website’s calendar. Watch the video or check out the release notes to learn more." ); ?></p>
-		<p><?php esc_html_e( 'Be sure to migrate to the new data storage system so you can take advantage of the improved performance.', 'the-events-calendar' ); ?></p>
+		<div class="tec-update-page-grid">
+			<div class="tec-update-page-grid__item">
+				<iframe src="https://evnt.is/1b8k" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen style="width:100%;height:280px;" title="<?php esc_attr_e( 'Video describing features on The Events Calendar 6.0', 'the-events-calendar' ); ?>"></iframe>
+				<script src="https://player.vimeo.com/api/player.js"></script>
+			</div>
 
-		<div style="padding:56.25% 0 0 0;position:relative;">
-			<iframe src="https://evnt.is/1b8k" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen style="position:absolute;top:0;left:0;width:100%;height:100%;" title="The Events Calendar 6.0"></iframe>
-			<script src="https://player.vimeo.com/api/player.js"></script>
-		</div>
+			<div class="tec-update-page-grid__item">
+				<p><?php esc_html_e( "We’re excited to bring all of our users faster event creation and editing in the WordPress dashboard as well as quicker loading times for your website’s calendar. Watch the video or check out the release notes to learn more." ); ?></p>
+				<p><?php esc_html_e( 'Be sure to migrate to the new data storage system so you can take advantage of the improved performance.', 'the-events-calendar' ); ?></p>
+			</div>
 	</div>
 
-	<div class="tec-update-page-event-series">
+	<div class="tec-update-page-block">
 		<h3><span>⚡</span> <?php esc_html_e( 'New Features for Events Calendar Pro', 'the-events-calendar' ); ?></h3>
 		<div class="tec-update-page-grid">
 			<div class="tec-update-page-grid__item">
@@ -54,11 +57,11 @@ $common_main = Tribe__Main::instance();
 		<div class="tec-update-page-grid">
 			<div class="tec-update-page-grid__item has-media">
 				<img src="<?php echo esc_url( tribe_resource_url( 'update-page-features-2.png', false, 'src/resources/images/', null ) ); ?>" alt="<?php esc_attr_e( 'Event series graphic', 'the-events-calendar' ); ?>" />
-				<p><b><?php esc_html_e( 'Advanced editing options', 'the-events-calendar' ); ?></b> <?php esc_html_e( 'for recurring events give you more possibilities for complex event management.', 'the-events-calendar' ); ?></p>				
+				<p><b><?php esc_html_e( 'Advanced editing options', 'the-events-calendar' ); ?></b> <?php esc_html_e( 'for recurring events give you more possibilities for complex event management.', 'the-events-calendar' ); ?></p>
 			</div>
 			<div class="tec-update-page-grid__item has-media">
 				<img src="<?php echo esc_url( tribe_resource_url( 'update-page-features-3.png', false, 'src/resources/images/', null ) ); ?>" alt="<?php esc_attr_e( 'Event series graphic', 'the-events-calendar' ); ?>" />
-				<p><b><?php esc_html_e( 'With the Duplicate Event Tool', 'the-events-calendar' ); ?></b> <?php esc_html_e( 'you can replicate and modify any events in seconds, making event creation a breeze.', 'the-events-calendar' ); ?></p>				
+				<p><b><?php esc_html_e( 'With the Duplicate Event Tool', 'the-events-calendar' ); ?></b> <?php esc_html_e( 'you can replicate and modify any events in seconds, making event creation a breeze.', 'the-events-calendar' ); ?></p>
 			</div>
 		</div>
 		<?php if( ! class_exists( 'Tribe__Events__Pro__Main' ) ): ?>
@@ -76,7 +79,7 @@ $common_main = Tribe__Main::instance();
 				<p><?php esc_html_e( 'We have fully removed the legacy views from The Events Calendar 6.0. Your views have been automatically updated.', 'the-events-calendar' ); ?></p>
 				<a href="<?php echo esc_url( tribe_get_events_link() ); ?>"><?php esc_html_e( 'View your calendar', 'the-events-calendar' ); ?></a>
 				<a href="https://theeventscalendar.com/knowledgebase/k/v1-deprecation-faqs/"><?php esc_html_e( 'Learn more', 'the-events-calendar' ); ?></a>
-			</div>	
+			</div>
 			<div class="tec-update-page-grid__item tec-update-page-box ">
 				<h3><?php esc_html_e( 'Resources', 'the-events-calendar' ); ?></h3>
 				<ul>
