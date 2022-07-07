5177f650c51a79b87749c848eb012a0fc5a307ce
Camwyn <stephen@theeventscalendar.com>
2022-07-07 17:37:39 -0400

    fix version

--- src/Tribe/Main.php
+++ src/Tribe/Main.php
@@ -33,7 +33,7 @@ if ( ! class_exists( 'Tribe__Events__Main' ) ) {
 		const VENUE_POST_TYPE     = 'tribe_venue';
 		const ORGANIZER_POST_TYPE = 'tribe_organizer';
 
-		const VERSION             = '5.15.2.1';
+		const VERSION             = '5.16.2.1';
 
 		/**
 		 * Min Pro Addon
