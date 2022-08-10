b2922cb1f2fecbb6812f6a6b997cbd2172d5e3bf
Zosterops lateralis <110761737+zosterops-lateralis@users.noreply.github.com>
2022-08-10 19:40:37 +0200

    [Tribe__Events__Main] Remove `comments` from the default post type supports
    If set, the `showComments` option will reinstate the post type support
    for `comments`, but they should not be in the default list.

--- src/Tribe/Main.php
+++ src/Tribe/Main.php
@@ -94,7 +94,6 @@ if ( ! class_exists( 'Tribe__Events__Main' ) ) {
 				'author',
 				'thumbnail',
 				'custom-fields',
-				'comments',
 				'revisions',
 			],
 			'taxonomies'      => [ 'post_tag' ],
