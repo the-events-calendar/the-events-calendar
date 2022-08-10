3582cb3ed56d93431ed8f3ca8e1b4bb8d66a9e9c
Eugene Kyale <eugene.kyale@gmail.com>
2022-08-10 15:35:38 +0300

    add link icons

--- src/admin-views/aggregator/origins/limit.php
+++ src/admin-views/aggregator/origins/limit.php
@@ -45,12 +45,16 @@ $import_limit_message = $global_limit_message . ' ' . sprintf( '<a href="%s" tar
 	<div class="tec-admin-ea-help-message">
 		<?php esc_html_e( 'Need assistance? You can find more information here:', 'the-events-calendar' ); ?>
 
-		<a class="tec-admin-ea-help-message--links" href="https://evnt.is/1bam" target="_blank" rel="noopener noreferrer">
-			<?php esc_html_e( 'Importing Calendar Data From a CSV File', 'the-events-calendar' ); ?>
-		</a>
+		<span class="tec-admin-ea-help-message__icon">
+			<a href="https://evnt.is/1bam" target="_blank" rel="noopener noreferrer">
+				<?php esc_html_e( 'Importing Calendar Data From a CSV File', 'the-events-calendar' ); ?>
+			</a>
+		</span>
 
-		<a class="tec-admin-ea-help-message--links" href="https://evnt.is/1ban" target="_blank" rel="noopener noreferrer">
-			<?php esc_html_e( 'CSV File Formatting and Examples ', 'the-events-calendar' ); ?>
-		</a>
+		<span class="tec-admin-ea-help-message__icon">
+			<a href="https://evnt.is/1ban" target="_blank" rel="noopener noreferrer">
+				<?php esc_html_e( 'CSV File Formatting and Examples ', 'the-events-calendar' ); ?>
+			</a>
+		</span>
 	</div>
 </div>
