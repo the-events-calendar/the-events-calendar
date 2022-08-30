398f61e3a2a6027d64d2e1cfc10f60757d0062bb
Luca Tumedei <luca@theaveragedev.com>
2022-08-30 12:17:12 +0200

    fix(CT1) use timezone to check duration, reword error messages

--- src/Events/Custom_Tables/V1/Models/Validators/Duration.php
+++ src/Events/Custom_Tables/V1/Models/Validators/Duration.php
@@ -29,9 +29,7 @@ class Duration extends Validator {
 	/**
 	 * Duration constructor.
 	 *
-	 * @param  Valid_Date        $date_validator              A reference to the Date validator.
-	 * @param  Range_Dates       $range_dates_validator       A reference to the Dates Range validator.
-	 * @param  Positive_Integer  $whole_number_validator  A reference to the Positive Integer validator.
+	 * @param Whole_Number $whole_number_validator A reference to the Positive Integer validator.
 	 */
 	public function __construct( Whole_Number $whole_number_validator) {
 		$this->whole_number_validator = $whole_number_validator;
@@ -52,8 +50,8 @@ class Duration extends Validator {
 
 		if ( $model->start_date && $model->end_date ) {
 			// If possible, validate against the entry Start and End Dates.
-			if ( ! $this->check_against_dates( $model->start_date, $model->end_date, $duration ) ) {
-				$this->add_error_message( "The {$duration} is greater than the duration of the event" );
+			if ( ! $this->check_against_dates( $model->start_date, $model->end_date, $duration, $model->timezone ) ) {
+				$this->add_error_message( "The Event duration ({$duration}) is greater than the one calculated using dates." );
 
 				return false;
 			}
@@ -61,8 +59,8 @@ class Duration extends Validator {
 
 		if ( $model->start_date_utc && $model->end_date_utc ) {
 			// If possible, validate against the entry Start and End Dates.
-			if ( ! $this->check_against_dates( $model->start_date_utc, $model->end_date_utc, $duration ) ) {
-				$this->add_error_message( "The {$duration} is greater than the duration of the event" );
+			if ( ! $this->check_against_dates( $model->start_date_utc, $model->end_date_utc, $duration, 'UTC' ) ) {
+				$this->add_error_message( "The Event duration ({$duration}) is greater than the one calculated using UTC dates." );
 
 				return false;
 			}
@@ -79,15 +77,16 @@ class Duration extends Validator {
 	 *
 	 * @since 6.0.0
 	 *
-	 * @param  string|int|\DateTimeInterface  $start_date  The Start Date.
-	 * @param  string|int|\DateTimeInterface  $end_date    The End Date.
-	 * @param  int                            $duration    The Duration value.
+	 * @param string|int|\DateTimeInterface $start_date The Start Date.
+	 * @param string|int|\DateTimeInterface $end_date   The End Date.
+	 * @param int                           $duration   The Duration value.
+	 * @param string|null                   $timezone   The timezone to use for the calculation.
 	 *
 	 * @return bool Whether the Duration value is valid when validated in the context of the Dates or not.
 	 */
-	private function check_against_dates( $start_date, $end_date, $duration ) {
-		$start_date_object = Dates::build_date_object( $start_date );
-		$end_date_object   = Dates::build_date_object( $end_date );
+	private function check_against_dates( $start_date, $end_date, int $duration, ?string $timezone = null ) {
+		$start_date_object = Dates::build_date_object( $start_date, $timezone );
+		$end_date_object = Dates::build_date_object( $end_date, $timezone );
 		$date_duration     = $end_date_object->getTimestamp() - $start_date_object->getTimestamp();
 
 		return $date_duration >= $duration;
