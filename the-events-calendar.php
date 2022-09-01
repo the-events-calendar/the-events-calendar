2e9eca6b185f1f80b5e311abcaff5cea7fb09047
Matthew Batchelder <borkweb@gmail.com>
2022-09-01 13:31:46 -0400

    Force a PHP change

--- the-events-calendar.php
+++ the-events-calendar.php
@@ -28,6 +28,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
+
 define( 'TRIBE_EVENTS_FILE', __FILE__ );
 
 // Load the required php min version functions.
