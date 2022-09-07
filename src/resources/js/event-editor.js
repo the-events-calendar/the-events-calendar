a4c088e28b6581cbe657bb938f75bf3843726b2f
Brian Krane <redscar@gmail.com>
2022-09-07 16:19:49 -0400

    Refactored the file to be more inline with ES6 format.

--- src/resources/js/event-editor.js
+++ src/resources/js/event-editor.js
@@ -3,53 +3,22 @@ var tribe_events_event_editor = tribe_events_event_editor || {};
 /**
  * Implements behaviours that are specific to the event editor.
  */
-jQuery( function( $ ) {
-	var obj = tribe_events_event_editor,
-		$sticky_in_month_view_checkbox = $( 'input[name="EventShowInCalendar"]' ),
-		$featured_event_checkbox = $( 'input[name="feature_event"]' ),
-		add_organizer_button = '#event_tribe_organizer .tribe-add-post',
-		organizer_area = '#event_tribe_organizer',
-		organizer_delete_button = '.tribe-delete-this';
+( function ( $, obj ) {
+	'use strict';
 
-	/**
-	 * If the 'feature event' box is checked, automatically check the
-	 * sticky-in-month-view box also.
-	 */
-	obj.auto_enable_sticky_field = function() {
-		if ( $( this ).prop( 'checked' ) ) {
-			$sticky_in_month_view_checkbox.prop( 'checked', true );
-		}
+	obj = obj || {};
+
+	obj.selectors = {
+		$sticky_in_month_view_checkbox: $( 'input[name="EventShowInCalendar"]' ),
+		$featured_event_checkbox: $( 'input[name="feature_event"]' ),
+		organizer: {
+			area: '#event_tribe_organizer',
+			delete_button: '.tribe-delete-this',
+			add_button: '#event_tribe_organizer .tribe-add-post',
+		},
 	};
-	/**
-	 * Logic for the organizer area -
-	 *"Add Organizer" button should be hidden by default, only appearing when there is more than 1 organizer.
-	 *"Trash" icon / delete button should be hidden by default, only appearing when there is more than 1 organizer
-	 *  or when an organizer has the value of -1.
-	 */
-	$( add_organizer_button ).hide();
-	organizer_delete_button_display_logic();
 
-	$( organizer_area ).on( 'change', '.linked-post-dropdown', function ( ) {
-
-		if ( this.value !== -1 ) {
-			$( add_organizer_button ).show();
-		} else {
-			$( add_organizer_button ).hide();
-		}
-
-		organizer_delete_button_display_logic();
-
-	} );
-
-	// Functions to run when the delete button is clicked.
-	$( organizer_area ).on( 'click', organizer_delete_button, function ( ) {
-		// We have to run this in a setTimeout because the original functionality uses a fade of 500ms.
-		// Therefore we use 525ms to run slightly after it is done.
-		setTimeout( function () {
-			organizer_delete_button_display_logic();
-		}, 525 );
-
-	} );
+	obj.organizer = {};
 	/**
 	 * Controls logic for the Organizer delete button to display.
 	 *
@@ -58,25 +27,81 @@ jQuery( function( $ ) {
 	 *
 	 * @return void
 	 */
-	function organizer_delete_button_display_logic(){
+	obj.organizer.deleteButtonDisplayLogic = () => {
 
-		$( organizer_area ).find( organizer_delete_button ).each( function () {
+		$( obj.selectors.organizer.area ).find( obj.selectors.organizer.delete_button ).each( function () {
 			// If you have more than 1 organizer than we display the delete button.
-			if ( $(organizer_area).find('.saved-linked-post').length > 1 ) {
+			if ( $( obj.selectors.organizer.area ).find( '.saved-linked-post' ).length > 1 ) {
 				$( this ).show();
 				return;
 			}
 			//If this is running, it's because we only have 1 organizer
-			if ( $( organizer_area + ' .linked-post-dropdown' ).val() !== -1 ) {
-				$( add_organizer_button ).show();
+			if ( $( obj.selectors.organizer.area + ' .linked-post-dropdown' ).val() !== -1 ) {
+				$( obj.selectors.organizer.add_button ).show();
 			}
 
 			$( this ).hide();
 
 		} );
 
-	}
+	};
 
-	$featured_event_checkbox.on( 'change', obj.auto_enable_sticky_field );
-	$( obj ).trigger( 'event-editor-post-init.tribe' );
-} );
+	obj.organizer.bindEvents = () => {
+
+		/**
+		 * Logic for the organizer area -
+		 *"Add Organizer" button should be hidden by default, only appearing when there is more than 1 organizer.
+		 *"Trash" icon / delete button should be hidden by default, only appearing when there is more than 1 organizer
+		 *  or when an organizer has the value of -1.
+		 */
+		$( obj.selectors.organizer.add_button ).hide();
+		obj.organizer.deleteButtonDisplayLogic();
+
+		$( obj.selectors.organizer.area ).on( 'change', '.linked-post-dropdown', function () {
+
+			if ( this.value !== -1 ) {
+				$( obj.selectors.organizer.add_button ).show();
+			} else {
+				$( obj.selectors.organizer.add_button ).hide();
+			}
+
+			obj.organizer.deleteButtonDisplayLogic();
+
+		} );
+
+		// Functions to run when the delete button is clicked.
+		$( obj.selectors.organizer.area ).on( 'click', obj.selectors.organizer.delete_button, function () {
+			// We have to run this in a setTimeout because the original functionality uses a fade of 500ms.
+			// Therefore we use 525ms to run slightly after it is done.
+			setTimeout( function () {
+				obj.organizer.deleteButtonDisplayLogic();
+			}, 525 );
+
+		} );
+
+	};
+
+	/**
+	 * If the 'feature event' box is checked, automatically check the
+	 * sticky-in-month-view box also.
+	 */
+	obj.auto_enable_sticky_field = function () {
+		if ( $( this ).prop( 'checked' ) ) {
+			obj.selectors.$sticky_in_month_view_checkbox.prop( 'checked', true );
+		}
+	};
+
+	obj.bindFeaturedEvents = () => {
+		obj.selectors.$featured_event_checkbox.on( 'change', obj.auto_enable_sticky_field );
+		$( obj ).trigger( 'event-editor-post-init.tribe' );
+	};
+
+	obj.init = () => {
+		obj.organizer.bindEvents();
+		obj.bindFeaturedEvents();
+	};
+
+	//Init our main object
+	obj.init();
+
+} )( jQuery );
