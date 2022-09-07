91b56eff700f57edacf3873360ca5540e6f5b246
Brian Krane <redscar@gmail.com>
2022-09-07 16:41:00 -0400

    Added more documentation.

--- src/resources/js/event-editor.js
+++ src/resources/js/event-editor.js
@@ -8,35 +8,42 @@ var tribe_events_event_editor = tribe_events_event_editor || {};
 
 	obj = obj || {};
 
+	/**
+	 *	Setup our selectors.
+	 *
+	 *	@since TBD
+	 */
 	obj.selectors = {
-		$sticky_in_month_view_checkbox: $( 'input[name="EventShowInCalendar"]' ),
-		$featured_event_checkbox: $( 'input[name="feature_event"]' ),
+		$sticky_in_month_view_checkbox: 'input[name="EventShowInCalendar"]',
+		$featured_event_checkbox: 'input[name="feature_event"]',
 		organizer: {
 			area: '#event_tribe_organizer',
 			delete_button: '.tribe-delete-this',
 			add_button: '#event_tribe_organizer .tribe-add-post',
+			post_dropdown: '.linked-post-dropdown',
 		},
 	};
 
 	obj.organizer = {};
 	/**
-	 * Controls logic for the Organizer delete button to display.
+	 *	Controls logic for the Organizer delete button to display.
 	 *
-	 * If more than 1 organizer exists, display the delete button.
-	 * If only 1 organizer exists, hide the delete button.
+	 *	If more than 1 organizer exists, display the delete button.
+	 *	If only 1 organizer exists, hide the delete button.
 	 *
-	 * @return void
+	 *	@since TBD
+	 *	@return void
 	 */
 	obj.organizer.deleteButtonDisplayLogic = () => {
 
 		$( obj.selectors.organizer.area ).find( obj.selectors.organizer.delete_button ).each( function () {
-			// If you have more than 1 organizer than we display the delete button.
+			// If you have more than 1 organizer then we display the delete button.
 			if ( $( obj.selectors.organizer.area ).find( '.saved-linked-post' ).length > 1 ) {
 				$( this ).show();
 				return;
 			}
 			//If this is running, it's because we only have 1 organizer
-			if ( $( obj.selectors.organizer.area + ' .linked-post-dropdown' ).val() !== -1 ) {
+			if ( $( obj.selectors.organizer.area + ' ' + obj.selectors.organizer.post_dropdown ).val() !== -1 ) {
 				$( obj.selectors.organizer.add_button ).show();
 			}
 
@@ -45,7 +52,11 @@ var tribe_events_event_editor = tribe_events_event_editor || {};
 		} );
 
 	};
-
+	/**
+	 *	Trigger events for bind events.
+	 *
+	 *	@since TBD
+	 */
 	obj.organizer.bindEvents = () => {
 
 		/**
@@ -57,8 +68,8 @@ var tribe_events_event_editor = tribe_events_event_editor || {};
 		$( obj.selectors.organizer.add_button ).hide();
 		obj.organizer.deleteButtonDisplayLogic();
 
-		$( obj.selectors.organizer.area ).on( 'change', '.linked-post-dropdown', function () {
-
+		$( obj.selectors.organizer.area ).on( 'change', obj.selectors.organizer.post_dropdown, function () {
+			console.log( 'I am changing the select' );
 			if ( this.value !== -1 ) {
 				$( obj.selectors.organizer.add_button ).show();
 			} else {
@@ -84,18 +95,31 @@ var tribe_events_event_editor = tribe_events_event_editor || {};
 	/**
 	 * If the 'feature event' box is checked, automatically check the
 	 * sticky-in-month-view box also.
+	 *
+	 * @since TBD
+	 *
 	 */
 	obj.auto_enable_sticky_field = function () {
 		if ( $( this ).prop( 'checked' ) ) {
-			obj.selectors.$sticky_in_month_view_checkbox.prop( 'checked', true );
+			$( obj.selectors.$sticky_in_month_view_checkbox ).prop( 'checked', true );
 		}
 	};
 
+	/**
+	 * Bind featured events logic.
+	 *
+	 * @since TBD
+	 */
 	obj.bindFeaturedEvents = () => {
-		obj.selectors.$featured_event_checkbox.on( 'change', obj.auto_enable_sticky_field );
+		$( obj.selectors.$featured_event_checkbox ).on( 'change', obj.auto_enable_sticky_field );
 		$( obj ).trigger( 'event-editor-post-init.tribe' );
 	};
 
+	/**
+	 * Initialize
+	 *
+	 * @since TBD
+	 */
 	obj.init = () => {
 		obj.organizer.bindEvents();
 		obj.bindFeaturedEvents();
