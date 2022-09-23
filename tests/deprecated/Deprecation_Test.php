5ef96108a90593c863f53f656784ba3a50e2ab36
Luca Tumedei <luca@theaveragedev.com>
2022-09-23 14:03:43 +0200

    test(deprecated) add major version check
    Following an idea came up in code review, add a check for the plugin
    major version to automatically scoop-up left-over deprecated methods.

--- tests/deprecated/Deprecation_Test.php
+++ tests/deprecated/Deprecation_Test.php
@@ -8,11 +8,24 @@
  */
 
 use Codeception\TestCase\WPTestCase;
+use PHPUnit\Framework\AssertionFailedError;
+
 
 class Expected_Deprecation_Signal extends Exception {
 }
 
 class Deprecation_Test extends WPTestCase {
+	private string $plugin_version;
+	private string $plugin_major_version;
+
+	/**
+	 * @before
+	 */
+	public function set_plugin_versions(): void {
+		$plugin_version = Tribe__Events__Main::VERSION;
+		$this->plugin_version = $plugin_version;
+		$this->plugin_major_version = substr( $plugin_version, 0, strpos( $plugin_version, '.' ) );
+	}
 
 	public function deprecated_methods_and_versions_data_provider(): Generator {
 		foreach ( glob( dirname( __DIR__, 2 ) . '/src/deprecated/Traits/*_Deprecated.php' ) as $file ) {
@@ -20,16 +33,21 @@ class Deprecation_Test extends WPTestCase {
 			$methods = ( new ReflectionClass( $trait ) )->getMethods( ReflectionMethod::IS_PUBLIC );
 			foreach ( $methods as $method ) {
 				$doc_block = $method->getDocComment();
-				$name = $method->getName();
+				$method_name = $method->getName();
 				if ( ! preg_match( '/@deprecated\s+([0-9.]+)/', $doc_block, $matches ) ) {
 					throw new RuntimeException(
-						'Could not find @deprecated version number in doc block for ' . $trait . '::' . $name
+						'Could not find @deprecated version number in doc block for ' . $trait . '::' . $method_name
 					);
 				}
 				$version = $matches[1];
 				$class = str_replace( '_Deprecated', '', $trait );
 
-				yield sprintf( '%s::%s', $trait, $method->name ) => [ $method->isStatic(), $class, $name, $version ];
+				yield sprintf( '%s::%s', $trait, $method->name ) => [
+					$method->isStatic(),
+					$class,
+					$method_name,
+					$version
+				];
 			}
 		}
 	}
@@ -47,10 +65,8 @@ class Deprecation_Test extends WPTestCase {
 		}, $args );
 		// Trigger the deprecation error, check it and then bail out of the function.
 		add_filter( 'deprecated_function_trigger_error', '__return_true' );
-		add_action( 'deprecated_function_run', function ( $function, $replacement, $deprecation_version ) use ( $class, $method, $version ) {
-			$this->assertContains( $class . '::' . $method, $function );
-			$this->assertEquals( $version, $deprecation_version );
-		}, 10, 3 );
+		// Work out the plugin major version now, to make sure methods are deprecated in the right version.
+		add_action( 'deprecated_function_run', $this->get_test_closure( $class, $method, $version ), 10, 3 );
 		set_error_handler( static function () {
 			// Throw to just get out of the function before any real logic runs.
 			throw new Expected_Deprecation_Signal();
@@ -65,5 +81,157 @@ class Deprecation_Test extends WPTestCase {
 		} catch ( Expected_Deprecation_Signal $e ) {
 			// Do nothing, this is expected.
 		}
+	}
+
+	/**
+	 * Returns the Closure that should be hooked to the `deprecated_function_run` action to test the deprecation.
+	 *
+	 * @param string $class   The fully-qualified class name of the deprecated method.
+	 * @param string $method  The name of the deprecated method.
+	 * @param string $version The version the method was deprecated in.
+	 *
+	 * @return Closure The Closure that should be hooked to the `deprecated_function_run` action to test the
+	 *                 deprecation.
+	 */
+	protected function get_test_closure( string $class, string $method, string $version ): Closure {
+		return function ( $function, $replacement, $deprecation_version )
+		use (
+			$class,
+			$method,
+			$version
+		) {
+			$this->assertContains( $class . '::' . $method, $function );
+			$this->assertEquals( $version, $deprecation_version );
+			$deprecation_major_version = substr( $deprecation_version, 0, strpos( $deprecation_version, '.' ) );
+			$this->assertEquals(
+				$this->plugin_major_version,
+				$deprecation_major_version,
+				"Deprecation version $deprecation_version of $class::$method is not the same major " .
+				"version as the plugin ($this->plugin_version); should this be removed?."
+			);
+		};
+	}
+
+	private function eval_class_template( string $deprecation_version ): string {
+		$class_name = 'Deprecation_Test_Class_' . md5( microtime() );
+
+		$class_code = <<<PHP
+class $class_name {
+	/**
+	 * @deprecated $deprecation_version
+	 */
+	public function deprecated_instance_method() {
+		_deprecated_function(
+			'$class_name::deprecated_instance_method',
+			'$deprecation_version',
+			'some_function'
+		);
+	}
+
+	/**
+	 * @deprecated $deprecation_version
+	 */
+	public function deprecated_static_method() {
+		_deprecated_function(
+			'$class_name::deprecated_static_method',
+			'$deprecation_version',
+			'some_static_function'
+		);
+	}
+}
+PHP;
+
+		eval( $class_code );
+
+		return $class_name;
+	}
+
+
+	public function major_version_diff_test_data_provider(): Generator {
+		$current_plugin_major_version = substr( Tribe__Events__Main::VERSION, 0, strpos( Tribe__Events__Main::VERSION, '.' ) );
+
+		$deprecation_version = $current_plugin_major_version - 1 . '.0.0';
+		$class_name = $this->eval_class_template( $deprecation_version  );
+		yield 'deprecation major lt plugin major, instance method' => [
+			$class_name,
+			false,
+			'deprecated_instance_method',
+			$deprecation_version,
+			true
+		];
+
+		yield 'deprecation major lt plugin major, static method' => [
+			$class_name,
+			false,
+			'deprecated_static_method',
+			$deprecation_version,
+			true
+		];
+
+		$deprecation_version = $current_plugin_major_version . '.0.0';
+		$class_name = $this->eval_class_template( $deprecation_version );
+		yield 'deprecation major same as plugin major, instance method' => [
+			$class_name,
+			false,
+			'deprecated_instance_method',
+			$deprecation_version,
+			false
+		];
+
+		yield 'deprecation major same as plugin major, static method' => [
+			$class_name,
+			false,
+			'deprecated_static_method',
+			$deprecation_version,
+			false
+		];
+
+		$deprecation_version = $current_plugin_major_version . '.2389.8923';
+		$class_name = $this->eval_class_template( $deprecation_version );
+		yield 'deprecation major same as plugin major, diff minor, instance method' => [
+			$class_name,
+			false,
+			'deprecated_instance_method',
+			$deprecation_version,
+			false
+		];
+
+		yield 'deprecation major same as plugin major, diff minor, static method' => [
+			$class_name,
+			false,
+			'deprecated_static_method',
+			$deprecation_version,
+			false
+		]
+		;
+
+		$deprecation_version = $current_plugin_major_version + 1 . '.0.0';
+		$class_name = $this->eval_class_template( $deprecation_version );
+		yield 'deprecation major gt plugin major, instance method' => [
+			$class_name,
+			false,
+			'deprecated_instance_method',
+			$deprecation_version,
+			true
+		];
+
+		yield 'deprecation major gt plugin major, static method' => [
+			$class_name,
+			false,
+			'deprecated_static_method',
+			$deprecation_version,
+			true
+		];
+	}
+
+	/**
+	 * @dataProvider major_version_diff_test_data_provider
+	 */
+	public function test_major_version_check( string $class, bool $is_static, string $method, string $version, bool $expect_exception ): void {
+		if ( $expect_exception ) {
+			$this->expectException( AssertionFailedError::class );
+		}
+
+		$this->test_deprecated_methods_and_versions( $is_static, $class, $method, $version );
 	}
 }
