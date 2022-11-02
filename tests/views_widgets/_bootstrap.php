1fec6110456121084f7228182d4d320bf7e48351
Camwyn <stephen@theeventscalendar.com>
2022-11-02 11:16:59 -0400

    Test something for github

--- tests/views_widgets/_bootstrap.php
+++ tests/views_widgets/_bootstrap.php
@@ -18,5 +18,7 @@ global $wp_rewrite;
 $wp_rewrite->permalink_structure = '/%postname%/';
 $wp_rewrite->rewrite_rules();
 
-update_option( 'theme', 'twentytwenty' );
-update_option( 'stylesheet', 'twentytwenty' );
+$stylesheet = 'twentytwenty';
+update_option( 'theme', $stylesheet );
+update_option( 'stylesheet', $stylesheet );
+switch_theme( $stylesheet );
