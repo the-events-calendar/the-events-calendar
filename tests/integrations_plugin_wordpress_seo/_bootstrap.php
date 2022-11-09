a6245fca1de2db4861ed48026eb3bc11f452b377
Gustavo Bordoni <bordoni.dev@gmail.com>
2022-11-08 21:26:48 -0500

    Remove update theme on tests

--- tests/integrations_plugin_wordpress_seo/_bootstrap.php
+++ tests/integrations_plugin_wordpress_seo/_bootstrap.php
@@ -3,7 +3,3 @@
 global $wp_rewrite;
 $wp_rewrite->permalink_structure = '/%postname%/';
 $wp_rewrite->rewrite_rules();
-
-update_option( 'theme', 'twentytwenty' );
-update_option( 'stylesheet', 'twentytwenty' );
-
