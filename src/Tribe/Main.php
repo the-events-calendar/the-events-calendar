dec46345a4d858af6471029bd761ce24bd40db9e
Camwyn <stephen@theeventscalendar.com>
2022-11-09 09:21:45 -0500

    Versioning

--- src/Tribe/Main.php
+++ src/Tribe/Main.php
@@ -37,7 +37,7 @@ if ( ! class_exists( 'Tribe__Events__Main' ) ) {
 		const VENUE_POST_TYPE     = 'tribe_venue';
 		const ORGANIZER_POST_TYPE = 'tribe_organizer';
 
-		const VERSION             = '6.0.3.1';
+		const VERSION             = '6.0.4';
 
 		/**
 		 * Min Pro Addon
