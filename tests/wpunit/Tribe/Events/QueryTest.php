9b997b0359bc9093c57e7f9c4b6b192a5b94b5ff
Luca Tumedei <luca@theaveragedev.com>
2022-11-24 17:00:49 +0100

    WiP

--- tests/wpunit/Tribe/Events/QueryTest.php
+++ tests/wpunit/Tribe/Events/QueryTest.php
@@ -4,8 +4,10 @@ namespace Tribe\Events;
 
 use Spatie\Snapshots\MatchesSnapshots;
 use Tribe\Events\Test\Testcases\Events_TestCase;
+use Tribe\Events\Test\Traits\With_Uopz;
 use Tribe__Events__Main as Main;
 use Tribe__Events__Query as Query;
+use Tribe__Events__Organizer as Organizer;
 use Tribe__Events__Venue as Venue;
 use WP_Query;
 
@@ -18,6 +20,7 @@ use WP_Query;
  */
 class QueryTest extends Events_TestCase {
 	use MatchesSnapshots;
+	use With_Uopz;
 
 	/**
 	 * It should allow getting found posts for arguments
@@ -185,5 +188,93 @@ class QueryTest extends Events_TestCase {
 		] );
 
 		$this->assertMatchesSnapshot( $query->request );
+	}
+
+	/**
+	 * It should not throw when trying to parse non query object
+	 *
+	 * @test
+	 */
+	public function should_not_throw_when_trying_to_parse_non_query_object(): void {
+		Query::parse_query( 'test' );
+	}
+
+	/**
+	 * It should not parse query in admin context
+	 *
+	 * @test
+	 */
+	public function should_not_parse_query_in_admin_context() {
+		// Simulate an admin context request.
+		$this->uopz_set_return( 'is_admin', true );
+		// Disconnect the query to make sure it will parse when explicitly called.
+		remove_action( 'parse_query', [ Query::class, 'parse_query' ], 50 );
+
+		$wp_query = new WP_Query( [ 'post_type' => 'tribe_events' ] );
+
+		Query::parse_query( $wp_query );
+
+		$this->assertFalse( isset( $wp_query->tribe_is_event ) );
+	}
+
+	/**
+	 * It should not parse a query if TEC query filters are suppressed
+	 *
+	 * @test
+	 */
+	public function should_not_parse_a_query_if_tec_query_filters_are_suppressed() {
+		// Disconnect the query to make sure it will parse when explicitly called.
+		remove_action( 'parse_query', [ Query::class, 'parse_query' ], 50 );
+
+		$wp_query = new WP_Query( [ 'post_type' => 'tribe_events', 'tribe_suppress_query_filters' => true ] );
+
+		Query::parse_query( $wp_query );
+
+		$this->assertFalse( isset( $wp_query->tribe_is_event ) );
+	}
+
+	/**
+	 * It should not filter main query for post or page
+	 *
+	 * @test
+	 */
+	public function should_not_filter_main_query_for_post_or_page() {
+		// Disconnect the query to make sure it will parse when explicitly called.
+		remove_action( 'parse_query', [ Query::class, 'parse_query' ], 50 );
+		// Simulate a main query to fetch a post.
+		global $wp_the_query;
+		$id       = $this->factory()->post->create();
+		$wp_the_query = new WP_Query( [ 'p' => $id ] );
+
+		Query::parse_query( $wp_the_query );
+
+		$this->assertFalse( isset( $wp_the_query->tribe_is_event ) );
+	}
+
+	public function tec_post_types_data_provider(): array {
+		return [
+			'Event'     => [ Main::POSTTYPE ],
+			'Organizer' => [ Organizer::POSTTYPE ],
+			'Venue'     => [ Venue::POSTTYPE ],
+		];
+	}
+
+	/**
+	 * It should set TEC post type main query paged prop based on Events paged query var
+	 *
+	 * @test
+	 * @dataProvider tec_post_types_data_provider
+	 */
+	public function should_set_TEC_post_type_main_query_paged_prop_based_on_events_paged_query_var(string $tec_post_type): void {
+		// Disconnect the query to make sure it will parse when explicitly called.
+		remove_action( 'parse_query', [ Query::class, 'parse_query' ], 50 );
+		// Simulate a main query to fetch a post.
+		global $wp_the_query;
+		$id       = $this->factory()->post->create();
+		$wp_the_query = new WP_Query( [ 'post_type' => $tec_post_type ] );
+
+		Query::parse_query( $wp_the_query );
+
+		$this->assertFalse( isset( $wp_the_query->tribe_is_event ) );
 	}
 }
