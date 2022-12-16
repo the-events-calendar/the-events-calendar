61c08bd43e664dc1e8ff7d815b1f1af3a8020b15
Eugene Kyale <eugene.kyale@gmail.com>
2022-12-17 02:51:37 +0300

    Test render google maps iframe.

--- tests/views_integration/Tribe/Events/Views/Modules/Map_BasicTest.php
+++ tests/views_integration/Tribe/Events/Views/Modules/Map_BasicTest.php
@@ -8,16 +8,17 @@ use Tribe\Test\Products\WPBrowser\Views\V2\HtmlTestCase;
 class Map_BasicTest extends HtmlTestCase {
 	use With_Post_Remapping;
 
-	public function test_render() {
+	/**
+	 * Test render google maps iframe.
+	 */
+	public function test_render_google_maps_iframe() {
 		$venue     = "My Test Venue";
 		$width     = "100";
 		$height    = "200";
 		$embed_url = "http://example.com";
 	  
 		ob_start();
-
 		include "src/views/modules/map-basic.php";
-
 		$html = ob_get_clean();
 
 		$this->assertMatchesSnapshot( $html );
