a91b75d02ce1eb51a98fba47f3dd51d1ca7aecc8
mike ritter <mikeritter@users.noreply.github.com>
2023-01-07 10:58:44 -0500

    Correct Shows View Posts instead of View Events
    In admin block editor on the Add / Edit Event screen the icon button in the header hows generic 'View Posts'.
    
    This change adds the omitted 'view_items' label.
    
    See: [https://developer.wordpress.org/reference/functions/get_post_type_labels/]

--- src/Tribe/Main.php
+++ src/Tribe/Main.php
@@ -1969,6 +1969,9 @@ if ( ! class_exists( 'Tribe__Events__Main' ) ) {
 					'view_item'                => sprintf(
 						esc_html__( 'View %s', 'the-events-calendar' ), $this->singular_event_label
 					),
+					'view_items'                => sprintf(
+						esc_html__( 'View %s', 'the-events-calendar' ), $this->plural_event_label
+					),
 					'search_items'             => sprintf(
 						esc_html__( 'Search %s', 'the-events-calendar' ), $this->plural_event_label
 					),
