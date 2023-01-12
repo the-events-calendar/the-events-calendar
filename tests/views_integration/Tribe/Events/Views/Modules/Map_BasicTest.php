54cb2c0ee7f0cdf82f23476f2856785dede000d2
Camwyn <stephen@theeventscalendar.com>
2023-01-12 11:30:58 -0500

    Update test and add another. Update template to accept passed venue, or attempt to use global.

--- tests/views_integration/Tribe/Events/Views/Modules/Map_BasicTest.php
+++ tests/views_integration/Tribe/Events/Views/Modules/Map_BasicTest.php
@@ -2,33 +2,68 @@
 
 namespace Tribe\Events\Views\Modules;
 
-use Tribe\Test\PHPUnit\Traits\With_Post_Remapping;
 use Tribe\Test\Products\WPBrowser\Views\V2\HtmlTestCase;
-
 /**
  * Class Map_BasicTest.
  *
  * @package Tribe\Events\Views\Modules
  */
 class Map_BasicTest extends HtmlTestCase {
-	use With_Post_Remapping;
 
 	/**
 	 * Test render Google Maps iframe with mock data.
 	 */
-	public function test_render_google_maps_iframe_with_mock_data() {
+	public function test_render_google_maps_iframe_with_passed_venue() {
 		ob_start();
+
 		tribe_get_template_part(
 			'modules/map-basic',
 			null,
 			[
-				'venue'     => 'My test venue',
+				'venue'     => 'Jazz Club',
 				'embed_url' => 'https://example.com',
 				'width'     => 100,
 				'height'    => 200,
 			]
 		);
 		$html = ob_get_clean();
+
+
+		$this->assertMatchesSnapshot( $html );
+	}
+
+	/**
+	 * Test render Google Maps iframe with mock data.
+	 */
+	public function test_render_google_maps_iframe_with_mocked_global() {
+		ob_start();
+		// Filter the output of tribe_get_venue to mock the global objects
+		add_filter(
+			'tribe_get_venue',
+			function( $venue ) {
+				return "Disco Club";
+			}
+		);
+
+		tribe_get_template_part(
+			'modules/map-basic',
+			null,
+			[
+				'venue'     => '',
+				'embed_url' => 'https://example.com',
+				'width'     => 100,
+				'height'    => 200,
+			]
+		);
+		$html = ob_get_clean();
+
+		remove_filter(
+			'tribe_get_venue',
+			function( $venue ) {
+				return "Disco Club";
+			}
+		);
+
 
 		$this->assertMatchesSnapshot( $html );
 	}
