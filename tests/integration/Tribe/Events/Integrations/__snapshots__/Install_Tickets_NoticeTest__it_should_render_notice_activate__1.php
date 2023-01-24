3b759ff850ea0b9b590f4908d6f53eda77a268bb
Juan Aldasoro <juanfraa@gmail.com>
2023-01-24 14:41:08 +0100

    Test admin template.

---
+++ tests/integration/Tribe/Events/Integrations/__snapshots__/Install_Tickets_NoticeTest__it_should_render_notice_activate__1.php
@@ -0,0 +1,28 @@
+<?php return '<div class="tec-admin__notice-install-aside">
+	<img
+		src="http://wordpress.test/wp-content/plugins/the-events-calendar/common//src/resources/images/tec-tickets-logo.svg"
+		alt="Event Tickets"
+	/>
+</div>
+<div class="tec-admin__notice-install-content">
+
+	<h3 class="tec-admin__notice-install-content-title">Start selling tickets to your Events</h3>
+
+	<div class="tec-admin__notice-install-content-description">
+		<p>You&#039;re almost there! Activate Event Tickets for free and you&#039;ll be able to sell tickets, collect RSVPs, and manage attendees all from your Dashboard.</p>
+	</div>
+
+	<button
+		id="tribe-tickets-install-plugin"
+		 class="components-button is-primary tec-admin__notice-install-content-button" 		data-plugin-slug="event-tickets"
+		data-nonce="61e185a7e5"
+		data-action="activate"
+		data-redirect-url="http://test.tri.be/wp-admin/admin.php?page=tec-tickets-settings"
+		data-installing-label="Installing..."
+		data-installed-label="Installed!"
+		data-activating-label="Activating..."
+		data-activated-label="Activated!"
+	>Activate Event Tickets</button>
+
+</div>
+';
---
+++ tests/integration/Tribe/Events/Integrations/__snapshots__/Install_Tickets_NoticeTest__it_should_render_notice_install__1.php
@@ -0,0 +1,28 @@
+<?php return '<div class="tec-admin__notice-install-aside">
+	<img
+		src="http://wordpress.test/wp-content/plugins/the-events-calendar/common//src/resources/images/tec-tickets-logo.svg"
+		alt="Event Tickets"
+	/>
+</div>
+<div class="tec-admin__notice-install-content">
+
+	<h3 class="tec-admin__notice-install-content-title">Start selling tickets to your Events</h3>
+
+	<div class="tec-admin__notice-install-content-description">
+		<p>Sell tickets, collect RSVPs, and manage attendees for free with Event Tickets.</p>
+	</div>
+
+	<button
+		id="tribe-tickets-install-plugin"
+		 class="components-button is-primary tec-admin__notice-install-content-button" 		data-plugin-slug="event-tickets"
+		data-nonce="61e185a7e5"
+		data-action="install"
+		data-redirect-url="http://test.tri.be/wp-admin/admin.php?page=tec-tickets-settings"
+		data-installing-label="Installing..."
+		data-installed-label="Installed!"
+		data-activating-label="Activating..."
+		data-activated-label="Activated!"
+	>Install Event Tickets</button>
+
+</div>
+';
