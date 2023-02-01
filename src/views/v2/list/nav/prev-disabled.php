f1a0e2e287e701c8d0dfa16fa6ccd9815726e050
Edwin Daniels <stratease@gmail.com>
2023-01-31 19:44:19 -0800

    Reverting markup, adding styles to accomodate. To avoid release complexity, removing the markup changes. This requires some CSS tweaks to accommodate.

--- src/views/v2/list/nav/prev-disabled.php
+++ src/views/v2/list/nav/prev-disabled.php
@@ -19,7 +19,7 @@ $label = sprintf( __( 'Previous %1$s', 'the-events-calendar' ), tribe_get_event_
 $events_mobile_friendly_label = sprintf( __( 'Previous %1$s', 'the-events-calendar' ), '<span class="tribe-events-c-nav__prev-label-plural tribe-common-a11y-visual-hide">' . tribe_get_event_label_plural() . '</span>' );
 ?>
 <li class="tribe-events-c-nav__list-item tribe-events-c-nav__list-item--prev">
-	<a
+	<button
 		class="tribe-events-c-nav__prev tribe-common-b2 tribe-common-b1--min-medium"
 		aria-label="<?php echo esc_attr( $label ); ?>"
 		title="<?php echo esc_attr( $label ); ?>"
@@ -29,5 +29,5 @@ $events_mobile_friendly_label = sprintf( __( 'Previous %1$s', 'the-events-calend
 		<span class="tribe-events-c-nav__prev-label">
 			<?php echo wp_kses( $events_mobile_friendly_label, [ 'span' => [ 'class' => [] ] ] ); ?>
 		</span>
-	</a>
+	</button>
 </li>
