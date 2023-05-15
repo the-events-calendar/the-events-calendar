e83cf9d357c956aefc58178366b18af01c994175
Edwin Daniels <stratease@gmail.com>
2023-05-15 11:29:26 -0700

    Fix version.

--- src/Tribe/Main.php
+++ src/Tribe/Main.php
@@ -41,7 +41,7 @@ if ( ! class_exists( 'Tribe__Events__Main' ) ) {
 		const VENUE_POST_TYPE     = 'tribe_venue';
 		const ORGANIZER_POST_TYPE = 'tribe_organizer';
 
-		const VERSION             = '6.1.1';
+		const VERSION             = '6.1.0';
 
 		/**
 		 * Min Pro Addon
