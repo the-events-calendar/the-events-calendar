56f17a8496e9c0aca3245317ad4af55b1931eddc
Edwin Daniels <stratease@gmail.com>
2023-05-25 20:24:26 -0700

    Bump common, cleanup config provider.

--- src/Events/Configuration/Provider.php
+++ src/Events/Configuration/Provider.php
@@ -2,18 +2,29 @@
 
 namespace TEC\Events\Configuration;
 
+use TEC\Common\Configuration\Configuration;
 use TEC\Common\Configuration\Configuration_Loader;
 use TEC\Common\Configuration\Constants_Provider;
 use TEC\Common\Provider\Controller;
 
 class Provider extends Controller {
 
+	/**
+	 * Registers Configuration provider.
+	 *
+	 * @since TBD
+	 */
 	protected function do_register(): void {
 		tribe( Configuration_Loader::class )->add( new Constants_Provider() );
-		// @todo
 	}
 
+	/**
+	 * Removes provider.
+	 *
+	 * @since TBD
+	 */
 	public function unregister(): void {
-		// @todo
+		tribe()->offsetUnset( Configuration_Loader::class );
+		tribe()->offsetUnset( Configuration::class );
 	}
 }
