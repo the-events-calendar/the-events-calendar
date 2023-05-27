c3b186750373c44d2ba0c124523d942bb91e0412
rafsuntaskin <rafsun.aiub@gmail.com>
2023-05-27 15:32:06 +0600

    use separate hook for event description

--- src/Events/Integrations/Plugins/Event_Tickets/Emails/Hooks.php
+++ src/Events/Integrations/Plugins/Event_Tickets/Emails/Hooks.php
@@ -49,6 +49,7 @@ class Hooks extends \tad_DI52_ServiceProvider {
 	protected function add_actions(): void {
 		add_action( 'tribe_template_before_include:tickets/emails/template-parts/body/tickets', [ $this, 'include_event_date_ticket_rsvp_emails' ], 10, 3 );
 		add_action( 'tribe_template_before_include:tickets/emails/template-parts/body/tickets', [ $this, 'include_event_title_ticket_rsvp_emails' ], 10, 3 );
+		add_action( 'tribe_template_before_include:tickets/emails/template-parts/body/tickets', [ $this, 'include_event_description_for_ticket_and_rsvp_emails' ], 10, 3 );
 		add_action( 'tribe_template_before_include:tickets/emails/template-parts/body/tickets', [ $this, 'include_event_image_ticket_rsvp_emails' ], 10, 3 );
 		add_action( 'tribe_template_before_include:tickets/emails/template-parts/header/head/styles', [ $this, 'include_event_ticket_rsvp_styles' ], 10, 3 );
 		add_action( 'tribe_template_after_include:tickets/emails/template-parts/body/tickets', [ $this, 'include_event_venue_ticket_rsvp_emails' ], 10, 3 );
@@ -197,7 +198,7 @@ class Hooks extends \tad_DI52_ServiceProvider {
 	}
 
 	/**
-	 * Include the Event title and description in the ticket and RSVP emails.
+	 * Include the Event title in the ticket and RSVP emails.
 	 *
 	 * @since TBD
 	 *
@@ -215,7 +216,25 @@ class Hooks extends \tad_DI52_ServiceProvider {
 		$args = $template->get_local_values();
 
 		$this->container->make( Template::class )->template( 'template-parts/body/event/title', $args, true );
+	}
 
+	/**
+	 * Include the Event description from excerpt into the ticket and RSVP emails.
+	 *
+	 * @since TBD
+	 *
+	 * @param string          $file     Template file.
+	 * @param string          $name     Template name.
+	 * @param Common_Template $template Event Tickets template object.
+	 *
+	 * @return void
+	 */
+	public function include_event_description_for_ticket_and_rsvp_emails( $file, $name, $template  ) {
+		if ( ! $template instanceof Common_Template ) {
+			return;
+		}
+
+		$args = $template->get_local_values();
 		$this->container->make( Template::class )->template( 'template-parts/body/event/description', $args, true );
 	}
 
