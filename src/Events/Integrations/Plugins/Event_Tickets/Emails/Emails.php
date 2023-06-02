0acd53da9dde8cf3492b0827916442d932d1d970
Gustavo Bordoni <bordoni.dev@gmail.com>
2023-06-02 12:26:44 -0400

    Prevent problems with merging values

--- src/Events/Integrations/Plugins/Event_Tickets/Emails/Emails.php
+++ src/Events/Integrations/Plugins/Event_Tickets/Emails/Emails.php
@@ -90,7 +90,7 @@ class Emails {
 		}
 
 		$tec_placeholders['{event_organizers_count}'] = $event->organizers->count();
-		$tec_placeholders['{event_organizers_names}'] = implode( ', ', $event->organizer_names->value() );
+		$tec_placeholders['{event_organizers_names}'] = ! empty( $event->organizer_names ) ? implode( ', ', $event->organizer_names->all() ) : '';
 
 		// If the event has an organizer, add the organizer placeholders.
 		if ( ! empty( $event->organizers->count() ) ) {
