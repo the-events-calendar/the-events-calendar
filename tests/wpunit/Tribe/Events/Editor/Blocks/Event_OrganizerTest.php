7044a8a1c6d0246e5477892d842f8df2270e7d8a
Eugene Kyale <eugene.kyale@gmail.com>
2023-06-26 20:06:52 +0300

    further test coverage

---
+++ tests/wpunit/Tribe/Events/Editor/Blocks/Event_OrganizerTest.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Tribe\Events\Editor\Blocks;
+
+use Tribe__Events__Editor__Blocks__Event_Organizer;
+
+class Event_OrganizerTest extends \Codeception\TestCase\WPTestCase {
+	private $template;
+
+	public function setUp(): void {
+		// make sure to call parent setUp
+		parent::setUp();
+
+		// set up the request time
+		$this->requestTimeSetUp();
+
+		// This is a mock. Adjust it according to your actual needs.
+		$this->template = $this->getMockBuilder( '\Tribe__Events__Editor__Template' )
+		                       ->setMethods( [ 'add_template_globals', 'template' ] )
+		                       ->getMock();
+
+		// Replace the instance in the container with the mock.
+		tribe_singleton( 'events.editor.template', $this->template );
+	}
+
+	/**
+	 * @return Tribe__Events__Editor__Blocks__Event_Organizer
+	 */
+	private function make_instance() {
+		return new Tribe__Events__Editor__Blocks__Event_Organizer();
+	}
+
+	/**
+	 * @test
+	 */
+	public function test_render_no_classes() {
+		$this->template->method( 'template' )->willReturn( '' );
+
+		$sut = $this->make_instance();
+
+		$attributes = [];
+		$result     = $sut->render( $attributes );
+
+		$this->assertNotContains( 'class="', $result );
+	}
+
+	/**
+	 * @test
+	 */
+	public function test_render_with_classes() {
+		$this->template->method( 'template' )->willReturn( '' );
+
+		$sut = $this->make_instance();
+
+		$attributes = [ 'className' => 'test-class' ];
+		$result     = $sut->render( $attributes );
+
+		$this->assertContains( 'class="test-class"', $result );
+	}
+}
