f629d07fb9f88877543625708092da0e617ed9ec
Eugene Kyale <eugene.kyale@gmail.com>
2023-07-12 16:19:48 +0300

    ensure we have valid html by making sure all <dd> elements are preceeded by <dt>

--- src/views/modules/meta/venue.php
+++ src/views/modules/meta/venue.php
@@ -23,10 +23,11 @@ $website_title = tribe_events_get_venue_website_title();
 	<h2 class="tribe-events-single-section-title"> <?php esc_html_e( tribe_get_venue_label_singular(), 'the-events-calendar' ) ?> </h2>
 	<dl>
 		<?php do_action( 'tribe_events_single_meta_venue_section_start' ) ?>
-
+		<dt style="display:none;"><?php // This element is just to make sure we have a valid HTML ?></dt>
 		<dd class="tribe-venue"> <?php echo tribe_get_venue() ?> </dd>
 
 		<?php if ( tribe_address_exists() ) : ?>
+			<dt style="display:none;"><?php // This element is just to make sure we have a valid HTML ?></dt>
 			<dd class="tribe-venue-location">
 				<address class="tribe-events-address">
 					<?php echo tribe_get_full_address(); ?>
