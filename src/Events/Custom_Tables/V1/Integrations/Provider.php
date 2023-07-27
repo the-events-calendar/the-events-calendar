90bf03f440e9c0049ee48477de82fcafb45d29b7
Brian Krane <redscar@gmail.com>
2023-07-27 10:35:47 -0400

    Fixing an issue with Event Tickets not loading properly.

--- src/Events/Custom_Tables/V1/Integrations/Provider.php
+++ src/Events/Custom_Tables/V1/Integrations/Provider.php
@@ -12,6 +12,7 @@ namespace TEC\Events\Custom_Tables\V1\Integrations;
 
 use TEC\Common\Contracts\Service_Provider;
 use TEC\Events\Custom_Tables\V1\Integrations\ACF\Controller as ACF_Controller;
+use TEC\Tickets\Custom_Tables\V1\Provider as ET_Custom_Tables;
 
 /**
  * Class Provider
@@ -24,13 +25,12 @@ class Provider extends Service_Provider {
 	/**
 	 * Registers the Service Providers required for the plugin to work with other plugins.
 	 *
+	 * @since TBD Load the Event Tickets Custom Tables logic by register_on_action.
 	 * @since 6.0.0
 	 */
 	public function register() {
 		// Class defined by the Event Events plugin.
-		if ( class_exists( '\\TEC\\Event_Tickets\\Custom_Tables\\V1\\Provider' ) ) {
-			$this->container->register( \TEC\Tickets\Custom_Tables\V1\Provider::class );
-		}
+		$this->container->register_on_action( 'tec_tickets_custom_tables_controller_registered', ET_Custom_Tables::class );
 
 		// Class defined by the Advanced Custom Fields plugin.
 		if ( class_exists( 'ACF' ) ) {
