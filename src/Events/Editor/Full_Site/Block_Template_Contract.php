a5312778fc0fb6663216ac51f79f27192491f291
Edwin Daniels <stratease@gmail.com>
2023-09-29 11:22:48 -0700

    Ditch return type, old code that isn't compatible.

--- src/Events/Editor/Full_Site/Block_Template_Contract.php
+++ src/Events/Editor/Full_Site/Block_Template_Contract.php
@@ -34,7 +34,7 @@ interface Block_Template_Contract {
 	 *
 	 * @return string
 	 */
-	public function get_namespace(): string;
+	public function get_namespace();
 
 	/**
 	 * The getter for this template service to retrieve a hydrated WP_Block_Template.
