eaf4c097d9620517ba5dff886cf260159f454504
rafsuntaskin <rafsun.aiub@gmail.com>
2023-11-23 00:23:37 +0600

    Revert "tests - fix more JSON LD permissions"
    This reverts commit 34aa7e46aa4b05468e9486af7f71b11b3412d76b.

--- tests/wpunit/Tribe/Events/REST/V1/Post_RepositoryTest.php
+++ tests/wpunit/Tribe/Events/REST/V1/Post_RepositoryTest.php
@@ -597,10 +597,6 @@ class Post_RepositoryTest extends Events_TestCase {
 
 		$id = $this->factory()->event->create();
 
-		// Set the current user to an admin so we can read the JSON LD data.
-		$user = $this->factory()->user->create( [ 'role' => 'administrator' ] );
-		wp_set_current_user( $user );
-
 		$data = $sut->get_event_data( $id, 'foo' );
 
 		$this->assertArrayHasKey( 'json_ld', $data );
@@ -628,10 +624,6 @@ class Post_RepositoryTest extends Events_TestCase {
 
 		$id = $this->factory()->venue->create();
 
-		// Set the current user to an admin so we can read the JSON LD data.
-		$user = $this->factory()->user->create( [ 'role' => 'administrator' ] );
-		wp_set_current_user( $user );
-
 		$data = $sut->get_venue_data( $id, 'foo' );
 
 		$this->assertArrayHasKey( 'json_ld', $data );
@@ -658,10 +650,6 @@ class Post_RepositoryTest extends Events_TestCase {
 		$sut = $this->make_instance();
 
 		$id = $this->factory()->organizer->create();
-
-		// Set the current user to an admin so we can read the JSON LD data.
-		$user = $this->factory()->user->create( [ 'role' => 'administrator' ] );
-		wp_set_current_user( $user );
 
 		$data = $sut->get_organizer_data( $id, 'foo' );
 
