6069a98cb6dbff36917e65544901e02c7d4fe66c
Stephen Page <stephen@theeventscalendar.com>
2024-01-05 10:50:35 -0500

    Some cleanup

--- src/Tribe/Aggregator.php
+++ src/Tribe/Aggregator.php
@@ -100,9 +100,9 @@ class Tribe__Events__Aggregator {
 		global $plugin_page;
 
 		$is_multisite_help_page = is_multisite()
-		                          && is_network_admin()
-		                          && ! empty( $_GET['page'] )
-		                          && $_GET['page'] === tribe( 'settings' )->get_help_slug();
+								&& is_network_admin()
+								&& ! empty( $_GET['page'] )
+								&& $_GET['page'] === tribe( 'settings' )->get_help_slug();
 
 		if ( ! ( 'tribe-help' === $plugin_page || $is_multisite_help_page ) ) {
 			return;
