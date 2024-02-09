625839db0149e22e7ab54620570be5712df7e21b
Brian Krane <redscar@gmail.com>
2024-02-09 08:35:40 -0500

    Date changes for single day, multiday, and series passes.

--- src/Events/Integrations/Plugins/Tickets_Wallet_Plus/Passes/Apple_Wallet/Event_Modifier.php
+++ src/Events/Integrations/Plugins/Tickets_Wallet_Plus/Passes/Apple_Wallet/Event_Modifier.php
@@ -2,7 +2,10 @@
 
 namespace TEC\Events\Integrations\Plugins\Tickets_Wallet_Plus\Passes\Apple_Wallet;
 
+use DateTimeImmutable;
+use TEC\Tickets\Commerce\Module;
 use TEC\Tickets_Wallet_Plus\Passes\Apple_Wallet\Pass;
+use Tribe__Tickets__Tickets;
 
 /**
  * Class Event_Modifier
@@ -14,9 +17,25 @@ use TEC\Tickets_Wallet_Plus\Passes\Apple_Wallet\Pass;
 class Event_Modifier {
 
 	/**
+	 * Format for displaying the Date
+	 *
+	 * @var string
+	 */
+	protected $date_format = 'M j';
+
+	/**
+	 * Format for displaying the time.
+	 *
+	 * @var string
+	 */
+	protected $time_format = 'g:ia';
+
+
+	/**
 	 * Add the Event date into the Apple Pass `back` data.
 	 *
 	 * @since 6.2.8
+	 * @since TBD Removed Event date from secondary array.
 	 *
 	 * @param array $data The Apple Pass data.
 	 * @param Pass  $pass The Apple Pass object.
@@ -30,7 +49,7 @@ class Event_Modifier {
 		}
 
 		// Bail if there is no attendee.
-		if (  ! $pass->attendee_exists() ) {
+		if ( ! $pass->attendee_exists() ) {
 			return $data;
 		}
 
@@ -59,14 +78,203 @@ class Event_Modifier {
 			'value' => $event->post_title,
 		];
 
-		// Add the event start date.
+		return $data;
+	}
+
+	/**
+	 * Helper function to format the date and time to display on the pass.
+	 *
+	 * @param $start
+	 * @param $end
+	 *
+	 * @return string
+	 */
+	private function format_date_time_range(
+		$start,
+		$end
+	) {
+		$formatted_start = $start->format( $this->date_format ) . ' @ ' . $start->format( $this->time_format );
+		$formatted_end   = $end->format( $this->date_format ) . ' @ ' . $end->format( $this->time_format );
+		return $formatted_start . ' - ' . $formatted_end;
+	}
+
+	/**
+	 * Add the Event Date for series.
+	 *
+	 * @param array $data The Apple Pass data.
+	 * @param Pass  $pass The Apple Pass object.
+	 *
+	 * @return array Modified pass data.
+	 */
+	public function include_event_date_series(
+		array $data,
+		Pass  $pass
+	) {
+		// Bail if `tribe_events` CPT is not enabled to have tickets.
+		if ( ! in_array(
+			\Tribe__Events__Main::POSTTYPE,
+			tribe( 'tickets.main' )->post_types(),
+			true
+		) ) {
+			return $data;
+		}
+
+		// Bail if there is no attendee.
+		if ( ! $pass->attendee_exists() ) {
+			return $data;
+		}
+
+		$event_id = $pass->get_event_id();
+		// @todo Redscar - Confirm tribe_is_event_series() is the correct function to use.
+		$is_series_pass = tribe_is_event_series( $event_id );
+
+		if ( ! $is_series_pass ) {
+			return $data;
+		}
+
+		$attendee  = $pass->get_attendee();
+		$ticket_id = $attendee['product_id'];
+		$provider  = tribe_tickets_get_ticket_provider( (int) $ticket_id );
+		$ticket    = $provider->get_ticket(
+			$event_id,
+			$ticket_id
+		);
+
+		$start_date = ( new DateTimeImmutable() )->setTimestamp( $ticket->start_date() );
+		$end_date   = ( new DateTimeImmutable() )->setTimestamp( $ticket->end_date() );
+
+		$event_time_value = $start_date->format( $this->date_format ) . '-' . $end_date->format( $this->date_format );
+
+		$event_dates_value = $this->format_date_time_range(
+			$start_date,
+			$end_date,
+		);
+
+
+		$data['header'][] = [
+			'key'   => 'event_date_time_range',
+			'label' => '', // No label for Series Passes.
+			'value' => $event_time_value,
+		];
+
 		$data['secondary'][] = [
-			'dateStyle'  => 'PKDateStyleMedium',
-			'isRelative' => true,
-			'key'        => 'event_start_date',
-			'label'      => esc_html__( 'Date', 'the-events-calendar' ),
-			'timeStyle'  => 'PKDateStyleShort',
-			'value'      => $event->dates->start->format( 'Y-m-d\TH:iP' ),
+			'key'   => 'event_dates',
+			'label' => esc_html__(
+				'Event Dates',
+				'the-events-calendar'
+			),
+			'value' => $event_dates_value,
+		];
+
+		return $data;
+	}
+
+	/**
+	 * Add the Event Date for multiday events.
+	 *
+	 * @param array $data The Apple Pass data.
+	 * @param Pass  $pass The Apple Pass object.
+	 *
+	 * @return array Modified pass data.
+	 */
+	public function include_event_date_multiday(
+		array $data,
+		Pass $pass
+	) {
+
+		// Bail if `tribe_events` CPT is not enabled to have tickets.
+		if ( ! in_array(
+			\Tribe__Events__Main::POSTTYPE,
+			tribe( 'tickets.main' )->post_types(),
+			true
+		) ) {
+			return $data;
+		}
+
+		// Bail if there is no attendee.
+		if ( ! $pass->attendee_exists() ) {
+			return $data;
+		}
+
+		$event_id = $pass->get_event_id();
+		$event    = tribe_get_event( $event_id );
+
+		$event_spans_multiple_days = $event->dates->start->format( 'Y-m-d' ) !== $event->dates->end->format( 'Y-m-d' );
+
+		if ( ! $event_spans_multiple_days ) {
+			return $data;
+		}
+
+		$event_time_value  = $event->dates->start->format( $this->date_format )
+							. '-' .
+							$event->dates->end->format( $this->date_format );
+		$event_dates_value = $this->format_date_time_range(
+			$event->dates->start,
+			$event->dates->end
+		);
+
+		$data['header'][] = [
+			'key'   => 'event_date_time_range',
+			'label' => '', // No label for Series Passes.
+			'value' => $event_time_value,
+		];
+
+		$data['secondary'][] = [
+			'key'   => 'event_dates',
+			'label' => esc_html__(
+				'Event Dates',
+				'the-events-calendar'
+			),
+			'value' => $event_dates_value,
+		];
+
+
+		return $data;
+	}
+
+	/**
+	 * Add the Event Date for single day events.
+	 *
+	 * @param array $data The Apple Pass data.
+	 * @param Pass  $pass The Apple Pass object.
+	 *
+	 * @return array
+	 */
+	public function include_event_date_single(
+		array $data,
+		Pass $pass
+	): array {
+		// Bail if `tribe_events` CPT is not enabled to have tickets.
+		if ( ! in_array(
+			\Tribe__Events__Main::POSTTYPE,
+			tribe( 'tickets.main' )->post_types(),
+			true
+		) ) {
+			return $data;
+		}
+
+		// Bail if there is no attendee.
+		if ( ! $pass->attendee_exists() ) {
+			return $data;
+		}
+
+		$event_id = $pass->get_event_id();
+		$event    = tribe_get_event( $event_id );
+
+		$event_spans_multiple_days = $event->dates->start->format( 'Y-m-d' ) !== $event->dates->end->format( 'Y-m-d' );
+
+		if ( $event_spans_multiple_days ) {
+			return $data;
+		}
+
+		$label            = $event->dates->start->format( $this->time_format );
+		$event_time_value = $event->dates->start->format( $this->date_format );
+
+
+		$data['header'][] = [
+			'key'   => 'event_date_time_range',
+			'label' => $label,
+			'value' => $event_time_value,
 		];
 
 		return $data;
@@ -89,7 +297,7 @@ class Event_Modifier {
 		}
 
 		// Bail if there is no attendee.
-		if (  ! $pass->attendee_exists() ) {
+		if ( ! $pass->attendee_exists() ) {
 			return $data;
 		}
 
@@ -117,6 +325,7 @@ class Event_Modifier {
 	 * Add event data to sample Apple Wallet pass.
 	 *
 	 * @since 6.2.8
+	 * @since TBD removed date from secondary and moved to header.
 	 *
 	 * @param array $data The Apple Pass data.
 	 * @param Pass  $pass The Apple Pass object.
