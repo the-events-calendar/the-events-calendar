6f2b3029322fb659961f5678b014a1d85da5e141
Gustavo Bordoni <bordoni.dev@gmail.com>
2024-04-03 12:43:13 -0400

    Update to starter template

--- src/views/integrations/elementor/templates/starter.php
+++ src/views/integrations/elementor/templates/starter.php
@@ -5,259 +5,272 @@
 			"settings": [],
 			"elements": [
 				{
-					"id": "63ab6d4a",
-					"settings": {},
-					"elements": [],
-					"isInner": false,
-					"widgetType": "tec_elementor_widget_event_backlink",
-					"elType": "widget"
-				},
-				{
-					"id": "5c4ca066",
-					"settings": {},
-					"elements": [],
-					"isInner": false,
-					"widgetType": "tec_elementor_widget_event_status",
-					"elType": "widget"
-				},
-				{
-					"id": "75c65b7c",
-					"settings": [],
-					"elements": [],
-					"isInner": false,
-					"widgetType": "tec_events_elementor_widget_event_title",
-					"elType": "widget"
-				},
-				{
-					"id": "7b679070",
+					"id": "5ebacd06",
 					"settings": {
-						"show_datetime_year": true,
-						"show_datetime_date": true,
-						"show_datetime_time": true
-					},
-					"elements": [],
-					"isInner": false,
-					"widgetType": "tec_events_elementor_widget_event_datetime",
-					"elType": "widget"
-				},
-				{
-					"id": "680ed4f4",
-					"settings": [],
-					"elements": [],
-					"isInner": false,
-					"widgetType": "tec_events_elementor_widget_event_cost",
-					"elType": "widget"
-				},
-				{
-					"id": "7d8adfbe",
-					"settings": {
-						"width": {
-							"unit": "%",
-							"size": 100,
-							"sizes": []
-						},
-						"height": {
-							"unit": "px",
-							"size": 650,
-							"sizes": []
-						}
-					},
-					"elements": [],
-					"isInner": false,
-					"widgetType": "tec_events_elementor_widget_event_image",
-					"elType": "widget"
-				},
-				{
-					"id": "191e47b3",
-					"settings": {
-						"layout": "full_width",
-						"structure": "20",
-						"padding": {
-							"unit": "px",
-							"top": "30",
-							"right": "0",
-							"bottom": "30",
-							"left": "0",
-							"isLinked": false
-						}
+						"_column_size": 100,
+						"_inline_size": null
 					},
 					"elements": [
 						{
-							"id": "3cd1f412",
-							"settings": {
-								"_column_size": 50,
-								"_inline_size": null
-							},
-							"elements": [
-								{
-									"id": "52f5067d",
-									"settings": [],
-									"elements": [],
-									"isInner": false,
-									"widgetType": "tec_events_elementor_widget_event_categories",
-									"elType": "widget"
-								},
-								{
-									"id": "45f7d26d",
-									"settings": [],
-									"elements": [],
-									"isInner": false,
-									"widgetType": "tec_events_elementor_widget_event_tags",
-									"elType": "widget"
-								},
-								{
-									"id": "5681e36c",
-									"settings": [],
-									"elements": [],
-									"isInner": false,
-									"widgetType": "tec_events_elementor_widget_event_website",
-									"elType": "widget"
-								}
-							],
-							"isInner": true,
-							"elType": "column"
+							"id": "63ab6d4a",
+							"settings": {},
+							"elements": [],
+							"isInner": false,
+							"widgetType": "tec_elementor_widget_event_backlink",
+							"elType": "widget"
 						},
 						{
-							"id": "550a4396",
-							"settings": {
-								"_column_size": 50,
-								"_inline_size": null
-							},
-							"elements": [
-								{
-									"id": "6b046395",
-									"settings": [],
-									"elements": [],
-									"isInner": false,
-									"widgetType": "tec_events_elementor_widget_event_organizer",
-									"elType": "widget"
-								}
-							],
-							"isInner": true,
-							"elType": "column"
-						}
-					],
-					"isInner": true,
-					"elType": "section"
-				},
-				{
-					"id": "40077d93",
-					"settings": {
-						"layout": "full_width",
-						"padding": {
-							"unit": "px",
-							"top": "30",
-							"right": "0",
-							"bottom": "30",
-							"left": "0",
-							"isLinked": false
-						}
-					},
-					"elements": [
+							"id": "5c4ca066",
+							"settings": {},
+							"elements": [],
+							"isInner": false,
+							"widgetType": "tec_elementor_widget_event_status",
+							"elType": "widget"
+						},
 						{
-							"id": "538afad8",
-							"settings": {
-								"_column_size": 100,
-								"_inline_size": null
-							},
-							"elements": [
-								{
-									"id": "eb9eabf",
-									"settings": [],
-									"elements": [],
-									"isInner": false,
-									"widgetType": "tec_events_elementor_widget_event_venue",
-									"elType": "widget"
-								}
-							],
-							"isInner": true,
-							"elType": "column"
-						}
-					],
-					"isInner": true,
-					"elType": "section"
-				},
-				{
-					"id": "2271a229",
-					"settings": {
-						"layout": "full_width",
-						"padding": {
-							"unit": "px",
-							"top": "30",
-							"right": "0",
-							"bottom": "30",
-							"left": "0",
-							"isLinked": false
-						}
-					},
-					"elements": [
+							"id": "75c65b7c",
+							"settings": [],
+							"elements": [],
+							"isInner": false,
+							"widgetType": "tec_events_elementor_widget_event_title",
+							"elType": "widget"
+						},
 						{
-							"id": "7b76859d",
+							"id": "7b679070",
 							"settings": {
-								"_column_size": 100,
-								"_inline_size": null
+								"show_datetime_year": true,
+								"show_datetime_date": true,
+								"show_datetime_time": true
 							},
-							"elements": [
-								{
-									"id": "2ae34f26",
-									"settings": [],
-									"elements": [],
-									"isInner": false,
-									"widgetType": "tec_events_elementor_widget_event_export",
-									"elType": "widget"
-								}
-							],
-							"isInner": true,
-							"elType": "column"
-						}
-					],
-					"isInner": true,
-					"elType": "section"
-				},
-				{
-					"id": "5ec37806",
-					"settings": {
-						"margin": {
-							"unit": "px",
-							"top": "0",
-							"right": 0,
-							"bottom": "50",
-							"left": 0,
-							"isLinked": false
-						}
-					},
-					"elements": [
+							"elements": [],
+							"isInner": false,
+							"widgetType": "tec_events_elementor_widget_event_datetime",
+							"elType": "widget"
+						},
 						{
-							"id": "594e2572",
+							"id": "680ed4f4",
+							"settings": [],
+							"elements": [],
+							"isInner": false,
+							"widgetType": "tec_events_elementor_widget_event_cost",
+							"elType": "widget"
+						},
+						{
+							"id": "7d8adfbe",
 							"settings": {
-								"_column_size": 100,
-								"_inline_size": null
+								"width": {
+									"unit": "%",
+									"size": 100,
+									"sizes": []
+								},
+								"height": {
+									"unit": "px",
+									"size": 650,
+									"sizes": []
+								}
+							},
+							"elements": [],
+							"isInner": false,
+							"widgetType": "tec_events_elementor_widget_event_image",
+							"elType": "widget"
+						},
+						{
+							"id": "191e47b3",
+							"settings": {
+								"layout": "full_width",
+								"structure": "20",
+								"padding": {
+									"unit": "px",
+									"top": "30",
+									"right": "0",
+									"bottom": "30",
+									"left": "0",
+									"isLinked": false
+								}
 							},
 							"elements": [
 								{
-									"id": "359b1fca",
-									"settings": [],
-									"elements": [],
-									"isInner": false,
-									"widgetType": "tec_events_elementor_widget_event_navigation",
-									"elType": "widget"
+									"id": "3cd1f412",
+									"settings": {
+										"_column_size": 50,
+										"_inline_size": null
+									},
+									"elements": [
+										{
+											"id": "52f5067d",
+											"settings": [],
+											"elements": [],
+											"isInner": false,
+											"widgetType": "tec_events_elementor_widget_event_categories",
+											"elType": "widget"
+										},
+										{
+											"id": "45f7d26d",
+											"settings": [],
+											"elements": [],
+											"isInner": false,
+											"widgetType": "tec_events_elementor_widget_event_tags",
+											"elType": "widget"
+										},
+										{
+											"id": "5681e36c",
+											"settings": [],
+											"elements": [],
+											"isInner": false,
+											"widgetType": "tec_events_elementor_widget_event_website",
+											"elType": "widget"
+										}
+									],
+									"isInner": true,
+									"elType": "column"
+								},
+								{
+									"id": "550a4396",
+									"settings": {
+										"_column_size": 50,
+										"_inline_size": null
+									},
+									"elements": [
+										{
+											"id": "6b046395",
+											"settings": [],
+											"elements": [],
+											"isInner": false,
+											"widgetType": "tec_events_elementor_widget_event_organizer",
+											"elType": "widget"
+										}
+									],
+									"isInner": true,
+									"elType": "column"
 								}
 							],
 							"isInner": true,
-							"elType": "column"
+							"elType": "section"
+						},
+						{
+							"id": "40077d93",
+							"settings": {
+								"layout": "full_width",
+								"padding": {
+									"unit": "px",
+									"top": "30",
+									"right": "0",
+									"bottom": "30",
+									"left": "0",
+									"isLinked": false
+								}
+							},
+							"elements": [
+								{
+									"id": "538afad8",
+									"settings": {
+										"_column_size": 100,
+										"_inline_size": null
+									},
+									"elements": [
+										{
+											"id": "eb9eabf",
+											"settings": [],
+											"elements": [],
+											"isInner": false,
+											"widgetType": "tec_events_elementor_widget_event_venue",
+											"elType": "widget"
+										}
+									],
+									"isInner": true,
+									"elType": "column"
+								}
+							],
+							"isInner": true,
+							"elType": "section"
+						},
+						{
+							"id": "2271a229",
+							"settings": {
+								"layout": "full_width",
+								"padding": {
+									"unit": "px",
+									"top": "30",
+									"right": "0",
+									"bottom": "30",
+									"left": "0",
+									"isLinked": false
+								}
+							},
+							"elements": [
+								{
+									"id": "7b76859d",
+									"settings": {
+										"_column_size": 100,
+										"_inline_size": null
+									},
+									"elements": [
+										{
+											"id": "2ae34f26",
+											"settings": [],
+											"elements": [],
+											"isInner": false,
+											"widgetType": "tec_events_elementor_widget_event_export",
+											"elType": "widget"
+										}
+									],
+									"isInner": true,
+									"elType": "column"
+								}
+							],
+							"isInner": true,
+							"elType": "section"
+						},
+						{
+							"id": "5ec37806",
+							"settings": {
+								"margin": {
+									"unit": "px",
+									"top": "0",
+									"right": 0,
+									"bottom": "50",
+									"left": 0,
+									"isLinked": false
+								}
+							},
+							"elements": [
+								{
+									"id": "594e2572",
+									"settings": {
+										"_column_size": 100,
+										"_inline_size": null
+									},
+									"elements": [
+										{
+											"id": "359b1fca",
+											"settings": [],
+											"elements": [],
+											"isInner": false,
+											"widgetType": "tec_events_elementor_widget_event_navigation",
+											"elType": "widget"
+										}
+									],
+									"isInner": true,
+									"elType": "column"
+								}
+							],
+							"isInner": true,
+							"elType": "section"
 						}
 					],
-					"isInner": true,
-					"elType": "section"
+					"isInner": false,
+					"elType": "column"
 				}
 			],
 			"isInner": false,
-			"elType": "container"
+			"elType": "section"
 		}
 	],
 	"page_settings": [],
 	"settings": [],
-	"import_settings": [],
+	"import_settings": {
+		"thumbnail": false,
+	},
 	"version": "0.4",
 	"title": "The Events Calendar - Starter",
 	"type": "tec_event_single"
