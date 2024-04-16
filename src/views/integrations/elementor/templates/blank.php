5ac44cb2817e349f1fd220f1b88d7585f6a8b6fc
Gustavo Bordoni <bordoni.dev@gmail.com>
2024-04-16 09:34:16 -0400

    Update blank.php based on Stephen comments

--- src/views/integrations/elementor/templates/blank.php
+++ src/views/integrations/elementor/templates/blank.php
@@ -6,8 +6,10 @@
  */
 use TEC\Events\Integrations\Plugins\Elementor\Controller as Elementor_Integration;
 
-if ( tribe( Elementor_Integration::class )->is_elementor_pro_active() && ! elementor_theme_do_location( 'single' ) ) {
-	the_content();
+if ( tribe( Elementor_Integration::class )->is_elementor_pro_active() ) {
+	if ( ! elementor_theme_do_location( 'single' ) ) {
+		the_content();
+	}
 } else {
 	the_content();
 }
