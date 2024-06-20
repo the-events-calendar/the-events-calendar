381630a6c38b86b88bad63d95dc205817e4a95f8
Sami Dokus <sami2464@gmail.com>
2024-06-20 15:55:59 -0600

    Attempted to add tests but need some help with setting variables up.

--- tests/elementor_integration/Template/ControllerTest.php
+++ tests/elementor_integration/Template/ControllerTest.php
@@ -9,10 +9,25 @@ class ControllerTest extends \Codeception\TestCase\WPTestCase {
 	public function setUp() {
 		parent::setUp();
 		tribe()->register( Elementor_Template_Controller::class );
+
+		// Create an event using The Events Calendar ORM before each test
+		$event_data = [
+			'title' => 'Sample Event',
+			'content' => 'This is a sample event description.',
+			'start_date' => '2024-09-23 01:00:00',
+			'end_date' => '2024-09-23 05:00:00',
+		];
+
+		$this->event_test = tribe_events()->set_args($event_data)->create();
+
 	}
 
 	public function tearDown(): void {
 		tribe( Elementor_Template_Controller::class )->unregister();
+		// Delete the event after each test
+		if ($this->event_test) {
+			tribe_events()->delete($this->event_test->ID);
+		}
 		parent::tearDown();
 	}
 
@@ -23,7 +38,17 @@ class ControllerTest extends \Codeception\TestCase\WPTestCase {
 	 */
 	public function should_allow_bypassing_override(): void {
 		// Set up filter to return true.
-		// Assert that `is_override` returns false
+		add_filter('tec_events_integration_elementor_bypass_template_override', function(){
+			return true;
+		});
+
+		// Create an instance of the Elementor_Template_Controller
+		$container = tribe();
+		$controller = new Elementor_Template_Controller($container);
+
+		// Assert that `is_override` returns false and is not overriding the template.
+		$is_overridden = $controller->is_override($this->event_test->ID);
+		$this->assertFalse($is_overridden);
 	}
 
 	/**
@@ -32,12 +57,22 @@ class ControllerTest extends \Codeception\TestCase\WPTestCase {
 	 * @test
 	 */
 	public function should_not_bypass_override_when_false_is_returned(): void {
-		// Set up filter to return true.
-		// Assert that `is_override` returns false
+		// Set up filter to return false.
+		add_filter('tec_events_integration_elementor_bypass_template_override', function(){
+			return false;
+		});
+
+		// Create an instance of the Elementor_Template_Controller with the required container
+		$container = tribe();
+		$controller = new Elementor_Template_Controller($container);
+
+		// Assert that `is_override` returns true and is overriding the template.
+		$is_overridden = $controller->is_override($this->event_test->ID);
+		$this->assertTrue($is_overridden);
 	}
 
 	/**
-	 * It should allow the Elementor template override to be bypassed on aspecific event.
+	 * It should allow the Elementor template override to be bypassed on a specific event.
 	 *
 	 * @test
 	 */
