15c79ab6c468f1111bc6922e8fd57e85f5b35621
Dimitrios Pantazis <dpanta94@gmail.com>
2024-07-23 17:34:00 +0300

    Version Bump

--- src/Tribe/Main.php
+++ src/Tribe/Main.php
@@ -39,7 +39,7 @@ if ( ! class_exists( 'Tribe__Events__Main' ) ) {
 		const POSTTYPE            = 'tribe_events';
 		const VENUE_POST_TYPE     = 'tribe_venue';
 		const ORGANIZER_POST_TYPE = 'tribe_organizer';
-		const VERSION             = '6.6.0';
+		const VERSION             = '6.6.0.1';
 
 		/**
 		 * Min Pro Addon.
