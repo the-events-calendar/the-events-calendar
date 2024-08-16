b7146d9d5271e47f9aae451ad4977b89cf197595
Santiago <37714098+Sjaure@users.noreply.github.com>
2024-08-16 17:49:46 -0300

    Fixed @since tag

--- src/functions/template-tags/link.php
+++ src/functions/template-tags/link.php
@@ -21,7 +21,7 @@ function tribe_the_prev_event_link( $anchor = false ) {
 /**
  * Returns a link to the previous event by start date for the given event.
  *
- * @since 5.14.0
+ * @since 3.0
  * 
  * @param bool|string $anchor (optional) The link text. Use %title% to place the post title in your string. Default is false.
  *
