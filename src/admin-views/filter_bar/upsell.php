dd7333508e072bf3728163a00fafa01a69a25902
Camwyn <stephen@theeventscalendar.com>
2024-08-16 01:30:30 -0400

    Upsell for Filterbar

--- src/admin-views/filter_bar/upsell.php
+++ src/admin-views/filter_bar/upsell.php
@@ -3,34 +3,33 @@
  * Filter bar upsell banner.
  *
  * @since 5.14.0
+ * @since TBD Drastically simplified the HTML, changed classes to use new admin styles
  */
 $main = Tribe__Events__Main::instance();
 ?>
-<div class="tec-filterbar-upsell">
-    <div class="tec-filterbar-upsell__content">
-        <div class="tec-filterbar-upsell__title">
+<div class="tec-settings__upsell">
+    <div class="tec-settings__upsell-content">
+        <div class="tec-settings__upsell-header">
             <img
                 src="<?php echo esc_url( tribe_resource_url( 'icons/filterbar.svg', false, null, $main ) ); ?>"
-                alt="<?php esc_attr_e( 'Filter Bar Icon', 'the-events-calendar' ); ?>"
+				class="tec-settings__upsell-logo"
+				role="presentation
+                alt=""
             >
-            <h3>
+            <h3 class="tec-settings__upsell-title">
                 <?php esc_html_e( 'Filter Bar', 'the-events-calendar' ); ?>
             </h3>
         </div>
         <p>
             <?php esc_html_e( 'Looking for front-end Event Filters so that your website visitors can find exactly the event they are looking for?', 'the-events-calendar' ); ?>
         </p>
-        <div class="tec-filterbar-upsell__btn">
-            <a href="https://evnt.is/1b31" target="_blank" rel="noopener noreferrer">
-                <?php esc_html_e( 'Check out our Filter Bar add-on', 'the-events-calendar' ); ?>
-            </a>
-        </div>
+		<a href="https://evnt.is/1b31" class="tec-settings__upsell-btn" target="_blank" rel="noopener noreferrer">
+			<?php esc_html_e( 'Check out our Filter Bar add-on', 'the-events-calendar' ); ?>
+		</a>
     </div>
-
-    <div class="tec-filterbar-upsell__icon">
-        <img
-            src="<?php echo esc_url( tribe_resource_url( 'icons/filterbar-banner.png', false, null, $main ) ); ?>"
-            alt="<?php esc_attr_e( 'Filter Bar Banner Icon', 'the-events-calendar' ); ?>"
-        >
-    </div>
+	<img
+		class="tec-settings__upsell-image"
+		src="<?php echo esc_url( tribe_resource_url( 'icons/filterbar-banner.png', false, null, $main ) ); ?>"
+		alt="<?php esc_attr_e( 'Filter Bar Banner Icon', 'the-events-calendar' ); ?>"
+	>
 </div>
