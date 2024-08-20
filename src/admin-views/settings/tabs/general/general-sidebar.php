4110beeb15f8b179cf95f7260d0824d721cc6d9b
Jeremy Pry <jeremy.pry@gmail.com>
2024-08-20 00:01:40 -0500

    Update for new Paragraph logic.

--- src/admin-views/settings/tabs/general/general-sidebar.php
+++ src/admin-views/settings/tabs/general/general-sidebar.php
@@ -7,17 +7,21 @@
 
 declare( strict_types=1 );
 
+use TEC\Common\Admin\Entities\Br;
 use TEC\Common\Admin\Entities\Field_Wrapper;
 use TEC\Common\Admin\Entities\Heading;
 use TEC\Common\Admin\Entities\Image;
 use TEC\Common\Admin\Entities\Link;
 use TEC\Common\Admin\Entities\Paragraph;
+use TEC\Common\Admin\Entities\Plain_Text;
 use TEC\Common\Admin\Settings_Section;
 use TEC\Common\Admin\Settings_Sidebar;
 use Tribe\Utils\Element_Attributes;
 
 // Exit if accessed directly.
 defined( 'ABSPATH' ) || exit;
+
+$break = new Br();
 
 $sidebar = new Settings_Sidebar();
 $sidebar->set_header_image(
@@ -37,8 +41,8 @@ $sidebar->add_section(
 	( new Settings_Section() )
 		->add_elements(
 			[
-				new Paragraph(
-					__( 'Looking for additional functionality including recurring events, custom meta, community events, ticket sales, and more?', 'tribe-common' )
+				( new Paragraph() )->add_child(
+					new Plain_Text( __( 'Looking for additional functionality including recurring events, custom meta, community events, ticket sales, and more?', 'tribe-common' ) )
 				),
 				new Link(
 					'https://example.com/add-ons', // todo: real link here.
@@ -57,10 +61,12 @@ $sidebar->add_section(
 					'https://example.com/welcome-page', // todo: real link here.
 					__( 'View Welcome Page', 'tribe-common' )
 				),
+				$break,
 				new Link(
 					'https://example.com/getting-started', // todo: real link here.
 					__( 'Getting started guide', 'tribe-common' )
 				),
+				$break,
 				new Link(
 					'https://example.com/knowledgebase', // todo: real link here.
 					__( 'Knowledgebase', 'tribe-common' )
@@ -73,8 +79,8 @@ $sidebar->add_section(
 	( new Settings_Section() )
 		->add_elements(
 			[
-				new Paragraph(
-					__( 'Where is my calendar?', 'tribe-common' )
+				( new Paragraph() )->add_child(
+					new Plain_Text( __( 'Where is my calendar?', 'tribe-common' ) )
 				),
 				new Link(
 					'https://example.com/right-here', // todo: real link here.
@@ -88,13 +94,15 @@ $sidebar->add_section(
 	( new Settings_Section() )
 		->add_elements(
 			[
-				new Paragraph(
-					__( 'Having trouble?', 'tribe-common' )
+				( new Paragraph() )->add_child(
+					new Plain_Text( __( 'Having trouble?', 'tribe-common' ) )
 				),
+
 				new Link(
 					'https://example.com/help', // todo: real link here.
 					__( 'Help', 'tribe-common' )
 				),
+				$break,
 				new Link(
 					'https://example.com/troubleshoot', // todo: real link here.
 					__( 'Troubleshoot', 'tribe-common' )
@@ -106,8 +114,8 @@ $sidebar->add_section(
 $sidebar->add_section(
 	( new Settings_Section() )
 		->add_element(
-			new Paragraph(
-				__( "If you're enjoying The Events Calendar, give us kudos by including a link in the footer of calendar views. It really helps us a lot.", 'tribe-common' )
+			( new Paragraph() )->add_child(
+				new Plain_Text( __( "If you're enjoying The Events Calendar, give us kudos by including a link in the footer of calendar views. It really helps us a lot.", 'tribe-common' ) )
 			)
 		)
 );
