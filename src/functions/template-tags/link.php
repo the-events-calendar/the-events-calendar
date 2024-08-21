eb0b1a7093cd3bd332c4eab27eb1019dd4bc2996
Santiago <37714098+Sjaure@users.noreply.github.com>
2024-08-21 01:28:57 -0300

    Replaced tabs with spaces as indicated

--- src/functions/template-tags/link.php
+++ src/functions/template-tags/link.php
@@ -262,10 +262,10 @@ function tribe_get_listview_dir_link( $direction = 'next', $term = null, $curren
  *                                          Default is null, which triggers a fallback to the default list view.
  *
  * @return array An associative array containing the updated display and page number.
- *		{
- *		@type string $display The view to be displayed, either 'list' or 'past'.
- *		@type int    $page    The updated page number.
- *		}
+ *      {
+ *      @type string $display The view to be displayed, either 'list' or 'past'.
+ *      @type int    $page    The updated page number.
+ *      }
  */
 function tribe_get_listview_args( $page = 1, $direction = 'next', $currently_displaying = null ) {
 	// if what we are currently displaying is not passed in, let's set a default and check $_GET
