274ac21c555d7befe586762a1911c50e92682648
Jeremy Pry <jeremy.pry@gmail.com>
2024-08-26 18:14:09 -0500

    Move Google Maps infobox to top

--- src/admin-views/settings/tabs/display/display-maps.php
+++ src/admin-views/settings/tabs/display/display-maps.php
@@ -6,6 +6,11 @@
  * @since TBD
  */
 
+use TEC\Common\Admin\Entities\Div;
+use TEC\Common\Admin\Entities\Field_Wrapper;
+use TEC\Common\Admin\Entities\Heading;
+use Tribe\Utils\Element_Classes as Classes;
+
 $tec_events_display_maps = [];
 
 
@@ -15,6 +20,32 @@ $tec_events_display_maps = [
 		'type' => 'html',
 		'html' => '<h3 id="tec-settings-events-settings-display-maps" class="tec-settings__section-header">' . esc_html_x( 'Maps', 'Map settings section header', 'the-events-calendar' ) . '</h3>',
 	],
+	( new Div( new Classes( [ 'tec-settings-infobox' ] ) ) )->add_children(
+		[
+			( new Heading(
+				__( 'Advanced Google Maps functionality', 'the-events-calendar' ),
+				3,
+				new Classes( [ 'tec-settings-infobox-title' ] )
+			) ),
+			new Field_Wrapper(
+				new Tribe__Field(
+					'tribe-google-maps-settings-infobox',
+					[
+						'type' => 'html',
+						// @TODO: The link in this and the next section should probably be different.
+						'html' => sprintf(
+							/* Translators: %1$s - opening paragraph tag, %2$s - opening anchor tag, %3$s - closing anchor tag, %4$s - closing paragraph tag */
+							__( '%1$sThe Events Calendar comes with a default API key for basic maps functionality. If you’d like to use more advanced features like custom map pins or dynamic map loads, you’ll need to get your own %2$sGoogle Maps API key%3$s.%4$s', 'the-events-calendar' ),
+							'<p>',
+							'<a href="' . esc_url( 'https://evnt.is/1bbu' ) . '" rel="noopener" target="_blank">',
+							'</a>',
+							'</p>'
+						),
+					]
+				)
+			),
+		]
+	),
 	'embedGoogleMaps'                  => [
 		'type'            => 'checkbox_bool',
 		'label'           => esc_html__( 'Enable Maps', 'the-events-calendar' ),
@@ -31,30 +62,6 @@ $tec_events_display_maps = [
 		'default'         => 10,
 		'class'           => 'google-embed-field',
 		'validation_type' => 'number_or_percent',
-	],
-	'tec-maps-infobox-start'           => [
-		'type' => 'html',
-		'html' => '<div class="tec-settings-infobox">',
-	],
-	'tec-maps-infobox-title'           => [
-		'type' => 'html',
-		'html' => '<h3 class="tec-settings-infobox-title">' . __( 'Advanced Google Maps functionality', 'the-events-calendar' ) . '</h3>',
-	],
-	/* @TODO: The link in this and the next section should probably be different. */
-	'tec-maps-infobox-content'         => [
-		'type' => 'html',
-		'html' => sprintf(
-		/* Translators: %1$s - opening paragraph tag, %2$s - opening anchor tag, %3$s - closing anchor tag, %4$s - closing paragraph tag */
-			__( '%1$sThe Events Calendar comes with a default API key for basic maps functionality. If you’d like to use more advanced features like custom map pins or dynamic map loads, you’ll need to get your own %2$sGoogle Maps API key%3$s.%4$s', 'the-events-calendar' ),
-			'<p>',
-			'<a href="' . esc_url( 'https://evnt.is/1bbu' ) . '" rel="noopener" target="_blank">',
-			'</a>',
-			'</p>'
-		),
-	],
-	'tec-maps-infobox-end'             => [
-		'type' => 'html',
-		'html' => '</div>',
 	],
 ];
 
