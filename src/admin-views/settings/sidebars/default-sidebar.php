0654fb7057ecc63a353998f7c2eac6c8932d57e4
Jeremy Pry <jeremy.pry@gmail.com>
2024-08-29 17:56:57 -0500

    Delete general sidebar, create default sidebar

--- src/admin-views/settings/sidebars/default-sidebar.php
+++ src/admin-views/settings/sidebars/default-sidebar.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Sidebar for the general settings tab.
+ * The default sidebar for the settings pages.
  *
  * @since TBD
  */
@@ -8,7 +8,6 @@
 declare( strict_types=1 );
 
 use TEC\Common\Admin\Entities\Br;
-use TEC\Common\Admin\Entities\Field_Wrapper;
 use TEC\Common\Admin\Entities\Heading;
 use TEC\Common\Admin\Entities\Image;
 use TEC\Common\Admin\Entities\Link;
@@ -16,19 +15,20 @@ use TEC\Common\Admin\Entities\Paragraph;
 use TEC\Common\Admin\Entities\Plain_Text;
 use TEC\Common\Admin\Settings_Section;
 use TEC\Common\Admin\Settings_Sidebar;
-use Tribe\Utils\Element_Attributes;
-use Tribe\Utils\Element_Classes;
+use Tribe\Utils\Element_Attributes as Attributes;
+use Tribe\Utils\Element_Classes as Classes;
 
 // Exit if accessed directly.
 defined( 'ABSPATH' ) || exit;
 
-$break = new Br();
+$break               = new Br();
+$external_attributes = new Attributes( [ 'target' => '_blank', 'rel' => 'noopener' ] );
 
 $sidebar = new Settings_Sidebar();
 $sidebar->set_header_image(
 	new Image(
 		tribe_resource_url( 'images/settings_illustration.jpg', false, null, Tribe__Events__Main::instance() ),
-		new Element_Attributes(
+		new Attributes(
 			[
 				'alt'  => '',
 				'role' => 'presentation',
@@ -37,7 +37,7 @@ $sidebar->set_header_image(
 	)
 );
 
-$sidebar->set_title( new Heading( __( 'Finding and extending your calendar', 'tribe-common' ), 2, new Element_Classes( 'tec-settings__sidebar-header' ) ) );
+$sidebar->set_title( new Heading( __( 'Finding and extending your calendar', 'tribe-common' ), 2, new Classes( 'tec-settings__sidebar-header' ) ) );
 $sidebar->add_section(
 	( new Settings_Section() )
 		->add_elements(
@@ -46,7 +46,7 @@ $sidebar->add_section(
 					new Plain_Text( __( 'Looking for additional functionality including recurring events, custom meta, community events, ticket sales, and more?', 'tribe-common' ) )
 				),
 				new Link(
-					'https://example.com/add-ons', // todo: real link here.
+					admin_url( 'edit.php?post_type=tribe_events&page=tribe-app-shop' ),
 					__( 'Check out the available add-ons.', 'tribe-common' )
 				),
 			]
@@ -59,18 +59,22 @@ $sidebar->add_section(
 		->add_elements(
 			[
 				new Link(
-					'https://example.com/welcome-page', // todo: real link here.
+					tribe( 'tec.main' )->settings()->get_url( [ Tribe__Events__Main::instance()->activation_page->welcome_slug => 1 ] ),
 					__( 'View Welcome Page', 'tribe-common' )
 				),
 				$break,
 				new Link(
-					'https://example.com/getting-started', // todo: real link here.
-					__( 'Getting started guide', 'tribe-common' )
+					'https://evnt.is/1bbv',
+					__( 'Getting started guide', 'tribe-common' ),
+					null,
+					$external_attributes
 				),
 				$break,
 				new Link(
-					'https://example.com/knowledgebase', // todo: real link here.
-					__( 'Knowledgebase', 'tribe-common' )
+					'https://evnt.is/1bbw',
+					__( 'Knowledgebase', 'tribe-common' ),
+					null,
+					$external_attributes
 				),
 			]
 		)
@@ -84,7 +88,7 @@ $sidebar->add_section(
 					new Plain_Text( __( 'Where is my calendar?', 'tribe-common' ) )
 				),
 				new Link(
-					'https://example.com/right-here', // todo: real link here.
+					tribe( 'tec.main' )->getLink(),
 					__( 'Right here', 'tribe-common' )
 				),
 			]
@@ -100,12 +104,12 @@ $sidebar->add_section(
 				),
 
 				new Link(
-					'https://example.com/help', // todo: real link here.
+					admin_url( 'edit.php?post_type=tribe_events&page=tec-events-help' ),
 					__( 'Help', 'tribe-common' )
 				),
 				$break,
 				new Link(
-					'https://example.com/troubleshoot', // todo: real link here.
+					admin_url( 'edit.php?post_type=tribe_events&page=tec-troubleshooting' ),
 					__( 'Troubleshoot', 'tribe-common' )
 				),
 			]
