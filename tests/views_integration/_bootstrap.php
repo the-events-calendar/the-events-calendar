1d58c8d83be53110effdf754586048f1676de305
Dimitrios Pantazis <dpanta94@gmail.com>
2024-09-17 18:34:21 +0300

    Fix bootstrap file

--- tests/views_integration/_bootstrap.php
+++ tests/views_integration/_bootstrap.php
@@ -1,13 +1,10 @@
 <?php
 
-use Tribe\Events\Views\V2\Service_Provider;
-
 // Disable Event Status.
 define( 'TEC_EVENT_STATUS_DISABLED', true );
 
 // Let's  make sure Views v2 are activated if not.
 putenv( 'TRIBE_EVENTS_V2_VIEWS=1' );
-tribe_register_provider( Service_Provider::class );
 
 // Let's make sure to set rewrite rules.
 global $wp_rewrite;
