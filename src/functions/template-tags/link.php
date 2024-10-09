47f6b756b12a08ec35b73ff36088d2bea3141166
Santiago <37714098+Sjaure@users.noreply.github.com>
2024-10-09 12:18:07 -0300

    Added missing line break

--- src/functions/template-tags/link.php
+++ src/functions/template-tags/link.php
@@ -491,6 +491,7 @@ function tribe_get_event_website_link( $event = null, $label = null, $target = '
  * @since 5.5.0
  *
  * @param null|int $post_id Optional. The event post object or event ID. Defaults to null, which uses the current event ID.
+ * 
  * @return string The formatted title for the event website link.
  */
 function tribe_events_get_event_website_title( $post_id = null ) {
