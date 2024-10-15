58d26ce3b33d65049255afc2814b49a8ef1f8754
Camwyn <stephen@theeventscalendar.com>
2024-10-15 16:38:36 -0400

    version

--- the-events-calendar.php
+++ the-events-calendar.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: The Events Calendar
  * Description: The Events Calendar is a carefully crafted, extensible plugin that lets you easily share your events. Beautiful. Solid. Awesome.
- * Version: 6.7.0
+ * Version: 6.7.1
  * Requires at least: 6.3
  * Requires PHP: 7.4
  * Author: The Events Calendar
