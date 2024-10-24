b6c6eecc74a3847d49c110cf3f2e4bcb89b9de7e
Camwyn <stephen@theeventscalendar.com>
2024-10-24 18:22:45 -0400

    make the script tell us where it is - be more verbose

--- bin/action-before-zip.sh
+++ bin/action-before-zip.sh
@@ -1,22 +1,22 @@
-# Move the Webpack 4 directory out of the way to prevent conflicts.
+echo "Moving the Webpack 4 directory out of the way to prevent conflicts."
 mv node_modules/webpack /tmp/_webpack
 mv node_modules/webpack-cli /tmp/_webpack-cli
 
-# Move configuration files out of the way.
+echo "Moving configuration files out of the way."
 mv webpack.config.js _webpack.config.js
 mv babel.config.json _babel.config.json
 
-# Build the JS with the correct Webpack version.
+echo "Building the JS with the correct Webpack version."
 npx \
 	--package="@tanstack/react-query" \
 	--package="@wordpress/scripts@27.9.0" \
 	--yes -- \
 	wp-scripts build --webpack-src-dir=src/resources/packages/ --output-path=src/build/
 
-# Move the Webpack 4 directory back to node_modules.
+echo "Moving the Webpack 4 directory back to node_modules."
 mv /tmp/_webpack node_modules/webpack
 mv /tmp/_webpack-cli node_modules/webpack-cli
 
-# Move configuration files back.
+echo "Moving configuration files back."
 mv _webpack.config.js webpack.config.js
 mv _babel.config.json babel.config.json
