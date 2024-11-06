b38110f35e7c11e96539adfab226125964e209f6
Camwyn <stephen@theeventscalendar.com>
2024-11-06 16:12:15 -0500

    remove build files before gitignor-ing them

--- src/build/wizard/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-data', 'wp-data-controls', 'wp-dom-ready', 'wp-element', 'wp-i18n'), 'version' => 'f62174d6ccde02c77d4b');
