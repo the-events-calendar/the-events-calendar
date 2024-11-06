d201a7f93b59dfff9127f44da1611453c3b60928
jesseeproductions <info@jesseeproductions.com>
2024-11-06 11:30:18 -0500

    Additional dependency updates

--- jest.setup.js
+++ jest.setup.js
@@ -1,6 +1,16 @@
-/**
- * External dependencies
- */
+// jest.setup.js
+
+// Mock TextEncoder and TextDecoder if they are not available globally.
+if (typeof TextEncoder === 'undefined') {
+console.log('require(\'util\').TextEncoder', require('util').TextEncoder);
+    global.TextEncoder = require('util').TextEncoder;
+}
+
+if (typeof TextDecoder === 'undefined') {
+    global.TextDecoder = require('util').TextDecoder;
+}
+
+// Now, import all other dependencies.
 import moment from 'moment-timezone';
 import React from 'react';
 import renderer from 'react-test-renderer';
@@ -8,28 +18,28 @@ import $ from 'jquery';
 import Enzyme, { shallow, render, mount } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
-Enzyme.configure( { adapter: new Adapter() } );
+Enzyme.configure({ adapter: new Adapter() });
 
 global.jQuery = $;
 global.$ = $;
 global.wp = {
-	element: React,
-	api: {},
-	apiRequest: () => $.Deferred(),
-	components: {},
-	data: {},
-	blockEditor: {},
-	editor: {},
-	hooks: {},
+    element: React,
+    api: {},
+    apiRequest: () => $.Deferred(),
+    components: {},
+    data: {},
+    blockEditor: {},
+    editor: {},
+    hooks: {},
 };
 global.shallow = shallow;
 global.render = render;
 global.mount = mount;
 global.renderer = renderer;
 global.console = {
-	error: jest.fn(),
-	log: jest.fn(),
-	warning: jest.fn(),
+    error: jest.fn(),
+    log: jest.fn(),
+    warning: jest.fn(),
 };
 
-moment.tz.setDefault( 'UTC' );
+moment.tz.setDefault('UTC');
