305d4990728263fee0722abd44d185e42f17c1ce
Andras <andras@theeventscalendar.com>
2024-11-07 22:48:19 +0100

    Fix a placeholder

--- src/Events/Custom_Tables/V1/Events/Event_Cleaner/Event_Cleaner.php
+++ src/Events/Custom_Tables/V1/Events/Event_Cleaner/Event_Cleaner.php
@@ -36,7 +36,7 @@ class Event_Cleaner {
 				FROM {$wpdb->posts}
 			    	INNER JOIN {$occurrence_table} ON {$wpdb->posts}.ID = {$occurrence_table}.post_id
 				WHERE {$wpdb->posts}.post_type = %s
-					AND {$occurrence_table}.end_date_utc <= DATE_SUB( CURRENT_TIMESTAMP(), INTERVAL %d %3s )
+					AND {$occurrence_table}.end_date_utc <= DATE_SUB( CURRENT_TIMESTAMP(), INTERVAL %d %s )
 					AND {$wpdb->posts}.post_status NOT IN ( 'trash', 'tribe-ignored' )
 				GROUP BY {$occurrence_table}.post_id
 				HAVING COUNT(*) = 1
