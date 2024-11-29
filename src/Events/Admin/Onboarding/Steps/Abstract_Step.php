3b44cbf66536afad731c4c77aa824339dc26dbd0
Camwyn <stephen@theeventscalendar.com>
2024-11-29 17:29:58 -0500

    Don't activate ET until we get there

--- src/Events/Admin/Onboarding/Steps/Abstract_Step.php
+++ src/Events/Admin/Onboarding/Steps/Abstract_Step.php
@@ -51,9 +51,7 @@ abstract class Abstract_Step implements Contracts\Step_Interface {
 			return $response;
 		}
 
-		$response = static::process( $response, $request );
-
-		return $response;
+		return static::process( $response, $request );
 	}
 
 	/**
@@ -68,7 +66,7 @@ abstract class Abstract_Step implements Contracts\Step_Interface {
 	public static function tab_check( $request ) {
 		$params = $request->get_params();
 		// If the current tab is less than this tab, we don't need to do anything yet.
-		return isset( $params['currentTab'] ) && $params['currentTab'] <= static::TAB_NUMBER;
+		return isset( $params['currentTab'] ) && $params['currentTab'] >= static::TAB_NUMBER;
 	}
 
 	/**
