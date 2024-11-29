d6fd50232fb5b28bd8728a9aeb9cb71988c74a70
Camwyn <stephen@theeventscalendar.com>
2024-11-29 13:44:49 -0500

    Add validation to Organizer Tab. Fix Continue button en/disabled state

--- src/resources/packages/wizard/components/tabs/organizer/tab.tsx
+++ src/resources/packages/wizard/components/tabs/organizer/tab.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { __, _x } from "@wordpress/i18n";
 import { TextControl, Button } from "@wordpress/components";
-import { useState } from '@wordpress/element';
+import { useState, useEffect } from '@wordpress/element';
 import { useSelect } from "@wordpress/data";
 import { SETTINGS_STORE_KEY } from "../../../data";
 import NextButton from "../../buttons/next";
@@ -22,18 +22,77 @@ const OrganizerContent = ({moveToNextTab, skipToNextTab}) => {
 	const [name, setName] = useState(organizer.name || "");
 	const [phone, setPhone] = useState(organizer.phone || "");
 	const [website, setWebsite] = useState(organizer.website || "");
-	const [email, setEmail] = useState(organizer.email || "");
+	const [email, setEmail] = useState(organizer.email || "");    const [showPhone, setShowPhone] = useState(false);
+	const [showWebsite, setShowWebsite] = useState(false);
+	const [showEmail, setShowEmail] = useState(false);
+	const [canContinue, setCanContinue] = useState(false);
 
-	// Check if any fields are filled.
+	// Check if any fields are pre-filled.
 	const disabled = !!organizer.name || !!organizer.phone || !!organizer.website || !!organizer.email;
 
-	/**
-	 * Function to show hidden fields.
-	 */
-	const showField = (event) => {
+	// Compute whether the "Continue" button should be enabled
+    useEffect(() => {
+        const fieldsToCheck = {
+            'organizer-name': isValidName(),
+            'organizer-phone': isValidPhone(),
+            'organizer-website': isValidWebsite(),
+            'organizer-email': isValidEmail(),
+		};
+		setCanContinue(Object.values(fieldsToCheck).every((field) => !!field));
+    }, [name, phone, website, email, showPhone, showWebsite, showEmail]);
+
+	const isValidName = () => {
+		return !!name;
+	}
+
+	const isValidEmail = () => {
+		const emailPattern = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+		const isValid = !showEmail || emailPattern.test(email);
+		const ele = document.getElementById("organizer-email");
+
+		if (showEmail && !isValid) {
+			ele?.classList.add("tec-events-onboarding__form-field--invalid");
+		} else if (showEmail) {
+			ele?.classList.remove("tec-events-onboarding__form-field--invalid");
+		}
+
+		return isValid;
+	}
+
+	const isValidPhone = () => {
+		const phonePattern = /^\+?\d?[\s.-]?(?:\(\d{3}\)|\d{3})[\s.-]?\d{3}[\s.-]?\d{4}$/;
+		const isValid = !showPhone || phonePattern.test(phone);
+		const ele = document.getElementById("organizer-phone");
+
+		if (showPhone && !isValid) {
+			ele?.classList.add("tec-events-onboarding__form-field--invalid");
+		} else if (showPhone) {
+			ele?.classList.remove("tec-events-onboarding__form-field--invalid");
+		}
+
+		return isValid;
+	}
+
+	const isValidWebsite = () => {
+		const websitePattern = /^(http|https):\/\/[^ "]+?$/;
+		const isValid = !showWebsite || websitePattern.test(website);
+		const ele = document.getElementById("organizer-website");
+
+		if (showWebsite && !isValid) {
+			ele?.classList.add("tec-events-onboarding__form-field--invalid");
+		} else if (showWebsite) {
+			ele?.classList.remove("tec-events-onboarding__form-field--invalid");
+		}
+
+		return isValid;
+	}
+
+	const showField = (event, fieldSetter) => {
 		const ele = event.target;
 		ele.nextSibling.classList.remove("tec-events-onboarding__form-field--hidden");
 		ele.style.display = "none";
+
+		fieldSetter(true);
 	}
 
 	// Create tabSettings object to pass to NextButton.
@@ -60,6 +119,7 @@ const OrganizerContent = ({moveToNextTab, skipToNextTab}) => {
 			<div className="tec-events-onboarding__form-wrapper">
 				<TextControl
 					__nextHasNoMarginBottom
+					id="organizer-name"
 					label={__("Organizer name", "the-events-calendar")}
 					onChange={setName}
 					defaultValue={name}
@@ -69,8 +129,9 @@ const OrganizerContent = ({moveToNextTab, skipToNextTab}) => {
 				{phone ? "" :
 				<Button
 					__next40pxDefaultSize
-					onClick={showField}
+					onClick={(event) => showField(event, setShowPhone)}
 					variant="tertiary"
+					className="tec-events-onboarding__form-field-trigger"
 				>
 					{_x("Add a phone number +", "Direction to add a phone number followed by a plus sign", "the-events-calendar")}
 				</Button>}
@@ -82,14 +143,15 @@ const OrganizerContent = ({moveToNextTab, skipToNextTab}) => {
 					onChange={setPhone}
 					type="tel"
 					defaultValue={phone}
-					disabled={disabled}
+					disabled={!showPhone || disabled}
 					placeholder={__("Enter phone number", "the-events-calendar")}
 				/>
 				{website ? "" :
 				<Button
 					__next40pxDefaultSize
-					onClick={showField}
+					onClick={(event) => showField(event, setShowWebsite)}
 					variant="tertiary"
+					className="tec-events-onboarding__form-field-trigger"
 				>
 					{_x("Add a website +", "Direction to add a website followed by a plus sign", "the-events-calendar")}
 				</Button>}
@@ -101,14 +163,15 @@ const OrganizerContent = ({moveToNextTab, skipToNextTab}) => {
 					onChange={setWebsite}
 					type="url"
 					defaultValue={website}
-					disabled={disabled}
+					disabled={!showWebsite || disabled}
 					placeholder={__("Enter website", "the-events-calendar")}
 				/>
 				{email ? "" :
 				<Button
 					__next40pxDefaultSize
-					onClick={showField}
+					onClick={(event) => showField(event, setShowEmail)}
 					variant="tertiary"
+					className="tec-events-onboarding__form-field-trigger"
 				>
 					{_x("Add an email +", "Direction to add an email followed by a plus sign", "the-events-calendar")}
 				</Button>}
@@ -120,12 +183,12 @@ const OrganizerContent = ({moveToNextTab, skipToNextTab}) => {
 					onChange={setEmail}
 					type="email"
 					defaultValue={email}
-					disabled={disabled}
+					disabled={!showEmail || disabled}
 					placeholder={__("Enter email", "the-events-calendar")}
 				/>
 			</div>
 
-			 <p className="tec-events-onboarding__element--center"><NextButton disabled={false} moveToNextTab={moveToNextTab}  tabSettings={tabSettings}/></p>
+			 <p className="tec-events-onboarding__element--center"><NextButton disabled={!canContinue} moveToNextTab={moveToNextTab}  tabSettings={tabSettings}/></p>
 			 <p className="tec-events-onboarding__element--center"><SkipButton skipToNextTab={skipToNextTab} /></p>
 		</>
 	);
