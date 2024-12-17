4ff0fec23678a002358b903415a0dbb4c2c75e8b
Luca Tumedei <luca@theaveragedev.com>
2024-12-17 15:30:48 +0100

    refactor(Editor/Blocks) move assets method to trait
    All the blocks implementations would define an identical `assets`
    method. This change moves the `assets` method to the `Block_Trait` to
    avoid duplicated code.

---
+++ src/Tribe/Editor/Blocks/Block_Trait.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * A trait to group the methods common to all The Events Calendar blocks.
+ *
+ * @since TBD
+ */
+
+/**
+ * Trait Block_Trait.
+ *
+ * @since TBD
+ */
+trait Block_Trait {
+
+	/**
+	 * Register the Assets for when this block is active
+	 *
+	 * @since 4.7
+	 * @since TBD Moved to a trait to avoid code duplication.
+	 *
+	 * @return void
+	 */
+	public function assets() {
+		tribe_asset(
+			tribe( 'tec.main' ),
+			'tribe-events-block-' . $this->slug(),
+			'app/' . $this->slug() . '/frontend.css',
+			[],
+			'wp_enqueue_scripts',
+			[
+				'conditionals' => [ $this, 'has_block' ],
+			]
+		);
+	}
+}
