254bb319e9fce611ec67cbf83ab2c56dd37f8a1c
Luca Tumedei <luca@theaveragedev.com>
2024-12-19 14:49:45 +0100

    build(webpack.config.js) fix nested package build

--- webpack.config.js
+++ webpack.config.js
@@ -16,16 +16,19 @@ const {dirname, basename,extname} = require('path');
  */
 
 /**
+ * Compiles a list of entry points for `@wordpress/scripts` to build.
  *
  * @param {Locations} locations A list of directories to search for entry points recursively.
+ *
+ * @return {Object<string,string>} A map from entry points to the file to build.
  */
-function getLegacyEntryPoints(locations) {
+function compileCustomEntryPoints(locations) {
 	const entries = {};
-	Object.keys(locations).forEach((fileRelativePath) => {
-		const schema = locations[fileRelativePath];
+	Object.keys(locations).forEach(( location) => {
+		const schema = locations[ location];
 		const fileExtensions = schema.fileExtensions;
 		const fileMatcher = schema.fileMatcher;
-		const locationAbsolutePath = __dirname + fileRelativePath;
+		const locationAbsolutePath = __dirname +  location;
 
 		if (!existsSync(locationAbsolutePath)) {
 			return;
@@ -48,6 +51,7 @@ function getLegacyEntryPoints(locations) {
 				return;
 			}
 
+			const fileRelativePath = fileAbsolutePath.replace(locationAbsolutePath, '');
 			const fileName = basename(fileAbsolutePath);
 			if (!fileMatcher(fileName, fileRelativePath, fileAbsolutePath)) {
 				return;
@@ -61,6 +65,40 @@ function getLegacyEntryPoints(locations) {
 }
 
 /**
+ * A list of package roots discovered so far.
+ *
+ * @type {string[]}
+ */
+const packageRoots = [];
+
+/**
+ * Returns whether a file is a package index file or not.
+ * Package index files are index files that are not found in sub-directories of previously discovered packages.
+ * This function leverages the fact that the node `readdir` function will scan directories depth-first.
+ *
+ * @param {string} fileRelativePath The file path relative to the schema location.
+ *
+ * @returns {boolean} Whether the file is a package index file or not.
+ */
+function isPackageRootIndex(fileRelativePath) {
+	const dirFrags = dirname(fileRelativePath).split('/').filter((frag) => frag !== '').reverse();
+	let curDir = dirFrags.pop();
+	let prevDir = null;
+	while (dirFrags.length !== 0 && prevDir !== curDir) {
+		if (packageRoots.includes(curDir)) {
+			return false;
+		}
+		prevDir = curDir;
+		curDir += '/' + dirFrags.pop();
+	}
+
+	return true;
+}
+
+/**
+ * The Events Calendar legacy JavaScript asset files.
+ * Javascript files are in `/src/resources/js` and each file should be built.
+ *
  * @type {LocationSchema}
  */
 const TECLegacyJsSchema = {
@@ -70,27 +108,42 @@ const TECLegacyJsSchema = {
 };
 
 /**
+ * The Events Calendar legacy PostCSS schema.
+ * PostCSS files are in `/src/resources/postcss`, index files are those not prefixed with `_`.
+ *
  * @type {LocationSchema}
  */
-const TECLegacyPostcssSchema = {
+const TECPostCssSchema = {
 	fileExtensions: ['.pcss'],
 	fileMatcher: (fileName) => !fileName.startsWith('_'),
 	getEntryPointName: (fileRelativePath) => 'css/' + fileRelativePath.replace('.pcss','')
 };
 
 /**
+ * The Events Calendar legacy Blocks schema to build front-end CSS files.
+ * Blocks are in `/src/modules` and their style are in `/src/styles`, by block.
+ *
  * @type {LocationSchema}
  */
-const TECLegacyBlocksFrontendPcssSchema = {
+const TECLegacyBlocksFrontendPostCssSchema = {
 	fileExtensions: ['.pcss'],
 	fileMatcher: (fileName) => fileName === 'frontend.pcss',
 	getEntryPointName: (fileRelativePath) => 'app/' + basename(dirname(fileRelativePath)) + '/frontend.css'
 };
 
+/**
+ * The Events Calendar non-block package schema.
+ * Packages are in `/src/packages` and each define a root index file. Sub-directory index files are ignored.
+ *
+ * @type {LocationSchema}
+ */
 const TECPackageSchema = {
 	fileExtensions: ['.js', '.jsx', '.ts', '.tsx'],
-	fileMatcher: (fileName, fileRelativePath) => fileName.match(/index\.(js|jsx|ts|tsx)$/),
-	getEntryPointName: (fileRelativePath) => dirname(fileRelativePath)
+	fileMatcher: (fileName, fileRelativePath, fileAbsolutePath) => fileName.match(/index\.(js|jsx|ts|tsx)$/) && isPackageRootIndex(fileRelativePath),
+	getEntryPointName: (fileRelativePath) => {
+		packageRoots.push(dirname(fileRelativePath));
+		return dirname(fileRelativePath);
+	}
 }
 /// END TYSON
 
@@ -100,17 +153,17 @@ const TECPackageSchema = {
 
 
 // This is what would be imported from the `@stellarwp/tyson` package:
-// import {JsSchema, PostcssSchema, getLegacyEntryPoints} from '@stellarwp/tyson';
+// import {TECLegacyJsSchema, TECPostCssSchema, TECLegacyBlocksFrontendPostCssSchema, TECPackageSchema, compileCustomEntryPoints} from '@stellarwp/tyson';
 
-const legacyEntryPoints = getLegacyEntryPoints({
+const customEntryPoints = compileCustomEntryPoints({
 	'/src/resources/js': TECLegacyJsSchema,
-	'/src/resources/postcss': TECLegacyPostcssSchema,
-	'/src/styles': TECLegacyBlocksFrontendPcssSchema,
+	'/src/resources/postcss': TECPostCssSchema,
+	'/src/styles': TECLegacyBlocksFrontendPostCssSchema,
 	'/src/resources/packages': TECPackageSchema,
 });
 // Blocks from `/src/modules/index.js` are built to `/build/app/main.js`.
-legacyEntryPoints['app/main.js'] = __dirname + '/src/modules/index.js';
-legacyEntryPoints['app/widgets.js'] = __dirname + '/src/modules/widgets/index.js';
+customEntryPoints['app/main.js'] = __dirname + '/src/modules/index.js';
+customEntryPoints['app/widgets.js'] = __dirname + '/src/modules/widgets/index.js';
 
 module.exports = {
 	...defaultConfig,
@@ -118,7 +171,7 @@ module.exports = {
 		entry:(buildType) => {
 			const defaultEntryPoints = defaultConfig.entry(buildType);
 			return {
-				...defaultEntryPoints, ...legacyEntryPoints
+				...defaultEntryPoints, ...customEntryPoints
 			};
 		},
 	},
