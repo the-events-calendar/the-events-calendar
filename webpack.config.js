7e1b6e774e8e8c6c1117c79254e7c394e0234f0c
Luca Tumedei <luca@theaveragedev.com>
2025-01-06 13:57:10 +0100

    doc(webpack.config.js) add considerations about todos

--- webpack.config.js
+++ webpack.config.js
@@ -317,5 +317,17 @@ module.exports = {
 };
 
 // @todo namespace for the project from dir or override from command
+// This will be addressed in the tyson binary, e.g. with `--namespace "@acme/project"
+// But is this required? This is a setting set in the `package.json` file I'm not sure
+// the packager/builder should touch at all.
+
 // @todo what to do with images moved/copied to /build/images?
+// images from the `src/resources/images` directory that are referenced in files (e.g. PCSS)
+// that are compiled are optimized and moved to the `build/images` directory. There is "duplication"
+// where the original image (from `src/resources/images`) is shipped when only the build one (from
+// `build/images` is actually used.
+// @todo detect images in `src/resources/images` that are used outside of compiled files.
+
 // @todo what to do with -rtl styles?
+// I _think_ they should be handled at the Assets level, loading the RTL styles on top of the existing
+// styles when `is_rtl` is true.
