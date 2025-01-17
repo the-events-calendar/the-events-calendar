4ea3770afd4d23fbba14f6e90696e354fcd9e118
Eddie <stratease@gmail.com>
2025-01-17 15:58:48 +0100

    Updated snapshot with new attrs

--- tests/views_integration/Tribe/Events/Views/V2/Partials/Day_View/Event/__snapshots__/Featured_ImageTest__test_render_with_event_with_featured_image__1.php
+++ tests/views_integration/Tribe/Events/Views/V2/Partials/Day_View/Event/__snapshots__/Featured_ImageTest__test_render_with_event_with_featured_image__1.php
@@ -2,13 +2,14 @@
 	<a
 		href="http://test.tri.be/?tribe_events=single-event-1"
 		title="Single Event 1"
+		aria-label="Single Event 1"
 		rel="bookmark"
 		class="tribe-events-calendar-day__event-featured-image-link"
 	>
 		<img
 			src="http://test.tri.be/wp-content/uploads/2018/08/featured-image.jpg"
 										alt=""
-										title="featured-image.jpg"
+										title="Single Event 1"
 						class="tribe-events-calendar-day__event-featured-image"
 		/>
 	</a>
