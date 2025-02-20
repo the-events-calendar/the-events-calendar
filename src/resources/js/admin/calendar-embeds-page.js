1e4e2262a8028c48a6d9de9c2d8e5238ebba16aa
Mike Cotton <codingmusician@gmail.com>
2025-02-20 15:03:52 -0600

    code tweak

--- src/resources/js/admin/calendar-embeds-page.js
+++ src/resources/js/admin/calendar-embeds-page.js
@@ -1,6 +1,7 @@
 /* global ClipboardJS */
 ( function ( cb ) {
-		const btns = document
-			.querySelectorAll( '.tec-events-calendar-embeds__snippet-modal-copy-button' );
+		const btns = document.querySelectorAll(
+			'.tec-events-calendar-embeds__snippet-modal-copy-button'
+		);
 		new cb( btns );
 } )( ClipboardJS );
