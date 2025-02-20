3372c3822a5ee2c2a25462373b5974b14a3147e9
Mike Cotton <codingmusician@gmail.com>
2025-02-20 12:04:11 -0600

    list table test

---
+++ tests/wpunit/TEC/Events/Calendar_Embeds/Admin/List_TableTest.php
@@ -0,0 +1,51 @@
+<?php
+namespace TEC\Events\Calendar_Embeds;
+
+use TEC\Events\Calendar_Embeds\Admin\List_Table;
+use Tribe\Tests\Traits\With_Uopz;
+use Spatie\Snapshots\MatchesSnapshots;
+
+class List_TableTest extends \Codeception\TestCase\WPTestCase {
+	use With_Uopz;
+	use MatchesSnapshots;
+
+	public function testManageColumns() {
+		$new_columns = tribe( List_Table::class )->manage_columns( [] );
+
+		$this->assertEquals( '<input type="checkbox" />', $new_columns['cb'] );
+		$this->assertEquals( 'Calendar Embeds', $new_columns['title'] );
+		$this->assertEquals( 'Categories', $new_columns['event_categories'] );
+		$this->assertEquals( 'Tags', $new_columns['post_tags'] );
+		$this->assertEquals( 'Embed Snippet', $new_columns['snippet'] );
+	}
+
+	public function test_manage_columns_with_filtered_columns() {
+
+		add_filter( 'tec_events_calendar_embeds_list_table_columns', function( $new_columns ) {
+			$new_columns['custom_column'] = 'Custom Column';
+			return $new_columns;
+		} );
+
+		$new_columns = tribe( List_Table::class )->manage_columns( [] );
+
+		$this->assertEquals( '<input type="checkbox" />', $new_columns['cb'] );
+		$this->assertEquals( 'Calendar Embeds', $new_columns['title'] );
+		$this->assertEquals( 'Categories', $new_columns['event_categories'] );
+		$this->assertEquals( 'Tags', $new_columns['post_tags'] );
+		$this->assertEquals( 'Embed Snippet', $new_columns['snippet'] );
+		$this->assertEquals( 'Custom Column', $new_columns['custom_column'] );
+	}
+
+	public function testManageColumnContent() {
+		// Create Calendar Embed
+		$post_id = wp_insert_post( [
+			'post_title' => 'Test Embed',
+			'post_type'  => Calendar_Embeds::POSTTYPE,
+		] );
+
+		ob_start();
+		tribe( List_Table::class )->manage_column_content( 'snippet', $post_id );
+		$snippet = ob_get_clean();
+		$this->assertMatchesSnapshot( $snippet );
+	}
+}
---
+++ tests/wpunit/TEC/Events/Calendar_Embeds/Admin/__snapshots__/List_TableTest__testManageColumnContent__1.php
@@ -0,0 +1,27 @@
+<?php return '<div id="tec_events_calendar_embeds_snippet_4" class="hidden">
+	<div>
+		<div class="tec-events-calendar-embeds__snippet-modal-text">
+			Copy and paste this code to embed the calendar on your website:		</div>
+		<textarea
+			id="tec_events_calendar_embeds_snippet_code_4"
+			class="tec-events-calendar-embeds__snippet-modal-textarea"
+			aria-label="Embed snippet code"
+			rows="3"
+			readonly>&lt;iframe src=&quot;http://wordpress.test/?post_type=tec_calendar_embed&#038;p=4&quot; width=&quot;100%&quot; height=&quot;600&quot; frameborder=&quot;0&quot;&gt;&lt;/iframe&gt;</textarea>
+		<button
+			class="button button-primary tec-events-calendar-embeds__snippet-modal-copy-button"
+			aria-controls="tec_events_calendar_embeds_snippet_code_4"
+			data-clipboard-action="copy"
+			data-clipboard-target="#tec_events_calendar_embeds_snippet_code_4"
+		>
+			Copy Embed Snippet		</button>
+	</div>
+</div>
+<a
+	name="Embed Snippet"
+	href="/?TB_inline&width=370&height=200&inlineId=tec_events_calendar_embeds_snippet_4"
+	class="thickbox button"
+>
+	Get Embed Snippet
+</a>
+';
