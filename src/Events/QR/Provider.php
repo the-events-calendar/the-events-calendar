ec71d3fdda91ee816a6f9a4c289ab0ef78ddfe34
George Pattichis <gpattihis@gmail.com>
2025-03-05 02:04:53 +0200

    QR Provider, Controller and Settings

---
+++ src/Events/QR/Provider.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Service Provider for interfacing with TEC\Common\phpqrcode
+ *
+ * @since   TBD
+ *
+ * @package TEC\Events\QR
+ */
+
+namespace TEC\Events\QR;
+
+use TEC\Common\Contracts\Service_Provider;
+
+/**
+ * Class Provider
+ *
+ * @since   TBD
+ * @package TEC\Events\QR
+ */
+class Provider extends Service_Provider {
+
+	/**
+	 * Handles the registering of the provider.
+	 *
+	 * @since TBD
+	 */
+	public function register() {
+		$this->container->register_on_action( 'tribe_plugins_loaded', Controller::class );
+	}
+}
