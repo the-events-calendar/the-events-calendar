99a4c3ef2dc26c7b73f200461ff0e0f179ecdcbb
Andras Guseo <andras@theeventscalendar.com>
2025-03-07 15:27:41 +0100

    Formatting multi-line and adding translator comment

--- src/admin-views/tribe-options-timezones.php
+++ src/admin-views/tribe-options-timezones.php
@@ -11,7 +11,9 @@ $args                = [
 ];
 $enable_button_url   = esc_url( wp_nonce_url( add_query_arg( $args, tribe( 'tec.main' )->settings()->get_url() ), 'timezone-settings' ) );
 $enable_button_text  = esc_html__( 'Update Time Zone Data', 'the-events-calendar' );
-$enable_button_help  = sprintf( __( 'Click this button to update your database and take advantage of additional time zone capabilities. Please <a href="%s" target="_blank">configure WordPress</a> to use the correct time zone before clicking this button!', 'the-events-calendar' ),
+$enable_button_help  = sprintf(
+	/* Translators: %s - URL pointing to the General Settings page. */
+	__( 'Click this button to update your database and take advantage of additional time zone capabilities. Please <a href="%s" target="_blank">configure WordPress</a> to use the correct time zone before clicking this button!', 'the-events-calendar' ),
 	esc_url( get_admin_url( null, 'options-general.php' ) )
 );
 
