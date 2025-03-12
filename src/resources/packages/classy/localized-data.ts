19204833c22de641e2ab3c99b1400bce5d7be7b0
Luca Tumedei <luca@theaveragedev.com>
2025-03-12 13:37:03 +0100

    refactor(Classy) point localized data to correct location

--- src/resources/packages/classy/localized-data.ts
+++ src/resources/packages/classy/localized-data.ts
@@ -1,3 +1,3 @@
 import {LocalizedData} from "./types/LocalizedData";
 
-export const localizedData:LocalizedData = window.tec.classy;
+export const localizedData:LocalizedData = window.tec.events.classy.data;
