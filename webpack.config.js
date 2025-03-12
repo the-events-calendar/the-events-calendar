caf705173e5c963ed5701349de07a04e831003e2
Luca Tumedei <luca@theaveragedev.com>
2025-03-12 11:49:39 +0100

    buid(package.json, webpack.config.js) update to use tyson 0.0.3

--- webpack.config.js
+++ webpack.config.js
@@ -9,14 +9,14 @@ const {readdirSync, statSync, existsSync} = require('fs');
 const defaultConfig = require('@wordpress/scripts/config/webpack.config');
 
 const {
-	TECLegacyJs,
-	TECPostCss,
-	TECLegacyBlocksFrontendPostCss,
-	TECPackage,
-	compileCustomEntryPoints,
-	exposeEntry,
-	doNotPrefixSVGIdsClasses,
-	WindowAssignPropertiesPlugin,
+  createTECLegacyJs,
+  createTECPostCss,
+  createTECLegacyBlocksFrontendPostCss,
+  createTECPackage,
+  compileCustomEntryPoints,
+  exposeEntry,
+  doNotPrefixSVGIdsClasses,
+  WindowAssignPropertiesPlugin,
 } = require('@stellarwp/tyson');
 
 /**
@@ -25,41 +25,41 @@ const {
  * See: https://webpack.js.org/concepts/entry-points/#multi-page-application
  */
 const customEntryPoints = compileCustomEntryPoints({
-	/**
-	 * All existing Javascript files will be compiled to ES6, most will not be changed at all,
-	 * minified and cleaned up.
-	 * This is mostly a pass-thru with the additional benefit that the compiled packages will be
-	 * exposed on the `window.tec` object.
-	 * E.g. the `src/resources/js/admin-ignored-events.js` file will be compiled to
-	 * `/build/js/admin-ignored-events.js` and exposed on `window.tec.adminIgnoredEvents`.
-	 */
-	'/src/resources/js': TECLegacyJs,
+  /**
+   * All existing Javascript files will be compiled to ES6, most will not be changed at all,
+   * minified and cleaned up.
+   * This is mostly a pass-thru with the additional benefit that the compiled packages will be
+   * exposed on the `window.tec.events` object.
+   * E.g. the `src/resources/js/admin-ignored-events.js` file will be compiled to
+   * `/build/js/admin-ignored-events.js` and exposed on `window.tec.events.adminIgnoredEvents`.
+   */
+  '/src/resources/js': createTECLegacyJs('tec.events'),
 
-	/**
-	 * Compile, recursively, the PostCSS file using PostCSS nesting rules.
-	 * By default, the `@wordpress/scripts` configuration would compile files using the CSS
-	 * nesting syntax (https://developer.mozilla.org/en-US/docs/Web/CSS/CSS_nesting) where
-	 * the `&` symbol indicates the parent element.
-	 * The PostCSS syntax followed in TEC files will instead use the `&` symbol to mean "this element".
-	 * Handling this correctly requires adding a PostCSS processor specific to the PostCSS files that
-	 * will handle the nesting correctly.
-	 */
-	'/src/resources/postcss': TECPostCss,
+  /**
+   * Compile, recursively, the PostCSS file using PostCSS nesting rules.
+   * By default, the `@wordpress/scripts` configuration would compile files using the CSS
+   * nesting syntax (https://developer.mozilla.org/en-US/docs/Web/CSS/CSS_nesting) where
+   * the `&` symbol indicates the parent element.
+   * The PostCSS syntax followed in TEC files will instead use the `&` symbol to mean "this element".
+   * Handling this correctly requires adding a PostCSS processor specific to the PostCSS files that
+   * will handle the nesting correctly.
+   */
+  '/src/resources/postcss': createTECPostCss('tec.events'),
 
-	/**
-	 * This deals with existing Blocks frontend styles being compiled separately.
-	 * The main function of this configuration schema is to ensure they are placed correctly.
-	 */
-	'/src/styles': TECLegacyBlocksFrontendPostCss,
+  /**
+   * This deals with existing Blocks frontend styles being compiled separately.
+   * The main function of this configuration schema is to ensure they are placed correctly.
+   */
+  '/src/styles': createTECLegacyBlocksFrontendPostCss('tec.events'),
 
-	/**
-	 * This deals with packages written following modern module-based approaches.
-	 * These packages are usually not Blocks and require `@wordpress/scripts` to be explicitly
-	 * instructed about them to compile correctly.
-	 * To avoid having to list each package, here the configuration schema is used to recursively
-	 * pick them up and namespace them.
-	 */
-	'/src/resources/packages': TECPackage,
+  /**
+   * This deals with packages written following modern module-based approaches.
+   * These packages are usually not Blocks and require `@wordpress/scripts` to be explicitly
+   * instructed about them to compile correctly.
+   * To avoid having to list each package, here the configuration schema is used to recursively
+   * pick them up and namespace them.
+   */
+  '/src/resources/packages': createTECPackage('tec.events'),
 }, defaultConfig);
 
 /**
@@ -73,17 +73,17 @@ const customEntryPoints = compileCustomEntryPoints({
  * The existing Block Editor code is not follow the `block.json` based convention expected by
  * `@wordpress/scripts` so here we explicitly point out the root index.
  */
-customEntryPoints['app/main'] = exposeEntry('tec.app.main', __dirname + '/src/modules/index.js');
+customEntryPoints['app/main'] = exposeEntry('tec.events.app.main', __dirname + '/src/modules/index.js');
 
 /**
  * Same as above, widgets are built like legacy blocks and are not following the `block.json` convention.
  */
-customEntryPoints['app/widgets'] = exposeEntry('tec.app.widgets', __dirname + '/src/modules/widgets/index.js');
+customEntryPoints['app/widgets'] = exposeEntry('tec.events.app.widgets', __dirname + '/src/modules/widgets/index.js');
 
 /**
  * Prepends a loader for SVG files that will be applied after the default one. Loaders are applied
  * in a LIFO queue in WebPack.
- * By default `@wordpress/scripts` uses `@svgr/webpack` to handle SVG files and, together with it,
+ * By default, `@wordpress/scripts` uses `@svgr/webpack` to handle SVG files and, together with it,
  * the default SVGO (package `svgo/svgo-loader`) configuration that includes the `prefixIds` plugin.
  * To avoid `id` and `class` attribute conflicts, the `prefixIds` plugin would prefix all `id` and
  * `class` attributes in SVG tags with a generated prefix. This would break TEC classes (already
@@ -96,23 +96,23 @@ doNotPrefixSVGIdsClasses(defaultConfig);
  * Finally the customizations are merged with the default WebPack configuration.
  */
 module.exports = {
-	...defaultConfig,
-	...{
-		entry: (buildType) => {
-			const defaultEntryPoints = defaultConfig.entry(buildType);
-			return {
-				...defaultEntryPoints, ...customEntryPoints,
-			};
-		},
-		output: {
-			...defaultConfig.output,
-			...{
-				enabledLibraryTypes: ['window'],
-			},
-		},
-		plugins: [
-			...defaultConfig.plugins,
-			new WindowAssignPropertiesPlugin(),
-		],
-	},
+  ...defaultConfig,
+  ...{
+    entry: (buildType) => {
+      const defaultEntryPoints = defaultConfig.entry(buildType);
+      return {
+        ...defaultEntryPoints, ...customEntryPoints,
+      };
+    },
+    output: {
+      ...defaultConfig.output,
+      ...{
+        enabledLibraryTypes: ['window'],
+      },
+    },
+    plugins: [
+      ...defaultConfig.plugins,
+      new WindowAssignPropertiesPlugin(),
+    ],
+  },
 };
