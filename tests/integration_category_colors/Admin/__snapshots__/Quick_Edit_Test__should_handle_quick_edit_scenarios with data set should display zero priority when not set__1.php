6941824edc742b50c88e011ec974ee27e9656842
Brian Krane <redscar@gmail.com>
2025-03-19 10:07:15 -0400

    Added snapshots.

---
+++ tests/integration_category_colors/Admin/__snapshots__/Quick_Edit_Test__should_handle_quick_edit_scenarios with data set should display zero priority when not set__1.php
@@ -0,0 +1,9 @@
+<?php return '<span class="tec-events-taxonomy-table__category-color-preview"
+	style="background-color: #00ff00; border: 3px solid #ff0000;"
+	data-primary="#ff0000"
+	data-secondary="#00ff00"
+	data-text="#000000"
+	data-priority=""
+	data-hidden="">
+</span>
+';
