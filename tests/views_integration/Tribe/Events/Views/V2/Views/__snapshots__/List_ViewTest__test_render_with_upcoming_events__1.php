02b66c212a786ef096c5b9d55b41201c3b669d77
Sami Dokus <sami2464@gmail.com>
2025-04-04 12:27:28 -0600

    Updating snapshots in views_integration

--- tests/views_integration/Tribe/Events/Views/V2/Views/__snapshots__/List_ViewTest__test_render_with_upcoming_events__1.php
+++ tests/views_integration/Tribe/Events/Views/V2/Views/__snapshots__/List_ViewTest__test_render_with_upcoming_events__1.php
@@ -255,7 +255,11 @@
 </header>
 
 		
-		<div class="tribe-events-calendar-list">
+		<div 
+			class="tribe-events-calendar-list"
+			aria-label="
+			List of Events			"
+		>
 
 							
 				<h2 class="tribe-events-calendar-list__month-separator">
