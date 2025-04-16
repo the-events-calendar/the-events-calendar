a6170380ab169be504f996024dfa79c96781db47
Jeremy Pry <jeremy.pry@gmail.com>
2025-04-15 21:59:03 -0500

    Remove post types that Classy supports from the block logic controller

--- src/Events/Classy/Controller.php
+++ src/Events/Classy/Controller.php
@@ -153,6 +153,7 @@ class Controller extends ControllerContract {
 
 		// Tell Common, TEC, ET and so on NOT to load blocks.
 		add_filter( 'tribe_editor_should_load_blocks', [ self::class, 'return_false' ] );
+		add_filter( 'tec_common_load_blocks_post_types', [ $this, 'filter_load_block_post_types' ], 100 );
 
 		// We're using TEC new editor.
 		add_filter( 'tec_using_classy_editor', [ self::class, 'return_true' ] );
@@ -229,6 +230,7 @@ class Controller extends ControllerContract {
 		remove_filter( 'block_editor_settings_all', [ $this, 'filter_block_editor_settings' ], 100 );
 		remove_filter( 'tec_using_classy_editor', [ self::class, 'return_true' ] );
 		remove_filter( 'tribe_editor_should_load_blocks', [ self::class, 'return_false' ] );
+		remove_filter( 'tec_common_load_blocks_post_types', [ $this, 'filter_load_block_post_types' ], 100 );
 
 		remove_action( 'init', [ $this, 'register_post_meta' ] );
 
@@ -326,6 +328,22 @@ class Controller extends ControllerContract {
 		);
 
 		return (array) $supported_post_types;
+	}
+
+	/**
+	 * Filters the list of post types that should load blocks.
+	 *
+	 * This will remove the post types that ARE supported by the Classy editor
+	 * from the list of post types
+	 *
+	 * @since TBD
+	 *
+	 * @param string[] $post_types
+	 *
+	 * @return string[] The filtered list of post types that should load blocks.
+	 */
+	public function filter_load_block_post_types( array $post_types ): array {
+		return array_diff( $post_types, $this->get_supported_post_types() );
 	}
 
 	/**
