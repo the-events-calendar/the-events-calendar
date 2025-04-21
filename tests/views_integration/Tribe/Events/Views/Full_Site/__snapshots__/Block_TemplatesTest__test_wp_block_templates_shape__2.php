c8b3552a22d506f8f039ee9ed2790a1609fa8761
Sami Dokus <sami2464@gmail.com>
2025-04-21 14:27:54 -0600

    Fixing views_integration snapshot again

--- tests/views_integration/Tribe/Events/Views/Full_Site/__snapshots__/Block_TemplatesTest__test_wp_block_templates_shape__2.php
+++ tests/views_integration/Tribe/Events/Views/Full_Site/__snapshots__/Block_TemplatesTest__test_wp_block_templates_shape__2.php
@@ -3,9 +3,6 @@
   'post_types' => NULL,
   'origin' => NULL,
   'has_theme_file' => false,
-  'area' => NULL,
-  'plugin' => NULL,
-  'author' => NULL,
   'content' => '<!-- wp:template-part {"slug":"header","tagName":"header","theme":"twentytwentyfour"} /-->
 <!-- wp:tec/archive-events /-->
 <!-- wp:template-part {"slug":"footer","tagName":"footer","theme":"twentytwentyfour"} /-->
