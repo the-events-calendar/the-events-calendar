4e38a0fe18c4292672444e46fd3237a6d591b34a
Sami Dokus <sami2464@gmail.com>
2025-05-01 13:59:49 -0600

    Pull out property and update snapshots again

--- src/views/v2/components/events-bar/views/list/item.php
+++ src/views/v2/components/events-bar/views/list/item.php
@@ -28,15 +28,7 @@ if ( $view_slug === $public_view_slug ) {
 		href="<?php echo esc_url( $public_view_data->view_url ); ?>"
 		class="tribe-events-c-view-selector__list-item-link"
 		data-js="tribe-events-view-link"
-		<?php
-		$aria_label = sprintf(
-			/* translators: %1$s: Events plural text, %2$s: View label */
-			esc_attr__( 'Display %1$s in %2$s View', 'the-events-calendar' ),
-			esc_html( tribe_get_event_label_plural() ),
-			esc_html( $public_view_data->view_label )
-		);
-		echo 'aria-label="' . esc_attr( $aria_label ) . '"';
-		?>
+		aria-label="<?php echo esc_attr( $public_view_data->aria_label ); ?>"
 	>
 		<span class="tribe-events-c-view-selector__list-item-icon">
 			<?php $this->template( 'components/icons/' . esc_attr( $public_view_slug ), [ 'classes' => [ 'tribe-events-c-view-selector__list-item-icon-svg' ] ] ); ?>
