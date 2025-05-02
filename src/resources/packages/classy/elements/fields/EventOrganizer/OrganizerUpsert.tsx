4fb3b85ddbde7ecdd7ab1f34f1ebdee930d94e91
Luca Tumedei <luca@theaveragedev.com>
2025-05-02 15:57:30 +0200

    feat(Classy) scaffold organizer upsert card more

--- src/resources/packages/classy/elements/fields/EventOrganizer/OrganizerUpsert.tsx
+++ src/resources/packages/classy/elements/fields/EventOrganizer/OrganizerUpsert.tsx
@@ -1,72 +1,96 @@
-import React, { Fragment } from "react";
-import { _x } from "@wordpress/i18n";
-import NewIcon from "../../components/Icons/New";
+import React from 'react';
+import { _x } from '@wordpress/i18n';
+import NewIcon from '../../components/Icons/New';
 import {
 	Button,
 	__experimentalInputControl as InputControl,
-} from "@wordpress/components";
+} from '@wordpress/components';
 
 const defaultValues = {
-	name: "",
-	phone: "",
-	website: "",
-	email: "",
+	name: '',
+	phone: '',
+	website: '',
+	email: '',
 };
 
-export default function OrganizerUpsert(props: {
+export default function OrganizerUpsert( props: {
 	onCancel: () => void;
 	onSave: () => void;
-}) {
+} ) {
 	const { onCancel, onSave } = props;
 
 	// States for name, phone, website and email.
-	const [values, setValues] = React.useState(defaultValues);
+	const [ values, setValues ] = React.useState( defaultValues );
 
 	return (
-		<Fragment>
+		<div className="classy-root">
 			<header className="classy-modal__header classy-modal__header--organizer">
 				<NewIcon />
 				<h4>
-					{_x(
-						"New organizer",
-						"Upsert modal header title",
-						"the-events-calendar",
-					)}
+					{ _x(
+						'New organizer',
+						'Upsert modal header title',
+						'the-events-calendar'
+					) }
 				</h4>
 			</header>
 
 			<section className="classy-modal__content classy-modal__content--organizer">
 				<InputControl
-                    className="classy-field__control classy-field__control--input"
-					label={_x("Name", "Name input label", "the-events-calendar")}
-					value={values.name}
-					onChange={(value) => setValues({ ...values, name: value || "" })}
+					className="classy-field__control classy-field__control--input"
+					label={ _x(
+						'Name',
+						'Name input label',
+						'the-events-calendar'
+					) }
+					value={ values.name }
+					onChange={ ( value ) =>
+						setValues( { ...values, name: value || '' } )
+					}
 					required
 				/>
 
 				<InputControl
-                    className="classy-field__control classy-field__control--input"
-					label={_x("Phone", "Phone input label", "the-events-calendar")}
-					value={values.phone}
-					onChange={(value) => setValues({ ...values, phone: value || "" })}
+					className="classy-field__control classy-field__control--input"
+					label={ _x(
+						'Phone',
+						'Phone input label',
+						'the-events-calendar'
+					) }
+					value={ values.phone }
+					onChange={ ( value ) =>
+						setValues( { ...values, phone: value || '' } )
+					}
 					type="tel"
 					placeholder=""
 				/>
 
 				<InputControl
-                    className="classy-field__control classy-field__control--input"
-					label={_x("Website", "Website input label", "the-events-calendar")}
-					value={values.website}
-					onChange={(value) => setValues({ ...values, website: value || "" })}
+					className="classy-field__control classy-field__control--input"
+					label={ _x(
+						'Website',
+						'Website input label',
+						'the-events-calendar'
+					) }
+					value={ values.website }
+					onChange={ ( value ) =>
+						setValues( { ...values, website: value || '' } )
+					}
 					type="url"
 					placeholder=""
 				/>
 
 				<InputControl
-                    className="classy-field__control classy-field__control--input"
-					label={_x("Email", "Email input label", "the-events-calendar")}
-					value={values.email}
-					onChange={(value) => setValues({ ...values, email: value || "" })}
+					className="classy-field__control classy-field__control--input"
+					label={ _x(
+						'Email',
+						'Email input label',
+						'the-events-calendar'
+					) }
+					value={ values.email }
+					onChange={ ( value ) =>
+						setValues( { ...values, email: value || '' } )
+					}
 					type="email"
 					placeholder=""
 				/>
@@ -75,17 +99,21 @@ export default function OrganizerUpsert(props: {
 			<footer className="classy-modal__footer classy-modal__footer--organizer">
 				<div className="classy-modal__actions classy-modal__actions--organizer">
 					<Button>
-						{_x(
-							"Create Organizer",
-							"Create organizer button label",
-							"the-events-calendar",
-						)}
+						{ _x(
+							'Create Organizer',
+							'Create organizer button label',
+							'the-events-calendar'
+						) }
 					</Button>
 					<Button>
-						{_x("Cancel", "Cancel button label", "the-events-calendar")}
+						{ _x(
+							'Cancel',
+							'Cancel button label',
+							'the-events-calendar'
+						) }
 					</Button>
 				</div>
 			</footer>
-		</Fragment>
+		</div>
 	);
 }
--- src/resources/packages/classy/elements/fields/EventOrganizer/OrganizerUpsertModal.tsx
+++ src/resources/packages/classy/elements/fields/EventOrganizer/OrganizerUpsertModal.tsx
@@ -1,27 +1,22 @@
 import React from 'react';
-import { Modal } from "@wordpress/components";
+import { Modal } from '@wordpress/components';
 import OrganizerUpsert from './OrganizerUpsert';
 
-export default function OrganizerUpsertModal( props:{
-    onCancel: ()=>void;
-    onClose: ()=>void;
-    onSave: ()=>void;
-} ){
-    const {
-        onCancel,
-        onClose, 
-        onSave, 
-    } = props;
+export default function OrganizerUpsertModal( props: {
+	onCancel: () => void;
+	onClose: () => void;
+	onSave: () => void;
+} ) {
+	const { onCancel, onClose, onSave } = props;
 
-    return (<Modal
-       __experimentalHideHeader={true}
-        className="classy-modal classy-modal--organizer"
-        onRequestClose={onClose}
-        overlayClassName="classy-modal__overlay classy-modal__overlay--organizer"
-    >
-        <OrganizerUpsert 
-            onCancel={onCancel}
-            onSave={onSave}
-        />
-    </Modal>);
+	return (
+		<Modal
+			__experimentalHideHeader={ true }
+			className="classy-modal classy-modal--organizer"
+			onRequestClose={ onClose }
+			overlayClassName="classy-modal__overlay classy-modal__overlay--organizer"
+		>
+			<OrganizerUpsert onCancel={ onCancel } onSave={ onSave } />
+		</Modal>
+	);
 }
