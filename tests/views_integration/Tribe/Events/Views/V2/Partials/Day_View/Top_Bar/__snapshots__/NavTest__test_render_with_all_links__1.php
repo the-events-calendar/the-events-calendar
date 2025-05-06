719d9de07d2950133b958e0d04628461e6bfd3e5
hoang <hoang@learndash.com>
2025-05-06 11:22:09 +0700

    TEC-3413: Reupdate snapshot and ensure it is LF format

--- tests/views_integration/Tribe/Events/Views/V2/Partials/Day_View/Top_Bar/__snapshots__/NavTest__test_render_with_all_links__1.php
+++ tests/views_integration/Tribe/Events/Views/V2/Partials/Day_View/Top_Bar/__snapshots__/NavTest__test_render_with_all_links__1.php
@@ -1,11 +1,11 @@
 <?php return '<nav class="tribe-events-c-top-bar__nav tribe-common-a11y-hidden">
 	<ul class="tribe-events-c-top-bar__nav-list">
-		<li class="tribe-events-c-top-bar__nav-list-item">
+		
+<li class="tribe-events-c-top-bar__nav-list-item">
 	<a
 		href="http://test.tri.be"
 		class="tribe-common-c-btn-icon tribe-common-c-btn-icon--caret-left tribe-events-c-top-bar__nav-link tribe-events-c-top-bar__nav-link--prev"
-		aria-label="Previous day"
-		title="Previous day"
+		aria-label="Previous day&#039;s Events"
 		data-js="tribe-events-view-link"
 		rel="noindex"
 	>
@@ -17,8 +17,7 @@
 	<a
 		href="http://test.tri.be"
 		class="tribe-common-c-btn-icon tribe-common-c-btn-icon--caret-right tribe-events-c-top-bar__nav-link tribe-events-c-top-bar__nav-link--next"
-		aria-label="Next day"
-		title="Next day"
+		aria-label="Next day&#039;s Events"
 		data-js="tribe-events-view-link"
 		rel="noindex"
 	>
--- tests/views_integration/Tribe/Events/Views/V2/Partials/Day_View/Top_Bar/__snapshots__/NavTest__test_render_without_prev_url__1.php
+++ tests/views_integration/Tribe/Events/Views/V2/Partials/Day_View/Top_Bar/__snapshots__/NavTest__test_render_without_prev_url__1.php
@@ -3,8 +3,7 @@
 		<li class="tribe-events-c-top-bar__nav-list-item">
 	<button
 		class="tribe-common-c-btn-icon tribe-common-c-btn-icon--caret-left tribe-events-c-top-bar__nav-link tribe-events-c-top-bar__nav-link--prev"
-		aria-label="Previous day"
-		title="Previous day"
+		aria-label="Previous day&#039;s Events"
 		disabled
 	>
 		<svg  class="tribe-common-c-svgicon tribe-common-c-svgicon--caret-left tribe-common-c-btn-icon__icon-svg tribe-events-c-top-bar__nav-link-icon-svg"  xmlns="http://www.w3.org/2000/svg" viewBox="0 0 10 16" aria-hidden="true"><path d="M9.7 14.4l-1.5 1.5L.3 8 8.2.1l1.5 1.5L3.3 8l6.4 6.4z"/></svg>
@@ -15,8 +14,7 @@
 	<a
 		href="http://test.tri.be"
 		class="tribe-common-c-btn-icon tribe-common-c-btn-icon--caret-right tribe-events-c-top-bar__nav-link tribe-events-c-top-bar__nav-link--next"
-		aria-label="Next day"
-		title="Next day"
+		aria-label="Next day&#039;s Events"
 		data-js="tribe-events-view-link"
 		rel="noindex"
 	>
