27aa6d2f265e4805b540657139d699195ff103f1
hoang <hoang@learndash.com>
2025-05-06 10:10:25 +0700

    TEC-3413: phpcs fix

--- src/views/v2/day/top-bar/nav/prev.php
+++ src/views/v2/day/top-bar/nav/prev.php
@@ -14,7 +14,7 @@
  * @version 5.3.0
  *
  */
-$events_label_plural   = tribe_get_event_label_plural();
+$events_label_plural = tribe_get_event_label_plural();
 
 // Translators: %s: Events label plural.
 $previous_day_label = sprintf( __( 'Previous day\'s %s', 'the-events-calendar' ), $events_label_plural );
@@ -23,7 +23,7 @@ $previous_day_label = sprintf( __( 'Previous day\'s %s', 'the-events-calendar' )
 	<a
 		href="<?php echo esc_url( $prev_url ); ?>"
 		class="tribe-common-c-btn-icon tribe-common-c-btn-icon--caret-left tribe-events-c-top-bar__nav-link tribe-events-c-top-bar__nav-link--prev"
-		aria-label="<?php echo esc_attr( $previous_day_label ) ?>"
+		aria-label="<?php echo esc_attr( $previous_day_label ); ?>"
 		data-js="tribe-events-view-link"
 		rel="<?php echo esc_attr( $prev_rel ); ?>"
 	>
