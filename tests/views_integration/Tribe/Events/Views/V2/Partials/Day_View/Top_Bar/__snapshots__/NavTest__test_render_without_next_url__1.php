f81e3a2f30d08d6d4398997160e7d301dbc20bcd
hoang <hoang@learndash.com>
2025-05-06 14:27:30 +0700

    TEC-3413: Update snapshot

--- tests/views_integration/Tribe/Events/Views/V2/Partials/Day_View/Top_Bar/__snapshots__/NavTest__test_render_without_next_url__1.php
+++ tests/views_integration/Tribe/Events/Views/V2/Partials/Day_View/Top_Bar/__snapshots__/NavTest__test_render_without_next_url__1.php
@@ -1,11 +1,11 @@
 <?php return '<nav class="tribe-events-c-top-bar__nav tribe-common-a11y-hidden">
 	<ul class="tribe-events-c-top-bar__nav-list">
-		<li class="tribe-events-c-top-bar__nav-list-item">
+		
+<li class="tribe-events-c-top-bar__nav-list-item">
 	<a
 		href="http://test.tri.be"
 		class="tribe-common-c-btn-icon tribe-common-c-btn-icon--caret-left tribe-events-c-top-bar__nav-link tribe-events-c-top-bar__nav-link--prev"
-		aria-label="Previous day"
-		title="Previous day"
+		aria-label="Previous day&#039;s Events"
 		data-js="tribe-events-view-link"
 		rel="noindex"
 	>
@@ -16,8 +16,7 @@
 		<li class="tribe-events-c-top-bar__nav-list-item">
 	<button
 		class="tribe-common-c-btn-icon tribe-common-c-btn-icon--caret-right tribe-events-c-top-bar__nav-link tribe-events-c-top-bar__nav-link--next"
-		aria-label="Next day"
-		title="Next day"
+		aria-label="Next day&#039;s Events"
 		disabled
 	>
 		<svg  class="tribe-common-c-svgicon tribe-common-c-svgicon--caret-right tribe-common-c-btn-icon__icon-svg tribe-events-c-top-bar__nav-link-icon-svg"  xmlns="http://www.w3.org/2000/svg" viewBox="0 0 10 16" aria-hidden="true"><path d="M.3 1.6L1.8.1 9.7 8l-7.9 7.9-1.5-1.5L6.7 8 .3 1.6z"/></svg>
--- tests/views_integration/Tribe/Events/Views/V2/Views/__snapshots__/Day_ViewTest__test_render_w_events__1.php
+++ tests/views_integration/Tribe/Events/Views/V2/Views/__snapshots__/Day_ViewTest__test_render_w_events__1.php
@@ -172,8 +172,7 @@
 		<li class="tribe-events-c-top-bar__nav-list-item">
 	<button
 		class="tribe-common-c-btn-icon tribe-common-c-btn-icon--caret-left tribe-events-c-top-bar__nav-link tribe-events-c-top-bar__nav-link--prev"
-		aria-label="Previous day"
-		title="Previous day"
+		aria-label="Previous day&#039;s Events"
 		disabled
 	>
 		<svg  class="tribe-common-c-svgicon tribe-common-c-svgicon--caret-left tribe-common-c-btn-icon__icon-svg tribe-events-c-top-bar__nav-link-icon-svg"  xmlns="http://www.w3.org/2000/svg" viewBox="0 0 10 16" aria-hidden="true"><path d="M9.7 14.4l-1.5 1.5L.3 8 8.2.1l1.5 1.5L3.3 8l6.4 6.4z"/></svg>
@@ -184,8 +183,7 @@
 	<a
 		href="http://test.tri.be/events/2019-01-02/"
 		class="tribe-common-c-btn-icon tribe-common-c-btn-icon--caret-right tribe-events-c-top-bar__nav-link tribe-events-c-top-bar__nav-link--next"
-		aria-label="Next day"
-		title="Next day"
+		aria-label="Next day&#039;s Events"
 		data-js="tribe-events-view-link"
 		rel="noindex"
 	>
