6a2b6231b6711f57af50d89b04dd583e303a831e
Luca Tumedei <luca@theaveragedev.com>
2025-05-19 16:43:17 +0200

    refactor(Classy) cleanup, move
    Move field rendering code into dedicated function, move over constants
    required only by TEC.
    
    The `renderFields` function will further check whether the curren post
    type is an Event, and render no field if not an Event post type.

---
+++ src/resources/packages/classy/constants.tsx
@@ -0,0 +1,8 @@
+// Post types.
+export const POST_TYPE_EVENT = 'tribe_events';
+
+// Meta keys.
+export const METADATA_EVENT_START_DATE = '_EventStartDate';
+export const METADATA_EVENT_END_DATE = '_EventEndDate';
+export const METADATA_EVENT_ALLDAY = '_EventAllDay';
+export const METADATA_EVENT_TIMEZONE = '_EventTimezone';
