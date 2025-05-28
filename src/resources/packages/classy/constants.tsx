c9f04efb2b39aa338fc29a42eba8d68cd57c889f
Jeremy Pry <jeremy.pry@gmail.com>
2025-05-28 14:13:02 -0500

    Create new event constants

--- src/resources/packages/classy/constants.tsx
+++ src/resources/packages/classy/constants.tsx
@@ -3,6 +3,9 @@ export const POST_TYPE_EVENT = 'tribe_events';
 
 // Meta keys.
 export const METADATA_EVENT_ALLDAY = '_EventAllDay';
+export const METADATA_EVENT_COST = '_EventCost';
+export const METADATA_EVENT_CURRENCY_POSITION = '_EventCurrencyPosition';
+export const METADATA_EVENT_CURRENCY_SYMBOL = '_EventCurrencySymbol';
 export const METADATA_EVENT_END_DATE = '_EventEndDate';
 export const METADATA_EVENT_START_DATE = '_EventStartDate';
 export const METADATA_EVENT_TIMEZONE = '_EventTimezone';
