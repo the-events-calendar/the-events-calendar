93d530f77eb2934f7ce194b783c38826ca31338d
Jeremy Pry <jeremy.pry@gmail.com>
2025-05-29 22:14:17 -0500

    Add event cost to admission section and update markup

--- src/resources/packages/classy/fields/EventAdmission/EventAdmission.tsx
+++ src/resources/packages/classy/fields/EventAdmission/EventAdmission.tsx
@@ -4,6 +4,7 @@ import { _x } from '@wordpress/i18n';
 import { FieldProps } from '@tec/common/classy/types/FieldProps.ts';
 import { IconTicket } from '@tec/common/classy/components';
 import { currencyDollar } from '@wordpress/icons';
+import { EventCost } from "../EventCost";
 
 export default function EventAdmission( props: FieldProps ) {
 	return (
@@ -12,31 +13,44 @@ export default function EventAdmission( props: FieldProps ) {
 				<h3>{ props.title }</h3>
 			</div>
 
-			<ButtonGroup>
-				<Button
-					__next40pxDefaultSize={ true }
-					variant="primary"
-				>
-					<IconTicket/>
-					{ _x(
-						'Sell Tickets',
-						'Event admission button label',
-						'the-events-calendar'
-					) }
-				</Button>
+			<div className="classy-field__inputs classy-field__inputs--boxed">
+				<div className="class-field__inputs-section classy-field__inputs-section--row">
+					<ButtonGroup>
+						<Button
+							__next40pxDefaultSize={ true }
+							variant="primary"
+						>
+							<IconTicket/>
+							{ _x(
+								'Sell Tickets',
+								'Event admission button label',
+								'the-events-calendar'
+							) }
+						</Button>
 
-				<Button
-					__next40pxDefaultSize={ true }
-					variant="secondary"
-				>
-					
-					{ _x(
-						'Manual Pricing',
-						'Event admission button label',
-						'the-events-calendar'
-					) }
-				</Button>
-			</ButtonGroup>
+						<Button
+							__next40pxDefaultSize={ true }
+							variant="secondary"
+						>
+							{ _x(
+								'Manual Pricing',
+								'Event admission button label',
+								'the-events-calendar'
+							) }
+						</Button>
+					</ButtonGroup>
+				</div>
+
+				<div className="classy-field__inputs-section classy-field__inputs-section--row">
+					<EventCost
+						title={ _x(
+							'Event Cost',
+							'Event admission cost field title',
+							'the-events-calendar'
+						) }
+					/>
+				</div>
+			</div>
 		</div>
 	);
 }
