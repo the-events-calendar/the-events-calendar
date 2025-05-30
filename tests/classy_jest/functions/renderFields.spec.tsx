0f1da8ed1c0672e84afeec76817db4d68d3ccd0f
Luca Tumedei <luca@theaveragedev.com>
2025-05-30 16:40:35 +0200

    style(classy_jest) fix

--- tests/classy_jest/functions/renderFields.spec.tsx
+++ tests/classy_jest/functions/renderFields.spec.tsx
@@ -1,26 +1,28 @@
 import * as React from 'react';
 import { render } from '@testing-library/react';
 import { beforeEach, afterEach, describe, expect, it, jest } from '@jest/globals';
-import renderFields from "../../../src/resources/packages/classy/functions/renderFields.tsx";
-import {RegistryProvider} from "@wordpress/data";
-import {createRegistry} from "@tec/common/classy/store";
+import renderFields from '../../../src/resources/packages/classy/functions/renderFields.tsx';
+import { RegistryProvider } from '@wordpress/data';
+import { createRegistry } from '@tec/common/classy/store';
 
-describe('renderFields',()=>{
-	beforeEach(()=>{
+describe( 'renderFields', () => {
+	beforeEach( () => {
 		jest.resetModules();
-	});
+	} );
 
-	afterEach(()=>{
+	afterEach( () => {
 		jest.resetModules();
-	});
+	} );
 
-	it('renders nothing if post is not Event', async ()=>{
+	it( 'renders nothing if post is not Event', async () => {
 		// Create the Classy registry; this will register the default stores as well.
 		const registry = await createRegistry();
 
 		// Render the component inside a RegistryProvider.
-		const { container } = render(<RegistryProvider value={registry}>
-			{/* Here call the filter that will trigger the render fields function. */}
-		</RegistryProvider>);
-	});
-});
+		const { container } = render(
+			<RegistryProvider value={ registry }>
+				{ /* Here call the filter that will trigger the render fields function. */ }
+			</RegistryProvider>
+		);
+	} );
+} );
