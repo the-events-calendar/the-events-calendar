2ee183dc2e5ec14d40c264971cf88a7d3827238a
Jeremy Pry <jeremy.pry@gmail.com>
2025-06-10 20:41:40 -0500

    Set up EventCost test using Cursor AI

---
+++ tests/classy_jest/fields/EventCost/EventCost.spec.tsx
@@ -0,0 +1,190 @@
+import * as React from 'react';
+import { render, screen } from '@testing-library/react';
+import { userEvent } from '@testing-library/user-event';
+import { beforeEach, describe, expect, it, jest } from '@jest/globals';
+import { RegistryProvider } from '@wordpress/data';
+import { createRegistry } from '../../../../common/src/resources/packages/classy/store';
+import EventCost from '../../../../src/resources/packages/classy/fields/EventCost/EventCost';
+import { Currency } from '../../../../common/src/resources/packages/classy/types/Currency';
+import { CurrencyPosition } from '../../../../common/src/resources/packages/classy/types/CurrencyPosition';
+
+type Selector = {
+	'core/editor': {
+		getEditedPostAttribute: (attribute: string) => any;
+	};
+	'tec/classy': {
+		getDefaultCurrency: () => Currency;
+	};
+};
+
+// Mock WordPress data store
+jest.mock('@wordpress/data', () => {
+	const mockSelect = (storeName: string) => {
+		if (storeName === 'core/editor') {
+			return {
+				getEditedPostAttribute: () => ({
+					meta: {
+						tribe_event_cost: '',
+						tribe_event_currency_symbol: '$',
+						tribe_event_currency_position: 'prefix',
+						tribe_event_is_free: false,
+					},
+				}),
+			};
+		}
+		if (storeName === 'tec/classy') {
+			return {
+				getDefaultCurrency: () => ({
+					code: 'USD',
+					symbol: '$',
+					position: 'prefix',
+				}),
+			};
+		}
+		return {};
+	};
+
+	const mockDispatch = (storeName: string) => {
+		if (storeName === 'core/editor') {
+			return {
+				editPost: jest.fn(),
+			};
+		}
+		return {};
+	};
+
+	return {
+		useSelect: (callback: (select: typeof mockSelect) => any) => callback(mockSelect),
+		useDispatch: (storeName: string) => mockDispatch(storeName),
+		RegistryProvider: ({ children }) => children,
+	};
+});
+
+// Mock WordPress components
+jest.mock('@wordpress/components', () => ({
+	__experimentalInputControl: ({ label, value, onChange, disabled, onFocus, onBlur }) => (
+		<input
+			aria-label={label.props.children}
+			value={value}
+			onChange={(e) => onChange(e.target.value)}
+			disabled={disabled}
+			onFocus={onFocus}
+			onBlur={onBlur}
+		/>
+	),
+	ToggleControl: ({ label, checked, onChange }) => (
+		<label>
+			{label}
+			<input type="checkbox" checked={checked} onChange={(e) => onChange(e.target.checked)} />
+		</label>
+	),
+}));
+
+// Mock CurrencySelector component
+jest.mock('../../../../common/src/resources/packages/classy/components', () => ({
+	CurrencySelector: () => <div className="classy-field__currency-selector" />,
+}));
+
+describe('EventCost Component', () => {
+	const mockDefaultCurrency: Currency = {
+		code: 'USD',
+		symbol: '$',
+		position: 'prefix' as CurrencyPosition,
+	};
+
+	const mockMeta = {
+		[ 'tribe_event_cost' ]: '',
+		[ 'tribe_event_currency_symbol' ]: '$',
+		[ 'tribe_event_currency_position' ]: 'prefix',
+		[ 'tribe_event_is_free' ]: false,
+	};
+
+	beforeEach(() => {
+		jest.resetAllMocks();
+	});
+
+	it('renders correctly with default props', async () => {
+		const registry = await createRegistry();
+		const { container } = render(
+			<RegistryProvider value={registry}>
+				<EventCost />
+			</RegistryProvider>
+		);
+
+		expect(container).toMatchSnapshot();
+	});
+
+	it('displays "Free" when event is free', async () => {
+		const registry = await createRegistry();
+		const { container } = render(
+			<RegistryProvider value={registry}>
+				<EventCost />
+			</RegistryProvider>
+		);
+
+		const freeToggle = screen.getByLabelText('Event is free');
+		await userEvent.click(freeToggle);
+
+		expect(container).toMatchSnapshot();
+	});
+
+	it('formats single cost value correctly', async () => {
+		const registry = await createRegistry();
+		const { container } = render(
+			<RegistryProvider value={registry}>
+				<EventCost />
+			</RegistryProvider>
+		);
+
+		const costInput = screen.getByLabelText('Event cost');
+		await userEvent.type(costInput, '10.50');
+
+		expect(container).toMatchSnapshot();
+	});
+
+	it('formats multiple cost values correctly', async () => {
+		const registry = await createRegistry();
+		const { container } = render(
+			<RegistryProvider value={registry}>
+				<EventCost />
+			</RegistryProvider>
+		);
+
+		const costInput = screen.getByLabelText('Event cost');
+		await userEvent.type(costInput, '10.50, 20.75, 15.25');
+
+		expect(container).toMatchSnapshot();
+	});
+
+	it('handles invalid cost values gracefully', async () => {
+		const registry = await createRegistry();
+		const { container } = render(
+			<RegistryProvider value={registry}>
+				<EventCost />
+			</RegistryProvider>
+		);
+
+		const costInput = screen.getByLabelText('Event cost');
+		await userEvent.type(costInput, 'invalid, 20.75');
+
+		expect(container).toMatchSnapshot();
+	});
+
+	it('updates currency symbol and position correctly', async () => {
+		const registry = await createRegistry();
+		const { container } = render(
+			<RegistryProvider value={registry}>
+				<EventCost />
+			</RegistryProvider>
+		);
+
+		const costInput = screen.getByLabelText('Event cost');
+		await userEvent.type(costInput, '10.50');
+
+		// Simulate currency change
+		const currencySelector = container.querySelector('.classy-field__currency-selector');
+		expect(currencySelector).not.toBeNull();
+
+		expect(container).toMatchSnapshot();
+	});
+});
---
+++ tests/classy_jest/jest.setup.ts
@@ -0,0 +1,163 @@
+/** @jest-environment jsdom */
+
+import { jest } from '@jest/globals';
+import { TextDecoder as NodeTextDecoder, TextEncoder as NodeTextEncoder } from 'util';
+import { Currency } from '../../common/src/resources/packages/classy/types/Currency';
+import { LocalizedData } from '../../common/src/resources/packages/classy/types/LocalizedData';
+import { StartOfWeek } from '../../common/src/resources/packages/classy/types/StartOfWeek';
+import { Hours } from '../../common/src/resources/packages/classy/types/Hours';
+import { Minutes } from '../../common/src/resources/packages/classy/types/Minutes';
+import type { StoreDescriptor } from '@wordpress/data';
+
+// Add TextDecoder and TextEncoder to global scope
+global.TextDecoder = NodeTextDecoder as unknown as typeof TextDecoder;
+global.TextEncoder = NodeTextEncoder as unknown as typeof TextEncoder;
+
+/**
+ * @see: https://github.com/WordPress/gutenberg/blob/trunk/test/unit/config/global-mocks.js
+ */
+
+// Mock client-zip package
+jest.mock('client-zip', () => ({
+	downloadZip: jest.fn(),
+	makeZip: jest.fn(),
+	predictLength: jest.fn(),
+}));
+
+/**
+ * @see: https://jestjs.io/docs/manual-mocks#mocking-methods-which-are-not-implemented-in-jsdom
+ * @see: https://github.com/WordPress/gutenberg/blob/trunk/packages/jest-preset-default/scripts/setup-globals.js
+ */
+global.window.matchMedia = () => ({
+	matches: false,
+	addListener: () => {},
+	addEventListener: () => {},
+	removeListener: () => {},
+	removeEventListener: () => {},
+	media: '',
+	onchange: null,
+	dispatchEvent: () => true,
+});
+
+// Mocking the `scrollIntoView` function; it's not implemented in JSDOM.
+// @see: https://github.com/jsdom/jsdom/issues/1695
+window.Element.prototype.scrollIntoView = function () {};
+
+/**
+ * Here we selectively silence some warnings coming from external (e.g. WordPress) components.
+ * This should not be abused: legitimate warnings coming from code that is part of the Classy package should
+ * be addressed and fixed, not silenced.
+ */
+const originalWarn = console.warn;
+
+console.warn = (msg: string | Error) => {
+	// From the `PostFeaturedImage` component of the `@wordpress/editor` package.
+	if (msg.toString().includes('motion() is deprecated. Use motion.create() instead')) {
+		return;
+	}
+	originalWarn(msg);
+};
+
+/**
+ * Mocks for the global TinyMCE instance loaded on the `window` object by the `wp-tinymce` dependency.
+ */
+interface TinyMCEMock {
+	get: (id: string) => {
+		initialized: boolean;
+		on: jest.Mock;
+		off: jest.Mock;
+		initialization: boolean;
+		init: jest.Mock;
+	};
+	EditorManager: {
+		editors: any[];
+	};
+}
+
+global.window.tinymce = {
+	get: () => ({
+		initialized: true,
+		on: jest.fn(),
+		off: jest.fn(),
+		initialization: false,
+		init: jest.fn((config: any, callback: () => void) => {
+			callback();
+		}),
+	}),
+	EditorManager: {
+		editors: [],
+	},
+} as TinyMCEMock;
+
+global.window.wp = {
+	...(global.window.wp || {}),
+	oldEditor: {
+		remove: jest.fn(),
+		initialize: jest.fn(),
+		getContent: jest.fn().mockReturnValue('<p>Initial content</p>'),
+	},
+};
+
+// Setup the localized data for the store.
+interface TECWindow extends Window {
+	tec: {
+		common: {
+			classy: {
+				data: LocalizedData;
+				registry: {
+					registerGenericStore: jest.Mock;
+					registerStore: jest.Mock;
+					subscribe: jest.Mock;
+					select: jest.Mock;
+					dispatch: jest.Mock;
+				};
+			};
+		};
+	};
+}
+
+const defaultCurrency: Currency = {
+	code: 'USD',
+	symbol: '$',
+	position: 'prefix',
+};
+
+const defaultSettings = {
+	timezoneString: 'UTC',
+	timezoneChoice:
+		'<optgroup label="Africa"><option value="Africa/Abidjan">Abidjan</option></optgroup>' +
+		'<optgroup label="Europe"><option value"Europe/Paris">Paris</optionvalue></optgroup>' +
+		'<optgroup label="North America"><option value="America/New_York">New York</option></optgroup>' +
+		'<optgroup label="UTC"><option value="UTC+0">UTC</option></optgroup>',
+	startOfWeek: 0 as StartOfWeek,
+	endOfDayCutoff: {
+		hours: 0 as Hours,
+		minutes: 0 as Minutes,
+	},
+	dateWithYearFormat: 'F j, Y',
+	dateWithoutYearFormat: 'F j',
+	monthAndYearFormat: 'F Y',
+	compactDateFormat: 'n/j/Y',
+	dataTimeSeparator: ' @ ',
+	timeRangeSeparator: ' - ',
+	timeFormat: 'g:i A',
+	timeInterval: 15,
+	defaultCurrency,
+};
+
+((global.window as unknown) as TECWindow).tec = {
+	common: {
+		classy: {
+			data: {
+				settings: defaultSettings,
+			},
+			registry: {
+				registerGenericStore: jest.fn(),
+				registerStore: jest.fn(),
+				subscribe: jest.fn(),
+				select: jest.fn(),
+				dispatch: jest.fn(),
+			},
+		},
+	},
+};
