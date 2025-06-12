3bb7f6adfb0a7e98ab7248db04e54224ac4a3eac
Luca Tumedei <luca@theaveragedev.com>
2025-06-12 14:36:59 +0200

    test(classy_integration) add missing method coverage

--- tests/classy_integration/Controller_Test.php
+++ tests/classy_integration/Controller_Test.php
@@ -2,11 +2,78 @@
 
 namespace TEC\Tests\Events\Classy;
 
-use TEC\Events\Classy\Controller;
 use TEC\Common\Tests\Provider\Controller_Test_Case;
+use TEC\Events\Classy\Controller;
+use Tribe__Events__Main as TEC;
 
 class Controller_Test extends Controller_Test_Case {
 	protected $controller_class = Controller::class;
 
+	/**
+	 * @covers \TEC\Events\Classy\Controller::add_supported_post_types
+	 */
+	public function test_add_supported_post_types(): void {
+		/** @var Controller $controller */
+		$controller = $this->make_controller();
 
+		$post_types = $controller->add_supported_post_types( [] );
+
+		$this->assertEquals( [ TEC::POSTTYPE ], $post_types );
+	}
+
+	/**
+	 * @covers \TEC\Events\Classy\Controller::filter_data
+	 */
+	public function test_filter_data(): void {
+		$controller = $this->make_controller();
+
+		$data = $controller->filter_data( [] );
+
+		$this->assertEquals( [
+			'settings'       =>
+				[
+					'defaultCurrency'    =>
+						[
+							'code'     => 'USD',
+							'symbol'   => '$',
+							'position' => 'prefix',
+						],
+					'timeRangeSeparator' => ' - ',
+					'venuesLimit'        => 1,
+				],
+			'endOfDayCutoff' =>
+				[
+					'hours'   => 0,
+					'minutes' => 0,
+				],
+		], $data );
+
+		// Update the options.
+		tribe_update_option( 'defaultCurrencyCode', 'EUR' );
+		tribe_update_option( 'defaultCurrencySymbol', '€' );
+		tribe_update_option( 'reverseCurrencyPosition', 'postfix' );
+		tribe_update_option( 'timeRangeSeparator', ' ~ ' );
+		tribe_update_option( 'multiDayCutoff', '02:30' );
+
+		$data = $controller->filter_data( [] );
+
+		$this->assertEquals( [
+			'settings'       =>
+				[
+					'defaultCurrency'    =>
+						[
+							'code'     => 'EUR',
+							'symbol'   => '€',
+							'position' => 'postfix',
+						],
+					'timeRangeSeparator' => ' ~ ',
+					'venuesLimit'        => 1,
+				],
+			'endOfDayCutoff' =>
+				[
+					'hours'   => 2,
+					'minutes' => 30,
+				],
+		], $data );
+	}
 }
