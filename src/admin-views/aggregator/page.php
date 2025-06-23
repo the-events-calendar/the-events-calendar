3a23f5f5d342beb986b6eb82911450ea07890b4f
Camwyn <stephen@theeventscalendar.com>
2025-06-23 15:07:19 -0400

    whitespace

--- src/admin-views/aggregator/page.php
+++ src/admin-views/aggregator/page.php
@@ -5,6 +5,7 @@ $state_class = 'tribe-aggregator-inactive';
 if ( tribe( 'events-aggregator.main' )->is_service_active() ) {
 	$state_class = 'tribe-aggregator-active';
 }
+
 do_action( 'tec_conditional_content_header_notice', $this );
 ?>
 <div class="tribe-ea wrap <?php echo esc_attr( $state_class ); ?>" data-datepicker_format="<?php echo esc_attr( $datepicker_format ); ?>">
