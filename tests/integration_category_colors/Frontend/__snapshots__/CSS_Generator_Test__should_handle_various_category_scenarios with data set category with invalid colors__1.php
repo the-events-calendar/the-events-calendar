8bf153442a46d4654108683d4a2805a9be70cf75
Brian Krane <redscar@gmail.com>
2025-06-25 14:29:02 -0400

    Updated snapshots.

--- tests/integration_category_colors/Frontend/__snapshots__/CSS_Generator_Test__should_handle_various_category_scenarios with data set category with invalid colors__1.php
+++ tests/integration_category_colors/Frontend/__snapshots__/CSS_Generator_Test__should_handle_various_category_scenarios with data set category with invalid colors__1.php
@@ -1,6 +1,1 @@
-<?php return '.tribe_events_cat-{Placeholder-1} {
-    --tec-color-category-primary: transparent;
-    --tec-color-category-secondary: transparent;
-    --tec-color-category-text: inherit;
-}
-';
+<?php return '';
--- tests/integration_category_colors/Frontend/__snapshots__/CSS_Generator_Test__should_handle_various_category_scenarios with data set category with only primary color__1.php
+++ tests/integration_category_colors/Frontend/__snapshots__/CSS_Generator_Test__should_handle_various_category_scenarios with data set category with only primary color__1.php
@@ -1,6 +1,4 @@
 <?php return '.tribe_events_cat-{Placeholder-1} {
     --tec-color-category-primary: #ff0000;
-    --tec-color-category-secondary: transparent;
-    --tec-color-category-text: inherit;
 }
 ';
