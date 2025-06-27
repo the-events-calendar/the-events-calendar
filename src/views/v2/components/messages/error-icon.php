7d52f6478ff0a56b20f91e6c3a0f697cfb039595
Camwyn <stephen@theeventscalendar.com>
2025-06-27 14:40:08 -0400

    Ensure template icons have accessible labels. Allow passing in an "override label" via $icon_description.

--- src/views/v2/components/messages/error-icon.php
+++ src/views/v2/components/messages/error-icon.php
@@ -9,7 +9,20 @@
  *
  * @link http://evnt.is/1aiy
  *
- * @version 5.3.0
+ * @since 5.3.0
+ * @since TBD Added $icon_description parameter and updated the template to use it for the accessible label.
+ *
+ * @version TBD
+ *
+ * @var string $icon_description The description of the icon. Used for the accessible label. (optional)
  */
 
+if ( empty( $icon_description ) ) {
+	$icon_description = __( 'Error:', 'the-events-calendar' );
+}
+
  $this->template( 'components/icons/error', [ 'classes' => [ 'tribe-events-c-messages__message-icon-svg' ] ] );
+?>
+<span class="tribe-common-a11y-visual-hide">
+	<?php echo esc_html( $icon_description ); ?>
+</span>
