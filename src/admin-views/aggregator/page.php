5a7c9c8b326a8965a5fecccdc26a756bef654bea
Camwyn <stephen@theeventscalendar.com>
2025-07-01 16:46:05 -0400

    Address code review

--- src/admin-views/aggregator/page.php
+++ src/admin-views/aggregator/page.php
@@ -5,7 +5,13 @@ $state_class = 'tribe-aggregator-inactive';
 if ( tribe( 'events-aggregator.main' )->is_service_active() ) {
 	$state_class = 'tribe-aggregator-active';
 }
-
+/**
+ * Trigger the conditional content header notice.
+ *
+ * @since TBD
+ *
+ * @param \Tribe__Admin__View $this The current view object.
+ */
 do_action( 'tec_conditional_content_header_notice', $this );
 ?>
 <div class="tribe-ea wrap <?php echo esc_attr( $state_class ); ?>" data-datepicker_format="<?php echo esc_attr( $datepicker_format ); ?>">
