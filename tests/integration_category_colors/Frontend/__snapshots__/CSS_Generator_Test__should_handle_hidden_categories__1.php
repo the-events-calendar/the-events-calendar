6cb8e038c521c9d330055e012d6405c359a80d53
rafsuntaskin <rafsun.aiub@gmail.com>
2025-07-10 00:26:23 +0600

    snapshot fixed

--- tests/integration_category_colors/Frontend/__snapshots__/CSS_Generator_Test__should_handle_hidden_categories__1.php
+++ tests/integration_category_colors/Frontend/__snapshots__/CSS_Generator_Test__should_handle_hidden_categories__1.php
@@ -1,12 +1,11 @@
-<?php return '.tribe_events_cat-visible-category {
-    --tec-color-category-primary: #ff0000;
-    --tec-color-category-secondary: #00ff00;
-    --tec-color-category-text: #0000ff;
-}
-
-.tribe_events_cat-hidden-category {
+<?php return '.tribe_events_cat-hidden-category {
     --tec-color-category-primary: #0000ff;
     --tec-color-category-secondary: #00ff00;
     --tec-color-category-text: #ff0000;
+}
+.tribe_events_cat-visible-category {
+    --tec-color-category-primary: #ff0000;
+    --tec-color-category-secondary: #00ff00;
+    --tec-color-category-text: #0000ff;
 }
 ';
