4c56d7eaad0e70aa5cf8b54f137d896bbac184eb
Stephen Page <stephen@theeventscalendar.com>
2025-07-17 13:47:05 -0400

    Fix views_integration snapshots.

--- tests/views_integration/Tribe/Events/Views/V2/Partials/Day_View/Event/__snapshots__/Featured_ImageTest__test_render_with_event_with_featured_image_without_alt__1.php
+++ tests/views_integration/Tribe/Events/Views/V2/Partials/Day_View/Event/__snapshots__/Featured_ImageTest__test_render_with_event_with_featured_image_without_alt__1.php
@@ -1,8 +1,11 @@
 <?php return '<div class="tribe-events-calendar-day__event-featured-image-wrapper">
 	<img
-		src="http://test.tri.be/wp-content/uploads/2018/08/featured-image.jpg"
-				alt="Single Event 1"
 		class="tribe-events-calendar-day__event-featured-image"
-	/>
+		src="http://test.tri.be/wp-content/uploads/2018/08/featured-image.jpg"
+							alt="featured-image.jpg"
+				class="tribe-events-calendar-day__event-featured-image"
+					width="400"
+							height="200"
+			/>
 </div>
 ';
--- tests/views_integration/Tribe/Events/Views/V2/Partials/List_View/Event/__snapshots__/Featured_ImageTest__test_render_with_event_with_featured_image_without_alt__1.php
+++ tests/views_integration/Tribe/Events/Views/V2/Partials/List_View/Event/__snapshots__/Featured_ImageTest__test_render_with_event_with_featured_image_without_alt__1.php
@@ -1,10 +1,12 @@
 <?php return '<div class="tribe-events-calendar-list__event-featured-image-wrapper tribe-common-g-col">
 	<img
-		src="http://test.tri.be/wp-content/uploads/2018/08/featured-image.jpg"
-				alt="Single Event 1"
 		class="tribe-events-calendar-list__event-featured-image"
-		width="400"
-		height="200"
-	/>
+		src="http://test.tri.be/wp-content/uploads/2018/08/featured-image.jpg"
+							alt=""
+							title="featured-image.jpg"
+				class="tribe-events-calendar-list__event-featured-image"
+					width="400"
+			height="200"
+			/>
 </div>
 ';
