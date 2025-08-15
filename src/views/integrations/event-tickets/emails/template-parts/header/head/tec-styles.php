6e6c6705033b8fc7328899b07cea3fe3f158e51e
Andras Guseo <andras@theeventscalendar.com>
2025-08-15 20:42:11 +0200

    Add more docblocks

--- src/views/integrations/event-tickets/emails/template-parts/header/head/tec-styles.php
+++ src/views/integrations/event-tickets/emails/template-parts/header/head/tec-styles.php
@@ -1,3 +1,8 @@
+<?php
+/**
+ * @since 6.1.1
+ */
+?>
 <style type="text/css">
 	td.tec-tickets__email-table-content-event-links-container,
 	td.tec-tickets__email-table-content-event-date-container,
