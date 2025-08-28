86e0814e1bf0840bcc3c11e99c1d02d713e1854f
Stephen Page <stephen@theeventscalendar.com>
2025-08-28 12:08:51 -0400

    Add iCal query var filtering logic.
    Hooks into the `tec_request_query_vars_registered` action to register the `ical` query var.
    Extends the `Abstract_Query_Var` class to provide the filtering logic.
    Ensures that the `ical` query var is correctly unset when its value is falsy, and converted to `1` when truthy or no value is explicitly set (i.e. `?ical`).

---
+++ src/Events/Request/iCal.php
@@ -0,0 +1,82 @@
+<?php
+/**
+ * Ical query var.
+ *
+ * @since TBD
+ *
+ * @package TEC\Events\Request
+ */
+
+namespace TEC\Events\Request;
+
+use TEC\Common\Request\Abstract_Query_Var;
+
+/**
+ * Class Ical
+ *
+ * @since TBD
+ *
+ * @package TEC\Events\Request
+ */
+class Ical extends Abstract_Query_Var {
+	/**
+	 * The query var name.
+	 *
+	 * @since TBD
+	 *
+	 * @var string
+	 */
+	protected string $name = 'ical';
+
+	/**
+	 * Whether the query var should be filtered.
+	 *
+	 * @since TBD
+	 *
+	 * @var bool
+	 */
+	protected bool $should_filter = true;
+
+	/**
+	 * Whether the query var is active.
+	 *
+	 * @since TBD
+	 *
+	 * @return bool Whether the query var is active.
+	 */
+	public function is_active(): bool {
+		return true;
+	}
+
+	/**
+	 * Filters if the superglobal is allowed to be filtered for this var.
+	 *
+	 * @since TBD
+	 *
+	 * @param string $key The query var name.
+	 * @param string $superglobal The superglobal key (GET, POST, REQUEST).
+	 *
+	 * @return string|bool Whether the value is allowed. Returning a string "key" will limit the superglobal modification to that key.
+	 */
+	public function filter_superglobal_allowed( string $key, string $superglobal ) {
+		return true;
+	}
+
+	/**
+	 * Filters the value to either `1` or `null` (to unset).
+	 *
+	 * @since TBD
+	 *
+	 * @param mixed $value The raw value to normalize.
+	 *
+	 * @return int|null `1` when truthy, `null` when not.
+	 */
+	public function filter_query_var( $value ) {
+		// Support presence-only query var (?ical) as truthy.
+		if ( '' === $value || null === $value ) {
+			return 1;
+		}
+
+		return tribe_is_truthy( $value ) ? 1 : null;
+	}
+}
