c144f8c733da22ae0ae131250fe3eda9e9d495c0
Jeremy Pry <jeremy.pry@gmail.com>
2025-08-28 18:40:39 -0500

    Move STORE_NAME constant to the constants file

--- src/resources/packages/classy/store/index.ts
+++ src/resources/packages/classy/store/index.ts
@@ -2,7 +2,6 @@ import { reducer } from './reducer';
 import * as actions from './actions';
 import * as selectors from './selectors';
 
-export const STORE_NAME = 'tec/classy/events';
 
 export const storeConfig = {
 	reducer,
