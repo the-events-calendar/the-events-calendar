3fb7ff0157d8d1625e6d3664cd69e4a119af5c83
Jeremy Pry <jeremy.pry@gmail.com>
2025-09-12 21:06:08 -0500

    Update venue API to use the new REST API

--- src/resources/packages/classy/api/venues.ts
+++ src/resources/packages/classy/api/venues.ts
@@ -1,14 +1,72 @@
-import apiFetch from '@wordpress/api-fetch';
 import { addQueryArgs } from '@wordpress/url';
 import { FetchedVenue } from '../types/FetchedVenue';
 import { VenueData } from '../types/VenueData';
+// todo: fix these imports to use @tec/common.
+import { getRoute, fetch } from '../../../../../common/src/resources/packages/classy/api';
+import { PostStatus } from '../../../../../common/src/resources/packages/classy/types/Api';
+
+const baseRoute = getRoute( '/venues' );
 
 /**
- * Result of a venue fetch operation.
+ * Formatted result of fetching venues, including the array of venues and the total count.
  */
 export type VenuesFetchResult = {
 	venues: FetchedVenue[];
 	total: number;
+};
+
+/**
+ * The structure of a venue as returned by the REST API.
+ */
+type VenueResponse = {
+	// These are the properties we care about.
+	id: number;
+	link: string;
+	title: {
+		rendered: string;
+	};
+	address: string;
+	city: string;
+	country: string;
+	state_province: string;
+	state: string;
+	province: string;
+	zip: string;
+	phone: string;
+	website: string;
+	directions_link: string;
+
+	// The rest of the properties are included for completeness, but not used in the mapping.
+	date: string;
+	date_gmt: string;
+	guid: {
+		rendered: string;
+	};
+	modified: string;
+	modified_gmt: string;
+	slug: string;
+	status: PostStatus;
+	type: string;
+	content: {
+		rendered: string;
+		protected: boolean;
+	};
+	template: string;
+};
+
+type VenueUpsertRequest = {
+	title: string;
+	status: PostStatus;
+	content?: string;
+	address?: string;
+	city?: string;
+	country?: string;
+	state_province?: string;
+	state?: string;
+	province?: string;
+	zip?: string;
+	phone?: string;
+	website?: string;
 };
 
 /**
@@ -27,36 +85,39 @@ export type VenuesFetchResult = {
  * @throws {Error} Will throw an error if the fetch request fails.
  */
 export const fetchVenues = async ( page: number ): Promise< VenuesFetchResult > => {
-	const results = await apiFetch( {
-		path: addQueryArgs( '/tribe/events/v1/venues', {
-			page,
-		} ),
+	return new Promise< VenuesFetchResult >( async ( resolve, reject ): Promise< void > => {
+		await fetch( {
+			path: addQueryArgs( baseRoute, { page } ),
+			parse: false,
+		} )
+			.then( async ( response: Response ) => {
+				if ( ! response.json ) {
+					reject( response );
+				}
+
+				const venues = await response.json();
+
+				// Check that the results have been returned in an object.
+				if ( ! ( venues && typeof venues === 'object' ) ) {
+					reject( new Error( 'Venues fetch request did not return an object.' ) );
+				}
+
+				// Check that the `venues` property is an array.
+				if ( ! Array.isArray( venues ) ) {
+					reject( new Error( 'Venues fetch request did not return an object.' ) );
+				}
+
+				const total = response.headers.has( 'x-wp-total' ) ? response.headers.get( 'x-wp-total' ) : 0;
+
+				resolve( {
+					venues: venues.map( mapVenueResponse ),
+					total: total,
+				} as VenuesFetchResult );
+			} )
+			.catch( ( error ) => {
+				reject( new Error( `Failed to fetch venue ${ error.message }` ) );
+			} );
 	} );
-
-	// Check that the results have been returned in an object.
-	if ( ! ( results && typeof results === 'object' ) ) {
-		throw new Error( 'Venues fetch request did not return an object.' );
-	}
-
-	// Check that the object has a 'venues' property.
-	if ( ! ( results.hasOwnProperty( 'venues' ) && results.hasOwnProperty( 'total' ) ) ) {
-		throw new Error( 'Venues fetch request did not return an object with venues and total properties.' );
-	}
-
-	// Check that the `venues` property is an array.
-	if ( ! Array.isArray( ( results as { venues: any } ).venues ) ) {
-		throw new Error( 'Venues fetch request did not return an array.' );
-	}
-
-	const safeResults = results as {
-		venues: FetchedVenue[];
-		total: number;
-	};
-
-	return {
-		venues: safeResults.venues,
-		total: safeResults.total,
-	};
 };
 
 /**
@@ -74,46 +135,64 @@ export const fetchVenues = async ( page: number ): Promise< VenuesFetchResult > 
  * @throws {Error} Will throw an error if the fetch request fails.
  */
 export const upsertVenue = async ( venueData: VenueData ): Promise< number > => {
-	let fetchPromise: Promise< FetchedVenue >;
+	const isUpdate = Boolean( venueData.id && venueData.id > 0 );
 	const isCountryUs = venueData.countryCode === 'US';
+	const upsertParams: VenueUpsertRequest = {
+		title: venueData.name,
+		status: 'publish' as PostStatus,
+		address: venueData.address,
+		city: venueData.city,
+		country: venueData.country,
+		state_province: venueData.stateprovince,
+		state: isCountryUs ? venueData.stateprovince : '',
+		province: isCountryUs ? '' : venueData.stateprovince,
+		zip: venueData.zip,
+		phone: venueData.phone,
+		website: venueData.website,
+	};
 
-	if ( venueData.id ) {
-		// Updating an existing venue.
-		fetchPromise = apiFetch( {
-			path: `/tribe/events/v1/venues/${ venueData.id }`,
-			method: 'PUT',
-			data: {
-				venue: venueData.name,
-				address: venueData.address,
-				city: venueData.city,
-				country: venueData.country,
-				province: isCountryUs ? '' : venueData.stateprovince,
-				state: isCountryUs ? venueData.stateprovince : '',
-				zip: venueData.zip,
-				phone: venueData.phone,
-				website: venueData.website,
-			},
-		} );
-	} else {
-		// Creating a new venue.
-		fetchPromise = apiFetch( {
-			path: '/tribe/events/v1/venues',
-			method: 'POST',
-			data: {
-				status: 'publish',
-				venue: venueData.name,
-				address: venueData.address,
-				city: venueData.city,
-				country: venueData.country,
-				province: isCountryUs ? '' : venueData.stateprovince,
-				state: isCountryUs ? venueData.stateprovince : '',
-				zip: venueData.zip,
-				phone: venueData.phone,
-				website: venueData.website,
-			},
-		} );
-	}
+	return new Promise< number >( async ( resolve, reject ): Promise< void > => {
+		await fetch( {
+			path: `${ baseRoute }${ isUpdate ? `/${ venueData.id }` : '' }`,
+			method: isUpdate ? 'PUT' : 'POST',
+			data: upsertParams,
+		} )
+			.then( ( response: VenueResponse ) => {
+				if ( ! response || typeof response !== 'object' || ! response.id ) {
+					reject(
+						new Error(
+							`Venue ${ isUpdate ? 'update' : 'creation' } request did not return a valid venue object.`
+						)
+					);
+				} else {
+					resolve( response.id );
+				}
+			} )
+			.catch( ( error ) => {
+				reject( new Error( `Failed to ${ isUpdate ? 'update' : 'create' } venue: ${ error.message }` ) );
+			} );
+	} );
+};
 
-	const data = await fetchPromise;
-	return data.id;
+/**
+ * Maps a venue response from the API to the FetchedVenue type.
+ *
+ * @since TBD
+ *
+ * @param {VenueResponse} venue The venue data from the API response.
+ * @return {FetchedVenue} The mapped venue data.
+ */
+const mapVenueResponse = ( venue: VenueResponse ): FetchedVenue => {
+	return {
+		id: venue.id,
+		venue: venue.title.rendered,
+		address: venue.address,
+		city: venue.city,
+		country: venue.country,
+		province: venue.province,
+		state: venue.state,
+		zip: venue.zip,
+		phone: venue.phone,
+		website: venue.website,
+	};
 };
