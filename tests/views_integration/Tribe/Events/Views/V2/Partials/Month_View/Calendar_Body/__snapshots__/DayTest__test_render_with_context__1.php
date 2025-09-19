9a2a01d9637663379588a75cc676375de5411c15
Sami Dokus <sami2464@gmail.com>
2025-09-19 14:30:28 -0600

    update snapshot again

--- tests/views_integration/Tribe/Events/Views/V2/Partials/Month_View/Calendar_Body/__snapshots__/DayTest__test_render_with_context__1.php
+++ tests/views_integration/Tribe/Events/Views/V2/Partials/Month_View/Calendar_Body/__snapshots__/DayTest__test_render_with_context__1.php
@@ -261,7 +261,8 @@
 		href="http://tribe.tests/events/2019-07-01"
 		class="tribe-events-calendar-month__more-events-link tribe-common-h8 tribe-common-h--alt tribe-common-anchor-thin"
 		data-js="tribe-events-view-link"
-		aria-label="+ 2 More for July 1, 2019"
+		aria-label="
+			+ 2 More for July 1, 2019		"
 	>
 		+ 2 More	</a>
 </div>
