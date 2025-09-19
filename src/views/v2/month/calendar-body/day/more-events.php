b04a201bf1676375a8b46574b5ac3c90779d01e9
Sami Dokus <sami2464@gmail.com>
2025-09-19 14:15:13 -0600

    PHPCS corrections and snapshot update

--- src/views/v2/month/calendar-body/day/more-events.php
+++ src/views/v2/month/calendar-body/day/more-events.php
@@ -27,7 +27,7 @@ if ( empty( $more_events ) || empty( $more_url ) ) {
 }
 
 // Get the formatted date for screen reader text.
-$date_format = tribe_get_date_option( 'dateWithYearFormat', get_option( 'date_format' ) );
+$date_format    = tribe_get_date_option( 'dateWithYearFormat', get_option( 'date_format' ) );
 $formatted_date = date_i18n( $date_format, strtotime( $day_date ) );
 ?>
 
@@ -36,7 +36,10 @@ $formatted_date = date_i18n( $date_format, strtotime( $day_date ) );
 		href="<?php echo esc_url( $more_url ); ?>"
 		class="tribe-events-calendar-month__more-events-link tribe-common-h8 tribe-common-h--alt tribe-common-anchor-thin"
 		data-js="tribe-events-view-link"
-		aria-label="<?php echo esc_attr( sprintf( _n( '+ %d More for %s', '+ %d More for %s', $more_events, 'the-events-calendar' ), $more_events, $formatted_date ) ); ?>"
+		aria-label="<?php
+		/* translators: %1$d: number of events, %2$s: formatted date. */
+		echo esc_attr( sprintf( _n( '+ %1$d More for %2$s', '+ %1$d More for %2$s', $more_events, 'the-events-calendar' ), $more_events, $formatted_date ) );
+		?>"
 	>
 		<?php
 		 echo esc_html(
