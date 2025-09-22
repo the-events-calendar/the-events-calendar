03605a2e2ad505ce9889343a65bc47305303cc81
rafsuntaskin <rafsun.aiub@gmail.com>
2025-09-22 23:29:48 +0600

    add controller test

---
+++ tests/classy_integration/Legacy_Blocks/Controller_Test.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace TEC\Tests\Events\Classy\Legacy_Blocks;
+
+use TEC\Common\Tests\Provider\Controller_Test_Case;
+use TEC\Events\Classy\Legacy_Blocks\Controller;
+
+class Controller_Test extends Controller_Test_Case{
+	protected $controller_class = Controller::class;
+}
