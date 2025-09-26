341171d89e7709bd8372d589a237bc80262fae04
Luca Tumedei <luca@theaveragedev.com>
2025-09-26 17:04:17 +0200

    feat(Classy) add missing types

--- src/resources/packages/classy/types/Settings.d.ts
+++ src/resources/packages/classy/types/Settings.d.ts
@@ -1,7 +1,19 @@
-import { Settings } from '@tec/common/classy/types/LocalizedData';
+import {LocalizedData, Settings} from '@tec/common/classy/types/LocalizedData';
+import {Minutes} from "@tec/common/classy/types/Minutes";
+import {Hours} from "@tec/common/classy/types/Hours";
 
 export type TECSettings = {
 	venuesLimit: number;
 	disableContent?: boolean;
 	disableContentReason?: string;
 } & Settings;
+
+export type EndOfDayCutoff = {
+	hours: Hours;
+	minutes: Minutes;
+}
+
+export type TECLocalizedData = {
+	endOfDayCutoff: EndOfDayCutoff;
+	settings: TECSettings;
+} & LocalizedData;
