92d40a59e905d773abb4f821ddf705a89d05132a
rafsuntaskin <rafsun.aiub@gmail.com>
2025-10-15 12:58:51 +0600

    added dismiss button

--- src/admin-views/settings/upsells/recurrence-banner.php
+++ src/admin-views/settings/upsells/recurrence-banner.php
@@ -2,15 +2,32 @@
 /**
  * Events Calendar Pro upsell banner for recurrence.
  *
+ * @var string $slug The slug of the upsell banner.
+ * @var string $nonce The nonce of the upsell banner.
+ *
  * @since TBD
  */
 
 $main = Tribe__Events__Main::instance();
 ?>
-<tr>
+<tr
+	data-tec-conditional-content-dismiss-container
+	data-tec-conditional-content-dismiss-slug="<?php echo esc_attr( $slug ); ?>"
+	data-tec-conditional-content-dismiss-nonce="<?php echo esc_attr( $nonce ); ?>"
+>
 	<td colspan="2">
 		<div class="tec-settings-form">
 			<div class="tec-settings-infobox is-dismissible recurrence-upsell-banner">
+				<button
+					class="dismiss-button"
+					data-tec-conditional-content-dismiss-button
+					data-tec-conditional-content-dismiss-slug="<?php echo esc_attr( $slug ); ?>"
+					data-tec-conditional-content-dismiss-nonce="<?php echo esc_attr( $nonce ); ?>"
+					type="button"
+					aria-label="<?php esc_attr_e( 'Dismiss this notice.', 'the-events-calendar' ); ?>"
+				>
+					<span class="dashicons dashicons-no-alt" aria-hidden="true"></span>
+				</button>
 			<img
 				src="<?php echo esc_url( tribe_resource_url( 'icons/recurrence.svg', false, null, $main ) ); ?>"
 				class="tec-settings-infobox-logo"
