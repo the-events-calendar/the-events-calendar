83dc27f646c2b2c71cca96c1003adb9c7382d396
Brian Krane <redscar@gmail.com>
2025-10-15 15:42:31 -0400

    Small CR tweaks.

--- tests/integrations_plugin_wordpress_seo/Events/Integrations/Plugins/WordPress_SEO/Events_PaginationTest.php
+++ tests/integrations_plugin_wordpress_seo/Events/Integrations/Plugins/WordPress_SEO/Events_PaginationTest.php
@@ -3,6 +3,7 @@
 namespace TEC\Events\Integrations\Plugins\WordPress_SEO;
 
 use Codeception\TestCase\WPTestCase;
+use Generator;
 
 /**
  * Class Events_PaginationTest
@@ -33,10 +34,10 @@ class Events_PaginationTest extends WPTestCase {
 		remove_all_filters( 'wpseo_next_rel_link' );
 		remove_all_filters( 'wpseo_prev_rel_link' );
 		remove_all_actions( 'template_redirect' );
-		
+
 		// Reset uopz mocks.
 		uopz_unset_return( 'tribe_is_event_query' );
-		
+
 		// Reset global query state.
 		global $wp_the_query, $wp_query;
 		$wp_the_query = null;
@@ -75,9 +76,9 @@ class Events_PaginationTest extends WPTestCase {
 	 */
 	public function it_should_register_template_redirect_hook() {
 		global $wp_filter;
-		
+
 		$this->sut->register();
-		
+
 		$this->assertArrayHasKey( 'template_redirect', $wp_filter );
 		$this->assertEquals( 5, has_action( 'template_redirect', [ $this->sut, 'disable_yoast_pagination' ] ) );
 	}
@@ -87,24 +88,24 @@ class Events_PaginationTest extends WPTestCase {
 	 *
 	 * @since TBD
 	 *
-	 * @return \Generator
+	 * @return Generator
 	 */
-	public function pagination_filter_data_provider(): \Generator {
+	public function pagination_filter_data_provider(): Generator {
 		yield 'event query should disable pagination' => [
-			true,  // tribe_is_event_query return value
-			true,  // should filters be applied
+			true,
+			true,
 			'Event queries should disable Yoast pagination filters.',
 		];
 
 		yield 'non-event query should not disable pagination' => [
-			false, // tribe_is_event_query return value
-			false, // should filters be applied
+			false,
+			false,
 			'Non-event queries should not disable Yoast pagination filters.',
 		];
 
 		yield 'missing tribe_is_event_query function should not disable pagination' => [
-			null,  // tribe_is_event_query return value (function doesn't exist)
-			false, // should filters be applied
+			null,
+			false,
 			'Missing tribe_is_event_query function should not disable pagination.',
 		];
 	}
@@ -116,7 +117,6 @@ class Events_PaginationTest extends WPTestCase {
 	public function it_should_handle_pagination_filters_correctly( $tribe_is_event_query_return, $should_apply_filters, $message ) {
 		$this->setup_and_execute_pagination_disabler( $tribe_is_event_query_return );
 
-		// Check that the filters are applied or not applied as expected.
 		if ( $should_apply_filters ) {
 			$this->assertNotFalse( has_filter( 'wpseo_next_rel_link', '__return_false' ), $message );
 			$this->assertNotFalse( has_filter( 'wpseo_prev_rel_link', '__return_false' ), $message );
@@ -131,16 +131,13 @@ class Events_PaginationTest extends WPTestCase {
 	 */
 	public function it_should_register_hook_with_correct_priority() {
 		$this->sut->register();
-		
-		// Check that the hook is registered with the correct callback and priority.
+
 		$this->assertEquals( 5, has_action( 'template_redirect', [ $this->sut, 'disable_yoast_pagination' ] ) );
-		
-		// Verify the priority by checking the global filter array.
+
 		global $wp_filter;
 		$template_redirect_hooks = $wp_filter['template_redirect'] ?? null;
 		$this->assertNotNull( $template_redirect_hooks, 'template_redirect hook should be registered.' );
-		
-		// Check that our callback is registered at priority 5.
+
 		$callbacks = $template_redirect_hooks->callbacks ?? [];
 		$this->assertArrayHasKey( 5, $callbacks, 'template_redirect hook should be registered with priority 5.' );
 	}
@@ -150,18 +147,18 @@ class Events_PaginationTest extends WPTestCase {
 	 *
 	 * @since TBD
 	 *
-	 * @return \Generator
+	 * @return Generator
 	 */
-	public function filter_behavior_data_provider(): \Generator {
+	public function filter_behavior_data_provider(): Generator {
 		yield 'event query should return false for pagination filters' => [
-			true,  // tribe_is_event_query return value
-			false, // expected filter return value
+			true,
+			false,
 			'Event queries should return false for pagination filters.',
 		];
 
 		yield 'non-event query should return original values for pagination filters' => [
-			false, // tribe_is_event_query return value
-			'original', // expected filter return value (original values)
+			false,
+			'original',
 			'Non-event queries should return original values for pagination filters.',
 		];
 	}
@@ -173,7 +170,6 @@ class Events_PaginationTest extends WPTestCase {
 	public function it_should_handle_filter_behavior_correctly( $tribe_is_event_query_return, $expected_behavior, $message ) {
 		$this->setup_and_execute_pagination_disabler( $tribe_is_event_query_return );
 
-		// Test the actual filter behavior.
 		if ( $expected_behavior === false ) {
 			$this->assertFalse( apply_filters( 'wpseo_next_rel_link', 'http://example.com/page/2' ), $message );
 			$this->assertFalse( apply_filters( 'wpseo_prev_rel_link', 'http://example.com/page/1' ), $message );
