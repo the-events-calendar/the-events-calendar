cc99e6d39047c44eb059d1636af1681f0cc0e545
Sami Dokus <sami2464@gmail.com>
2025-11-19 16:00:46 -0700

    Replace placeholder with floating label

--- src/views/v2/components/events-bar/search/keyword.php
+++ src/views/v2/components/events-bar/search/keyword.php
@@ -9,25 +9,23 @@
  *
  * @link http://evnt.is/1aiy
  *
+ * @version TBD
+ * @since 5.3.0
+ * @since TBD Replace placeholder with floating label for improved accessibility.
+ *
  * @var array $bar The search bar contents.
- *
- * @version 5.3.0
- *
  */
 
 /* translators: %s: events (plural). */
-$placeholder = sprintf( __( 'Search for %s', 'the-events-calendar' ), tribe_get_event_label_plural_lowercase() );
+$aria_label = sprintf( __( 'Enter Keyword. Search for %s by Keyword.', 'the-events-calendar' ), tribe_get_event_label_plural_lowercase() );
 
 /* translators: %s: events (plural). */
-$aria_label = sprintf( __( 'Enter Keyword. Search for %s by Keyword.', 'the-events-calendar' ), tribe_get_event_label_plural_lowercase() );
+$label_text = sprintf( __( 'Search for %s', 'the-events-calendar' ), tribe_get_event_label_plural_lowercase() );
 ?>
 <div
 	class="tribe-common-form-control-text tribe-events-c-search__input-control tribe-events-c-search__input-control--keyword"
 	data-js="tribe-events-events-bar-input-control"
 >
-	<label class="tribe-common-form-control-text__label" for="tribe-events-events-bar-keyword">
-		<?php printf( esc_html__( 'Enter Keyword. Search for %s by Keyword.', 'the-events-calendar' ), tribe_get_event_label_plural() ); ?>
-	</label>
 	<input
 		class="tribe-common-form-control-text__input tribe-events-c-search__input"
 		data-js="tribe-events-events-bar-input-control-input"
@@ -35,8 +33,10 @@ $aria_label = sprintf( __( 'Enter Keyword. Search for %s by Keyword.', 'the-even
 		id="tribe-events-events-bar-keyword"
 		name="tribe-events-views[tribe-bar-search]"
 		value="<?php echo esc_attr( tribe_events_template_var( [ 'bar', 'keyword' ], '' ) ); ?>"
-		placeholder="<?php echo esc_attr( $placeholder ); ?>"
 		aria-label="<?php echo esc_attr( $aria_label ); ?>"
 	/>
+	<label class="tribe-common-form-control-text__label" for="tribe-events-events-bar-keyword">
+		<?php echo esc_html( $label_text ); ?>
+	</label>
 	<?php $this->template( 'components/icons/search', [ 'classes' => [ 'tribe-events-c-search__input-control-icon-svg' ] ] ); ?>
 </div>
