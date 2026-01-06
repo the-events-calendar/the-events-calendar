52f3123de57dfb668f12513a40aae0bf6a72816e
Sami Dokus <sami2464@gmail.com>
2026-01-06 09:34:38 -0700

    Revert "Revert changes for TEC-5913"
    This reverts commit ee0bf611d9ea344b98f169e5719c5b8789aa1e0a.

--- tests/views_integration/Tribe/Events/Views/V2/Partials/Components/__snapshots__/Content_TitleTest__test_content_title_filter__1.php
+++
@@ -1,3 +0,0 @@
-<?php return '<div class="tribe-events-header__content-title">
-	<h1 class="tribe-events-header__content-title-text tribe-common-h7 tribe-common-h3--min-medium tribe-common-h--alt">Filtered Custom Title</h1></div>
-';
