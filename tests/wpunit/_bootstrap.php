bd3e51c1c72495afe0c22f74270ce3bb87bdf1f1
Dimitrios Pantazis <dpanta94@gmail.com>
2026-04-07 18:36:23 +0300

    tests: update start of AI to make snapshots less prone to error

--- tests/wpunit/_bootstrap.php
+++ tests/wpunit/_bootstrap.php
@@ -1,8 +1,11 @@
 <?php
 // Here you can initialize variables that will be available to your tests
 use Codeception\Configuration;
+use TEC\Common\StellarWP\DB\DB;
 
 $support = Configuration::supportDir();
+
+DB::query( DB::prepare( 'ALTER TABLE %i AUTO_INCREMENT = 37694575', DB::prefix( 'posts' ) ) );
 
 include_once codecept_data_dir( 'classes/WP_Screen.php' );
 include_once  $support . 'utils.php';
