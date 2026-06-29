e8d9336a9b452951826e9879403f117e7e092fcb
Wayne Rocha <62760711+WayneRocha@users.noreply.github.com>
2026-06-29 15:14:45 -0300

    test: add PHP file with space in name for phpcs CI validation

---
+++ test phpcs file.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Test file with space in filename for phpcs CI validation.
+ *
+ * DO NOT MERGE - This file exists only to validate phpcs CI behavior with
+ * filenames that contain spaces (previously caused word-splitting bug).
+ *
+ * @package Test
+ */
