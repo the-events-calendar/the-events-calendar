f294cdb039f3c3f6f44af46dabfe2e83eef98f58
Wayne Rocha <62760711+WayneRocha@users.noreply.github.com>
2026-07-02 10:24:46 -0300

    test 3 - file clean

--- src/functions/views/provider.php
+++ src/functions/views/provider.php
@@ -39,9 +39,9 @@ function tribe_events_views_v2_is_enabled() {
 	 * @deprecated 6.0.0
 	 *
 	 * @param boolean $enabled Determining if V2 Views is enabled
-	 *    
+	 *
 	 */
-	apply_filters_deprecated('tribe_events_views_v2_is_enabled',[true],'6.0.0','No replacement. Legacy views have been removed.');     
+	apply_filters_deprecated( 'tribe_events_views_v2_is_enabled', [ true ] , '6.0.0', 'No replacement. Legacy views have been removed.' );
 
 	return true;
 }
