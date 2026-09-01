806084ae83d50ae151848e7015f315d2a9b335cd
Gustavo Bordoni <bordoni.dev@gmail.com>
2026-09-01 08:26:18 -0400

    style: add the blank line before the migration provider comment block

--- src/Events/Custom_Tables/V1/Provider.php
+++ src/Events/Custom_Tables/V1/Provider.php
@@ -91,6 +91,7 @@ class Provider extends Service_Provider {
 			$this->container->singleton( 'tec.custom-tables.v1.provider', self::class );
 			$this->container->register( Tables\Provider::class );
 			$this->container->register( Migration\Provider::class );
+
 			/*
 			 * The date-list migration strategy must be selectable WHILE the migration runs,
 			 * when the Recurrence feature Controller (gated on full activation) is inactive:
