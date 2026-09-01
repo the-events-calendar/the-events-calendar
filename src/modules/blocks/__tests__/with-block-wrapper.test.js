942ba416e62e804af467307cea99e72660c5aa6e
Filipe Coutinho <coutinhocouto@hotmail.com>
2026-09-01 08:54:20 -0300

    Update tests to use block API version 3

--- src/modules/blocks/__tests__/with-block-wrapper.test.js
+++ src/modules/blocks/__tests__/with-block-wrapper.test.js
@@ -1,4 +1,10 @@
 /**
+ * Tests for the Block API version adapter.
+ *
+ * @since TBD
+ */
+
+/**
  * External dependencies
  */
 import renderer from 'react-test-renderer';
@@ -8,6 +14,9 @@ import React from 'react';
  * Internal dependencies
  */
 import withBlockWrapper, { BLOCK_API_VERSION } from '@moderntribe/events/blocks/with-block-wrapper';
+
+/* Mirrors the className the `useBlockProps` mock in jest.setup.js returns. */
+const WRAPPER_CLASS_NAME = 'wp-block';
 
 const Edit = ( { title } ) => <span>{ title }</span>;
 
@@ -21,15 +30,19 @@ const blockDefinition = {
 };
 
 describe( 'Block wrapper', () => {
-	it( 'Should register at the Block API version the editor expects', () => {
-		expect( withBlockWrapper( blockDefinition ).apiVersion ).toBe( BLOCK_API_VERSION );
+	it( 'Should register at Block API version 3', () => {
+		/* 3 is the version WordPress 6.9 requires of a block that runs in the iframed editor. */
+		expect( BLOCK_API_VERSION ).toBe( 3 );
+		expect( withBlockWrapper( blockDefinition ).apiVersion ).toBe( 3 );
 	} );
 
 	it( 'Should leave the rest of the block definition untouched', () => {
-		const { edit, apiVersion, ...rest } = withBlockWrapper( blockDefinition );
-		const { edit: originalEdit, ...originalRest } = blockDefinition;
+		const wrapped = withBlockWrapper( blockDefinition );
+		const untouched = Object.keys( blockDefinition ).filter( ( key ) => key !== 'edit' );
 
-		expect( rest ).toEqual( originalRest );
+		untouched.forEach( ( key ) => {
+			expect( wrapped[ key ] ).toEqual( blockDefinition[ key ] );
+		} );
 	} );
 
 	it( 'Should render the block edit component inside the wrapper element', () => {
@@ -39,7 +52,7 @@ describe( 'Block wrapper', () => {
 		expect( component.root.findByType( Edit ).props.title ).toBe( blockDefinition.title );
 		expect( component.toJSON() ).toMatchObject( {
 			type: 'div',
-			props: { className: 'wp-block' },
+			props: { className: WRAPPER_CLASS_NAME },
 		} );
 	} );
 } );
