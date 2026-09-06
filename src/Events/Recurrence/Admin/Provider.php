25aa87744eaaead192b1eb2bbabbebef6d02fd4e
Gustavo Bordoni <bordoni.dev@gmail.com>
2026-09-06 18:07:52 -0400

    style: match WordPress badges and align the SVG lock

--- src/Events/Recurrence/Admin/Provider.php
+++ src/Events/Recurrence/Admin/Provider.php
@@ -270,7 +270,8 @@ class Provider extends Service_Provider {
 		if ( $data['locked'] ) {
 			$status     = $this->container->make( Pro_Status::class )->get( true );
 			$tooltip_id = 'tec-recurrence-lock-' . $post->ID;
-			echo ' <button type="button" class="tec-occurrence-admin__lock" aria-label="' . esc_attr__( 'Why recurrence editing is locked', 'the-events-calendar' ) . '" aria-describedby="' . esc_attr( $tooltip_id ) . '"><span class="dashicons dashicons-lock" aria-hidden="true"></span></button>';
+			// Use the WordPress @wordpress/icons lock SVG in the server-rendered list.
+			echo ' <button type="button" class="tec-occurrence-admin__lock" aria-label="' . esc_attr__( 'Why recurrence editing is locked', 'the-events-calendar' ) . '" aria-describedby="' . esc_attr( $tooltip_id ) . '"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="16" height="16" fill="currentColor" aria-hidden="true" focusable="false"><path d="M17 10h-1.2V7c0-2.1-1.7-3.8-3.8-3.8-2.1 0-3.8 1.7-3.8 3.8v3H7c-.6 0-1 .4-1 1v8c0 .6.4 1 1 1h10c.6 0 1-.4 1-1v-8c0-.6-.4-1-1-1zm-2.8 0H9.8V7c0-1.2 1-2.2 2.2-2.2s2.2 1 2.2 2.2v3z" /></svg></button>';
 			echo '<span class="tec-occurrence-admin__tooltip" role="tooltip" id="' . esc_attr( $tooltip_id ) . '">' . esc_html( $status['title'] . '. ' . $status['message'] ) . '</span>';
 		}
 		echo '</span>';
